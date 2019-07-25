d32719acacc40a768d85bdc8c8dd828657ba580e
Will McVay <wmcvay@reapit.com>
2019-07-25 14:45:50 +0100

    [CLOUD-61] Adds CORS support

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import router from './router'
 
 const expressApp = express()
+
+expressApp.use(cors())
 expressApp.use(bodyParser.urlencoded({ extended: true }))
 expressApp.use(bodyParser.json())
 expressApp.use('/api', router)
