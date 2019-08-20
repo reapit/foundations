287fa396501c7c82ea24e1acaf0d8b7c15f0774a
Dan Nguyen <45675930+DanND96@users.noreply.github.com>
2019-08-20 20:13:13 +0700

    [CLD-160][CLD-162] add Loader and Tabs (#5)

---
+++ src/components/Tabs/__tests__/index.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { Tabs, TabConfig } from '../index'
+import toJson from 'enzyme-to-json'
+
+const tabConfigs = ({ tab, handleChangeTab }: any): TabConfig[] => [
+  {
+    tabIdentifier: 'ITEMONE',
+    displayText: 'Item one',
+    onTabClick: handleChangeTab,
+    active: tab === 'ITEMONE'
+  },
+  {
+    tabIdentifier: 'ITEMTWO',
+    displayText: 'Item two',
+    onTabClick: handleChangeTab,
+    active: tab === 'ITEMTWO'
+  }
+]
+
+const props: Partial<any> = {
+  tab: 'ITEMONE',
+  handleChangeTab: jest.fn()
+}
+
+describe('Tabs', () => {
+  it('should match a snapshot', () => {
+    expect(toJson(shallow(<Tabs tabConfigs={tabConfigs(props)} />))).toMatchSnapshot()
+  })
+
+  it('simulates afterClose event', () => {
+    const wrapper = shallow(<Tabs tabConfigs={tabConfigs(props)} />)
+    wrapper
+      .find('a')
+      .first()
+      .simulate('click', {
+        preventDefault: jest.fn()
+      })
+    expect(props.handleChangeTab).toBeCalledTimes(1)
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+})
