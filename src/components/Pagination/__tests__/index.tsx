3f229650d7b4222299ab1735ed24b31fda150ad8
Thanh Pham <zlatan.pham@gmail.com>
2019-08-21 14:47:34 +0700

    * [CLD-161] add pagination (#10)
    * [CLD-161] add pagination
    
    * add behavior tests

---
+++ src/components/Pagination/__tests__/index.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import { shallow, mount } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import { Pagination, PaginationProps, generatePagination } from '..'
+
+const props: PaginationProps = {
+  pageNumber: 4,
+  pageSize: 10,
+  totalCount: 999,
+  onChange: jest.fn()
+}
+
+describe('Pagination render test', () => {
+  it('Should match a snapshot', () => {
+    expect(toJson(shallow(<Pagination {...props} />))).toMatchSnapshot()
+  })
+
+  it('Page 4 should be the current', () => {
+    const wrapper = shallow(<Pagination {...props} />)
+    expect(wrapper.find('[aria-label="Goto page 4"]').hasClass('is-current')).toBeTruthy()
+  })
+
+  it('onChange should only be called on not a is-current button', () => {
+    const wrapper = shallow(<Pagination {...props} />)
+    const event = { preventDefault: jest.fn() }
+    wrapper.find('[aria-label="Goto page 4"]').simulate('click', event)
+    wrapper.find('[aria-label="Goto page 2"]').simulate('click', event)
+    wrapper.find('.pagination-next').simulate('click', event)
+    wrapper.find('.pagination-previous').simulate('click', event)
+    expect(props.onChange).toHaveBeenCalledTimes(3)
+  })
+})
+
+const PaginatorBehaviorTest = () => {
+  const [pageNumber, setPageNumber] = useState(1)
+  return <Pagination pageNumber={pageNumber} onChange={setPageNumber} pageSize={10} totalCount={100} />
+}
+
+describe('Pagination behavior test', () => {
+  it('Previous/Next button should work as expected', () => {
+    const wrapper = mount(<PaginatorBehaviorTest />)
+    expect(wrapper.find('[aria-label="Goto page 1"]').hasClass('is-current')).toBeTruthy()
+    wrapper
+      .find('.pagination-next')
+      .simulate('click')
+      .simulate('click')
+    expect(wrapper.find('[aria-label="Goto page 3"]').hasClass('is-current')).toBeTruthy()
+    wrapper.find('.pagination-previous').simulate('click')
+    expect(wrapper.find('[aria-label="Goto page 2"]').hasClass('is-current')).toBeTruthy()
+  })
+
+  it('Paging button should work as expected', () => {
+    const wrapper = mount(<PaginatorBehaviorTest />)
+    expect(wrapper.find('[aria-label="Goto page 2"]').hasClass('is-current')).not.toBeTruthy()
+    wrapper.find('[aria-label="Goto page 2"]').simulate('click')
+    expect(wrapper.find('[aria-label="Goto page 2"]').hasClass('is-current')).toBeTruthy()
+  })
+})
+
+describe('generatePagination', () => {
+  it('Should always generate the first, last page & current page', () => {
+    const paginator = generatePagination(99, 9999)
+    expect(paginator[0]).toBe(1)
+    expect(paginator[paginator.length - 1]).toBe(9999)
+    expect(paginator).toContain(99)
+  })
+
+  it('Should generate a maximum of 9 items', () => {
+    expect(generatePagination(5, 9999)).toHaveLength(9)
+  })
+})
---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,104 @@
+import * as React from 'react'
+
+export interface PaginationProps {
+  pageNumber?: number
+  pageSize?: number
+  totalCount?: number
+  onChange?: (page: number) => void
+}
+
+export const generatePagination = (currentPage: number, pageCount: number) => {
+  const delta = 2
+
+  let range: (number | string)[] = []
+  for (let i = Math.max(2, currentPage - delta); i <= Math.min(pageCount - 1, currentPage + delta); i++) {
+    range.push(i)
+  }
+
+  if (currentPage - delta > 2) {
+    range.unshift('...')
+  }
+  if (currentPage + delta < pageCount - 1) {
+    range.push('...')
+  }
+
+  range.unshift(1)
+  range.push(pageCount)
+
+  return range
+}
+
+export const Pagination = ({ onChange, pageNumber = 1, pageSize = 1, totalCount = 1 }: PaginationProps) => {
+  const maxPage = Math.ceil(totalCount / pageSize)
+
+  if (maxPage < 2) {
+    return null
+  }
+
+  const paginator = generatePagination(pageNumber, maxPage)
+
+  return (
+    <div className="flex justify-center">
+      <nav className="pagination is-centered" role="navigation" aria-label="pagination">
+        {
+          // @ts-ignore: skip bulma link disabled complaint
+          <a
+            onClick={e => {
+              e.preventDefault()
+              if (pageNumber >= 2 && onChange) {
+                onChange(pageNumber - 1)
+              }
+            }}
+            className="pagination-previous"
+            disabled={pageNumber < 2}
+          >
+            Previous
+          </a>
+        }
+        {
+          // @ts-ignore: skip bulma link disabled complaint
+          <a
+            onClick={e => {
+              e.preventDefault()
+              if (pageNumber + 1 <= maxPage && onChange) {
+                onChange(pageNumber + 1)
+              }
+            }}
+            className="pagination-next"
+            disabled={pageNumber + 1 > maxPage}
+          >
+            Next page
+          </a>
+        }
+        <ul className="pagination-list">
+          {paginator.map((pg, i) => {
+            if (pg === '...') {
+              return (
+                <li key={i}>
+                  <span className="pagination-ellipsis has-text-grey">&hellip;</span>
+                </li>
+              )
+            }
+
+            return (
+              <li key={i}>
+                <a
+                  className={'pagination-link' + (pg === pageNumber ? ' is-current' : '')}
+                  onClick={e => {
+                    e.preventDefault()
+                    if (pg !== pageNumber && onChange) {
+                      onChange(pg as number)
+                    }
+                  }}
+                  aria-label={`Goto page ${pg}`}
+                >
+                  {pg}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+    </div>
+  )
+}
