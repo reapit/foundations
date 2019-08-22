b5190bd449105a2faeedb5e2921e2ba0c059b978
Vinh Nguyen <nhquocvinh@gmail.com>
2019-08-22 15:38:51 +0700

    [CLD-23] abstract away Cognito auth behaviours to element project (#14)
    * [CLD-23] abstract away Cognito auth behaviours to element project

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,11 +5,11 @@
 declare module '*.jpg'
 
 declare module '*.css' {
-  const classes: { [key: string]: string };
-  export default classes;
+  const classes: { [key: string]: string }
+  export default classes
 }
- 
+
 declare module '*.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
+  const classes: { [key: string]: string }
+  export default classes
 }
