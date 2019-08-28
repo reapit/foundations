f3221ce0b66ad9ae65c866799fcdd2febe60190b
Duong Pham <tanphamhaiduong@gmail.com>
2019-08-28 18:10:03 +0700

    Change appointment Tile to adapt with more cases Change appointment tile to adapt with more cases Move formik to dependencies to avoid cannot find formmik when import

--- src/components/AppointmentTile/appointmentTile.stories.tsx
+++ src/components/AppointmentTile/appointmentTile.stories.tsx
@@ -3,11 +3,10 @@ import { storiesOf } from '@storybook/react'
 import { AppointmentTile } from '.'
 
 storiesOf('AppointmentTile', module).add('Primary', () => (
-  <AppointmentTile
-    address="Address"
-    city="City"
-    country="Country"
-    postalCode="PostalCode"
-    contactPerson="contactPerson"
-  />
+  <AppointmentTile heading="Title">
+    <div>City</div>
+    <div>Country</div>
+    <div>PostalCode</div>
+    <div>contactPerson</div>
+  </AppointmentTile>
 ))
