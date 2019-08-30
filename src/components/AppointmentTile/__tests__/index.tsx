2cb1c0ec79633fd5c2111721c3b4ea226b857227
Dan Nguyen <dannd96.it@gmail.com>
2019-08-30 14:34:52 +0700

    [CLD-194] Add hightlight border appointment title

--- src/components/AppointmentTile/__tests__/index.tsx
+++ src/components/AppointmentTile/__tests__/index.tsx
@@ -4,9 +4,26 @@ import toJson from 'enzyme-to-json'
 import AppointmentTile from '../index'
 
 describe('AppointmentTile', () => {
-  it('should match snapshot', () => {
+  it('should match snapshot when HIGHTLIGHT false', () => {
     const mockProps = {
-      heading: 'mockHeading'
+      heading: 'mockHeading',
+      hightlight: false
+    }
+    const wrapper = shallow(
+      <AppointmentTile {...mockProps}>
+        <div>City</div>
+        <div>Country</div>
+        <div>PostalCode</div>
+        <div>"contactPerson"</div>
+      </AppointmentTile>
+    )
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('should match snapshot when HIGHTLIGHT true', () => {
+    const mockProps = {
+      heading: 'mockHeading',
+      hightlight: true
     }
     const wrapper = shallow(
       <AppointmentTile {...mockProps}>
