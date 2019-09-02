3979fc0cf932c52b4d9617b095f3c6144052a262
Duong Pham <tanphamhaiduong@gmail.com>
2019-09-02 13:15:56 +0700

    [CLD-102] Integrate appointment modal to redux Integrated appointment modal with redux and fetch api Add test

---
+++ src/components/container/view-detail-button.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Button } from '@reapit/elements'
+import { appointmentDetailRequestData } from '@/actions/appointment-detail'
+import { Dispatch } from 'redux'
+
+export type ViewDetailButtonProps = {
+  onClick: () => void
+}
+
+export const ViewDetailButton: React.FC<ViewDetailButtonProps> = ({ onClick }) => {
+  return (
+    <Button variant="primary" type="button" onClick={onClick}>
+      View Detail
+    </Button>
+  )
+}
+
+export type MapDisPatchToPropsType = {
+  dispatch: Dispatch
+  ownProps: { id: string | undefined }
+}
+
+export const handleOnClick = (dispatch, ownProps) => () => {
+  if (!ownProps || !ownProps.id) {
+    return null
+  }
+  dispatch(appointmentDetailRequestData({ id: ownProps.id }))
+}
+
+export const mapDispatchToProps = (
+  dispatch: Dispatch,
+  ownProps: { id: string | undefined }
+): ViewDetailButtonProps => ({
+  onClick: handleOnClick(dispatch, ownProps)
+})
+
+const CurrentLocButtonWithRedux = connect(
+  null,
+  mapDispatchToProps
+)(ViewDetailButton)
+
+CurrentLocButtonWithRedux.displayName = 'CurrentLocButtonWithRedux'
+
+export default CurrentLocButtonWithRedux
