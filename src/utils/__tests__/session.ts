519b900addb4933afb0a8cb1217090c27afe6f0c
Vinh Nguyen <nhquocvinh@gmail.com>
2019-09-06 15:00:00 +0700

    [CLD-228] prevent user in offline mode and update unit test

---
+++ src/utils/__tests__/session.ts
@@ -0,0 +1,117 @@
+import {
+  setLoginSession,
+  getLoginSession,
+  removeLoginSession,
+  getTokenFromQueryString,
+  verifyAccessToken
+} from '../session'
+import { LOCAL_STORAGE_SESSION_KEY } from '../../constants/session'
+import { mockLoginSession } from '../__mocks__/session'
+import { authLogout } from '@/actions/auth'
+
+import store from '@/core/store'
+import { LoginSession, RefreshParams } from '@reapit/elements'
+
+jest.mock('@reapit/elements')
+jest.mock('@/core/store', () => ({
+  dispatch: jest.fn(),
+  state: {
+    online: {},
+    auth: {}
+  }
+}))
+
+describe('session utils', () => {
+  describe('setLoginSession', () => {
+    it('should correctly set localStorage', () => {
+      jest.spyOn(window.localStorage, 'setItem')
+      setLoginSession(mockLoginSession)
+      expect(window.localStorage.setItem).toHaveBeenCalledWith(
+        LOCAL_STORAGE_SESSION_KEY,
+        JSON.stringify(mockLoginSession)
+      )
+    })
+  })
+
+  describe('getLoginSession', () => {
+    describe('getLoginSession', () => {
+      it('should correctly set localStorage', () => {
+        window.localStorage[LOCAL_STORAGE_SESSION_KEY] = mockLoginSession
+        jest.spyOn(window.localStorage, 'getItem')
+        const result = getLoginSession()
+        expect(window.localStorage.getItem).toHaveBeenCalledWith(LOCAL_STORAGE_SESSION_KEY)
+        expect(result).toEqual(mockLoginSession)
+      })
+    })
+  })
+
+  describe('removeLoginSession', () => {
+    it('should correctly set localStorage', () => {
+      window.localStorage[LOCAL_STORAGE_SESSION_KEY] = mockLoginSession
+      jest.spyOn(window.localStorage, 'removeItem')
+      removeLoginSession()
+      expect(window.localStorage.removeItem).toHaveBeenCalledWith(LOCAL_STORAGE_SESSION_KEY)
+      expect(window.localStorage.getItem(LOCAL_STORAGE_SESSION_KEY)).toBeUndefined()
+    })
+  })
+
+  describe('getTokenFromQueryString', () => {
+    it('should correctly return RefreshParams', () => {
+      const validQuery = '?username=wmcvay@reapit.com&desktopToken=TOKEN'
+      expect(getTokenFromQueryString(validQuery)).toEqual({
+        refreshToken: 'TOKEN',
+        userName: 'wmcvay@reapit.com',
+        loginType: 'CLIENT'
+      })
+    })
+
+    it('should correctly return null for an invalid string', () => {
+      const invalidQuery = '?somerandomquery=wmcvay@reapit.com&somerandomtoken=TOKEN'
+      expect(getTokenFromQueryString(invalidQuery)).toBe(null)
+    })
+  })
+
+  describe('verifyAccessToken', () => {
+    it('should correctly return null when app is not online', () => {
+      store.state.online.online = false
+
+      const result = verifyAccessToken()
+
+      expect(store.dispatch).toHaveBeenCalledWith(authLogout())
+    })
+
+    it('should correctly return null when sessions are not available', () => {
+      store.state.online.online = true
+      store.state.auth.loginSession = null
+      store.state.auth.desktopSession = null
+
+      const result = verifyAccessToken()
+
+      expect(store.dispatch).toHaveBeenCalledWith(authLogout())
+    })
+
+    it('should logout user when need to get refreshed session but it is not correct', () => {
+      store.state.online.online = true
+      store.state.auth.loginSession = null
+      store.state.auth.desktopSession = {} as RefreshParams
+
+      const result = verifyAccessToken()
+
+      expect(store.dispatch).toHaveBeenCalledTimes(0)
+    })
+
+    it('should correctly return value', () => {
+      store.state.online.online = true
+      store.state.auth.loginSession = {} as LoginSession
+      store.state.auth.desktopSession = {} as RefreshParams
+
+      const result = verifyAccessToken()
+
+      expect(store.dispatch).toHaveBeenCalledTimes(0)
+    })
+
+    afterEach(() => {
+      jest.resetAllMocks()
+    })
+  })
+})
