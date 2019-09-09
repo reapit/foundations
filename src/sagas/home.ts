9fc4ef45081158e2bab1949e72d02dc360b1e366
Huy Giang <gnhuy91@gmail.com>
2019-09-09 15:29:39 +0700

    initialize

---
+++ src/sagas/home.ts
@@ -0,0 +1,35 @@
+import { homeLoading, homeReceiveData, homeRequestDataFailure } from '../actions/home'
+import { put, fork, takeLatest, all } from '@redux-saga/core/effects'
+import ActionTypes from '../constants/action-types'
+import { errorThrownServer } from '../actions/error'
+import errorMessages from '../constants/error-messages'
+import { Action } from '@/types/core'
+
+export const homeDataFetch = function*() {
+  yield put(homeLoading(true))
+
+  try {
+    const response = yield true // Your fetch module here
+
+    yield put(homeReceiveData({ data: response }))
+  } catch (err) {
+    console.error(err.message)
+    yield put(homeRequestDataFailure())
+    yield put(
+      errorThrownServer({
+        type: 'SERVER',
+        message: errorMessages.DEFAULT_SERVER_ERROR
+      })
+    )
+  }
+}
+
+export const homeDataListen = function*() {
+  yield takeLatest<Action<number>>(ActionTypes.HOME_REQUEST_DATA, homeDataFetch)
+}
+
+const homeSagas = function*() {
+  yield all([fork(homeDataListen)])
+}
+
+export default homeSagas
---
+++ src/scripts/fetch-definitions.js
@@ -0,0 +1,62 @@
+require('dotenv').config({
+  path: '../src/constants/.env'
+})
+require('isomorphic-fetch')
+
+const fs = require('fs')
+const sw2dts = require('sw2dts')
+
+const BASE_URL = 'https://reapit.cloud.tyk.io'
+
+const apiSchema = [
+  {
+    definitionFile: `${__dirname}/../types/platform-api-schema.ts`,
+    endpoint: `${BASE_URL}/api/swagger/v1/swagger.json`,
+    headers: {
+      Authorisation: process.env.PLATFORM_API_KEY
+    }
+  },
+  {
+    definitionFile: `${__dirname}/../types/marketplace-api-schema.ts`,
+    endpoint: `${BASE_URL}/marketplace/swagger/v1/swagger.json`,
+    headers: {
+      Authorisation: process.env.MARKETPLACE_API_KEY
+    }
+  }
+]
+
+// Fetch definitions for a given schema
+const fetchDefinitionsForSchema = async schemaConfig => {
+    const { definitionFile, endpoint, headers } = schemaConfig
+    const response = await fetch(endpoint, {
+      headers
+    })
+
+    if (response.status < 400) {
+      const definitions = await response.json()
+
+      // Convert definitions to TypeScript interfaces
+      const convertedDefinitions = await sw2dts.convert(definitions, { withQuery: true })
+      console.log(`Successfully fetched type definitions for: ${endpoint}`)
+
+      // Write interfaces to file
+      fs.writeFile(definitionFile, convertedDefinitions, error => {
+        if (error) {
+          console.error(`Failed to write type definitions for: ${endpoint}`)
+          throw error
+        } else {
+          console.log(`Successfully wrote type definitions for: ${endpoint}`)
+        }
+      })
+    } else {
+      throw new Error(`Failed to fetch type definitions for: ${endpoint}`)
+    }
+  }
+
+  // Fetch definitions for all schemas
+;(async () => {
+  return Promise.all(apiSchema.map(fetchDefinitionsForSchema))
+})().catch(err => {
+  console.error(JSON.stringify(err))
+  process.exit(1)
+})
