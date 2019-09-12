f1b9fb617d19194ce5dc1a7eb5924e3d3877e523
Vinh Nguyen <nhquocvinh@gmail.com>
2019-09-12 14:15:49 +0700

    [CLD-119] apply authentication for AML (#7)

--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { Formik, Form } from 'formik'
+import { Dispatch } from 'redux'
 import { withRouter, RouteComponentProps } from 'react-router'
+import { ReduxState } from '@/types/core'
+import { authLogin } from '@/actions/auth'
+import { validate } from '@/utils/form/login'
+import Routes from '@/constants/routes'
+import { LOGIN_TYPE } from '@/constants/auth'
+import { Input, Button, LoginParams } from '@reapit/elements'
 
-export interface LoginMappedActions {}
+import loginStyles from '@/styles/pages/login.scss?mod'
+import bulma from '@/styles/vendor/bulma'
+import logoImage from '@/assets/images/reapit-graphic.jpg'
 
-export interface LoginMappedProps {}
+export interface LoginMappedActions {
+  login: (params: LoginParams) => void
+}
+
+export interface LoginMappedProps {
+  isLogin: boolean
+  error: boolean
+}
 
 export interface LoginFormValues {
   email: string
@@ -13,13 +31,87 @@ export interface LoginFormValues {
 
 export type LoginProps = LoginMappedActions & LoginMappedProps & RouteComponentProps
 
-export const Login: React.FunctionComponent<LoginProps> = () => {
-  return <h1>Login Page</h1>
+export const onSubmitHandler = (setIsSubmitting: any, login: any, values: LoginFormValues) => {
+  const { email, password } = values
+
+  setIsSubmitting(true)
+  login({ userName: email, password, loginType: LOGIN_TYPE.CLIENT } as LoginParams)
 }
 
-const mapStateToProps = (): LoginMappedProps => ({})
+export const Login: React.FunctionComponent<LoginProps> = (props: LoginProps) => {
+  const [isSubmitting, setIsSubmitting] = React.useState(false)
+  const { isLogin, error, login } = props
+  const { disabled, wrapper, container, image } = loginStyles
+  const { level, title, isH1, isCentered } = bulma
 
-const mapDispatchToProps = (): LoginMappedActions => ({})
+  React.useEffect(() => {
+    if (error) {
+      setIsSubmitting(false)
+    }
+  }, [error])
+
+  const LoginForm = (
+    <div className={container}>
+      <div className={`${wrapper} ${isSubmitting && disabled}`}>
+        <h1 className={`${title} ${isH1} ${isCentered}`}>Sign in</h1>
+        <p className="pb-8">Welcome to Reapit Online Check List</p>
+
+        <Formik
+          validate={validate}
+          initialValues={{ email: '', password: '' } as LoginFormValues}
+          onSubmit={values => onSubmitHandler(setIsSubmitting, login, values)}
+          render={() => (
+            <Form data-test="login-form">
+              <Input
+                dataTest="login-email"
+                type="text"
+                labelText="Email"
+                id="email"
+                name="email"
+                placeholder="name@address.com"
+              />
+              <Input
+                dataTest="login-password"
+                type="password"
+                labelText="Password"
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+              />
+
+              <div className={level}>
+                <Button type="submit" loading={isSubmitting} variant="primary" disabled={isSubmitting}>
+                  Login
+                </Button>
+              </div>
+
+              {error && '<Alert component here />'}
+            </Form>
+          )}
+        />
+      </div>
+
+      <div className={image}>
+        <img src={logoImage} alt="Reapit Graphic" />
+      </div>
+    </div>
+  )
+
+  if (isLogin) {
+    return <Redirect to={Routes.HOME} />
+  }
+
+  return LoginForm
+}
+
+const mapStateToProps = (state: ReduxState): LoginMappedProps => ({
+  isLogin: state.auth.isLogin,
+  error: state.auth.error
+})
+
+const mapDispatchToProps = (dispatch: Dispatch): LoginMappedActions => ({
+  login: (params: LoginParams) => dispatch(authLogin(params))
+})
 
 export default withRouter(
   connect(
---
+++ src/sagas/__tests__/auth.ts
@@ -0,0 +1,56 @@
+import authSagas, { doLogin, doLogout, loginListen, logoutListen } from '../auth'
+import ActionTypes from '../../constants/action-types'
+import { put, all, takeLatest } from '@redux-saga/core/effects'
+import { authLogoutSuccess } from '../../actions/auth'
+import { removeLoginSession } from '../../utils/session'
+import { history } from '../../core/router'
+import Routes from '../../constants/routes'
+
+jest.mock('../../utils/session')
+jest.mock('../../core/router', () => ({
+  history: {
+    push: jest.fn()
+  }
+}))
+
+describe('auth thunks', () => {
+  describe('authLogout', () => {
+    it('should redirect to login page', () => {
+      const gen = doLogout()
+      gen.next()
+      expect(removeLoginSession).toHaveBeenCalledTimes(1)
+      expect(gen.next().value).toEqual(put(authLogoutSuccess()))
+      gen.next()
+      expect(history.push).toHaveBeenCalledTimes(1)
+      expect(history.push).toHaveBeenLastCalledWith(Routes.LOGIN)
+      expect(gen.next().done).toBe(true)
+    })
+  })
+
+  describe('authLoginListen', () => {
+    it('should trigger login action', () => {
+      const gen = loginListen()
+
+      expect(gen.next().value).toEqual(takeLatest(ActionTypes.AUTH_LOGIN, doLogin))
+      expect(gen.next().done).toBe(true)
+    })
+  })
+
+  describe('authLogoutListen', () => {
+    it('should trigger logout action', () => {
+      const gen = logoutListen()
+
+      expect(gen.next().value).toEqual(takeLatest(ActionTypes.AUTH_LOGOUT, doLogout))
+      expect(gen.next().done).toBe(true)
+    })
+  })
+
+  describe('itemSagas', () => {
+    it('should wait for login and logout action get called', () => {
+      const gen = authSagas()
+
+      expect(gen.next().value).toEqual(all([loginListen(), logoutListen()]))
+      expect(gen.next().done).toBe(true)
+    })
+  })
+})
