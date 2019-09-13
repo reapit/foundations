cf449a085dc3bfc7117eb636599fdc7672cab8ea
Dan Nguyen <dannd96.it@gmail.com>
2019-09-13 12:45:56 +0700

    Fix review

--- src/components/pages/client-search.tsx
+++ src/components/pages/client-search.tsx
@@ -1,22 +1,25 @@
 import * as React from 'react'
 import { withRouter, RouteComponentProps } from 'react-router'
-import { Button } from '@reapit/elements'
+import { Button, Input, SelectBox } from '@reapit/elements'
 import ErrorBoundary from '@/components/hocs/error-boundary'
 import searchStyle from '@/styles/pages/client-search.scss?mod'
-import { SearchParams } from '@/actions/result'
+import { Formik, Form } from 'formik'
 
 export type ClientSearchProps = RouteComponentProps<{ page?: any }>
 
 const { searchContainer } = searchStyle
-const identityCheckType = ['PASS', 'FAIL', 'PENDING', 'CANCELLED', 'WARNINGS', 'UNCHECKED']
+
+const identityCheckList = [
+  { label: 'Please select', value: '' },
+  { label: 'Pass', value: 'PASS' },
+  { label: 'Fail', value: 'FAIL' },
+  { label: 'Pending', value: 'PENDING' },
+  { label: 'Cancelled', value: 'CANCELLED' },
+  { label: 'Warnings', value: 'WARNINGS' },
+  { label: 'Unchecked', value: 'UNCHECKED' }
+]
 
 export const ClientSearch: React.FunctionComponent<ClientSearchProps> = ({ history }) => {
-  const [values, setValues] = React.useState<SearchParams>({ name: '', address: '', identityCheck: 'PASS' })
-
-  const handleChange = evt => {
-    setValues({ ...values, [evt.target.name]: evt.target.value })
-  }
-
   const searchContacts = value => {
     history.push('/result', value)
   }
@@ -25,59 +28,48 @@ export const ClientSearch: React.FunctionComponent<ClientSearchProps> = ({ histo
     <ErrorBoundary>
       <div className={searchContainer}>
         <h2 className="title is-2">Client Search</h2>
-        <div className="field">
-          <label className="label">Enter Name</label>
-          <div className="control flex">
-            <input
-              className="input mr-4"
-              type="text"
-              name="name"
-              placeholder="First or last name"
-              value={values.name}
-              onChange={handleChange}
-            />
-            <Button type="button" variant="primary" onClick={() => searchContacts({ name: values.name })}>
-              Search
-            </Button>
-          </div>
-        </div>
-        <div className="field">
-          <label className="label">Enter Address</label>
-          <div className="control flex">
-            <input
-              className="input mr-4"
-              type="text"
-              name="address"
-              placeholder="Streetname, Town or postcode"
-              value={values.address}
-              onChange={handleChange}
-            />
-            <Button type="button" variant="primary" onClick={() => searchContacts({ address: values.address })}>
-              Search
-            </Button>
-          </div>
-        </div>
-        <div className="field">
-          <label className="label">Enter Address</label>
-          <div className="control">
-            <div className="select is-primary mr-4">
-              <select name="identityCheck" value={values.identityCheck} onChange={handleChange}>
-                {identityCheckType.map(item => (
-                  <option key={item} value={item}>
-                    {item}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <Button
-              type="button"
-              variant="primary"
-              onClick={() => searchContacts({ identityCheck: values.identityCheck })}
-            >
-              Search
-            </Button>
-          </div>
-        </div>
+        <Formik
+          initialValues={{ name: '' }}
+          onSubmit={values => searchContacts(values)}
+          render={() => (
+            <Form className="mb-8">
+              <Input id="name" type="text" placeholder="First or last name" name="name" labelText="Enter name" />
+              <Button type="submit" variant="primary">
+                Search
+              </Button>
+            </Form>
+          )}
+        />
+        <Formik
+          initialValues={{ address: '' }}
+          onSubmit={values => searchContacts(values)}
+          render={() => (
+            <Form className="mb-8">
+              <Input
+                id="address"
+                type="text"
+                placeholder="Streetname, Town or postcode"
+                name="address"
+                labelText="Enter address"
+              />
+              <Button type="submit" variant="primary">
+                Search
+              </Button>
+            </Form>
+          )}
+        />
+        <Formik
+          initialValues={{ identityCheck: '' }}
+          onSubmit={values => searchContacts(values)}
+          render={() => (
+            <Form>
+              <SelectBox id="identityCheck" name="identityCheck" labelText="ID Status" options={identityCheckList} />
+              <Button type="submit" variant="primary">
+                Search
+              </Button>
+            </Form>
+          )}
+        />
       </div>
     </ErrorBoundary>
   )
