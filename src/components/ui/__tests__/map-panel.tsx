c87e2dabddffaafc9655846c36967980767dfd1c
Pham Hai Duong <tanphamhaiduong@gmail.com>
2019-09-25 15:03:19 +0700

    [CLD-273] show all direction for desktop mode (#36)
    Add feature show all direction for desktop mode
    Add test
    Modify test

--- src/components/ui/__tests__/map-panel.tsx
+++ src/components/ui/__tests__/map-panel.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import { MapPanel, MapPanelProps } from '../map-panel'
+import { MapPanel, MapPanelProps, mapStateToProps, mapDispatchToProps } from '../map-panel'
 import toJson from 'enzyme-to-json'
+import { ReduxState } from '@/types/core'
 
 describe('MapPanel', () => {
   it('should match a snapshot when valid LAT, LONG', () => {
@@ -9,7 +10,9 @@ describe('MapPanel', () => {
       duration: '15 mins',
       distance: '3 miles',
       currentLocation: { lat: 52.522905940278065, lng: -1.241455078125 },
-      destination: { lat: 52.158215, lng: -0.433459 }
+      destination: { lat: 52.158215, lng: -0.433459 },
+      filterType: 'Today',
+      showAllAppointment: jest.fn()
     }
     expect(toJson(shallow(<MapPanel {...props} />))).toMatchSnapshot()
   })
@@ -19,8 +22,43 @@ describe('MapPanel', () => {
       duration: '15 mins',
       distance: '3 miles',
       currentLocation: { lat: 52.522905940278065, lng: -1.241455078125 },
-      destination: { lat: undefined, lng: undefined }
+      destination: { lat: undefined, lng: undefined },
+      filterType: 'Today',
+      showAllAppointment: jest.fn()
     }
     expect(toJson(shallow(<MapPanel {...props} />))).toMatchSnapshot()
+  })
+
+  describe('mapStateToProps', () => {
+    it('should return correctly', () => {
+      const state = {
+        appointments: {
+          time: 'Today'
+        }
+      } as ReduxState
+      const expected = {
+        filterType: 'Today'
+      }
+      const result = mapStateToProps(state)
+      expect(result).toEqual(expected)
+    })
+
+    it('should return when undefined', () => {
+      const state = {} as ReduxState
+      const expected = {
+        filterType: 'Today'
+      }
+      const result = mapStateToProps(state)
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should run correctly', () => {
+      const mockDispatch = jest.fn()
+      const { showAllAppointment } = mapDispatchToProps(mockDispatch)
+      showAllAppointment()
+      expect(mockDispatch).toBeCalled()
+    })
   })
 })
