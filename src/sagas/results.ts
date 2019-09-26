a0b8462a3903117218fea293c59c03b93c865701
dannd <dannd96.it@gmail.com>
2019-09-26 17:22:59 +0700

    Fix test results

--- src/sagas/results.ts
+++ src/sagas/results.ts
@@ -4,19 +4,19 @@ import { errorThrownServer } from '../actions/error'
 import errorMessages from '../constants/error-messages'
 import { Action } from '@/types/core'
 import { fetcher, setQueryParams } from '@reapit/elements'
-import { URLS, REAPIT_API_BASE_URL, CONTACTS_HEADERS } from '@/constants/api'
+import { URLS, REAPIT_API_BASE_URL } from '@/constants/api'
 import { resultReceiveData, resultRequestDataFailure, ContactsParams } from '@/actions/results'
 import { CONTACTS_PER_PAGE } from '@/constants/paginator'
-// import { initAuthorizedRequestHeaders } from '@/utils/api'
+import { initAuthorizedRequestHeaders } from '@/utils/api'
 
 export const resultFetch = function*(params: Action<ContactsParams>) {
   try {
-    // const headers = yield call(initAuthorizedRequestHeaders)
+    const headers = yield call(initAuthorizedRequestHeaders)
     const response = yield call(fetcher, {
       url: `${URLS.contacts}/?${setQueryParams({ ...params.data, pageSize: CONTACTS_PER_PAGE })}`,
       api: REAPIT_API_BASE_URL,
       method: 'GET',
-      headers: CONTACTS_HEADERS
+      headers: headers
     })
     yield put(resultReceiveData(response))
   } catch (err) {
