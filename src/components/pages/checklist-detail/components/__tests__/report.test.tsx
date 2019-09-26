c5a8bf0cf7eb75d244bb57790f0a413d34f761bd
Pham Hai Duong <tanphamhaiduong@gmail.com>
2019-09-26 14:14:59 +0700

    [CLD-284] Mock UI for report component (#19)
    Add mock UI for report component
    Add test

---
+++ src/components/pages/checklist-detail/components/__tests__/report.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { ReportContainer, handleContent, handleTrigger, Report } from '../report'
+import { contact } from '@/sagas/__stubs__/contact'
+
+describe('ReportContainer', () => {
+  it('should match snapshot', () => {
+    const wrapper = shallow(<ReportContainer contact={contact} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('handleContent', () => {
+    const mockRef = {
+      current: 'mockRef'
+    }
+    const fn = handleContent({ printRef: mockRef })
+    const result = fn()
+    expect(result).toEqual('mockRef')
+  })
+
+  it('handleTrigger', () => {
+    const component = handleTrigger()
+    const wrapper = shallow(<div>{component}</div>)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('Report', () => {
+    const wrapper = shallow(<Report />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
