e7aeaf6f617e2b831bda16fb065b774f941a36ca
NghiaPham <nghiapn@dwarvesv.com>
2019-10-02 16:06:36 +0700

    Feature/cld 278 update date picker component (#74)

--- src/components/DatePicker/__tests__/index.tsx
+++ src/components/DatePicker/__tests__/index.tsx
@@ -4,13 +4,47 @@ import { DatePicker, DatePickerProps } from '../index'
 import { Formik, Form } from 'formik'
 import toJson from 'enzyme-to-json'
 
+jest.unmock('dayjs')
+
 const props: DatePickerProps = {
   name: 'abc',
   labelText: 'abc',
   id: '123'
 }
 
-describe('Checkbox', () => {
+describe('Date-time picker', () => {
+  it('should map value correctly to text box', done => {
+    const wrapper = mount(
+      <Formik
+        initialValues={{ test: '11/20/1997' }}
+        onSubmit={jest.fn()}
+        render={() => {
+          return (
+            <Form>
+              <DatePicker name="test" id="test" labelText="test" />
+            </Form>
+          )
+        }}
+      />
+    )
+
+    const input = wrapper.find('input')
+    expect(input.props().value).toBe('11/20/1997')
+    input.simulate('change', {
+      target: {
+        value: '1'
+      }
+    })
+
+    // onChange
+    setTimeout(() => {
+      // Element found is just a snapshoot. Have to get a new snapshoot again.
+      const input = wrapper.find('input')
+      expect(input.props().value).toBe('1_/__/____')
+      done()
+    }, 1)
+  })
+
   it('should match a snapshot', () => {
     expect(toJson(shallow(<DatePicker {...props} />))).toMatchSnapshot()
   })
@@ -28,9 +62,5 @@ describe('Checkbox', () => {
       />
     )
     expect(wrapper.find('label')).toHaveLength(1)
-  })
-
-  afterEach(() => {
-    jest.resetAllMocks()
   })
 })
--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,11 +1,144 @@
 import * as React from 'react'
 import { Field } from 'formik'
 import ReactDatePicker from 'react-datepicker'
+import dayjs from 'dayjs'
+
+const { useState, useEffect } = React
 
 export interface DatePickerProps {
   name: string
   id: string
   labelText: string
+}
+
+const Input = ({
+  onChange,
+  value,
+  id,
+  onClick,
+  className
+}: {
+  onChange?: any
+  placeholder?: any
+  value?: any
+  id?: any
+  onClick?: any
+  className: string
+}) => {
+  const [inputValue, setInputValue] = useState(value)
+
+  // sync with the real value
+  useEffect(() => {
+    setInputValue(value)
+  }, [value])
+
+  const [isBackspaceKeyPressed, setIsBackspaceKeyPressed] = useState(false)
+
+  // COOKED https://stackoverflow.com/questions/12578507/implement-an-input-with-a-mask
+  const _onChange = e => {
+    const inputElement = e.target
+    const mask = '__/__/____'
+
+    if (!inputElement) {
+      return
+    }
+
+    const castedInputElement = inputElement as HTMLInputElement
+
+    let oldStart = castedInputElement.selectionStart
+    const textFieldValue = e.target.value
+
+    const splitTextFieldValue = textFieldValue.split('').filter(char => char !== '/')
+
+    const transformedTextFieldValue = mask
+      .split('')
+      .map(maskChar => {
+        if (maskChar !== '_') {
+          return maskChar
+        }
+        if (textFieldValue.length === 0) return maskChar
+
+        const extractedSplitTextFieldValue = splitTextFieldValue.shift()
+        if (!extractedSplitTextFieldValue) {
+          return maskChar
+        }
+        return extractedSplitTextFieldValue
+      })
+      .join('')
+
+    setInputValue(transformedTextFieldValue)
+
+    const parsedTransformedTextFieldValue = dayjs(transformedTextFieldValue)
+    if (parsedTransformedTextFieldValue.isValid()) {
+      onChange({
+        target: {
+          value: transformedTextFieldValue
+        }
+      })
+    }
+
+    // set state ^ affect asynchronous. event loop thing...
+    setTimeout(() => {
+      if (oldStart) {
+        /**
+         * handle
+         * "11<cusor>/" -> press 1 -> "11/<cursor>1"
+         */
+        if (!isBackspaceKeyPressed && transformedTextFieldValue[oldStart - 1] === '/') {
+          castedInputElement.selectionStart = oldStart + 1
+          castedInputElement.selectionEnd = oldStart + 1
+          return
+        }
+
+        /**
+         * Reset the flag if it happened
+         */
+        if (isBackspaceKeyPressed) {
+          setIsBackspaceKeyPressed(false)
+        }
+      }
+
+      castedInputElement.selectionStart = oldStart
+      castedInputElement.selectionEnd = oldStart
+    }, 1)
+  }
+
+  const onKeyUp = e => {
+    const inputChar = String.fromCharCode(e.keyCode)
+
+    // Allow: cmd + anything, ctrl + anything (copy, paste, select all... etc)
+    if (e.metaKey || e.ctrlKey) {
+      return
+    }
+
+    // prevent user from input number
+    if (![8, 37, 38, 39, 40].includes(e.keyCode) && !inputChar.match(/[0-9]/)) {
+      e.preventDefault()
+    }
+
+    // flag to checked if backspace key was pressed
+    if (e.keyCode === 8) {
+      setIsBackspaceKeyPressed(true)
+    }
+  }
+
+  const onBlur = () => {
+    setInputValue(value)
+  }
+
+  return (
+    <input
+      type="input"
+      className={className}
+      placeholder="__/__/____"
+      value={inputValue}
+      id={id}
+      onChange={_onChange}
+      onKeyDown={onKeyUp}
+      onBlur={onBlur}
+      onClick={onClick}
+    />
+  )
 }
 
 export const DatePicker = ({ name, id, labelText }: DatePickerProps) => {
@@ -32,6 +165,7 @@ export const DatePicker = ({ name, id, labelText }: DatePickerProps) => {
                 onChange={value => {
                   setFieldValue(name, value)
                 }}
+                customInput={<Input className={className} />}
               />
               {hasError && (
                 <div className="has-text-danger" data-test="input-error">
