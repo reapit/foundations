0fb0d982249f10b27530b162a391efeaf113cf08
Will McVay <willmcvay@pm.me>
2019-10-04 16:01:54 +0100

    Feature/cld 289 new nav (#80)
    * [CLD-289] Creates new nav and tests
    
    * v0.3.0
    
    * v0.3.2
    
    * [CLD-289] New nav ready to PR

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,131 +1,68 @@
 import * as React from 'react'
 import { RouteProps } from 'react-router'
 import { Link } from 'react-router-dom'
-import { Toggle } from './Toggle'
-import { Caret } from './Caret'
 import { Location } from 'history'
+import { LoginMode } from '../../utils/cognito/types'
 
 export interface MenuConfig extends RouteProps {
-  title: string
-  logo: React.ReactNode
-  homeUrl: string
-  defaultActiveKey?: string
+  defaultActiveKey: string
+  mode: LoginMode
   menu: MenuItem[]
-  isResponsive?: boolean
 }
 
 export interface MenuItem {
-  title: string
+  type: 'PRIMARY' | 'SECONDARY' | 'LOGO'
+  title?: string
   key: string
+  icon: React.ReactNode
+  url?: string
   callback?: () => void
-  toUrl?: string
-  subMenu?: MenuItem[]
 }
 
-export const isActiveSubmenu = (pathname: string, itemUrl: string) => {
-  const isValidParams = !!pathname && !!itemUrl
-  if (!isValidParams) {
-    return false
-  }
-  return pathname === itemUrl
-}
-
-export const renderLink = (subItem: MenuItem) => {
-  if (subItem.toUrl) {
-    return <Link to={subItem.toUrl}>{subItem.title}</Link>
+export const getActiveItemKey = (menu: MenuItem[], location?: Location<any>): string | null => {
+  if (location && location.pathname) {
+    const activeItem = menu.find(item => location.pathname === item.url)
+    if (activeItem && activeItem.url) {
+      return activeItem.key
+    }
   }
   return null
 }
 
-export const hrefCallBack = (callback: Function) => (e: React.SyntheticEvent) => {
-  e.preventDefault()
-  callback()
-}
-
-export const renderHref = (subItem: MenuItem) => {
-  if (subItem.callback) {
-    return (
-      <a data-test="logout-cta" onClick={hrefCallBack(subItem.callback)}>
-        Logout
-      </a>
-    )
-  }
-  return null
-}
-
-export const renderSubItem = (items: MenuItem[] | undefined, location: Location) => {
-  if (!items) {
-    return null
-  }
-  return items.map((subItem: MenuItem) => {
-    return (
-      <li
-        data-test="sub-menu-label"
-        key={subItem.key}
-        className={isActiveSubmenu(location.pathname, subItem.toUrl || '') ? 'is-active' : ''}
-      >
-        {subItem.toUrl ? renderLink(subItem) : renderHref(subItem)}
-      </li>
-    )
-  })
-}
-
-export const handleSetToggleMenu = (setToggleMenu: (isMenuOpen: boolean) => void, isMenuOpen: boolean) => () =>
-  setToggleMenu(!isMenuOpen)
-
-export const handleSetCaretToggle = (setCaretToggleKey: (key: string) => void, item: MenuItem) => () =>
-  setCaretToggleKey(item.key)
-
-export const Menu: React.FC<MenuConfig> = ({
-  title,
-  homeUrl,
-  logo,
-  menu,
-  location,
-  defaultActiveKey,
-  isResponsive = true
-}) => {
-  const [isMenuOpen, setToggleMenu] = React.useState(false)
-  const [caretToggleKey, setCaretToggleKey] = React.useState(defaultActiveKey)
-  return (
-    <aside className={`menu ${isResponsive ? 'is-responsive' : ''}`}>
-      <Link className={`logo ${isResponsive ? 'is-responsive' : ''}`} to={homeUrl}>
-        {logo}
-        <h5 className="title is-5">{title}</h5>
-      </Link>
-      <div className="toggle-container">
-        <Toggle
-          data-test="toogle-btn"
-          onChange={handleSetToggleMenu(setToggleMenu, isMenuOpen)}
-          isChecked={isMenuOpen}
-          isResponsive={isResponsive}
-        />
-      </div>
-      <div
-        data-test="menu-bar"
-        className={`menu-bar ${isMenuOpen ? 'menu-is-open' : ''} ${isResponsive ? 'is-responsive' : ''}`}
-      >
-        {menu.map((item: MenuItem) => {
-          return (
-            <React.Fragment key={item.key}>
-              <p
-                data-test="menu-label"
-                className="menu-label sub-menu-label"
-                onClick={handleSetCaretToggle(setCaretToggleKey, item)}
-              >
-                <Caret data-test="menu-caret" isActive={caretToggleKey === item.key} />
-                {item.title}
-              </p>
-              <ul
-                data-test="menu-item"
-                className={`menu-list sub-menu ${caretToggleKey === item.key ? 'menu-is-open' : ''}`}
-              >
-                {renderSubItem(item.subMenu, location as Location)}
-              </ul>
-            </React.Fragment>
-          )
-        })}
-      </div>
-    </aside>
+export const LinkItem: React.SFC<{ item: MenuItem; children: React.ReactNode }> = ({ item, children }) =>
+  item.url ? (
+    <Link className="nav-item-link" to={item.url}>
+      {children}
+    </Link>
+  ) : item.callback ? (
+    <a className="nav-item-link" onClick={item.callback}>
+      {children}
+    </a>
+  ) : (
+    <span className="nav-item-link">{children}</span>
   )
-}
+
+export const Menu: React.FC<MenuConfig> = React.memo(({ menu, location, mode, defaultActiveKey }) => {
+  const activeItem = getActiveItemKey(menu, location)
+  const [activeKey, setIsActive] = React.useState(activeItem || defaultActiveKey)
+  return (
+    <nav className={`nav-bar ${mode === 'DESKTOP' ? 'is-desktop' : ''}`}>
+      <ul>
+        {menu.map((item: MenuItem) => (
+          <li
+            className={`nav-item ${activeKey === item.key ? 'is-active' : ''} ${
+              item.type === 'LOGO' ? 'is-logo' : item.type === 'SECONDARY' ? 'is-secondary' : 'is-primary'
+            }`}
+            key={item.key}
+            onClick={() => item.type !== 'LOGO' && setIsActive(item.key)}
+          >
+            <LinkItem item={item}>
+              <div>{item.icon}</div>
+              {item.title && <div className="nav-item-title">{item.title}</div>}
+            </LinkItem>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+})
--- src/components/Menu/__tests__/Menu.tsx
+++ src/components/Menu/__tests__/Menu.tsx
@@ -1,297 +1,64 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
-import {
-  Menu,
-  MenuConfig,
-  isActiveSubmenu,
-  renderLink,
-  hrefCallBack,
-  MenuItem,
-  renderHref,
-  renderSubItem,
-  handleSetToggleMenu,
-  handleSetCaretToggle
-} from '../Menu'
-import { getMockRouterProps } from '../../../helpers/mock-router'
+import { Menu, getActiveItemKey, LinkItem } from '../Menu'
+import { Location } from 'history'
+import { mockMenuProps } from '../__mocks__/menu-props'
 
 describe('Menu', () => {
-  it('should render correctly', () => {
-    const mockLocation = getMockRouterProps({}).location
-    const mockProps = {
-      title: 'mockTitle',
-      homeUrl: '/mockHomeUrl',
-      logo: <div></div>,
-      menu: [
-        {
-          title: 'mockMenu',
-          key: '/mockMenu',
-          toUrl: '/mockMenu',
-          subMenu: [
-            {
-              title: '/mockSubmenu',
-              key: '/mockSubmenu',
-              callback: () => {
-                return null
-              }
-            }
-          ]
-        }
-      ],
-      location: mockLocation,
-      defaultActiveKey: '/mockSubmenu'
-    } as MenuConfig
-    const wrapper = shallow(<Menu {...mockProps} />)
+  it('should render menu correctly', () => {
+    const wrapper = shallow(<Menu {...mockMenuProps} />)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  it('should show logo', () => {
-    const mockLocation = getMockRouterProps({}).location
-    const mockProps = {
-      title: 'mockTitle',
-      homeUrl: '/mockHomeUrl',
-      logo: <div data-test="logo">Logo</div>,
-      menu: [
-        {
-          title: 'mockMenu',
-          key: '/mockMenu',
-          toUrl: '/mockMenu',
-          subMenu: [
-            {
-              title: '/mockSubmenu',
-              key: '/mockSubmenu',
-              toUrl: '/mockSubmenu'
-            }
-          ]
-        }
-      ],
-      location: mockLocation,
-      defaultActiveKey: '/mockSubmenu'
-    } as MenuConfig
-    const wrapper = shallow(<Menu {...mockProps} />)
-    const logo = wrapper.find('[data-test="logo"]')
-    expect(logo).toHaveLength(1)
+  it('should render link item logo correctly', () => {
+    const item = mockMenuProps.menu[0]
+    const children = (
+      <>
+        <div className="nav-item-icon">{item.icon}</div>
+        {item.title && <div className="nav-item-title">{item.title}</div>}
+      </>
+    )
+    const wrapper = shallow(<LinkItem item={item} children={children} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  it('should show menu', () => {
-    const mockLocation = getMockRouterProps({}).location
-    const mockProps = {
-      title: 'mockTitle',
-      homeUrl: '/mockHomeUrl',
-      logo: <div data-test="logo">Logo</div>,
-      menu: [
-        {
-          title: 'mockMenu',
-          key: '/mockMenu',
-          toUrl: '/mockMenu',
-          subMenu: [
-            {
-              title: '/mockSubmenu',
-              key: '/mockSubmenu',
-              toUrl: '/mockSubmenu'
-            }
-          ]
-        }
-      ],
-      location: mockLocation,
-      defaultActiveKey: '/mockSubmenu'
-    } as MenuConfig
-    const wrapper = shallow(<Menu {...mockProps} />)
-    const logo = wrapper.find('[data-test="menu-label"]')
-    expect(logo).toHaveLength(1)
+  it('should render link item primary correctly', () => {
+    const item = mockMenuProps.menu[1]
+    const children = (
+      <>
+        <div className="nav-item-icon">{item.icon}</div>
+        {item.title && <div className="nav-item-title">{item.title}</div>}
+      </>
+    )
+    const wrapper = shallow(<LinkItem item={item} children={children} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  it('should show sub-menu', () => {
-    const mockLocation = getMockRouterProps({}).location
-    const mockProps = {
-      title: 'mockTitle',
-      homeUrl: '/mockHomeUrl',
-      logo: <div data-test="logo">Logo</div>,
-      menu: [
-        {
-          title: 'mockMenu',
-          key: '/mockMenu',
-          toUrl: '/mockMenu',
-          subMenu: [
-            {
-              title: '/mockSubmenu',
-              key: '/mockSubmenu',
-              toUrl: '/mockSubmenu'
-            }
-          ]
-        }
-      ],
-      location: mockLocation,
-      defaultActiveKey: '/mockSubmenu'
-    } as MenuConfig
-    const wrapper = shallow(<Menu {...mockProps} />)
-    const subMenu = wrapper.find('[data-test="sub-menu-label"]')
-    expect(subMenu).toHaveLength(1)
+  it('should render link item secondary correctly', () => {
+    const item = mockMenuProps.menu[mockMenuProps.menu.length - 1]
+    const children = (
+      <>
+        <div className="nav-item-icon">{item.icon}</div>
+        {item.title && <div className="nav-item-title">{item.title}</div>}
+      </>
+    )
+    const wrapper = shallow(<LinkItem item={item} children={children} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  it('should show active menu', () => {
-    const mockLocation = getMockRouterProps({}).location
-    const mockProps = {
-      title: 'mockTitle',
-      homeUrl: '/mockHomeUrl',
-      logo: <div data-test="logo">Logo</div>,
-      menu: [
-        {
-          title: 'mockMenu',
-          key: '/mockMenu',
-          toUrl: '/mockMenu',
-          subMenu: [
-            {
-              title: '/mockSubmenu',
-              key: '/mockSubmenu',
-              toUrl: '/mockSubmenu'
-            }
-          ]
-        }
-      ],
-      location: mockLocation,
-      defaultActiveKey: '/mockMenu'
-    } as MenuConfig
-    const wrapper = shallow(<Menu {...mockProps} />)
-    const menuItem = wrapper.find('[data-test="menu-item"]')
-    const caret = wrapper.find('[data-test="menu-caret"]')
-    expect(menuItem.prop('className')).toEqual('menu-list sub-menu menu-is-open')
-    expect(caret.prop('isActive')).toEqual(true)
-  })
-
-  it('should show inactive menu', () => {
-    const mockLocation = getMockRouterProps({}).location
-    const mockProps = {
-      title: 'mockTitle',
-      homeUrl: '/mockHomeUrl',
-      logo: <div data-test="logo">Logo</div>,
-      menu: [
-        {
-          title: 'mockMenu',
-          key: '/mockMenu',
-          toUrl: '/mockMenu',
-          subMenu: [
-            {
-              title: '/mockSubmenu',
-              key: '/mockSubmenu',
-              toUrl: '/mockSubmenu'
-            }
-          ]
-        }
-      ],
-      location: mockLocation,
-      defaultActiveKey: '/mockMenu'
-    } as MenuConfig
-    const wrapper = shallow(<Menu {...mockProps} />)
-    const toogleBtn = wrapper.find('[data-test="toogle-btn"]')
-    toogleBtn.simulate('onChange')
-    const menuBar = wrapper.find('[data-test="menu-bar"]')
-    expect(menuBar.prop('className')).toEqual('menu-bar  is-responsive')
-  })
-  describe('isActiveSubmenu', () => {
-    it('should return true', () => {
-      const result = isActiveSubmenu('/admin/admin', '/admin/admin')
-      expect(result).toEqual(true)
+  describe('getActiveItemKey', () => {
+    it('should return url if matches an item', () => {
+      const result = getActiveItemKey(mockMenuProps.menu, { pathname: mockMenuProps.menu[1].url } as Location<any>)
+      expect(result).toEqual(mockMenuProps.menu[1].key)
     })
-    it('should return false', () => {
-      const result = isActiveSubmenu('/admin/dashboard', '/dashboard/admin')
-      expect(result).toEqual(false)
-    })
-    it('should return false', () => {
-      const result = isActiveSubmenu('', '/dashboard/admin')
-      expect(result).toEqual(false)
-    })
-    it('should return false', () => {
-      const result = isActiveSubmenu('/admin/admin', '')
-      expect(result).toEqual(false)
-    })
-    it('should return false', () => {
-      const result = isActiveSubmenu('', '')
-      expect(result).toEqual(false)
-    })
-  })
-  describe('renderLink', () => {
-    it('should show one Link', () => {
-      const result = renderLink({ toUrl: '/admin' } as MenuItem)
-      expect(result).not.toBeNull()
-    })
-    it('should show no Link', () => {
-      const result = renderLink({} as MenuItem)
+    it('should return null if location is undefined', () => {
+      const result = getActiveItemKey(mockMenuProps.menu)
       expect(result).toBeNull()
     })
-  })
-  describe('hrefCallBack', () => {
-    it('should call func', () => {
-      const mockEvent = {
-        preventDefault: jest.fn()
-      } as any
-      const mockCallback = jest.fn()
-      const fn = hrefCallBack(mockCallback)
-      fn(mockEvent)
-      expect(mockEvent.preventDefault).toBeCalled()
-      expect(mockCallback).toBeCalled()
-    })
-  })
-
-  describe('renderHref', () => {
-    it('should render <a>', () => {
-      const mockProps = {
-        callback: jest.fn()
-      } as any
-      const result = renderHref(mockProps)
-      expect(result).not.toBeNull()
-    })
-    it('should not render <a>', () => {
-      const mockProps = {} as any
-      const result = renderHref(mockProps)
+    it('should return null if location does not match an item', () => {
+      const result = getActiveItemKey(mockMenuProps.menu, { pathname: '/some-random-path' } as Location<any>)
       expect(result).toBeNull()
-    })
-  })
-  describe('renderSubItem', () => {
-    it('should render items', () => {
-      const item = [
-        {
-          title: 'App Revisions',
-          key: '/admin',
-          toUrl: '/admin'
-        },
-        {
-          title: 'Approvals',
-          key: '/admin/approvals',
-          toUrl: '/admin/approvals'
-        }
-      ]
-      const location = getMockRouterProps({}).location
-      const result = renderSubItem(item, location)
-      expect(result).not.toBeNull()
-    })
-    it('should not render items', () => {
-      const item = undefined
-      const location = getMockRouterProps({}).location
-      const result = renderSubItem(item, location)
-      expect(result).toBeNull()
-    })
-  })
-  describe('handleSetToggleMenu', () => {
-    it('should call setToggleMenu', () => {
-      const setToggleMenu = jest.fn()
-      const isMenuOpen = true
-      const fn = handleSetToggleMenu(setToggleMenu, isMenuOpen)
-      fn()
-      expect(setToggleMenu).toBeCalledWith(!isMenuOpen)
-    })
-  })
-  describe('handleSetCaretToggle', () => {
-    it('should call setCaretToggleKey', () => {
-      const setCaretToggleKey = jest.fn()
-      const mockItem = {
-        title: 'Approvals',
-        key: '/admin/approvals',
-        toUrl: '/admin/approvals'
-      }
-      const fn = handleSetCaretToggle(setCaretToggleKey, mockItem)
-      fn()
-      expect(setCaretToggleKey).toBeCalledWith(mockItem.key)
     })
   })
 })
