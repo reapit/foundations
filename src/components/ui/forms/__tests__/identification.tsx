a75849c2241345625fafb35a0dc5a6103e6d241b
Vinh Nguyen <nhquocvinh@gmail.com>
2019-10-07 14:04:59 +0700

    [CLD-123] update changes for primary and secondary AML forms (#31)

--- src/components/ui/forms/__tests__/identification.tsx
+++ src/components/ui/forms/__tests__/identification.tsx
@@ -1,33 +1,31 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { CreateIdentityDocumentModel } from '@/types/contact-api-schema'
-import Identification, { renderFormHandler, onSubmitHandler } from '../identification'
+import { CreateIdentityDocumentModel, ContactModel } from '@/types/contact-api-schema'
+import Identification, { renderFormHandler, onSubmitHandler, IdentificationFormValues } from '../identification'
 
 describe('identification forms', () => {
   describe('renderFormHandler', () => {
     it('should render correctly', () => {
       const mockProps = {
+        data: { id: '1' } as ContactModel,
         loading: false,
         onNextHandler: jest.fn(),
         onPrevHandler: jest.fn()
       }
-
       const component = renderFormHandler(mockProps)
       const wrapper = shallow(<div>{component}</div>)
       expect(wrapper).toMatchSnapshot()
     })
   })
-
   describe('onSubmitHandler', () => {
     it('should work as expectations', () => {
-      const mockValues = {}
+      const mockValues = {} as IdentificationFormValues
       const mockOnSaveHandler = jest.fn()
-
-      onSubmitHandler(mockValues, mockOnSaveHandler)
-      expect(mockOnSaveHandler).toBeCalledWith(mockValues)
+      const mockData = {} as ContactModel
+      onSubmitHandler(mockData, mockValues, mockOnSaveHandler)
+      expect(mockOnSaveHandler).toBeCalled()
     })
   })
-
   describe('Identification', () => {
     it('should render correctly', () => {
       const mockProps = {
@@ -38,7 +36,6 @@ describe('identification forms', () => {
         onNextHandler: jest.fn(),
         onPrevHandler: jest.fn()
       }
-
       const wrapper = shallow(<Identification {...mockProps} />)
       expect(wrapper).toMatchSnapshot()
     })
--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -8,9 +8,9 @@ import SecondaryIdentification from './secondary-identification'
 import DeclarationAndRiskAssessment from './declaration-and-risk-assessment'
 
 export const STEPS = {
-  PROFILE: '‘Personal Details',
-  PRIMARY_IDENTIFICATION: 'Primary Identification',
-  SECONDARY_IDENTIFICATION: 'Secondary Identification',
+  PROFILE: 'Personal Details',
+  PRIMARY_IDENTIFICATION: 'Primary ID',
+  SECONDARY_IDENTIFICATION: 'Secondary ID',
   ADDRESS_INFORMATION: 'Address Information',
   REPORT: 'Report',
   DECLARATION_RISK_MANAGEMENT: 'Declaration Risk Management',
@@ -29,16 +29,22 @@ export const renderContent = ({ modalContentType, id }) => {
   switch (modalContentType) {
     case STEPS.PROFILE:
       return <Profile id={id} />
+
     case STEPS.PRIMARY_IDENTIFICATION:
       return <PrimaryIdentification />
+
     case STEPS.SECONDARY_IDENTIFICATION:
       return <SecondaryIdentification />
+
     case STEPS.ADDRESS_INFORMATION:
       return <AddressInformation id={id} />
+
     case STEPS.REPORT:
       return <Report />
+
     case STEPS.DECLARATION_RISK_MANAGEMENT:
-      return <DeclarationAndRiskAssessment id={id} />
+      return <DeclarationAndRiskAssessment />
+
     default:
       return null
   }
