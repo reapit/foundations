7701623cdec082b5647e768e545fb3ebbd4bec3a
dannd <dannd96.it@gmail.com>
2019-10-08 14:29:25 +0700

    Refactor client search to search more options

--- src/components/pages/client-search.tsx
+++ src/components/pages/client-search.tsx
@@ -17,7 +17,7 @@ export type ClientSearchProps = ClientSearchMappedActions & RouteComponentProps
 const { searchContainer } = searchStyle
 
 const identityCheckList = [
-  { label: 'Please select', value: '' },
+  { label: 'Please select…', value: '' },
   { label: 'Pass', value: 'PASS' },
   { label: 'Fail', value: 'FAIL' },
   { label: 'Pending', value: 'PENDING' },
@@ -37,46 +37,38 @@ export const ClientSearch: React.FunctionComponent<ClientSearchProps> = ({ setSe
       <div className={searchContainer}>
         <h2 className="title is-2">Client Search</h2>
         <Formik
-          initialValues={{ name: '' }}
+          initialValues={{ name: '', address: '', identityCheck: '' }}
           onSubmit={values => searchContacts(values)}
-          render={({ values }) => (
-            <Form className="mb-8">
-              <Input id="name" type="text" placeholder="First or last name" name="name" labelText="Enter name" />
-              <Button type="submit" variant="primary" disabled={!values.name}>
-                Search
-              </Button>
-            </Form>
-          )}
-        />
-        <Formik
-          initialValues={{ address: '' }}
-          onSubmit={values => searchContacts(values)}
-          render={({ values }) => (
-            <Form className="mb-8">
-              <Input
-                id="address"
-                type="text"
-                placeholder="Streetname, Town or postcode"
-                name="address"
-                labelText="Enter address"
-              />
-              <Button type="submit" variant="primary" disabled={!values.address}>
-                Search
-              </Button>
-            </Form>
-          )}
-        />
-        <Formik
-          initialValues={{ identityCheck: '' }}
-          onSubmit={values => searchContacts(values)}
-          render={({ values }) => (
-            <Form>
-              <SelectBox id="identityCheck" name="identityCheck" labelText="ID Status" options={identityCheckList} />
-              <Button type="submit" variant="primary" disabled={!values.identityCheck}>
-                Search
-              </Button>
-            </Form>
-          )}
+          render={({ values }) => {
+            const disabled = !values.name && !values.address && !values.identityCheck
+            return (
+              <Form className="mb-8">
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="Firstname or Surname"
+                  name="name"
+                  labelText="Search by name"
+                />
+                <Input
+                  id="address"
+                  type="text"
+                  placeholder="Streetname, Village, Town or Postcode"
+                  name="address"
+                  labelText="Search by address"
+                />
+                <SelectBox
+                  id="identityCheck"
+                  name="identityCheck"
+                  labelText="Search by ID Status"
+                  options={identityCheckList}
+                />
+                <Button className="is-right" type="submit" variant="primary" disabled={disabled}>
+                  Search
+                </Button>
+              </Form>
+            )
+          }}
         />
       </div>
     </ErrorBoundary>
