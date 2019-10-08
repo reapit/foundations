05c2b290a94747263bae46a41d00f019d907fec5
dannd <dannd96.it@gmail.com>
2019-10-08 14:28:57 +0700

    Refactor client search to search more options

--- src/components/pages/__tests__/client-search.tsx
+++ src/components/pages/__tests__/client-search.tsx
@@ -4,6 +4,7 @@ import toJson from 'enzyme-to-json'
 import { ClientSearch, ClientSearchProps } from '../client-search'
 
 const props: ClientSearchProps = {
+  setSearchParams: jest.fn(),
   // @ts-ignore: just pick the needed props for the test
   history: {}
 }
