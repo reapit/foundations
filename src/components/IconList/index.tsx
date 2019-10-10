38c27f0855bfb9dca06337a4b2c9674000972bce
Will McVay <willmcvay@pm.me>
2019-10-10 16:27:12 +0100

    [CLD-291] Allows button centering and icon list classes

--- src/components/IconList/index.tsx
+++ src/components/IconList/index.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react'
 
+export interface IcomListProps {
+  items: IconListItem[]
+  textClassName?: string
+  listClassName?: string
+  iconClassName?: string
+}
+
 export interface IconListItem {
   icon: React.ReactNode
   text: React.ReactNode | string
 }
 
-export const IconList: React.SFC<{ items: IconListItem[] }> = ({ items }) => (
-  <ul className="icon-list">
+export const IconList: React.SFC<IcomListProps> = ({
+  items,
+  iconClassName = '',
+  listClassName = '',
+  textClassName = ''
+}) => (
+  <ul className={`icon-list ${iconClassName}`}>
     {items.map((item: IconListItem, index: number) => (
-      <li className="icon-list-item" key={index}>
+      <li className={`icon-list-item ${listClassName}`} key={index}>
         {item.icon}
-        <span className="icon-list-text">{item.text}</span>
+        <span className={`icon-list-text ${textClassName}`}>{item.text}</span>
       </li>
     ))}
   </ul>
