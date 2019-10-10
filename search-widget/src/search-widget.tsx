4e74f22becd75bd5ff01215f823578477d7a31e2
Duong Pham <tanphamhaiduong@gmail.com>
2019-10-10 16:11:30 +0700

    Remove unuse file

--- search-widget/src/search-widget.tsx
+++ search-widget/src/search-widget.tsx
@@ -3,23 +3,31 @@ import styled, { ThemeProvider } from 'styled-components'
 
 const theme = {
   colors: {
-    jade: '#00a569',
-    jumbo: '#828085',
-    white: '#fff'
+    backgroundColor: '#00a569',
+    titleColor: 'white',
+    buttonTextColor: 'gray',
+    buttonBackgroundColor: 'white',
+    inputTextColor: 'gray',
+    inputBackgroundColor: 'white',
+  },
+  font: {
+    baseFontSize: '1rem',
+    fontFamily: 'inherit',
+    headingFontSize: ''
   }
 }
 const WidgetContainer = styled.div`
-  background-color: ${props => props.theme.colors.jade };
+  background-color: ${props => props.theme.colors.backgroundColor };
   display: inline-block;
-  width: 800px;
+  width: 50rem;
   padding: 2rem 0;
   text-align: center;
   margin: auto 0;
-  font-family: inherit;
+  font-family: ${props => props.theme.font.fontFamily};
 `
 
 const Title = styled.div`
-  color: ${props => props.theme.colors.white };
+  color: ${props => props.theme.colors.titleColor };
   font-size: 2.5rem;
   margin: 0.7rem;
   font-family: inherit;
@@ -27,28 +35,27 @@ const Title = styled.div`
 
 const Button = styled.button`
   width: 40%;
-  height: 50px;
-  color: gray;
+  height: 3rem;
+  color: ${props => props.theme.colors.buttonTextColor };
   font-size: 1rem;
-  background: ${props => props.theme.colors.white };
-  font-family: inherit;
+  background: ${props => props.theme.colors.buttonBackgroundColor };
+  font-family: ${props => props.theme.font.fontFamily};
 `
 
 const ButtonContainer = styled.div`
   margin: 0.7rem;
-  color: ${props => props.theme.colors.jumbo };
 `
 
 const Input = styled.input`
-  height: 50px;
+  height: 3rem;
   width: 80%;
   font-size: 2rem;
   text-align: center;
   font-weight: 150;
-  background: white;
+  background: ${props => props.theme.colors.inputBackgroundColor };
   margin: 0.7rem;
-  color: ${props => props.theme.colors.jumbo };
-  font-family: inherit;
+  color: ${props => props.theme.colors.inputTextColor };
+  font-family: ${props => props.theme.font.fontFamily};
 `
 
 const API_KEY = 'Bearer eyJraWQiOiJTSGNVVnpHamIyQXFFSW9MSFpEbHVwR0hHUlwvTmlLKzN0dTEybm5JcXZMOD0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI5ZTQxZTlkNC1kMmY1LTRjZmMtOTcwYi0zNjE5Y2JmNGI1MWEiLCJldmVudF9pZCI6IjliNDg1NDk1LTEwZDUtNDkwNy1hODFhLTc5MzJhYzVkZWFiMCIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE1NzA2MTE4MDgsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5ldS13ZXN0LTIuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0yX0loRUh6U1Q3YiIsImV4cCI6MTU3MDY4MjQwMiwiaWF0IjoxNTcwNjc4ODAyLCJqdGkiOiJiYjkyYWVmYS00NjUxLTQxZmEtOWU3Mi1mNDgwNmFhMTJmZGUiLCJjbGllbnRfaWQiOiI1dHZnc2sxdTA2YnB1bWJsb2ZiN2s4bGU5ZCIsInVzZXJuYW1lIjoiOWU0MWU5ZDQtZDJmNS00Y2ZjLTk3MGItMzYxOWNiZjRiNTFhIn0.LxNnkgZI4_aHGaO4n6_QHNDGqffNxHjx4dCLTHandBgwLhR8fTZsaW_M9u3dXVGz3ZZtWY0R0NrzHqJAUg3J-g5rc4CMp-5f03kRduNCiULikBPYK4xdpaxXrvoTn2KhRM1tvfIGXYgv-Nwo289y93fZtU-UCqN_IBu2IpRILQStrYukt_8P6vMx-_0PlG9wUzujIVJ2XpgWvRU9cPgEm0hIRqN0W1jEH5kaYjeVEsxMamiQaw8KjFvbDAtkV_-yySfPNQwXXlJwXgz5XxgYC6yhlzn4y3WOPrbl2I2B8g94zImT8iLFmz_mrHNIi8cH68jnUguVFhfGem70m5s4hw'
@@ -94,7 +101,8 @@ export const searchToRent = (setSearching: Function) => async (event: React.Synt
 const SearchWidget: React.FC = () => {
   const [isSearching, setSearching] = React.useState(false)
   return (
-    <ThemeProvider theme={theme}>
+    // @ts-ignore
+    <ThemeProvider theme={window.theme || theme}>
       <WidgetContainer>
       <Title>Find your perfect home</Title>
         <Input name="search" placeholder="enter your town or postcode"/>
