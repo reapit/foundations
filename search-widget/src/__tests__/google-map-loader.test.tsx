5caa553b1869311c2179a60707b0c2c081df253e
Duong Pham <tanphamhaiduong@gmail.com>
2019-10-14 18:52:58 +0700

    [CLD-135] Map Component

---
+++ search-widget/src/__tests__/google-map-loader.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { loadedCallback, GoogleMapLoader } from '../map/google-map-loader'
+import { shallow } from 'enzyme'
+
+describe('GoogleMapLoader', () => {
+  describe('loadedCallback', () => {
+    it('should call setError', () => {
+      const mockSetError = jest.fn()
+      const mockSetGoogleMap = jest.fn()
+      const fn = loadedCallback({ setError: mockSetError, setGoogleMap: mockSetGoogleMap })
+      fn('mockError')
+      expect(mockSetError).toBeCalledWith('mockError')
+    })
+
+    it('should call setGoogleMap', () => {
+      const mockSetError = jest.fn()
+      const mockSetGoogleMap = jest.fn()
+      const fn = loadedCallback({ setError: mockSetError, setGoogleMap: mockSetGoogleMap })
+      fn(undefined)
+      expect(mockSetGoogleMap).toBeCalled()
+    })
+  })
+
+  describe('GoogleMapLoader', () => {
+    it('should match snapshot', () => {
+      const mockProps = {
+        params: { key: "123" },
+        render: jest.fn()
+      }
+      const wrapper = shallow(<GoogleMapLoader {...mockProps}/>)
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+})
--- search-widget/src/index.tsx
+++ search-widget/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchWidget from './search-widget';
+import GoogleMap from './map/google-map'
+import { property } from './map/mock-property'
 
 ReactDOM.render(<SearchWidget />, document.getElementById('reapit-search-widget'));
+ReactDOM.render(<GoogleMap params={{ key: "AIzaSyCsCzPm4VwBHlr7ERyNGizRWMQMNdswzSo" }} property={property} />, document.getElementById('reapit-search-map'))
