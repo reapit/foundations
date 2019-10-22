6f9ea453e08007825d19ed72e3da3b7fb5b56f3f
Will McVay <willmcvay@pm.me>
2019-10-22 15:41:35 +0100

    [CLD-312] Adds link back to desktop

--- src/actions/submit-checks.ts
+++ src/actions/submit-checks.ts
@@ -1,6 +1,9 @@
 import { actionCreator } from '../utils/actions'
 import ActionTypes from '../constants/action-types'
 import { FormState } from '@/types/core'
+import { DynamicLinkParams } from '@reapit/elements'
 
-export const submitChecks = actionCreator<string>(ActionTypes.SUBMIT_CHECKS)
+export const submitChecks = actionCreator<{ id: string; dynamicLinkParams: DynamicLinkParams }>(
+  ActionTypes.SUBMIT_CHECKS
+)
 export const submitChecksSetFormState = actionCreator<FormState>(ActionTypes.SUBMIT_CHECKS_SET_FORM_STATE)
