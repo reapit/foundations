1cd2e58297080315e677eeeee54a83674ba9831f
Huy <gnhuy91@gmail.com>
2019-10-22 13:52:49 +0700

    update s3 deploy step & disable src map for prod (#7)

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ profile ?= default
 bucket ?= reapit-demo-site
 
 dep:
-	cd search-widget && yarn install
+	yarn install
 
 s3:
-	aws --profile $(profile) s3 sync ./public s3://$(bucket) --delete --acl public-read
+	aws --profile $(profile) s3 sync ./build s3://$(bucket) --delete --acl public-read
--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,26 @@
 module.exports = {
   webpack: function(config, env) {
-    config.output.filename = 'bundle.min.js'
+    if (env === "production") {
+      // disable prod source map
+      config.devtool = false;
+    }
 
-    // Disable chunk - create only one file when building, without hash
-    delete config.output.chunkFileName
-    delete config.optimization.splitChunks
-    delete config.optimization.runtimeChunk
-    config.plugins.splice(1,1)
+    // Disable chunk - create only one file when building
+    delete config.output.chunkFileName;
+    delete config.optimization.splitChunks;
+    delete config.optimization.runtimeChunk;
+    config.plugins.splice(1, 1);
+
     return config;
   },
   jest: function(config) {
-    config.setupFiles = [...config.setupFiles, '<rootDir>/jest-setup.js']
-    config.snapshotSerializers = ['enzyme-to-json/serializer']
+    config.setupFiles = [...config.setupFiles, "<rootDir>/jest-setup.js"];
+    config.snapshotSerializers = ["enzyme-to-json/serializer"];
     return config;
   },
   // The paths config to use when compiling your react app for development or production.
   paths: function(paths, env) {
     // ...add your paths config
     return paths;
-  },
-}
+  }
+};
