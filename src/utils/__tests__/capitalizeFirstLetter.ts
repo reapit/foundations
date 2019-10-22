34f72b449fe69b210793ae74cfc4d5a3bba508b1
Will McVay <willmcvay@pm.me>
2019-10-22 22:44:32 +0100

    Revert "Feature/cld 314 update ui details modal in geo diary"

--- src/utils/__tests__/capitalizeFirstLetter.ts
+++
@@ -1,8 +0,0 @@
-import { capitalizeFirstLetter } from '../capitalizeFirstLetter'
-
-describe('capitalizeFirstLetter', () => {
-  it('runs correctly', () => {
-    expect(capitalizeFirstLetter('abc')).toBe('Abc')
-    expect(capitalizeFirstLetter('')).toBe('')
-  })
-})
