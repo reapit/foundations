bf429bb66c0d3f09a9068572207c5790aa78f8af
Thanh Pham <zlatan.pham@gmail.com>
2019-10-22 15:13:28 +0700

    [CLD-313] update layout to have room for ETA btn (#59)
    * [CLD-313] update layout to have room for ETA btn
    
    * [CLD-313] show eta if neither negotiator or office
    
    * [CLD-313] use oc to simplify condition

--- src/components/ui/eta-button.tsx
+++ src/components/ui/eta-button.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { isMacLike } from '@/utils/device-detection'
-import styles from '@/styles/ui/eta-button.scss?mod'
 
 interface ETAButtonProps {
   tel: string | string[]
@@ -15,7 +14,7 @@ export const ETAButton: React.FC<ETAButtonProps> = ({ body, children, tel }) => 
   }
 
   return (
-    <a href={href} className={`${styles.btnEta} button is-fullwidth is-centered is-primary`} data-test="eta-button">
+    <a href={href} className="button is-centered is-primary" data-test="eta-button">
       {children}
     </a>
   )
