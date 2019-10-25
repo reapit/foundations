512127e9f1940c80d6725628c44c52887d76079c
Will McVay <willmcvay@pm.me>
2019-10-25 14:25:42 +0100

    [CLD-350] Tweaks to build and fixes tests

---
+++ public/scripts/theme.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function() {
+  if (window.initReaptSearchWidget) {
+    window.initReaptSearchWidget({
+      API_KEY: "abc",
+      theme: {
+        colors: {}
+      }
+    });
+  }
+});
--- src/__tests__/search-widget.test.tsx
+++ src/__tests__/search-widget.test.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
 import SearchWidget from '../search-widget'
+import { theme } from '../theme'
 
 describe('Search widget', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<SearchWidget />, div)
+    ReactDOM.render(<SearchWidget theme={theme}  API_KEY=''/>, div)
     ReactDOM.unmountComponentAtNode(div)
   })
   it('should match snapshot', () => {
-    const wrapper = shallow(<SearchWidget />)
+    const wrapper = shallow(<SearchWidget theme={theme}  API_KEY=''/>)
     expect(wrapper).toMatchSnapshot()
   })
 })
