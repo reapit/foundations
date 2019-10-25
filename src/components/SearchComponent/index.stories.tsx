3ef655ae19881ad79b0ce9f03f4bbb9362c0bd3c
Pham Hai Duong <tanphamhaiduong@gmail.com>
2019-10-25 11:18:33 +0700

    Add themed search widget (#112)

--- src/components/SearchComponent/index.stories.tsx
+++ src/components/SearchComponent/index.stories.tsx
@@ -2,6 +2,36 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import SearchComponent from '.'
 
-storiesOf('SearchComponent', module).add('SearchComponent', () => {
-  return <SearchComponent />
-})
+storiesOf('SearchComponent', module)
+  .add('Default Theme', () => {
+    return <SearchComponent />
+  })
+  .add('Custom Theme', () => {
+    const theme = {
+      base: {
+        font: {
+          family: 'arial',
+          sizes: {
+            headings: {
+              h1: '7rem',
+              h2: '6rem',
+              h3: '5rem',
+              h4: '4rem',
+              h5: '3rem',
+              h6: '2rem'
+            }
+          }
+        }
+      },
+      button: {
+        background: 'green'
+      },
+      colors: {
+        base: 'white',
+        primary: 'yellow',
+        inputBackgroundColor: 'red',
+        widgetHeading: 'orange'
+      }
+    }
+    return <SearchComponent theme={theme} />
+  })
