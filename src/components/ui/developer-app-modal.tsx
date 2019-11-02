f9cf0d0d172c614ecf027832264b466ce9a71766
Will McVay <willmcvay@pm.me>
2019-11-02 14:27:02 +0000

    [CLD-375] Fixes docs, forms, final launch tweaks (#140)

--- src/components/ui/developer-app-modal.tsx
+++ src/components/ui/developer-app-modal.tsx
@@ -16,7 +16,13 @@ import {
   Loader,
   Alert,
   ModalBody,
-  ModalHeader
+  ModalHeader,
+  FormSection,
+  FormHeading,
+  FormSubHeading,
+  GridItem,
+  Grid,
+  LevelRight
 } from '@reapit/elements'
 import { Form, Formik } from 'formik'
 import { validate } from '@/utils/form/submit-revision'
@@ -42,10 +48,44 @@ export interface DeveloperAppModalMappedActions {
 export type DeveloperAppInnerProps = DeveloperAppModalMappedProps & DeveloperAppModalMappedActions & {}
 export type DeveloperAppModalProps = Pick<ModalProps, 'visible' | 'afterClose'> & {}
 
-export const renderScopesCheckbox = (scopes: ScopeModel[] = []) =>
-  scopes.map((item: ScopeModel) => (
-    <Checkbox name={`scopes.${item.name}`} labelText={item.description || ''} id={item.name || ''} />
-  ))
+export const CheckboxElement: React.SFC<{ scopes?: ScopeModel[] }> = ({ scopes = [] }) => {
+  const oddCheckboxes: JSX.Element[] = []
+  const evenCheckboxes: JSX.Element[] = []
+
+  scopes.forEach((item: ScopeModel, index: number) => {
+    const currentCheckbox = (
+      <Checkbox name={`scopes.${item.name}`} labelText={item.description || ''} id={item.name || ''} />
+    )
+    // TODO - short term hack to remove temporary scopes from API response
+    if (
+      item.name !== 'Marketplace/developers.read' &&
+      item.name !== 'Marketplace/developers.write' &&
+      item.name !== 'TestResourceServer/test.scope'
+    ) {
+      // Sorting evens and odds so they can go in separate grid columns
+      if (index % 2) {
+        evenCheckboxes.push(currentCheckbox)
+      } else {
+        oddCheckboxes.push(currentCheckbox)
+      }
+    }
+  })
+
+  return (
+    <>
+      <GridItem>
+        {oddCheckboxes.map((checkbox: JSX.Element, index: number) => (
+          <span key={index}>{checkbox}</span>
+        ))}
+      </GridItem>
+      <GridItem>
+        {evenCheckboxes.map((checkbox: JSX.Element, index: number) => (
+          <span key={index}>{checkbox}</span>
+        ))}
+      </GridItem>
+    </>
+  )
+}
 
 export const DeveloperAppModalInner: React.FunctionComponent<DeveloperAppInnerProps> = ({
   allScopes,
@@ -191,113 +231,170 @@ export const DeveloperAppModalInner: React.FunctionComponent<DeveloperAppInnerPr
               <ModalBody
                 body={
                   <Form>
-                    <Input dataTest="submit-revision-name" type="text" labelText="Name" id="name" name="name" />
-                    <Input
-                      dataTest="submit-revision-support-email"
-                      type="text"
-                      labelText="Support Email"
-                      id="supportEmail"
-                      name="supportEmail"
-                    />
-                    <Input
-                      dataTest="submit-revision-telephone"
-                      type="text"
-                      labelText="Telephone"
-                      id="telephone"
-                      name="telephone"
-                    />
-                    <Input
-                      dataTest="submit-revision-launchUri"
-                      type="text"
-                      labelText="Launch URI"
-                      id="launchUri"
-                      name="launchUri"
-                    />
-                    <Input
-                      dataTest="submit-revision-homepage"
-                      type="text"
-                      labelText="Homepage"
-                      id="homePage"
-                      name="homePage"
-                    />
-                    <TextArea
-                      id="description"
-                      dataTest="submit-revision-description"
-                      labelText="Description"
-                      name="description"
-                    />
-                    <TextArea id="summary" dataTest="submit-revision-summary" labelText="Sumary" name="summary" />
-                    {renderScopesCheckbox(allScopes)}
-                    <ImageInput
-                      id="iconImageData"
-                      dataTest="submit-app-iconImageData"
-                      labelText="Icon"
-                      name="iconImageData"
-                      allowClear
-                    />
+                    <FormSection>
+                      <FormHeading>App Listing</FormHeading>
+                      <FormSubHeading>
+                        These fields refer to the name and icon of your application as they will appear to a user in the
+                        Marketplace and in their installed apps. The "Is Listed" checkbox determines if the app appears
+                        as a public listing on the marketplace, available for client installation.
+                      </FormSubHeading>
+                      <Input dataTest="submit-revision-name" type="text" labelText="Name" id="name" name="name" />
+                      <div className="control mb-4">
+                        <label className="label">Icon</label>
+                        <ImageInput
+                          id="iconImageData"
+                          dataTest="submit-app-iconImageData"
+                          labelText="Icon"
+                          name="iconImageData"
+                          allowClear
+                        />
+                      </div>
+                      <div className="control">
+                        <label className="label">Is Listed</label>
+                        <Checkbox id="isListed" dataTest="submit-revision-isListed" labelText="" name="isListed" />
+                      </div>
+                    </FormSection>
+                    <FormSection>
+                      <FormHeading>App Details</FormHeading>
+                      <FormSubHeading>
+                        Information that will be the user facing listing for your app. "Summary" will be the short app
+                        strapline whereas "description", will be more detailed. These fields have a min/max charset of
+                        50/150 and 150/1000 respectively.
+                      </FormSubHeading>
+                      <TextArea id="summary" dataTest="submit-revision-summary" labelText="Sumary" name="summary" />
+                      <TextArea
+                        id="description"
+                        dataTest="submit-revision-description"
+                        labelText="Description"
+                        name="description"
+                      />
+                    </FormSection>
+                    <FormSection>
+                      <FormHeading>Contact Details</FormHeading>
+                      <FormSubHeading>
+                        Should one of our developers need to get in touch about your app listing.
+                      </FormSubHeading>
+                      <Input
+                        dataTest="submit-revision-support-email"
+                        type="text"
+                        labelText="Support Email"
+                        id="supportEmail"
+                        name="supportEmail"
+                      />
+                      <Input
+                        dataTest="submit-revision-telephone"
+                        type="text"
+                        labelText="Telephone"
+                        id="telephone"
+                        name="telephone"
+                      />
+                    </FormSection>
+                    <FormSection>
+                      <FormHeading>Websites</FormHeading>
+                      <FormSubHeading>
+                        Homepage refers to your company's corporate site. The launch URI is default homepage for your
+                        listed application.
+                      </FormSubHeading>
+                      <Input
+                        dataTest="submit-revision-launchUri"
+                        type="text"
+                        labelText="Launch URI"
+                        id="launchUri"
+                        name="launchUri"
+                      />
+                      <Input
+                        dataTest="submit-revision-homepage"
+                        type="text"
+                        labelText="Homepage"
+                        id="homePage"
+                        name="homePage"
+                      />
+                    </FormSection>
+                    <FormSection>
+                      <FormHeading>Screenshots</FormHeading>
+                      <FormSubHeading>
+                        You can select a minimum of one and up to four screenshots of your application, that will appear
+                        in a carousel in the details view of your app listing.
+                      </FormSubHeading>
 
-                    <ImageInput
-                      id="screenshot1"
-                      dataTest="submit-app-screenshoot1"
-                      labelText="Screenshot 1"
-                      name="screen1ImageData"
-                      allowClear
-                    />
+                      <div className="control mb-4">
+                        <label className="label">Screenshot 1</label>
+                        <ImageInput
+                          id="screenshot1"
+                          dataTest="submit-app-screenshoot1"
+                          labelText="Upload Image"
+                          name="screen1ImageData"
+                          allowClear
+                        />
+                      </div>
+                      <div className="control mb-4">
+                        <label className="label">Screenshot 2</label>
+                        <ImageInput
+                          id="screenshot2"
+                          dataTest="submit-app-screenshoot2"
+                          labelText="Upload Image"
+                          name="screen2ImageData"
+                          allowClear
+                        />
+                      </div>
 
-                    <ImageInput
-                      id="screenshot2"
-                      dataTest="submit-app-screenshoot2"
-                      labelText="Screenshot 2"
-                      name="screen2ImageData"
-                      allowClear
-                    />
-
-                    <ImageInput
-                      id="screenshot3"
-                      dataTest="submit-app-screenshoot3"
-                      labelText="Screenshot 3"
-                      name="screen3ImageData"
-                      allowClear
-                    />
-
-                    <ImageInput
-                      id="screenshot4"
-                      dataTest="submit-app-screenshoot4"
-                      labelText="Screenshot 4"
-                      name="screen4ImageData"
-                      allowClear
-                    />
-
-                    <ImageInput
-                      id="screenshot5"
-                      dataTest="submit-app-screenshoot5"
-                      labelText="Screenshot 5"
-                      name="screen5ImageData"
-                      allowClear
-                    />
-
-                    <Checkbox id="isListed" dataTest="submit-revision-isListed" labelText="Is listed" name="isListed" />
-
-                    <Button
-                      type="button"
-                      className="mr-2"
-                      variant="secondary"
-                      disabled={Boolean(isLoading)}
-                      onClick={() => {
-                        setIsEditDetail(false)
-                      }}
-                    >
-                      Cancel
-                    </Button>
-                    <Button
-                      type="submit"
-                      variant="primary"
-                      loading={Boolean(isLoading)}
-                      disabled={Boolean(isLoading)}
-                      dataTest="submit-revision-modal-edit-button"
-                    >
-                      Submit revision
-                    </Button>
+                      <div className="control mb-4">
+                        <label className="label">Screenshot 3</label>
+                        <ImageInput
+                          id="screenshot3"
+                          dataTest="submit-app-screenshoot3"
+                          labelText="Upload Image"
+                          name="screen3ImageData"
+                          allowClear
+                        />
+                      </div>
+                      <div className="control mb-4">
+                        <label className="label">Screenshot 4</label>
+                        <ImageInput
+                          id="screenshot4"
+                          dataTest="submit-app-screenshoot4"
+                          labelText="Upload Image"
+                          name="screen4ImageData"
+                          allowClear
+                        />
+                      </div>
+                    </FormSection>
+                    <FormSection>
+                      <FormHeading>Permssions</FormHeading>
+                      <FormSubHeading>
+                        To access a client's data, you will need to specify the entities you need access to on a read or
+                        write basis. You should be familar with these entities from the sandbox. When the user installs
+                        your application, they will have to consent to your usage based on these permissions. If you do
+                        not have the correct permissions on an entity basis, your app will receive a 403 error.
+                      </FormSubHeading>
+                      <Grid>
+                        <CheckboxElement scopes={allScopes} />
+                      </Grid>
+                    </FormSection>
+                    <FormSection>
+                      <LevelRight>
+                        <Button
+                          type="button"
+                          className="mr-2"
+                          variant="secondary"
+                          disabled={Boolean(isLoading)}
+                          onClick={() => {
+                            setIsEditDetail(false)
+                          }}
+                        >
+                          Cancel
+                        </Button>
+                        <Button
+                          type="submit"
+                          variant="primary"
+                          loading={Boolean(isLoading)}
+                          disabled={Boolean(isLoading)}
+                          dataTest="submit-revision-modal-edit-button"
+                        >
+                          Submit revision
+                        </Button>
+                      </LevelRight>
+                    </FormSection>
                   </Form>
                 }
               />
