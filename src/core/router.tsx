6e8241c3db0040c357abfbfb88b2fa981a42c082
Will McVay <willmcvay@pm.me>
2019-11-06 15:50:31 +0000

    Hotfix/typo (#142)
    * Fixes small typo
    
    * Removes registration

--- src/core/router.tsx
+++ src/core/router.tsx
@@ -11,7 +11,7 @@ export const history = createBrowserHistory()
 const Login = React.lazy(() => import('../components/pages/login'))
 const Client = React.lazy(() => import('../components/pages/client'))
 const MyApps = React.lazy(() => import('../components/pages/my-apps'))
-const Register = React.lazy(() => import('../components/pages/register'))
+// const Register = React.lazy(() => import('../components/pages/register'))
 const DeveloperHome = React.lazy(() => import('../components/pages/developer-home'))
 const DeveloperSubmitApp = React.lazy(() => import('../components/pages/developer-submit-app'))
 const AdminApprovalsPage = React.lazy(() => import('../components/pages/admin-approvals'))
@@ -29,7 +29,7 @@ const Router = () => (
       <Switch>
         <Route path={Routes.LOGIN} exact render={() => <Login />} />
         <Route path={Routes.ADMIN_LOGIN} exact render={() => <Login />} />
-        <Route path={Routes.REGISTER} exact render={() => <Register />} />
+        {/* <Route path={Routes.REGISTER} exact render={() => <Register />} /> */}
         <Route path={Routes.FOUR_O_FOUR} exact render={() => <Info infoType="404" />} />
         <PrivateRouteWrapper path="/">
           <Switch>
