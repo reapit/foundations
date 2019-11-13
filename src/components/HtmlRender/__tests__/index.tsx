c51a8665d2f2bafc930ffd2363fa062e92cd21be
Thanh Pham <zlatan.pham@gmail.com>
2019-11-13 17:26:35 +0700

    [CLD-336] add diffing option for HTMLRender (#131)
    * [CLD-336] add diffing option for HTMLRender
    
    * v0.4.65

--- src/components/HtmlRender/__tests__/index.tsx
+++ src/components/HtmlRender/__tests__/index.tsx
@@ -55,7 +55,7 @@ const itemIncrementor = (initialItem: Element[], spy: any) => {
     // Each time I call to sortContentType, I increase the calls
     numberIterations++
     // And check my iterable item was used as an arg to call sort
-    expect(spy).toHaveBeenCalledWith(item, index)
+    expect(spy).toHaveBeenCalledWith(item, index, false)
     return item.children && itemIncrementor(item.children, spy)
   })
 }
@@ -63,7 +63,7 @@ const itemIncrementor = (initialItem: Element[], spy: any) => {
 describe('renderer', () => {
   it('should call out to sortFunction for number of items in content array', () => {
     const sortContentSpy = jest.spyOn(rendererModule, 'sortContentType')
-    const rendered = rendererModule.renderer(htmlElements)
+    const rendered = rendererModule.renderer(htmlElements, false)
     itemIncrementor(htmlElements, sortContentSpy)
     expect(rendered.length).toEqual(htmlElements.length)
     expect(sortContentSpy).toHaveBeenCalledTimes(numberIterations)
@@ -80,16 +80,16 @@ describe('sortContentType', () => {
   it('should call out to sortTags if an item, is of type tag', () => {
     jest.spyOn(rendererModule, 'getChildren').mockImplementation()
     const sortTagsSpy = jest.spyOn(rendererModule, 'sortTags')
-    rendererModule.sortContentType(htmlElements[0], 0)
+    rendererModule.sortContentType(htmlElements[0], 0, false)
     expect(sortTagsSpy).toHaveBeenCalledTimes(1)
-    expect(sortTagsSpy).toHaveBeenCalledWith(htmlElements[0], 0)
+    expect(sortTagsSpy).toHaveBeenCalledWith(htmlElements[0], 0, false)
   })
 
   it('should not call out to sortTags if an item, is of type text and instead return the text', () => {
     jest.spyOn(rendererModule, 'getChildren').mockImplementation()
     const sortTagsSpy = jest.spyOn(rendererModule, 'sortTags')
     const textNode = htmlElements[0].children![1]
-    const expectedText = rendererModule.sortContentType(textNode, 0)
+    const expectedText = rendererModule.sortContentType(textNode, 0, false)
     expect(sortTagsSpy).not.toHaveBeenCalled()
     expect(expectedText).toEqual(textNode.content)
   })
@@ -103,14 +103,14 @@ describe('sortContentType', () => {
 describe('getChildren', () => {
   it('should map children to sortContentType if there are any', () => {
     const sortContentTypeSpy = jest.spyOn(rendererModule, 'sortContentType')
-    rendererModule.getChildren(htmlElements[0])
+    rendererModule.getChildren(htmlElements[0], false)
     itemIncrementor(htmlElements[0].children!, sortContentTypeSpy)
     expect(sortContentTypeSpy).toHaveBeenCalledTimes(numberIterations)
-    expect(sortContentTypeSpy).toHaveBeenCalledWith(htmlElements[0].children![0], 0)
+    expect(sortContentTypeSpy).toHaveBeenCalledWith(htmlElements[0].children![0], 0, false)
   })
   it('should not call out to sortContentType if no children and instead return null', () => {
     const sortContentTypeSpy = jest.spyOn(rendererModule, 'sortContentType')
-    const children = rendererModule.getChildren(htmlElements[0].children![1])
+    const children = rendererModule.getChildren(htmlElements[0].children![1], false)
     expect(sortContentTypeSpy).not.toHaveBeenCalled()
     expect(children).toBeNull()
   })
@@ -211,7 +211,7 @@ describe('sortTags', () => {
   cases.forEach((node: any) => {
     it(`should match a snapshot and be of correct type for the returned component from domTag ${node.tag}`, () => {
       const element = { tagName: node.tag, ...coreProps } as Element
-      const styledComponent = rendererModule.sortTags(element, 0) as React.ReactElement
+      const styledComponent = rendererModule.sortTags(element, 0, false) as React.ReactElement
       const wrappedComponent = shallow(styledComponent)
       expect(wrappedComponent.type()).toEqual(node.actualTag || node.tag)
       expect(toJson(wrappedComponent)).toMatchSnapshot()
@@ -229,7 +229,7 @@ describe('should not render empty tags', () => {
       },
       children: []
     } as Partial<Element>
-    const styledComponent = rendererModule.sortTags({ ...coreProps } as Element, 0)
+    const styledComponent = rendererModule.sortTags({ ...coreProps } as Element, 0, false)
     expect(styledComponent).toBe(null)
   })
   it('no children array', () => {
@@ -240,7 +240,7 @@ describe('should not render empty tags', () => {
         id
       }
     } as Partial<Element>
-    const styledComponent = rendererModule.sortTags({ ...coreProps } as Element, 0)
+    const styledComponent = rendererModule.sortTags({ ...coreProps } as Element, 0, false)
     expect(styledComponent).toBe(null)
   })
 })
