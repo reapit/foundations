0f72285c45fd4b529f0c273fd1f6a33a6e6c4ebb
Will McVay <willmcvay@pm.me>
2019-11-20 18:12:12 +0000

    Feature/cld 459 update endpoints (#135)
    * [CLD-459] Update cognito endpoint
    
    * v0.4.92

--- src/utils/cognito/constants.ts
+++ src/utils/cognito/constants.ts
@@ -1,4 +1,4 @@
-export const COGNITO_API_BASE_URL = 'https://1wke0xp728.execute-api.eu-west-2.amazonaws.com/dev/api'
+export const COGNITO_API_BASE_URL = 'https://rbsbshnxvb.execute-api.eu-west-2.amazonaws.com/dev/api'
 
 export const COGNITO_HEADERS = {
   'Content-Type': 'application/json'
