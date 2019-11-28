02f4fc4c201d3f65bb42649109e5daa4dff38ed8
Will McVay <willmcvay@pm.me>
2019-11-28 14:19:31 +0000

    [CLD-477] Works locally, ready to deploy

--- src/mailer/templates/index.ts
+++ src/mailer/templates/index.ts
@@ -3,3 +3,6 @@ import { ForgottenPasswordTemplateParams } from './types'
 
 export const forgotPasswordTemplate = async (data: ForgottenPasswordTemplateParams) =>
   ejs.renderFile(`${__dirname}/ejs/forgotten-password.ejs`, data)
+
+export const confirmRegistrationTemplate = async (data: ForgottenPasswordTemplateParams) =>
+  ejs.renderFile(`${__dirname}/ejs/confirm-registration.ejs`, data)
