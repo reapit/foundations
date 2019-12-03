58bd692bd9017416450d5217b887d58d37dc3e29
Duong Pham <tanphamhaiduong@gmail.com>
2019-12-03 17:55:26 +0700

    [CLD-483] registration confirmation email

--- src/mailer/custom-mailer.ts
+++ src/mailer/custom-mailer.ts
@@ -6,15 +6,21 @@ export const customMailer: CognitoUserPoolTriggerHandler = async (event, _contex
     switch (event.triggerSource) {
       case 'CustomMessage_ForgotPassword':
         event.response.emailSubject = 'Reapit Foundations: Forgotten Password'
+        // TODO will replace by env
+        const resetPasswordUrl = 'https://dev.marketplace.reapit.com/developer/reset-password'
         event.response.emailMessage = await forgotPasswordTemplate({
           verificationCode: event.request.codeParameter as string,
-          userName: event.request.userAttributes.email
+          userName: event.request.userAttributes.email,
+          url: resetPasswordUrl
         })
         break
       case 'CustomMessage_SignUp':
         event.response.emailSubject = 'Welcome to Reapit Foundations'
+        // TODO will replace by env
+        const confirmRegistrationUrl = 'https://dev.marketplace.reapit.com/register/confirm'
         event.response.emailMessage = await confirmRegistrationTemplate({
-          userName: event.request.userAttributes.email
+          userName: event.request.userAttributes.email,
+          url: confirmRegistrationUrl
         })
         break
     }
