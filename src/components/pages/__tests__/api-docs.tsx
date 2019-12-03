469519a2b9fcbda886f747266d7bb4e5903d8504
Vũ Hữu Cường <vuhuucuong1310@gmail.com>
2019-12-03 17:35:14 +0700

    [CLD-406] Fix scroll to link in Docs (Dev portals) (#181)
    * [CLD-406] Fix scroll to link in Docs (Dev portals), scroll to anchor link when directly go into route that have `#`
    
    * Remove unnecessary links
    
    * Update snapshot
    
    * Add tests

--- src/components/pages/__tests__/api-docs.tsx
+++ src/components/pages/__tests__/api-docs.tsx
@@ -1,12 +1,54 @@
 import * as React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import toJson from 'enzyme-to-json'
-import ApiDocs from '../api-docs'
+import ApiDocs, { handleUseLayoutEffect } from '../api-docs'
 
 jest.mock('../../../core/store')
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: jest.fn(() => ({
+    location: { hash: '' }
+  }))
+}))
+
+let scrollIntoViewMock
+beforeEach(() => {
+  document.body.innerHTML = '<div><p id="api"></p><p id="authorization"></p><div>'
+  scrollIntoViewMock = jest.fn()
+  HTMLElement.prototype.scrollIntoView = scrollIntoViewMock
+})
 
 describe('ApiDocs', () => {
   it('should match a snapshot', () => {
-    expect(toJson(shallow(<ApiDocs />))).toMatchSnapshot()
+    expect(shallow(<ApiDocs />)).toMatchSnapshot()
+    jest.resetModules()
+  })
+
+  it('should match snapshot after changing hash', () => {
+    jest.mock('react-router-dom', () => ({
+      ...jest.requireActual('react-router-dom'),
+      useLocation: jest.fn(() => ({
+        location: { hash: '#authorization' }
+      }))
+    }))
+    expect(shallow(<ApiDocs />)).toMatchSnapshot()
+  })
+
+  it('should call scrollIntoView when have matching id', () => {
+    const fn = handleUseLayoutEffect('#authorization')
+    fn()
+    expect(scrollIntoViewMock).toBeCalled()
+  })
+
+  it('should not call scrollIntoView when no matching id', () => {
+    const fn = handleUseLayoutEffect('#authorization123')
+    fn()
+    expect(scrollIntoViewMock).toHaveBeenCalledTimes(0)
+  })
+
+  it('should call scrollIntoView to id="api" when there no hash', () => {
+    const fn = handleUseLayoutEffect('')
+    fn()
+    expect(scrollIntoViewMock).toBeCalled()
   })
 })
