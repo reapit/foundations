15088f381b4b7475aa806297fc3863d23d7b883f
Khac Vy <khacvy93@gmail.com>
2019-12-04 11:36:35 +0700

    [CLD-505] Password validator (#139)
    * [CLD-505] Password validator
    
    * v0.5.2-beta

--- src/utils/validators/error-messages.ts
+++ src/utils/validators/error-messages.ts
@@ -7,6 +7,8 @@ const errorMessages = {
   DEFAULT_COMPONENT_ERROR: 'Something went wrong with this component',
   FIELD_REQUIRED: 'Required',
   FIELD_WRONG_EMAIL_FORMAT: 'Invalid email format',
+  FIELD_INVALID_PASSWORD:
+    'Your Password should be a minimum of 8 characters; must contain at least one lowercase letter, one uppercase letter and one number.',
   FIELD_WRONG_URI_FORMAT: 'Invalid uri format',
   MINIMUM_CHARACTER_LENGTH: minCharLengthErrMessage,
   MAXIMUM_CHARACTER_LENGTH: maxCharLengthErrMessage
