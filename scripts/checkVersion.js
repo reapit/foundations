1e05eeb687a561a2dc04f4ca95e210ee9ad91f00
NghiaPham <nghiapn@dwarvesv.com>
2019-12-11 13:58:30 +0700

    Feature/cld 432 pipeline (#13)

---
+++ scripts/checkVersion.js
@@ -0,0 +1,14 @@
+const {npm_package_version: currentPackageVersion, npm_package_name: npmPackageName} = process.env
+const { execSync } = require('child_process');
+const remotePackageVersion = execSync(`yarn info ${npmPackageName} version`).toString().trim()
+const compareVersions = require('compare-versions');
+
+if (remotePackageVersion === '') {
+  return
+}
+
+// currentPackageVersion < remotePackageVersion
+if (compareVersions(currentPackageVersion, remotePackageVersion) < 1) {
+  console.log('Current version is smaller than remote version. Please bump your package version')
+  process.exit(1)
+}
