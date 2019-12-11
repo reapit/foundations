202e677105d0f8af95bdcebfc5047bc6e1a6cc41
NghiaPham <nghiapn@dwarvesv.com>
2019-12-11 13:57:04 +0700

    [CLD-431]Fix pipeline (#151)

--- scripts/releaseMaster.js
+++ scripts/releaseMaster.js
@@ -1,5 +1,27 @@
-const release = require('./release')
-release('prod').catch(err => {
-  console.error(err.message)
-  process.exit(1)
-})
+const Octokit = require('@octokit/rest')
+module.exports = async env => {
+  const currentVersion = process.env.npm_package_version
+  let tagName = 'v' + currentVersion
+
+  const token = process.env.GITHUB_TOKEN
+
+  const octokit = new Octokit({
+    auth: token
+  })
+
+  await octokit.git
+    .createRef({
+      owner: 'reapit',
+      repo: 'elements',
+      ref: 'refs/tags/' + tagName,
+      sha: process.env.GITHUB_SHA
+    })
+
+  // create new release based on tag
+  await octokit.repos
+    .createRelease({
+      owner: 'reapit',
+      repo: 'elements',
+      tag_name: tagName
+    })
+}
