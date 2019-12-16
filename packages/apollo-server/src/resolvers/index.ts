2f416867786b7d67a3c30a9bab59c53127279608
Duong Pham <tanphamhaiduong@gmail.com>
2019-12-16 11:13:10 +0700

    [CLD-611] Add types for queries and mutations

--- packages/apollo-server/src/resolvers/index.ts
+++ packages/apollo-server/src/resolvers/index.ts
@@ -1,3 +1,11 @@
-export const resolvers = {}
+import GraphQLJSON from 'graphql-type-json'
+import contactResolvers from './contact/resolvers'
+import contactIdentityCheckResolvers from './contact-identity-check/resolvers'
+
+export const resolvers = {
+  ...contactResolvers,
+  ...contactIdentityCheckResolvers,
+  JSON: GraphQLJSON,
+}
 
 export default resolvers
