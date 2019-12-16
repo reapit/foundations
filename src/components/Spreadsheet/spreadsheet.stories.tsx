6e2cec36cdeeb86393a0f0ed64d23ec7dc9f15fe
vuhuucuong <vuhuucuong1310@gmail.com>
2019-12-16 16:24:52 +0700

    [CLD-586] Create Helper component

---
+++ src/components/Spreadsheet/spreadsheet.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Spreadsheet } from './index'
+
+storiesOf('Spreadsheet', module).add('Spreadsheet', () => <Spreadsheet />)
