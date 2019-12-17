ec6239ee0e00b2d2739a4bee7209ee1616bbd85f
Dan Nguyen <45675930+danndz@users.noreply.github.com>
2019-12-17 17:24:31 +0700

    [CLD-571] Change UI Installed apps (#210)
    * [CLD-571] Change UI Installed apps
    
    * [CLD-571] fix review
    
    * [CLD-571] Remove grid and use flex

--- src/components/ui/installed-app-card.tsx
+++ src/components/ui/installed-app-card.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { AppSummaryModel } from '@/types/marketplace-api-schema'
 import { FaEllipsisH } from 'react-icons/fa'
 import installedAppCardStyles from '@/styles/blocks/installed-app-card.scss?mod'
+import { isMobile, Tile } from '@reapit/elements'
 
 export interface InstalledAppCardProps {
   app: AppSummaryModel
@@ -9,13 +10,29 @@ export interface InstalledAppCardProps {
 }
 
 const InstalledAppCard: React.FC<InstalledAppCardProps> = ({ app, onClick }: InstalledAppCardProps) => {
-  return (
-    <div className={installedAppCardStyles.container}>
-      <div id="installed-card-icon" onClick={onClick} className={installedAppCardStyles['wrap-icon']}>
-        <img className={installedAppCardStyles.icon} src={app.iconUri} alt="iconUri" />
+  if (isMobile()) {
+    return (
+      <div className={installedAppCardStyles.container}>
+        <div data-test={`app-installed_${app.id}`} onClick={onClick} className={installedAppCardStyles.wrapIcon}>
+          <img className={installedAppCardStyles.icon} src={app.iconUri} alt="iconUri" />
+        </div>
+        <p className={installedAppCardStyles.appTitle}>{app.name}</p>
       </div>
-      <p className={installedAppCardStyles['app-title']}>{app.name}</p>
-    </div>
+    )
+  }
+
+  return (
+    <Tile
+      onClick={onClick as () => void}
+      heading={app.name || ''}
+      subHeading={app.developer || ''}
+      image={
+        <img className="image" src={app.iconUri || 'https://bulma.io/images/placeholders/48x48.png'} alt={app.name} />
+      }
+      menu={<FaEllipsisH className="media-icon" onClick={onClick} data-test={`app-installed_${app.id}`} />}
+    >
+      <p className={installedAppCardStyles.content}>{app.summary}</p>
+    </Tile>
   )
 }
 
--- src/components/ui/installed-app-list.tsx
+++ src/components/ui/installed-app-list.tsx
@@ -2,7 +2,19 @@ import * as React from 'react'
 import { AppSummaryModel } from '@/types/marketplace-api-schema'
 import InstalledAppCard from './installed-app-card'
 import installedAppListStyles from '@/styles/blocks/installed-app-list.scss?mod'
-import { Loader, H3, InfoType, Info, PaginationProps, Section, Pagination, FlexContainerBasic } from '@reapit/elements'
+import {
+  Loader,
+  H3,
+  InfoType,
+  Info,
+  PaginationProps,
+  Section,
+  Pagination,
+  FlexContainerBasic,
+  isMobile,
+  GridFiveCol,
+  GridFourColItem
+} from '@reapit/elements'
 
 export type InstalledAppListProps = {
   list: AppSummaryModel[]
@@ -22,6 +34,45 @@ export const onClickHandler = (onCardClick: ((app: AppSummaryModel) => void) | u
   }
 }
 
+export const ListMobileScreen = ({ list, loading, onCardClick, infoType }) => {
+  const isNoResult = !list.length && !loading
+
+  return (
+    <div
+      className={`${installedAppListStyles['wrap-list']} ${
+        loading ? installedAppListStyles['content-is-loading'] : ''
+      }`}
+    >
+      {isNoResult ? (
+        <Info infoType={infoType}>{!infoType && 'UNFORTUNATELY, YOUR SEARCH RETURNED NO RESULTS'}</Info>
+      ) : (
+        list.map(app => <InstalledAppCard key={app.id} app={app} onClick={onClickHandler(onCardClick, app)} />)
+      )}
+    </div>
+  )
+}
+
+export const ListDesktopScreen = ({ list, loading, infoType, onCardClick }) => {
+  const isNoResult = !list.length && !loading
+
+  return (
+    <GridFiveCol
+      className={` ${loading ? installedAppListStyles.contentIsLoading : ''}`}
+      data-test="app-list-container"
+    >
+      {isNoResult ? (
+        <Info infoType={infoType}>{!infoType && 'UNFORTUNATELY, YOUR SEARCH RETURNED NO RESULTS'}</Info>
+      ) : (
+        list.map(app => (
+          <GridFourColItem key={app.id}>
+            <InstalledAppCard key={app.id} app={app} onClick={onClickHandler(onCardClick, app)} />
+          </GridFourColItem>
+        ))
+      )}
+    </GridFiveCol>
+  )
+}
+
 export const InstalledAppList: React.FC<InstalledAppListProps> = ({
   list,
   loading,
@@ -30,21 +81,14 @@ export const InstalledAppList: React.FC<InstalledAppListProps> = ({
   infoType,
   pagination
 }) => {
-  const isNoResult = !list.length && !loading
   return (
     <FlexContainerBasic hasPadding flexColumn>
       {title && <H3>{title}</H3>}
-      <div
-        className={`${installedAppListStyles['wrap-list']} ${
-          loading ? installedAppListStyles['content-is-loading'] : ''
-        }`}
-      >
-        {isNoResult ? (
-          <Info infoType={infoType}>{!infoType && 'UNFORTUNATELY, YOUR SEARCH RETURNED NO RESULTS'}</Info>
-        ) : (
-          list.map(app => <InstalledAppCard key={app.id} app={app} onClick={onClickHandler(onCardClick, app)} />)
-        )}
-      </div>
+      {isMobile() ? (
+        <ListMobileScreen list={list} loading={loading} infoType={infoType} onCardClick={onCardClick} />
+      ) : (
+        <ListDesktopScreen list={list} loading={loading} infoType={infoType} onCardClick={onCardClick} />
+      )}
       {loading && <Loader body />}
       {pagination && (
         <Section>
