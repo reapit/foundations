632eeb02fd5debfcdca6c86f88d725b0e894ca0a
Thanh Pham <zlatan.pham@gmail.com>
2019-12-18 18:04:39 +0700

    [CLD-623] fix edit app images not show (#215)

--- src/components/pages/__tests__/developer-submit-app.tsx
+++ src/components/pages/__tests__/developer-submit-app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { match } from 'react-router'
 import toJson from 'enzyme-to-json'
-import { shallow, mount } from 'enzyme'
+import { shallow, mount, render } from 'enzyme'
 import { getMockRouterProps } from '@/utils/mock-helper'
 import { SubmitApp, SubmitAppProps, renderScopesCheckbox, SubmitAppMappedActions } from '../developer-submit-app'
 import { appDetailDataStub } from '../../../sagas/__stubs__/app-detail'
@@ -80,6 +80,37 @@ describe('DeveloperSubmitApp', () => {
     }
 
     expect(toJson(shallow(<SubmitApp {...props} />))).toMatchSnapshot()
+  })
+
+  it('should match snapshot when there are two app images', () => {
+    const props: SubmitAppProps = {
+      ...submitAppMappedActionsProps,
+      appDetailState: { loading: false, error: false, appDetailData: appDetailDataStub },
+      submitRevisionState: { formState: 'PENDING' },
+      submitAppState: {
+        loading: false,
+        submitAppData: {
+          scopes: [{ name: 'AgencyCloud/properties.read', description: 'Read data about properties' }]
+        },
+        formState: 'PENDING'
+      },
+      developerId: '2',
+      match: {
+        params: {
+          appid: '1'
+        }
+      } as match<{ appid?: string }>,
+      history: mockRouterProps.history,
+      location: mockRouterProps.location,
+      categories: appCategorieStub?.data || []
+    }
+
+    const wrapper = render(<SubmitApp {...props} />)
+    const screenshot1Value = wrapper.find('#screenshot1+span+span>a') // see css selectors
+    const screenshot2Value = wrapper.find('#screenshot2+span+span>a') // see css selectors
+
+    expect(screenshot1Value).toBeDefined()
+    expect(screenshot2Value).toBeDefined()
   })
 
   it('should match submit revision form when appDetailState is loading snapshot', () => {
