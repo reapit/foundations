5621d55cfc2bd3d0b0211d2b2fbd9ad5bf6a6c93
Nghia Pham <nghiapn@dwarvesv.com>
2019-12-21 15:58:35 +0700

    [CLD-415]Self install this package and add package to story for testing purpose

--- src/components/search-widget/search-widget.stories.tsx
+++ src/components/search-widget/search-widget.stories.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import SearchWidget from './search-widget'
+import { SearchWidget as SearchWidgetNPM } from '@reapit/web-components-test'
 
 export default { title: 'SearchWidget' }
 
-export const withSearchWidget = () => (
+export const withSearchWidgetDev = () => (
   <div>
     <SearchWidget />
+    <div id="reapit-search-widget-result" />
+  </div>
+)
+
+export const withSearchWidgetNpm = () => (
+  <div>
+    <SearchWidgetNPM />
     <div id="reapit-search-widget-result" />
   </div>
 )
