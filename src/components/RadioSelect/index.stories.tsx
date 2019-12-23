5538feb61d6e22a47dfc2a2f822401dba757585a
Thanh Pham <zlatan.pham@gmail.com>
2019-12-23 09:56:41 +0700

    [CLD-602][CLD-628] general update and radio fix (#183)
    * [CLD-602][CLD-628] general update and radio fix
    
    * fix wizard invisible
    
    * bump v0.5.19

--- src/components/RadioSelect/index.stories.tsx
+++ src/components/RadioSelect/index.stories.tsx
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react'
 import { RadioSelect } from '.'
 import { Formik, Form } from 'formik'
 import { action } from '@storybook/addon-actions'
+import { Button } from '../Button'
 
 storiesOf('RadioSelect', module).add('Primary', () => {
   const mockProps = {
@@ -16,16 +17,20 @@ storiesOf('RadioSelect', module).add('Primary', () => {
   return (
     <section className="section">
       <Formik
-        initialValues={{ demo: 'b' }}
+        initialValues={{ [mockProps.name]: '' }}
         onSubmit={values => {
           action('Form Values' + values)
+          console.log(values)
         }}
       >
-        {() => (
+        {({ setFieldValue, values }) => (
           <Form>
             <div className="column is-half-desktop">
-              <RadioSelect {...mockProps} />
+              <RadioSelect setFieldValue={setFieldValue} state={values[mockProps.name]} {...mockProps} />
             </div>
+            <Button variant={'primary'} type={'submit'}>
+              Submit
+            </Button>
           </Form>
         )}
       </Formik>
