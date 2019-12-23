3b15f6a5ce7d582606834df2e8cbc1bd87e7d89b
Duong Pham <tanphamhaiduong@gmail.com>
2019-12-23 17:06:15 +0700

    [CLD-609] Fix PR for login mutation

--- packages/smb/src/components/pages/login/login.tsx
+++ packages/smb/src/components/pages/login/login.tsx
@@ -1,99 +1,85 @@
 import * as React from 'react'
 import { withRouter, RouteComponentProps } from 'react-router'
-import { Input, Button, H1, Level, Alert, isEmail, Formik, Form } from '@reapit/elements'
-import { LoginParams } from '@reapit/cognito-auth'
+import { Input, Button, H1, Level, Alert, Formik, Form } from '@reapit/elements'
 import loginStyles from '@/styles/pages/login.scss?mod'
 import logoImage from '@/assets/images/reapit-graphic.jpg'
 import Routes from '@/constants/routes'
-import { useMutation } from '@apollo/react-hooks'
-import LOGIN_MUTATION from './login-mutation.graphql'
-
-export type LoginMappedActions = {
-  login: (params: LoginParams) => void
-}
+import { useMutation, MutationHookOptions } from '@apollo/react-hooks'
+import LOGIN from './login.graphql'
+import { LoginParams } from '@reapit/cognito-auth'
 
 export type LoginFormValues = {
-  email: string
+  userName: string
   password: string
+}
+
+export type LoginResponse = {
+  accessToken: string
+  refreshToken: string
 }
 
 export type LoginMutationResponse = {
   login: LoginResponse
 }
 
-export type LoginResponse = {
-  token: string
+export const handleOnSubmit = ({ login }) => (values: LoginFormValues) => {
+  login({
+    variables: {
+      userName: values.userName,
+      password: values.password,
+      loginType: 'CLIENT',
+    } as LoginParams,
+  })
 }
 
-export type LoginFormError = {
-  email?: string
-  password?: string
-}
-
-export function validate(values: LoginFormValues) {
-  let errors = {} as LoginFormError
-
-  if (!values.email) {
-    errors.email = 'Required'
-  } else if (!isEmail(values.email)) {
-    errors.email = 'Invalid email address'
-  }
-
-  if (!values.password) {
-    errors.password = 'Required'
-  }
-
-  return errors
+export const handleOnCompleted = ({ history }) => (cache: any, response: LoginMutationResponse) => {
+  const { refreshToken, accessToken } = response?.login
+  localStorage.setItem('refreshToken', refreshToken)
+  localStorage.set('accessToken', accessToken)
+  history.push(Routes.HOME)
 }
 
 export type LoginProps = RouteComponentProps
 
-export const Login: React.FunctionComponent<LoginProps> = ({ history }) => {
+export const Login: React.FC<LoginProps> = ({ history }: LoginProps) => {
   const { wrapper, container, image } = loginStyles
 
-  const [login, { data, loading, error }] = useMutation<LoginMutationResponse, LoginFormValues>(LOGIN_MUTATION)
-  if (!loading && data) {
-    history.push(Routes.HOME)
-  }
-
+  const [login, { loading, error }] = useMutation<LoginMutationResponse, LoginParams>(LOGIN, {
+    onCompleted: handleOnCompleted({ history }),
+  } as MutationHookOptions<LoginMutationResponse, LoginParams>)
+  console.log(loading)
   return (
     <div className={container}>
       <div className={`${wrapper}`}>
         <H1 isCentered>Sign in</H1>
         <p className="pb-8">Welcome to smb</p>
-        <Formik
-          validate={validate}
-          initialValues={{ email: 'admin@yahoo.com', password: '' } as LoginFormValues}
-          onSubmit={values => {
-            login({ variables: { email: values.email, password: values.password } })
-          }}
-        >
-          {() => (
-            <Form data-test="login-form">
-              <Input
-                dataTest="login-email"
-                type="email"
-                labelText="Email"
-                id="email"
-                name="email"
-                placeholder="name@address.com"
-              />
-              <Input
-                dataTest="login-password"
-                type="password"
-                labelText="Password"
-                id="password"
-                name="password"
-                placeholder="Enter your password"
-              />
-              <Level>
-                <Button type="submit" loading={loading} variant="primary" disabled={loading}>
-                  Login
-                </Button>
-              </Level>
-              {error && <Alert message="Login failed, user credentials not recognised" type="danger" />}
-            </Form>
-          )}
+        <Formik initialValues={{ userName: '', password: '' } as LoginFormValues} onSubmit={handleOnSubmit({ login })}>
+          <Form data-test="login-form">
+            <Input
+              required
+              dataTest="login-email"
+              type="email"
+              labelText="Email"
+              id="userName"
+              name="userName"
+              placeholder="Enter your user's name"
+            />
+            <Input
+              required
+              dataTest="login-password"
+              type="password"
+              labelText="Password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+            <Level>
+              <Button type="submit" loading={loading} variant="primary" disabled={loading}>
+                Login
+              </Button>
+            </Level>
+            {error && <Alert message="Login failed, user credentials not recognised" type="danger" />}
+          </Form>
         </Formik>
       </div>
 
