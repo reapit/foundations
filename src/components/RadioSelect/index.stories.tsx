ed488737d14a37738d95b6442b6bfeb9c63ab888
Khac Vy <khacvy93@gmail.com>
2019-12-24 17:14:57 +0700

    Fix disabled and error state in radio button (#193)

--- src/components/RadioSelect/index.stories.tsx
+++ src/components/RadioSelect/index.stories.tsx
@@ -6,15 +6,40 @@ import { Formik, Form } from 'formik'
 import { action } from '@storybook/addon-actions'
 import { Button } from '../Button'
 
-storiesOf('RadioSelect', module).add('Primary', () => {
-  const mockProps = {
-    name: 'mockName',
-    labelText: 'mockLabelText',
-    id: 'mockId',
-    dataTest: 'mockDatatest',
-    options: [{ label: 'label', value: 'value' }, { label: 'label1', value: 'value1' }]
-  }
-  return (
+const mockProps = {
+  name: 'mockName',
+  labelText: 'mockLabelText',
+  id: 'mockId',
+  dataTest: 'mockDatatest',
+  options: [{ label: 'label', value: 'value' }, { label: 'label1', value: 'value1' }]
+}
+
+storiesOf('RadioSelect', module)
+  .add('Primary', () => {
+    return (
+      <section className="section">
+        <Formik
+          initialValues={{ [mockProps.name]: '' }}
+          onSubmit={values => {
+            action('Form Values' + values)
+            console.log(values)
+          }}
+        >
+          {({ setFieldValue, values }) => (
+            <Form>
+              <div className="column is-half-desktop">
+                <RadioSelect setFieldValue={setFieldValue} state={values[mockProps.name]} {...mockProps} />
+              </div>
+              <Button variant={'primary'} type={'submit'}>
+                Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </section>
+    )
+  })
+  .add('Disabled', () => (
     <section className="section">
       <Formik
         initialValues={{ [mockProps.name]: '' }}
@@ -23,17 +48,11 @@ storiesOf('RadioSelect', module).add('Primary', () => {
           console.log(values)
         }}
       >
-        {({ setFieldValue, values }) => (
+        {({ setFieldValue }) => (
           <Form>
-            <div className="column is-half-desktop">
-              <RadioSelect setFieldValue={setFieldValue} state={values[mockProps.name]} {...mockProps} />
-            </div>
-            <Button variant={'primary'} type={'submit'}>
-              Submit
-            </Button>
+            <RadioSelect disabled setFieldValue={setFieldValue} state={'value'} {...mockProps} />
           </Form>
         )}
       </Formik>
     </section>
-  )
-})
+  ))
