514a386392c467ba1fc866986587a62276beda4b
Duong Pham <tanphamhaiduong@gmail.com>
2019-12-27 16:42:51 +0700

    FIX: remove unuse code from mailer

--- src/app.dev.ts
+++ src/app.dev.ts
@@ -3,7 +3,6 @@ import yaml from 'yaml'
 import path from 'path'
 import fs from 'fs'
 import server from './core/server'
-import ejs from 'ejs'
 ;(global as any).navigator = {}
 
 const envFile = fs.readFileSync(path.join(__dirname, '../env.yml'), 'utf8')
@@ -13,10 +12,5 @@ process.env.COGNITO_USERPOOL_ID = env.dev.COGNITO_USERPOOL_ID
 process.env.COGNITO_CLIENT_ID = env.dev.COGNITO_CLIENT_ID
 
 const port = 3000
-
-server.engine('ejs', ejs.renderFile)
-server.set('view engine', 'ejs')
-server.set('views', path.join(__dirname, '/mailer/templates/ejs'))
-server.get('/emails', (_req, res) => res.render('./index', { userName: 'tanphamhaiduong@gmail.com' }))
 
 server.listen(port, () => console.log(`Dev app listening on port ${port}!`))
