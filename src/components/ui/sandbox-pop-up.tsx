64f87f3a7a8a005595d711667b9e76c41949fea5
Cuong Vu <vuhuucuong1310@gmail.com>
2019-12-31 17:29:00 +0700

    [CLD-589] As a ‘Developer’, I want to see a message appear when I access the Apps and API pages (#233)
    * [CLD-589] As a ‘Developer’, I want to see a message appear when I access the Apps and API pages
    
    * Fix as comment

---
+++ src/components/ui/sandbox-pop-up.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { ToastMessage } from '@reapit/elements'
+import styles from '@/styles/blocks/sandbox-pop-up.scss?mod'
+
+export const HALF_SECOND = 500
+
+export const popUp = (setOpen, loading) => () => {
+  if (!loading) setTimeout(setOpen, HALF_SECOND)
+}
+
+export const SandboxPopUp = ({
+  loading = false,
+  message = 'Data in the dev portal APIs is sandbox and is regularly refreshed. You should not use these feeds for production apps, they are for development purposes only.'
+}) => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(false)
+  const setOpen = React.useCallback(setIsOpen.bind(null, true), [setIsOpen])
+  const setClose = React.useCallback(setIsOpen.bind(null, false), [setIsOpen])
+
+  React.useEffect(popUp(setOpen, loading), [loading])
+
+  return (
+    <div className={styles['wrap-pop-up']}>
+      <ToastMessage visible={isOpen} message={message} variant="info" onCloseToast={setClose} />
+    </div>
+  )
+}
