29de233af732d135caaefe8ec2ed9405a0d7aa26
Cuong Vu <vuhuucuong1310@gmail.com>
2020-01-03 16:10:22 +0700

    [CLD-589] Increase pop up duration to 10s (#240)

--- src/components/ui/sandbox-pop-up.tsx
+++ src/components/ui/sandbox-pop-up.tsx
@@ -3,6 +3,7 @@ import { ToastMessage } from '@reapit/elements'
 import styles from '@/styles/blocks/sandbox-pop-up.scss?mod'
 
 export const HALF_SECOND = 500
+export const TEN_SECONDS = 10000
 
 export const popUp = (setOpen, loading) => () => {
   if (!loading) setTimeout(setOpen, HALF_SECOND)
@@ -10,7 +11,8 @@ export const popUp = (setOpen, loading) => () => {
 
 export const SandboxPopUp = ({
   loading = false,
-  message = 'Data in the dev portal APIs is sandbox and is regularly refreshed. You should not use these feeds for production apps, they are for development purposes only.'
+  message = 'Data in the dev portal APIs is sandbox and is regularly refreshed. You should not use these feeds for production apps, they are for development purposes only.',
+  displayDuration = TEN_SECONDS
 }) => {
   const [isOpen, setIsOpen] = React.useState<boolean>(false)
   const setOpen = React.useCallback(setIsOpen.bind(null, true), [setIsOpen])
@@ -20,7 +22,13 @@ export const SandboxPopUp = ({
 
   return (
     <div className={styles['wrap-pop-up']}>
-      <ToastMessage visible={isOpen} message={message} variant="info" onCloseToast={setClose} />
+      <ToastMessage
+        visible={isOpen}
+        message={message}
+        variant="info"
+        onCloseToast={setClose}
+        displayDuration={displayDuration}
+      />
     </div>
   )
 }
