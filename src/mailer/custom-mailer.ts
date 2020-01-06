19e7e02f5988a45561953dd819bff133d0ce3c29
Duong Pham <tanphamhaiduong@gmail.com>
2020-01-06 12:55:36 +0700

    config: serverless for mailer

--- src/mailer/custom-mailer.ts
+++ src/mailer/custom-mailer.ts
@@ -1,29 +1,27 @@
 import { CognitoUserPoolTriggerHandler } from 'aws-lambda'
 import { forgotPasswordTemplate, confirmRegistrationTemplate } from './templates/index'
 
-export const customMailer: CognitoUserPoolTriggerHandler = async (event, _context, callback) => {
-  if (event.userPoolId === process.env.COGNITO_USERPOOL_ID) {
-    switch (event.triggerSource) {
-      case 'CustomMessage_ForgotPassword':
-        event.response.emailSubject = 'Reapit Foundations: Forgotten Password'
-        // TODO will replace by env
-        const resetPasswordUrl = 'https://dev.marketplace.reapit.com/developer/reset-password'
-        event.response.emailMessage = await forgotPasswordTemplate({
-          verificationCode: event.request.codeParameter as string,
-          userName: event.request.userAttributes.email,
-          url: resetPasswordUrl
-        })
-        break
-      case 'CustomMessage_SignUp':
-        event.response.emailSubject = 'Welcome to Reapit Foundations'
-        // TODO will replace by env
-        const confirmRegistrationUrl = 'https://dev.marketplace.reapit.com/register/confirm'
-        event.response.emailMessage = await confirmRegistrationTemplate({
-          userName: event.request.userAttributes.email,
-          url: confirmRegistrationUrl
-        })
-        break
-    }
+export const sendForgotPasswordMail: CognitoUserPoolTriggerHandler = async (event, _context, callback) => {
+  if (event.userPoolId === process.env.COGNITO_USERPOOL_ID && event.triggerSource === 'CustomMessage_ForgotPassword') {
+    event.response.emailSubject = 'Reapit Foundations: Forgotten Password'
+    const resetPasswordUrl = `${process.env.MARKET_PLACE_URL}/developer/reset-password`
+    event.response.emailMessage = await forgotPasswordTemplate({
+      verificationCode: event.request.codeParameter as string,
+      userName: event.request.userAttributes.email,
+      url: resetPasswordUrl
+    })
+  }
+  callback(null, event)
+}
+
+export const sendConfirmRegistrationMail: CognitoUserPoolTriggerHandler = async (event, _context, callback) => {
+  if (event.userPoolId === process.env.COGNITO_USERPOOL_ID && event.triggerSource === 'CustomMessage_SignUp') {
+    event.response.emailSubject = 'Welcome to Reapit Foundations'
+    const confirmRegistrationUrl = `${process.env.MARKET_PLACE_URL}/register/confirm`
+    event.response.emailMessage = await confirmRegistrationTemplate({
+      userName: event.request.userAttributes.email,
+      url: confirmRegistrationUrl
+    })
   }
   callback(null, event)
 }
