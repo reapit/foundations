6f0e0bd8a60f91e33236703a84830171167cbe81
Nghia Pham <nghiapn@dwarvesv.com>
2020-01-06 13:16:56 +0700

    [CLD-679]Update release-master script: use the lastest SHA to release, get repo and owner name from env GITHUB_REPOSITORY instead of hardcode values. Update workflow check.yml to use SSH credential to authenticate git action

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -10,11 +10,13 @@ const packageJsonPath = path.resolve(__dirname, '../package.json')
 module.exports = () => {
   const remotePackageVersionStdOut = execSync(`yarn info ${npm_package_name} version`).toString().trim().split('\n')
   let remotePackageVersion = ''
-  if (!remotePackageVersionStdOut || !(typeof Array.isArray(remotePackageVersionStdOut)) || remotePackageVersionStdOut.length < 3) {
+  
+  // Ex of remotePackageVersionStdOut: ['0.0.0']
+  if (!remotePackageVersionStdOut || !(typeof Array.isArray(remotePackageVersionStdOut)) || remotePackageVersionStdOut.length !== 1) {
     return
   }
 
-  remotePackageVersion = remotePackageVersionStdOut[1]
+  remotePackageVersion = remotePackageVersionStdOut[0]
   const bumpedVersion = semver.inc(remotePackageVersion, 'patch')
   
   const packageJsonContent = fs.readFileSync(packageJsonPath).toString()
@@ -22,10 +24,14 @@ module.exports = () => {
   parsedPackageJsonContent.version = bumpedVersion
   fs.writeFileSync(packageJsonPath, JSON.stringify(parsedPackageJsonContent, null, 2))
 
+  execSync(`git remote add sshOrigin git@github.com:${process.env.GITHUB_REPOSITORY}.git`)
+  execSync(`git config --global user.email "GithubActions@email.com"`);
+  execSync(`git config --global user.name "Github Actions"`);
+
   execSync("git add .");
   execSync(
     `git commit -m 'Update TypeScript definition - version: ${bumpedVersion}'`
   );
   execSync(`yarn publish`)
-  execSync(`git push -u authOrigin HEAD:master`);
+  execSync(`git push -u sshOrigin HEAD:master`);
 }
