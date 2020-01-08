95aa2a5ead5e84f33764c6119cd331af9dae035a
Duong Pham <tanphamhaiduong@gmail.com>
2020-01-08 11:42:01 +0700

    refactor: remove unuse code and scripts

--- src/services/password/__tests__/reset-password.ts
+++
@@ -1,9 +0,0 @@
-import { resetPasswordService } from '../reset-password'
-
-jest.mock('amazon-cognito-identity-js', () => require('../../../__mocks__/cognito-session').mockCognito)
-
-describe('resetPasswordService', () => {
-  it('should return a success message', async () => {
-    expect(await resetPasswordService({ userName: 'bob@acme.com' })).toEqual('SUCCESS')
-  })
-})
