d0c8482b0242a8edbfaa5998daaf837d16b84f1f
Cuong Vu <vuhuucuong1310@gmail.com>
2020-01-13 15:47:38 +0700

    [CLD-687] Change the toast notification in the Developers Portal from 10 seconds to permanent (#264)

--- src/components/ui/sandbox-pop-up.tsx
+++ src/components/ui/sandbox-pop-up.tsx
@@ -3,7 +3,6 @@ import { ToastMessage } from '@reapit/elements'
 import styles from '@/styles/blocks/sandbox-pop-up.scss?mod'
 
 export const HALF_SECOND = 500
-export const TEN_SECONDS = 10000
 
 export const popUp = (setOpen, loading) => () => {
   if (!loading) setTimeout(setOpen, HALF_SECOND)
@@ -11,8 +10,7 @@ export const popUp = (setOpen, loading) => () => {
 
 export const SandboxPopUp = ({
   loading = false,
-  message = 'Data in the dev portal APIs is sandbox and is regularly refreshed. You should not use these feeds for production apps, they are for development purposes only.',
-  displayDuration = TEN_SECONDS
+  message = 'Data in the dev portal APIs is sandbox and is regularly refreshed. You should not use these feeds for production apps, they are for development purposes only.'
 }) => {
   const [isOpen, setIsOpen] = React.useState<boolean>(false)
   const setOpen = React.useCallback(setIsOpen.bind(null, true), [setIsOpen])
@@ -22,13 +20,7 @@ export const SandboxPopUp = ({
 
   return (
     <div className={styles['wrap-pop-up']}>
-      <ToastMessage
-        visible={isOpen}
-        message={message}
-        variant="info"
-        onCloseToast={setClose}
-        displayDuration={displayDuration}
-      />
+      <ToastMessage visible={isOpen} message={message} variant="info" onCloseToast={setOpen} />
     </div>
   )
 }
