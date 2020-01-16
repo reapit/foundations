5ece1177ef58207ae95340090d560901e681e220
vuhuucuong <vuhuucuong1310@gmail.com>
2020-01-16 15:27:15 +0700

    [CLD-724] Fix status not showing

---
+++ src/utils/map-identities-to-contacts.ts
@@ -0,0 +1,23 @@
+import { PagedResultContactIdentityCheckModel_, PagedResultContactModel_ } from '@reapit/foundations-ts-definitions'
+
+export const mapIdentitiesToContacts = (
+  responseContacts: PagedResultContactModel_,
+  responseIdentities: PagedResultContactIdentityCheckModel_
+) => {
+  /**
+   * for each contact, find the first identity in the list, take status and set as identityCheck
+   */
+  const responseContactsEmbeddedWithStatus = responseContacts._embedded?.map(({ id, ...rest }) => {
+    const identity = responseIdentities._embedded?.find(({ contactId }) => contactId === id)
+
+    return {
+      ...rest,
+      id,
+      identityCheck: identity?.status
+    }
+  })
+  return {
+    ...responseContacts,
+    _embedded: responseContactsEmbeddedWithStatus
+  }
+}
