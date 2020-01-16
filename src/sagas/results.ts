74784e482f0bc3a87a42da9f5ae0c2da7881f2a0
vuhuucuong <vuhuucuong1310@gmail.com>
2020-01-16 18:00:32 +0700

    [CLD-724] Fix status not showing

--- src/sagas/results.ts
+++ src/sagas/results.ts
@@ -8,17 +8,28 @@ import { URLS } from '@/constants/api'
 import { resultReceiveData, resultRequestDataFailure, ContactsParams } from '@/actions/results'
 import { CONTACTS_PER_PAGE } from '@/constants/paginator'
 import { initAuthorizedRequestHeaders } from '@/utils/api'
+import { mapIdentitiesToContacts } from '@/utils/map-identities-to-contacts'
 
 export const resultFetch = function*(params: Action<ContactsParams>) {
   try {
     const headers = yield call(initAuthorizedRequestHeaders)
-    const response = yield call(fetcher, {
+    const responseContacts = yield call(fetcher, {
       url: `${URLS.contacts}/?${setQueryParams({ ...params.data, pageSize: CONTACTS_PER_PAGE })}`,
       api: process.env.PLATFORM_API_BASE_URL as string,
       method: 'GET',
-      headers: headers
+      headers
     })
-    yield put(resultReceiveData(response))
+
+    const listContactId = responseContacts._embedded.map(({ id }) => id)
+    /* fetch all identities which have contactId in listContactId */
+    const responseIdentities = yield call(fetcher, {
+      url: `${URLS.idChecks}/?${setQueryParams({ ContactId: [...listContactId] })}`,
+      api: process.env.PLATFORM_API_BASE_URL as string,
+      method: 'GET',
+      headers
+    })
+    const responseContactsWithStatus = mapIdentitiesToContacts(responseContacts, responseIdentities)
+    yield put(resultReceiveData(responseContactsWithStatus))
   } catch (err) {
     yield put(resultRequestDataFailure())
     yield put(
