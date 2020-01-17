71980220a9665197e50b53c2973bc037cf43cd3c
Dan Nguyen <45675930+danndz@users.noreply.github.com>
2020-01-17 11:15:31 +0700

    [CLD-477] Create UI button group Area and Range (#279)

--- src/components/pages/__tests__/admin-stats.tsx
+++ src/components/pages/__tests__/admin-stats.tsx
@@ -7,4 +7,83 @@ describe('Admin Stats', () => {
     const wrapper = shallow(<AdminStats />)
     expect(wrapper).toMatchSnapshot()
   })
+
+  describe('setArea APPS', () => {
+    const wrapper = shallow(<AdminStats />)
+    const button = wrapper.find('[dataTest="area-apps-btn"]')
+    button.simulate('click')
+
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('button APPS should have variant primary', () => {
+      expect(button.prop('variant')).toEqual('primary')
+    })
+  })
+
+  describe('setArea DEVELOPRES', () => {
+    const wrapper = shallow(<AdminStats />)
+    wrapper.find('[dataTest="area-developers-btn"]').simulate('click')
+
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('button DEVELOPERS should have variant primary', () => {
+      expect(wrapper.find('[dataTest="area-developers-btn"]').prop('variant')).toEqual('primary')
+    })
+  })
+
+  describe('setArea INSTALLATIONS', () => {
+    const wrapper = shallow(<AdminStats />)
+    wrapper.find('[dataTest="area-installations-btn"]').simulate('click')
+
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('button INSTALLATIONS should have variant primary', () => {
+      expect(wrapper.find('[dataTest="area-installations-btn"]').prop('variant')).toEqual('primary')
+    })
+  })
+
+  describe('setRange WEEK', () => {
+    const wrapper = shallow(<AdminStats />)
+    wrapper.find('[dataTest="range-week-btn"]').simulate('click')
+
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('button LAST WEEK should have variant primary', () => {
+      expect(wrapper.find('[dataTest="range-week-btn"]').prop('variant')).toEqual('primary')
+    })
+  })
+
+  describe('setRange MONTH', () => {
+    const wrapper = shallow(<AdminStats />)
+    wrapper.find('[dataTest="range-month-btn"]').simulate('click')
+
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('button LAST MONTH should have variant primary', () => {
+      expect(wrapper.find('[dataTest="range-month-btn"]').prop('variant')).toEqual('primary')
+    })
+  })
+
+  describe('setRange ALL', () => {
+    const wrapper = shallow(<AdminStats />)
+    wrapper.find('[dataTest="range-all-btn"]').simulate('click')
+
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+
+    it('button ALL TIME should have variant primary', () => {
+      expect(wrapper.find('[dataTest="range-all-btn"]').prop('variant')).toEqual('primary')
+    })
+  })
 })
