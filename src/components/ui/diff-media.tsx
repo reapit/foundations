9cf19b332fbdbd55dcef9c43425f0088a7fc595b
Pham Hai Duong <tanphamhaiduong@gmail.com>
2020-01-17 22:13:48 +0700

    fix: cld 275 image not display in diff (#281)
    * fix: image not display in diff for app approve modal
    
    * fix: cld-275 image not display in diff of approval modal
    
    * fix: as comment from cuong

--- src/components/ui/diff-media.tsx
+++ src/components/ui/diff-media.tsx
@@ -10,14 +10,14 @@ export interface DiffMediaProps {
 const DiffMedia = ({ currentMedia, changedMedia, type }: DiffMediaProps) => {
   const blockStyle = type === 'icon' ? styles.iconBlock : styles.mediaBlock
   const imageStyle = type === 'icon' ? styles.iconImage : styles.mediaImage
-
+  const isDiff = currentMedia !== changedMedia
   return (
     <div className={styles.container}>
-      <div className={`${styles.block} ${blockStyle} ${styles.red}`}>
+      <div className={`${styles.block} ${blockStyle} ${isDiff ? styles.red : ''}`}>
         <div className={`${styles.image} ${imageStyle}`} style={{ backgroundImage: `url(${currentMedia})` }} />
       </div>
       <span className={styles.arrow}>&#8594;</span>
-      <div className={`${styles.block} ${blockStyle} ${styles.green}`}>
+      <div className={`${styles.block} ${blockStyle} ${isDiff ? styles.green : ''}`}>
         <div className={`${styles.image} ${imageStyle}`} style={{ backgroundImage: `url(${changedMedia})` }} />
       </div>
     </div>
