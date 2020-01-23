59df2b92618867ec72bbf48a693b816ec03c6e6d
Will McVay <willmcvay@pm.me>
2020-01-23 16:24:33 +0000

    chore: #35 Migrate Ts Defintions

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,47 +13,51 @@ module.exports = {
     'prettier',
     'prettier/@typescript-eslint',
     'prettier/react',
-    'prettier/standard'
+    'prettier/standard',
   ],
   globals: {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      'jsx': true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'prettier',
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  ignorePatterns: [
+    '__mocks__/',
+    'node_modules/',
+    'setup-tests.ts',
+    'jest.config.js',
+    '.prettierrc.js',
+    'react-app-scaffolder/',
+    'marketplace-api-schema.ts',
+    'platform-schema.ts',
   ],
-  ignorePatterns: ['__mocks__/', 'node_modules/', 'setup-tests.ts', 'jest.config.js', '.prettierrc.js', 'react-app-scaffolder/'],
   rules: {
-    
     'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': false }],
-    'semi': ['error', 'never'],
-    'no-unused-vars': ['error', { 'vars': 'all', 'args': 'after-used' }],
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+    semi: ['error', 'never'],
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used' }],
     '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
     'prettier/prettier': 'error',
-    'max-len': ['error', { 'code': 120, 'ignoreUrls': true }],
-    'no-confusing-arrow': ['error', { 'allowParens': false }],
+    'max-len': ['error', { code: 120, ignoreUrls: true }],
+    'no-confusing-arrow': ['error', { allowParens: false }],
     'no-mixed-operators': 'error',
-    'no-tabs': ['error', {'allowIndentationTabs': true }],
+    'no-tabs': ['error', { allowIndentationTabs: true }],
     'no-unexpected-multiline': 'error',
     // Disabling as conflicts with Prettier
-    'indent': 0,
+    indent: 0,
     // Disabling as we are validating types with TypeScript not PropTypes
-    'react/prop-types': 0 
+    'react/prop-types': 0,
   },
   settings: {
     react: {
-      version: 'detect'
+      version: 'detect',
     },
   },
 }
