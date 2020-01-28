2b2bf7dd242c1ba0bbb69cd4a09d930aecd90e61
Will McVay <willmcvay@pm.me>
2020-01-28 11:12:17 +0000

    chore: fixes s3 deploy script (#108)

--- scripts/release/release-s3.js
+++ scripts/release/release-s3.js
@@ -4,14 +4,20 @@ const { execSync } = require('child_process')
 
 const [, , ...args] = process.argv
 const packageName = args[0]
+const bucketName = args[1]
 
 if (!packageName) {
   console.error('No package name was specified for your deployment')
   process.exit(1)
 }
 
+if (!bucketName) {
+  console.error('No bucket name was specified for your deployment')
+  process.exit(1)
+}
+
 const distPath = path.resolve(__dirname, '../../', 'packages', packageName, 'public', 'dist')
 
 execSync(
-  `aws s3 cp ${distPath} s3://${packageName} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive`,
+  `aws s3 cp ${distPath} s3://${bucketName} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive`,
 )
