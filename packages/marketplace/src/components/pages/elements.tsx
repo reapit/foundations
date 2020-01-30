6038a2f910488fe4846e49c6a780a642edf23d71
Will McVay <willmcvay@pm.me>
2020-01-30 10:08:37 +0000

    chore: #40 finished tidy up of docs (#135)

--- packages/marketplace/src/components/pages/elements.tsx
+++ packages/marketplace/src/components/pages/elements.tsx
@@ -1,27 +1,13 @@
 import * as React from 'react'
 import ErrorBoundary from '@/components/hocs/error-boundary'
-import { Content, FlexContainerBasic, FlexContainerResponsive, H3 } from '@reapit/elements'
+import { FlexContainerBasic } from '@reapit/elements'
+import { IFRAME_URLS } from '../../constants/iframe-urls'
 
 const ElementsPage: React.SFC = () => (
   <ErrorBoundary>
     <FlexContainerBasic flexColumn hasPadding>
-      <Content>
-        <FlexContainerResponsive flexColumn hasBackground hasPadding>
-          <H3>Elements </H3>
-          <p>
-            Elements is a React UI Component and utility library we have developed internally and open sourced. The
-            Developer Portal and Marketplace itself uses Elements extensively and if you are building a React app to be
-            installed by clients, we recommend you do too.
-          </p>
-          <p>
-            When using this page, you can toggle between code examples and rendered output of components by using the
-            &lsquo;Canvas&rsquo; and &lsquo;Docs&rsquo; tabs. For general usage and other NPM packages, visit the
-            package documentation.
-          </p>
-        </FlexContainerResponsive>
-      </Content>
       <FlexContainerBasic className="container">
-        <iframe style={{ border: 'none' }} src="https://reapit.github.io/elements/" width="100%" height="100%" />
+        <iframe style={{ border: 'none' }} src={IFRAME_URLS.elements} width="100%" height="100%" />
       </FlexContainerBasic>
     </FlexContainerBasic>
   </ErrorBoundary>
