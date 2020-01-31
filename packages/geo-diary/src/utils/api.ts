c8ca2a43148cfa2b7e83626faf46e7620f28c12a
Thanh Pham <zlatan.pham@gmail.com>
2020-01-31 10:59:36 +0700

    chore: #96 add api version to geo diary (#143)

--- packages/geo-diary/src/utils/api.ts
+++ packages/geo-diary/src/utils/api.ts
@@ -1,7 +1,8 @@
 import { getAccessToken } from '@/utils/session'
-import { APPOINTMENTS_HEADERS } from '@/constants/api'
+import { APPOINTMENTS_HEADERS, API_VERSION } from '@/constants/api'
 
 export const initAuthorizedRequestHeaders = async () => ({
   ...APPOINTMENTS_HEADERS,
   Authorization: `Bearer ${await getAccessToken()}`,
+  'api-version': API_VERSION,
 })
