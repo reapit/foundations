7746cb9719fe69e2f20e225749ac039951fd4ff3
Duong Pham <tanphamhaiduong@gmail.com>
2020-01-31 20:29:27 +0700

    feat: #120 release npm package

--- packages/cognito-custom-mail-lambda/public/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const custom_mailer_1 = require("./mailer/custom-mailer");
-exports.cognitoCustomMailerHandler = (event, context, callback) => custom_mailer_1.customMailer(event, context, callback);
--- scripts/release/utils.js
+++ scripts/release/utils.js
@@ -2,12 +2,12 @@
 const { execSync } = require('child_process')
 const Octokit = require('@octokit/rest')
 
-export const removeUnuseChar = value => {
+const removeUnuseChar = value => {
   return value.replace(/(\r\n\t|\n|\r\t)/gm, '')
 }
 
-export const getVersionTag = () => {
-  const tagName = execSync('git describe --tags $(git rev-list --tags --max-count=1)').toString()
+const getVersionTag = () => {
+  const tagName = execSync('git describe --always --tags $(git rev-list --tags --max-count=1)').toString()
   const tagNameArr = removeUnuseChar(tagName).split('_')
   const PACKAGE_NAME_INDEX = 0
   const VERSION_INDEX = 1
@@ -19,54 +19,94 @@ export const getVersionTag = () => {
   }
 }
 
-export const getPreviousTag = ({ packageName }) => {
-  const tagName = execSync(`git describe --tags $(git rev-list --tags ) | grep ${packageName}`).toString()
+const getPreviousTag = ({ packageName }) => {
+  const tagName = execSync(`git describe --always --tags $(git rev-list --tags) | grep ${packageName}`).toString()
   const tagNameArr = tagName.split('\n')
   const PREVIOUS_TAG_INDEX = 1
-  return tagNameArr[PREVIOUS_TAG_INDEX]
+  if (tagNameArr[PREVIOUS_TAG_INDEX]) {
+    tagNameArr[PREVIOUS_TAG_INDEX]
+  }
+  return ''
 }
 
-export const formatReleaseNote = ({ previousTag, packageName, version, commitLog }) => {
-  let releaseNote = `
-    Release: ${packageName}_${version}
-    Rollback: ${previousTag}
-    Changes:
-    commit | author | description
-  `
-  const commitLogArr = commitLog.split('\n')
-  for (let i = 0; i < commitLogArr.length; i += 6) {
-    const COMMIT_INDEX = i
-    const COMMIT_AUTHOR_INDEX = i + 1
-    const COMMIT_MESSAGE_INDEX = i + 4
-    releaseNote = releaseNote.concat(`
-      ${commitLogArr[COMMIT_INDEX].replace('commit ', '')} | ${commitLogArr[COMMIT_AUTHOR_INDEX].replace(
-      'Author: ',
-      '',
-    )} | ${commitLogArr[COMMIT_MESSAGE_INDEX]}
-    `)
-  }
-
+const appendCommitInfo = ({ releaseNote, commitLogArr }) => {
+  let newReleaseNote = releaseNote
+  const COMMIT_INDEX = 0
+  const COMMIT_AUTHOR_INDEX = 1
   releaseNote = releaseNote.concat(`
-    approver: @Will McVay
-    monitor: https://sentry.io/organizations/reapit-ltd/projects/
-  `)
+- ${commitLogArr[COMMIT_INDEX]} | ${
+    commitLogArr[COMMIT_AUTHOR_INDEX]
+      ? commitLogArr[COMMIT_AUTHOR_INDEX].replace('Author: ', '')
+      : commitLogArr[COMMIT_AUTHOR_INDEX]
+  } | `)
+  return newReleaseNote
+}
+
+const appendCommitMessage = ({ releaseNote, commitLogArr }) => {
+  let newReleaseNote = releaseNote
+  for (let i = 4; i < commitLogArr.length; i++) {
+    newReleaseNote = releaseNote.concat(
+      `${commitLogArr[i] ? commitLogArr[i].replace('\n').replace(/\s{2,}/g, '') : ''}`,
+    )
+  }
+  return newReleaseNote
+}
+
+const formatReleaseNote = ({ previousTag, packageName, version, commitLog }) => {
+  let releaseNote = `
+Release: ${packageName}_${version}
+Rollback: ${previousTag}
+Changes:
+commit | author |description
+  `
+  const footer = `
+
+approver: @willmcvay
+monitor: https://sentry.io/organizations/reapit-ltd/projects/`
+  if (!commitLog) {
+    return releaseNote.concat(footer)
+  }
+  const commitArr = commitLog.split('commit ')
+  commitArr.forEach(item => {
+    const commitLogArr = item.split('\n')
+    if (commitLogArr.length > 1) {
+      releaseNote = appendCommitInfo({ releaseNote, commitLogArr })
+      releaseNote = appendCommitMessage({ releaseNote, commitLogArr })
+    }
+  })
+  releaseNote = releaseNote.concat(footer)
   return releaseNote
 }
 
-export const editReleaseNote = async ({ packageName, version, previousTag }) => {
-  const commitLog = execSync(`git log ${packageName}-${version}...${previousTag} -- packages/${packageName}`).toString()
-  const token = process.env.GITHUB_TOKEN
-  const octokit = new Octokit({ auth: token })
-  const latestRelease = await octokit.repos.getReleaseByTag({
-    owner: 'reapit',
-    repo: 'foundations',
-    tag: `${packageName}-${version}`,
-  })
-  const bodyRelease = formatReleaseNote({ previousTag, version, packageName, commitLog })
-  await octokit.repos.updateRelease({
-    owner: 'reapit',
-    repo: 'foundations',
-    body: bodyRelease,
-    release_id: latestRelease.id,
-  })
+const editReleaseNote = async ({ packageName, version, previousTag }) => {
+  try {
+    const commitLog = execSync(`git log ${packageName}_${version}...${previousTag}`).toString()
+    const token = process.env.GITHUB_TOKEN
+    const octokit = new Octokit({ auth: token })
+    const latestRelease = await octokit.repos.getReleaseByTag({
+      owner: 'reapit',
+      repo: 'foundations',
+      tag: `${packageName}_${version}`,
+    })
+    if (latestRelease && latestRelease.data) {
+      const bodyRelease = formatReleaseNote({ previousTag, version, packageName, commitLog })
+      await octokit.repos.updateRelease({
+        owner: 'reapit',
+        repo: 'foundations',
+        body: bodyRelease,
+        name: `${latestRelease.data.name}_released`,
+        release_id: latestRelease.data.id,
+      })
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = {
+  removeUnuseChar,
+  getVersionTag,
+  getPreviousTag,
+  formatReleaseNote,
+  editReleaseNote,
 }
