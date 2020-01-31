fff03a7377087c91958d54be1cd34c1e6f2df9fa
Cuong Vu <vuhuucuong1310@gmail.com>
2020-01-31 22:41:26 +0700

    fix: #84 general ui fixes for marketplace (#150)
    * fix: #84 general ui fixes for marketplace
    
    * fix: remove css important, change render logic

--- packages/marketplace/src/components/pages/developer-submit-app-successfully.tsx
+++ packages/marketplace/src/components/pages/developer-submit-app-successfully.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { FlexContainerResponsive } from '@reapit/elements'
 import CallToAction from '../ui/call-to-action'
+import styles from '@/styles/pages/developer-submit-app-successfully.scss?mod'
 
 export interface DeveloperSubmitAppSuccessfullyProps {
   onClickHandler: () => void
 }
 export const DeveloperSubmitAppSuccessfully: React.FC<DeveloperSubmitAppSuccessfullyProps> = ({ onClickHandler }) => (
-  <FlexContainerResponsive hasPadding flexColumn>
+  <FlexContainerResponsive hasPadding flexColumn centerContent className={styles.wrapDeveloperSuccess}>
     <FlexContainerResponsive flexColumn hasBackground hasPadding>
       <CallToAction
         dataTest="submit-success-section"
@@ -16,19 +17,25 @@ export const DeveloperSubmitAppSuccessfully: React.FC<DeveloperSubmitAppSuccessf
         buttonDataTest="submit-another-button"
         isCard
       >
+        <p>Your App has been submitted successfully.</p>
+        <br />
         <p>
-          Your App has been submitted successfully. For it to be available on the Marketplace, you will need to make it
-          ‘Listed’. To do this, click ‘Apps’ from the menu bar, open your submitted App and click ‘Edit Details’, simply
-          tick the ‘Is Listed’ checkbox.
+          For it to be available on the Marketplace, you will need to make it ‘Listed’. To do this, click ‘Apps’ from
+          the menu bar, open your submitted App and click ‘Edit Details’, simply tick the ‘Is Listed’ checkbox.
         </p>
+        <br />
         <p>
           You can also use this section to make any additional changes. Once you are ready to proceed, click ‘Submit
           App’. Our Administration department will then review the details you have submitted. Whilst this review is
           taking place, this App will be marked as ‘Pending Revision’ and you will not be able to make any further
           changes.
         </p>
+        <br />
         <p>You can check the status of any submissions by accessing ‘Apps’ from the menu bar.</p>
+        <br />
         <p>For any help or support, please visit the ‘Help’ page.</p>
+
+        <br />
       </CallToAction>
     </FlexContainerResponsive>
   </FlexContainerResponsive>
