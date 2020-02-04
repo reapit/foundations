b8498be05b3f5f525954f90026eac780694b3318
Nghia Pham <nghiapn@dwarvesv.com>
2020-02-04 10:35:10 +0700

    chore: add comments to scripts

--- scripts/webpack/webpack-sass-prod/index.js
+++ scripts/webpack/webpack-sass-prod/index.js
@@ -1,3 +1,14 @@
+// flows
+// glob the tsx, ts of src of elements/src
+// build elements scss-es in a single css
+// purge it against contents of files in {1}
+// webpack sass config's entry will be tweaked
+// dev: include elements/src/styles/index.scss
+// the prod script will:
+// execute 1,2,3
+// collect CSS to scripts/webpack/webpack-sass-prod/.temp/index.css
+// Add index.css to the entry of sass prod build script
+
 const webpack = require('webpack')
 const buildConfiguration = require('./build-configuration')
 const buildElementScss = require('./build-element-scss')
