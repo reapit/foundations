5636f21c2c9c5305d0c477ab0ca93c85136e4e3a
NghiaPham <nghiapn@dwarvesv.com>
2020-02-05 05:13:08 +0700

    fix: #132 - replace hard code login credentials with secret credentials (#166)

--- packages/web-components/.storybook/webpack.config.js
+++ packages/web-components/.storybook/webpack.config.js
@@ -1,26 +1,43 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = async ({ config }) => {
-  
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    exclude: /(node_modules|bower_components|types)/,
-    use: {
-      loader: 'babel-loader'
-    }
-  })
+  // Configurations copied from elements storybook configurations
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      use: [require.resolve('awesome-typescript-loader'), require.resolve('react-docgen-typescript-loader')],
+    },
+    {
+      test: /\.stories\.tsx?$/,
+      loaders: [
+        {
+          loader: require.resolve('@storybook/addon-storysource/loader'),
+          options: { parser: 'typescript' },
+        },
+      ],
+      enforce: 'pre',
+    },
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    },
+  )
 
-  
+  config.resolve.extensions.push('.ts', '.tsx')
+
+  /**
+   * This plugin maps all path aliases in tsconfig.json to webpack alias
+   */
   config.resolve = {
     ...config.resolve,
-    plugins: [new TsconfigPathsPlugin({
-      configFile: path.resolve(__dirname, '../tsconfig.json')
-    })]
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ],
   }
-
-  config.resolve.extensions = [...config.resolve.extensions, '.ts', '.tsx']
-  
-
   return config
 }
--- packages/web-components/src/utils/get-access-token.ts
+++ packages/web-components/src/utils/get-access-token.ts
@@ -1,11 +1,3 @@
-import { setUserSession, LoginSession } from '@reapit/cognito-auth'
-
 export const getAccessToken = async () => {
-  const response = await setUserSession({
-    userName: 'cbryan@reapit.com',
-    password: 'T00lb0x53',
-    loginType: 'CLIENT',
-    mode: 'WEB',
-  })
-  return (response as LoginSession).accessToken
+  return ''
 }
