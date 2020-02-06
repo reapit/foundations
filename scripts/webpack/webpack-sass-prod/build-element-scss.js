f790ffef46c488286ad42d13cf4128c4d7fb4e1b
NghiaPham <nghiapn@dwarvesv.com>
2020-02-06 20:24:41 +0700

    fix: purge css build issue (#193)

--- scripts/webpack/webpack-sass-prod/build-element-scss.js
+++ scripts/webpack/webpack-sass-prod/build-element-scss.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const nodeSass = require('node-sass')
 const path = require('path')
 const glob = require('glob')
@@ -9,12 +10,17 @@ const purifyOptions = require(path.resolve(__dirname, '../../../packages/element
  * then purge it by comparing it against all ts, tsx files in src/elements
  */
 const buildElementScss = async () => {
+  const tempFolderPath = path.resolve(__dirname, './.temp')
+  if (!fs.existsSync(tempFolderPath)) {
+    fs.mkdirSync(tempFolderPath)
+  }
   const result = nodeSass.renderSync({
     file: path.resolve(__dirname, '../../../packages/elements/src/styles/index.scss'),
   })
   const css = result.css.toString()
   const globElementSrc = glob.sync(path.resolve(__dirname, '../../../packages/elements/src/**/*.{ts,tsx}'))
   purifyOptions.output = path.resolve(__dirname, './.temp/index.css')
+
   purifyCss(globElementSrc, css, purifyOptions)
 }
 
