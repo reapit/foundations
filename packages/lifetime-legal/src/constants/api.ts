bd4caf991cb192148ab2a4f88dcef01780ba121f
Thanh Pham <zlatan.pham@gmail.com>
2020-02-06 19:49:30 +0700

    chore: #96 add api version to lifetime legal (#145)
    * chore: #96 add api version to lifetime legal
    
    * chore: updated headers
    
    Co-authored-by: Will McVay <willmcvay@pm.me>

--- packages/lifetime-legal/src/constants/api.ts
+++ packages/lifetime-legal/src/constants/api.ts
@@ -8,6 +8,8 @@ export const COGNITO_HEADERS = {
   'Content-Type': 'application/json',
 } as StringMap
 
+export const API_VERSION = '2020-01-31'
+
 export const URLS = {
   idChecks: '/identityChecks',
   contacts: '/contacts',
--- packages/lifetime-legal/src/utils/api.ts
+++ packages/lifetime-legal/src/utils/api.ts
@@ -1,7 +1,8 @@
 import { getAccessToken } from './session'
-import { CONTACTS_HEADERS } from '../constants/api'
+import { CONTACTS_HEADERS, API_VERSION } from '../constants/api'
 
 export const initAuthorizedRequestHeaders = async () => ({
   ...CONTACTS_HEADERS,
   Authorization: `Bearer ${await getAccessToken()}`,
+  'api-version': API_VERSION,
 })
