f107a434e553c6caf5f18cd2c357df25bad44b34
NghiaPham <nghiapn@dwarvesv.com>
2020-02-06 18:08:22 +0700

    Fix: #102 - purge unused bulma styles from mono repo prod styles (#175)
    * poc
    
    * fix: typos
    
    * chore: add comments to scripts
    
    * rename file purifyOptions to purify-options

--- scripts/webpack/webpack.sass.dev.js
+++ scripts/webpack/webpack.sass.dev.js
@@ -1,7 +1,22 @@
+// flows
+// glob the tsx, ts of src of elements/src
+// build elements scss-es in a single css
+// purge it against contents of files in {1}
+// webpack sass config's entry will be tweaked
+// dev: include elements/src/styles/index.scss
+// the prod script will:
+// execute 1,2,3
+// collect CSS to scripts/webpack/webpack-sass-prod/.temp/index.css
+// Add index.css to the entry of sass prod build script
+
 const webpackBase = require('./webpack.base.dev')
+const { PATHS } = require('./constants')
 
 module.exports = {
-  ...webpackBase,
+  ...{
+    ...webpackBase,
+    entry: [...webpackBase.entry, PATHS.elementsIndexSass],
+  },
   module: {
     rules: [
       ...webpackBase.module.rules,
