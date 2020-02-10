b2a78bfad5f3b0162d218de5632defff557a8b75
Will McVay <willmcvay@pm.me>
2020-02-10 07:52:03 +0000

    fix: oauth LTL, AML (#212)

--- packages/aml-checklist/src/constants/api.ts
+++ packages/aml-checklist/src/constants/api.ts
@@ -1,10 +1,13 @@
 import { StringMap } from '@/types/core'
+import { COOKIE_SESSION_KEY } from '@reapit/cognito-auth'
 
 export const CONTACTS_HEADERS = {
   'Content-Type': 'application/json',
 } as StringMap
 
 export const API_VERSION = '2020-01-31'
+
+export const COOKIE_SESSION_KEY_AML_APP = `${COOKIE_SESSION_KEY}-aml-app`
 
 export const URLS = {
   contacts: '/contacts',
--- packages/lifetime-legal/src/components/pages/login.tsx
+++ packages/lifetime-legal/src/components/pages/login.tsx
@@ -1,88 +1,25 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-import { Dispatch } from 'redux'
-import { withRouter, RouteComponentProps } from 'react-router'
+import { withRouter } from 'react-router'
 import { ReduxState } from '@/types/core'
-import { authLogin } from '@/actions/auth'
-import { validate } from '@/utils/form/login'
 import Routes from '@/constants/routes'
-import { LOGIN_TYPE } from '@/constants/auth'
-import { Input, Button, Alert, Level, Formik, Form } from '@reapit/elements'
-import { LoginParams } from '@reapit/cognito-auth'
+import { Button, Level } from '@reapit/elements'
+import { redirectToLogin } from '@reapit/cognito-auth'
 
 import loginStyles from '@/styles/pages/login.scss?mod'
 import logoImage from '@/assets/images/lifetimelegalwhite.png'
+import connectImage from '@/assets/images/reapit-connect.png'
 
-export interface LoginMappedActions {
-  login: (params: LoginParams) => void
-}
-
-export interface LoginMappedProps {
+export interface LoginProps {
   hasSession: boolean
-  error: boolean
 }
 
-export interface LoginFormValues {
-  email: string
-  password: string
-}
-
-export type LoginProps = LoginMappedActions & LoginMappedProps & RouteComponentProps
-
-export const handleUseEffect = ({ setIsSubmitting, error }) => () => {
-  if (error) {
-    setIsSubmitting(false)
-  }
-}
-
-export const renderForm = ({ isSubmitting, error }) => () => (
-  <Form data-test="login-form">
-    <Input
-      dataTest="login-email"
-      type="email"
-      labelText="Email"
-      id="email"
-      name="email"
-      placeholder="name@address.com"
-    />
-    <Input
-      dataTest="login-password"
-      type="password"
-      labelText="Password"
-      id="password"
-      name="password"
-      placeholder="Enter your password"
-    />
-
-    <Level>
-      <Button type="submit" loading={isSubmitting} variant="primary" fullWidth disabled={isSubmitting}>
-        Login
-      </Button>
-    </Level>
-
-    {error && <Alert type="danger" message="Login failed, user credentials not recognised" />}
-  </Form>
-)
-
-export const onSubmitHandler = (setIsSubmitting: any, login: any) => (values: LoginFormValues) => {
-  const { email, password } = values
-
-  setIsSubmitting(true)
-  login({
-    userName: email,
-    password,
-    loginType: LOGIN_TYPE.CLIENT,
-    cognitoClientId: process.env.COGNITO_CLIENT_ID_LTL_APP as string,
-  } as LoginParams)
-}
+const loginHandler = () => redirectToLogin(process.env.COGNITO_CLIENT_ID_LTL_APP as string, `${window.location.origin}`)
 
 export const Login: React.FunctionComponent<LoginProps> = (props: LoginProps) => {
-  const [isSubmitting, setIsSubmitting] = React.useState(false)
-  const { hasSession, error, login } = props
-  const { disabled, wrapper, container, imageLogo, wrapperBorder } = loginStyles
-
-  React.useEffect(handleUseEffect({ setIsSubmitting, error }), [error])
+  const { hasSession } = props
+  const { wrapper, container, imageLogo, wrapperBorder } = loginStyles
 
   if (hasSession) {
     return <Redirect to={Routes.SEARCH} />
@@ -90,16 +27,17 @@ export const Login: React.FunctionComponent<LoginProps> = (props: LoginProps) =>
 
   const LoginForm = (
     <div className={container}>
-      <div className={`${wrapper} ${isSubmitting && disabled}`}>
+      <div className={wrapper}>
         <img className={imageLogo} src={logoImage} alt="lifetimelegal" />
         <div className={wrapperBorder}>
-          <Formik
-            validate={validate}
-            initialValues={{ email: '', password: '' } as LoginFormValues}
-            onSubmit={onSubmitHandler(setIsSubmitting, login)}
-          >
-            {renderForm({ isSubmitting, error })}
-          </Formik>
+          <Level>
+            <img src={connectImage} alt="Reapit Connect Graphic" />
+          </Level>
+          <Level>
+            <Button type="button" onClick={loginHandler} loading={false} variant="primary" disabled={false} fullWidth>
+              Login
+            </Button>
+          </Level>
         </div>
       </div>
     </div>
@@ -108,13 +46,8 @@ export const Login: React.FunctionComponent<LoginProps> = (props: LoginProps) =>
   return LoginForm
 }
 
-export const mapStateToProps = (state: ReduxState): LoginMappedProps => ({
+export const mapStateToProps = (state: ReduxState): LoginProps => ({
   hasSession: !!state.auth.loginSession || !!state.auth.refreshSession,
-  error: state.auth.error,
 })
 
-export const mapDispatchToProps = (dispatch: Dispatch): LoginMappedActions => ({
-  login: (params: LoginParams) => dispatch(authLogin(params)),
-})
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Login))
+export default withRouter(connect(mapStateToProps, {})(Login))
