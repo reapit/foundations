e948a971613df608bef5c629586619acfe21d981
Will McVay <willmcvay@pm.me>
2020-02-13 15:45:15 +0000

    fix: revert swagger changes, fix: broken link, feat: base reapit config (#272)
    * chore: fixed broken link to license, add prod badge
    
    * feat: provide base config
    
    * fix: revert swagger breaking changes

--- packages/marketplace/src/components/pages/__tests__/swagger.tsx
+++ packages/marketplace/src/components/pages/__tests__/swagger.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
 
-import Swagger, { handleOnComplete } from '../swagger'
+import Swagger, { handleOnComplete, fetchInterceptor } from '../swagger'
 
 jest.mock('../../../core/store')
 
@@ -9,6 +9,33 @@ describe('Swagger', () => {
   it('should match a snapshot', () => {
     expect(shallow(<Swagger />)).toMatchSnapshot()
   })
+
+  it('should have a fetchInterceptor that adds a token when the url is not swagger', async () => {
+    process.env.SWAGGER_BASE_URL = 'https://some-url.com'
+    const request = {
+      url: 'https://some-other-url.com',
+    }
+    const result = await fetchInterceptor(request)
+    const output = {
+      ...request,
+      headers: {
+        'Content-Type': 'application/json',
+        'api-version': 'latest',
+        Authorization: 'Bearer null',
+      },
+    }
+    expect(result).toEqual(output)
+  })
+
+  it('should have a fetchInterceptor that returns the params when the url is swagger', async () => {
+    process.env.SWAGGER_BASE_URL = 'https://some-url.com'
+    const request = {
+      url: process.env.SWAGGER_BASE_URL,
+    }
+    const result = await fetchInterceptor(request)
+    expect(result).toEqual(request)
+  })
+
   it('handleOnComplete', () => {
     const setLoading = jest.fn()
     const fn = handleOnComplete(setLoading)
--- packages/marketplace/src/components/pages/swagger.tsx
+++ packages/marketplace/src/components/pages/swagger.tsx
@@ -5,8 +5,24 @@ import '../../styles/vendor/swagger.scss'
 import ErrorBoundary from '@/components/hocs/error-boundary'
 import { Loader } from '@reapit/elements'
 import { SandboxPopUp } from '../ui/sandbox-pop-up'
+import { getAccessToken } from '@/utils/session'
+import { StringMap } from '../../types/core'
 
 export const handleOnComplete = setLoading => () => setLoading(false)
+
+export const fetchInterceptor = async (params: StringMap) => {
+  if (params.url === process.env.SWAGGER_BASE_URL) {
+    return params
+  }
+  return {
+    ...params,
+    headers: {
+      'Content-Type': 'application/json',
+      'api-version': 'latest',
+      Authorization: `Bearer ${await getAccessToken()}`,
+    },
+  }
+}
 
 export const SwaggerPage: React.SFC = () => {
   const [loading, setLoading] = React.useState(true)
@@ -16,7 +32,12 @@ export const SwaggerPage: React.SFC = () => {
       <div className="swagger">
         {loading && <Loader />}
         <div className={`${loading ? 'swagger-loading' : ''}`}>
-          <SwaggerUI url={process.env.SWAGGER_BASE_URL} onComplete={handleOnComplete(setLoading)} docExpansion="none" />
+          <SwaggerUI
+            url={process.env.SWAGGER_BASE_URL}
+            onComplete={handleOnComplete(setLoading)}
+            docExpansion="none"
+            requestInterceptor={fetchInterceptor}
+          />
         </div>
         <SandboxPopUp loading={loading} />
       </div>
