2e861e5e1fbed7b469150057ed6afea959e933d6
Will McVay <wmcvay@reapit.com>
2020-02-17 19:50:38 +0000

    fix: scaffolder auth

--- packages/react-app-scaffolder/app/templates/no-redux/src/core/private-route-wrapper.tsx
+++ packages/react-app-scaffolder/app/templates/no-redux/src/core/private-route-wrapper.tsx
@@ -1,47 +1,35 @@
-import * as React from "react";
-import { withRouter, RouteComponentProps } from "react-router-dom";
-import Menu from "@/components/ui/menu";
-import { Loader, AppNavContainer, Section } from "@reapit/elements";
-import {
-  RefreshParams,
-  getTokenFromQueryString,
-  redirectToOAuth,
-  getSessionCookie
-} from "@reapit/cognito-auth";
-import { useAuthContext } from "@/context/auth-context";
-import { COOKIE_SESSION_KEY } from '../constants/api';
+import * as React from 'react'
+import { withRouter, RouteComponentProps } from 'react-router-dom'
+import Menu from '@/components/ui/menu'
+import { Loader, AppNavContainer, Section } from '@reapit/elements'
+import { RefreshParams, getTokenFromQueryString, redirectToOAuth, getSessionCookie } from '@reapit/cognito-auth'
+import { useAuthContext } from '@/context/auth-context'
+import { COOKIE_SESSION_KEY } from '../constants/api'
 
-const { Suspense } = React;
+const { Suspense } = React
 
 export type PrivateRouteWrapperProps = RouteComponentProps & {
-  path: string;
-};
+  path: string
+}
 
-export const PrivateRouteWrapper: React.FunctionComponent<PrivateRouteWrapperProps> = ({
-  children,
-  location
-}) => {
-  const cognitoClientId = process.env.COGNITO_CLIENT_ID_<%= nameInConstantCase %> as string;
+export const PrivateRouteWrapper: React.FunctionComponent<PrivateRouteWrapperProps> = ({ children, location }) => {
+  const cognitoClientId = process.env.COGNITO_CLIENT_ID_<%= nameInConstantCase %> as string
   const cookieParams = getSessionCookie(COOKIE_SESSION_KEY)
-  const urlParams: RefreshParams | null = getTokenFromQueryString(
-    location.search,
-    cognitoClientId
-  );
+  const urlParams: RefreshParams | null = getTokenFromQueryString(location.search, cognitoClientId)
   const refreshParams = cookieParams ? cookieParams : urlParams
-  const { loginSession, getLoginSession, fetching } = useAuthContext();
+  const { loginSession, getLoginSession, fetching } = useAuthContext()
 
   if (!loginSession && !fetching && !refreshParams) {
-    const cognitoClientId = process.env.COGNITO_CLIENT_ID_<%= nameInConstantCase %> as string;
-    redirectToOAuth(cognitoClientId);
+    redirectToOAuth(cognitoClientId)
     return null
   }
 
   if (!loginSession && refreshParams) {
     getLoginSession(refreshParams)
-  } 
+  }
 
   if (!loginSession) {
-    return null;
+    return null
   }
 
   return (
@@ -57,7 +45,7 @@ export const PrivateRouteWrapper: React.FunctionComponent<PrivateRouteWrapperPro
         {children}
       </Suspense>
     </AppNavContainer>
-  );
-};
+  )
+}
 
-export default withRouter(PrivateRouteWrapper);
+export default withRouter(PrivateRouteWrapper)
