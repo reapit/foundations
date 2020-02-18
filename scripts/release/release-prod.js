5e92e03917b4248594101bd504706d4fd3037473
Duong Pham <tanphamhaiduong@gmail.com>
2020-02-18 12:59:47 +0700

    chore: add try catch to catch the error and show on pipeline

--- scripts/release/release-prod.js
+++ scripts/release/release-prod.js
@@ -3,34 +3,39 @@ const path = require('path')
 const { getPreviousTag, editReleaseNote, getVersionTag, runCommand } = require('./utils')
 
 const releaseProd = async () => {
-  const [, , ...args] = process.argv
-  const packageName = args[0]
-  const bucketName = args[1]
-  const { version, packageName: packageNameOnTag } = getVersionTag()
-  if (!packageName) {
-    console.error('No package name was specified for your deployment')
-    process.exit(1)
-  }
+  try {
+    const [, , ...args] = process.argv
+    const packageName = args[0]
+    const bucketName = args[1]
+    const { version, packageName: packageNameOnTag } = getVersionTag()
+    if (!packageName) {
+      console.error('No package name was specified for your deployment')
+      process.exit(1)
+    }
 
-  if (!bucketName) {
-    console.error('No bucket name was specified for your deployment')
-    process.exit(1)
-  }
+    if (!bucketName) {
+      console.error('No bucket name was specified for your deployment')
+      process.exit(1)
+    }
 
-  if (packageName === packageNameOnTag) {
-    const distPath = path.resolve(__dirname, '../../', 'packages', packageName, 'public', 'dist')
-    runCommand('aws', [
-      's3',
-      'cp',
-      distPath,
-      `s3://${bucketName}`,
-      '--grants',
-      'read=uri=http://acs.amazonaws.com/groups/global/AllUsers',
-      '--recursive',
-    ])
-    const previousTag = getPreviousTag({ packageName: packageNameOnTag })
+    if (packageName === packageNameOnTag) {
+      const distPath = path.resolve(__dirname, '../../', 'packages', packageName, 'public', 'dist')
+      runCommand('aws', [
+        's3',
+        'cp',
+        distPath,
+        `s3://${bucketName}`,
+        '--grants',
+        'read=uri=http://acs.amazonaws.com/groups/global/AllUsers',
+        '--recursive',
+      ])
+      const previousTag = getPreviousTag({ packageName: packageNameOnTag })
 
-    await editReleaseNote({ packageName: packageNameOnTag, version, previousTag })
+      await editReleaseNote({ packageName: packageNameOnTag, version, previousTag })
+    }
+  } catch (err) {
+    console.error(err)
+    throw new Error(err)
   }
 }
 
--- scripts/release/release-serverless.js
+++ scripts/release/release-serverless.js
@@ -2,19 +2,24 @@
 const { getPreviousTag, editReleaseNote, getVersionTag, runCommand } = require('./utils')
 
 const releaseServerless = async () => {
-  const [, , ...args] = process.argv
-  const packageName = args[0]
-  const { version, packageName: packageNameOnTag } = getVersionTag()
-  if (!packageName) {
-    console.error('No package name was specified for your deployment')
-    process.exit(1)
-  }
+  try {
+    const [, , ...args] = process.argv
+    const packageName = args[0]
+    const { version, packageName: packageNameOnTag } = getVersionTag()
+    if (!packageName) {
+      console.error('No package name was specified for your deployment')
+      process.exit(1)
+    }
 
-  if (packageName === packageNameOnTag) {
-    runCommand('cross-env', ['CI=true', 'serverless', 'deploy', '--stage', 'prod'])
-    const previousTag = getPreviousTag({ packageName: packageNameOnTag })
+    if (packageName === packageNameOnTag) {
+      runCommand('cross-env', ['CI=true', 'serverless', 'deploy', '--stage', 'prod'])
+      const previousTag = getPreviousTag({ packageName: packageNameOnTag })
 
-    await editReleaseNote({ packageName: packageNameOnTag, version, previousTag })
+      await editReleaseNote({ packageName: packageNameOnTag, version, previousTag })
+    }
+  } catch (err) {
+    console.error(err)
+    throw new Error(err)
   }
 }
 
