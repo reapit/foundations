3b193828e78003debf19ebc0f1e883758adeb1c4
Khac Vy <khacvy93@gmail.com>
2020-02-20 14:12:53 +0700

    feat: #371 Implement resolver for graphql server: negotiator services

--- packages/graphql-server/src/resolvers/negotiators/__mocks__/create-arg.ts
+++ packages/graphql-server/src/resolvers/negotiators/__mocks__/create-arg.ts
@@ -9,4 +9,17 @@ export const createArgStub: CreateNegotiatorModel = {
   mobilePhone: null,
   email: 'abel.robertson@reapitestates.net',
   metadata: {},
+<<<<<<< HEAD
+=======
+  _eTag: '"10109C0209C684789B72FFC53730E31C"',
+  _links: {
+    self: {
+      href: '/negotiators/MGL',
+    },
+    office: {
+      href: '/offices/NPG',
+    },
+  },
+  _embedded: null,
+>>>>>>> feat: #371 Implement resolver for graphql server: negotiator services
 }
