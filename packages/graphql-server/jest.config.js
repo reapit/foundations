25ea0c1b02e024487d2ae1fb07d00fffae7c3962
Cuong Vu <vuhuucuong1310@gmail.com>
2020-02-20 10:35:24 +0700

    fix: reconfigure graphql server for monorepo (#378)
    * fix: reconfigure graphql-server for monorepo
    
    * fix: reconfigure graphql-server for monorepo
    
    * fix: reconfigure graphql-server for monorepo
    
    * fix: add eslint cache
    
    * fix: remove .eslintcache
    
    * fix: fix jest config
    
    * fix: remove jsx react

--- packages/graphql-server/jest.config.js
+++ packages/graphql-server/jest.config.js
@@ -1,16 +1,14 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
 const baseConfig = require('../../scripts/jest/jest.config')
-const { compilerOptions } = require('./tsconfig.json')
 
 module.exports = {
   ...baseConfig,
-  moduleNameMapper: {
-    ...baseConfig.moduleNameMapper,
-    ...pathsToModuleNameMapper(compilerOptions.paths, {
-      prefix: '<rootDir>/',
-    }),
-  },
-  coveragePathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|src/types|src/tests|src/scripts|dist)[/\\\\]', 'api.ts', '.d.ts', 'index.ts'],
+  testPathIgnorePatterns: ['<rootDir>/src/tests/'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>[/\\\\](node_modules|src/types|src/tests|src/scripts|dist)[/\\\\]',
+    'api.ts',
+    '.d.ts',
+    'index.ts',
+  ],
   coverageThreshold: {
     global: {
       branches: 80,
