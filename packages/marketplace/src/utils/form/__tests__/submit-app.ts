3859da6bab7f4b07e178bb2de9a5e664a06aaa6e
Pham Hai Duong <tanphamhaiduong@gmail.com>
2020-02-21 10:25:49 +0700

    fix: validation for submit app (#385)

--- packages/marketplace/src/utils/form/__tests__/submit-app.ts
+++ packages/marketplace/src/utils/form/__tests__/submit-app.ts
@@ -38,10 +38,12 @@ describe('submitAppValidation', () => {
       'authFlow',
     ]
 
-    const output = {}
+    const output = {} as any
     for (let key of validateRequiredKeys) {
       output[key] = errorMessages.FIELD_REQUIRED
     }
+    output.description = 'Must be between 150 and 1000 characters'
+    output.summary = 'Must be between 50 and 150 characters'
 
     expect(validate(input)).toEqual(output)
   })
@@ -58,8 +60,9 @@ describe('submitAppValidation', () => {
       launchUri: 'http://localhost:8000',
       iconImageUrl: 'test',
       homePage: 'http://localhost:8000',
-      description: 'test',
-      summary: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
       scopes: ['scope1'],
       redirectUris: 'https://google.com,https://twitter.com,http://localhost:8080',
       signoutUris: 'http://localhost:8080',
@@ -83,8 +86,9 @@ describe('submitAppValidation', () => {
       launchUri: 'http://localhost:8000',
       iconImageUrl: 'test',
       homePage: 'http://localhost:8000',
-      description: 'test',
-      summary: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
       scopes: ['scope1'],
       redirectUris: 'http://google.com,https://twitter.com,http://localhost:8080',
       signoutUris: 'http://localhost:8080',
@@ -108,8 +112,9 @@ describe('submitAppValidation', () => {
       launchUri: 'http://localhost:8000',
       iconImageUrl: 'test',
       homePage: 'http://localhost:8000',
-      description: 'test',
-      summary: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
       scopes: ['scope1'],
       redirectUris: 'https://google.com,https://twitter.com,http://localhost:8080',
       signoutUris: 'https://localhost:8080',
@@ -133,8 +138,9 @@ describe('submitAppValidation', () => {
       launchUri: 'http://localhost:8000',
       iconImageUrl: 'test',
       homePage: 'test',
-      description: 'test',
-      summary: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
       scopes: [],
       redirectUris: 'https://google.com,https://twitter.com,http://localhost:8080',
       signoutUris: 'https://google.com,https://twitter.com,http://localhost:8080',
@@ -158,8 +164,9 @@ describe('submitAppValidation', () => {
       launchUri: 'test',
       iconImageUrl: 'test',
       homePage: 'http://localhost:8000',
-      description: 'test',
-      summary: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
       scopes: [],
       redirectUris: 'https://google.com,https://twitter.com,http://localhost:8080',
       signoutUris: 'https://google.com,https://twitter.com,http://localhost:8080',
@@ -183,8 +190,9 @@ describe('submitAppValidation', () => {
       launchUri: 'http://localhost:8000',
       iconImageUrl: 'test',
       homePage: 'http://localhost:8000',
-      description: 'test',
-      summary: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
       scopes: ['scope1'],
       redirectUris: 'https://google.com,https://twitter.com,http://localhost:8080',
       signoutUris: 'http://localhost:8080',
@@ -206,15 +214,19 @@ describe('submitAppValidation', () => {
       launchUri: 'http://localhost:8000',
       iconImageUrl: 'test',
       homePage: 'http://localhost:8000',
-      description: 'test',
-      summary: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
       scopes: ['scope1'],
       redirectUris: 'not valid',
       signoutUris: 'not valid',
       authFlow: 'clientCredentials',
     }
 
-    expect(validate(input)).toEqual({})
+    expect(validate(input)).toEqual({
+      redirectUris: 'Invalid redirect uri(s)',
+      signoutUris: 'Invalid sign out uri(s)',
+    })
   })
   it('should validate scopes if authFlow = clientCredentials', () => {
     const input: CustomCreateAppModel = {
@@ -228,15 +240,21 @@ describe('submitAppValidation', () => {
       launchUri: 'test',
       iconImageUrl: 'test',
       homePage: 'test',
-      description: 'test',
-      summary: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
       scopes: ['scope1', 'scope2'],
       redirectUris: 'not valid',
       signoutUris: 'not valid',
       authFlow: 'clientCredentials',
     }
 
-    expect(validate(input)).toEqual({})
+    expect(validate(input)).toEqual({
+      homePage: 'Invalid Home Page URL',
+      launchUri: 'Invalid Launch URI',
+      redirectUris: 'Invalid redirect uri(s)',
+      signoutUris: 'Invalid sign out uri(s)',
+    })
   })
   it('should run correctly with invalid scopes if authFlow = clientCredentials', () => {
     const input: CustomCreateAppModel = {
@@ -250,14 +268,81 @@ describe('submitAppValidation', () => {
       launchUri: 'test',
       iconImageUrl: 'test',
       homePage: 'test',
-      description: 'test',
-      summary: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
       scopes: [],
       redirectUris: 'not valid',
       signoutUris: 'not valid',
       authFlow: 'clientCredentials',
     }
 
-    expect(validate(input)).toEqual({ scopes: 'At least one Permission is required' })
+    expect(validate(input)).toEqual({
+      homePage: 'Invalid Home Page URL',
+      launchUri: 'Invalid Launch URI',
+      redirectUris: 'Invalid redirect uri(s)',
+      scopes: 'At least one Permission is required',
+      signoutUris: 'Invalid sign out uri(s)',
+    })
+  })
+
+  it('should run correctly with invalid description', () => {
+    const input: CustomCreateAppModel = {
+      screen4ImageUrl: 'test',
+      screen3ImageUrl: 'test',
+      screen2ImageUrl: 'test',
+      screen1ImageUrl: 'test',
+      name: 'test',
+      telephone: 'test',
+      supportEmail: 'test@test.com',
+      launchUri: 'test',
+      iconImageUrl: 'test',
+      homePage: 'test',
+      // eslint-disable-next-line
+      description: '123',
+      summary: 'Amazon.com, Inc., is an American multinational technology company based in Seattle',
+      scopes: ['scope1', 'scope2'],
+      redirectUris: 'not valid',
+      signoutUris: 'not valid',
+      authFlow: 'clientCredentials',
+    }
+
+    expect(validate(input)).toEqual({
+      description: 'Must be between 150 and 1000 characters',
+      homePage: 'Invalid Home Page URL',
+      launchUri: 'Invalid Launch URI',
+      redirectUris: 'Invalid redirect uri(s)',
+      signoutUris: 'Invalid sign out uri(s)',
+    })
+  })
+
+  it('should run correctly with invalid summary', () => {
+    const input: CustomCreateAppModel = {
+      screen4ImageUrl: 'test',
+      screen3ImageUrl: 'test',
+      screen2ImageUrl: 'test',
+      screen1ImageUrl: 'test',
+      name: 'test',
+      telephone: 'test',
+      supportEmail: 'test@test.com',
+      launchUri: 'test',
+      iconImageUrl: 'test',
+      homePage: 'test',
+      // eslint-disable-next-line
+      description: 'Amazon.com, Inc., is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook.',
+      summary: '123',
+      scopes: ['scope1', 'scope2'],
+      redirectUris: 'not valid',
+      signoutUris: 'not valid',
+      authFlow: 'clientCredentials',
+    }
+
+    expect(validate(input)).toEqual({
+      homePage: 'Invalid Home Page URL',
+      launchUri: 'Invalid Launch URI',
+      redirectUris: 'Invalid redirect uri(s)',
+      signoutUris: 'Invalid sign out uri(s)',
+      summary: 'Must be between 50 and 150 characters',
+    })
   })
 })
