ea797bb5c3a498f54f5678bdff95eebc0375d991
Duong Pham <duongph@reapit.com>
2020-02-25 15:06:49 +0700

    chore: revert back fetcher from @reapit/elements
    Revert change code in fetcher api

--- packages/graphql-server/src/resolvers/contact-identity-check/api.ts
+++ packages/graphql-server/src/resolvers/contact-identity-check/api.ts
@@ -1,8 +1,9 @@
+import { fetcher } from '@reapit/elements'
 import { CreateContactIdentityCheckArgs } from './contact-identity-check'
 import logger from '../../logger'
 import { ServerContext } from '../../app'
 import errors from '../../errors'
-// import { API_VERSION } from '../../constants/api'
+import { API_VERSION } from '../../constants/api'
 
 export const REAPIT_API_BASE_URL = 'https://dev.platform.reapit.net'
 
@@ -15,21 +16,19 @@ export const callCreateIdentityCheckAPI = async (args: CreateContactIdentityChec
   const traceId = context.traceId
   try {
     logger.info('callCreateIdentityCheckAPI', { traceId, args })
-    // TODO: temporary disable fetcher because it'll add react to dependencies, and no need
-    // const headers = {
-    //   Authorization: context.authorization,
-    //   'Content-Type': 'application/json',
-    //   'api-version': API_VERSION,
-    // }
-    // const createResponse = await fetcher({
-    //   url: `${URLS.contacts}/${args.contactId}${URLS.identityChecks}`,
-    //   api: REAPIT_API_BASE_URL,
-    //   method: 'POST',
-    //   headers,
-    //   body: args,
-    // })
-    // return createResponse
-    return {}
+    const headers = {
+      Authorization: context.authorization,
+      'Content-Type': 'application/json',
+      'api-version': API_VERSION,
+    }
+    const createResponse = await fetcher({
+      url: `${URLS.contacts}/${args.contactId}${URLS.identityChecks}`,
+      api: REAPIT_API_BASE_URL,
+      method: 'POST',
+      headers,
+      body: args,
+    })
+    return createResponse
   } catch (error) {
     logger.error('callCreateIdentityCheckAPI', { traceId, error })
     return errors.generateUserInputError(traceId)
--- packages/graphql-server/src/resolvers/contact/api.ts
+++ packages/graphql-server/src/resolvers/contact/api.ts
@@ -1,8 +1,9 @@
+import { fetcher, setQueryParams } from '@reapit/elements'
 import logger from '../../logger'
 import { ServerContext } from '../../app'
 import { GetContactByIdArgs, CreateContactArgs, GetContactsArgs } from './contact'
 import errors from '../../errors'
-// import { API_VERSION } from '../../constants/api'
+import { API_VERSION } from '../../constants/api'
 
 export const REAPIT_API_BASE_URL = 'https://dev.platform.reapit.net'
 export const CONTACTS_PER_PAGE = 10
@@ -15,19 +16,17 @@ export const callGetContactByIdAPI = async (args: GetContactByIdArgs, context: S
   const traceId = context.traceId
   try {
     logger.info('callGetContactByIdAPI', { traceId, args })
-    // TODO: temporary disable fetcher because it'll add react to dependencies, and no need
-    // const getResponse = await fetcher({
-    //   url: `${URLS.contacts}/${args.id}`,
-    //   api: REAPIT_API_BASE_URL,
-    //   method: 'GET',
-    //   headers: {
-    //     Authorization: context.authorization,
-    //     'Content-Type': 'application/json',
-    //     'api-version': API_VERSION,
-    //   },
-    // })
-    // return getResponse
-    return {}
+    const getResponse = await fetcher({
+      url: `${URLS.contacts}/${args.id}`,
+      api: REAPIT_API_BASE_URL,
+      method: 'GET',
+      headers: {
+        Authorization: context.authorization,
+        'Content-Type': 'application/json',
+        'api-version': API_VERSION,
+      },
+    })
+    return getResponse
   } catch (error) {
     logger.error('callGetContactByIdAPI', error)
     return errors.generateUserInputError(traceId)
@@ -38,19 +37,17 @@ export const callGetContactsAPI = async (args: GetContactsArgs, context: ServerC
   const traceId = context.traceId
   logger.info('callGetContactsAPI', { args, traceId })
   try {
-    // TODO: temporary disable fetcher because it'll add react to dependencies, and no need
-    // const response = fetcher({
-    //   url: `${URLS.contacts}/?${setQueryParams({ ...args, pageSize: CONTACTS_PER_PAGE })}`,
-    //   api: REAPIT_API_BASE_URL,
-    //   method: 'GET',
-    //   headers: {
-    //     Authorization: context.authorization,
-    //     'Content-Type': 'application/json',
-    //     'api-version': API_VERSION,
-    //   },
-    // })
-    // return response
-    return []
+    const response = fetcher({
+      url: `${URLS.contacts}/?${setQueryParams({ ...args, pageSize: CONTACTS_PER_PAGE })}`,
+      api: REAPIT_API_BASE_URL,
+      method: 'GET',
+      headers: {
+        Authorization: context.authorization,
+        'Content-Type': 'application/json',
+        'api-version': API_VERSION,
+      },
+    })
+    return response
   } catch (error) {
     logger.error('callGetContactsAPI', error)
     return errors.generateUserInputError(traceId)
