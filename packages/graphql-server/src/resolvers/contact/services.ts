ed8bb732e6f7cf8cf65dc5fa6fae4734191fd7ed
Vu Nguyen <nphivu414@gmail.com>
2020-03-02 17:46:59 +0700

    Fix: #372 Update resolver for contact services after break changes (#393)
    * fix: #372: Update contact graphql types and schema
    
    * fix: #372: Fix Add and Update contact service
    
    * fix: #372 Fix contact graphql resolvers
    
    * fix: #372 Add comments for schema - Update required fields
    
    * fix: #372 Add identityCheck field to contact type
    
    * fix: #372 Update contact service test
    
    * fix: #372 Fix contact field type
    
    * fix: #372 -Update graphql contact typpe
    
    * fix: #372 - update git ignore
    
    * fix: #372 - Update contact service type
    
    * fix: #372 - Fix query and mutation name - Update test

--- packages/graphql-server/src/resolvers/contact/services.ts
+++ packages/graphql-server/src/resolvers/contact/services.ts
@@ -1,7 +1,16 @@
 import logger from '../../logger'
 import { ServerContext } from '../../app'
-import { GetContactByIdArgs, CreateContactArgs, GetContactsArgs } from './contact'
-import { callGetContactByIdAPI, callGetContactsAPI, callCreateContactAPI } from './api'
+import {
+  GetContactByIdArgs,
+  CreateContactArgs,
+  UpdateContactArgs,
+  GetContactsArgs,
+  GetContactByIdReturn,
+  GetContactsReturn,
+  CreateContactReturn,
+  UpdateContactReturn,
+} from './types'
+import { callGetContactByIdAPI, callGetContactsAPI, callCreateContactAPI, callUpdateContactAPI } from './api'
 
 export const REAPIT_API_BASE_URL = 'https://dev.platform.reapit.net'
 export const CONTACTS_PER_PAGE = 10
@@ -10,31 +19,39 @@ export const URLS = {
   identityChecks: '/identitychecks',
 }
 
-export const getContactById = (args: GetContactByIdArgs, context: ServerContext) => {
+export const getContactById = (args: GetContactByIdArgs, context: ServerContext): GetContactByIdReturn => {
   const traceId = context.traceId
   logger.info('getContactById', { traceId, args })
   const contact = callGetContactByIdAPI(args, context)
   return contact
 }
 
-export const getContacts = (args: GetContactsArgs, context: ServerContext) => {
+export const getContacts = (args: GetContactsArgs, context: ServerContext): GetContactsReturn => {
   const traceId = context.traceId
   logger.info('getContacts', { traceId, args })
   const contact = callGetContactsAPI(args, context)
   return contact
 }
 
-export const createContact = (args: CreateContactArgs, context: ServerContext) => {
+export const createContact = (args: CreateContactArgs, context: ServerContext): CreateContactReturn => {
   const traceId = context.traceId
   logger.info('createContact', { traceId, args })
-  const contact = callCreateContactAPI(args)
+  const contact = callCreateContactAPI(args, context)
+  return contact
+}
+
+export const updateContact = (args: UpdateContactArgs, context: ServerContext): UpdateContactReturn => {
+  const traceId = context.traceId
+  logger.info('updateContact', { traceId, args })
+  const contact = callUpdateContactAPI(args, context)
   return contact
 }
 
 const contactServices = {
   getContactById,
-  createContact,
   getContacts,
+  createContact,
+  updateContact,
 }
 
 export default contactServices
