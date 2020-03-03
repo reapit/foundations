248a9d26a9bdb5d298e5b2a61e1c0696ea36b22d
Nghia Pham <nghiapn@reapit.com>
2020-03-03 16:39:05 +0700

    chore: fix typos

--- packages/config-manager/src/utils/generateConfigTsDef.js
+++ packages/config-manager/src/utils/generateConfigTsDef.js
@@ -1,21 +1,20 @@
 const { json2ts } = require('json-ts')
 const fs = require('fs')
-const { REAPIT_TS_DEF_IN_CWD_PATH } = require('../paths')
+const { REAPIT_TS_DEF_IN_CWD_PATH, REAPIT_TS_DEF_NAME } = require('../paths')
 const formatCode = require('../../../../scripts/foundations-ts-definitions/format-code')
 
 exports.generateConfigTsDef = config => {
   try {
     const generatedTsDef = json2ts(config, {
-      namespace: 'NodeJS',
+      namespace: '',
       rootName: 'ProcessEnv',
       prefix: '',
     })
 
-    const formattedGeneratedTsDef = formatCode(generatedTsDef)
-
+    let formattedGeneratedTsDef = `export ${formatCode(generatedTsDef)}`
     fs.writeFileSync(REAPIT_TS_DEF_IN_CWD_PATH, formattedGeneratedTsDef)
 
-    console.log('reapit-config.d.ts has been saved - be sure to add to your .gitignore file')
+    console.log(`${REAPIT_TS_DEF_NAME} has been saved - be sure to add to your .gitignore file`)
   } catch (err) {
     console.log(
       'Something went wrong when generating typescript definition. Detailed error with stack trace is provided below:',
