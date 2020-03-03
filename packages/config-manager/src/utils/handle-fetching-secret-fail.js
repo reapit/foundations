fbbac1aed51a343203746f667ddf7be95d4ca41d
NghiaPham <nghiapn@dwarvesv.com>
2020-03-03 19:06:58 +0700

    Chore/126 refactor config manager (#514)

---
+++ packages/config-manager/src/utils/handle-fetching-secret-fail.js
@@ -0,0 +1,27 @@
+const { writeConfigInCWD } = require('./write-config-in-cwd')
+const { generateConfigTsDef } = require('./generate-config-ts-def')
+const fs = require('fs')
+
+const { REAPIT_BASE_CONFIG_PATH } = require('../paths')
+
+module.exports = (err, isGenerateConfigTsDef) => {
+  console.log('Something went wrong when fetching config. Detailed error with stack trace is provided below:')
+  console.error(err, err.stack)
+  console.log('A base configuration file will be provided')
+
+  let config = ''
+  try {
+    config = fs.readFileSync(REAPIT_BASE_CONFIG_PATH)
+  } catch (err) {
+    console.log(
+      'Something went wrong when reading base configuration. Detailed error with stack trace is provided below:',
+    )
+    return console.error(err, err.stack)
+  }
+
+  if (isGenerateConfigTsDef) {
+    generateConfigTsDef(config)
+  }
+
+  return writeConfigInCWD(config)
+}
