7d531742268c7f83a13fe9b1f632097a8ab9dc5b
Khac Vy <vytk@reapit.com>
2020-03-03 15:19:34 +0700

    feat: #66 Offices page on SMB

---
+++ packages/smb/src/components/pages/offices/index.js
@@ -0,0 +1,3 @@
+import Offices from './offices'
+
+export default Offices
---
+++ packages/smb/src/components/ui/global-settings-tab.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { mount } from 'enzyme'
+import { GlobalSettingsTab, GlobalSettingsTabProps } from './global-settings-tab'
+
+describe('GlobalSettingsTab', () => {
+  describe('GlobalSettingsTab', () => {
+    it('should match a snapshot', () => {
+      const mockProps: GlobalSettingsTabProps = {}
+      const wrapper = mount(<GlobalSettingsTab {...mockProps} />)
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+})
