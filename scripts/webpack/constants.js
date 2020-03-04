d70f609d4adef13f09c4ff6fc3c00c5a841074fe
Duong Pham <duongph@reapit.com>
2020-03-05 00:31:18 +0700

    fix: path resolve for reapit config

--- scripts/webpack/constants.js
+++ scripts/webpack/constants.js
@@ -1,15 +1,13 @@
-const path = require('path')
-
 const PATHS = {
   src: `${process.cwd()}/src`,
   entryWeb: `${process.cwd()}/src/core/index.tsx`,
-  config: path.resolve(process.cwd(), '../../', 'reapit-config.json'),
+  config: `${process.cwd()}/../../reapit-config.json`,
   template: `${process.cwd()}/public/index.html`,
   tsConfig: `${process.cwd()}/tsconfig.json`,
   logo: `${process.cwd()}/public/logo.png`,
   output: `${process.cwd()}/public/dist`,
-  elementsSass: path.resolve(process.cwd(), '../', 'elements/src/styles/**.scss'),
-  elementsIndexSass: path.resolve(process.cwd(), '../', 'elements/src/styles/index.scss'),
+  elementsSass: `${process.cwd()}/../elements/src/styles/**.scss`,
+  elementsIndexSass: `${process.cwd()}/../elements/src/styles/index.scss`,
   cacheWebpackDir: `${process.cwd()}/.webpack-cache`,
 }
 
