a08248ce1ac96211f4b6bd79fe594d12714780aa
NghiaPham <nghiapn@dwarvesv.com>
2020-03-05 11:28:14 +0700

    chore: fix config-manager - handle end signals and errors correctly (#531)

--- packages/config-manager/src/utils/write-config-in-cwd.js
+++ packages/config-manager/src/utils/write-config-in-cwd.js
@@ -5,7 +5,8 @@ exports.writeConfigInCWD = config => {
   return fs.writeFile(REAPIT_CONFIG_IN_CWD_PATH, config, 'utf8', err => {
     if (err) {
       console.error('An error occured while writing JSON Object to File.', err)
-      return console.error(err)
+      console.error(err)
+      process.exit(1)
     }
 
     console.log('reapit-config.json has been saved - be sure to add to your .gitignore file')
