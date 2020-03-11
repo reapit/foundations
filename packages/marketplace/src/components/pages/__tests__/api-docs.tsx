2afeeaa78fb4c25fc4b72645ad8596395476a4dc
Pham Hai Duong <duongph@reapit.com>
2020-03-11 14:08:07 +0700

    feat: #557 add direct link to specific section in document (#589)
    Changes
    - [x] Add function for parse hash on location and map to correct url on iframe
    - [x] Add unit test for function parse hash on location

--- packages/marketplace/src/components/pages/__tests__/api-docs.tsx
+++ packages/marketplace/src/components/pages/__tests__/api-docs.tsx
@@ -1,9 +1,87 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import ApiDocs from '../api-docs'
+import { createBrowserHistory } from 'history'
+import ApiDocs, { parseIframeUrl } from '../api-docs'
+import { Router, Route } from 'react-router-dom'
 
 describe('ApiDocs', () => {
   it('should match a snapshot', () => {
-    expect(shallow(<ApiDocs />)).toMatchSnapshot()
+    const history = createBrowserHistory()
+    const wrapper = shallow(
+      <Router history={history}>
+        <Route>
+          <ApiDocs />
+        </Route>
+      </Router>,
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('parseIframeUrl', () => {
+    it('should return /api', () => {
+      const input = '#api'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/api')
+    })
+    it('should return /api/api-documentation', () => {
+      const input = '#api-documentation'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/api/api-documentation')
+    })
+    it('should return /api/reapit-connect', () => {
+      const input = '#reapit-connect'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/api/reapit-connect')
+    })
+    it('should return /api/reapit-connect', () => {
+      const input = '#reapit-connect'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/api/reapit-connect')
+    })
+    it('should return /api/web', () => {
+      const input = '#web'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/api/web')
+    })
+    it('should return /api/desktop-api', () => {
+      const input = '#desktop-api'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/api/desktop-api')
+    })
+    it('should return /open-source', () => {
+      const input = '#open-source'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/open-source')
+    })
+    it('should return /open-source/contributing', () => {
+      const input = '#contributing'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/open-source/contributing')
+    })
+    it('should return /open-source/packages', () => {
+      const input = '#package'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/open-source/packages')
+    })
+    it('should return /whats-new', () => {
+      const input = '#whats-new'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/whats-new')
+    })
+    it('should return /whats-new/platform-changelog', () => {
+      const input = '#platform-changelog'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/whats-new/platform-changelog')
+    })
+    it('should return /developer-portal', () => {
+      const input = '#developer-portal'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/developer-portal')
+    })
+    it('should return /', () => {
+      const input = '#mockHashForDefault'
+      const result = parseIframeUrl(input)
+      expect(result).toEqual('/')
+    })
   })
 })
