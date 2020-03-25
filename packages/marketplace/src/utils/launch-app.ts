0df93d25ccd242c39425f0d98ce081cac638d10a
Vu Nguyen <vunp@reapit.com>
2020-03-25 15:31:04 +0700

    feat: #697: Update urls for desktop to use agency (#717)

--- packages/marketplace/src/utils/launch-app.ts
+++ packages/marketplace/src/utils/launch-app.ts
@@ -4,7 +4,7 @@ export const handleLaunchApp = (app: AppSummaryModel) => {
   import('../core/store').then(store => {
     if (app.homePage && app.id) {
       if (store?.default?.state?.auth?.refreshSession?.mode === 'DESKTOP') {
-        window.location.href = `desktop://app?id=${app.id}&launchUri=${app.homePage}`
+        window.location.href = `agencycloud://app?id=${app.id}&launchUri=${app.homePage}`
       } else {
         window.location.href = app.homePage
       }
