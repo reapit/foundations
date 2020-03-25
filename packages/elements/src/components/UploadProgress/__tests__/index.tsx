1b8a0ee8e01629a59757a5f3fe8a6d6cf444a073
Khac Vy <vytk@reapit.com>
2020-03-25 14:46:20 +0700

    feat: #653 Upload Progress (#716)

---
+++ packages/elements/src/components/UploadProgress/__tests__/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { UploadProgress, UploadProgressProps } from '../.'
+
+describe('UploadProgress', () => {
+  it('should match snapshot', () => {
+    const mockProps: UploadProgressProps = {
+      visible: true,
+      percentage: 100,
+      totalCount: 10,
+      completedCount: 4,
+    }
+    const wrapper = shallow(<UploadProgress {...mockProps} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should run correctly when percentage < 0', () => {
+    const mockProps: UploadProgressProps = {
+      percentage: -1,
+      visible: true,
+    }
+    const wrapper = shallow(<UploadProgress {...mockProps} />)
+    expect(wrapper.find('.upload-progress-bg').prop('style')).toEqual({ width: '0%' })
+  })
+
+  it('should run correctly when percentage > 100', () => {
+    const mockProps: UploadProgressProps = {
+      percentage: 101,
+      visible: true,
+    }
+    const wrapper = shallow(<UploadProgress {...mockProps} />)
+    expect(wrapper.find('.upload-progress-bg').prop('style')).toEqual({ width: '100%' })
+  })
+})
