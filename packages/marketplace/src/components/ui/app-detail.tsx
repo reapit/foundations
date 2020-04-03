2c49e90fa459a20bb08fa68d6bf7617517be3493
Duong Pham <duongph@reapit.com>
2020-04-03 22:38:16 +0700

    feat: #741 ui for view api key of web componentts
    Changes
    - UI for view api key of web components and tests

--- packages/marketplace/src/components/ui/app-detail.tsx
+++ packages/marketplace/src/components/ui/app-detail.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react'
-import { ReduxState } from '@/types/core'
 import Slider, { Settings } from 'react-slick'
-import carouselStyles from '../../styles/elements/carousel.scss?mod'
-import ChevronLeftIcon from '@/components/svg/chevron-left'
-import '@/styles/vendor/slick.scss'
+import { FaCheck, FaTimes, FaCopy } from 'react-icons/fa'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { connect } from 'react-redux'
-import { setAppDetailModalStateInstall, setAppDetailModalStateUninstall } from '@/actions/app-detail-modal'
+import { Tile, ModalHeader, ModalBody, ModalFooter, H6, GridThreeColItem, Grid, SubTitleH6 } from '@reapit/elements'
 import { AppDetailModel } from '@reapit/foundations-ts-definitions'
-import { Tile, ModalHeader, ModalBody, ModalFooter, H6, GridThreeColItem, Grid } from '@reapit/elements'
+import { ReduxState } from '@/types/core'
+import ChevronLeftIcon from '@/components/svg/chevron-left'
+import { setAppDetailModalStateInstall, setAppDetailModalStateUninstall } from '@/actions/app-detail-modal'
 import { setDeveloperAppModalStateDelete } from '@/actions/developer-app-modal'
-import styles from '@/styles/blocks/app-detail.scss?mod'
 import appCardStyles from '@/styles/blocks/app-card.scss?mod'
-import { FaCheck, FaTimes } from 'react-icons/fa'
-import AppAuthenticationDetail from './app-authentication-detail'
 import AuthFlow from '@/constants/app-auth-flow'
+import AppAuthenticationDetail from './app-authentication-detail'
+import carouselStyles from '../../styles/elements/carousel.scss?mod'
+import styles from '@/styles/blocks/app-detail.scss?mod'
+import '@/styles/vendor/slick.scss'
 
 export interface AppDetailModalInnerProps {
   data: AppDetailModel
@@ -35,6 +36,61 @@ export interface AppDetailModalMappedActions {
 export type AppDetailProps = AppDetailModalMappedActions & AppDetailModalMappedProps & AppDetailModalInnerProps
 
 export const SlickButtonNav = ({ children, ...props }) => <button {...props}>{children}</button>
+
+export type HandleShowApiKey = {
+  setIsShowApikey: React.Dispatch<React.SetStateAction<boolean>>
+  isShowApiKey: boolean
+}
+
+export const handleShowApiKey = ({ setIsShowApikey, isShowApiKey }: HandleShowApiKey) => () => {
+  setIsShowApikey(!isShowApiKey)
+}
+
+export const handleCopyAlert = () => alert('Copied')
+
+export type RenderShowApiKeyForWebComponent = HandleShowApiKey & {
+  isWebComponent?: boolean
+  isCurrentLoggedUserDeveloper: boolean
+  apiKey: string
+}
+
+export const renderShowApiKeyForWebComponent = ({
+  isWebComponent,
+  setIsShowApikey,
+  isShowApiKey,
+  apiKey,
+  isCurrentLoggedUserDeveloper,
+}: RenderShowApiKeyForWebComponent) => {
+  const isShow = isWebComponent && !isCurrentLoggedUserDeveloper
+  if (!isShow) {
+    return null
+  }
+  return (
+    <>
+      <SubTitleH6 className="mb-0">API key</SubTitleH6>
+      <p>
+        To obtain your unique API key, click on &apos;Show API Key&apos; below. For installation instructions, please
+        click here
+      </p>
+      <span>Authentication:</span>&nbsp;
+      <span>
+        <a onClick={handleShowApiKey({ setIsShowApikey, isShowApiKey })}>
+          <u>Show API key</u>
+        </a>
+      </span>
+      {isShowApiKey && (
+        <CopyToClipboard text={apiKey} onCopy={handleCopyAlert}>
+          <div className="control has-icons-right">
+            <input className="input is-primary" id="apiKey" type="text" name="apiKey" value={apiKey} />
+            <span className="icon is-right">
+              <FaCopy />
+            </span>
+          </div>
+        </CopyToClipboard>
+      )}
+    </>
+  )
+}
 
 export const AppDetail: React.FunctionComponent<AppDetailProps> = ({
   data,
@@ -59,9 +115,11 @@ export const AppDetail: React.FunctionComponent<AppDetailProps> = ({
     isDirectApi,
     authFlow,
     externalId,
+    isWebComponent,
   } = data
   const icon = media.filter(({ type }) => type === 'icon')[0]
   const carouselImages = media.filter(({ type }) => type === 'image')
+  const [isShowApiKey, setIsShowApikey] = React.useState<boolean>(false)
 
   const settings: Settings = {
     dots: false,
@@ -122,6 +180,13 @@ export const AppDetail: React.FunctionComponent<AppDetailProps> = ({
                 {authFlow === AuthFlow.CLIENT_SECRET && id && <AppAuthenticationDetail appId={id} />}
               </>
             )}
+            {renderShowApiKeyForWebComponent({
+              isWebComponent,
+              isShowApiKey,
+              setIsShowApikey,
+              apiKey: 'mockApiKey',
+              isCurrentLoggedUserDeveloper,
+            })}
             {carouselImages.length > 0 && (
               <div className={carouselStyles.container}>
                 <Slider {...settings}>
