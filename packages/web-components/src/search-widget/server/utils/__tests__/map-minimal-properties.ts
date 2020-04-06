3c24e3cc125bb07fc0f0cc85083237855c265539
Cuong Vu <cuongvh@reapit.com>
2020-04-06 14:41:58 +0700

    feat: #728 test for undefined _embedded in map-minimal-properties

--- packages/web-components/src/search-widget/server/utils/__tests__/map-minimal-properties.ts
+++ packages/web-components/src/search-widget/server/utils/__tests__/map-minimal-properties.ts
@@ -1,8 +1,9 @@
 import { mapMinimalProperties } from '../map-minimal-properties'
 import { propertiesStub, propertiesMinimalStub } from '../../api/__stubs__/properties'
+import { propertyImagesStub, propertyImagesMinimalStub } from '../../api/__stubs__/property-images'
 
 describe('mappingMinimalProperties', () => {
-  it('should return correct minimalResult', () => {
+  it('should return correct minimalResult for properties', () => {
     const includedProps = [
       'address',
       'bathrooms',
@@ -17,5 +18,18 @@ describe('mappingMinimalProperties', () => {
     ]
     const result = mapMinimalProperties(propertiesStub, includedProps)
     expect(result).toEqual(propertiesMinimalStub)
+  })
+
+  it('should return correct minimalResult for propertyImages', () => {
+    const includedProps = ['id', 'url', 'propertyId']
+    const result = mapMinimalProperties(propertyImagesStub, includedProps)
+    expect(result).toEqual(propertyImagesMinimalStub)
+  })
+
+  it('should return correctly with _embedded undefined', () => {
+    const includedProps = ['id', 'url', 'propertyId']
+    const propertyImagesStubWithUndefinedEmbedded = { ...propertyImagesStub, _embedded: undefined }
+    const result = mapMinimalProperties(propertyImagesStubWithUndefinedEmbedded, includedProps)
+    expect(result).toEqual(propertyImagesStubWithUndefinedEmbedded)
   })
 })
