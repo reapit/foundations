440ad25f9e367001f7069255c0b284c113677217
Cuong Vu <cuongvh@reapit.com>
2020-04-07 13:22:29 +0700

    feat: #728 The search widget back end should return a sub-set of properties data (#826)

--- packages/web-components/src/search-widget/server/api/__stubs__/property-images.ts
+++ packages/web-components/src/search-widget/server/api/__stubs__/property-images.ts
@@ -1,42 +1,68 @@
 import { PagedResultPropertyImageModel_ } from '@reapit/foundations-ts-definitions'
+import { PickedPagedResultPropertyImageModel_ } from '../../../types'
 
 export const propertyImagesStub: PagedResultPropertyImageModel_ = {
   _embedded: [
     {
-      id: 'OXF100008_01.jpg',
-      created: '2018-12-12T12:30:23.0000000Z',
-      modified: '2019-01-08T12:30:34.0000000Z',
-      propertyId: 'OXF100008',
-      url: 'https://www.exampleurl.com/images/OXF100008_01.jpg',
+      id: 'RPT200079_02.png',
+      created: '2020-03-03T12:18:36.0000000Z',
+      modified: '2020-03-04T10:02:19.0000000Z',
+      propertyId: 'RPT200079',
+      url: 'https://reapit-dev.s3.eu-west-2.amazonaws.com/pictures/RPT/20/RPT200079_02.png',
       caption: 'Living room',
       type: 'photograph',
       order: 1,
-      _links: {
-        self: {
-          href: '/propertyImages/OXF100008_01.jpg',
-        },
-        property: {
-          href: '/properties/OXF100008',
-        },
-      },
+      _eTag: '"81BA2F4F26E91F78B131A773A06BA7BD"',
+      _links: { self: { href: '/propertyImages/RPT200079_02.png' }, property: { href: '/properties/RPT200079' } },
+      _embedded: undefined,
+    },
+    {
+      id: 'RPT200073_02.png',
+      created: '2020-02-24T13:36:30.0000000Z',
+      modified: '2020-02-24T13:36:30.0000000Z',
+      propertyId: 'RPT200073',
+      url: 'https://reapit-dev.s3.eu-west-2.amazonaws.com/pictures/RPT/20/RPT200073_02.png',
+      caption: 'Living room',
+      type: 'photograph',
+      order: 2,
+      _eTag: '"6A5698239EBD2E01C1EC03FF547F9D2A"',
+      _links: { self: { href: '/propertyImages/RPT200073_02.png' }, property: { href: '/properties/RPT200073' } },
+      _embedded: undefined,
     },
   ],
   pageNumber: 1,
-  pageSize: 1,
-  pageCount: 1,
-  totalCount: 25,
+  pageSize: 2,
+  pageCount: 2,
+  totalCount: 9244,
   _links: {
-    self: {
-      href: '/propertyImages/?PageNumber=1&PageSize=1',
-    },
-    first: {
-      href: '/propertyImages/?PageNumber=1&PageSize=1',
-    },
-    next: {
-      href: '/propertyImages/?PageNumber=2&PageSize=1',
-    },
-    last: {
-      href: '/propertyImages/?PageNumber=25&PageSize=1',
-    },
+    self: { href: '/propertyImages/?PageNumber=1&PageSize=2' },
+    first: { href: '/propertyImages/?PageNumber=1&PageSize=2' },
+    next: { href: '/propertyImages/?PageNumber=2&PageSize=2' },
+    last: { href: '/propertyImages/?PageNumber=4622&PageSize=2' },
   },
+}
+
+export const propertyImagesMinimalStub: PickedPagedResultPropertyImageModel_ = {
+  pageNumber: 1,
+  pageSize: 2,
+  pageCount: 2,
+  totalCount: 9244,
+  _links: {
+    self: { href: '/propertyImages/?PageNumber=1&PageSize=2' },
+    first: { href: '/propertyImages/?PageNumber=1&PageSize=2' },
+    next: { href: '/propertyImages/?PageNumber=2&PageSize=2' },
+    last: { href: '/propertyImages/?PageNumber=4622&PageSize=2' },
+  },
+  _embedded: [
+    {
+      id: 'RPT200079_02.png',
+      propertyId: 'RPT200079',
+      url: 'https://reapit-dev.s3.eu-west-2.amazonaws.com/pictures/RPT/20/RPT200079_02.png',
+    },
+    {
+      id: 'RPT200073_02.png',
+      propertyId: 'RPT200073',
+      url: 'https://reapit-dev.s3.eu-west-2.amazonaws.com/pictures/RPT/20/RPT200073_02.png',
+    },
+  ],
 }
---
+++ packages/web-components/src/search-widget/types/picked-properties.d.ts
@@ -0,0 +1,19 @@
+import { PropertyModel, PagedResultPropertyModel_ } from '@reapit/foundations-ts-definitions'
+
+export type PickedPropertyModel = Pick<
+  PropertyModel,
+  | 'address'
+  | 'bathrooms'
+  | 'bedrooms'
+  | 'description'
+  | 'id'
+  | 'letting'
+  | 'marketingMode'
+  | 'selling'
+  | 'style'
+  | 'type'
+>
+
+export type PickedPagedResultPropertyModel_ = Omit<PagedResultPropertyModel_, '_embedded'> & {
+  _embedded?: PickedPropertyModel[]
+}
