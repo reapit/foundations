b9b95a4eff9864f6b4122056ba9556b5bccdf226
Khac Vy <vytk@reapit.com>
2020-04-09 16:42:49 +0700

    feat: #849 link valid foundations-doc url via the iframe (#860)
    * feat: #849 link valid foundations-doc url via the iframe

--- packages/marketplace/src/components/pages/api-docs.tsx
+++ packages/marketplace/src/components/pages/api-docs.tsx
@@ -3,45 +3,23 @@ import { useHistory } from 'react-router'
 import { FlexContainerBasic } from '@reapit/elements'
 import ErrorBoundary from '@/components/hocs/error-boundary'
 import { IFRAME_URLS } from '../../constants/iframe-urls'
+import Routes from '../../constants/routes'
 
-export const parseIframeUrl = (hash: string): string => {
-  switch (hash) {
-    case '#api':
-      return '/api'
-    case '#api-documentation':
-      return '/api/api-documentation'
-    case '#reapit-connect':
-      return '/api/reapit-connect'
-    case '#web':
-      return '/api/web'
-    case '#desktop-api':
-      return '/api/desktop-api'
-    case '#open-source':
-      return '/open-source'
-    case '#contributing':
-      return '/open-source/contributing'
-    case '#packages':
-      return '/open-source/packages'
-    case '#whats-new':
-      return '/whats-new'
-    case '#platform-changelog':
-      return '/whats-new/platform-changelog'
-    case '#developer-portal':
-      return '/developer-portal'
-    default:
-      return '/'
-  }
+export const parseIframeUrl = (pathname: string, hash: string): string => {
+  const path = pathname.split(Routes.DEVELOPER_API_DOCS)[1]
+  return `${path}${hash}`
 }
 
 const ApiDocsPage: React.FC = () => {
   const { location } = useHistory()
+
   return (
     <ErrorBoundary>
       <FlexContainerBasic flexColumn hasPadding className="is-full-height">
         <FlexContainerBasic className="container is-full-height">
           <iframe
             style={{ border: 'none' }}
-            src={`${IFRAME_URLS.documentation}${parseIframeUrl(location.hash)}`}
+            src={`${IFRAME_URLS.documentation}${parseIframeUrl(location.pathname, location.hash)}`}
             width="100%"
             height="100%"
           />
