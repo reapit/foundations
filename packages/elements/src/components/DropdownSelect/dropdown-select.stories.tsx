c2b181790523e262f3b9eb15fe35efcd933f1b22
nhalx <nhalx@reapit.com>
2020-04-09 18:50:31 +0700

    put tag component into formik and testing

--- packages/elements/src/components/DropdownSelect/dropdown-select.stories.tsx
+++ packages/elements/src/components/DropdownSelect/dropdown-select.stories.tsx
@@ -5,10 +5,25 @@ import { storiesOf } from '@storybook/react'
 import { DropdownSelect } from '.'
 import { action } from '@storybook/addon-actions'
 
+const options = [
+  { value: 'jack', label: 'Jack' },
+  { value: 'lucy', label: 'Lucy' },
+  { value: 'yiminghe', label: 'Yiminghe' }
+]
+
 storiesOf('DropdownSelect', module).add('Primary', () => {
   return (
     <section className="section">
-      <DropdownSelect mode="tags" />
+      <Formik
+        initialValues={{ value: '' }}
+        onSubmit={values => {
+          action('Form Values' + values)
+        }}
+      >
+        <Form>
+          <DropdownSelect name='dropdown-select-field' mode="tags" labelText='Dropdown Select' options={options} />
+        </Form>
+      </Formik>
     </section>
   )
 })
--- packages/elements/src/components/DropdownSelect/index.tsx
+++ packages/elements/src/components/DropdownSelect/index.tsx
@@ -1,14 +1,37 @@
 import * as React from 'react'
 import Select, { Option, SelectProps } from 'rc-select'
+import { Field, FieldProps } from 'formik'
 import './index.scss'
 
-export const DropdownSelect: React.FC<SelectProps> = props => {
+const handleChangeOption = field => (value) => {
+  field.onChange({ target: { value: value, name: field.name } })
+}
+
+export interface DropdownSelectProps {
+  labelText: string
+  name: string
+}
+
+export const DropdownSelect: React.FC<SelectProps & DropdownSelectProps> = props => {
   return (
-    <Select {...props}>
-      <Option value="jack">jack</Option>
-      <Option value="lucy">lucy</Option>
-      <Option value="yiminghe">yiminghe</Option>
-    </Select>
+    <div className="field pb-4">
+      <div className="control">
+        <Field name={props.name}>
+          {({ field }: FieldProps<string | string[]>) => {
+            return (
+              <div className="field field-dropdown-select">
+                <label className="label" htmlFor=''>
+                  {props.labelText}
+                </label>
+                <Select {...props} onChange={handleChangeOption(field)}>
+                  {props.options?.map(option => <Option value={option.value}>{option.label}</Option>)}
+                </Select>
+              </div>
+            )
+          }}
+        </Field>
+      </div>
+    </div>
   )
 }
 
