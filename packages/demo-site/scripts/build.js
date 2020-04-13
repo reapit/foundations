878c66b40aee4184fc8f470e69551b2b96c03e7a
Pham Hai Duong <duongph@reapit.com>
2020-04-13 20:28:08 +0700

    fix: deploy demo-site (#888)

--- packages/demo-site/scripts/build.js
+++ packages/demo-site/scripts/build.js
@@ -6,11 +6,13 @@ const compileTemplate = async () => {
   try {
     const config = require('../config.json')
     const ejsFileDir = path.resolve(__dirname, '..', 'index.ejs')
-    const indexHtmlDir = path.resolve(__dirname, '..', 'public', 'index.html')
+    const distDir = path.resolve(__dirname, '..', 'public', 'dist')
+    const indexHtmlDir = path.resolve(__dirname, '..', 'public', 'dist', 'index.html')
     const ejsTemplate = await fs.readFileSync(ejsFileDir, { encoding: 'utf8' })
 
     const template = ejs.compile(ejsTemplate)
     const html = template({ API_KEY: config.API_KEY, CDN_URL: config.CDN_URL })
+    fs.mkdirSync(distDir, { recursive: true })
     fs.writeFileSync(indexHtmlDir, html)
   } catch (err) {
     console.error(err)
