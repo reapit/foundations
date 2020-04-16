db2ccabdde164557548f803949691d74f6330875
Vu Nguyen <vunp@reapit.com>
2020-04-16 10:29:51 +0700

    feat: #910: Fix whats new link in Help page

--- packages/marketplace/src/constants/developer-help-links.ts
+++ packages/marketplace/src/constants/developer-help-links.ts
@@ -1,5 +1,5 @@
 export const HelpLinks = {
-  WHATS_NEW: '/developer/api-docs#whats-new',
+  WHATS_NEW: '/developer/api-docs/whats-new',
   BUG_REPORT:
     'https://github.com/reapit/foundations/issues/new?assignees=&labels=bug%2C+needs-triage&template=bug_report.md&title=',
   API_REQUEST:
