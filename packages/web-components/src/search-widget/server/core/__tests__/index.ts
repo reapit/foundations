7486865cd0cf4760e8c6341b2f27ae27498b6c69
Duong Pham <duongph@reapit.com>
2020-04-20 11:35:51 +0700

    Fix: serverless headers when request go through authorizer
    Changes
    -Add parse headers for requestContext
    -Parse headers to correct one

--- packages/web-components/src/search-widget/server/core/__tests__/index.ts
+++ packages/web-components/src/search-widget/server/core/__tests__/index.ts
@@ -1,4 +1,5 @@
-import { searchWidgetHandler } from '../index'
+import { APIGatewayProxyEvent, APIGatewayEventRequestContextWithAuthorizer } from 'aws-lambda'
+import { searchWidgetHandler, parseHeadersFromEvent } from '../index'
 
 describe('searchWidgetHandler', () => {
   it('should launch without crashing', async () => {
@@ -7,5 +8,53 @@ describe('searchWidgetHandler', () => {
     } catch (err) {
       expect(err).toBeUndefined()
     }
+  })
+})
+
+describe('parseHeadersFromEvent', () => {
+  const mockEvent = {
+    headers: {
+      Accept: '*/*',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Cache-Control': 'no-cache',
+      'Content-Type': 'application/json',
+      Host: 'mockHost',
+      'User-Agent': 'PostmanRuntime/7.24.1',
+      'X-Api-Key': '123456',
+      'X-Forwarded-Port': '443',
+      'X-Forwarded-Proto': 'https',
+    },
+    requestContext: {
+      authorizer: {
+        authorization: ' mockAuthorization',
+        'reapit-customer': 'DXX',
+        principalid: 'Allow',
+      },
+    } as APIGatewayEventRequestContextWithAuthorizer<any>,
+    body: '',
+    multiValueHeaders: {},
+    httpMethod: 'GET',
+    isBase64Encoded: false,
+    path: '',
+    pathParameters: {},
+    queryStringParameters: null,
+    multiValueQueryStringParameters: null,
+    stageVariables: null,
+    resource: '',
+  } as APIGatewayProxyEvent
+  const result = parseHeadersFromEvent(mockEvent)
+  expect(result).toEqual({
+    Accept: '*/*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Cache-Control': 'no-cache',
+    'Content-Type': 'application/json',
+    Host: 'mockHost',
+    'User-Agent': 'PostmanRuntime/7.24.1',
+    'X-Api-Key': '123456',
+    'X-Forwarded-Port': '443',
+    'X-Forwarded-Proto': 'https',
+    authorization: 'Bearer mockAuthorization',
+    principalid: 'Allow',
+    'reapit-customer': 'DXX',
   })
 })
