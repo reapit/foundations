995e717253357d291139f5a70f8b5a81e854d637
Cuong Vu <cuongvh@reapit.com>
2020-04-21 12:07:51 +0700

    fix: #965 fix geo-diary cannot refresh token

--- packages/geo-diary/src/utils/session.ts
+++ packages/geo-diary/src/utils/session.ts
@@ -1,5 +1,5 @@
 import store from '@/core/store'
-import { authLoginSuccess, authLogout } from '@/actions/auth'
+import { authLoginSuccess, authLogout, authSetRefreshSession } from '@/actions/auth'
 import { getSession } from '@reapit/cognito-auth'
 import { COOKIE_SESSION_KEY_GEO_DIARY } from '../constants/api'
 
@@ -10,6 +10,16 @@ export const getAccessToken = async (): Promise<string | null> => {
 
   if (session) {
     store.dispatch(authLoginSuccess(session))
+    const oldRefreshSession = store.state.auth.refreshSession
+    const refreshByTokenParams = ['refreshToken', 'userName', 'cognitoClientId']
+    const isRefreshByToken = refreshByTokenParams.every(key => session[key])
+    const isNeedToUpdate = refreshByTokenParams.some(key => !oldRefreshSession?.[key])
+    // only update if oldRefreshSession refresh type is by authorizationCode,
+    // to prevent "invalid_grant" error when refreshing token
+    // https://docs.aws.amazon.com/cognito/latest/developerguide/token-endpoint.html
+    if (isRefreshByToken && isNeedToUpdate) {
+      store.dispatch(authSetRefreshSession({ ...session, redirectUri: null, state: null, authorizationCode: null }))
+    }
     return session.accessToken
   }
 
