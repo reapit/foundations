d1335771486ac580866f2ca2990dbd757cf87719
Khac Vy <vytk@reapit.com>
2020-04-22 14:59:42 +0700

    chore: Update agency integration type section text

--- packages/marketplace/src/components/pages/developer-submit-app.tsx
+++ packages/marketplace/src/components/pages/developer-submit-app.tsx
@@ -564,8 +564,15 @@ export const SubmitApp: React.FC<SubmitAppProps> = ({
                     <FormSubHeading>
                       To be able to associate your application with an action in Agency Cloud the application will need
                       to be given a desktop type. Please select the type of integration your app requires from the list
-                      below. For more information on Desktop Types, please click here (link to
-                      https://foundations-documentation.reapit.cloud/api/desktop-api#desktop-types)
+                      below. For more information on Desktop Types, please{' '}
+                      <a
+                        className={linkStyles.link}
+                        href="https://foundations-documentation.reapit.cloud/api/desktop-api#desktop-types"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        click here
+                      </a>
                     </FormSubHeading>
                     <Grid>
                       <GridItem>
