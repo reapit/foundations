fffbfdc6b7665e256a8e3a149e572143cc8a9e97
Nghia Pham <nghiapn@reapit.com>
2020-04-22 21:14:22 +0700

    fix conflicts

--- packages/graphql-server/src/resolvers/index.ts
+++ packages/graphql-server/src/resolvers/index.ts
@@ -5,12 +5,8 @@ import Areas from './areas/resolvers'
 import Offices from './offices/resolvers'
 import Appointments from './appointments/resolvers'
 import Negotiators from './negotiators/resolvers'
-<<<<<<< HEAD
 import Properties from './properties/resolvers'
-import PropertyTypes from './property-types/resolver'
-=======
 import Configurations from './configurations/resolver'
->>>>>>> support GET configurations and configuration (by ID) by pre-defined types
 
 export const resolvers = merge(
   {
@@ -21,11 +17,7 @@ export const resolvers = merge(
   Offices,
   Appointments,
   Negotiators,
-<<<<<<< HEAD
   Properties,
-  PropertyTypes,
-=======
   Configurations,
->>>>>>> support GET configurations and configuration (by ID) by pre-defined types
 )
 export default resolvers
