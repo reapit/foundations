db79b24b5da4ed34a201cbe7fef8504bd9a5af27
NghiaPham <nghiapn@reapit.com>
2020-04-23 08:43:38 +0700

    feat: #934 - Create GraphQL resolver for PropertyTypes in GraphQL Server (#962)

---
+++ packages/graphql-server/src/resolvers/configurations/__mocks__/appointmentTypes.ts
@@ -0,0 +1,85 @@
+import { ListItemModel } from '../../../types'
+
+export const appointmentTypeMock: ListItemModel = {
+  id: 'AP',
+  value: 'Appointment',
+}
+
+export const appointmentTypesMock: ListItemModel[] = [
+  {
+    id: 'AP',
+    value: 'Appointment',
+  },
+  {
+    id: 'CI',
+    value: 'Check In',
+  },
+  {
+    id: 'CO',
+    value: 'Check Out',
+  },
+  {
+    id: 'ES',
+    value: 'EPC Survey',
+  },
+  {
+    id: 'FA',
+    value: 'Financial Services Appointment',
+  },
+  {
+    id: 'HY',
+    value: 'Holiday',
+  },
+  {
+    id: 'IA',
+    value: 'Instruction Appointment',
+  },
+  {
+    id: 'VL',
+    value: 'Market Appraisal',
+  },
+  {
+    id: 'ME',
+    value: 'Meeting',
+  },
+  {
+    id: 'MI',
+    value: 'Miscellaneous',
+  },
+  {
+    id: 'OH',
+    value: 'Open House',
+  },
+  {
+    id: 'HV',
+    value: 'Open House Viewing',
+  },
+  {
+    id: 'PI',
+    value: 'Property Inspection',
+  },
+  {
+    id: 'PL',
+    value: 'Property Launch',
+  },
+  {
+    id: 'RR',
+    value: 'Rent Review',
+  },
+  {
+    id: 'RV',
+    value: 'RICS Valuation',
+  },
+  {
+    id: 'SY',
+    value: 'Survey',
+  },
+  {
+    id: '',
+    value: 'Unknown',
+  },
+  {
+    id: 'VW',
+    value: 'Viewing',
+  },
+]
