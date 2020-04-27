0ef552c738b7a99ebb75eb3ed94c474b35d4e4ac
Pham Hai Duong <duongph@reapit.com>
2020-04-27 17:21:09 +0700

    fix: #1071 change api-version to correct one 2020-01-31 (#1084)

--- packages/geo-diary/src/constants/api.ts
+++ packages/geo-diary/src/constants/api.ts
@@ -5,7 +5,7 @@ export const APPOINTMENTS_HEADERS = {
   'Content-Type': 'application/json',
 } as StringMap
 
-export const API_VERSION = '2020-02-06'
+export const API_VERSION = '2020-01-31'
 
 export const COOKIE_SESSION_KEY_GEO_DIARY = `${COOKIE_SESSION_KEY}-geo-diary`
 
