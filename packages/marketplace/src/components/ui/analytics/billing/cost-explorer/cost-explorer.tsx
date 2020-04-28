308ed8738216eefa6a471c55341cf06523dceeb7
Vu Nguyen <vunp@reapit.com>
2020-04-28 15:48:30 +0700

    feat: #1078 - Add explorer cost section

---
+++ packages/marketplace/src/components/ui/analytics/billing/cost-explorer/cost-explorer.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { H4 } from '@reapit/elements'
+
+export type CostExplorerProps = {}
+
+const CostExplorer: React.FC<CostExplorerProps> = () => {
+  return (
+    <>
+      <H4>Cost Explorer: Cost & Usage</H4>
+    </>
+  )
+}
+export default CostExplorer
