65b2295616a55face74b2a79729dce456aaab6bf
Vu Nguyen <vunp@reapit.com>
2020-04-28 17:16:34 +0700

    feat: #1089 Update cost calculator table UI (#1096)
    * feat: #1089 - Update total calculator table layout

--- packages/marketplace/src/components/ui/analytics/billing/cost-calculator/__test__/total-cost-table.test.tsx
+++ packages/marketplace/src/components/ui/analytics/billing/cost-calculator/__test__/total-cost-table.test.tsx
@@ -5,12 +5,10 @@ import TotalCostTable, {
   TotalCostTableProps,
   TotalCostTableData,
   prepareData,
-  renderEndpointsUsed,
-  renderApiCalls,
-  renderBand,
-  renderCostPerBand,
-  renderBandCostPerCall,
+  prepareTableColumns,
+  toggleShowTable,
 } from '../total-cost-table'
+import { formatNumber, formatCurrency } from '@/utils/number-formatter'
 
 const mockProps: TotalCostTableProps = {
   endpointsUsedRange: endpointsUsedRange,
@@ -23,9 +21,43 @@ const mockProps: TotalCostTableProps = {
 const mockEndpointsUsed = 'tier4'
 const mockApiCalls = '100000'
 const mockTotalCostTableData = {
-  bands: [1000, 1500, 2500, 5000, 15000, 25000, 50000],
-  bandCostPerCall: [0.0145, 0.0116, 0.0087, 0.00725, 0.0058, 0.003625, 0.00145],
-  costPerBand: [14.5, 17.4, 21.75, 36.25, 87, 90.625, 72.5],
+  tableData: [
+    {
+      numberOfApiCalls: 1000,
+      costPerApiCall: 0.0145,
+      totalCost: 14.5,
+    },
+    {
+      numberOfApiCalls: 1500,
+      costPerApiCall: 0.0116,
+      totalCost: 17.4,
+    },
+    {
+      numberOfApiCalls: 2500,
+      costPerApiCall: 0.0087,
+      totalCost: 21.75,
+    },
+    {
+      numberOfApiCalls: 5000,
+      costPerApiCall: 0.00725,
+      totalCost: 36.25,
+    },
+    {
+      numberOfApiCalls: 15000,
+      costPerApiCall: 0.0058,
+      totalCost: 87,
+    },
+    {
+      numberOfApiCalls: 25000,
+      costPerApiCall: 0.003625,
+      totalCost: 90.625,
+    },
+    {
+      numberOfApiCalls: 50000,
+      costPerApiCall: 0.00145,
+      totalCost: 72.5,
+    },
+  ],
   totalMonthlyCost: 340.025,
 }
 
@@ -35,43 +67,57 @@ describe('CostCalculator', () => {
   })
   describe('prepareData', () => {
     it('should run correctly', () => {
-      const result: TotalCostTableData = prepareData(mockEndpointsUsed, mockApiCalls, foundationPricing)
+      const fn = prepareData(mockEndpointsUsed, mockApiCalls, foundationPricing)
+      const result: TotalCostTableData = fn()
       expect(result).toEqual(mockTotalCostTableData)
     })
   })
-  describe('renderEndpointsUsed', () => {
-    it('should run correctly', () => {
-      const result = renderEndpointsUsed(mockTotalCostTableData.bands, endpointsUsedRange, mockEndpointsUsed)
-      const wrapper = shallow(<tr>{result}</tr>)
-      expect(wrapper).toMatchSnapshot()
+  describe('prepareTableColumns', () => {
+    const fn = prepareTableColumns(mockTotalCostTableData.totalMonthlyCost)
+    const columns = fn()
+    it('should return 3 columns in total', () => {
+      expect(columns).toHaveLength(3)
+    })
+    it('should return valid Header, accessor and Footer for each column', () => {
+      mockTotalCostTableData.tableData.forEach(row => {
+        columns.forEach(({ Header, accessor, Footer }, index) => {
+          if (index === 0) {
+            expect(Header).toEqual('Number of API Calls')
+            if (typeof accessor === 'function') {
+              const numberOfApiCalls = accessor(row)
+              expect(numberOfApiCalls).toEqual(formatNumber(row.numberOfApiCalls))
+            }
+          }
+          if (index === 1) {
+            expect(Header).toEqual('Cost Per API Call')
+            if (typeof accessor === 'function') {
+              const costPerApiCall = accessor(row)
+              expect(costPerApiCall).toBe(formatCurrency(row.costPerApiCall, 6))
+            }
+            expect(Footer).toEqual('Estimated total monthly cost')
+          }
+          if (index === 2) {
+            expect(Header).toEqual('Total Cost')
+            if (typeof accessor === 'function') {
+              const totalCost = accessor(row)
+              expect(totalCost).toEqual(formatCurrency(row.totalCost))
+            }
+            if (typeof Footer === 'function') {
+              const totalMonthlyCost = Footer()
+              expect(totalMonthlyCost).toBe(formatCurrency(mockTotalCostTableData.totalMonthlyCost))
+            }
+          }
+        })
+      })
     })
   })
-  describe('renderApiCalls', () => {
+  describe('toggleShowTable', () => {
     it('should run correctly', () => {
-      const result = renderApiCalls(mockTotalCostTableData.bands, mockApiCalls)
-      const wrapper = shallow(<tr>{result}</tr>)
-      expect(wrapper).toMatchSnapshot()
-    })
-  })
-  describe('renderBand', () => {
-    it('should run correctly', () => {
-      const result = renderBand(mockTotalCostTableData.bands)
-      const wrapper = shallow(<tr>{result}</tr>)
-      expect(wrapper).toMatchSnapshot()
-    })
-  })
-  describe('renderCostPerBand', () => {
-    it('should run correctly', () => {
-      const result = renderCostPerBand(mockTotalCostTableData.costPerBand)
-      const wrapper = shallow(<tr>{result}</tr>)
-      expect(wrapper).toMatchSnapshot()
-    })
-  })
-  describe('renderBandCostPerCall', () => {
-    it('should run correctly', () => {
-      const result = renderBandCostPerCall(mockTotalCostTableData.bandCostPerCall)
-      const wrapper = shallow(<tr>{result}</tr>)
-      expect(wrapper).toMatchSnapshot()
+      const mockIsTableExpanded = false
+      const mockSetIsTableExpanded = jest.fn()
+      const fn = toggleShowTable(mockIsTableExpanded, mockSetIsTableExpanded)
+      fn()
+      expect(mockSetIsTableExpanded).toBeCalledWith(!mockIsTableExpanded)
     })
   })
 })
