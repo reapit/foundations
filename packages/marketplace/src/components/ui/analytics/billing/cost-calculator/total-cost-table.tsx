59a7c25234a16433e4828920a84aa1181b899f0e
Vu Nguyen <vunp@reapit.com>
2020-04-28 14:36:16 +0700

    feat: #1089 - Fix typo issues

--- packages/marketplace/src/components/ui/analytics/billing/cost-calculator/total-cost-table.tsx
+++ packages/marketplace/src/components/ui/analytics/billing/cost-calculator/total-cost-table.tsx
@@ -128,7 +128,7 @@ const TotalCostTable: React.FC<TotalCostTableProps> = ({
     <div className="mt-5">
       <Grid>
         <GridItem className="is-half-desktop has-text-right">
-          <H6>{endpointsUsedRange[endpointsUsed]} Endoints Used</H6>
+          <H6>{endpointsUsedRange[endpointsUsed]} Endpoints Used</H6>
           <H6>{formatNumber(parseFloat(apiCalls))} Monthly API Calls</H6>
           <H4 className="mt-mt-5">Estimated total monthly cost: {formatCurrency(totalMonthlyCost)}</H4>
           <Button type="button" variant="secondary" onClick={toggleShowTable(isTableExpanded, setIsTableExpanded)}>
