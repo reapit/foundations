800bba78524ddb93d5342a5b15678af7aa609af5
NghiaPham <nghiapn@reapit.com>
2020-04-28 21:00:44 +0700

    fix: #1065 fix loading chunk fail (#1092)

--- packages/lifetime-legal/src/core/app.tsx
+++ packages/lifetime-legal/src/core/app.tsx
@@ -1,15 +1,27 @@
-import { PortalProvider } from '@reapit/elements'
+import { PortalProvider, useOfflinePLugin, ToastMessage } from '@reapit/elements'
 import Router from './router'
 import { Provider } from 'react-redux'
 import store from './store'
 import * as React from 'react'
 
-const App = () => (
-  <Provider store={store.reduxStore}>
-    <PortalProvider>
-      <Router />
-    </PortalProvider>
-  </Provider>
-)
+const App = () => {
+  const { isNewVersionAvailable } = useOfflinePLugin()
+
+  return (
+    <Provider store={store.reduxStore}>
+      <PortalProvider>
+        <Router />
+        <ToastMessage
+          preventClose={true}
+          visible={isNewVersionAvailable}
+          variant="primary"
+          onCloseToast={location.reload}
+          /* eslint-disable-next-line max-len */
+          message="A new version is available. Please refresh your browser or click on this notification to receive the latest update."
+        />
+      </PortalProvider>
+    </Provider>
+  )
+}
 
 export default App
