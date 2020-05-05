05abc21889d0aadc1c6e0b20750f422f52773e34
Nghia Pham <nghiapn@reapit.com>
2020-05-05 13:20:00 +0700

    remove unused utility by pre-built utility: get hash commit - revert scafolder webpack scripts

--- scripts/utils/get-head-commit-hash.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process')
-
-const headCommitHashBuffer = execSync('git rev-parse HEAD')
-exports.headCommitHash = headCommitHashBuffer.toString().trim()
--- scripts/webpack/webpack-sass-prod/build-configuration.js
+++ scripts/webpack/webpack-sass-prod/build-configuration.js
@@ -1,8 +1,9 @@
 const webpackBase = require('../webpack.base.prod')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { buildCssFilePath } = require('../build-element-scss')
-const { headCommitHash } = require('../../utils/get-head-commit-hash')
-const outputCSSFileName = `css/[name].${headCommitHash}.css`
+const { getRef } = require('../release/utils')
+const hashOfCommit = getRef()
+const outputCSSFileName = `css/[name].${hashOfCommit}.css`
 
 module.exports = {
   ...{
