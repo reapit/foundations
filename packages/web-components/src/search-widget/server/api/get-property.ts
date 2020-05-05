bf0ce4cd68fb928004bc14ff344532247b3849a2
Khac Vy <vytk@reapit.com>
2020-05-05 20:19:45 +0700

    chore: add unity funciton picking property object

--- packages/web-components/src/search-widget/server/api/get-property.ts
+++ packages/web-components/src/search-widget/server/api/get-property.ts
@@ -1,23 +1,27 @@
 import { Request, Response } from 'express'
 import { fetcher } from '../../../common/utils/fetcher-server'
-import pickBy from 'lodash.pickby'
+import { PropertyModel } from '@reapit/foundations-ts-definitions'
 import { errorHandler } from '../../../common/utils/error-handler'
 import { getServerHeaders } from '../../../common/utils/get-server-headers'
 import { PACKAGE_SUFFIXES } from '../../../common/utils/constants'
 import { PickedPropertyModel } from '../../types'
+import { mapMinimalProperty } from '../utils/map-minimal-property'
 import { INCLUDED_PROPS } from '../constants/api'
 
 export const getProperty = async (req: Request, res: Response) => {
   try {
     const headers = await getServerHeaders(req, PACKAGE_SUFFIXES.SEARCH_WIDGET)
     const url = new URL(`${process.env.PLATFORM_API_BASE_URL}${req.url}`)
-    const result = await fetcher<PickedPropertyModel, undefined>({
+    const result = await fetcher<PropertyModel, undefined>({
       url: String(url),
       headers,
     })
 
     if (result) {
-      const minimalResult = pickBy(result, (_, key) => INCLUDED_PROPS.GET_PROPERTIES.includes(key))
+      const minimalResult = mapMinimalProperty<PropertyModel, PickedPropertyModel>(
+        result,
+        INCLUDED_PROPS.GET_PROPERTIES,
+      )
       res.status(200)
       res.json(minimalResult)
       res.end()
