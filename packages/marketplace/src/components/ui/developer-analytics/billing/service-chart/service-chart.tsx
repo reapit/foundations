533632f4e57d39a9ae17ab798573516416f1fa3a
Pham Hai Duong <duongph@reapit.com>
2020-05-07 22:51:32 +0700

    feat:  integrate api for billing bar chart (#1168)
    Changes
    - Integrate api for billing bar chart
    - Write test
    - Refactor some places

--- packages/marketplace/src/components/ui/developer-analytics/billing/service-chart/service-chart.tsx
+++ packages/marketplace/src/components/ui/developer-analytics/billing/service-chart/service-chart.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Bar } from 'react-chartjs-2'
-import { H4, FlexContainerResponsive } from '@reapit/elements'
+import { Bar, ChartData } from 'react-chartjs-2'
+import { H4, FlexContainerResponsive, DATE_TIME_FORMAT, Loader } from '@reapit/elements'
+import { AppSummaryModel, DeveloperModel } from '@reapit/foundations-ts-definitions'
 import styles from '@/styles/pages/developer-analytics.scss?mod'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  selectDeveloperApps,
+  selectMyIdentity,
+  selectBilling,
+  selectDeveloperLoading,
+  selectIsServiceChartLoading,
+} from '@/selector'
+import { fetchBilling } from '@/actions/developer'
+import dayjs from 'dayjs'
+import { Billing, RequestByPeriod } from '@/reducers/developer'
+import { Dispatch } from 'redux'
 
-// TODO will replace mock data when have API
-const data = {
-  labels: ['January', 'February', 'March', 'April'],
-  datasets: [
+export const mapServiceChartDataSet = (billing: Billing | null) => {
+  let datasets = [
     {
       label: 'Resource',
       backgroundColor: 'rgba(255,99,132,0.2)',
@@ -14,34 +25,93 @@ const data = {
       borderWidth: 1,
       hoverBackgroundColor: 'rgba(255,99,132,0.4)',
       hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [10, 5, 10, 20],
+      data: [] as number[],
     },
-  ],
+  ]
+  let labels: string[] = []
+  if (!billing) {
+    return {
+      labels,
+      datasets,
+    }
+  }
+  billing.requestsByPeriod.forEach((item: RequestByPeriod) => {
+    labels.push(item.periodName)
+    datasets[0].data.push(item.grossAmount)
+  })
+  return {
+    labels,
+    datasets,
+  }
+}
+
+export type StateProps = {
+  myApps: AppSummaryModel[]
+  myIdentity: DeveloperModel
+  billing: Billing | null
+  loading: boolean
+  isServiceChartLoading: boolean
+}
+
+export type HandleUseEffectParams = {
+  myAppIds: string[]
+  dateFrom: string
+  dateTo: string
+  dispatch: Dispatch
+}
+
+export const handleUseEffect = ({ myAppIds, dateFrom, dateTo, dispatch }: HandleUseEffectParams) => () => {
+  const isHaveDeveloperApps = myAppIds.length > 0
+  if (isHaveDeveloperApps) {
+    dispatch(fetchBilling({ applicationId: myAppIds, dateFrom: dateFrom, dateTo: dateTo }))
+  }
+}
+
+export const renderChart = (isLoading: boolean, datasets: ChartData<any>) => {
+  if (isLoading) {
+    return <Loader />
+  }
+  return (
+    <Bar
+      data={datasets}
+      width={50}
+      height={50}
+      options={{
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: 'Total cost',
+              },
+            },
+          ],
+        },
+      }}
+    />
+  )
 }
 
 export const ServiceChart: React.FC = () => {
+  const dispatch = useDispatch()
+  const myApps = useSelector(selectDeveloperApps)
+  const myIdentity = useSelector(selectMyIdentity)
+  const billing = useSelector(selectBilling)
+  const loading = useSelector(selectDeveloperLoading)
+  const isServiceChartLoading = useSelector(selectIsServiceChartLoading)
+
+  const myAppIds = myApps.map((item: AppSummaryModel) => item.id) as string[]
+  const dateFrom = dayjs(myIdentity.created).format(DATE_TIME_FORMAT.YYYY_MM) as string
+  const dateTo = dayjs().format(DATE_TIME_FORMAT.YYYY_MM)
+  React.useEffect(handleUseEffect({ myAppIds, dateFrom, dateTo, dispatch }), [myIdentity.id, myApps.length])
+  const datasets = mapServiceChartDataSet(billing)
+  const isLoading = loading || isServiceChartLoading
   return (
     <FlexContainerResponsive className={styles.serviceChart}>
       <div className={styles.barChartContainer}>
         <H4>Services</H4>
-        <Bar
-          data={data}
-          width={50}
-          height={50}
-          options={{
-            maintainAspectRatio: false,
-            scales: {
-              yAxes: [
-                {
-                  scaleLabel: {
-                    display: true,
-                    labelString: 'Total cost',
-                  },
-                },
-              ],
-            },
-          }}
-        />
+        {renderChart(isLoading, datasets)}
       </div>
     </FlexContainerResponsive>
   )
