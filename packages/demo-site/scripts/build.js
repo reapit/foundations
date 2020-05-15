54bd639d38a646561ca8266e55d679a2658a0d22
Khac Vy <vytk@reapit.com>
2020-05-15 09:39:26 +0700

    chore: Update build script, add detail page

--- packages/demo-site/scripts/build.js
+++ packages/demo-site/scripts/build.js
@@ -2,14 +2,13 @@ const ejs = require('ejs')
 const fs = require('fs')
 const path = require('path')
 
-const compileTemplate = async () => {
+const compileTemplate = async filename => {
   try {
     const config = require('../config.json')
-    const ejsFileDir = path.resolve(__dirname, '..', 'index.ejs')
     const distDir = path.resolve(__dirname, '..', 'public', 'dist')
-    const indexHtmlDir = path.resolve(__dirname, '..', 'public', 'dist', 'index.html')
+    const ejsFileDir = path.resolve(__dirname, '..', `${filename}.ejs`)
+    const htmlDir = path.resolve(__dirname, '..', 'public', 'dist', `${filename}.html`)
     const ejsTemplate = await fs.readFileSync(ejsFileDir, { encoding: 'utf8' })
-
     const template = ejs.compile(ejsTemplate)
 
     let CDN_URL = config.CDN_URL
@@ -22,11 +21,21 @@ const compileTemplate = async () => {
     const html = template({ API_KEY: config.API_KEY, CUSTOMER_ID: config.CUSTOMER_ID, CDN_URL })
 
     fs.mkdirSync(distDir, { recursive: true })
-    fs.writeFileSync(indexHtmlDir, html)
+    fs.writeFileSync(htmlDir, html)
   } catch (err) {
     console.error(err)
     process.exit(1)
   }
 }
 
-compileTemplate()
+const build = async () => {
+  try {
+    const filenames = ['index', 'detail']
+    await Promise.all(filenames.map(fileName => compileTemplate(fileName)))
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+build()
