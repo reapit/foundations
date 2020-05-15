3e4325a9135bbcef9c9580e358166d1a59ebe118
Pham Hai Duong <duongph@reapit.com>
2020-05-15 14:18:43 +0700

    fix: change from cp to S3 instead of sync (#1264)
    Changes
    - Add delete from remote bucket repo
    - Change from sync to cp to s3 cli

--- scripts/release/release-dev.js
+++ scripts/release/release-dev.js
@@ -20,14 +20,17 @@ const releaseDev = () => {
 
     const distPath = path.resolve(__dirname, '../../', 'packages', packageName, 'public', 'dist')
     runCommand('rimraf', [`${distPath}/**/*.map`])
+    // This command remove the old version file in bucket
+    runCommand('aws', ['s3', 'rm', '--recursive', `s3://${bucketName}`])
     // Sync new version to the bucket
     runCommand('aws', [
       's3',
-      'sync',
+      'cp',
       distPath,
       `s3://${bucketName}`,
       '--grants',
       'read=uri=http://acs.amazonaws.com/groups/global/AllUsers',
+      '--recursive',
       '--cache-control',
       'max-age=31536000',
     ])
