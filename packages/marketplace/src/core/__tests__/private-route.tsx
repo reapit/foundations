8e234d0d16e860738aea0f1cc5ca501658f89773
Vu Nguyen <nphivu414@gmail.com>
2020-05-18 16:46:10 +0700

    feat: #101: Update tests

--- packages/marketplace/src/core/__tests__/private-route.tsx
+++ packages/marketplace/src/core/__tests__/private-route.tsx
@@ -1,43 +1,124 @@
 import * as React from 'react'
-import { shallow, mount } from 'enzyme'
-
-import { MemoryRouter, Route } from 'react-router'
-import { PrivateRoute } from '../private-route'
+import * as ReactRedux from 'react-redux'
+import { mount } from 'enzyme'
+import configureStore from 'redux-mock-store'
+import {
+  PrivateRoute,
+  isNotAllowedToAccess,
+  handleChangeLoginType,
+  handleRedirectToAuthenticationPage,
+} from '../private-route'
+import appState from '@/reducers/__stubs__/app-state'
+import { LoginIdentity } from '@reapit/cognito-auth'
+import { authChangeLoginType } from '@/actions/auth'
+import Routes from '@/constants/routes'
+import { MemoryRouter } from 'react-router'
 
 describe('PrivateRouter', () => {
+  let store
+  let spyDispatch
+  beforeEach(() => {
+    /* mocking store */
+    const mockStore = configureStore()
+    store = mockStore(appState)
+    spyDispatch = jest.spyOn(ReactRedux, 'useDispatch').mockImplementation(() => store.dispatch)
+  })
+
   it('should match a snapshot', () => {
-    expect(shallow(<PrivateRoute allow="CLIENT" component={() => null} />)).toMatchSnapshot()
+    expect(
+      mount(
+        <ReactRedux.Provider store={store}>
+          <MemoryRouter initialEntries={['/client']}>
+            <PrivateRoute allow="CLIENT" component={() => null} />
+          </MemoryRouter>
+        </ReactRedux.Provider>,
+      ),
+    ).toMatchSnapshot()
   })
 
-  it('should redirect to /404 page if isLogin is false', () => {
-    const wrapper = mount(
-      <MemoryRouter initialEntries={['/my-path']}>
-        <PrivateRoute allow="CLIENT" component={() => null} path="/my-path" />
-        <Route path="/404" render={() => <div className="not-found" />} />
-      </MemoryRouter>,
-    )
-    expect(wrapper.find('.not-found')).toHaveLength(1)
+  describe('isNotAllowedToAccess', () => {
+    it('should return false if loginIdentity is empty', () => {
+      expect(isNotAllowedToAccess()).toBeFalsy
+    })
+    it('should return true if both clientId and developerId are empty', () => {
+      const mockLoginIdentity = {
+        clientId: '',
+        developerId: '',
+      } as LoginIdentity
+      expect(isNotAllowedToAccess(mockLoginIdentity)).toBeTruthy
+    })
+    it('should return false if clientId is existed', () => {
+      const mockLoginIdentity = {
+        clientId: 'testClientId',
+        developerId: '',
+      } as LoginIdentity
+      expect(isNotAllowedToAccess(mockLoginIdentity)).toBeFalsy
+    })
+    it('should return false if developerId is existed', () => {
+      const mockLoginIdentity = {
+        clientId: '',
+        developerId: 'testDeveloperId',
+      } as LoginIdentity
+      expect(isNotAllowedToAccess(mockLoginIdentity)).toBeFalsy
+    })
   })
 
-  it('should return render component if loginType matches allow is true', () => {
-    const wrapper = mount(
-      <MemoryRouter initialEntries={['/client']}>
-        <PrivateRoute allow="CLIENT" component={() => <div className="render-class" />} path="/client" />
-      </MemoryRouter>,
-    )
-    expect(wrapper.find('.render-class')).toHaveLength(1)
+  describe('handleChangeLoginType', () => {
+    it('should dispatch authChangeLoginType to change loginType to DEVELOPER', () => {
+      const mockLoginIdentity = {
+        clientId: '',
+        developerId: 'testDeveloperId',
+      } as LoginIdentity
+      const fn = handleChangeLoginType(mockLoginIdentity, 'CLIENT', 'DEVELOPER', spyDispatch)
+      fn()
+      expect(spyDispatch).toBeCalledWith(authChangeLoginType('DEVELOPER'))
+    })
+    it('should dispatch authChangeLoginType to change loginType to CLIENT', () => {
+      const mockLoginIdentity = {
+        clientId: 'testClient',
+        developerId: '',
+      } as LoginIdentity
+      const fn = handleChangeLoginType(mockLoginIdentity, 'DEVELOPER', 'CLIENT', spyDispatch)
+      fn()
+      expect(spyDispatch).toBeCalledWith(authChangeLoginType('CLIENT'))
+    })
   })
 
-  it('should return render component if loginType is included in allow array is true', () => {
-    const wrapper = mount(
-      <MemoryRouter initialEntries={['/developer/my-apps']}>
-        <PrivateRoute
-          allow={['CLIENT', 'DEVELOPER']}
-          component={() => <div className="render-class" />}
-          path="/developer/my-apps"
-        />
-      </MemoryRouter>,
-    )
-    expect(wrapper.find('.render-class')).toHaveLength(1)
+  describe('handleRedirectToAuthenticationPage', () => {
+    const { location } = window
+
+    beforeAll((): void => {
+      delete window.location
+      // @ts-ignore
+      window.location = {
+        href: '',
+      }
+    })
+
+    afterAll((): void => {
+      window.location = location
+    })
+
+    it('should redirect to authentication page for CLIENT', () => {
+      const mockLoginIdentity = {
+        clientId: '',
+        developerId: 'testDeveloperId',
+      } as LoginIdentity
+      const mockAllow = 'CLIENT'
+      const fn = handleRedirectToAuthenticationPage(mockLoginIdentity, mockAllow)
+      fn()
+      expect(window.location.href).toBe(`${Routes.Authentication}/${mockAllow}`)
+    })
+
+    it('should redirect to authentication page for DEVELOPER', () => {
+      const mockLoginIdentity = {
+        clientId: 'testClientId',
+        developerId: '',
+      } as LoginIdentity
+      const mockAllow = 'DEVELOPER'
+      const fn = handleRedirectToAuthenticationPage(mockLoginIdentity, mockAllow)
+      fn()
+      expect(window.location.href).toBe(`${Routes.Authentication}/${mockAllow}`)
+    })
   })
 })
