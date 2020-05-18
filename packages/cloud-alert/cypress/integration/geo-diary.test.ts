3df7abe3780b8a48fd592408a0ff95fdded4f475
Duong Pham <tanphamhaiduong@gmail.com>
2020-05-18 18:16:55 +0700

    feat: #1221 add cloud alert for production env web apps
    Changes
    - Add cypress request cronjob for apps each 30 mins
    - Add push notification to cloud-sentry channel if fail

---
+++ packages/cloud-alert/cypress/integration/geo-diary.test.ts
@@ -0,0 +1,28 @@
+import {
+  LOGIN_USERNAME_INPUT_SELECTOR,
+  LOGIN_PASSWORD_INPUT_SELECTOR,
+  SUBMIT_LOGIN_SELECTOR,
+} from '../support/constants'
+
+Cypress.on('fail', (error, runnable) => {
+  fetch(Cypress.env('SLACK_WEB_HOOK_URL'), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      text: `Failed on ${runnable.title}`,
+    }),
+  })
+  throw error
+})
+
+describe('Geo Diary App', () => {
+  it('user should able to login Geo Diary App', () => {
+    cy.visit(Cypress.env('GEO_DIARY_URL')).then(() => {
+      cy.get(LOGIN_USERNAME_INPUT_SELECTOR).type(Cypress.env('USERNAME'))
+      cy.get(LOGIN_PASSWORD_INPUT_SELECTOR).type(Cypress.env('PASSWORD'))
+      cy.get(SUBMIT_LOGIN_SELECTOR).click()
+    })
+  })
+})
---
+++ packages/cloud-alert/cypress/integration/marketplace.test.ts
@@ -0,0 +1,32 @@
+import {
+  LOGIN_USERNAME_INPUT_SELECTOR,
+  LOGIN_PASSWORD_INPUT_SELECTOR,
+  LOGIN_BUTTON_SELECTOR,
+  SUBMIT_LOGIN_SELECTOR,
+} from '../support/constants'
+
+Cypress.on('fail', (error, runnable) => {
+  fetch(Cypress.env('SLACK_WEB_HOOK_URL'), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      text: `Failed on ${runnable.title}`,
+    }),
+  })
+  throw error
+})
+
+describe('Marketplace', () => {
+  it('user should able to login Marketplace', () => {
+    cy.visit(Cypress.env('MARKETPLACE_URL')).then(() => {
+      cy.get(LOGIN_BUTTON_SELECTOR)
+        .should('contain', 'Login')
+        .click()
+      cy.get(LOGIN_USERNAME_INPUT_SELECTOR).type(Cypress.env('USERNAME'))
+      cy.get(LOGIN_PASSWORD_INPUT_SELECTOR).type(Cypress.env('PASSWORD'))
+      cy.get(SUBMIT_LOGIN_SELECTOR).click()
+    })
+  })
+})
