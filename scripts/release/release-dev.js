111cc88e960a9dc1e469098c45e5f609ba0b7335
Duong Pham <tanphamhaiduong@gmail.com>
2020-05-19 10:48:59 +0700

    ci: fix aws cp command to exclude sw.js and index.html for caching

--- scripts/release/release-dev.js
+++ scripts/release/release-dev.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const path = require('path')
 const { runCommand } = require('./utils')
+const execSync = require('child_process').execSync
 
 const releaseDev = () => {
   try {
@@ -21,19 +22,18 @@ const releaseDev = () => {
     const distPath = path.resolve(__dirname, '../../', 'packages', packageName, 'public', 'dist')
     runCommand('rimraf', [`${distPath}/**/*.map`])
     // This command remove the old version file in bucket
-    runCommand('aws', ['s3', 'rm', '--recursive', `s3://${bucketName}`])
-    // Sync new version to the bucket
-    runCommand('aws', [
-      's3',
-      'cp',
-      distPath,
-      `s3://${bucketName}`,
-      '--grants',
-      'read=uri=http://acs.amazonaws.com/groups/global/AllUsers',
-      '--recursive',
-      '--cache-control',
-      'max-age=31536000',
-    ])
+    const deleteResult = execSync(`aws s3 rm --recursive s3://${bucketName}`).toString()
+    console.info(deleteResult)
+    // cp all assert with cache-control 365 days exclude sw.js and index.html
+    const copyWithCache = execSync(
+      `aws s3 cp ${distPath} s3://${bucketName} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive --exclude "index.html" --exclude "sw.js" --exclude "config.json" --cache-control "max-age=31536000"`,
+    ).toString()
+    console.info(copyWithCache)
+    // cp index.html and sw.js with no-cache control
+    const copyWithNoCache = execSync(
+      `aws s3 cp ${distPath} s3://${bucketName} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive --exclude "*" --include "sw.js" --include "index.html" --include "config.json"`,
+    ).toString()
+    console.info(copyWithNoCache)
   } catch (err) {
     console.error(err)
     throw new Error(err)
