394f5f26073eeb0f5c58c843c02c18ac11af0d47
Khac Vy <vytk@reapit.com>
2020-05-21 20:38:55 +0700

    fix: #1292 fix Toast should be full width in desktop mode (#1306)

--- packages/marketplace/src/components/ui/sandbox-pop-up.tsx
+++ packages/marketplace/src/components/ui/sandbox-pop-up.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { ToastMessage } from '@reapit/elements'
 import styles from '@/styles/blocks/sandbox-pop-up.scss?mod'
+import { useSelector } from 'react-redux'
+import { selectRefreshSession } from '@/selector/auth'
 
 export const HALF_SECOND = 500
 
@@ -12,13 +14,16 @@ export const SandboxPopUp = ({
   loading = false,
   message = 'This is a sandbox environment, with anonymised test data and isolated from production',
 }) => {
+  const refreshSession = useSelector(selectRefreshSession)
+  const loginMode = refreshSession?.mode
+
   const [isOpen, setIsOpen] = React.useState<boolean>(false)
   const setOpen = React.useCallback(setIsOpen.bind(null, true), [setIsOpen])
 
   React.useEffect(popUp(setOpen, loading), [loading])
 
   return (
-    <div className={styles['wrap-pop-up']}>
+    <div className={`${styles['wrap-pop-up']} ${loginMode === 'DESKTOP' ? styles['wrap-pop-up-desktop'] : ''}`}>
       <ToastMessage visible={isOpen} message={message} variant="info" onCloseToast={setOpen} />
     </div>
   )
