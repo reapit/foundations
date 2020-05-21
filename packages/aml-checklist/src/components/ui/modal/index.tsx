207be384959f958fd73562b123ee81c606583849
Khac Vy <vytk@reapit.com>
2020-05-21 10:06:55 +0700

    fix: #964 fix unable to view an uploaded file in AML (#1286)

--- packages/aml-checklist/src/components/ui/modal/index.tsx
+++ packages/aml-checklist/src/components/ui/modal/index.tsx
@@ -1,3 +1,14 @@
 import Modal from './modal'
 
 export default Modal
+
+export const STEPS = {
+  PROFILE: 'Personal Details',
+  PRIMARY_IDENTIFICATION: 'Primary ID',
+  SECONDARY_IDENTIFICATION: 'Secondary ID',
+  ADDRESS_INFORMATION: 'Address Information',
+  REPORT: 'Report',
+  DECLARATION_RISK_MANAGEMENT: 'Declaration Risk Management',
+  EXPERIAN: 'Experian',
+  PEP_SEARCH: 'PEP Search',
+}
