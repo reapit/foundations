195c0994cc242ac6b5fe5c2111039393a3988373
Cuong Vu <cuongvh@reapit.com>
2020-05-27 18:27:03 +0700

    refactor: #1336 refactor private-route-wrapper (#1363)

--- packages/marketplace/src/core/__tests__/private-route-wrapper.tsx
+++ packages/marketplace/src/core/__tests__/private-route-wrapper.tsx
@@ -1,140 +1,154 @@
 import * as React from 'react'
-import { shallow } from 'enzyme'
+import { MemoryRouter, useLocation } from 'react-router'
+import { Provider, useDispatch, useSelector } from 'react-redux'
+import { shallow, mount } from 'enzyme'
+import configureStore from 'redux-mock-store'
+import appState from '@/reducers/__stubs__/app-state'
+import { PrivateRouteWrapper, handleSetTermsAcceptFromCookie } from '../private-route-wrapper'
+import { selectLoginSession, selectRefreshSession, selectLoginType } from '@/selector/auth'
+import { getTokenFromQueryString, redirectToOAuth, RefreshParams } from '@reapit/cognito-auth'
+import { getAuthRouteByLoginType } from '@/utils/auth-route'
 import {
-  PrivateRouteWrapper,
-  PrivateRouteWrapperProps,
-  mapDispatchToProps,
-  mapStateToProps,
-} from '../private-route-wrapper'
-import { RefreshParams } from '@reapit/cognito-auth'
-import { ReduxState } from '@/types/core'
+  getCookieString,
+  COOKIE_DEVELOPER_FIRST_TIME_LOGIN_COMPLETE,
+  COOKIE_CLIENT_FIRST_TIME_LOGIN_COMPLETE,
+} from '@/utils/cookie'
+import {
+  authSetRefreshSession,
+  setInitDeveloperTermsAcceptedStateFromCookie,
+  setInitClientTermsAcceptedStateFromCookie,
+} from '@/actions/auth'
 
-jest.mock('../store')
+const locationMock = { search: '?state=CLIENT', pathname: '/test' }
+const refreshParams = appState.auth.refreshSession as RefreshParams
+const dispatch = jest.fn()
 
-// const FakeRoute = ({ path }: { path: string }) => <div className="render-class" />
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(() => dispatch),
+  useSelector: jest.fn(() => jest.fn()),
+}))
 
-const props: PrivateRouteWrapperProps = {
-  path: '/',
-  isLogin: false,
-  setDesktopSession: jest.fn(),
-  // @ts-ignore: ignore to fullfil the definition of RouteComponentProps
-  location: {
-    search: '/client/apps?username=wmcvay@reapit.com&desktopToken=TOKEN',
-  },
-}
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useLocation: jest.fn(() => locationMock),
+}))
 
-// @ts-ignore:
-const desktopProps: PrivateRouteWrapperProps = { ...props, isLogin: true, location: { search: '' } }
+jest.mock('@reapit/cognito-auth', () => ({
+  ...jest.requireActual('@reapit/cognito-auth'),
+  getTokenFromQueryString: jest.fn(() => refreshParams),
+  redirectToOAuth: jest.fn(),
+}))
 
-describe('PrivateRouter', () => {
-  it('should match a snapshot', () => {
-    expect(shallow(<PrivateRouteWrapper {...props} />)).toMatchSnapshot()
+jest.mock('@/utils/auth-route', () => ({
+  getDefaultRouteByLoginType: jest.fn(() => 'login-type-route'),
+  getAuthRouteByLoginType: jest.fn(() => 'auth-route'),
+}))
+
+jest.mock('@/utils/cookie', () => ({
+  ...jest.requireActual('@/utils/cookie'),
+  getCookieString: jest.fn(() => 'cookie-string'),
+}))
+
+jest.mock('@/actions/auth')
+
+describe('PrivateRouteWrapper', () => {
+  let store
+  beforeEach(() => {
+    const mockStore = configureStore()
+    store = mockStore(appState)
   })
 
-  it('should match a snapshot for desktop login', () => {
-    expect(shallow(<PrivateRouteWrapper {...desktopProps} />)).toMatchSnapshot()
+  afterEach(() => {
+    jest.clearAllMocks()
   })
 
-  describe('mapDispatchToProps', () => {
-    it('should call dispatch correctly', () => {
-      const mockDispatch = jest.fn()
-
-      const {
-        setRefreshSession,
-        setInitDeveloperTermsAcceptedStateFromCookie,
-        setClientTermAcceptedCookieAndState,
-      } = mapDispatchToProps(mockDispatch)
-
-      setRefreshSession(({ key: 'val' } as unknown) as RefreshParams)
-      expect(mockDispatch).toHaveBeenCalledTimes(1)
-
-      setInitDeveloperTermsAcceptedStateFromCookie()
-      expect(mockDispatch).toHaveBeenCalledTimes(2)
-
-      setClientTermAcceptedCookieAndState()
-      expect(mockDispatch).toHaveBeenCalledTimes(3)
-    })
+  it('should match snapshot', () => {
+    const wrapper = shallow(<PrivateRouteWrapper path="/" />)
+    expect(wrapper).toMatchSnapshot()
   })
 
-  describe('mapStateToProps', () => {
-    it('return correct state', () => {
-      const mockedState = {
-        auth: {
-          refreshSession: {
-            mode: 'DESKTOP',
-          },
-          isTermAccepted: true,
-          loginType: 'DEVELOPER',
-        },
-      }
+  it('should call hooks and functions with correct params', () => {
+    ;(useSelector as jest.Mocked<any>).mockImplementationOnce(() => appState.auth.loginSession)
+    ;(useSelector as jest.Mocked<any>).mockImplementationOnce(() => appState.auth.refreshSession)
+    ;(useLocation as jest.Mocked<any>).mockImplementationOnce(() => ({ ...locationMock, pathname: '/' }))
+    mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <PrivateRouteWrapper path="/" />
+        </MemoryRouter>
+      </Provider>,
+    )
+    expect(useDispatch).toHaveBeenCalled()
+    expect(useSelector).toHaveBeenCalledWith(selectLoginSession)
+    expect(useSelector).toHaveBeenCalledWith(selectRefreshSession)
+    expect(useSelector).toHaveBeenCalledWith(selectLoginType)
+    expect(useLocation).toHaveBeenCalled()
+    expect(getCookieString).toHaveBeenCalledWith(COOKIE_DEVELOPER_FIRST_TIME_LOGIN_COMPLETE)
+    expect(getCookieString).toHaveBeenCalledWith(COOKIE_CLIENT_FIRST_TIME_LOGIN_COMPLETE)
+    expect(getTokenFromQueryString).toHaveBeenCalledWith(
+      locationMock.search,
+      window.reapit.config.cognitoClientId,
+      'CLIENT',
+      'login-type-route',
+    )
+  })
 
-      expect(mapStateToProps((mockedState as unknown) as ReduxState)).toMatchObject({
-        isDesktopMode: true,
-        hasSession: true,
-        isTermAccepted: true,
-        loginType: 'DEVELOPER',
-      })
+  it('should call correct functions with refreshParams && !hasSession case', () => {
+    // mock useSelector to return loginSession & refreshSession as null
+    ;(useSelector as jest.Mocked<any>).mockImplementationOnce(() => null)
+    ;(useSelector as jest.Mocked<any>).mockImplementationOnce(() => null)
+    mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <PrivateRouteWrapper path="/" />
+        </MemoryRouter>
+      </Provider>,
+    )
+    expect(dispatch).toHaveBeenCalledWith(authSetRefreshSession(refreshParams))
+  })
+
+  it('should call correct functions with type && location.pathname ===  / case', () => {
+    // mock useSelector to return loginSession & refreshSession
+    ;(useLocation as jest.Mocked<any>).mockImplementationOnce(() => ({ ...locationMock, pathname: '/' }))
+    ;(useSelector as jest.Mocked<any>).mockImplementationOnce(() => appState.auth.loginSession)
+    ;(useSelector as jest.Mocked<any>).mockImplementationOnce(() => appState.auth.refreshSession)
+    mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <PrivateRouteWrapper path="/" />
+        </MemoryRouter>
+      </Provider>,
+    )
+    expect(getAuthRouteByLoginType).toHaveBeenCalledWith('CLIENT')
+  })
+
+  it('should call correct functions with !hasSession case', () => {
+    // mock useSelector to return loginSession & refreshSession as null
+    ;(useLocation as jest.Mocked<any>).mockImplementationOnce(() => ({ ...locationMock, pathname: '/test' }))
+    ;(useSelector as jest.Mocked<any>).mockImplementationOnce(() => null)
+    ;(useSelector as jest.Mocked<any>).mockImplementationOnce(() => null)
+    ;(getTokenFromQueryString as jest.Mocked<any>).mockImplementationOnce(() => null),
+      mount(
+        <Provider store={store}>
+          <MemoryRouter>
+            <PrivateRouteWrapper path="/" />
+          </MemoryRouter>
+        </Provider>,
+      )
+    expect(redirectToOAuth).toHaveBeenCalledWith(window.reapit.config.cognitoClientId, 'login-type-route', 'CLIENT')
+  })
+})
+
+describe('handleSetTermsAcceptFromCookie', () => {
+  it('should call 2 dispatch with correct params', () => {
+    const fn = handleSetTermsAcceptFromCookie({
+      dispatch,
+      setInitClientTermsAcceptedStateFromCookie,
+      setInitDeveloperTermsAcceptedStateFromCookie,
     })
-    describe('should return correct value of key "isDesktopMode"', () => {
-      it('return true if "state?.auth?.refreshSession?.mode" === "DESKTOP"', () => {
-        const mockedState = {
-          auth: {
-            refreshSession: {
-              mode: 'DESKTOP',
-            },
-          },
-        }
-
-        expect(mapStateToProps((mockedState as unknown) as ReduxState)).toMatchObject({
-          isDesktopMode: true,
-        })
-      })
-      it('return true if "state?.auth?.refreshSession?.mode" !== "DESKTOP"', () => {
-        const mockedState = {
-          auth: {
-            refreshSession: {
-              mode: 'NOT DESKTOP',
-            },
-          },
-        }
-
-        expect(mapStateToProps((mockedState as unknown) as ReduxState)).toMatchObject({
-          isDesktopMode: false,
-        })
-      })
-    })
-    describe('should return correct value of key "hasSession"', () => {
-      it('return true if "state.auth.loginSession" truthy', () => {
-        const mockedState = {
-          auth: {
-            loginSession: 'truthy',
-          },
-        }
-
-        expect(mapStateToProps((mockedState as unknown) as ReduxState)).toMatchObject({
-          hasSession: true,
-        })
-      })
-      it('return true if "state.auth.refreshSession" truthy', () => {
-        const mockedState = {
-          auth: {
-            refreshSession: 'truthy',
-          },
-        }
-
-        expect(mapStateToProps((mockedState as unknown) as ReduxState)).toMatchObject({
-          hasSession: true,
-        })
-      })
-      it('return true if both "state.auth.refreshSession" and "state.auth.refreshSession" is falsy', () => {
-        const mockedState = {
-          auth: {},
-        }
-
-        expect(mapStateToProps((mockedState as unknown) as ReduxState)).toMatchObject({
-          hasSession: false,
-        })
-      })
-    })
+    fn()
+    expect(dispatch).toHaveBeenCalledWith(setInitClientTermsAcceptedStateFromCookie())
+    expect(dispatch).toHaveBeenCalledWith(setInitDeveloperTermsAcceptedStateFromCookie())
   })
 })
