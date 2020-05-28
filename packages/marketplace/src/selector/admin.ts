802edb599c956d67fc19d2ca0fdde7a1263a6679
andt <andt@reapit.com>
2020-05-28 11:24:39 +0700

    refactor: #1327 Refactor Admin Dev Management using react-redux hooks

--- packages/marketplace/src/selector/admin.ts
+++ packages/marketplace/src/selector/admin.ts
@@ -1,6 +1,7 @@
 import { ReduxState } from '@/types/core'
 import { AdminApprovalsState } from '@/reducers/admin-approvals'
 import { PagedResultApprovalModel_ } from '@reapit/foundations-ts-definitions'
+import { AdminDevManamgenetState } from '@/reducers/admin-dev-management'
 
 export const selectAdminAppsState = (state: ReduxState) => {
   return state.adminApps
@@ -16,4 +17,8 @@ export const selectAdminApprovalsState = (state: ReduxState): AdminApprovalsStat
 
 export const selectWaitingApprovalData = (state: ReduxState): PagedResultApprovalModel_ => {
   return state?.adminApprovals?.adminApprovalsData?.data || {}
+}
+
+export const selectAdminDevManagement = (state: ReduxState): AdminDevManamgenetState => {
+  return state.adminDevManagement
 }
