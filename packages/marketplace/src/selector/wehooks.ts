c1d015d97d4f9bc2c30b316ca1f9fd7f00f713c1
Vu Nguyen <nphivu414@gmail.com>
2020-05-29 10:41:48 +0700

    fix: #1325 Update definition types for selector functions

--- packages/marketplace/src/selector/wehooks.ts
+++ packages/marketplace/src/selector/wehooks.ts
@@ -1,25 +1,26 @@
 import { ReduxState } from '@/types/core'
+import { TopicModel, WebhookModel } from '@/reducers/webhook-subscriptions'
 
-export const selectSubscriptionsData = (state: ReduxState) => {
+export const selectSubscriptionsData = (state: ReduxState): WebhookModel[] => {
   return state?.webhooks?.subscriptions?.subscriptions?._embedded || []
 }
 
-export const selectSubscriptionsLoading = (state: ReduxState) => {
+export const selectSubscriptionsLoading = (state: ReduxState): boolean => {
   return state?.webhooks?.subscriptions?.loading
 }
 
-export const selectTopicsData = (state: ReduxState) => {
+export const selectTopicsData = (state: ReduxState): TopicModel[] => {
   return state?.webhooks?.topics?.topics?._embedded || []
 }
 
-export const selectTopicsLoading = (state: ReduxState) => {
+export const selectTopicsLoading = (state: ReduxState): boolean => {
   return state?.webhooks?.topics?.loading
 }
 
-export const selectApplicationId = (state: ReduxState) => {
+export const selectApplicationId = (state: ReduxState): string => {
   return state?.webhooks?.topics?.applicationId
 }
 
-export const selectWebhookEditModalType = (state: ReduxState) => {
+export const selectWebhookEditModalType = (state: ReduxState): string => {
   return state.webhookEdit.modalType
 }
