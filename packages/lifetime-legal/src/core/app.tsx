78db4a6fe056181b77a49c3dbf34cda296f5b5aa
NghiaPham <nghiapn@reapit.com>
2020-05-29 10:05:24 +0700

    chore: remove offline plugin (#1387)

--- packages/lifetime-legal/src/core/app.tsx
+++ packages/lifetime-legal/src/core/app.tsx
@@ -1,25 +1,14 @@
-import { PortalProvider, useOfflinePLugin, ToastMessage } from '@reapit/elements'
+import { PortalProvider } from '@reapit/elements'
 import Router from './router'
 import { Provider } from 'react-redux'
 import store from './store'
 import * as React from 'react'
 
 const App = () => {
-  const bindedWindowLocation = React.useMemo(() => window.location.reload.bind(window.location), [])
-  const { isNewVersionAvailable } = useOfflinePLugin()
-
   return (
     <Provider store={store.reduxStore}>
       <PortalProvider>
         <Router />
-        <ToastMessage
-          preventClose={true}
-          visible={isNewVersionAvailable}
-          variant="primary"
-          onCloseToast={bindedWindowLocation}
-          /* eslint-disable-next-line max-len */
-          message="A new version is available. Please refresh your browser or click on this notification to receive the latest update."
-        />
       </PortalProvider>
     </Provider>
   )
