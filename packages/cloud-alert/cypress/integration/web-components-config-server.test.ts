6383dfef8a9ee9bbc9dd6c233758f1202a4b89a7
Duong Pham <duongph@reapit.cloud>
2020-06-04 16:42:07 +0700

    feat: 1287 automation ping for graphql-server and web-components-config-server
    Changes
    - Add automation ping for graphql server
    - Add automation ping for web-components
    - Add automation ping for web-components-config-server
    - Add ping resolver for graphql server

--- packages/cloud-alert/cypress/integration/web-components-config-server.test.ts
+++ packages/cloud-alert/cypress/integration/web-components-config-server.test.ts
@@ -11,29 +11,19 @@ Cypress.on('fail', (error, runnable) => {
   throw error
 })
 
-if (Cypress.env('PACKAGE_NAME') === 'all' || Cypress.env('PACKAGE_NAME') === 'appointment-planner-component') {
-  const WEB_COMPONENTS_API_URL = Cypress.env(`WEB_COMPONENTS_API_URL_${Cypress.env('ENVIRONMENT')}`)
-  describe('appointment-planner-component API', () => {
-    it('user should able to call /ping', () => {
-      cy.request({
-        url: `${WEB_COMPONENTS_API_URL}/ping`,
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': Cypress.env('WEB_COMPONENTS_X_API_KEY'),
-        },
-      }).as('appointmentSlots')
-      cy.get('@appointmentSlots').should((response: any) => {
-        expect(response).to.have.property('headers')
-        expect(response).to.have.property('body')
-        expect(response).to.have.property('status')
-        expect(response.status).to.equal(200)
-        expect(response.body).not.to.be.undefined
+if (Cypress.env('PACKAGE_NAME') === 'all' || Cypress.env('PACKAGE_NAME') === 'web-components-config-server') {
+  const WEB_COMPONENTS_CONFIG_API_URL = Cypress.env(`WEB_COMPONENTS_CONFIG_API_URL_${Cypress.env('ENVIRONMENT')}`)
+  describe('web-components-config-server API', () => {
+    let config = {}
+
+    beforeEach(function() {
+      cy.fixture('web-components-config-server/web-components-config.json').then(data => {
+        config = data
       })
     })
-    it('user should able to call /appointment-slots', () => {
+    it('user should able to call /v1/health', () => {
       cy.request({
-        url: `${WEB_COMPONENTS_API_URL}/appointment-slots`,
+        url: `${WEB_COMPONENTS_CONFIG_API_URL}/v1/health`,
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -46,6 +36,27 @@ if (Cypress.env('PACKAGE_NAME') === 'all' || Cypress.env('PACKAGE_NAME') === 'ap
         expect(response).to.have.property('status')
         expect(response.status).to.equal(200)
         expect(response.body).not.to.be.undefined
+        expect(response.body).to.deep.equal({
+          message: 'Service is running',
+        })
+      })
+    })
+    it('user should able to call /v1/web-components-config/<customer_id>', () => {
+      cy.request({
+        url: `${WEB_COMPONENTS_CONFIG_API_URL}/v1/web-components-config/ZZA`,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': Cypress.env('WEB_COMPONENTS_X_API_KEY'),
+        },
+      }).as('appointmentSlots')
+      cy.get('@appointmentSlots').should((response: any) => {
+        expect(response).to.have.property('headers')
+        expect(response).to.have.property('body')
+        expect(response).to.have.property('status')
+        expect(response.status).to.equal(200)
+        expect(response.body).not.to.be.undefined
+        expect(response.body).to.deep.equal(config)
       })
     })
   })
