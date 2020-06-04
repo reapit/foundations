afe790efe90f3e0bd1708f87e7a90dee8a6c3c93
Pham Hai Duong <duongph@reapit.com>
2020-06-04 12:40:32 +0700

    feat: #1287 automation ping for search-widget (#1549)

---
+++ packages/cloud-alert/cypress/integration/search-widget.test.ts
@@ -0,0 +1,92 @@
+Cypress.on('fail', (error, runnable) => {
+  fetch(Cypress.env('SLACK_WEB_HOOK_URL'), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      text: `Failed on ${runnable.title}`,
+    }),
+  })
+  throw error
+})
+
+const SEARCH_WIDGET_API_URL = Cypress.env(`SEARCH_WIDGET_API_URL_${Cypress.env('ENVIRONMENT')}`)
+const PACKAGE_NAME = Cypress.env('PACKAGE_NAME')
+
+if (PACKAGE_NAME === 'all' || PACKAGE_NAME === 'search-widget') {
+  describe('Search widget API', () => {
+    let property = {}
+    let properties = [] as any
+    let propertyImages = [] as any
+
+    beforeEach(function() {
+      cy.fixture('search-widget/property.json').then(data => {
+        property = data
+      })
+      cy.fixture('search-widget/properties.json').then(data => {
+        properties = data
+      })
+      cy.fixture('search-widget/property-images.json').then(data => {
+        propertyImages = data
+      })
+    })
+    it('user should able to call /properties/<id>', () => {
+      cy.request({
+        url: `${SEARCH_WIDGET_API_URL}/properties/BED150319`,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': Cypress.env('SEARCH_WIDGET_X_API_KEY'),
+        },
+      }).as('property')
+      cy.get('@property').should((response: any) => {
+        expect(response).to.have.property('headers')
+        expect(response).to.have.property('body')
+        expect(response).to.have.property('status')
+        expect(response.status).to.equal(200)
+        expect(response.body).not.to.be.undefined
+        expect(response.body).to.deep.equal(property)
+      })
+    })
+    it('user should able to call /properties?<query params>', () => {
+      cy.request({
+        // eslint-disable-next-line max-len
+        url: `${SEARCH_WIDGET_API_URL}/properties?keywords=london&isRental=true&apiKey&customerId=DXX&pageNumber=1&bedroomsFrom=1&bedroomsTo=2&priceFrom=300000&priceTo=500000&sortBy=price&propertyType=house&addedIn`,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': Cypress.env('SEARCH_WIDGET_X_API_KEY'),
+        },
+      }).as('properties')
+      cy.get('@properties').should((response: any) => {
+        expect(response).to.have.property('headers')
+        expect(response).to.have.property('body')
+        expect(response).to.have.property('status')
+        expect(response.status).to.equal(200)
+        expect(response.body._embedded).to.have.length(25)
+        expect(response.body._embedded).to.deep.equal(properties._embedded)
+      })
+    })
+
+    it('user should able to call /propertyImages', () => {
+      cy.request({
+        // eslint-disable-next-line max-len
+        url: `${SEARCH_WIDGET_API_URL}/propertyImages`,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': Cypress.env('SEARCH_WIDGET_X_API_KEY'),
+        },
+      }).as('propertyImages')
+      cy.get('@propertyImages').should((response: any) => {
+        expect(response).to.have.property('headers')
+        expect(response).to.have.property('body')
+        expect(response).to.have.property('status')
+        expect(response.status).to.equal(200)
+        expect(response.body._embedded).to.have.length(25)
+        expect(response.body._embedded).to.deep.equal(propertyImages._embedded)
+      })
+    })
+  })
+}
