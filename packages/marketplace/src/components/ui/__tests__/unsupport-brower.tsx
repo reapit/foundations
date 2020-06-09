022815fa521084da4ac5463e9c22a3590bcd20e3
Khac Vy <vytk@reapit.com>
2020-06-09 12:02:20 +0700

    fix: #1421 update unsupported brower popup

--- packages/marketplace/src/components/ui/__tests__/unsupport-brower.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { shallow } from 'enzyme'
-import UnsupportBrower from '../unsupport-brower'
-
-describe('UnsupportBrower', () => {
-  it('should match a snapshot', () => {
-    expect(shallow(<UnsupportBrower />)).toMatchSnapshot()
-  })
-})
