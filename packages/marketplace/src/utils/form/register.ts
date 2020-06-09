5f027163ade7f1cdabf424be7c93b4d5997108d7
undefined <nghiapn@reapit.com>
2020-06-09 11:56:43 +0700

    fix: #1627 - trim text before validate empty for fields company name and telephone

--- packages/marketplace/src/utils/form/register.ts
+++ packages/marketplace/src/utils/form/register.ts
@@ -3,26 +3,37 @@ import { CreateDeveloperModel } from '@reapit/foundations-ts-definitions'
 
 export type RegisterFormError = Partial<CreateDeveloperModel>
 
+export const trimValues: (values: CreateDeveloperModel) => CreateDeveloperModel = values => {
+  const trimmedValues = Object.keys(values).reduce((processTrimmedValues, key) => {
+    const trimmedValue = values[key].trim()
+    processTrimmedValues[key] = trimmedValue
+    return processTrimmedValues
+  }, {})
+
+  return trimmedValues
+}
+
 export function registerValidate(values: CreateDeveloperModel) {
   let errors = {} as RegisterFormError
+  const trimmedValues = trimValues(values)
 
-  if (!values.name || !values.name.trim()) {
+  if (!trimmedValues.name) {
     errors.name = 'Required'
-  } else if (!isValidPersonName(values.name)) {
+  } else if (!isValidPersonName(trimmedValues.name)) {
     errors.name = 'Invalid full name'
   }
 
-  if (!values.companyName) {
+  if (!trimmedValues.companyName) {
     errors.companyName = 'Required'
   }
 
-  if (!values.email) {
+  if (!trimmedValues.email) {
     errors.email = 'Required'
-  } else if (!isEmail(values.email)) {
+  } else if (!isEmail(trimmedValues.email)) {
     errors.email = 'Invalid email address'
   }
 
-  if (!values.telephone) {
+  if (!trimmedValues.telephone) {
     errors.telephone = 'Required'
   }
 
