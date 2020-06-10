ba417265381cca4e6ba926581cfe5ee144d84f04
undefined <nghiapn@reapit.com>
2020-06-10 13:09:43 +0700

    fix: #1420 - fix lint error

--- packages/utils/src/node/__tests__/sentry-logger.test.ts
+++ packages/utils/src/node/__tests__/sentry-logger.test.ts
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/node'
-import { createWistonLoggerErrorFn, stringifyError, cleanObject } from '../sentry-logger'
+import { createWistonLoggerErrorFn, stringifyError } from '../sentry-logger'
 
 jest.mock('serialize-error', () => ({
   serializeError: jest.fn(() => ({
@@ -11,15 +11,6 @@ describe('stringifyError', () => {
   it('should serialize correct error', () => {
     const err = new Error('err')
     expect(stringifyError(err)).toEqual(JSON.stringify({ stack: 'test' }))
-  })
-})
-
-describe('cleanObject', () => {
-  it('should run correctly', () => {
-    expect(cleanObject({})).toEqual({})
-    expect(cleanObject({ a: undefined })).toEqual({})
-    expect(cleanObject({ a: undefined, b: 'a' })).toEqual({ b: 'a' })
-    expect(cleanObject({ a: null, b: 'a' })).toEqual({ b: 'a' })
   })
 })
 
