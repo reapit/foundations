a377622887e319fb4fe732dfb1b14b43bc1aec07
undefined <nghiapn@reapit.com>
2020-06-11 16:59:05 +0700

    fix: #1340 - remove unused file

--- packages/marketplace/src/utils/action.ts
+++
@@ -1,6 +0,0 @@
-import { authChangeLoginType } from '@/actions/auth'
-import { Dispatch } from 'redux'
-
-export const changeLoginTypeToDeveloper = (displatch: Dispatch) => {
-  displatch(authChangeLoginType('DEVELOPER'))
-}
