baa62cf32c3847d24610f25c7dce9f8929208cfd
Vu Nguyen <vunp@reapit.com>
2020-06-12 13:37:35 +0700

    fix: #1624 Update validation

--- packages/aml-checklist/src/components/ui/modal/profile/form-schema/validation-schema.ts
+++ packages/aml-checklist/src/components/ui/modal/profile/form-schema/validation-schema.ts
@@ -16,23 +16,33 @@ const {
 } = formFields
 
 const profileValidationSchema = Yup.object().shape({
-  [titleField.name]: Yup.string().required(errorMessages.FIELD_REQUIRED),
-  [forenameField.name]: Yup.string().required(errorMessages.FIELD_REQUIRED),
-  [surnameField.name]: Yup.string().required(errorMessages.FIELD_REQUIRED),
+  [titleField.name]: Yup.string()
+    .trim()
+    .required(errorMessages.FIELD_REQUIRED),
+  [forenameField.name]: Yup.string()
+    .trim()
+    .required(errorMessages.FIELD_REQUIRED),
+  [surnameField.name]: Yup.string()
+    .trim()
+    .required(errorMessages.FIELD_REQUIRED),
   [dateOfBirthField.name]: Yup.string().required(errorMessages.FIELD_REQUIRED),
   [emailField.name]: Yup.string()
+    .trim()
     .required(errorMessages.FIELD_REQUIRED)
     .email(emailField.errorMessage),
   [homePhoneField.name]: Yup.string()
     .nullable()
+    .trim()
     .required(errorMessages.FIELD_REQUIRED)
     .matches(telephoneRegex, homePhoneField.errorMessage),
   [mobilePhoneField.name]: Yup.string()
     .nullable()
+    .trim()
     .required(errorMessages.FIELD_REQUIRED)
     .matches(telephoneRegex, mobilePhoneField.errorMessage),
   [workPhoneField.name]: Yup.string()
     .nullable()
+    .trim()
     .required(errorMessages.FIELD_REQUIRED)
     .matches(telephoneRegex, workPhoneField.errorMessage),
 })
