61fcafbb1d3edf84bdca8b5bf1a1805bc632b729
Vu Nguyen <vunp@reapit.com>
2020-06-12 14:08:55 +0700

    fix: #1624 Fix phone number field validation in AML profile modal (#1687)
    * fix: #1624 Fix phone number field validation in profile modal
    
    * fix: #1624 Migrate profile forms validation to yup

--- packages/elements/src/types/core.ts
+++ packages/elements/src/types/core.ts
@@ -9,3 +9,10 @@ export interface FetcherParams<T> {
   headers: StringMap
   body?: T
 }
+
+export type FormFieldInfo = {
+  name: string
+  label?: string
+  placceHolder?: string
+  errorMessage?: string
+}
