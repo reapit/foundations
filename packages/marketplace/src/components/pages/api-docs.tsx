755d9cc519839e7df8f604c09c3fce1d01ec46e0
willmcvay@pm.me <willmcvay@pm.me>
2020-06-12 10:50:37 +0100

    fix: display a message to user while we work on iframe fix

--- packages/marketplace/src/components/pages/api-docs.tsx
+++ packages/marketplace/src/components/pages/api-docs.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { useHistory } from 'react-router'
-import { FlexContainerBasic } from '@reapit/elements'
+import { FlexContainerBasic, Alert } from '@reapit/elements'
 import ErrorBoundary from '@/components/hocs/error-boundary'
 import { UnsupportBrowserPopUp } from '@/components/ui/unsupport-browser-pop-up'
 import { IFRAME_URLS } from '../../constants/iframe-urls'
@@ -12,22 +12,51 @@ export const parseIframeUrl = (pathname: string, hash: string): string => {
   return `${path}${hash}`
 }
 
+export const checkIframeLoad = (
+  event: React.SyntheticEvent<HTMLIFrameElement, Event>,
+  setIframeFailed: (iframeFailed: boolean) => void,
+) => {
+  if (window.reapit.config.appEnv === 'local') return
+
+  const iframe = event.target as HTMLIFrameElement
+
+  if (!iframe || !iframe.contentWindow) return
+
+  const gitbookContent = iframe.contentWindow.document.getElementById('__GITBOOK__ROOT__CLIENT__')
+
+  if (!gitbookContent) {
+    setIframeFailed(true)
+  }
+}
+
 const ApiDocsPage: React.FC = () => {
   const { location } = useHistory()
-  const isXIFrame = window.reapit.config.appEnv !== 'local' ? 'x-frame-bypass' : ''
+  const iframeUrl = `${IFRAME_URLS.documentation}${parseIframeUrl(location.pathname, location.hash)}`
+  const [iframeFailed, setIframeFailed] = React.useState(false)
+  const onIframeLoad = (event: React.SyntheticEvent<HTMLIFrameElement, Event>) =>
+    checkIframeLoad(event, setIframeFailed)
 
   return (
     <ErrorBoundary>
       <FlexContainerBasic flexColumn hasPadding className="is-full-height">
+        {iframeFailed && (
+          <Alert
+            type="info"
+            message={
+              <>
+                We are aware of an itermittent issue where our documentation fails to load on this page. Apologies if
+                you see this - we are actively working on a fix. You can still visit the{' '}
+                <a href={iframeUrl} target="_blank" rel="noreferrer">
+                  standalone documentation app here
+                </a>{' '}
+                while we address this issue
+              </>
+            }
+          />
+        )}
         <FlexContainerBasic className="container is-full-height">
           {!isIE() && (
-            <iframe
-              is={isXIFrame}
-              style={{ border: 'none' }}
-              src={`${IFRAME_URLS.documentation}${parseIframeUrl(location.pathname, location.hash)}`}
-              width="100%"
-              height="100%"
-            />
+            <iframe style={{ border: 'none' }} src={iframeUrl} width="100%" height="100%" onLoad={onIframeLoad} />
           )}
         </FlexContainerBasic>
       </FlexContainerBasic>
