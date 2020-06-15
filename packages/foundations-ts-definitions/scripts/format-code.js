50ee71ec7ed8f27ca7155ccc99bf07a917bc8efb
NghiaPham <nghiapn@reapit.com>
2020-06-15 21:52:35 +0700

    fix: #1704 update ts def to include traffic event (#1706)

--- packages/foundations-ts-definitions/scripts/format-code.js
+++ packages/foundations-ts-definitions/scripts/format-code.js
@@ -1,11 +1,7 @@
 const prettier = require('prettier')
-const path = require('path')
-const fs = require('fs')
-const prettierBaseConfigPath = path.resolve(__dirname, '../../../.prettierrc.js')
+const prettierBaseConfig = require('../../../.prettierrc')
 
 module.exports = content => {
-  const prettierBaseconfigBuffer = fs.readfilesync(prettierBaseConfigPath)
-  const prettierBaseConfig = JSON.parse(prettierBaseconfigBuffer.toString())
   const formatcontent = prettier.format(content, {
     parser: 'typescript',
     ...prettierBaseConfig,
