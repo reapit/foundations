6bb796756dd9fa3779b5e35f7ba8601cc0bf2042
willmcvay@pm.me <willmcvay@pm.me>
2020-06-15 15:42:30 +0100

    feat: adds lambda to proxy gitbook

---
+++ packages/api-docs-lamda/src/index.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+// Path to the hosted docs app
+export const API_DOCS_URL = 'https://foundations-documentation.reapit.cloud'
+// List of domains I allow to load in an iframe
+export const CONTENT_SECURITY_WHITELIST =
+  'http://localhost:8080 https://marketplace.reapit.cloud https://dev.marketplace.reapit.cloud'
+
+export const apiDocsHandler = async () => {
+  try {
+    const response = await axios.get(API_DOCS_URL)
+    return {
+      statusCode: 200,
+      headers: {
+        ...response.headers,
+        'Content-Security-Policy': `frame-ancestors 'self' ${CONTENT_SECURITY_WHITELIST}`,
+      },
+      body: response.data,
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
