70ae5b4b767308fad837634554cc0bb418994863
Trường An <andt@reapit.com>
2020-06-18 00:02:50 -0700

    feat: #1718 Add a ‘Subscriptions’ table on the ‘Billing’ tab in developers portal (#1739)

---
+++ packages/marketplace/src/components/ui/developer-settings/billing/delete-confirm.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from '@reapit/elements'
+
+export interface ConfirmModalProps {
+  visible: boolean
+  title?: string
+  subtitle?: string
+  onCancel?: () => void
+  onConfirm?: () => void
+}
+
+export const ConfirmModal: React.FC<ConfirmModalProps> = ({
+  visible = false,
+  title = 'Confirm',
+  subtitle = 'Are you sure?',
+  onCancel,
+  onConfirm,
+}) => {
+  if (!visible) return null
+  return (
+    <Modal visible renderChildren afterClose={onCancel}>
+      <>
+        <ModalHeader title={title}></ModalHeader>
+        <ModalBody body={<p>{subtitle}</p>} />
+        <ModalFooter
+          footerItems={
+            <>
+              <Button className="mr-2" type="button" onClick={onCancel} variant="danger" fullWidth={true}>
+                CANCEL
+              </Button>
+              <Button variant="primary" type="submit" fullWidth={true} onClick={onConfirm}>
+                OK
+              </Button>
+            </>
+          }
+        />
+      </>
+    </Modal>
+  )
+}
+
+export default ConfirmModal
--- packages/marketplace/src/sagas/__stubs__/developer.ts
+++ packages/marketplace/src/sagas/__stubs__/developer.ts
@@ -22,6 +22,10 @@ export const developerState: DeveloperState = {
     data: appDetailDataStub.data,
     isAppDetailLoading: false,
   },
+  subscriptions: {
+    data: null,
+    loading: false,
+  },
   developerData: {
     data: {
       data: [
