4abf08fa3203a43144de693ce03debc914277bfd
Trường An <andt@reapit.com>
2020-06-18 02:33:27 -0700

    chore: #1718 update Confirm Cancellation modal title, subtext (#1773)

--- packages/marketplace/src/components/ui/developer-settings/billing/subscriptions.tsx
+++ packages/marketplace/src/components/ui/developer-settings/billing/subscriptions.tsx
@@ -116,8 +116,8 @@ export const Subcriptions: React.FC = () => {
       {loading ? <Loader /> : <Table scrollable columns={columns} data={subscriptionsData} loading={false} bordered />}
       <ConfirmModal
         visible={isConfirmModalOpen}
-        title="Cancel Confirm"
-        subtitle="Are you sure to cancel this subscription?"
+        title="Confirm Cancellation"
+        subtitle="Are you sure you want to cancel this subscription?"
         onCancel={handleCloseModal}
         onConfirm={handleDeleteSubscription(dispatch, subscriptionIdToCancel, handleCloseModal)}
       />
