12df7ad4b97c00016c0a8f81386f94f4e37dccef
Duong Pham <tanphamhaiduong@gmail.com>
2020-06-19 15:58:17 +0700

    fix: #1743 responsive error on wizard

--- packages/elements/src/components/HelpGuide/context.tsx
+++ packages/elements/src/components/HelpGuide/context.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react'
-import { HelpGuideStepProps } from '.'
 
 function createContext<A>() {
   const ctx = React.createContext<A | undefined>(undefined)
-  function useCtx() {
+  const useCtx = () => {
     const c = React.useContext(ctx)
     if (!c) throw new Error('useContext must be inside a Provider with a value')
     return c
@@ -12,14 +11,11 @@ function createContext<A>() {
 }
 
 export type HelpGuideContextValues = {
-  current: string
+  current: number
   goNext: () => void
   goPrev: () => void
-  goTo: (stepIndex: number) => void
-  currentIndex: number
   isFirst: boolean
   isLast: boolean
-  steps: HelpGuideStepProps[]
   isLoading: boolean
 }
 
--- packages/elements/src/components/HelpGuide/index.tsx
+++ packages/elements/src/components/HelpGuide/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import { HelpGuideContextValues, HelpGuideContextProvider } from './context'
 import { FlexContainerBasic } from '../Layout'
 import { VerticalTimeline } from './vertical-timeline'
@@ -8,7 +8,7 @@ import { HorizontalTimeline } from './horizontal-timeline'
 
 export interface HelpGuideProps {
   children: React.ReactElement<HelpGuideStepProps> | React.ReactElement<HelpGuideStepProps>[]
-  current?: string
+  current?: number
   isLoading?: boolean
 }
 
@@ -22,110 +22,47 @@ export interface HelpGuideStepProps {
 }
 
 export interface NavigationProps {
-  steps: HelpGuideStepProps[]
-  currentIndex: number
   isFirst?: boolean
   isLast?: boolean
-  setInternalCurrent: (stepId: string) => void
+  currentStep?: number
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>
 }
 
-export const caculateStepChange = ({
-  currentStepRef,
-  wrapperStepRef,
-  helpguideRef,
-  isMobileScreen,
-}: {
-  currentStepRef: React.RefObject<HTMLElement>
-  wrapperStepRef: React.RefObject<HTMLElement>
-  helpguideRef: React.RefObject<HTMLElement>
-  isMobileScreen: boolean
-}) => () => {
-  if (currentStepRef.current && wrapperStepRef.current && helpguideRef.current && isMobileScreen) {
-    helpguideRef.current.style.height = '100%'
-    wrapperStepRef.current.style.transform = `translate3d(-${currentStepRef.current.offsetLeft}px, 0, 0)`
-  }
-
-  if (currentStepRef.current && wrapperStepRef.current && helpguideRef.current && !isMobileScreen) {
-    helpguideRef.current.style.height = `${currentStepRef.current.offsetHeight}px`
-    wrapperStepRef.current.style.transform = `translate3d(0, -${currentStepRef.current.offsetTop}px, 0)`
-  }
-}
-
-export const caculateWrapperWith = ({
-  isMobileScreen,
-  total,
-  helpguideRef,
-  wrapperStepRef,
-}: {
-  wrapperStepRef: React.RefObject<HTMLElement>
-  helpguideRef: React.RefObject<HTMLElement>
-  total: number
-  isMobileScreen: boolean
-}) => () => {
-  if (helpguideRef.current && wrapperStepRef.current && isMobileScreen) {
-    const wrapperWidth = helpguideRef.current.clientWidth * total
-    wrapperStepRef.current.style.width = `${wrapperWidth}px`
-  }
-  if (helpguideRef.current && wrapperStepRef.current && !isMobileScreen) {
-    wrapperStepRef.current.style.width = '100%'
-  }
-}
-
-export const handleGoNext = ({ steps, currentIndex, isLast, setInternalCurrent }: NavigationProps) => () => {
+export const handleGoNext = ({ isLast, setCurrentStep, currentStep = 0 }: NavigationProps) => () => {
   if (!isLast) {
-    setInternalCurrent(steps[currentIndex + 1].id)
+    setCurrentStep(currentStep + 1)
   }
 }
 
-export const handleGoPrev = ({ steps, currentIndex, isFirst, setInternalCurrent }: NavigationProps) => () => {
+export const handleGoPrev = ({ setCurrentStep, isFirst, currentStep = 0 }: NavigationProps) => () => {
   if (!isFirst) {
-    setInternalCurrent(steps[currentIndex - 1].id)
+    setCurrentStep(currentStep - 1)
   }
 }
 
-export const handleGoTo = ({ steps, setInternalCurrent }: NavigationProps) => (stepIndex: number) => {
-  setInternalCurrent(steps[stepIndex].id)
+export const handleGoTo = ({ setCurrentStep }: NavigationProps) => (stepIndex: number) => {
+  setCurrentStep(stepIndex)
 }
 
-export const renderTimeline = ({ total, currentIndex, isMobileScreen, goTo }) => {
+export const renderTimeline = ({ total, currentStep, isMobileScreen, goTo }) => {
   if (isMobileScreen) {
-    return <HorizontalTimeline total={total} currentIndex={currentIndex} onSelect={goTo} />
+    return <HorizontalTimeline total={total} currentIndex={currentStep} onSelect={goTo} />
   }
-  return <VerticalTimeline total={total} currentIndex={currentIndex} onSelect={goTo} />
+  return <VerticalTimeline total={total} currentIndex={currentStep} onSelect={goTo} />
 }
 
-export const HelpGuide = ({ children, current, isLoading = false }: HelpGuideProps) => {
-  const currentStepRef = useRef<HTMLDivElement>(null)
-  const wrapperStepRef = useRef<HTMLDivElement>(null)
-  const helpguideRef = useRef<HTMLDivElement>(null)
-
+export const HelpGuide = ({ children, current = 0, isLoading = false }: HelpGuideProps) => {
   const isMobileScreen = isMobile()
-
-  const steps = React.Children.toArray(children).map(({ props }: any) => ({ ...props }))
-
-  const [internalCurrent, setInternalCurrent] = useState(current || steps[0].id || '')
-
-  const total = steps.length
-  const isFirst = steps[0].id === internalCurrent
-  const isLast = steps[steps.length - 1].id === internalCurrent
-  const currentIndex = steps.findIndex(({ id }) => id === internalCurrent)
-
-  useEffect(caculateWrapperWith({ isMobileScreen, total, helpguideRef, wrapperStepRef }), [isMobileScreen])
-
-  useEffect(caculateStepChange({ currentStepRef, wrapperStepRef, helpguideRef, isMobileScreen }), [
-    internalCurrent,
-    isMobileScreen,
-  ])
-
-  const goTo = handleGoTo({ steps, currentIndex, setInternalCurrent })
+  const [currentStep, setCurrentStep] = React.useState<number>(current)
+  const total = (children as React.ReactNode[]).length
+  const isFirst = currentStep === 0
+  const isLast = currentStep === total - 1
+  const goTo = handleGoTo({ setCurrentStep })
 
   const value: HelpGuideContextValues = {
-    current: internalCurrent,
-    goNext: handleGoNext({ steps, currentIndex, isLast, setInternalCurrent }),
-    goPrev: handleGoPrev({ steps, currentIndex, isFirst, setInternalCurrent }),
-    goTo,
-    currentIndex,
-    steps,
+    current: currentStep,
+    goNext: handleGoNext({ currentStep, isLast, setCurrentStep }),
+    goPrev: handleGoPrev({ currentStep, isFirst, setCurrentStep }),
     isFirst,
     isLast,
     isLoading,
@@ -134,18 +71,10 @@ export const HelpGuide = ({ children, current, isLoading = false }: HelpGuidePro
   return (
     <HelpGuideContextProvider value={value}>
       <FlexContainerBasic hasPadding flexColumn={isMobileScreen}>
-        {renderTimeline({ total, currentIndex, isMobileScreen, goTo })}
-        <div ref={helpguideRef} className="helpguide">
-          <div ref={wrapperStepRef} className="helpguide-wrapper">
-            {React.Children.toArray(children).map((child, index) => (
-              <div
-                key={`helper-guide-child-${index}`}
-                ref={currentIndex === index ? currentStepRef : null}
-                className="helpguide-steps"
-              >
-                {child}
-              </div>
-            ))}
+        {renderTimeline({ total, currentStep, isMobileScreen, goTo })}
+        <div className="helpguide">
+          <div className="helpguide-wrapper">
+            <div className="helpguide-steps">{children[currentStep]}</div>
           </div>
         </div>
       </FlexContainerBasic>
