4291692453a337a9150421aa0f6d4c0223a0a59c
undefined <nghiapn@reapit.com>
2020-06-23 10:31:15 +0700

    feat: #1342 remove formik render props from company-information

--- packages/marketplace/src/components/pages/settings/developer-settings-organisation-tab/company-information.tsx
+++ packages/marketplace/src/components/pages/settings/developer-settings-organisation-tab/company-information.tsx
@@ -57,83 +57,81 @@ export const CompanyInformation = () => {
               console.log(values)
             }}
           >
-            {() => (
-              <Form>
-                <Grid>
-                  <GridItem>
-                    <Input type="text" labelText="Company Name" id="companyName" name="companyName" />
-                  </GridItem>
-                  <GridItem>
-                    <Input type="text" labelText="Telephone" id="tel" name="tel" />
-                  </GridItem>
-                </Grid>
-                <Grid>
-                  <GridItem>
-                    <Input type="text" labelText="Website" id="website" name="website" />
-                  </GridItem>
-                  <GridItem>
-                    <Input type="text" labelText="Office Email" id="officeEmail" name="officeEmail" />
-                  </GridItem>
-                </Grid>
-
-                <Grid>
-                  <GridItem>
-                    <Input type="text" labelText="VAT Number" id="name" name="name" />
-                  </GridItem>
-                  <GridItem>
-                    <Input type="text" labelText="Company registration number" id="reg" name="reg" />
-                  </GridItem>
-                </Grid>
-
-                <Grid>
-                  <GridItem>
-                    <TextArea className={styles.aboutField} id="about" labelText="About" name="about" />
-                  </GridItem>
-                  <GridItem>
-                    <div className="control">
-                      <label className="label">Logo</label>
-                      <ImageInput id="iconImage" labelText="Upload Logo" name="iconImageUrl" allowClear />
-                    </div>
-                  </GridItem>
-                </Grid>
-
+            <Form>
+              <Grid>
                 <GridItem>
-                  <H3>Company Address</H3>
+                  <Input type="text" labelText="Company Name" id="companyName" name="companyName" />
                 </GridItem>
-                <Grid>
-                  <GridItem>
-                    <Input type="text" labelText="Line 1*" id="line1" name="line1" />
-                  </GridItem>
-                  <GridItem>
-                    <Input type="text" labelText="Line 2" id="line2" name="line2" />
-                  </GridItem>
-                </Grid>
+                <GridItem>
+                  <Input type="text" labelText="Telephone" id="tel" name="tel" />
+                </GridItem>
+              </Grid>
+              <Grid>
+                <GridItem>
+                  <Input type="text" labelText="Website" id="website" name="website" />
+                </GridItem>
+                <GridItem>
+                  <Input type="text" labelText="Office Email" id="officeEmail" name="officeEmail" />
+                </GridItem>
+              </Grid>
 
-                <Grid>
-                  <GridItem>
-                    <Input type="text" labelText="Line 3" id="line3" name="line3" />
-                  </GridItem>
-                  <GridItem>
-                    <Input type="text" labelText="Line 4" id="line4" name="line4" />
-                  </GridItem>
-                </Grid>
+              <Grid>
+                <GridItem>
+                  <Input type="text" labelText="VAT Number" id="name" name="name" />
+                </GridItem>
+                <GridItem>
+                  <Input type="text" labelText="Company registration number" id="reg" name="reg" />
+                </GridItem>
+              </Grid>
 
-                <Grid>
-                  <GridItem>
-                    <SelectBox id="countryId" name="countryId" options={[]} labelText="Country" />
-                  </GridItem>
-                  <GridItem>
-                    <Input type="text" labelText="Postcode" id="postCode" name="postCode" />
-                  </GridItem>
-                </Grid>
+              <Grid>
+                <GridItem>
+                  <TextArea className={styles.aboutField} id="about" labelText="About" name="about" />
+                </GridItem>
+                <GridItem>
+                  <div className="control">
+                    <label className="label">Logo</label>
+                    <ImageInput id="iconImage" labelText="Upload Logo" name="iconImageUrl" allowClear />
+                  </div>
+                </GridItem>
+              </Grid>
 
-                <LevelRight>
-                  <Button className={styles.buttonSave} type="submit" variant="primary" onClick={() => {}}>
-                    Save
-                  </Button>
-                </LevelRight>
-              </Form>
-            )}
+              <GridItem>
+                <H3>Company Address</H3>
+              </GridItem>
+              <Grid>
+                <GridItem>
+                  <Input type="text" labelText="Line 1*" id="line1" name="line1" />
+                </GridItem>
+                <GridItem>
+                  <Input type="text" labelText="Line 2" id="line2" name="line2" />
+                </GridItem>
+              </Grid>
+
+              <Grid>
+                <GridItem>
+                  <Input type="text" labelText="Line 3" id="line3" name="line3" />
+                </GridItem>
+                <GridItem>
+                  <Input type="text" labelText="Line 4" id="line4" name="line4" />
+                </GridItem>
+              </Grid>
+
+              <Grid>
+                <GridItem>
+                  <SelectBox id="countryId" name="countryId" options={[]} labelText="Country" />
+                </GridItem>
+                <GridItem>
+                  <Input type="text" labelText="Postcode" id="postCode" name="postCode" />
+                </GridItem>
+              </Grid>
+
+              <LevelRight>
+                <Button className={styles.buttonSave} type="submit" variant="primary" onClick={() => {}}>
+                  Save
+                </Button>
+              </LevelRight>
+            </Form>
           </Formik>
         </FlexContainerResponsive>
       </Content>
