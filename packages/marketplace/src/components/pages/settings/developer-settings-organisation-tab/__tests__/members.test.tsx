cade9619c8c226d7ac13c95ef6bd745a2329df6b
Khac Vy <vytk@reapit.com>
2020-06-24 14:56:12 +0700

    feat: #1346 Create set member status modal

--- packages/marketplace/src/components/pages/settings/developer-settings-organisation-tab/__tests__/members.test.tsx
+++ packages/marketplace/src/components/pages/settings/developer-settings-organisation-tab/__tests__/members.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Members, CellName } from '../members'
+import { Members, CellName, closeSetMemberStatusModal, openSetMemberStatusModal } from '../members'
 import { shallow } from 'enzyme'
+import { developerStub } from '@/sagas/__stubs__/developer'
 
 describe('Members', () => {
   test('CellName should match snapshots', () => {
@@ -19,5 +20,27 @@ describe('Members', () => {
   })
   it('should match a snapshot when no error', () => {
     expect(shallow(<Members />)).toMatchSnapshot()
+  })
+
+  describe('openSetMemberStatusModal', () => {
+    it('should run correctly', () => {
+      const user = developerStub
+      const setSelectedUser = jest.fn()
+      const setEditStatusModalVisible = jest.fn()
+      openSetMemberStatusModal(setSelectedUser, setEditStatusModalVisible, user)()
+      expect(setEditStatusModalVisible).toBeCalled()
+      expect(setSelectedUser).toBeCalled()
+      expect(setEditStatusModalVisible).toBeCalledWith(true)
+      expect(setSelectedUser).toBeCalledWith(user)
+    })
+  })
+
+  describe('closeSetMemberStatusModal', () => {
+    it('should run correctly', () => {
+      const setEditStatusModalVisible = jest.fn()
+      closeSetMemberStatusModal(setEditStatusModalVisible)()
+      expect(setEditStatusModalVisible).toBeCalled()
+      expect(setEditStatusModalVisible).toBeCalledWith(false)
+    })
   })
 })
