85e6d976361dfbe379f5f01a124c4b0635a4dd25
Cuong Vu <cuongvh@reapit.com>
2020-07-06 13:54:26 +0700

    fix: #1956 add onError

--- packages/marketplace/src/components/ui/installed-app-card.tsx
+++ packages/marketplace/src/components/ui/installed-app-card.tsx
@@ -17,7 +17,12 @@ const InstalledAppCard: React.FC<InstalledAppCardProps> = ({ app, onClick }: Ins
     return (
       <div data-test-app-id={app.id} data-test-app-name={app.name} className={installedAppCardStyles.container}>
         <div data-test={`app-installed_${app.id}`} onClick={onClick} className={installedAppCardStyles.wrapIcon}>
-          <img className={installedAppCardStyles.icon} src={app.iconUri || defaultAppIcon} alt="iconUri" />
+          <img
+            className={installedAppCardStyles.icon}
+            src={app.iconUri || defaultAppIcon}
+            alt="iconUri"
+            onError={onImageError}
+          />
         </div>
         <p title={app.name} className={installedAppCardStyles.appTitle}>
           {app.name}
