07de0ab182d640120b07891b0fb7e6e7caa4b1d5
Duong Pham <tanphamhaiduong@gmail.com>
2020-07-08 17:53:02 +0700

    feat: #1037 add mrelation resolver for query appointment

---
+++ packages/graphql-server/src/resolvers/negotiators/__tests__/dataloader.ts
@@ -0,0 +1,37 @@
+import { generateNegotiatorBatchLoaderFn, generateNegotiatorLoader } from '../dataloader'
+import { negotiatorMock } from '../__mocks__/negotiator'
+import { negotiatorsMock } from '../__mocks__/negotiators'
+import { mockContext } from '../../../__mocks__/context'
+
+jest.mock('../../../logger')
+jest.mock('../api', () => ({
+  callGetNegotiatorByIdAPI: jest.fn(() => Promise.resolve(negotiatorMock)),
+  callGetNegotiatorsAPI: jest.fn(() => Promise.resolve(negotiatorsMock)),
+  callCreateNegotiatorAPI: jest.fn(() => Promise.resolve(true)),
+  callUpdateNegotiatorAPI: jest.fn(() => Promise.resolve(true)),
+}))
+
+describe('property-dataloader', () => {
+  describe('generateNegotiatorBatchLoaderFn', () => {
+    it('should run correctly', () => {
+      const fn = generateNegotiatorBatchLoaderFn(mockContext)
+      const mockKeys = ['AP']
+      const result = fn(mockKeys)
+      expect(result).toEqual(expect.any(Object))
+    })
+
+    it('should run correctly without keys', () => {
+      const fn = generateNegotiatorBatchLoaderFn(mockContext)
+      const mockKeys = []
+      const result = fn(mockKeys)
+      expect(result).toEqual(expect.any(Object))
+    })
+  })
+
+  describe('generateNegotiatorLoader', () => {
+    it('should return correctly', () => {
+      const result = generateNegotiatorLoader(mockContext)
+      expect(result).toEqual(expect.any(Object))
+    })
+  })
+})
