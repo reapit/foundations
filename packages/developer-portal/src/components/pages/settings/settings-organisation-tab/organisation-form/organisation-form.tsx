765fecc9fe15262c4efdd934d709eae02cc7cd38
Cuong Vu <cuongvh@reapit.com>
2020-07-13 11:46:18 +0700

    feat: #1986 routedisaptcher update

--- packages/developer-portal/src/components/pages/settings/settings-organisation-tab/organisation-form/organisation-form.tsx
+++ packages/developer-portal/src/components/pages/settings/settings-organisation-tab/organisation-form/organisation-form.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Button, Formik, Form, LevelRight, H3, FormSection } from '@reapit/elements'
+import { useSelector } from 'react-redux'
+import { Button, Formik, Form, LevelRight, H3, FormSection, Loader } from '@reapit/elements'
 import { companyInformationFormSchema } from './form-schema/validation-schema'
 import { OrganisationFormValues } from './form-schema/form-fields'
 import CompanyInformationSection from './company-information-section'
 import CompanyAddressSection from './company-address-section'
+import { DeveloperModel } from '@reapit/foundations-ts-definitions'
+import { selectSettingsPageDeveloperInformation, selectSettingsPageIsLoading } from '@/selector/settings'
 
-export const initialValues: OrganisationFormValues = {
+export const defaultInitialValues: OrganisationFormValues = {
   about: '',
   countryId: '',
   companyName: '',
@@ -27,12 +30,76 @@ export const initialValues: OrganisationFormValues = {
   nationalInsurance: '',
 }
 
+export const generateInitialValues = ({
+  developerInfo,
+  defaultInitialValues,
+}: {
+  developerInfo: DeveloperModel | null
+  defaultInitialValues: OrganisationFormValues
+}): OrganisationFormValues => {
+  if (!developerInfo) {
+    return defaultInitialValues
+  }
+  const { companyAddress = {}, ...otherData } = developerInfo
+  const {
+    line1 = '',
+    line2 = '',
+    line3 = '',
+    line4 = '',
+    buildingName = '',
+    buildingNumber = '',
+    postcode = '',
+    countryId = '',
+  } = companyAddress
+  const {
+    about = '',
+    companyName = '',
+    iconImageUrl = '',
+    taxNumber = '',
+    noTaxRegistration = false,
+    email = '',
+    registrationNumber = '',
+    noRegistrationNumber = false,
+    telephone = '',
+    website = '',
+    nationalInsurance = '',
+  } = otherData
+  return {
+    about,
+    countryId,
+    companyName,
+    iconImageUrl,
+    buildingName,
+    buildingNumber,
+    line1,
+    line2,
+    line3,
+    line4,
+    taxNumber,
+    noTaxRegistration,
+    email,
+    postcode,
+    registrationNumber,
+    noRegistrationNumber,
+    telephone,
+    website,
+    nationalInsurance,
+  }
+}
+
 export type OrganisationFormProps = {}
 
 const OrganisationForm: React.FC<OrganisationFormProps> = () => {
+  const isLoading: boolean = useSelector(selectSettingsPageIsLoading)
+  const developerInfo: DeveloperModel | null = useSelector(selectSettingsPageDeveloperInformation)
+
+  if (isLoading) {
+    return <Loader />
+  }
+
   return (
     <Formik
-      initialValues={initialValues}
+      initialValues={generateInitialValues({ developerInfo, defaultInitialValues })}
       validationSchema={companyInformationFormSchema}
       onSubmit={values => {
         // TBC
