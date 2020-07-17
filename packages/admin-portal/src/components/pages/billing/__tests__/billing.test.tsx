e63766ac243f7cc866731e2847225153589ee011
Truong An <andt@reapit.com>
2020-07-17 10:48:44 +0700

    feat: update billing page admin portal

--- packages/admin-portal/src/components/pages/billing/__tests__/billing.test.tsx
+++ packages/admin-portal/src/components/pages/billing/__tests__/billing.test.tsx
@@ -1,9 +1,99 @@
 import * as React from 'react'
-import { shallow } from 'enzyme'
-import AdminBilling from '../billing'
+import { mount, shallow } from 'enzyme'
+import AdminBilling, {
+  handleDownloadBillingPeriod,
+  renderDownloadCell,
+  genarateYearsListOptions,
+  handleChangePeriod,
+} from '../admin-billing'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import appState from '@/reducers/__stubs__/app-state'
+// import * as ReapitElements from '../admin-billing'
+import * as developerServices from '@/services/developers'
+
+const spyFetcher = jest.spyOn(developerServices, 'fetchDeveloperBillingPeriod').mockImplementation(
+  () =>
+    new Promise(resolve => {
+      resolve()
+    }),
+)
 
 describe('AdminBilling', () => {
-  it('should match a snapshot when no error', () => {
-    expect(shallow(<AdminBilling />)).toMatchSnapshot()
+  it('should match a snapshot', () => {
+    const mockStore = configureStore()
+    const store = mockStore(appState)
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <AdminBilling />
+      </Provider>,
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('genarateYearsListOptions', () => {
+  it('should run correctly', () => {
+    const yearFrom = 2019
+    const result = genarateYearsListOptions(yearFrom)
+    expect(result).toEqual([{ value: '2019', label: '2019' }])
+  })
+})
+
+describe('renderDownloadCell', () => {
+  it('should run correctly', () => {
+    window.URL.createObjectURL = jest.fn(() => 'test')
+    const input = {
+      row: {
+        original: {
+          proiod: '2020-02',
+        },
+      },
+    }
+    expect(shallow(<div>{renderDownloadCell(input)}</div>)).toMatchSnapshot()
+  })
+})
+
+describe('handleDownloadBillingPeriod', () => {
+  it('should run correctly', () => {
+    const period = '2020-02'
+    const setFileBlob = jest.fn()
+
+    const fn = handleDownloadBillingPeriod(period, setFileBlob)
+    fn()
+    expect(spyFetcher).toBeCalledWith({ period })
+  })
+})
+
+describe('handleChangePeriod', () => {
+  const setMonth = jest.fn()
+  const setYear = jest.fn()
+
+  it('should run correctly', () => {
+    const event = {
+      nativeEvent: {
+        target: {
+          name: 'month',
+          value: '09',
+        },
+      },
+    }
+    const fn = handleChangePeriod(setMonth, setYear)
+    fn(event)
+    expect(setMonth).toBeCalledWith(event.nativeEvent.target.value)
+  })
+  it('should run correctly', () => {
+    const event = {
+      nativeEvent: {
+        target: {
+          name: 'year',
+          value: '2020',
+        },
+      },
+    }
+    const fn = handleChangePeriod(setMonth, setYear)
+    fn(event)
+    expect(setYear).toBeCalledWith(event.nativeEvent.target.value)
   })
 })
--- packages/admin-portal/src/components/pages/billing/billing.tsx
+++ packages/admin-portal/src/components/pages/billing/billing.tsx
@@ -1,23 +1,165 @@
-import React from 'react'
-import { H3, Table, Section } from '@reapit/elements'
+import React, { useEffect, useState, Dispatch } from 'react'
+import { H3, Table, Section, FetchError, SelectBox, Formik, SelectOption, Form, Grid, GridItem } from '@reapit/elements'
+import { fetchDeveloperBillingPeriod } from '@/services/developers'
+import dayjs from 'dayjs'
+import { notification } from '@reapit/elements'
+import errorMessages from '../../../../../elements/src/utils/validators/error-messages'
+
+export const renderDownloadCell = ({ row: { original } }) => {
+  const { proiod, fileBlob } = original
+  const url = window.URL.createObjectURL(fileBlob)
+  const filename = `Billing_Period_${proiod}.csv`
+
+  return (
+    <a href={url} download={filename}>
+      Download
+    </a>
+  )
+}
 
 export const columns = [
   {
     Header: 'Billing Period',
-    accessor: 'billingPeriod',
+    accessor: 'period',
   },
   {
     Header: 'File',
-    accessor: 'file',
+    Cell: renderDownloadCell,
   },
 ]
 
+export const handleDownloadBillingPeriod = (period: string, setFileBlob: Dispatch<Blob | undefined>) => () => {
+  if (!period) return
+  fetchDeveloperBillingPeriod({ period })
+    .then(blob => {
+      if (blob instanceof FetchError) throw blob
+      setFileBlob(blob)
+    })
+    .catch(error => {
+      notification.error({
+        message: error.message || errorMessages.DEFAULT_SERVER_ERROR,
+        placement: 'bottomRight',
+        duration: 5,
+      })
+    })
+}
+
+type Month = {
+  proiod: string
+}
+
+export const genarateYearsListOptions = (yearFrom: number) => {
+  const years: SelectOption[] = []
+  const currentYear = dayjs().year()
+  while (yearFrom <= currentYear) {
+    const option = {
+      value: `${yearFrom}`,
+      label: `${yearFrom++}`,
+    }
+    years.push(option)
+  }
+  return years
+}
+
+const monthOptions = [
+  {
+    label: 'January',
+    value: '01',
+  },
+  {
+    label: 'February',
+    value: '02',
+  },
+  {
+    label: 'March',
+    value: '03',
+  },
+  {
+    label: 'April',
+    value: '04',
+  },
+  {
+    label: 'May',
+    value: '05',
+  },
+  {
+    label: 'June',
+    value: '06',
+  },
+  {
+    label: 'July',
+    value: '07',
+  },
+  {
+    label: 'August',
+    value: '08',
+  },
+  {
+    label: 'September',
+    value: '09',
+  },
+  {
+    label: 'October',
+    value: '10',
+  },
+  {
+    label: 'November',
+    value: '11',
+  },
+  {
+    label: 'December',
+    value: '12',
+  },
+]
+const yearOptions = genarateYearsListOptions(2010)
+
+export const handleChangePeriod = (setMonth, setYear) => event => {
+  const { value, name } = event.nativeEvent.target
+  if (name === 'month') setMonth(value)
+  if (name === 'year') setYear(value)
+}
+
 export const AdminBilling: React.FC = () => {
+  const [fileBlob, setFileBlob] = useState<Blob | undefined>()
+  const [month, setMonth] = useState<string>(dayjs().format('MM'))
+  const [year, setYear] = useState<string>(dayjs().format('YYYY'))
+
+  const period = `${year}-${month}`
+
+  useEffect(handleDownloadBillingPeriod(period, setFileBlob), [period])
+
+  const onChangePeriod = handleChangePeriod(setMonth, setYear)
+  const tableData = [
+    {
+      period,
+      fileBlob,
+    },
+  ]
+
   return (
     <>
       <H3 isHeadingSection>Billing</H3>
       <Section>
-        <Table scrollable={true} loading={false} data={[]} columns={columns} />
+        <Formik
+          initialValues={{
+            year,
+            month,
+          }}
+          onSubmit={onChangePeriod}
+        >
+          <Form onChange={onChangePeriod}>
+            <Grid>
+              <GridItem>
+                <SelectBox id="month" name="month" labelText="Month" options={monthOptions} />
+              </GridItem>
+              <GridItem>
+                <SelectBox id="year" name="year" labelText="Year" options={yearOptions} />
+              </GridItem>
+            </Grid>
+          </Form>
+        </Formik>
+
+        <Table scrollable={true} loading={!fileBlob} data={tableData} columns={columns} />
       </Section>
     </>
   )
