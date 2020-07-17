5e4ce587eb7f914798e3efe2f18c19eba359ea01
undefined <nghiapn@reapit.com>
2020-07-17 10:29:18 +0700

    feat: #2064 remove toJson in test files

--- packages/cra-external-template/template/src/components/hocs/__tests__/error-boundary.tsx
+++ packages/cra-external-template/template/src/components/hocs/__tests__/error-boundary.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
 import { ErrorBoundary } from '../error-boundary'
 
 const Component: React.FC = () => <div>I am a component!</div>
@@ -12,7 +11,7 @@ const props = {
 
 describe('ErrorBoundary', () => {
   it('should match a snapshot when no error', () => {
-    expect(toJson(shallow(<ErrorBoundary {...props} />))).toMatchSnapshot()
+    expect(shallow(<ErrorBoundary {...props} />)).toMatchSnapshot()
   })
 
   it('should match a snapshot when has an error', () => {
@@ -20,7 +19,7 @@ describe('ErrorBoundary', () => {
     component.setState({
       hasFailed: true,
     })
-    expect(toJson(component)).toMatchSnapshot()
+    expect(component).toMatchSnapshot()
   })
 
   afterEach(() => {
