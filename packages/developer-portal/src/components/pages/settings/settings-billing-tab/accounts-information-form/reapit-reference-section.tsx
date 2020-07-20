71c29a7230caca6e2e74fcf7dee7601f91c9538b
NghiaPham <nghiapn@reapit.com>
2020-07-20 15:06:10 +0700

    fix: #1987 update billing page (#2082)

--- packages/developer-portal/src/components/pages/settings/settings-billing-tab/accounts-information-form/reapit-reference-section.tsx
+++ packages/developer-portal/src/components/pages/settings/settings-billing-tab/accounts-information-form/reapit-reference-section.tsx
@@ -17,7 +17,10 @@ const hasReapitAccountsRefRadioOptions = [
 
 const ReapitReferenceSection: React.FC<ReapitReferenceSectionProps> = ({ setFieldValue, values }) => {
   const { hasReapitAccountsRef } = values
-  const hasReapitAccountsRefFieldDisabled = hasReapitAccountsRef === 'no'
+
+  const pending = values?.status === 'pending'
+  const hasReapitAccountsRefFieldDisabled = hasReapitAccountsRef === 'no' || pending
+
   return (
     <>
       <FormHeading>{hasReapitAccountsRefField.heading}</FormHeading>
@@ -30,6 +33,7 @@ const ReapitReferenceSection: React.FC<ReapitReferenceSectionProps> = ({ setFiel
         options={hasReapitAccountsRefRadioOptions}
         name={hasReapitAccountsRefField.name}
         id={hasReapitAccountsRefField.name}
+        disabled={pending}
       />
       <Input
         dataTest="reapitAccountsRef"
