f6fdd96c44b3563516479c9aa982f9a98b8bd356
Khac Vy <vytk@reapit.com>
2020-07-28 16:00:37 +0700

    fix: #2120 fix apps are duplicated after go back from app detail

--- packages/marketplace/src/utils/browse-app.ts
+++ packages/marketplace/src/utils/browse-app.ts
@@ -1,3 +1,5 @@
+import { AppSummaryModel } from '@reapit/foundations-ts-definitions'
+
 export const HEADER_HEIGHT = 68
 export const FEATURED_APP_HEIGHT = 200
 export const NORMAL_APP_MIN_HEIGHT = 180
@@ -7,4 +9,14 @@ export const getNumberOfItems = () => {
   const normalAppContainerHeight = window.innerHeight - HEADER_HEIGHT - FEATURED_APP_HEIGHT
   const numOfRow = Math.ceil(normalAppContainerHeight / NORMAL_APP_MIN_HEIGHT)
   return numOfRow * MAXIMUM_ITEMS_PER_ROW
+}
+
+export const mergeAppsWithoutDuplicateId = (
+  oldApps: AppSummaryModel[] | undefined = [],
+  newApps: AppSummaryModel[] | undefined = [],
+): AppSummaryModel[] => {
+  const newAppIds = newApps.map(app => app.id)
+  const filteredOldApps = oldApps.filter(app => !newAppIds.includes(app.id))
+
+  return [...filteredOldApps, ...newApps]
 }
