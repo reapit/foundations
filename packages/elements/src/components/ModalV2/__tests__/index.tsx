8580b5c9a155c5cae36a44526e32479269b8ab17
Trường An <andt@reapit.com>
2020-07-29 10:59:02 +0700

    chore: #2096 update modal v2 header style (#2203)

--- packages/elements/src/components/ModalV2/__tests__/index.tsx
+++ packages/elements/src/components/ModalV2/__tests__/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { ModalV2 } from '../index'
+import { ModalV2, ModalHeaderV2 } from '../index'
 
 describe('modal', () => {
   describe('Modal', () => {
@@ -10,6 +10,12 @@ describe('modal', () => {
           <div>children</div>
         </ModalV2>,
       )
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+  describe('ModalHeaderV2', () => {
+    it('should match snapshot', () => {
+      const wrapper = shallow(<ModalHeaderV2>ModalHeaderV2</ModalHeaderV2>)
       expect(wrapper).toMatchSnapshot()
     })
   })
