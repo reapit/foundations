47858ea82ae36229ad2e12f14132efbb771fcb94
Trường An <andt@reapit.com>
2020-07-30 15:33:12 +0700

    feat: add a ‘login’ button the Invite ‘success’ modal (#2223)

--- packages/developer-portal/src/components/pages/invite/__tests__/accepted.test.tsx
+++ packages/developer-portal/src/components/pages/invite/__tests__/accepted.test.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import AcceptedModal from '../accepted'
+import AcceptedModal, { handleLogin } from '../accepted'
 
 describe('AcceptedModal', () => {
   it('should match snapshot', () => {
     expect(shallow(<AcceptedModal visible />)).toMatchSnapshot()
+  })
+})
+
+describe('handleLogin', () => {
+  it('should run correctly', () => {
+    const reapitConnectBrowserSession = {
+      connectHasSession: false,
+      connectLogoutRedirect: jest.fn(),
+    }
+    jest.mock('@/core/connect-session', () => ({
+      reapitConnectBrowserSession,
+    }))
+    const history = {
+      replace: jest.fn(),
+    }
+    const fn = handleLogin(history)
+    fn()
+    expect(history.replace).toBeCalled()
   })
 })
