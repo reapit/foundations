02c318d95079ed8c7cc3e22dee0bdd17ad8c766a
undefined <nghiapn@reapit.com>
2020-08-04 17:16:39 +0700

    chore: #2255 fix test

--- packages/smb-onboarder/src/core/__tests__/private-route-wrapper.tsx
+++ packages/smb-onboarder/src/core/__tests__/private-route-wrapper.tsx
@@ -5,34 +5,6 @@ import { render } from '@testing-library/react'
 import { PrivateRouteWrapper, PrivateRouteWrapperProps } from '../private-route-wrapper'
 import { getMockRouterProps } from '../__mocks__/mock-router'
 
-const session = {
-  accessToken: '123',
-  accessTokenExpiry: 1583492838,
-  idToken: '123',
-  idTokenExpiry: 1583492838,
-  refreshToken: '123',
-  cognitoClientId: '123',
-  loginType: 'CLIENT',
-  userName: 'cbryan@reapit.com',
-  mode: 'WEB',
-  loginIdentity: {
-    name: 'Craig Bryan',
-    email: 'cbryan@reapit.com',
-    developerId: '909dcdc1-6657-4a37-a5cc-05acd79d6a47',
-    clientId: 'DXX',
-    adminId: '1',
-    userCode: 'LJW',
-  },
-}
-
-jest.mock('@reapit/cognito-auth', () => ({
-  redirectToLogin: jest.fn(),
-  getSessionCookie: jest.fn(),
-  getTokenFromQueryString: jest.fn(),
-  redirectToOAuth: jest.fn(),
-  getSession: jest.fn(() => session),
-}))
-
 describe('PrivateRouter', () => {
   it('should match a snapshot', () => {
     const props: PrivateRouteWrapperProps = {
