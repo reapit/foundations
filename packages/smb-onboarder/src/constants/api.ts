af094ac20a11e6342c3b6805a4610ce4a19175c2
undefined <nghiapn@reapit.com>
2020-08-04 13:57:18 +0700

    chore: #2255 remove @reapit/cognito-auth

--- packages/smb-onboarder/src/constants/api.ts
+++ packages/smb-onboarder/src/constants/api.ts
@@ -1,17 +1,9 @@
 import { StringMap } from '@/types/core'
-/*
- * TODOME(cognito)
- * remove
- */
-
-import { COOKIE_SESSION_KEY as COGNITIO_COOKIE_SESSION_KEY } from '@reapit/cognito-auth'
 
 export const CONTACTS_HEADERS = {
   'Content-Type': 'application/json',
 } as StringMap
 
 export const API_VERSION = '2020-02-13'
-
-export const COOKIE_SESSION_KEY = `${COGNITIO_COOKIE_SESSION_KEY}-smb`
 
 export const URLS = {}
--- packages/smb-onboarder/src/graphql/__mocks__/token.ts
+++ packages/smb-onboarder/src/graphql/__mocks__/token.ts
@@ -1,19 +1,5 @@
-import { LoginParams } from '@reapit/cognito-auth'
-
 export const token = {
   __typename: 'Token',
   accessToken: 'mockAccessToken',
   refreshToken: 'mockRefreshToken',
 }
-
-/*
- * TODOME(cognito)
- * remove
- */
-
-export const loginParams = {
-  userName: 'mockUsername',
-  password: 'mockPassword',
-  loginType: 'CLIENT',
-  mode: 'WEB',
-} as LoginParams
