ee2c4b7dccf2eeaff789d873332aa975833ce240
Cuong Vu <vuhuucuong1310@gmail.com>
2020-08-04 18:01:38 +0700

    feat: #2215 download CSV for cost explorer

--- packages/developer-portal/src/components/pages/analytics/cost-explorer/cost-explorer-component/__test__/cost-explorer.test.tsx
+++ packages/developer-portal/src/components/pages/analytics/cost-explorer/cost-explorer-component/__test__/cost-explorer.test.tsx
@@ -5,12 +5,16 @@ import CostExplorer, {
   CostFilterFormValues,
   handleOnSave,
   handleFetchMonthlyBilling,
+  prepareTableData,
+  convertTableDataToArray,
+  recursiveFlattenTableRows,
 } from '../cost-explorer'
 import { fetchMonthlyBilling } from '@/actions/developer'
 import * as ReactRedux from 'react-redux'
 import configureStore from 'redux-mock-store'
 import { developerState } from '@/sagas/__stubs__/developer'
 import { ReduxState } from '@/types/core'
+import { monthlyBillingData, tableData } from '@/sagas/__stubs__/monthly-billing'
 
 const mockState = {
   developer: developerState,
@@ -66,5 +70,185 @@ describe('CostCalculator', () => {
       fn()
       expect(dispatch).toBeCalledWith(fetchMonthlyBilling({ month, developerId }))
     })
+  })
+})
+
+describe('prepareTableData', () => {
+  it('should run correctly', () => {
+    const { services = [] } = monthlyBillingData
+    expect(prepareTableData(services)).toEqual(tableData)
+  })
+
+  it('should return correct data', () => {
+    const { services = [] } = monthlyBillingData
+    const input = [
+      ...services,
+      {
+        cost: 7.975,
+        itemCount: 7,
+        amount: 638,
+        items: [{ name: 'contacts', amount: 157, cost: 1.9625, itemCount: 2 }],
+        name: 'TEST',
+      },
+    ]
+
+    const expected = [
+      {
+        amount: 638,
+        cost: 7.975,
+        itemCount: 7,
+        name: 'API Requests',
+        subRows: [{ name: 'contacts', amount: 157, cost: 1.9625, itemCount: 2, subRows: [] }],
+      },
+      {
+        cost: 7.975,
+        itemCount: null,
+        amount: 638,
+        subRows: [{ name: 'contacts', amount: 157, cost: 1.9625, itemCount: null, subRows: [] }],
+        name: 'TEST',
+      },
+    ]
+
+    expect(prepareTableData(input)).toEqual(expected)
+  })
+})
+
+describe('convertTableDataToArray', () => {
+  it('should return correctly', () => {
+    const tableData = [
+      {
+        name: 'API Requests',
+        amount: 0,
+        cost: 0,
+        itemCount: null,
+        subRows: [],
+      },
+      {
+        name: 'Reapit Connect',
+        amount: 0,
+        cost: 0,
+        itemCount: null,
+        subRows: [],
+      },
+      {
+        name: 'Application Listing',
+        amount: 1,
+        cost: 49.5,
+        itemCount: null,
+        subRows: [
+          {
+            name: 'Application listing for TestApp',
+            amount: 1,
+            cost: 49.5,
+            itemCount: null,
+            subRows: [],
+          },
+        ],
+      },
+      {
+        name: 'Developer Edition',
+        amount: 22,
+        cost: 3900,
+        itemCount: null,
+        subRows: [
+          {
+            name: 'Developer edition license for cbryan@reapit.com',
+            amount: 22,
+            cost: 3900,
+            itemCount: null,
+            subRows: [],
+          },
+        ],
+      },
+    ]
+    const columns = [
+      {
+        Header: 'Services',
+      },
+      {
+        Header: 'Endpoints',
+      },
+      {
+        Header: 'Amount',
+      },
+      {
+        Header: 'Cost',
+      },
+    ]
+
+    const result = convertTableDataToArray(tableData, columns, 3949)
+    const expected = [
+      ['Services', 'Endpoints', 'Amount', 'Cost'],
+      ['API Requests', null, 0, 0],
+      ['Reapit Connect', null, 0, 0],
+      ['Application Listing', null, 1, 49.5],
+      ['Application listing for TestApp', null, 1, 49.5],
+      ['Developer Edition', null, 22, 3900],
+      ['Developer edition license for cbryan@reapit.com', null, 22, 3900],
+      ['Total', null, null, 3949],
+    ]
+    expect(result).toEqual(expected)
+  })
+})
+
+describe('recursiveFlattenTableData', () => {
+  it('should return correctly', () => {
+    const tableData = [
+      {
+        name: 'API Requests',
+        amount: 0,
+        cost: 0,
+        itemCount: null,
+        subRows: [],
+      },
+      {
+        name: 'Reapit Connect',
+        amount: 0,
+        cost: 0,
+        itemCount: null,
+        subRows: [],
+      },
+      {
+        name: 'Application Listing',
+        amount: 1,
+        cost: 49.5,
+        itemCount: null,
+        subRows: [
+          {
+            name: 'Application listing for TestApp',
+            amount: 1,
+            cost: 49.5,
+            itemCount: null,
+            subRows: [],
+          },
+        ],
+      },
+      {
+        name: 'Developer Edition',
+        amount: 22,
+        cost: 3900,
+        itemCount: null,
+        subRows: [
+          {
+            name: 'Developer edition license for cbryan@reapit.com',
+            amount: 22,
+            cost: 3900,
+            itemCount: null,
+            subRows: [],
+          },
+        ],
+      },
+    ]
+
+    const result = recursiveFlattenTableRows(tableData)
+    const expected = [
+      ['API Requests', null, 0, 0],
+      ['Reapit Connect', null, 0, 0],
+      ['Application Listing', null, 1, 49.5],
+      ['Application listing for TestApp', null, 1, 49.5],
+      ['Developer Edition', null, 22, 3900],
+      ['Developer edition license for cbryan@reapit.com', null, 22, 3900],
+    ]
+    expect(result).toEqual(expected)
   })
 })
--- packages/developer-portal/src/components/pages/analytics/cost-explorer/cost-explorer-component/cost-explorer-table.tsx
+++ packages/developer-portal/src/components/pages/analytics/cost-explorer/cost-explorer-component/cost-explorer-table.tsx
@@ -1,88 +1,17 @@
 import * as React from 'react'
-import { formatNumber, formatCurrency } from '@/utils/number-formatter'
 import { Table, Loader } from '@reapit/elements'
 import { useSelector } from 'react-redux'
-import { selectMonthlyBilling, selectMonthlyBillingLoading } from '@/selector/developer'
-import { BillingBreakdownForMonthV2Model, ServiceItemBillingV2Model } from '@reapit/foundations-ts-definitions'
+import { selectMonthlyBillingLoading } from '@/selector/developer'
+import { TableData } from './cost-explorer'
 
-export const prepareTableData = (data: ServiceItemBillingV2Model[], serviceName?: string) => {
-  if (!data || !data.length) return []
-
-  return data.map(({ items = [], itemCount, ...row }) => {
-    const service = serviceName || row.name
-    const isApiRequests = service === 'API Requests'
-
-    const rowData = {
-      ...row,
-      itemCount: isApiRequests && itemCount ? itemCount : null,
-      subRows: prepareTableData(items, service),
-    }
-    return rowData
-  })
+type CostExplorerTableProps = {
+  tableData: TableData
+  columns: any[]
 }
-
-export const prepareTableColumns = (monthlyBilling?: BillingBreakdownForMonthV2Model | null) => {
-  const totalCost = monthlyBilling?.totalCost || 0
-
-  return [
-    {
-      Header: 'Services',
-      accessor: 'name',
-      columnProps: {
-        width: 200,
-      },
-      Footer: 'Total',
-    },
-    {
-      Header: 'Endpoints',
-      accessor: row => {
-        return row.itemCount && formatNumber(row.itemCount)
-      },
-    },
-    {
-      Header: 'Amount',
-      accessor: row => {
-        return row.amount && formatNumber(row.amount)
-      },
-    },
-    {
-      Header: 'Cost',
-      accessor: row => {
-        return row.cost && formatCurrency(row.cost)
-      },
-      Footer: formatCurrency(totalCost),
-    },
-  ]
-}
-
-export const createCSVRow = ({ name: Services, amount: Amount, itemCount: Endpoints, cost: Cost, subRows }) => {
-  if (Array.isArray(subRows) && subRows.length < 0) {
-    return [Services, Endpoints, Amount, Cost]
-  }
-  if (Array.isArray(subRows) && subRows.length > 0) {
-    const result = subRows.map(subRow => createCSVRow(subRow))
-    return result
-  }
-  return []
-}
-
-export const convertTableDataToCSV = (tableData, columns) => {
-  const titleRows = columns.map(({ Header }) => Header)
-  console.log(titleRows)
-  console.log(tableData)
-}
-
-const CostExplorerTable: React.FC = () => {
-  const monthlyBilling = useSelector(selectMonthlyBilling)
+const CostExplorerTable: React.FC<CostExplorerTableProps> = ({ columns, tableData }) => {
   const isLoading = useSelector(selectMonthlyBillingLoading)
 
   if (isLoading) return <Loader />
-
-  const { services = [] } = monthlyBilling
-
-  const columns = prepareTableColumns(monthlyBilling)
-  const tableData = prepareTableData(services)
-  const CSVData = convertTableDataToCSV(tableData, columns)
 
   return (
     <>
