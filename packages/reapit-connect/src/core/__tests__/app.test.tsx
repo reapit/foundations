0cf979c3c6fa6a41079673502f9d331bdb217bc8
NghiaPham <nghiapn@reapit.com>
2020-08-05 17:28:28 +0700

    chore: #2252 developers should redirect to developer portal when landing on the reapit connect splash page (#2273)

--- packages/reapit-connect/src/core/__tests__/app.test.tsx
+++ packages/reapit-connect/src/core/__tests__/app.test.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import { App } from '../app'
+import { App, isUserWithDevIdOnly } from '../app'
+import { LoginIdentity, useReapitConnect } from '@reapit/connect-session'
+import { DEVELOPER_PORTAL_APPS } from '@/constants/routes'
+
+const mockedUseReapitConnect = (useReapitConnect as unknown) as jest.Mock
+
+jest.mock('@reapit/connect-session')
 
 const session = {
   accessToken: '123',
@@ -22,7 +28,36 @@ const session = {
   },
 }
 
+const loginIdentitiyWithDeveloperIdOnly = ({ developerId: '1' } as unknown) as LoginIdentity
+
+describe('isUserWithDevIdOnly', () => {
+  test('user with developer id only', () => {
+    expect(isUserWithDevIdOnly(loginIdentitiyWithDeveloperIdOnly)).toBeTruthy()
+  })
+  test('user with developer id, client id, admin id', () => {
+    expect(isUserWithDevIdOnly(session.loginIdentity as LoginIdentity)).toBeFalsy()
+  })
+})
+
 describe('App', () => {
+  test('redirect to https://dev.developers.reapit.cloud/apps when isUserWithDevIdOnly is true', () => {
+    window.location.href = ''
+    mockedUseReapitConnect.mockReturnValueOnce({
+      connectSession: { ...session, loginIdentity: loginIdentitiyWithDeveloperIdOnly },
+    })
+    shallow(<App />)
+    expect(location.href).toBe(DEVELOPER_PORTAL_APPS)
+  })
+
+  test('redirect to https://dev.developers.reapit.cloud/apps when isUserWithDevIdOnly is false', () => {
+    window.location.href = ''
+    mockedUseReapitConnect.mockReturnValue({
+      connectSession: session,
+    })
+    shallow(<App />)
+    expect(location.href).toBe('')
+  })
+
   it('should match snapshot', () => {
     jest.mock('@reapit/connect-session', () => ({
       connectSession: session,
