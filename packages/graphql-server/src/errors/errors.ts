2f48826747ebbb32897575855064af9bd6e6e547
Truong An <andt@reapit.com>
2020-08-06 10:24:23 +0700

    chore: change graphql server return error message

--- packages/graphql-server/src/errors/errors.ts
+++ packages/graphql-server/src/errors/errors.ts
@@ -10,37 +10,37 @@ export const errorMessages = {
 }
 
 export const generateAuthenticationError = (traceId?: string) => {
-  const error = new AuthenticationError(`${traceId || ''} - ${errorMessages.notAuthorized}`)
+  const error = new AuthenticationError(`${errorMessages.notAuthorized}`)
   logger.info('generateAuthenticationError', { traceId, error: JSON.stringify(error) })
   return error
 }
 
 export const generateUserInputError = (traceId?: string) => {
-  const error = new UserInputError(`${traceId || ''} - ${errorMessages.badRequest}`)
+  const error = new UserInputError(`${errorMessages.badRequest}`)
   logger.info('generateAuthenticationError', { traceId, error: JSON.stringify(error) })
   return error
 }
 
 export const generateValidationError = (traceId?: string) => {
-  const error = new ValidationError(`${traceId || ''} - ${errorMessages.badRequest}`)
+  const error = new ValidationError(`${errorMessages.badRequest}`)
   logger.info('generateValidationError', { traceId, error: JSON.stringify(error) })
   return error
 }
 
 export const generateForbiddenError = (traceId?: string) => {
-  const error = new ForbiddenError(`${traceId || ''} - ${errorMessages.forbidden}`)
+  const error = new ForbiddenError(`${errorMessages.forbidden}`)
   logger.info('generateForbiddenError', { traceId, error: JSON.stringify(error) })
   return error
 }
 
 export const generateInternalServerError = (traceId?: string) => {
-  const error = new ApolloError(`${traceId || ''} - ${errorMessages.internalServer}`, 'INTERNAL_SERVER_ERROR')
+  const error = new ApolloError(`${errorMessages.internalServer}`, 'INTERNAL_SERVER_ERROR')
   logger.info('generateInternalServerError', { traceId, error: JSON.stringify(error) })
   return error
 }
 
 export const generateNotFoundError = (traceId?: string) => {
-  const error = new ApolloError(`${traceId || ''} - ${errorMessages.notFound}`, 'NOT_FOUND')
+  const error = new ApolloError(`${errorMessages.notFound}`, 'NOT_FOUND')
   logger.info('generateNotFoundError', { traceId, error: JSON.stringify(error) })
   return error
 }
