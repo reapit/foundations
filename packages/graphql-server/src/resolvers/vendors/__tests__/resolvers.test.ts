bacf4aa45bdb7afc4192e894dd4e15594d27db07
Trường An <andt@reapit.com>
2020-08-13 14:28:43 +0700

    feat: #2144 Expand GraphQL Service service for Vendors entity (#2348)

---
+++ packages/graphql-server/src/resolvers/vendors/__tests__/resolvers.test.ts
@@ -0,0 +1,188 @@
+import vendorServices from '../services'
+import { checkPermission } from '../../../utils/check-permission'
+import errors from '../../../errors'
+import {
+  queryGetVendorById,
+  queryGetVendors,
+  mutationUpdateVendor,
+  queryGetVendorRelationships,
+  queryGetVendorRelationshipById,
+  mutationCreateVendorRelationship,
+  mutationDeleteVendorRelationship,
+} from '../resolvers'
+import { updateVendorArgsStub } from '../__stubs__/update-vendor'
+import { vendorStub } from '../__stubs__/vendor'
+import { vendorsStub } from '../__stubs__/vendors'
+import { mockContext } from '../../../__stubs__/context'
+
+jest.mock('../services', () => ({
+  getVendorById: jest.fn(() => vendorStub),
+  getVendors: jest.fn(() => vendorsStub),
+  createVendor: jest.fn(() => true),
+  updateVendor: jest.fn(() => true),
+  getVendorRelationships: jest.fn(() => []),
+  getVendorRelationshipById: jest.fn(() => {}),
+  createVendorRelationship: jest.fn(() => true),
+  deleteVendorRelationship: jest.fn(() => true),
+}))
+jest.mock('../../../errors', () => ({
+  generateAuthenticationError: jest.fn(() => 'authentication error'),
+}))
+jest.mock('../../../logger')
+jest.mock('../../../utils/check-permission', () => ({
+  checkPermission: jest.fn(() => true),
+}))
+
+describe('queryGetVendorById', () => {
+  it('should return correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(true)
+    const args = { id: 'id' }
+    const result = queryGetVendorById(null, args, mockContext)
+    expect(result).toEqual(vendorServices.getVendorById(args, mockContext))
+  })
+
+  it('should return auth error correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(false)
+    const args = { id: 'id' }
+    const result = queryGetVendorById(null, args, mockContext)
+    expect(result).toEqual(errors.generateAuthenticationError(mockContext.traceId))
+  })
+})
+
+describe('queryGetVendors', () => {
+  it('should return correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(true)
+    const args = {
+      pageSize: 1,
+      pageNumber: 1,
+      sortBy: 'string',
+      id: ['id'],
+      embed: ['embed'],
+      negotiatorId: ['string'],
+      officeId: ['string'],
+      address: 'string',
+      name: 'string',
+      createdFrom: 'string',
+      createdTo: 'string',
+      lastCallFrom: 'string',
+      lastCallTo: 'string',
+      nextCallFrom: 'string',
+      nextCallTo: 'string',
+      metadata: ['string'],
+    }
+    const result = queryGetVendors(null, args, mockContext)
+    expect(result).toEqual(vendorServices.getVendors(args, mockContext))
+  })
+
+  it('should return auth error correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(false)
+    const args = {
+      pageSize: 1,
+      pageNumber: 1,
+      sortBy: 'string',
+      id: ['id'],
+      embed: ['embed'],
+      negotiatorId: ['string'],
+      officeId: ['string'],
+      address: 'string',
+      name: 'string',
+      createdFrom: 'string',
+      createdTo: 'string',
+      lastCallFrom: 'string',
+      lastCallTo: 'string',
+      nextCallFrom: 'string',
+      nextCallTo: 'string',
+      metadata: ['string'],
+    }
+    const result = queryGetVendors(null, args, mockContext)
+    expect(result).toEqual(errors.generateAuthenticationError(mockContext.traceId))
+  })
+})
+
+describe('mutationUpdateVendor', () => {
+  it('should return correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(true)
+    const result = mutationUpdateVendor(null, updateVendorArgsStub, mockContext)
+    expect(result).toEqual(vendorServices.updateVendor(updateVendorArgsStub, mockContext))
+  })
+
+  it('should return auth error correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(false)
+    const result = mutationUpdateVendor(null, updateVendorArgsStub, mockContext)
+    expect(result).toEqual(errors.generateAuthenticationError(mockContext.traceId))
+  })
+})
+
+describe('queryGetVendorRelationships', () => {
+  it('should return correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(true)
+    const args = { id: 'id', pageSize: 5, pageNumber: 1 }
+    const result = queryGetVendorRelationships(null, args, mockContext)
+    expect(result).toEqual(vendorServices.getVendorRelationships(args, mockContext))
+  })
+
+  it('should return auth error correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(false)
+    const args = { id: 'id', pageSize: 5, pageNumber: 1 }
+    const result = queryGetVendorById(null, args, mockContext)
+    expect(result).toEqual(errors.generateAuthenticationError(mockContext.traceId))
+  })
+})
+
+describe('queryGetVendorRelationshipById', () => {
+  it('should return correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(true)
+    const args = { id: 'id', relationshipId: 'relationshipId' }
+    const result = queryGetVendorRelationshipById(null, args, mockContext)
+    expect(result).toEqual(vendorServices.getVendorRelationshipById(args, mockContext))
+  })
+
+  it('should return auth error correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(false)
+    const args = { id: 'id', relationshipId: 'relationshipId' }
+    const result = queryGetVendorRelationshipById(null, args, mockContext)
+    expect(result).toEqual(errors.generateAuthenticationError(mockContext.traceId))
+  })
+})
+
+describe('mutationCreateVendorRelationship', () => {
+  it('should return correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(true)
+    const args = {
+      id: 'id',
+      associatedId: 'associatedId',
+      associatedType: 'associatedType',
+      isMain: true,
+    }
+    const result = mutationCreateVendorRelationship(null, args, mockContext)
+    expect(result).toEqual(vendorServices.createVendorRelationship(args, mockContext))
+  })
+
+  it('should return auth error correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(false)
+    const args = {
+      id: 'id',
+      associatedId: 'associatedId',
+      associatedType: 'associatedType',
+      isMain: true,
+    }
+    const result = mutationCreateVendorRelationship(null, args, mockContext)
+    expect(result).toEqual(errors.generateAuthenticationError(mockContext.traceId))
+  })
+})
+describe('mutationDeleteVendorRelationship', () => {
+  it('should return correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(true)
+    const args = { id: 'id', relationshipId: 'relationshipId' }
+
+    const result = mutationDeleteVendorRelationship(null, args, mockContext)
+    expect(result).toEqual(vendorServices.deleteVendorRelationship(args, mockContext))
+  })
+
+  it('should return auth error correctly', () => {
+    ;(checkPermission as jest.Mock).mockReturnValue(false)
+    const args = { id: 'id', relationshipId: 'relationshipId' }
+    const result = mutationDeleteVendorRelationship(null, args, mockContext)
+    expect(result).toEqual(errors.generateAuthenticationError(mockContext.traceId))
+  })
+})
---
+++ packages/graphql-server/src/resolvers/vendors/__tests__/services.test.ts
@@ -0,0 +1,126 @@
+import {
+  callGetVendorByIdAPI,
+  callGetVendorsAPI,
+  callUpdateVendorAPI,
+  callGetVendorRelationshipsAPI,
+  callGetVendorRelationshipByIdAPI,
+  callCreateVendorRelationshipAPI,
+  callDeleteVendorRelationshipAPI,
+} from '../api'
+import { mockContext } from '../../../__stubs__/context'
+import { updateVendorArgsStub } from '../__stubs__/update-vendor'
+import {
+  getVendorById,
+  getVendors,
+  updateVendor,
+  getVendorRelationships,
+  getVendorRelationshipById,
+  createVendorRelationship,
+  deleteVendorRelationship,
+} from '../services'
+import { vendorStub } from '../__stubs__/vendor'
+import { vendorsStub } from '../__stubs__/vendors'
+
+jest.mock('../../../logger')
+jest.mock('../api', () => ({
+  callGetVendorByIdAPI: jest.fn(() => Promise.resolve(vendorStub)),
+  callGetVendorsAPI: jest.fn(() => Promise.resolve(vendorsStub)),
+  callUpdateVendorAPI: jest.fn(() => Promise.resolve(true)),
+  callGetVendorRelationshipsAPI: jest.fn(() => Promise.resolve([])),
+  callGetVendorRelationshipByIdAPI: jest.fn(() => Promise.resolve({})),
+  callCreateVendorRelationshipAPI: jest.fn(() => Promise.resolve(true)),
+  callDeleteVendorRelationshipAPI: jest.fn(() => Promise.resolve(true)),
+}))
+
+describe('getVendorById', () => {
+  it('should return correctly', async () => {
+    const args = { id: 'id' }
+    const result = await getVendorById(args, mockContext)
+    expect(callGetVendorByIdAPI).toHaveBeenCalledWith(args, mockContext)
+    expect(result).toEqual(vendorStub)
+  })
+})
+
+describe('getVendors', () => {
+  it('should return correctly', async () => {
+    const args = {
+      pageSize: 1,
+      pageNumber: 1,
+      sortBy: 'string',
+      id: ['id'],
+      embed: ['embed'],
+      negotiatorId: ['string'],
+      officeId: ['string'],
+      address: 'string',
+      name: 'string',
+      createdFrom: 'string',
+      createdTo: 'string',
+      lastCallFrom: 'string',
+      lastCallTo: 'string',
+      nextCallFrom: 'string',
+      nextCallTo: 'string',
+      metadata: ['string'],
+    }
+    const result = await getVendors(args, mockContext)
+    expect(callGetVendorsAPI).toHaveBeenCalledWith(args, mockContext)
+    expect(result).toEqual(vendorsStub)
+  })
+})
+
+describe('updateVendor', () => {
+  it('should return correctly', async () => {
+    const result = await updateVendor(updateVendorArgsStub, mockContext)
+    expect(callUpdateVendorAPI).toHaveBeenCalledWith(updateVendorArgsStub, mockContext)
+    expect(result).toEqual(true)
+  })
+})
+
+describe('getVendorRelationships', () => {
+  it('should return correctly', async () => {
+    const args = { id: 'id', pageSize: 5, pageNumber: 1 }
+    const result = await getVendorRelationships(args, mockContext)
+    expect(callGetVendorRelationshipsAPI).toHaveBeenCalledWith(args, mockContext)
+    expect(result).toEqual([])
+  })
+})
+
+describe('getVendorRelationshipById', () => {
+  it('should return correctly', async () => {
+    const args = { id: 'id', relationshipId: 'relationshipId' }
+    const result = await getVendorRelationshipById(args, mockContext)
+    expect(callGetVendorRelationshipByIdAPI).toHaveBeenCalledWith(args, mockContext)
+    expect(result).toEqual({})
+  })
+})
+
+describe('getVendorRelationshipById', () => {
+  it('should return correctly', async () => {
+    const args = { id: 'id', relationshipId: 'relationshipId' }
+    const result = await getVendorRelationshipById(args, mockContext)
+    expect(callGetVendorRelationshipByIdAPI).toHaveBeenCalledWith(args, mockContext)
+    expect(result).toEqual({})
+  })
+})
+
+describe('createVendorRelationship', () => {
+  it('should return correctly', async () => {
+    const args = {
+      id: 'id',
+      associatedId: 'associatedId',
+      associatedType: 'associatedType',
+      isMain: true,
+    }
+    const result = await createVendorRelationship(args, mockContext)
+    expect(callCreateVendorRelationshipAPI).toHaveBeenCalledWith(args, mockContext)
+    expect(result).toEqual(true)
+  })
+})
+
+describe('deleteVendorRelationship', () => {
+  it('should return correctly', async () => {
+    const args = { id: 'id', relationshipId: 'relationshipId' }
+    const result = await deleteVendorRelationship(args, mockContext)
+    expect(callDeleteVendorRelationshipAPI).toHaveBeenCalledWith(args, mockContext)
+    expect(result).toEqual(true)
+  })
+})
