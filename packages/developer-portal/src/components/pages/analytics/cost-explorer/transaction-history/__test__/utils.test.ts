a91fb5f08d819dede3acf9dfaa8171adb1376b39
Pham Hai Duong <duongph@reapit.com>
2020-08-13 09:27:18 +0700

    fix: #2346 transaction history bug (#2352)

--- packages/developer-portal/src/components/pages/analytics/cost-explorer/transaction-history/__test__/utils.test.ts
+++ packages/developer-portal/src/components/pages/analytics/cost-explorer/transaction-history/__test__/utils.test.ts
@@ -5,15 +5,28 @@ const inputDayjs = dayjs('2020-01-01T00:00:00.000')
 const developerCreatedDayjs = dayjs('2019-10-01T00:00:00.000')
 
 describe('transacion history utils', () => {
-  test('formatRenderDate should run correctly', () => {
-    expect(formatRenderDate(inputDayjs)).toBe('January 2020')
+  describe('formatRenderDate', () => {
+    it('formatRenderDate should run correctly', () => {
+      expect(formatRenderDate(inputDayjs)).toBe('January 2020')
+    })
+    it('formatRequestDate should run correctly', () => {
+      expect(formatRequestDate(inputDayjs)).toBe('2020-01')
+    })
   })
-  test('formatRequestDate should run correctly', () => {
-    expect(formatRequestDate(inputDayjs)).toBe('2020-01')
-  })
-  test('generatePreviousTransactionDate should run correctly', () => {
-    expect(
-      generatePreviousTransactionDate({ currentDate: inputDayjs, developerCreateDate: developerCreatedDayjs }),
-    ).toEqual([dayjs('2019-12-01T00:00:00.000'), dayjs('2019-11-01T00:00:00.000'), dayjs('2019-10-01T00:00:00.000')])
+
+  describe('generatePreviousTransactionDate', () => {
+    it('generatePreviousTransactionDate should run correctly', () => {
+      expect(
+        generatePreviousTransactionDate({ currentDate: inputDayjs, developerCreateDate: developerCreatedDayjs }),
+      ).toEqual([dayjs('2019-12-01T00:00:00.000'), dayjs('2019-11-01T00:00:00.000'), dayjs('2019-10-01T00:00:00.000')])
+    })
+    it('generatePreviousTransactionDate should run correctly', () => {
+      expect(
+        generatePreviousTransactionDate({
+          currentDate: dayjs('2020-08-12T00:00:00.000'),
+          developerCreateDate: dayjs('2020-08-03T00:00:00.000'),
+        }),
+      ).toEqual([])
+    })
   })
 })
