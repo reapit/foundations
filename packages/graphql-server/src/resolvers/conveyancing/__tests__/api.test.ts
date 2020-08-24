ca1c039f1681958e62998a03ce8123ac013de1f7
Cuong Vu <cuongvh@reapit.com>
2020-08-24 17:03:38 +0700

    feat: #2398 fix failing tests

--- packages/graphql-server/src/resolvers/conveyancing/__tests__/api.test.ts
+++ packages/graphql-server/src/resolvers/conveyancing/__tests__/api.test.ts
@@ -1,132 +1,200 @@
-import { updateConveyancingArgsMock } from '../__stubs__/update-conveyancing'
+import { mockContext } from '../../../__stubs__/context'
+import {
+  callGetConveyancingAPI,
+  callGetConveyancingByIdAPI,
+  callGetConveyancingChainAPI,
+  callUpdateConveyancingAPI,
+  callCreateUpwardLinkModelAPI,
+  callDeleteUpwardLinkModelAPI,
+  callCreateDownwardLinkModelAPI,
+  callDeleteDownwardLinkModelAPI,
+} from '../api'
+import { createPlatformAxiosInstance } from '../../../utils/axios-instances'
+import { conveyancingChainMock } from '../__stubs__/conveyancing-chain'
 import { conveyancingDetailMock } from '../__stubs__/conveyancing-detail'
+import { conveyancingMock } from '../__stubs__/conveyancing'
 import { createDownwardLinkModelArgsMock } from '../__stubs__/create-downward-link-model'
 import { createUpwardLinkModelArgsMock } from '../__stubs__/create-upward-link-model'
 import { deleteDownwardLinkModelArgsMock } from '../__stubs__/delete-downward-link-model'
 import { deleteUpwardLinkModelArgsMock } from '../__stubs__/delete-upward-link-model'
-import { conveyancingMock } from '../__stubs__/conveyancing'
-import { mockContext } from '../../../__stubs__/context'
-import {
-  callGetConveyancingByIdAPI,
-  callGetConveyancingAPI,
-  callUpdateConveyancingAPI,
-  callCreateDownwardLinkModelAPI,
-  callCreateUpwardLinkModelAPI,
-  callDeleteDownwardLinkModelAPI,
-  callDeleteUpwardLinkModelAPI,
-} from '../api'
-import { URLS } from '../../../constants/api'
+import { updateConveyancingArgsMock } from '../__stubs__/update-conveyancing'
+import { getIdFromCreateHeaders } from '../../../utils/get-id-from-create-headers'
 
-jest.mock('../../../utils/axios-instances', () => ({
-  createPlatformAxiosInstance: jest.fn(() => {
-    return {
-      get: jest.fn().mockImplementation(values => {
-        if (values === `${URLS.conveyancing}/${conveyancingDetailMock.id}`) {
-          return {
-            data: conveyancingDetailMock,
-          }
-        }
-        return {
-          data: conveyancingMock,
-        }
-      }),
-      patch: jest.fn(() => {
-        return {
-          status: 204,
-          data: conveyancingDetailMock,
-        }
-      }),
-      post: jest.fn(() => {
-        return {
-          data: conveyancingDetailMock,
-        }
-      }),
-      delete: jest.fn(() => {
-        return {
-          data: null,
-        }
-      }),
-    }
-  }),
+jest.mock('../../../utils/get-id-from-create-headers', () => ({
+  getIdFromCreateHeaders: jest.fn(),
 }))
 
-describe('conveyancing api', () => {
-  describe('callGetConveyancingByIdAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const mockArgs = { id: conveyancingDetailMock.id }
-        const result = await callGetConveyancingByIdAPI(mockArgs, mockContext)
-        expect(result).toEqual(conveyancingDetailMock)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+jest.mock('../../../utils/handle-error', () => ({
+  handleError: jest.fn(() => Promise.resolve('caught error')),
+}))
+jest.mock('../../../logger')
+jest.mock('../../../utils/axios-instances', () => ({
+  createPlatformAxiosInstance: jest.fn(() => ({
+    get: jest.fn(),
+    post: jest.fn(),
+    patch: jest.fn(),
+    put: jest.fn(),
+    delete: jest.fn(),
+  })),
+}))
+
+describe('callGetConveyancingAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.resolve({ data: conveyancingMock })),
     })
+    const args = { pageSize: 1 }
+    const result = await callGetConveyancingAPI(args, mockContext)
+    expect(result).toEqual(conveyancingMock)
+  })
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.reject('error caught')),
+    })
+    const args = { pageSize: 1 }
+    const result = await callGetConveyancingAPI(args, mockContext)
+    expect(result).toEqual('caught error')
+  })
+})
+
+describe('callGetConveyancingByIdAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.resolve({ data: conveyancingDetailMock })),
+    })
+    const args = { id: conveyancingDetailMock.id }
+    const result = await callGetConveyancingByIdAPI(args, mockContext)
+    expect(result).toEqual(conveyancingDetailMock)
   })
 
-  describe('callGetConveyancingAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const args = { id: ['id1', 'id2'], pageSize: 10, pageNumber: 1 }
-        const result = await callGetConveyancingAPI(args, mockContext)
-        expect(result).toEqual(conveyancingMock)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.reject('error caught')),
     })
+    const args = { id: conveyancingDetailMock.id }
+    const result = await callGetConveyancingByIdAPI(args, mockContext)
+    expect(result).toEqual('caught error')
   })
+})
 
-  describe('callUpdateConveyancingAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const result = await callUpdateConveyancingAPI(updateConveyancingArgsMock, mockContext)
-        expect(result).toEqual(conveyancingDetailMock)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+describe('callGetConveyancingChainAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.resolve({ data: conveyancingChainMock })),
     })
+    const args = { pageSize: 1 }
+    const result = await callGetConveyancingChainAPI(args, mockContext)
+    expect(result).toEqual(conveyancingChainMock)
   })
-
-  describe('callCreateDownwardLinkModelAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const result = await callCreateDownwardLinkModelAPI(createDownwardLinkModelArgsMock, mockContext)
-        expect(result).toEqual(conveyancingDetailMock)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.reject('error caught')),
     })
+    const args = { pageSize: 1 }
+    const result = await callGetConveyancingChainAPI(args, mockContext)
+    expect(result).toEqual('caught error')
   })
+})
 
-  describe('callCreateUpwardLinkModelAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const result = await callCreateUpwardLinkModelAPI(createUpwardLinkModelArgsMock, mockContext)
-        expect(result).toEqual(conveyancingDetailMock)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+///asdas d
+describe('callCreateUpwardLinkModelAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      post: jest.fn(() => Promise.resolve({ headers: 'header' })),
+      get: jest.fn(() => Promise.resolve({ data: conveyancingDetailMock })),
     })
+    ;(getIdFromCreateHeaders as jest.Mocked<any>).mockReturnValueOnce(conveyancingDetailMock.id)
+    await callCreateUpwardLinkModelAPI(createUpwardLinkModelArgsMock, mockContext)
   })
-
-  describe('callDeleteDownwardLinkModelAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const result = await callDeleteDownwardLinkModelAPI(deleteDownwardLinkModelArgsMock, mockContext)
-        expect(result).toEqual(deleteDownwardLinkModelArgsMock.id)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      post: jest.fn(() => Promise.reject('error caught')),
     })
+    const result = await callCreateUpwardLinkModelAPI(createUpwardLinkModelArgsMock, mockContext)
+    expect(result).toEqual('caught error')
   })
+})
 
-  describe('callDeleteUpwardLinkModelAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const result = await callDeleteUpwardLinkModelAPI(deleteUpwardLinkModelArgsMock, mockContext)
-        expect(result).toEqual(deleteUpwardLinkModelArgsMock.id)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+describe('callCreateDownwardLinkModelAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      post: jest.fn(() => Promise.resolve({ headers: 'header' })),
+      get: jest.fn(() => Promise.resolve({ data: conveyancingDetailMock })),
     })
+    ;(getIdFromCreateHeaders as jest.Mocked<any>).mockReturnValueOnce(conveyancingDetailMock.id)
+    await callCreateDownwardLinkModelAPI(createDownwardLinkModelArgsMock, mockContext)
+  })
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      post: jest.fn(() => Promise.reject('error caught')),
+    })
+    const result = await callCreateDownwardLinkModelAPI(createDownwardLinkModelArgsMock, mockContext)
+    expect(result).toEqual('caught error')
+  })
+})
+
+describe('callCreateDownwardLinkModelAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      post: jest.fn(() => Promise.resolve({ headers: 'header' })),
+      get: jest.fn(() => Promise.resolve({ data: conveyancingDetailMock })),
+    })
+    ;(getIdFromCreateHeaders as jest.Mocked<any>).mockReturnValueOnce(conveyancingDetailMock.id)
+    await callCreateDownwardLinkModelAPI(createDownwardLinkModelArgsMock, mockContext)
+  })
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      post: jest.fn(() => Promise.reject('error caught')),
+    })
+    const result = await callCreateDownwardLinkModelAPI(createDownwardLinkModelArgsMock, mockContext)
+    expect(result).toEqual('caught error')
+  })
+})
+
+describe('callDeleteUpwardLinkModelAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      delete: jest.fn(() => Promise.resolve({ headers: 'header' })),
+    })
+    await callDeleteUpwardLinkModelAPI(deleteUpwardLinkModelArgsMock, mockContext)
+  })
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      delete: jest.fn(() => Promise.reject('error caught')),
+    })
+    const result = await callDeleteUpwardLinkModelAPI(deleteUpwardLinkModelArgsMock, mockContext)
+    expect(result).toEqual('caught error')
+  })
+})
+
+describe('callDeleteDownwardLinkModelAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      delete: jest.fn(() => Promise.resolve({ headers: 'header' })),
+    })
+    await callDeleteDownwardLinkModelAPI(deleteDownwardLinkModelArgsMock, mockContext)
+  })
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      delete: jest.fn(() => Promise.reject('error caught')),
+    })
+    const result = await callDeleteDownwardLinkModelAPI(deleteDownwardLinkModelArgsMock, mockContext)
+    expect(result).toEqual('caught error')
+  })
+})
+
+describe('callUpdateConveyancingAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      patch: jest.fn(() => Promise.resolve({ headers: 'header' })),
+    })
+    await callUpdateConveyancingAPI(updateConveyancingArgsMock, mockContext)
+  })
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      patch: jest.fn(() => Promise.reject('error caught')),
+    })
+    const result = await callUpdateConveyancingAPI(updateConveyancingArgsMock, mockContext)
+    expect(result).toEqual('caught error')
   })
 })
--- packages/graphql-server/src/resolvers/tasks/__tests__/api.test.ts
+++ packages/graphql-server/src/resolvers/tasks/__tests__/api.test.ts
@@ -1,86 +1,100 @@
-import { createTaskArgsMock } from '../__stubs__/create-task'
-import { updateTaskArgsMock } from '../__stubs__/update-task'
+import { mockContext } from '../../../__stubs__/context'
+import { callGetTasksAPI, callCreateTaskAPI, callUpdateTaskAPI, callGetTaskByIdAPI } from '../api'
+import { createPlatformAxiosInstance } from '../../../utils/axios-instances'
 import { taskMock } from '../__stubs__/task'
 import { tasksMock } from '../__stubs__/tasks'
-import { mockContext } from '../../../__stubs__/context'
-import { callCreateTaskAPI, callGetTaskByIdAPI, callGetTasksAPI, callUpdateTaskAPI } from '../api'
-import { URLS } from '../../../constants/api'
+import { createTaskArgsMock } from '../__stubs__/create-task'
+import { updateTaskArgsMock } from '../__stubs__/update-task'
+import { getIdFromCreateHeaders } from '../../../utils/get-id-from-create-headers'
 
-jest.mock('../../../utils/axios-instances', () => ({
-  createPlatformAxiosInstance: jest.fn(() => {
-    return {
-      get: jest.fn().mockImplementation(values => {
-        if (values === `${URLS.tasks}/id`) {
-          return {
-            data: taskMock,
-          }
-        }
-        return {
-          data: tasksMock,
-        }
-      }),
-      post: jest.fn(() => {
-        return {
-          headers: {
-            location: '/task/id',
-          },
-          data: taskMock,
-        }
-      }),
-      patch: jest.fn(() => {
-        return {
-          status: 204,
-          data: null,
-        }
-      }),
-    }
-  }),
+jest.mock('../../../utils/get-id-from-create-headers', () => ({
+  getIdFromCreateHeaders: jest.fn(),
 }))
 
-describe('tasks api', () => {
-  describe('callGetTaskByIdAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const mockArgs = { id: 'id' }
-        const result = await callGetTaskByIdAPI(mockArgs, mockContext)
-        expect(result).toEqual(taskMock)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+jest.mock('../../../utils/handle-error', () => ({
+  handleError: jest.fn(() => Promise.resolve('caught error')),
+}))
+jest.mock('../../../logger')
+jest.mock('../../../utils/axios-instances', () => ({
+  createPlatformAxiosInstance: jest.fn(() => ({
+    get: jest.fn(),
+    post: jest.fn(),
+    patch: jest.fn(),
+    put: jest.fn(),
+    delete: jest.fn(),
+  })),
+}))
+
+describe('callGetTasksAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.resolve({ data: tasksMock })),
     })
+    const args = { pageSize: 1 }
+    const result = await callGetTasksAPI(args, mockContext)
+    expect(result).toEqual(tasksMock)
+  })
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.reject('error caught')),
+    })
+    const args = { pageSize: 1 }
+    const result = await callGetTasksAPI(args, mockContext)
+    expect(result).toEqual('caught error')
+  })
+})
+
+describe('callGetTaskByIdAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.resolve({ data: taskMock })),
+    })
+    const args = { id: taskMock.id }
+    const result = await callGetTaskByIdAPI(args, mockContext)
+    expect(result).toEqual(taskMock)
   })
 
-  describe('callGetTasksAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const args = { id: ['id1', 'id2'], pageSize: 10, pageNumber: 1 }
-        const result = await callGetTasksAPI(args, mockContext)
-        expect(result).toEqual(tasksMock)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      get: jest.fn(() => Promise.reject('error caught')),
     })
+    const args = { id: taskMock.id }
+    const result = await callGetTaskByIdAPI(args, mockContext)
+    expect(result).toEqual('caught error')
   })
+})
 
-  describe('callCreateTaskAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const result = await callCreateTaskAPI(createTaskArgsMock, mockContext)
-        expect(result).toEqual(taskMock)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+describe('callCreateTaskAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      post: jest.fn(() => Promise.resolve({ headers: 'header' })),
+      get: jest.fn(() => Promise.resolve({ data: taskMock })),
     })
+    ;(getIdFromCreateHeaders as jest.Mocked<any>).mockReturnValueOnce(taskMock.id)
+    await callCreateTaskAPI(createTaskArgsMock, mockContext)
+    expect(getIdFromCreateHeaders).toHaveBeenCalledWith({ headers: 'header' })
   })
-
-  describe('callUpdateTaskAPI', () => {
-    it('should run correctly', async () => {
-      try {
-        const result = await callUpdateTaskAPI(updateTaskArgsMock, mockContext)
-        expect(result).toEqual(taskMock)
-      } catch (error) {
-        expect(error).toBeUndefined()
-      }
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      post: jest.fn(() => Promise.reject('error caught')),
     })
+    const result = await callCreateTaskAPI(createTaskArgsMock, mockContext)
+    expect(result).toEqual('caught error')
+  })
+})
+
+describe('callUpdateTaskAPI', () => {
+  it('should work correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      patch: jest.fn(() => Promise.resolve({ headers: 'header' })),
+    })
+    await callUpdateTaskAPI(updateTaskArgsMock, mockContext)
+  })
+  it('should catch error correctly', async () => {
+    ;(createPlatformAxiosInstance as jest.Mocked<any>).mockReturnValueOnce({
+      patch: jest.fn(() => Promise.reject('error caught')),
+    })
+    const result = await callUpdateTaskAPI(updateTaskArgsMock, mockContext)
+    expect(result).toEqual('caught error')
   })
 })
