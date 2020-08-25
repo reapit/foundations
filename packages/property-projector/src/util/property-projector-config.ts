0c4fa7136ac70a1b517977b7b6eef0d4bafa1d5d
root <root@DESKTOP-LKI033U.localdomain>
2020-08-25 15:16:43 +0100

    Added default configuration if there is no config to fetch. Added error handling around the fetch.

--- packages/property-projector/src/util/property-projector-config.ts
+++ packages/property-projector/src/util/property-projector-config.ts
@@ -1,12 +1,45 @@
 import { PropertyProjectorConfig } from '@/types/global'
 import { ReapitConnectSession } from '@reapit/connect-session'
-import { constants } from 'os';
-import React, { useState, useEffect } from "react";
 
 
 export const getPropertyProjectorConfig = async (session: ReapitConnectSession): Promise<PropertyProjectorConfig> => {
+  let propertyProjectorConfig: any = {
+    logo: '',
+    primaryColour: '#006580',
+    secondaryColour: '#FFFFFF',
+    interval: 5,
+    propertyLimit: 25,
+    marketingMode: ['sales', 'lettings'],
+    sellingStatus: ['forSale', 'underOffer'],
+    lettingStatus: ['toLet', 'underOffer'],
+    minPrice: 0,
+    maxPrice: 0,
+    minRent: 0,
+    maxRent: 0,
+    showAddress: true,
+    sortBy: 'created',
+    departments: {},
+    offices: [],
+  };
 
-  const response = await fetch("http://localhost:3000/dev/v1/property-projector-config/ABC/RPT");
-  return response.json();
+  const response = await fetch("http://localhost:3000/dev/v1/property-projector-config/ABC/RPT")
+  .then(async response => {
+    const data = await response.json();
+    // check for error response
+    if (!response.ok) {
+        // get error message from body or default to response statusText
+        const error = (data && data.message) || response.statusText;
+        return Promise.reject(error);
+    }
 
+    delete data['customerId'];
+    delete data['officeId'];  
+    propertyProjectorConfig = {...propertyProjectorConfig, ...data};
+    return propertyProjectorConfig;
+  })
+  .catch(error => {
+    console.error('There was an error retrieving the configuration.', error);
+  });
+
+ return propertyProjectorConfig;
 }
