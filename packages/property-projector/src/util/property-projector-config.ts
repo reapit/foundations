b33cd1c14ac96db10f3de4fb612c1b96be3ebb92
root <root@DESKTOP-LKI033U.localdomain>
2020-08-25 09:14:05 +0100

    Removed hard coded configuration and replaced with simple GET from DynamoDB

--- packages/property-projector/src/util/property-projector-config.ts
+++ packages/property-projector/src/util/property-projector-config.ts
@@ -1,29 +1,12 @@
 import { PropertyProjectorConfig } from '@/types/global'
 import { ReapitConnectSession } from '@reapit/connect-session'
+import { constants } from 'os';
+import React, { useState, useEffect } from "react";
 
-/**
- * @todo remove test data
- * @todo add refreshHour - every 'x' hours the projector should refresh when it's open
- */
+
 export const getPropertyProjectorConfig = async (session: ReapitConnectSession): Promise<PropertyProjectorConfig> => {
-  const propertyProjectorConfig: any = {
-    logo: '',
-    primaryColour: '#006580',
-    secondaryColour: '#FFFFFF',
-    interval: 5,
-    propertyLimit: 25,
-    marketingMode: ['selling', 'letting'],
-    sellingStatuses: ['forSale', 'underOffer'],
-    lettingStatuses: ['toLet', 'underOffer'],
-    minPrice: 0,
-    maxPrice: 0,
-    minRent: 0,
-    maxRent: 0,
-    showAddress: true,
-    sortBy: 'created',
-    departments: { G: ['house', 'bungalow', 'land'] },
-    offices: [],
-  }
 
-  return propertyProjectorConfig
+  const response = await fetch("http://localhost:3000/dev/v1/property-projector-config/ABC/RPT");
+  return response.json();
+
 }
