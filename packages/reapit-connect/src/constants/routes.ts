120f50f5b9197d272de1335b28beeb88580a3b62
undefined <nghiapn@reapit.com>
2020-08-26 11:27:14 +0700

    fix: reapit connect should redirect to prod instead dev 2252

--- packages/reapit-connect/src/constants/routes.ts
+++ packages/reapit-connect/src/constants/routes.ts
@@ -3,6 +3,6 @@ const ROUTES = {
   LOGIN: '/welcome/login',
 }
 
-export const DEVELOPER_PORTAL_APPS = 'https://dev.developers.reapit.cloud/apps'
+export const DEVELOPER_PORTAL_APPS = 'https://developers.reapit.cloud/apps'
 
 export default ROUTES
