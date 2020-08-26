a821b448bbf63db3f99e8722183ee4106cdf4cf6
Pham Hai Duong <duongph@reapit.com>
2020-08-26 13:41:50 +0700

    fix: #2451 land on page not found (#2483)

--- packages/developer-portal/src/constants/routes.ts
+++ packages/developer-portal/src/constants/routes.ts
@@ -1,7 +1,6 @@
 const Routes = {
   AUTHENTICATION: '/authentication',
   AUTHENTICATION_LOGIN_TYPE: '/authentication/:loginType',
-  DEVELOPER: '/',
   WELCOME: '/welcome',
   APPS: '/apps',
   APP_DETAIL: '/apps/:appid',
