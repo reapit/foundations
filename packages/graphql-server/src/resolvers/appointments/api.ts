b2ded465966da4d3159e64f4f329471dcc3961fe
Pham Hai Duong <duongph@reapit.com>
2020-08-27 14:24:27 +0700

    chore: remove logger for debugging (#2506)

--- packages/graphql-server/src/resolvers/appointments/api.ts
+++ packages/graphql-server/src/resolvers/appointments/api.ts
@@ -36,8 +36,9 @@ export const callGetAppointmentByIdAPI = async (
     )
     return response?.data
   } catch (error) {
-    const handleErrorResult = await handleError({ error, traceId, caller: 'callGetAppointmentByIdAPI' })
-    return handleErrorResult
+    // const handleErrorResult = await handleError({ error, traceId, caller: 'callGetAppointmentByIdAPI' })
+    // return handleErrorResult
+    return error
   }
 }
 
@@ -56,8 +57,9 @@ export const callGetAppointmentsAPI = async (
     })
     return response?.data
   } catch (error) {
-    const handleErrorResult = await handleError({ error, traceId, caller: 'callGetAppointmentsAPI' })
-    return handleErrorResult
+    // const handleErrorResult = await handleError({ error, traceId, caller: 'callGetAppointmentsAPI' })
+    // return handleErrorResult
+    return error
   }
 }
 
