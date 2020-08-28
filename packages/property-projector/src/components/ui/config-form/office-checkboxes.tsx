0a2680c5edca3978905fd7e25cfaa7e20dafccfb
Amal <ahaji@reapit.com>
2020-08-28 10:49:32 +0100

    added a css change to display offices in columns

--- packages/property-projector/src/components/ui/config-form/office-checkboxes.tsx
+++ packages/property-projector/src/components/ui/config-form/office-checkboxes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FormSection, FormHeading, FormSubHeading, Checkbox } from '@reapit/elements'
+import { FormSection, FormHeading, FormSubHeading, Checkbox, Grid, GridItem } from '@reapit/elements'
 
 type OfficeCheckboxesProps = {
   offices: any[]
@@ -14,17 +14,21 @@ const OfficeCheckboxes: React.FC<OfficeCheckboxesProps> = props => {
       <FormSubHeading>
         Only properties from the selected offices will be shown in your Property Projector.
       </FormSubHeading>
-      {offices.map(office => {
-        return (
-          <Checkbox
-            name={`offices`}
-            id={`office-${office.id}`}
-            labelText={office.name}
-            value={office.id}
-            key={office.id}
-          />
-        )
-      })}
+      <Grid>
+        <GridItem className="office-checkbox">
+          {offices.map(office => {
+            return (
+              <Checkbox
+                name={`offices`}
+                id={`office-${office.id}`}
+                labelText={office.name}
+                value={office.id}
+                key={office.id}
+              />
+            )
+          })}
+        </GridItem>
+      </Grid>
     </FormSection>
   )
 }
