bf5a1f485ab21e20b9e98b46ae68d6f5056c2d79
Trường An <truongan106@gmail.com>
2020-09-07 17:34:01 +0700

    feat: #2581 Update the ‘Existing Subscription’ modal for Developer Edition  (#2585)

--- packages/developer-portal/src/components/ui/developer-edition-modal/__tests__/error-content.tsx
+++ packages/developer-portal/src/components/ui/developer-edition-modal/__tests__/error-content.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import { ErrorContent } from '../error-content'
+import { ErrorContent, handleDownload } from '../error-content'
 
 describe('ErrorContent', () => {
   it('should match snapshot', () => {
     const wrapper = shallow(<ErrorContent afterClose={jest.fn()} />)
     expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('handleDownload', () => {
+  it('should open download link', () => {
+    const downloadURL = 'downloadURL'
+    window.reapit.config.developerEditionDownloadUrl = downloadURL
+    window.open = jest.fn()
+
+    handleDownload()
+    expect(window.open).toBeCalledWith(downloadURL, '_self')
   })
 })
--- packages/developer-portal/src/components/ui/developer-edition-modal/error-content.tsx
+++ packages/developer-portal/src/components/ui/developer-edition-modal/error-content.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react'
 import { Button, SubTitleH6, ModalHeader, ModalBody, ModalFooter, ModalProps } from '@reapit/elements'
 import { link } from '@/styles/elements/link'
-import routes from '@/constants/routes'
+
+import developerEditionGuide from '@/assets/files/developer-edition-guide.pdf'
+
+export const handleDownload = () => {
+  window.open(window.reapit.config.developerEditionDownloadUrl, '_self')
+}
 
 export type ErrorContentProps = Pick<ModalProps, 'afterClose'>
 
@@ -12,21 +17,25 @@ export const ErrorContent: React.FC<ErrorContentProps> = ({ afterClose }) => {
       <ModalBody
         body={
           <SubTitleH6 className="has-text-weight-normal">
-            It looks as though you already have a subscription in place for the Developer Edition of Agency Cloud. If
-            you have not received your email with instructions on how to download, please let us know using the live
-            chat feature on the&nbsp;
-            <a className={link} target="_blank" rel="noopener noreferrer" href={routes.HELP}>
-              &apos;Help&apos;
+            It looks as though you already have a subscription in place for the Developer Edition of Agency Cloud. To
+            download, please use the ‘Download Now’ button below. For more information or support using the Developer
+            Edition, please&nbsp;
+            <a className={link} target="_blank" rel="noopener noreferrer" href={developerEditionGuide}>
+              click here
             </a>
-            &nbsp;page.
           </SubTitleH6>
         }
       />
       <ModalFooter
         footerItems={
-          <Button variant="primary" type="button" onClick={afterClose}>
-            CLOSE
-          </Button>
+          <>
+            <Button variant="primary" type="button" onClick={afterClose}>
+              CLOSE
+            </Button>
+            <Button variant="primary" type="button" onClick={handleDownload}>
+              DOWNLOAD NOW
+            </Button>
+          </>
         }
       />
     </>
