5f279026709479339662e69427f257090fd27d15
Duong Pham <tanphamhaiduong@gmail.com>
2020-09-09 16:21:48 +0700

    feat: customer dynamic render form

---
+++ packages/elements-next/src/styles/sizing.ts
@@ -0,0 +1,120 @@
+import { css } from 'linaria'
+
+// Utilities for width
+export const elWZero = css`
+  width: 0;
+`
+export const elWOne = css`
+  width: 0.25rem;
+`
+export const elWTwo = css`
+  width: 0.5rem;
+`
+export const elWThree = css`
+  width: 0.75rem;
+`
+export const elWFour = css`
+  width: 1rem;
+`
+export const elWFive = css`
+  width: 1.25rem;
+`
+export const elWSix = css`
+  width: 1.5rem;
+`
+export const elWEight = css`
+  width: 2rem;
+`
+export const elWTen = css`
+  width: 2.5rem;
+`
+export const elWTwelve = css`
+  width: 3rem;
+`
+export const elWAuto = css`
+  width: auto;
+`
+export const elWOneHalf = css`
+  width: 50%;
+`
+export const elWOneThird = css`
+  width: 33.333333%;
+`
+export const elWTwoThird = css`
+  width: 66.666667%;
+`
+export const elWOneFourth = css`
+  width: 25%;
+`
+export const elWTwoFourth = css`
+  width: 50%;
+`
+export const elWThreeFourth = css`
+  width: 75%;
+`
+export const elWOneFifth = css`
+  width: 20%;
+`
+export const elWTwoFifth = css`
+  width: 40%;
+`
+export const elWThreeFifth = css`
+  width: 60%;
+`
+export const elWFourFifth = css`
+  width: 80%;
+`
+export const elWOneSixth = css`
+  width: 16.666667;
+`
+export const elWTwoSixth = css`
+  width: 33.333333%;
+`
+export const elWThreeSix = css`
+  width: 50%;
+`
+export const elWFourSixth = css`
+  width: 66.666667%;
+`
+export const elWFiveSixth = css`
+  width: 83.333333%;
+`
+export const elWOneTwelveth = css`
+  width: 8.333333%;
+`
+export const elWTwoTwelveth = css`
+  width: 16.666667%;
+`
+export const elWThreeTwelveth = css`
+  width: 25%;
+`
+export const elWFourthTwelveth = css`
+  width: 33.333333%;
+`
+export const elWFiveTwelveth = css`
+  width: 41.666667%;
+`
+export const elWSixTwelveth = css`
+  width: 50%;
+`
+export const elWSevenTwelveth = css`
+  width: 58.333333%;
+`
+export const elWEightTwelveth = css`
+  width: 66.666667%;
+`
+export const elWNineTwelveth = css`
+  width: 75%;
+`
+export const elWTenTwelveth = css`
+  width: 83.333333%;
+`
+export const elElevenTwelveth = css`
+  width: 91.666667%;
+`
+export const elWFull = css`
+  width: 100%;
+`
+export const elWScreen = css`
+  width: 100vw;
+`
