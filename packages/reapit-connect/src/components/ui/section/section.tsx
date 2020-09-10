026dbd64c6044334eeed2c8a6ccf1476a286616e
NghiaPham <nghiapn@reapit.com>
2020-09-10 11:00:17 +0700

    chore: #2618 reapit connect landing page to include agency cloud section (#2644)

--- packages/reapit-connect/src/components/ui/section/section.tsx
+++ packages/reapit-connect/src/components/ui/section/section.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { H2, SubTitleH4, SubTitleH6, GridItem } from '@reapit/elements'
+import { H2, SubTitleH4, SubTitleH6, GridItem, FlexContainerResponsive } from '@reapit/elements'
 import { cx } from 'linaria'
 import { sectionContainer, hasBackgroundSection, gridItem } from './__styles__'
 
@@ -29,22 +29,24 @@ export const Section: React.FC<SectionProps> = ({
         background: background || 'white',
       }}
     >
-      {imageLeft && (
-        <GridItem className={cx('flex items-center justify-center', gridItem)}>
-          <img alt={heading} src={image} />
+      <FlexContainerResponsive>
+        {imageLeft && (
+          <GridItem className={cx('flex items-center justify-center', gridItem)}>
+            <img alt={heading} src={image} />
+          </GridItem>
+        )}
+        <GridItem className={cx('flex items-start justify-center', gridItem, !!background && hasBackgroundSection)}>
+          <H2>{heading}</H2>
+          <SubTitleH4>{subheading}</SubTitleH4>
+          <SubTitleH6 className="mb-4">{description}</SubTitleH6>
+          {!!button && button}
         </GridItem>
-      )}
-      <GridItem className={cx('flex items-start justify-center', gridItem, !!background && hasBackgroundSection)}>
-        <H2>{heading}</H2>
-        <SubTitleH4>{subheading}</SubTitleH4>
-        <SubTitleH6 className="mb-4">{description}</SubTitleH6>
-        {!!button && button}
-      </GridItem>
-      {!imageLeft && (
-        <GridItem className={cx('flex items-center justify-center', gridItem)}>
-          <img alt={heading} src={image} />
-        </GridItem>
-      )}
+        {!imageLeft && (
+          <GridItem className={cx('flex items-center justify-center', gridItem)}>
+            <img alt={heading} src={image} />
+          </GridItem>
+        )}
+      </FlexContainerResponsive>
     </div>
   )
 }
