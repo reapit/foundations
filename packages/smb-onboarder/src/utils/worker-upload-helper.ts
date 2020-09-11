857bc6a59dac21f9ef4683148e58495963366c39
Truong An <truongan106@gmail.com>
2020-09-11 09:38:31 +0700

    chore: update follow pr cmts

--- packages/smb-onboarder/src/utils/worker-upload-helper.ts
+++ packages/smb-onboarder/src/utils/worker-upload-helper.ts
@@ -39,6 +39,32 @@ export interface MutationResult<T> {
   response?: ExecutionResult
 }
 
+const NEGOTIATOR_INDEXES = {
+  name: 0,
+  jobTitle: 1,
+  email: 2,
+  mobilePhone: 3,
+  officeId: 4,
+  active: 5,
+  id: 6,
+  _eTag: 7,
+}
+
+const OFFICE_INDEXES = {
+  id: 0,
+  _eTag: 1,
+  name: 2,
+  buildingName: 3,
+  buildingNumber: 4,
+  line1: 5,
+  line2: 6,
+  line3: 7,
+  line4: 8,
+  postcode: 9,
+  workPhone: 10,
+  email: 11,
+}
+
 /*
  * serial executes Promises sequentially.
  * @param {funcs} An array of funcs that return promises.
@@ -101,61 +127,61 @@ export const mutation = <T>({
 
 export const prepareCreateOfficeParams = (rowData: Cell[]): CreateOfficeModel => {
   return {
-    name: rowData[2].value as string, // name column
+    name: rowData[OFFICE_INDEXES.name].value as string,
     address: {
-      buildingName: rowData[3].value as string, // buildingName column
-      buildingNumber: rowData[4].value as string, // buildingNumber column
-      line1: rowData[5].value as string, // line1 column
-      line2: rowData[6].value as string, // line2 column
-      line3: rowData[7].value as string, // line3 column
-      line4: rowData[8].value as string, // line4 column
-      postcode: rowData[9].value as string, // postcode column
+      buildingName: rowData[OFFICE_INDEXES.buildingName].value as string,
+      buildingNumber: rowData[OFFICE_INDEXES.buildingNumber].value as string,
+      line1: rowData[OFFICE_INDEXES.line1].value as string,
+      line2: rowData[OFFICE_INDEXES.line2].value as string,
+      line3: rowData[OFFICE_INDEXES.line3].value as string,
+      line4: rowData[OFFICE_INDEXES.line4].value as string,
+      postcode: rowData[OFFICE_INDEXES.postcode].value as string,
     },
-    workPhone: rowData[10].value as string, // workPhone column
-    email: rowData[11].value as string, // email column
+    workPhone: rowData[OFFICE_INDEXES.workPhone].value as string,
+    email: rowData[OFFICE_INDEXES.email].value as string,
   }
 }
 
 export const prepareUpdateOfficeParams = (rowData: Cell[]): UpdateOfficeParams => {
   return {
-    id: rowData[0].value as string,
-    _eTag: rowData[1].value as string,
-    name: rowData[2].value as string, // name column
+    id: rowData[OFFICE_INDEXES.id].value as string,
+    _eTag: rowData[OFFICE_INDEXES._eTag].value as string,
+    name: rowData[OFFICE_INDEXES.name].value as string,
     address: {
-      buildingName: rowData[3].value as string, // buildingName column
-      buildingNumber: rowData[4].value as string, // buildingNumber column
-      line1: rowData[5].value as string, // line1 column
-      line2: rowData[6].value as string, // line2 column
-      line3: rowData[7].value as string, // line3 column
-      line4: rowData[8].value as string, // line4 column
-      postcode: rowData[9].value as string, // postcode column
+      buildingName: rowData[OFFICE_INDEXES.buildingName].value as string,
+      buildingNumber: rowData[OFFICE_INDEXES.buildingNumber].value as string,
+      line1: rowData[OFFICE_INDEXES.line1].value as string,
+      line2: rowData[OFFICE_INDEXES.line2].value as string,
+      line3: rowData[OFFICE_INDEXES.line3].value as string,
+      line4: rowData[OFFICE_INDEXES.line4].value as string,
+      postcode: rowData[OFFICE_INDEXES.postcode].value as string,
     },
-    workPhone: rowData[10].value as string, // workPhone column
-    email: rowData[11].value as string, // email column
+    workPhone: rowData[OFFICE_INDEXES.workPhone].value as string,
+    email: rowData[OFFICE_INDEXES.email].value as string,
   }
 }
 
 export const prepareUpdateNegotiatorParams = (rowData: Cell[]): NegotiatorUpdateParams => {
-  const active = Boolean(rowData[5].value?.toLowerCase() === 'true')
+  const active = Boolean(rowData[NEGOTIATOR_INDEXES.active].value?.toLowerCase() === 'true')
   return {
-    name: rowData[0].value as string, // name column
-    jobTitle: rowData[1].value as string, // jobTitle column
-    email: rowData[2].value as string, // email column
-    mobilePhone: rowData[3].value as string, // mobilePhone column
-    active: active, // active column
-    id: rowData[6]?.value as string,
-    _eTag: rowData[7]?.value as string,
+    name: rowData[NEGOTIATOR_INDEXES.name].value as string,
+    jobTitle: rowData[NEGOTIATOR_INDEXES.jobTitle].value as string,
+    email: rowData[NEGOTIATOR_INDEXES.email].value as string,
+    mobilePhone: rowData[NEGOTIATOR_INDEXES.mobilePhone].value as string,
+    active: active,
+    id: rowData[NEGOTIATOR_INDEXES.id]?.value as string,
+    _eTag: rowData[NEGOTIATOR_INDEXES._eTag]?.value as string,
   }
 }
 
 export const prepareCreateNegotiatorParams = (rowData: Cell[]): NegotiatorCreateParams => {
-  const active = Boolean(rowData[5].value?.toLowerCase() === 'true')
+  const active = Boolean(rowData[NEGOTIATOR_INDEXES.active].value?.toLowerCase() === 'true')
   return {
-    name: rowData[0].value as string, // name column
-    jobTitle: rowData[1].value as string, // jobTitle column
-    email: rowData[2].value as string, // email column
-    mobilePhone: rowData[3].value as string, // mobilePhone column
-    officeId: rowData[4].value as string, // mobilePhone column
-    active: active, // active column
+    name: rowData[NEGOTIATOR_INDEXES.name].value as string,
+    jobTitle: rowData[NEGOTIATOR_INDEXES.jobTitle].value as string,
+    email: rowData[NEGOTIATOR_INDEXES.email].value as string,
+    mobilePhone: rowData[NEGOTIATOR_INDEXES.mobilePhone].value as string,
+    officeId: rowData[NEGOTIATOR_INDEXES.officeId].value as string,
+    active: active,
   }
 }
