880233e8452c0fdf0acfc4307a404731a7dd79ec
Pham Hai Duong <duongph@reapit.com>
2020-09-11 16:03:31 +0700

    feat: 2611 expand central lib of utilities (#2655)

---
+++ packages/elements-next/src/base/sizing.ts
@@ -0,0 +1,240 @@
+import { css } from 'linaria'
+
+// Utilities for width
+export const elW0 = css`
+  width: 0;
+`
+export const elW1 = css`
+  width: 0.25rem;
+`
+export const elW2 = css`
+  width: 0.5rem;
+`
+export const elW3 = css`
+  width: 0.75rem;
+`
+export const elW4 = css`
+  width: 1rem;
+`
+export const elW5 = css`
+  width: 1.25rem;
+`
+export const elW6 = css`
+  width: 1.5rem;
+`
+export const elW8 = css`
+  width: 2rem;
+`
+export const elW10 = css`
+  width: 2.5rem;
+`
+export const elW12 = css`
+  width: 3rem;
+`
+export const elWAuto = css`
+  width: auto;
+`
+export const elW1_2 = css`
+  width: 50%;
+`
+export const elW1_3 = css`
+  width: 33.333333%;
+`
+export const elW2_3 = css`
+  width: 66.666667%;
+`
+export const elW1_4 = css`
+  width: 25%;
+`
+export const elW2_4 = css`
+  width: 50%;
+`
+export const elW3_4 = css`
+  width: 75%;
+`
+export const elW1_5 = css`
+  width: 20%;
+`
+export const elW2_5 = css`
+  width: 40%;
+`
+export const elW3_5 = css`
+  width: 60%;
+`
+export const elW4_5 = css`
+  width: 80%;
+`
+export const elW1_6 = css`
+  width: 16.666667;
+`
+export const elW2_6 = css`
+  width: 33.333333%;
+`
+export const elW3_6 = css`
+  width: 50%;
+`
+export const elW4_6 = css`
+  width: 66.666667%;
+`
+export const elW5_6 = css`
+  width: 83.333333%;
+`
+export const elW1_12 = css`
+  width: 8.333333%;
+`
+export const elW2_12 = css`
+  width: 16.666667%;
+`
+export const elW3_12 = css`
+  width: 25%;
+`
+export const elW4_12 = css`
+  width: 33.333333%;
+`
+export const elW5_12 = css`
+  width: 41.666667%;
+`
+export const elW6_12 = css`
+  width: 50%;
+`
+export const elW7_12 = css`
+  width: 58.333333%;
+`
+export const elW8_12 = css`
+  width: 66.666667%;
+`
+export const elW9_12 = css`
+  width: 75%;
+`
+export const elW10_12 = css`
+  width: 83.333333%;
+`
+export const elW11_12 = css`
+  width: 91.666667%;
+`
+export const elWFull = css`
+  width: 100%;
+`
+export const elWScreen = css`
+  width: 100vw;
+`
+
+// Utilities for width
+
+export const elH0 = css`
+  height: 0;
+`
+export const elH1 = css`
+  height: 0.25rem;
+`
+export const elH2 = css`
+  height: 0.5rem;
+`
+export const elH3 = css`
+  height: 0.75rem;
+`
+export const elH4 = css`
+  height: 1rem;
+`
+export const elH5 = css`
+  height: 1.25rem;
+`
+export const elH6 = css`
+  height: 1.5rem;
+`
+export const elH8 = css`
+  height: 2rem;
+`
+export const elH10 = css`
+  height: 2.5rem;
+`
+export const elH12 = css`
+  height: 3rem;
+`
+export const elHAuto = css`
+  height: auto;
+`
+export const elH1_2 = css`
+  height: 50%;
+`
+export const elH1_3 = css`
+  height: 33.333333%;
+`
+export const elH2_3 = css`
+  height: 66.666667%;
+`
+export const elH1_4 = css`
+  height: 25%;
+`
+export const elH2_4 = css`
+  height: 50%;
+`
+export const elH3_4 = css`
+  height: 75%;
+`
+export const elH1_5 = css`
+  height: 20%;
+`
+export const elH2_5 = css`
+  height: 40%;
+`
+export const elH3_5 = css`
+  height: 60%;
+`
+export const elH4_5 = css`
+  height: 80%;
+`
+export const elH1_6 = css`
+  height: 16.666667;
+`
+export const elH2_6 = css`
+  height: 33.333333%;
+`
+export const elH3_6 = css`
+  height: 50%;
+`
+export const elH4_6 = css`
+  height: 66.666667%;
+`
+export const elH5_6 = css`
+  height: 83.333333%;
+`
+export const elH1_12 = css`
+  height: 8.333333%;
+`
+export const elH2_12 = css`
+  height: 16.666667%;
+`
+export const elH3_12 = css`
+  height: 25%;
+`
+export const elH4_12 = css`
+  height: 33.333333%;
+`
+export const elH5_12 = css`
+  height: 41.666667%;
+`
+export const elH6_12 = css`
+  height: 50%;
+`
+export const elH7_12 = css`
+  height: 58.333333%;
+`
+export const elH8_12 = css`
+  height: 66.666667%;
+`
+export const elH9_12 = css`
+  height: 75%;
+`
+export const elH10_12 = css`
+  height: 83.333333%;
+`
+export const elH11_12 = css`
+  height: 91.666667%;
+`
+export const elHFull = css`
+  height: 100%;
+`
+export const elHScreen = css`
+  height: 100vw;
+`
--- packages/elements-next/src/styles/typography.ts
+++
@@ -1,5 +0,0 @@
-import { css } from 'linaria'
-
-export const elIsTextCentered = css`
-  text-align: center;
-`
