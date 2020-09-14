b97cc28abf305c58ea78fb0662dd3ca68b82bc1e
Vu Nguyen <vunp@reapit.com>
2020-09-14 18:25:25 +0700

    fix: #2626 - Remove web-kit prefix

--- packages/elements-next/src/components/loader/__styles__/loader-styles.ts
+++ packages/elements-next/src/components/loader/__styles__/loader-styles.ts
@@ -65,31 +65,17 @@ export const elLoader = css`
     height: 100%;
     width: 6px;
     display: inline-block;
-    -webkit-animation: sk-stretchdelay 1.2s infinite ease-in-out;
     animation: sk-stretchdelay 1.2s infinite ease-in-out;
     margin: 0 1.5px;
   }
   .rect2 {
-    -webkit-animation-delay: -1.1s;
     animation-delay: -1.1s;
   }
   .rect3 {
-    -webkit-animation-delay: -1s;
     animation-delay: -1s;
   }
   .rect4 {
-    -webkit-animation-delay: -0.9s;
     animation-delay: -0.9s;
-  }
-  @-webkit-keyframes sk-stretchdelay {
-    0%,
-    40%,
-    100% {
-      -webkit-transform: scaleY(0.4);
-    }
-    20% {
-      -webkit-transform: scaleY(1);
-    }
   }
 
   @keyframes sk-stretchdelay {
@@ -97,11 +83,9 @@ export const elLoader = css`
     40%,
     100% {
       transform: scaleY(0.4);
-      -webkit-transform: scaleY(0.4);
     }
     20% {
       transform: scaleY(1);
-      -webkit-transform: scaleY(1);
     }
   }
 `
