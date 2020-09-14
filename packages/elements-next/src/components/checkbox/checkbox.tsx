f43ad36893e4bccf33b36109934a366cd4756079
Duong Pham <tanphamhaiduong@gmail.com>
2020-09-14 14:52:17 +0700

    feat: #50 migrate checkbox to elements next

---
+++ packages/elements-next/src/components/checkbox/checkbox.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { cx } from 'linaria'
+import { elCheckbox } from './styles'
+
+interface CheckboxProps {
+  name: string
+  value: boolean | string
+  id?: string
+  label?: string
+  className?: string
+  disabled?: boolean
+}
+
+export const Checkbox: React.FC<CheckboxProps> = ({ id, name, label, className, value, disabled = false }) => {
+  return (
+    <label htmlFor={id} className={cx(elCheckbox, className)}>
+      {label}
+      <input id={id} name={name} type="checkbox" checked={!!value} disabled={disabled} />
+      <span />
+    </label>
+  )
+}
+
+export default Checkbox
