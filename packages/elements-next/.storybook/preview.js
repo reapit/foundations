6b40983b054eae6e0ba6e50fee665d9d35f0e443
Pham Hai Duong <duongph@reapit.com>
2020-09-15 16:18:12 +0700

    feat: #50 style for checkbox (#2710)

--- packages/elements-next/.storybook/preview.js
+++ packages/elements-next/.storybook/preview.js
@@ -3,6 +3,7 @@ import { withHTML } from '@whitespace/storybook-addon-html/react'
 export const parameters = {
   controls: { expanded: true },
   actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: { expanded: true },
 }
 
 export const decorators = [
--- packages/elements-next/src/components/checkbox/checkbox.stories.tsx
+++ packages/elements-next/src/components/checkbox/checkbox.stories.tsx
@@ -3,34 +3,40 @@ import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
 import { Checkbox, CheckboxProps } from './checkbox'
+import { elFlex } from '@/base/flexbox'
+import { elMr4 } from '@/base/spacing'
 
 export default {
   title: 'Checkbox',
   component: Checkbox,
-  argTypes: {
-    backgroundColor: { control: 'color' },
+  args: {
+    name: 'field_name',
+    checked: false,
+    id: undefined,
+    label: undefined,
+    className: undefined,
+    disabled: false,
+    onChange: e => {
+      console.log(e.target.checked)
+    },
   },
 } as Meta
 
 const Template: Story<CheckboxProps> = args => <Checkbox {...args} />
 
-export const Primary = Template.bind({})
-Primary.args = {
-  value: true,
+export const UseAsSingle = Template.bind({})
+UseAsSingle.args = {
+  checked: true,
   label: 'Checkbox',
 }
 
-export const Secondary = Template.bind({})
-Secondary.args = {
-  label: 'Checkbox',
-}
+const GroupTemplate: Story<CheckboxProps> = () => (
+  <div className={elFlex}>
+    <Checkbox className={elMr4} name="field_name" label="Option 1" value="option_1" />
+    <Checkbox className={elMr4} name="field_name" label="Option 2" value="option_2" />
+    <Checkbox className={elMr4} name="field_name" label="Option 3" value="option_3" />
+    <Checkbox className={elMr4} name="field_name" label="Option 4" value="option_4" />
+  </div>
+)
 
-export const Large = Template.bind({})
-Large.args = {
-  label: 'Checkbox',
-}
-
-export const Small = Template.bind({})
-Small.args = {
-  label: 'Checkbox',
-}
+export const UseAsGroup = GroupTemplate.bind({})
