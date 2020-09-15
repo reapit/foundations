f24989f3abfefe8e0ccace9b92890e1d858d57b6
Khac Vy <vytk@reapit.com>
2020-09-15 15:34:16 +0700

    chore: #2615 update story example

--- packages/elements-next/src/components/message/message.stories.tsx
+++ packages/elements-next/src/components/message/message.stories.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
 
 import { Message, MessageProps } from './message'
 
 export default {
-  title: 'Example/Message',
+  title: 'Message',
   component: Message,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as Meta
 
 const Template: Story<MessageProps> = args => <Message {...args} />
