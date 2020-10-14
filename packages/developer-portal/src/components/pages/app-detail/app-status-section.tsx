5bcc05cb3a2bdf65f3d1a7f730d5ddc936cf2b62
willmcvay@pm.me <wmcvay@reapit.com>
2020-10-14 10:15:20 +0100

    fix: #2776 revert sandbox install

--- packages/developer-portal/src/components/pages/app-detail/app-status-section.tsx
+++ packages/developer-portal/src/components/pages/app-detail/app-status-section.tsx
@@ -1,60 +1,60 @@
-import React, { FC, useState } from 'react'
+import React, { FC /**  useState */ } from 'react'
 import { IsSidebar } from './app-sections'
 import { AppDetailSection } from './app-ui-helpers'
-import { useDispatch, useSelector } from 'react-redux'
-import { AppDetailModel, InstallationModel } from '@reapit/foundations-ts-definitions'
-import { useParams } from 'react-router-dom'
-import { selectSboxInstallation } from '@/selector/installations'
-import { Dispatch } from 'redux'
+// import { useDispatch, useSelector } from 'react-redux'
+import { AppDetailModel /** , InstallationModel */ } from '@reapit/foundations-ts-definitions'
+// import { useParams } from 'react-router-dom'
+// import { selectSboxInstallation } from '@/selector/installations'
+// import { Dispatch } from 'redux'
 import { FaCheck } from 'react-icons/fa'
 import { check } from './__styles__/app-detail'
-import { installApp, uninstallApp } from '@/actions/installations'
-import { ToastMessage } from '@reapit/elements'
+// import { installApp, uninstallApp } from '@/actions/installations'
+// import { ToastMessage } from '@reapit/elements'
 
 interface StatusSectionProps extends IsSidebar {
   appDetail: AppDetailModel
 }
 
-const closeToast = (setVisible: React.Dispatch<React.SetStateAction<boolean>>) => () => {
-  setVisible(false)
-}
+// const closeToast = (setVisible: React.Dispatch<React.SetStateAction<boolean>>) => () => {
+//   setVisible(false)
+// }
 
-const handleInstallledCheckboxChange = (
-  isInstalledSboxState: boolean,
-  appId: string,
-  sboxInstallation: InstallationModel | undefined,
-  setToastVisible: React.Dispatch<React.SetStateAction<boolean>>,
-  setIsInstalledSbox: React.Dispatch<React.SetStateAction<boolean>>,
-  dispatch: Dispatch,
-) => () => {
-  if (isInstalledSboxState && sboxInstallation) {
-    dispatch(
-      uninstallApp({
-        appId,
-        installationId: sboxInstallation.id as string,
-        terminatedReason: 'Terminated by developer from app detail page',
-        callback: () => setToastVisible(true),
-      }),
-    )
-  } else {
-    dispatch(
-      installApp({
-        appId,
-        callback: () => setToastVisible(true),
-      }),
-    )
-  }
+// const handleInstallledCheckboxChange = (
+//   isInstalledSboxState: boolean,
+//   appId: string,
+//   sboxInstallation: InstallationModel | undefined,
+//   setToastVisible: React.Dispatch<React.SetStateAction<boolean>>,
+//   setIsInstalledSbox: React.Dispatch<React.SetStateAction<boolean>>,
+//   dispatch: Dispatch,
+// ) => () => {
+//   if (isInstalledSboxState && sboxInstallation) {
+//     dispatch(
+//       uninstallApp({
+//         appId,
+//         installationId: sboxInstallation.id as string,
+//         terminatedReason: 'Terminated by developer from app detail page',
+//         callback: () => setToastVisible(true),
+//       }),
+//     )
+//   } else {
+//     dispatch(
+//       installApp({
+//         appId,
+//         callback: () => setToastVisible(true),
+//       }),
+//     )
+//   }
 
-  setIsInstalledSbox(!isInstalledSboxState)
-}
+//   setIsInstalledSbox(!isInstalledSboxState)
+// }
 
 export const StatusSection: FC<StatusSectionProps> = ({ appDetail, isSidebar }) => {
   const { isListed, authFlow } = appDetail
-  const sboxInstallation = useSelector(selectSboxInstallation)
-  const [isInstalledSboxState, setIsInstalledSbox] = useState<boolean>(Boolean(sboxInstallation))
-  const { appid } = useParams<{ appid: string }>()
-  const [toastVisible, setToastVisible] = useState<boolean>(false)
-  const dispatch = useDispatch()
+  // const sboxInstallation = useSelector(selectSboxInstallation)
+  // const [isInstalledSboxState, setIsInstalledSbox] = useState<boolean>(Boolean(sboxInstallation))
+  // const { appid } = useParams<{ appid: string }>()
+  // const [toastVisible, setToastVisible] = useState<boolean>(false)
+  // const dispatch = useDispatch()
 
   return (
     <>
@@ -72,7 +72,7 @@ export const StatusSection: FC<StatusSectionProps> = ({ appDetail, isSidebar }) 
           )}
         </div>
       </AppDetailSection>
-      <AppDetailSection headerText="Sandbox Installation" isSidebar={isSidebar}>
+      {/* <AppDetailSection headerText="Sandbox Installation" isSidebar={isSidebar}>
         <div className="field pt-2">
           <div className="control">
             <div className="field field-checkbox">
@@ -103,7 +103,7 @@ export const StatusSection: FC<StatusSectionProps> = ({ appDetail, isSidebar }) 
         message={`Successfully ${isInstalledSboxState ? '' : 'un'}installed app for sandbox`}
         variant="info"
         onCloseToast={closeToast(setToastVisible)}
-      />
+      /> */}
     </>
   )
 }
