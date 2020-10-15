e41617dd6bca863cbae25e97d1135082cb17637e
kbusby <kbusby@reapit.com>
2020-10-15 16:29:06 +0100

    moved validation code out of the config form and into its own file

--- packages/property-projector/src/components/ui/config-form/config-form.tsx
+++ packages/property-projector/src/components/ui/config-form/config-form.tsx
@@ -7,8 +7,8 @@ import { getOffices } from '@/platform-api/offices-api'
 import DepartmentCheckboxes from './department-checkboxes'
 import OfficeCheckboxes from './office-checkboxes'
 import Projector from '../projector'
+import { validate } from './validation'
 import ColourPicker from '../colour-picker'
-import { ERROR_MESSAGES } from '@/constants/errors'
 import { SELLING_STATUS, LETTING_STATUS } from '@/constants/statuses'
 import { getNegotiatorOfficeId } from '@/util/negotiator-helper'
 import { PropertyProjectorConfig, Department, Office } from '@/types/global'
@@ -192,60 +192,7 @@ const ConfigForm: React.FC<ConfigFormProps> = () => {
         enableReinitialize={true}
         initialValues={getInitialFormValues()}
         onSubmit={values => submitForm(values)}
-        validate={values => {
-          const errors: any = {}
-          const hexCodeRegex = /^#[0-9A-F]{6}$/i
-
-          if (values.logo === '') {
-            errors.logo = ERROR_MESSAGES.MISSING_LOGO
-          }
-
-          if (!hexCodeRegex.test(values.primaryColour)) {
-            errors.primaryColour = ERROR_MESSAGES.INCORRECT_HEX_PRIMARY
-          }
-
-          if (!hexCodeRegex.test(values.secondaryColour)) {
-            errors.secondaryColour = ERROR_MESSAGES.INCORRECT_HEX_SECONDARY
-          }
-
-          if (!hexCodeRegex.test(values.headerTextColour)) {
-            errors.headerTextColour = ERROR_MESSAGES.INCORRECT_HEX_HEADER_TEXT
-          }
-
-          if (values.marketingMode.length === 0) {
-            errors.marketingMode = ERROR_MESSAGES.MISSING_MARKETING_MODE
-          }
-
-          if (Number.isNaN(Number(values.minPrice))) {
-            errors.minPrice = ERROR_MESSAGES.INCORRECT_MIN_PRICE
-          }
-
-          if (Number.isNaN(Number(values.maxPrice))) {
-            errors.maxPrice = ERROR_MESSAGES.INCORRECT_MAX_PRICE
-          }
-
-          if (Number.isNaN(Number(values.minRent))) {
-            errors.minRent = ERROR_MESSAGES.INCORRECT_MIN_RENT
-          }
-
-          if (Number.isNaN(Number(values.maxRent))) {
-            errors.maxRent = ERROR_MESSAGES.INCORRECT_MAX_RENT
-          }
-
-          if (Number.isNaN(Number(values.propertyLimit))) {
-            errors.propertyLimit = ERROR_MESSAGES.INCORRECT_PROPERTY_LIMIT
-          }
-
-          if (Number.isNaN(Number(values.interval))) {
-            errors.interval = ERROR_MESSAGES.INCORRECT_ROTATION_INTERVAL
-          }
-
-          if (values.departments.length === 0) {
-            errors.departments = ERROR_MESSAGES.MISSING_DEPARTMENTS
-          }
-
-          return errors
-        }}
+        validate={validate}
         component={({ setFieldValue, values, touched, errors }) => {
           const errorCount = Object.keys(errors).length
 
