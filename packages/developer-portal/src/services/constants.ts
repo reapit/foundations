e20b950384413bf3caa58d1978ff0bade1df23cb
willmcvay@pm.me <wmcvay@reapit.com>
2020-11-03 17:01:33 +0000

    feat: #2866 dev portal all working

--- packages/developer-portal/src/services/constants.ts
+++ packages/developer-portal/src/services/constants.ts
@@ -1,23 +1,23 @@
-export const API_VERSION = '2020-01-31'
+export const API_VERSION = 'latest'
 export const MARKETPLACE_API_VERSION = '2'
 
 export const URLS = {
   developers: '/marketplace/developers',
   apps: '/marketplace/apps',
   approvals: '/marketplace/approvals',
-  installations: '/approvals/installations',
-  scopes: '/marketplace/approvals/scopes',
-  categories: '/approvals/categories',
+  installations: '/marketplace/installations',
+  scopes: '/marketplace/scopes',
+  categories: '/marketplace/categories',
   docs: '/docs',
   statistics: '/statistics',
   trafficEvents: '/trafficevents',
-  desktopIntegrationTypes: '/approvals/DesktopIntegrationTypes',
+  desktopIntegrationTypes: '/marketplace/DesktopIntegrationTypes',
   webhooks: '/webhooks',
   webhookSubscriptions: '/webhooks/subscriptions',
   webhooksTopics: '/webhooks/topics',
   trafficEventBilling: '/trafficevents/billing',
   trafficEventStatistics: '/trafficevents/trafficStatistics',
   negotiators: '/negotiators',
-  developerSubscriptions: '/approvals/subscriptions',
+  developerSubscriptions: '/marketplace/subscriptions',
   fileUpload: '/fileUpload',
 }
