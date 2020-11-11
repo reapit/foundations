05860b2f4d1c5661f1046c03650a31031b1de170
willmcvay@pm.me <wmcvay@reapit.com>
2020-11-11 17:04:49 +0000

    fix: broken test

--- packages/data-warehouse/src/components/ui/__tests__/toast.test.tsx
+++ packages/data-warehouse/src/components/ui/__tests__/toast.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import Toast from '../toast'
 import { MessageProvider } from '../../../context/message-context'
 
 describe('Toast', () => {
   it('should match a snapshot', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <MessageProvider>
         <Toast />
       </MessageProvider>,
