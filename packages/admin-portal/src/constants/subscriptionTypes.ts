04faf29fdcf77b14a68aed7e012d864de01fdf18
Bdragon <Bdragon93@users.noreply.github.com>
2020-11-24 17:49:35 +0700

    feat: #2877 Rename developer label, change type to select input (#3031)

---
+++ packages/admin-portal/src/constants/subscriptionTypes.ts
@@ -0,0 +1,7 @@
+import { SelectBoxOptions } from '@reapit/elements'
+
+export const subscriptionTypes: SelectBoxOptions[] = [
+  { label: 'Application Listing', value: 'applicationListing' },
+  { label: 'Developer Registration', value: 'developerRegistration' },
+  { label: 'Developer Edition', value: 'developerEdition' },
+]
