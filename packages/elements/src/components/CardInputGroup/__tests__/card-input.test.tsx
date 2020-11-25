e7c9887f35702b133f99fc29d8ce9d1d83c741b9
Will McVay <wmcvay@reapit.com>
2020-11-25 13:15:38 +0000

    feat: #2974 elements custom card form (#3036)
    * feat: #2974 custom card form
    
    * feat: #2974 adds card input group component to Elements
    
    * fix: #2974 pr feedback
    
    * fix: #2974 broken script name

---
+++ packages/elements/src/components/CardInputGroup/__tests__/card-input.test.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react'
+import { mount } from 'enzyme'
+import { Formik, Form } from 'formik'
+import { act } from 'react-dom/test-utils'
+import { CardInput, CardInputProps } from '../card-input'
+
+const props: CardInputProps = {
+  id: 'cardNumber',
+  name: 'cardNumber',
+  labelText: 'Card Number',
+  required: true,
+  disabled: false,
+  className: 'some-class',
+  setCardType: jest.fn(),
+  cardType: 'unknown',
+}
+
+describe('CardInput', () => {
+  it('should match a snapshot for an unknown card', () => {
+    const wrapper = mount(
+      <Formik initialValues={{ cardNumber: '' }} onSubmit={jest.fn()}>
+        {() => (
+          <Form>
+            <CardInput {...props} />
+          </Form>
+        )}
+      </Formik>,
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match a snapshot for visa', () => {
+    const wrapper = mount(
+      <Formik initialValues={{ cardNumber: '' }} onSubmit={jest.fn()}>
+        {() => (
+          <Form>
+            <CardInput {...{ ...props, cardType: 'visa' }} />
+          </Form>
+        )}
+      </Formik>,
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match a snapshot for mastercard', () => {
+    const wrapper = mount(
+      <Formik initialValues={{ cardNumber: '' }} onSubmit={jest.fn()}>
+        {() => (
+          <Form>
+            <CardInput {...{ ...props, cardType: 'mastercard' }} />
+          </Form>
+        )}
+      </Formik>,
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match a snapshot for amex', () => {
+    const wrapper = mount(
+      <Formik initialValues={{ cardNumber: '' }} onSubmit={jest.fn()}>
+        {() => (
+          <Form>
+            <CardInput {...{ ...props, cardType: 'amex' }} />
+          </Form>
+        )}
+      </Formik>,
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should work when integrating with Formik', async () => {
+    const wrapper = mount(
+      <Formik initialValues={{ cardNumber: '' }} onSubmit={jest.fn()}>
+        {() => (
+          <Form>
+            <CardInput {...props} />
+          </Form>
+        )}
+      </Formik>,
+    )
+    expect(wrapper.find('label')).toHaveLength(1)
+    await act(async () => {
+      wrapper.find('input').simulate('change', {
+        target: {
+          value: '4444444444444444',
+          name: 'cardNumber',
+        },
+      })
+    })
+    wrapper.update()
+    expect(wrapper.find('input').prop('value')).toEqual('4444 4444 4444 4444')
+  })
+})
