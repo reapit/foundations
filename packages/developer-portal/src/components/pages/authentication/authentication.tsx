20fc48e375a75e4c6157ec414c9b1b535357478e
Will McVay <wmcvay@reapit.com>
2020-12-11 12:40:46 +0000

    feat: #3159 #3140 installs table non dev flow (#3162)
    * feat: #3159 #3140 adds email install table, improves non-dev reg flow
    
    * fix: husky not running

--- packages/developer-portal/src/components/pages/authentication/authentication.tsx
+++ packages/developer-portal/src/components/pages/authentication/authentication.tsx
@@ -1,13 +1,32 @@
 import * as React from 'react'
 import { History } from 'history'
-import { useHistory } from 'react-router'
+import { Redirect, useHistory } from 'react-router'
 import { Modal, Button } from '@reapit/elements'
 import Routes from '@/constants/routes'
 import { reapitConnectBrowserSession } from '@/core/connect-session'
+import { ReapitConnectSession, useReapitConnect } from '@reapit/connect-session'
+import { useDispatch, useSelector } from 'react-redux'
+import { COGNITO_GROUP_ADMIN_USERS } from '../../../selector/auth'
+import { CreateDeveloperModel } from '@reapit/foundations-ts-definitions'
+import { Dispatch } from 'redux'
+import { developerCreate } from '../../../actions/developer'
+import { selectDeveloperFormState } from '../../../selector'
 
 export const onRegisterButtonClick = (history: History) => {
   return () => {
     history.replace(Routes.REGISTER)
+  }
+}
+
+export const onContinueButtonClick = (dispatch: Dispatch, session: ReapitConnectSession) => {
+  const { loginIdentity } = session
+  const values: CreateDeveloperModel = {
+    name: loginIdentity.name ?? '',
+    companyName: loginIdentity.orgName ?? '',
+    email: loginIdentity.email ?? '',
+  }
+  return () => {
+    dispatch(developerCreate(values))
   }
 }
 
@@ -21,24 +40,52 @@ export const onLogoutButtonClick = () => {
 
 const Authentication: React.FC = () => {
   const history = useHistory()
+  const dispatch = useDispatch()
+  const { connectSession } = useReapitConnect(reapitConnectBrowserSession)
+  const developerCreateState = useSelector(selectDeveloperFormState)
+  const orgName = connectSession?.loginIdentity.orgName ?? ''
+  const isUserAdmin = connectSession?.loginIdentity?.groups.includes(COGNITO_GROUP_ADMIN_USERS)
+  const isLoading = developerCreateState === 'SUBMITTING'
+
+  if (!connectSession) {
+    return null
+  }
+
+  if (developerCreateState === 'SUCCESS') {
+    return <Redirect to={Routes.SETTINGS_ORGANISATION_TAB} />
+  }
+
+  if (developerCreateState === 'ERROR') {
+    return <Redirect to={Routes.LOGIN} />
+  }
+
   return (
     <Modal
-      title="Register as a Developer?"
+      title="Welcome to the Developers Portal"
       visible={true}
       tapOutsideToDissmiss={false}
       footerItems={
         <>
-          <Button onClick={onRegisterButtonClick(history)}>Register</Button>
-          <Button onClick={onMarketplaceButtonClick}>Marketplace</Button>
-          <Button onClick={onLogoutButtonClick}>Logout</Button>
+          {isUserAdmin ? (
+            <Button onClick={onContinueButtonClick(dispatch, connectSession)} disabled={isLoading} loading={isLoading}>
+              Continue
+            </Button>
+          ) : (
+            <Button onClick={onRegisterButtonClick(history)}>Register</Button>
+          )}
+          <Button onClick={onMarketplaceButtonClick} disabled={isLoading}>
+            Marketplace
+          </Button>
+          <Button onClick={onLogoutButtonClick} disabled={isLoading}>
+            Logout
+          </Button>
         </>
       }
     >
       <p>
-        You are not currently registered as a Reapit Developer, if you would like to register please visit
-        <span>
-          <a onClick={onRegisterButtonClick(history)}> our registration page</a>
-        </span>
+        {isUserAdmin
+          ? `To continue using the Developers Portal, you will first need to setup your profile information for your organisation ‘${orgName}’. Please click ‘Continue’ to procced, where you will be redirected to setup your organisation's profile.`
+          : `As you are a part of the ‘${orgName}’ group, you will need to be invited to join this Developer organisation by an Administrator. Please contact an Administrator within your organisation to request an invitation.`}
       </p>
     </Modal>
   )
