886b197e7a522d6b5ad24cdc6b3a3eac3eac6edf
Bdragon <longtr268@gmail.com>
2020-12-15 18:45:58 +0700

    feat: #2971 update test snapshot

--- packages/payments-client/src/components/pages/__tests__/payment.test.tsx
+++ packages/payments-client/src/components/pages/__tests__/payment.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import Payment from '../payment'
+import PaymentPage from '../payment'
 
 jest.mock('react-router', () => ({
   ...jest.requireActual('react-router'),
@@ -9,8 +9,41 @@ jest.mock('react-router', () => ({
   }),
 }))
 
+jest.mock('swr', () =>
+  jest.fn(() => ({
+    data: {
+      amount: 50,
+      clientAccountName: 'Lettings Client Acc',
+      companyId: null,
+      customer: {
+        id: 'RPT17000005',
+        email: 'max@atkinson.co.uk',
+        forename: 'Max',
+        mobilePhone: '077865932145',
+        name: 'Mr Max Atkinson',
+        primaryAddress: {
+          buildingName: 'Flat 5',
+          buildingNumber: '17',
+          countryId: 'GB',
+          line1: 'Hans Crescent',
+          line2: 'Knightbridge',
+          line3: 'London',
+          line4: '',
+          postcode: 'SW1X 0LG',
+        },
+        surname: 'Atkinson',
+        title: 'Mr',
+      },
+      description: 'Tenant Receipt - Payment Request: Pete Fee request',
+      id: 'MKT20000010',
+      status: 'posted',
+      type: 'paymentRequest',
+    },
+  })),
+)
+
 describe('Payment', () => {
   it('should match a snapshot', () => {
-    expect(shallow(<Payment />)).toMatchSnapshot()
+    expect(shallow(<PaymentPage />)).toMatchSnapshot()
   })
 })
