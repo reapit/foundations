8c5e93338604401c0c41497562678da88828f61e
Bdragon <longtr268@gmail.com>
2020-12-15 15:53:11 +0700

    feat: #2937 use global variable for fetcher url

--- packages/payments-client/src/utils/fetcher.ts
+++ packages/payments-client/src/utils/fetcher.ts
@@ -2,6 +2,6 @@ import { StringMap } from '@reapit/elements'
 import { genPlatformHeaders } from '../utils/headers'
 
 export const fetcher = async (path: string) =>
-  fetch(`https://platform.dev.paas.reapit.cloud${path}`, {
+  fetch(`${window.reapit.config.platformApiUrl}${path}`, {
     headers: (await genPlatformHeaders()) as StringMap,
   }).then(res => res.json())
