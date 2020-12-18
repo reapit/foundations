99af58081f8399f2e2da9113d916511c19a8d90e
Bdragon <Bdragon93@users.noreply.github.com>
2020-12-18 11:48:39 +0700

    feat: #2971 populate data take payment page billing details (#3171)
    * feat: #2971 populate data take payment page billing details
    
    * feat: #2971 remove type and rename component
    
    * feat: #2971 update test snapshot
    
    * feat: #2971 resolve comments

--- packages/payments-client/src/components/pages/__tests__/payment.test.tsx
+++ packages/payments-client/src/components/pages/__tests__/payment.test.tsx
@@ -1,16 +1,58 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import Payment from '../payment'
-
-const locationMock = { pathname: '/payments' }
+import PaymentPage, { PaymentForm } from '../payment'
 
 jest.mock('react-router', () => ({
   ...jest.requireActual('react-router'),
-  useLocation: jest.fn(() => locationMock),
+  useParams: () => ({
+    paymentId: 'MKT20000010',
+  }),
 }))
+
+const data = {
+  amount: 50,
+  clientAccountName: 'Lettings Client Acc',
+  customer: {
+    id: 'RPT17000005',
+    email: 'max@atkinson.co.uk',
+    forename: 'Max',
+    mobilePhone: '077865932145',
+    name: 'Mr Max Atkinson',
+    primaryAddress: {
+      buildingName: 'Flat 5',
+      buildingNumber: '17',
+      countryId: 'GB',
+      line1: 'Hans Crescent',
+      line2: 'Knightbridge',
+      line3: 'London',
+      line4: '',
+      postcode: 'SW1X 0LG',
+    },
+    surname: 'Atkinson',
+    title: 'Mr',
+  },
+  description: 'Tenant Receipt - Payment Request: Pete Fee request',
+  id: 'MKT20000010',
+  status: 'posted',
+  type: 'paymentRequest',
+}
+
+jest.mock('swr', () =>
+  jest.fn(() => ({
+    data,
+  })),
+)
 
 describe('Payment', () => {
   it('should match a snapshot', () => {
-    expect(shallow(<Payment />)).toMatchSnapshot()
+    expect(shallow(<PaymentPage />)).toMatchSnapshot()
+  })
+})
+
+describe('PaymentForm', () => {
+  it('should match a snapshot', () => {
+    expect(
+      shallow(<PaymentForm data={data} merchantKey={{ merchantSessionKey: 'merchantSessionKey', expiry: 'expiry' }} />),
+    ).toMatchSnapshot()
   })
 })
--- packages/payments-client/src/components/pages/payment.tsx
+++ packages/payments-client/src/components/pages/payment.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import useSWR from 'swr'
 import {
   Section,
   H3,
@@ -13,11 +15,118 @@ import {
   unformatCard,
   unformatCardExpires,
 } from '@reapit/elements'
+import { URLS } from '../../constants/api'
 import { MerchantKey, opayoMerchantKeyService } from '../../opayo-api/merchant-key'
 import { opayoCreateTransactionService } from '../../opayo-api/transactions'
-// import * as Yup from 'yup'
+import { PaymentModel } from '@reapit/foundations-ts-definitions'
 
-export const PaymentsPage: React.FC = () => {
+export const PaymentForm: React.FC<{ data: PaymentModel; merchantKey: MerchantKey }> = ({ data, merchantKey }) => {
+  const customer = data.customer || {}
+  const { forename = '', surname = '', primaryAddress = {} } = customer
+  const { buildingName, buildingNumber, line1, line3, line4, postcode = '', countryId = '' } = primaryAddress
+  let address1
+  if (buildingName && line1) {
+    address1 = `${buildingName} ${line1}`
+  } else if (buildingNumber && line1) {
+    address1 = `${buildingNumber} ${line1}`
+  } else {
+    address1 = buildingName || buildingNumber || line1 || ''
+  }
+
+  return (
+    <Formik
+      initialValues={{
+        customerFirstName: forename,
+        customerLastName: surname,
+        address1,
+        city: line3 || line4 || '',
+        postalCode: postcode,
+        country: countryId,
+        cardholderName: '',
+        cardNumber: '',
+        expiryDate: '',
+        securityCode: '',
+        cardIdentifier: '',
+      }}
+      onSubmit={cardDetails => {
+        const {
+          cardholderName,
+          cardNumber,
+          expiryDate,
+          securityCode,
+          customerFirstName,
+          customerLastName,
+          address1,
+          city,
+          postalCode,
+          country,
+        } = cardDetails
+        window
+          .sagepayOwnForm({
+            merchantSessionKey: merchantKey.merchantSessionKey,
+          })
+          .tokeniseCardDetails({
+            cardDetails: {
+              cardholderName,
+              cardNumber: unformatCard(cardNumber),
+              expiryDate: unformatCardExpires(expiryDate),
+              securityCode,
+            },
+            onTokenised: async result => {
+              if (result.success) {
+                const payment = await opayoCreateTransactionService({
+                  transactionType: 'Payment',
+                  paymentMethod: {
+                    card: {
+                      merchantSessionKey: merchantKey.merchantSessionKey,
+                      cardIdentifier: result.cardIdentifier,
+                      save: false,
+                    },
+                  },
+                  vendorTxCode: `demotransaction-${Math.floor(Math.random() * 1000)}`,
+                  amount: 1000,
+                  currency: 'GBP',
+                  description: 'Rent payment',
+                  apply3DSecure: 'Disable',
+                  customerFirstName,
+                  customerLastName,
+                  billingAddress: {
+                    address1,
+                    city,
+                    postalCode,
+                    country,
+                  },
+                  entryMethod: 'Ecommerce',
+                })
+
+                console.log('payment result is', payment)
+              } else {
+                console.log(JSON.stringify(result))
+              }
+            },
+          })
+      }}
+    >
+      {() => (
+        <Form className="form">
+          <CardInputGroup hasBillingAddress whiteListTestCards={['4929000000006']} />
+          <Section>
+            <Input id="cardIdentifier" type="hidden" name="cardIdentifier" />
+            <LevelRight>
+              <Button variant="primary" type="submit">
+                Make Payment
+              </Button>
+            </LevelRight>
+          </Section>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+const PaymentPage: React.FC = () => {
+  const { paymentId } = useParams<{ paymentId: string }>()
+  const { data } = useSWR(`${URLS.PAYMENTS}/${paymentId}`)
   const [loading, setLoading] = useState(false)
   const [merchantKey, setMerchantKey] = useState<MerchantKey>()
 
@@ -48,104 +157,9 @@ export const PaymentsPage: React.FC = () => {
           talk to your administrator to set this up for you.
         </Helper>
       )}
-      {merchantKey && (
-        <Formik
-          initialValues={{
-            customerFirstName: '',
-            customerLastName: '',
-            address1: '',
-            city: '',
-            postalCode: '',
-            country: '',
-            cardholderName: '',
-            cardNumber: '',
-            expiryDate: '',
-            securityCode: '',
-            cardIdentifier: '',
-          }}
-          onSubmit={cardDetails => {
-            console.log(cardDetails)
-            const {
-              cardholderName,
-              cardNumber,
-              expiryDate,
-              securityCode,
-              customerFirstName,
-              customerLastName,
-              address1,
-              city,
-              postalCode,
-              country,
-            } = cardDetails
-            window
-              .sagepayOwnForm({
-                merchantSessionKey: merchantKey.merchantSessionKey,
-              })
-              .tokeniseCardDetails({
-                cardDetails: {
-                  cardholderName,
-                  cardNumber: unformatCard(cardNumber),
-                  expiryDate: unformatCardExpires(expiryDate),
-                  securityCode,
-                },
-                onTokenised: async result => {
-                  if (result.success) {
-                    // cardDetails.cardIdentifier = result.cardIdentifier
-
-                    const payment = await opayoCreateTransactionService({
-                      transactionType: 'Payment',
-                      paymentMethod: {
-                        card: {
-                          merchantSessionKey: merchantKey.merchantSessionKey,
-                          cardIdentifier: result.cardIdentifier,
-                          save: false,
-                        },
-                      },
-                      vendorTxCode: `demotransaction-${Math.floor(Math.random() * 1000)}`,
-                      amount: 1000,
-                      currency: 'GBP',
-                      description: 'Rent payment',
-                      apply3DSecure: 'Disable',
-                      customerFirstName,
-                      customerLastName,
-                      billingAddress: {
-                        address1,
-                        city,
-                        postalCode,
-                        country,
-                      },
-                      entryMethod: 'Ecommerce',
-                    })
-
-                    console.log('payment result is', payment)
-                  } else {
-                    console.log(JSON.stringify(result))
-                  }
-                },
-              })
-          }}
-          // validationSchema={Yup.object({
-          //   cardholderName: Yup.string().required('Required'),
-          //   cardNumber: Yup.string().required('Required'),
-          // })}
-        >
-          {() => (
-            <Form className="form">
-              <CardInputGroup hasBillingAddress whiteListTestCards={['4929000000006']} />
-              <Section>
-                <Input id="cardIdentifier" type="hidden" name="cardIdentifier" />
-                <LevelRight>
-                  <Button variant="primary" type="submit">
-                    Make Payment
-                  </Button>
-                </LevelRight>
-              </Section>
-            </Form>
-          )}
-        </Formik>
-      )}
+      {merchantKey && data ? <PaymentForm data={data} merchantKey={merchantKey} /> : <Loader />}
     </>
   )
 }
 
-export default PaymentsPage
+export default PaymentPage
