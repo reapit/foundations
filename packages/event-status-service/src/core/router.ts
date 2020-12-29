82eb15e3ada0ed44e314c45a14a15fbb153e0976
Jonathon Blok <jonathon@jblok.co.uk>
2020-12-29 14:19:24 +0000

    feat: #3144 - scaffold the events-status-service

---
+++ packages/event-status-service/src/core/router.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+// import { AppRequest, AppResponse } from './logger'
+
+const router = Router()
+
+router.get('/', (req: any, res: any) => {
+  console.log(req)
+  return res.send('event-status-service OK')
+})
+router.get('/test', (req: any, res: any) => {
+  console.log(req)
+  res.send('event-status-service test endpoint OK')
+})
+
+export default router
