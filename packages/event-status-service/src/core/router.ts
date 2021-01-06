0490acf609faabdf29f25cb2f39a0bd5cc6a7522
Jonathon Blok <jonathon@jblok.co.uk>
2021-01-06 12:19:56 +0000

    feat: #3144 - add validation to list route

--- packages/event-status-service/src/core/router.ts
+++ packages/event-status-service/src/core/router.ts
@@ -7,12 +7,12 @@ import { createEventStatus } from '../controllers/event-status/create'
 import { updateStatusById } from '../controllers/event-status/update'
 
 import { validateRequest } from '../middlewares/validate-request'
-import { create } from '../validations/event-status'
+import { create, list } from '../validations/event-status'
 
 const router = Router()
 
 router.get('/event-status/:eventId', getStatusById)
-router.get('/event-status', listStatuses)
+router.get('/event-status', checkSchema(list), validateRequest, listStatuses)
 router.post('/event-status', checkSchema(create), validateRequest, createEventStatus)
 router.put('/event-status/:eventId', updateStatusById)
 
--- packages/event-status-service/src/validations/event-status.ts
+++ packages/event-status-service/src/validations/event-status.ts
@@ -40,4 +40,48 @@ const create = {
   },
 } as Schema
 
-export { create }
+const list = {
+  clientCode: {
+    in: ['query'],
+    isString: true,
+    isLength: {
+      errorMessage: 'clientCode should be 3 or 4 characters long',
+      options: {
+        min: 3,
+        max: 4,
+      },
+    },
+  },
+  dateFrom: {
+    in: ['query'],
+    isISO8601: {
+      errorMessage:
+        "dateFrom should be a valid ISO8601 format, e.g. '2020-01-01T21:02:12', or if using a timezone, '2020-01-01T21:02:12-09:00'",
+      options: {
+        strict: true,
+        strictSeparator: true,
+      },
+    },
+  },
+  dateTo: {
+    in: ['query'],
+    isISO8601: {
+      errorMessage:
+        "dateTo should be a valid ISO8601 format, e.g. '2020-01-01T21:02:12', or if using a timezone, '2020-01-01T21:02:12-09:00'",
+      options: {
+        strict: true,
+        strictSeparator: true,
+      },
+    },
+  },
+  status: {
+    optional: true,
+    in: ['query'],
+    isIn: {
+      options: [['outstanding', 'actioned', 'dismissed']],
+      errorMessage: 'status should be one of outstanding, actioned, dismissed',
+    },
+  },
+} as Schema
+
+export { create, list }
