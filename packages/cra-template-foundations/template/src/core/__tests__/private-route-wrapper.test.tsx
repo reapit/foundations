7671b7ab28b180fd458f86b976c510e88c84af99
Will McVay <wmcvay@reapit.com>
2021-03-02 20:06:52 +0000

    feat: #3599 #3543 publish apps to npm

--- packages/cra-template-foundations/template/src/core/__tests__/private-route-wrapper.test.tsx
+++ packages/cra-template-foundations/template/src/core/__tests__/private-route-wrapper.test.tsx
@@ -2,11 +2,9 @@ import * as React from 'react'
 import { shallow } from 'enzyme'
 import { PrivateRouteWrapper } from '../private-route-wrapper'
 
-const locationMock = { pathname: '/test' }
-
 jest.mock('react-router', () => ({
   ...(jest.requireActual('react-router') as Object),
-  useLocation: jest.fn(() => locationMock),
+  useLocation: jest.fn(() => ({ pathname: '/test' })),
 }))
 
 jest.mock('@reapit/connect-session', () => ({
