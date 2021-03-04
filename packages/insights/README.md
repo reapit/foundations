c68c4d9985cf45779b9fd838ec868222b8391b5f
Will McVay <wmcvay@reapit.com>
2021-03-04 14:41:46 +0000

    feat: #3640 add initial scaffold for insights app

---
+++ packages/insights/README.md
@@ -0,0 +1,5 @@
+# Reapit Insights
+
+Simple proxy to Metabase
+
+![lines](./src/tests/badges/badge-lines.svg) ![functions](./src/tests/badges/badge-functions.svg) ![branches](./src/tests/badges/badge-branches.svg) ![statements](./src/tests/badges/badge-statements.svg)
---
+++ packages/insights/jest.config.js
@@ -0,0 +1,27 @@
+const { jestGlobalConfig } = require('@reapit/ts-scripts')
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig')
+
+module.exports = {
+  ...jestGlobalConfig,
+  testPathIgnorePatterns: ['<rootDir>/src/tests/'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>[/\\\\](node_modules|src/types|src/tests|src/scripts|src/__stubs__|dist)[/\\\\]',
+    '.d.ts',
+    'index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 53,
+      lines: 74,
+      statements: 74,
+    },
+  },
+  moduleNameMapper: {
+    ...jestGlobalConfig.moduleNameMapper,
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>/',
+    }),
+  },
+}
