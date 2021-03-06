320f08e15c3e004a6076ebcf8e06ab408f0e9b14
Will McVay <wmcvay@reapit.com>
2021-03-06 17:05:44 +0000

    fix: #3645 fix compile mailer lambda (#3659)
    * fix: #3654 fixes mailer lambda build
    
    * fix: #3654 fixes mailer lambda build
    
    * remove build step in error

--- packages/cognito-custom-mail-lambda/src/mailer/custom-mailer.ts
+++ packages/cognito-custom-mail-lambda/src/mailer/custom-mailer.ts
@@ -1,11 +1,11 @@
 import { CognitoUserPoolTriggerHandler } from 'aws-lambda'
 import { forgotPasswordTemplate, confirmRegistrationTemplate, adminUserInviteTemplate } from './templates/index'
 
-export const customMailer: CognitoUserPoolTriggerHandler = async (event, _context, callback) => {
+export const customMailer: CognitoUserPoolTriggerHandler = (event, _context, callback) => {
   if (event.userPoolId === process.env.COGNITO_USERPOOL_ID && event.triggerSource === 'CustomMessage_ForgotPassword') {
     event.response.emailSubject = 'Reapit Connect - Forgotten Password'
     const resetPasswordUrl = `${process.env.MARKET_PLACE_URL}/reset-password`
-    event.response.emailMessage = await forgotPasswordTemplate({
+    event.response.emailMessage = forgotPasswordTemplate({
       verificationCode: event.request.codeParameter as string,
       userName: event.request.userAttributes.email,
       url: resetPasswordUrl,
@@ -14,7 +14,7 @@ export const customMailer: CognitoUserPoolTriggerHandler = async (event, _contex
   if (event.userPoolId === process.env.COGNITO_USERPOOL_ID && event.triggerSource === 'CustomMessage_SignUp') {
     event.response.emailSubject = 'Welcome to Reapit Connect'
     const confirmRegistrationUrl = `${process.env.MARKET_PLACE_URL}/register/confirm`
-    event.response.emailMessage = await confirmRegistrationTemplate({
+    event.response.emailMessage = confirmRegistrationTemplate({
       userName: event.request.userAttributes.email,
       url: confirmRegistrationUrl,
     })
@@ -22,7 +22,7 @@ export const customMailer: CognitoUserPoolTriggerHandler = async (event, _contex
   if (event.userPoolId === process.env.COGNITO_USERPOOL_ID && event.triggerSource === 'CustomMessage_AdminCreateUser') {
     event.response.emailSubject = 'Welcome to Reapit Connect'
     const confirmRegistrationUrl = `${process.env.MARKET_PLACE_URL}/login`
-    event.response.emailMessage = await adminUserInviteTemplate({
+    event.response.emailMessage = adminUserInviteTemplate({
       userName: event.request.userAttributes.email,
       url: confirmRegistrationUrl,
     })
