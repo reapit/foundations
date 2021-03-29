4ea3d33883745eeb39fa0a827d9bee2e9fa17480
Will McVay <wmcvay@reapit.com>
2021-03-29 17:04:39 +0100

    feat: #3774 #3836 #3775 #3834 #3850 #3833 various billing go live issues (#3856)

--- packages/developer-portal/src/components/ui/developer-edition-modal/developer-edition-content.tsx
+++ packages/developer-portal/src/components/ui/developer-edition-modal/developer-edition-content.tsx
@@ -10,11 +10,16 @@ import {
   SelectOption,
   ModalProps,
   ButtonGroup,
+  Loader,
+  Content,
 } from '@reapit/elements'
 import { FormValues } from './form-fields'
-import pdfLink from '@/assets/files/reapit-foundations-developer-agreement.pdf'
 import { formFields } from './form-fields'
 import validationSchema from './validation-schema'
+import { selectSettingsPageDeveloperInformation, selectSettingsPageIsLoading } from '../../../selector/settings'
+import { selectCurrentMemberData, selectCurrentMemberIsLoading } from '../../../selector/current-member'
+import { useSelector } from 'react-redux'
+import { modalContent } from './modal-content'
 
 const { developerList } = formFields
 
@@ -30,51 +35,78 @@ export const DeveloperEditionContent: React.FC<DeveloperEditionContentProps> = (
   afterClose,
   onFormSubmit,
 }) => {
+  const currentUser = useSelector(selectCurrentMemberData)
+  const currentUserLoading = useSelector(selectCurrentMemberIsLoading)
+  const currentDeveloper = useSelector(selectSettingsPageDeveloperInformation)
+  const currentDeveloperLoading = useSelector(selectSettingsPageIsLoading)
+
   const initialValues: FormValues = {
     developerList: dropdownOptions.map(({ value }) => value),
   }
+  const billingContent =
+    currentUser?.role &&
+    currentDeveloper?.status &&
+    modalContent?.[currentUser.role]?.[currentDeveloper.status]?.content
+
+  const content =
+    currentUserLoading || currentDeveloperLoading ? (
+      <Loader />
+    ) : billingContent ? (
+      <>
+        <Content>{billingContent}</Content>
+        <ButtonGroup hasSpacing isCentered>
+          <Button variant="secondary" onClick={afterClose}>
+            Cancel
+          </Button>
+        </ButtonGroup>
+      </>
+    ) : (
+      <>
+        <SubTitleH6 className="has-text-weight-normal">
+          The Agency Cloud Developer Edition is a licensed product. By confirming the subscription below, a subscription
+          of £300 will automatically be added to your monthly billing. For more information regarding the Developer
+          Edition licence please refer to your Developer Registration{' '}
+          <a href="/api-docs/developer-terms-and-conditions" rel="noreferrer" target="_blank">
+            Terms and Conditions.
+          </a>
+          <br />
+          <br />
+          To proceed, please confirm your subscription below
+        </SubTitleH6>
+        <Formik initialValues={initialValues} onSubmit={onFormSubmit} validationSchema={validationSchema}>
+          <Form className="form">
+            <DropdownSelect
+              mode="multiple"
+              options={dropdownOptions}
+              name={developerList.name}
+              id={developerList.name}
+              placeholder={developerList.placeHolder}
+              fixedPosition
+              hasLabel={false}
+            />
+            <ButtonGroup hasSpacing isCentered>
+              <Button variant="secondary" onClick={afterClose}>
+                Cancel
+              </Button>
+              <Button variant="primary" type="submit" loading={loading}>
+                Confirm Subscription
+              </Button>
+            </ButtonGroup>
+          </Form>
+        </Formik>
+      </>
+    )
+
+  const heading =
+    (currentUser?.role &&
+      currentDeveloper?.status &&
+      modalContent?.[currentUser.role]?.[currentDeveloper.status]?.title) ??
+    'Agency Cloud Developer Edition'
 
   return (
     <>
-      <ModalHeader title="Agency Cloud Developer Edition" />
-      <ModalBody
-        body={
-          <>
-            <SubTitleH6 className="has-text-weight-normal">
-              The Agency Cloud Developer Edition is a licensed product. However, there will be no charge for the licence
-              during the Beta phase. For more information regarding the Developer Edition licence please refer to your
-              Developer Registration&nbsp;
-              <a href={pdfLink} rel="noreferrer" target="_blank">
-                Terms and Conditions
-              </a>
-              <br />
-              <br />
-              To proceed, please confirm your subscription below
-            </SubTitleH6>
-            <Formik initialValues={initialValues} onSubmit={onFormSubmit} validationSchema={validationSchema}>
-              <Form className="form">
-                <DropdownSelect
-                  mode="multiple"
-                  options={dropdownOptions}
-                  name={developerList.name}
-                  id={developerList.name}
-                  placeholder={developerList.placeHolder}
-                  fixedPosition
-                  hasLabel={false}
-                />
-                <ButtonGroup hasSpacing isCentered>
-                  <Button variant="secondary" onClick={afterClose}>
-                    Cancel
-                  </Button>
-                  <Button variant="primary" type="submit" loading={loading}>
-                    Confirm Subscription
-                  </Button>
-                </ButtonGroup>
-              </Form>
-            </Formik>
-          </>
-        }
-      />
+      <ModalHeader title={heading} />
+      <ModalBody body={content} />
     </>
   )
 }
