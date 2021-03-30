c31cd0ae3da2d5443f1b423791bd813912788f55
Will McVay <wmcvay@reapit.com>
2021-03-30 13:56:27 +0100

    fix: api docs and mailer fixes

--- packages/api-docs-lamda/src/index.ts
+++ packages/api-docs-lamda/src/index.ts
@@ -1,22 +1,15 @@
 import axios from 'axios'
+import { createLogger } from '@reapit/node-utils'
 // Path to the hosted docs app
 export const API_DOCS_URL = 'https://foundations-documentation.reapit.cloud'
-// List of domains I allow to load in an iframe
-export const CONTENT_SECURITY_WHITELIST =
-  'http://localhost:8080 https://developers.reapit.cloud https://developers.dev.paas.reapit.cloud'
+
+const logger = createLogger('api-docs-lambda')
 
 export const apiDocsHandler = async () => {
   try {
     const response = await axios.get(API_DOCS_URL)
-    return {
-      statusCode: 200,
-      headers: {
-        ...response.headers,
-        'Content-Security-Policy': `frame-ancestors 'self' ${CONTENT_SECURITY_WHITELIST}`,
-      },
-      body: response.data,
-    }
+    return response.data
   } catch (error) {
-    console.error(error)
+    logger.error(error)
   }
 }
