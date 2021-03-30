fc3698d5b5ad2fd17d423c52d34bf06c17e17f51
Jonathon Blok <jonathon@jblok.co.uk>
2021-03-30 17:03:57 +0100

    add an outline that aligns to the intent

--- packages/elements/src/components-v3/Button/__styles__/index.tsx
+++ packages/elements/src/components-v3/Button/__styles__/index.tsx
@@ -52,26 +52,31 @@ export const ElButton = styled.button`
   &${elIntentPrimary} {
     background-image: linear-gradient(to right, var(--intent-primary), var(--intent-primary));
     color: var(--intent-primary-text);
+    outline-color: var(--intent-primary-dark);
   }
 
   &${elIntentSecondary} {
     background-image: linear-gradient(to right, var(--intent-secondary), var(--intent-secondary));
     color: var(--intent-secondary-text);
+    outline-color: var(--intent-secondary-dark);
   }
 
   &${elIntentCritical} {
     background-image: linear-gradient(to right, var(--intent-critical), var(--intent-critical));
     color: var(--intent-critical-text);
+    outline-color: var(--intent-critical-dark);
   }
 
   &${elIntentSuccess} {
     background-image: linear-gradient(to right, var(--intent-success), var(--intent-success));
     color: var(--intent-success-text);
+    outline-color: var(--intent-success-dark);
   }
 
   &${elIntentDanger} {
     background-image: linear-gradient(to right, var(--intent-danger), var(--intent-danger));
     color: var(--intent-danger-text);
+    outline-color: var(--intent-danger-dark);
   }
 
   &[disabled] {
