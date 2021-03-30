b78d8b3aca0b4ac2e59c8628d8889f13af547977
Will McVay <wmcvay@reapit.com>
2021-03-30 14:21:56 +0100

    fix: cost explorer filter

--- packages/developer-portal/src/services/billing.ts
+++ packages/developer-portal/src/services/billing.ts
@@ -83,6 +83,7 @@ export const fetchBillingsByMonth = async (
     const queryParams = customerId
       ? {
           ...rest,
+          customerId,
           type: 'trafficEvents',
         }
       : rest
