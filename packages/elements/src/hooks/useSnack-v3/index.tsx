f4c4e44e4826570a50bb076e095ef294d43df719
Jonathon Blok <jonathon@jblok.co.uk>
2021-04-13 11:23:20 +0100

    fix lint and typescript issue

--- packages/elements/src/hooks/useSnack-v3/index.tsx
+++ packages/elements/src/hooks/useSnack-v3/index.tsx
@@ -3,7 +3,12 @@ import { createPortal } from 'react-dom'
 import usePortal from '../usePortal-v3'
 import { Snack } from '../../components-v3/Snack'
 
-function useSnack() {
+interface IUseSnack {
+  snackbar: React.ReactPortal
+  addSnack: (text: string) => void
+}
+
+function useSnack(): IUseSnack {
   const SNACK_PORTAL_ID = 'SNACK_PORTAL_ID'
 
   const [snacks, setSnacks] = useState<string[]>([])
@@ -12,7 +17,7 @@ function useSnack() {
   const snackbar = createPortal(
     <div style={{ position: 'fixed', top: '0', right: 0, background: 'green', width: '500px', height: '500px' }}>
       {snacks.map((text) => (
-        <Snack>{text}</Snack>
+        <Snack key={text}>{text}</Snack>
       ))}
     </div>,
     portal,
