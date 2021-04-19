0c4fd24b1d47aa6353cd833d4fcd34a018283060
Jonathon Blok <jonathon@jblok.co.uk>
2021-04-19 17:25:11 +0100

    allow multiple stories in one canvas

--- packages/elements/src/helpers/v3/render-html-markup.tsx
+++ packages/elements/src/helpers/v3/render-html-markup.tsx
@@ -4,14 +4,13 @@ import { Canvas } from '@storybook/addon-docs/blocks'
 
 type RenderHtmlMarkupProps = {
   component: string
-  story: string
+  story: string | string[]
+  label?: string
 }
 
-const storyBlockIdFromId = (storyId: string) => `story--${storyId}`
-
-export const RenderHtmlMarkup = ({ component, story }: RenderHtmlMarkupProps) => {
-  const [domElement, setDomElement] = useState<Element | null>()
+export const RenderHtmlMarkup = ({ component, story, label }: RenderHtmlMarkupProps) => {
   const MAX_ATTEMPTS = 10
+  const [domElements, setDomElements] = useState<(Element | null)[]>([])
   const [attempts, setAttempts] = useState<number>(0)
 
   // also use a ref so that inside the setTimeout callback, `attempts` is the
@@ -19,38 +18,50 @@ export const RenderHtmlMarkup = ({ component, story }: RenderHtmlMarkupProps) =>
   const attemptsRef = useRef(attempts)
   attemptsRef.current = attempts
 
-  const domId = storyBlockIdFromId(toId(component, story))
+  // turn the story prop into a stories array (even if only one story name is supplied)
+  let stories: string[]
+  if (typeof story === 'string') {
+    stories = [story]
+  } else {
+    stories = story
+  }
 
-  const findAndSetDom = () => {
-    const storyDom = document.querySelector(`#${domId}`)
+  const domIds = stories.map((story) => `story--${toId(component, story)}`)
 
-    if (storyDom) {
-      setDomElement(storyDom)
+  const findAndSetDoms = () => {
+    const storyDoms = domIds.map((id) => document.querySelector(`#${id}`)).filter((item) => !!item)
+
+    if (storyDoms && storyDoms.length === domIds.length) {
+      setDomElements(storyDoms)
     } else {
       // attempt to find the element with the ID every 1.5seconds, up to 10
       // times (to wait enough time for the react render to have finished)
       if (attemptsRef.current < MAX_ATTEMPTS) {
         setAttempts(attemptsRef.current + 1)
-        window.setTimeout(findAndSetDom, 1500)
+        window.setTimeout(findAndSetDoms, 1500)
       }
     }
   }
 
   useEffect(() => {
-    findAndSetDom()
+    findAndSetDoms()
   }, [component, story])
 
-  if (domElement) {
-    return (
-      <Canvas mdxSource={domElement?.innerHTML}>
-        <p>HTML of {story}</p>
-      </Canvas>
-    )
-  } else {
-    return (
-      <p>
-        Cannot render HTML code for {component}-{story}
-      </p>
-    )
+  const getLabel = () => {
+    if (!domElements) return `Cannot render HTML code for ${component}-${story}`
+    if (label) return label
+    return `HTML of ${stories.join(', ')}`
   }
+
+  const getMdxSource = () => {
+    if (!domElements) return ''
+    if (domElements.length === 1) return domElements[0]?.innerHTML
+    if (domElements.length > 1) return domElements.reduce((acc, el) => `${acc}\n${el?.innerHTML}`, '')
+  }
+
+  return (
+    <Canvas mdxSource={getMdxSource()}>
+      <p>{getLabel()}</p>
+    </Canvas>
+  )
 }
