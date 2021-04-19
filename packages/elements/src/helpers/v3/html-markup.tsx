6a9638927f98518851d345a69557a39e10e542b6
Jonathon Blok <jonathon@jblok.co.uk>
2021-04-19 09:21:42 +0100

    WIP renderHtml component

---
+++ packages/elements/src/helpers/v3/html-markup.tsx
@@ -0,0 +1,6 @@
+// import * as ReactDOMServer from 'react-dom-server'
+//
+// export const RenderHtmlMarkup = (children) => {
+//   const markup = ReactDOMServer.renderDomToStaticMarkup(children)
+//   console.log(markup)
+// }
