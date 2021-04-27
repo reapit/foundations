1061791020f04e2dcc7f90d9bf0cba1e3715c697
Will McVay <wmcvay@reapit.com>
2021-04-27 13:11:09 +0100

    fix: #4051 office groups fetching fix (#4053)

--- packages/marketplace-management/src/components/ui/offices/__tests__/offices-groups-tab.test.tsx
+++ packages/marketplace-management/src/components/ui/offices/__tests__/offices-groups-tab.test.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import OfficesGroupsTab, { OfficeGroupsContent, onPageChangeHandler, renderResult } from '../offices-groups-tab'
+import OfficesGroupsTab, { onPageChangeHandler } from '../offices-groups-tab'
 import { createBrowserHistory } from 'history'
 import Routes from '@/constants/routes'
-import { data } from '../__stubs__/office-groups'
 
 jest.mock('../../../../core/connect-session')
 jest.mock('react-router', () => ({
@@ -24,22 +23,6 @@ describe('OfficesGroupsTab', () => {
   })
 })
 
-const columns = [
-  { Header: 'Group Name', accessor: 'name' },
-  { Header: 'Office List', accessor: 'officeIds' },
-  { Header: 'Last Updated', accessor: 'description' },
-  { Header: 'Edit' },
-]
-
-describe('OfficeGroupsContent', () => {
-  const onPageChange = jest.fn()
-  it('should match a snapshot', () => {
-    expect(
-      shallow(<OfficeGroupsContent officeGroups={data} columns={columns} onPageChange={onPageChange} />),
-    ).toMatchSnapshot()
-  })
-})
-
 describe('onPageChangeHandler', () => {
   it('should return a function when executing', () => {
     const history = createBrowserHistory()
@@ -49,23 +32,5 @@ describe('onPageChangeHandler', () => {
 
     onPageChangeHandlerFn(2)
     expect(history.push).toHaveBeenCalledWith(`${Routes.OFFICES_GROUPS}?pageNumber=2`)
-  })
-})
-
-describe('renderResult', () => {
-  const columns = [
-    { Header: 'Group Name', accessor: 'name' },
-    { Header: 'Office List', accessor: 'officeIds' },
-    { Header: 'Last Updated' },
-    { Header: 'Edit' },
-  ]
-  it('should match snapshot when no result', () => {
-    const result = renderResult(columns, [])
-    expect(result).toMatchSnapshot()
-  })
-
-  it('should match snapshot when has result data', () => {
-    const result = renderResult(columns, data._embedded)
-    expect(result).toMatchSnapshot()
   })
 })
--- packages/marketplace-management/src/components/ui/offices/office-list-cell.tsx
+++ packages/marketplace-management/src/components/ui/offices/office-list-cell.tsx
@@ -1,27 +1,15 @@
 import React from 'react'
-import useSWR from 'swr'
-import { URLS } from '../../../constants/api'
 import { FadeIn } from '@reapit/elements'
 import { OfficeModel } from '@reapit/foundations-ts-definitions'
 
-export const OfficeName: React.FC<{ id: string; isLast: boolean }> = ({ id, isLast }) => {
-  const { data } = useSWR<OfficeModel>(id && `${URLS.OFFICES}/${id}`)
-  if (!data) return null
-  const { name } = data
-
-  return (
-    <FadeIn>
-      <span>{name ? `${name}${isLast ? '' : ', '}` : ''}</span>
-    </FadeIn>
-  )
-}
-
 const OfficeListCell = ({ cell: { value } }) => {
-  const officeArr = value.split(',')
+  const offices = value ?? []
   return (
     <>
-      {officeArr.map((id: string, index: number) => (
-        <OfficeName id={id.trim()} isLast={index === officeArr.length - 1} key={id} />
+      {offices.map(({ name, id }: OfficeModel) => (
+        <FadeIn key={id}>
+          <span>{`${name} `}</span>
+        </FadeIn>
       ))}
     </>
   )
