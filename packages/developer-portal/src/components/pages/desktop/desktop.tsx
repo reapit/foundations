65cc409d16cdcf397bdfdb8a1793e794786b8073
Will McVay <wmcvay@reapit.com>
2021-05-12 12:12:36 +0100

    feat: #4113 wip

--- packages/developer-portal/src/components/pages/desktop/desktop.tsx
+++ packages/developer-portal/src/components/pages/desktop/desktop.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ErrorBoundary from '@/components/hocs/error-boundary'
-import { H5, Grid, GridItem, Content, Section, Button, H3 } from '@reapit/elements'
+import { H5, Content, Section, Button, H3 } from '@reapit/elements'
 import Routes from '@/constants/routes'
 import DeveloperEditonModal from '@/components/ui/developer-edition-modal'
 import { developerDesktopPricingTile } from './__styles__/pricing-tile'
-import FadeIn from '../../../styles/fade-in'
+import { Grid, Col } from '../../../styles/grid'
+// import FadeIn from '../../../styles/fade-in'
 
 export const handleToggleVisibleModal = (
   setIsDeveloperEditionModalOpen: React.Dispatch<boolean>,
@@ -19,6 +20,14 @@ export const DeveloperDesktopContentPartOne: React.FC = () => {
         leading agency products, including a Sales CRM, Lettings CRM, Client Accounts, Property Management and real-time
         Analytics.
       </p>
+    </Content>
+  )
+}
+
+export const DeveloperDesktopContentPartTwo: React.FC = () => {
+  return (
+    <Content>
+      <H5>About Foundations Desktop API</H5>
       <p>
         Developers that would like to integrate with or extend the functionality of Agency Cloud can use the Foundations
         Desktop API to build web applications for Reapit&#39;s app Marketplace that trigger events in the Agency Cloud
@@ -40,55 +49,64 @@ export const DeveloperDesktopContentPartOne: React.FC = () => {
   )
 }
 
-export const DeveloperDesktopContentPartTwo: React.FC = () => {
+export const DeveloperDesktopContentPartThree: React.FC = () => {
+  return (
+    <Content>
+      <H5>How your app integrates with the Developer Edition of Agency Cloud</H5>
+      video here
+      <p>
+        The Developer Edition of Agency Cloud allows developers using the Desktop API to test their apps within the
+        desktop application using sandbox data.
+      </p>
+    </Content>
+  )
+}
+
+export const DeveloperDesktopContentPartFour: React.FC = () => {
   const [isDeveloperEditionModalOpen, setIsDeveloperEditionModalOpen] = React.useState<boolean>(false)
   return (
     <Content>
-      <Grid>
-        <GridItem>
-          <H5>Developer Edition</H5>
-          <p>
-            The Developer Edition of Agency Cloud allows developers using the Desktop API to test their apps within the
-            desktop application using sandbox data.
-          </p>
-          <p>
-            From 1st April, the Developer Edition of Agency Cloud will no longer be a free subscription. If you had
-            subscribed during the Beta Phase and still wish to continue to use the software, please re subscribe.
-          </p>
-          <p>
-            The application is licenced per user/developer of an organisation and will require a Windows machine to
-            install it.
-          </p>
-        </GridItem>
-        <GridItem>
-          <div className={developerDesktopPricingTile}>
-            <div className="desktop-inner-container">
-              <div className="modal-card-head mb-4">
-                <H5 className="mb-0" isCentered>
-                  Developer Edition
-                </H5>
-              </div>
-              <div className="justify-center items-center is-flex py-2">
-                <span className="desktop-price">£300</span>&nbsp;
-                <span className="desktop-price-period">/ Month</span>
-              </div>
-              <div className="text-center px-2 py-1">Become familiar with our CRM software using sandbox data</div>
-              <div className="text-center px-2 py-1">Test your Marketplace application inside Agency Cloud</div>
-              <div className="text-center px-2 py-1">Understand how customers will interact with your application</div>
-              <Section hasMargin={false}>
-                <Button
-                  type="button"
-                  variant="primary"
-                  fullWidth
-                  onClick={handleToggleVisibleModal(setIsDeveloperEditionModalOpen, true)}
-                >
-                  Subscribe now
-                </Button>
-              </Section>
-            </div>
+      <H5>Developer Edition</H5>
+      <p>
+        The Developer Edition of Agency Cloud allows developers using the Desktop API to test their apps within the
+        desktop application using sandbox data.
+      </p>
+      <p>
+        From 1st April, the Developer Edition of Agency Cloud will no longer be a free subscription. If you had
+        subscribed during the Beta Phase and still wish to continue to use the software, please re subscribe.
+      </p>
+      <p>
+        The application is licenced per user/developer of an organisation and will require a Windows machine to install
+        it.
+      </p>
+
+      <div className={developerDesktopPricingTile}>
+        <div className="desktop-inner-container">
+          <div className="modal-card-head mb-4">
+            <H5 className="mb-0" isCentered>
+              Developer Edition
+            </H5>
           </div>
-        </GridItem>
-      </Grid>
+          <div className="justify-center items-center is-flex py-2">
+            <span className="desktop-price">£300</span>&nbsp;
+            <span className="desktop-price-period">/ Month</span>
+          </div>
+          <div className="text-center px-2 py-1">Become familiar with our CRM software using sandbox data</div>
+          <div className="text-center px-2 py-1">Test your Marketplace application inside Agency Cloud</div>
+          <div className="text-center px-2 py-1">Understand how customers will interact with your application</div>
+          <Section hasMargin={false}>
+            <Button
+              type="button"
+              variant="primary"
+              fullWidth
+              onClick={handleToggleVisibleModal(setIsDeveloperEditionModalOpen, true)}
+            >
+              Subscribe now
+            </Button>
+          </Section>
+        </div>
+      </div>
+
       <DeveloperEditonModal
         visible={isDeveloperEditionModalOpen}
         afterClose={handleToggleVisibleModal(setIsDeveloperEditionModalOpen, false)}
@@ -100,16 +118,22 @@ export const DeveloperDesktopContentPartTwo: React.FC = () => {
 export const DeveloperDesktopPage: React.FC = () => (
   <ErrorBoundary>
     <H3>Desktop</H3>
-    <FadeIn>
-      <Section hasPadding={false}>
+    <Grid>
+      <Col span={8}>
         <DeveloperDesktopContentPartOne />
-      </Section>
-    </FadeIn>
-    <FadeIn>
-      <Section hasPadding={false}>
-        <DeveloperDesktopContentPartTwo />
-      </Section>
-    </FadeIn>
+        <Grid>
+          <Col span={6}>
+            <DeveloperDesktopContentPartTwo />
+          </Col>
+          <Col span={6}>
+            <DeveloperDesktopContentPartThree />
+          </Col>
+        </Grid>
+      </Col>
+      <Col span={4}>
+        <DeveloperDesktopContentPartFour />
+      </Col>
+    </Grid>
   </ErrorBoundary>
 )
 
