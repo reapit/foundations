cbd15d0d6124c7026f5444511b47874d33e7310c
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-05-13 10:15:34 +0100

    refactor: updated tenacy resolvers with wrapper

--- packages/graphql-server/src/resolvers/tenancies/resolvers.ts
+++ packages/graphql-server/src/resolvers/tenancies/resolvers.ts
@@ -1,8 +1,6 @@
 import tenancyServices from './services'
-import { checkPermission } from '../../utils/check-permission'
 import logger from '../../logger'
-import errors from '../../errors'
-import { ServerContext } from '../../utils'
+import { resolverHandler, ServerContext } from '../../utils'
 import {
   GetTenancyByIdArgs,
   QueryGetTenancyByIdReturn,
@@ -24,127 +22,91 @@ import {
   MutationUpdateTenancyCheckReturn,
 } from './tenancies'
 
-export const queryGetTenancyById = (
+export const queryGetTenancyById = resolverHandler<GetTenancyByIdArgs, QueryGetTenancyByIdReturn>((
   _: any,
   args: GetTenancyByIdArgs,
   context: ServerContext,
 ): QueryGetTenancyByIdReturn => {
   const traceId = context.traceId
   logger.info('queryGetTenancyById', { traceId, args })
-  const isPermit = checkPermission(context)
-  if (!isPermit) {
-    return errors.generateAuthenticationError(context.traceId)
-  }
   return tenancyServices.getTenancyById(args, context)
-}
+})
 
-export const queryGetTenancies = (_: any, args: GetTenanciesArgs, context: ServerContext): QueryGetTenanciesReturn => {
+export const queryGetTenancies = resolverHandler<GetTenanciesArgs, QueryGetTenanciesReturn>((_: any, args: GetTenanciesArgs, context: ServerContext): QueryGetTenanciesReturn => {
   const traceId = context.traceId
   logger.info('queryGetTenancies', { traceId, args })
-  const isPermit = checkPermission(context)
-  if (!isPermit) {
-    return errors.generateAuthenticationError(context.traceId)
-  }
   return tenancyServices.getTenancies(args, context)
-}
+})
 
-export const queryGetTenancyCheckById = (
+export const queryGetTenancyCheckById = resolverHandler<GetTenancyCheckByIdArgs, QueryGetTenancyCheckByIdReturn>((
   _: any,
   args: GetTenancyCheckByIdArgs,
   context: ServerContext,
 ): QueryGetTenancyCheckByIdReturn => {
   const traceId = context.traceId
   logger.info('queryGetTenancyCheckById', { traceId, args })
-  const isPermit = checkPermission(context)
-  if (!isPermit) {
-    return errors.generateAuthenticationError(context.traceId)
-  }
   return tenancyServices.getTenancyCheckById(args, context)
-}
+})
 
-export const queryGetTenancyChecks = (
+export const queryGetTenancyChecks = resolverHandler<GetTenancyChecksArgs, QueryGetTenancyChecksReturn>((
   _: any,
   args: GetTenancyChecksArgs,
   context: ServerContext,
 ): QueryGetTenancyChecksReturn => {
   const traceId = context.traceId
   logger.info('queryGetTenancyChecks', { traceId, args })
-  const isPermit = checkPermission(context)
-  if (!isPermit) {
-    return errors.generateAuthenticationError(context.traceId)
-  }
   return tenancyServices.getTenancyChecks(args, context)
-}
+})
 
-export const queryGetTenancyRelationships = (
+export const queryGetTenancyRelationships = resolverHandler<GetTenancyRelationshipsArgs, QueryGetTenancyRelationshipsReturn>((
   _: any,
   args: GetTenancyRelationshipsArgs,
   context: ServerContext,
 ): QueryGetTenancyRelationshipsReturn => {
   const traceId = context.traceId
   logger.info('queryGetTenancyRelationships', { traceId, args })
-  const isPermit = checkPermission(context)
-  if (!isPermit) {
-    return errors.generateAuthenticationError(context.traceId)
-  }
   return tenancyServices.getTenancyRelationships(args, context)
-}
+})
 
-export const mutationCreateTenancy = (
+export const mutationCreateTenancy = resolverHandler<CreateTenancyArgs, MutationCreateTenancyReturn>((
   _: any,
   args: CreateTenancyArgs,
   context: ServerContext,
 ): MutationCreateTenancyReturn => {
   const traceId = context.traceId
   logger.info('mutationCreateTenancy', { traceId, args })
-  const isPermit = checkPermission(context)
-  if (!isPermit) {
-    return errors.generateAuthenticationError(context.traceId)
-  }
   return tenancyServices.createTenancy(args, context)
-}
+})
 
-export const mutationCreateTenancyCheck = (
+export const mutationCreateTenancyCheck = resolverHandler<CreateTenancyCheckArgs, MutationCreateTenancyReturn>((
   _: any,
   args: CreateTenancyCheckArgs,
   context: ServerContext,
 ): MutationCreateTenancyCheckReturn => {
   const traceId = context.traceId
   logger.info('mutationCreateTenancyCheck', { traceId, args })
-  const isPermit = checkPermission(context)
-  if (!isPermit) {
-    return errors.generateAuthenticationError(context.traceId)
-  }
   return tenancyServices.createTenancyCheck(args, context)
-}
+})
 
-export const mutationDeleteTenancyCheck = (
+export const mutationDeleteTenancyCheck = resolverHandler<DeleteTenancyCheckArgs, MutationDeleteTenancyCheckReturn>((
   _: any,
   args: DeleteTenancyCheckArgs,
   context: ServerContext,
 ): MutationDeleteTenancyCheckReturn => {
   const traceId = context.traceId
   logger.info('mutationDeleteTenancyCheck', { traceId, args })
-  const isPermit = checkPermission(context)
-  if (!isPermit) {
-    return errors.generateAuthenticationError(context.traceId)
-  }
   return tenancyServices.deleteTenancyCheck(args, context)
-}
+})
 
-export const mutationUpdateTenancyCheck = (
+export const mutationUpdateTenancyCheck = resolverHandler<UpdateTenancyCheckArgs, MutationUpdateTenancyCheckReturn>((
   _: any,
   args: UpdateTenancyCheckArgs,
   context: ServerContext,
 ): MutationUpdateTenancyCheckReturn => {
   const traceId = context.traceId
   logger.info('mutationUpdateTenancyCheck', { traceId, args })
-  const isPermit = checkPermission(context)
-  if (!isPermit) {
-    return errors.generateAuthenticationError(context.traceId)
-  }
   return tenancyServices.updateTenancyCheck(args, context)
-}
+})
 
 export default {
   Query: {
