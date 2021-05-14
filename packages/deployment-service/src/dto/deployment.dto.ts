85b30a0a0bf51ea2537b20836e10ca38a81c416d
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-05-14 17:48:22 +0100

    feat: begining implementation of handler functions

--- packages/deployment-service/src/dto/deployment.dto.ts
+++ packages/deployment-service/src/dto/deployment.dto.ts
@@ -2,5 +2,5 @@ import {IsString} from 'class-validator'
 
 export class DeploymentDto {
   @IsString()
-  name: string;
+  name: string
 }
--- packages/deployment-service/src/services/deployment.ts
+++ packages/deployment-service/src/services/deployment.ts
@@ -5,6 +5,28 @@ import { db } from './../core'
 export const createDeploymentModel = (dto: DeploymentDto): Promise<DeploymentModel> => {
   return db.put({
     ...dto,
+    create: new Date().toISOString(),
+    apiKey: '', // TODO solve api key, think every propert on model with key value (or is that GSI I'm thinking of?)
+  })
+}
+
+export const updateDeploymentModel = (model: DeploymentModel, dto: DeploymentDto): Promise<DeploymentModel> => {
+  return db.put({
+    ...model,
+    ...dto,
+    modified: new Date().toISOString(),
     apiKey: '', // TODO solve api key
   })
+}
+
+export const deleteDeploymentModel = async (model: DeploymentModel): Promise<void> => {
+  await db.delete(model)
+}
+
+export const getByKey = (key: string): Promise<DeploymentModel | undefined> => {
+  return db.get(key)
+}
+
+export const batchGet = async (organisationId: string): Promise<DeploymentModel[]> => {
+  return db.batchGet() // TODO get by organisation
 }
