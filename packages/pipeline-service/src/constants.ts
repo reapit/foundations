b4f4af79d811f3aa2ae91395dd647660d3b13c8c
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-05-18 13:10:01 +0100

    chore: remove dead enum

--- packages/pipeline-service/src/constants.ts
+++ packages/pipeline-service/src/constants.ts
@@ -2,11 +2,3 @@ export enum TABLE_NAMES {
   PIPELINE = 'Cloud_Pipeline_Service',
   TASK = 'Cloud_Task_Service',
 }
-
-export enum Status {
-  FAILED = 'failed',
-  SUCCESS = 'success',
-  CANCELED = 'canceled',
-  PENDING = 'pending',
-  RUNNING = 'running',
-}
