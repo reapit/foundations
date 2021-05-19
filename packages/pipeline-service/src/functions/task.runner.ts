52f2ea112cd51a1e17e3249f56160da5d9a95171
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-05-19 11:53:01 +0100

    feat: added basic executable functions

--- packages/pipeline-service/src/functions/task.runner.ts
+++ packages/pipeline-service/src/functions/task.runner.ts
@@ -18,16 +18,16 @@ const nextTask = (pipeline: PipelineModel, rightSibling?: string): TaskModel | u
   return rightSibling ? pipeline.tasks?.find((task) => task.id === rightSibling) : undefined
 }
 
-type TaskExecutionType = {
+type TaskExecutionContextType = {
   currentTask?: TaskModel
   pipeline: PipelineModel
 }
 /**
  * Task runner executable and pipeline
  */
-export const taskRunner = SQSHandler<TaskExecutionType>(sqs)(
-  async (execution: TaskExecutionType): Promise<SQSHandleActions> => {
-    console.log('payload', execution)
+export const taskRunner = SQSHandler<TaskExecutionContextType>(sqs)(
+  async (executionContext: TaskExecutionContextType): Promise<SQSHandleActions> => {
+    console.log('payload', executionContext)
 
     // TODO check pipeline is finished
     // TODO check if pipeline is canceled
@@ -35,50 +35,57 @@ export const taskRunner = SQSHandler<TaskExecutionType>(sqs)(
     // TODO update task on result
     // TODO start next task || send complete
 
-    const pipeline = await services.findById(execution.pipeline.id as string)
-    execution.pipeline = pipeline
+    const pipeline = await services.findById(executionContext.pipeline.id as string)
+    executionContext.pipeline = pipeline
 
     // check pipeline is canceled to cancel task
-    if (execution.pipeline.buildStatus === DeploymentStatus.CANCELED) {
+    if (executionContext.pipeline.buildStatus === DeploymentStatus.CANCELED) {
       return Promise.resolve(SQSHandleActions.DELETE)
     }
 
     // Complete task
-    if (!execution.currentTask) {
-      await pipelineStatusUpdate(execution.pipeline, DeploymentStatus.SUCCESS)
+    if (!executionContext.currentTask) {
+      await pipelineStatusUpdate(executionContext.pipeline, DeploymentStatus.SUCCESS)
       return Promise.resolve(SQSHandleActions.DELETE)
     }
 
     // Set pipeline and task as running
-    execution.currentTask.status = DeploymentStatus.RUNNING
-    await Promise.all([updateTasks([execution.currentTask]), pipelineStatusUpdate(pipeline, DeploymentStatus.RUNNING)])
+    executionContext.currentTask.status = DeploymentStatus.RUNNING
+    await Promise.all([
+      updateTasks([executionContext.currentTask]),
+      pipelineStatusUpdate(pipeline, DeploymentStatus.RUNNING),
+    ])
 
     // this will be the result of running the executable task
+    // TODO add executable runner result and returns etc
     const executableResult = true
 
     // execution failure
     if (!executableResult) {
-      execution.currentTask.status = DeploymentStatus.FAILED
+      executionContext.currentTask.status = DeploymentStatus.FAILED
 
-      const tasksToUpdate = [execution.currentTask]
+      const tasksToUpdate = [executionContext.currentTask]
 
-      const next = nextTask(pipeline, execution.currentTask.rightSibling)
+      const next = nextTask(pipeline, executionContext.currentTask.rightSibling)
       if (next) {
         next.status = DeploymentStatus.CANCELED
         tasksToUpdate.push(next)
       }
 
-      await Promise.all([updateTasks(tasksToUpdate), pipelineStatusUpdate(execution.pipeline, DeploymentStatus.FAILED)])
+      await Promise.all([
+        updateTasks(tasksToUpdate),
+        pipelineStatusUpdate(executionContext.pipeline, DeploymentStatus.FAILED),
+      ])
       return Promise.resolve(SQSHandleActions.DELETE)
     }
 
-    const next = nextTask(pipeline, execution.currentTask.rightSibling)
-    execution.currentTask.status = DeploymentStatus.SUCCESS
+    const next = nextTask(pipeline, executionContext.currentTask.rightSibling)
+    executionContext.currentTask.status = DeploymentStatus.SUCCESS
 
-    await updateTasks([execution.currentTask])
+    await updateTasks([executionContext.currentTask])
 
-    const nextExecution: TaskExecutionType = {
-      pipeline: execution.pipeline,
+    const nextExecution: TaskExecutionContextType = {
+      pipeline: executionContext.pipeline,
       currentTask: next,
     }
 
