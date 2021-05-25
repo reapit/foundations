f8c4bbe51d85ab89f1ad096a0e12b5add129f937
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-05-25 14:02:53 +0100

    feat: added api key authorise func

--- packages/utils/src/node/authorisation/authorised.ts
+++ packages/utils/src/node/authorisation/authorised.ts
@@ -5,3 +5,9 @@ export const authorised = (event: { headers: { [s: string]: any } }): void | nev
     throw new UnauthorizedException()
   }
 }
+
+export const authoriseApiKey = (event: { headers: { [s: string]: any } }): void | never => {
+  if (!event.headers || !event.headers['X-Api-Key']) {
+    throw new UnauthorizedException()
+  }
+}
