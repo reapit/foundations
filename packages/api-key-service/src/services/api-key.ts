9c4d0412421417d4a19b49ba4c15ca274102df87
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-05-26 17:30:53 +0100

    feat: added getBatch service for api key model

--- packages/api-key-service/src/services/api-key.ts
+++ packages/api-key-service/src/services/api-key.ts
@@ -1,5 +1,7 @@
 import { ApiKeyModel } from '@/models'
 import { db } from '@/core'
+import { LoginIdentity } from '@reapit/connect-session'
+import { QueryIterator } from '@aws/dynamodb-data-mapper'
 
 export const createApiKey = (apiKey: Partial<ApiKeyModel>): Promise<ApiKeyModel> => {
   return db.put(Object.assign(new ApiKeyModel(), { apiKey }))
@@ -16,4 +18,29 @@ export const updateApiKey = (apiKey: ApiKeyModel, dto: Partial<ApiKeyModel>): Pr
 
 export const getApiKey = (apiKey: string): Promise<ApiKeyModel | undefined> => {
   return db.get(Object.assign(new ApiKeyModel(), { apiKey }))
+}
+
+export const batchGetApiKeys = async (
+  customer: LoginIdentity,
+  startKey?: { [s: string]: string },
+): Promise<[QueryIterator<ApiKeyModel>, { count: number; nextCursor: string }]> => {
+  const dynamoResponse = await db.query(
+    ApiKeyModel,
+    {
+      organisationId: customer.orgId ? customer.orgId : undefined,
+      developerId: customer.orgId ? undefined : customer.developerId,
+    },
+    {
+      limit: 10,
+      startKey,
+    },
+  )
+
+  return [
+    dynamoResponse,
+    {
+      count: dynamoResponse.count,
+      nextCursor: '',
+    },
+  ]
 }
