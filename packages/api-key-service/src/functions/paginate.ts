a080ccc529ac86d5a3517a2bc479005845724c33
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-05-26 17:31:06 +0100

    feat: added pagination function

---
+++ packages/api-key-service/src/functions/paginate.ts
@@ -0,0 +1,43 @@
+import { ApiKeyModel } from '@/models'
+import { httpHandler, UnauthorizedException } from '@homeservenow/serverless-aws-handler'
+import { batchGetApiKeys } from './../services'
+import { connectSessionVerifyDecodeIdToken, LoginIdentity } from '@reapit/connect-session'
+
+type Pagintation<T> = {
+  items: T[]
+  meta: {
+    count: number
+    nextCursor: string
+  }
+}
+
+export const paginateApiKeys = httpHandler<void, Pagintation<ApiKeyModel>>({
+  handler: async ({ event }): Promise<Pagintation<ApiKeyModel>> => {
+    let customer: LoginIdentity | undefined
+
+    try {
+      customer = await connectSessionVerifyDecodeIdToken(
+        event.headers['reapit-connect-token'] as string,
+        process.env.CONNECT_USER_POOL as string,
+      )
+    } catch (e) {
+      throw new UnauthorizedException(e.message)
+    }
+
+    const [items, meta] = await batchGetApiKeys(
+      customer as LoginIdentity,
+      event?.queryStringParameters?.nextCursor ? { id: event?.queryStringParameters?.nextCursor } : undefined,
+    )
+
+    const pagination: Pagintation<ApiKeyModel> = {
+      items: [],
+      meta,
+    }
+
+    for await (const apiKey of items) {
+      pagination.items.push(apiKey)
+    }
+
+    return pagination
+  },
+})
