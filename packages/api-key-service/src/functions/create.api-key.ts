18f5f965b39d16f5c936b24a01da522483c08bdb
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-05-27 15:09:08 +0100

    fix: input doesn't reslove promise

--- packages/api-key-service/src/functions/create.api-key.ts
+++ packages/api-key-service/src/functions/create.api-key.ts
@@ -7,38 +7,32 @@ import { createApiKey as create } from '@/services'
 import { connectSessionVerifyDecodeIdToken, LoginIdentity } from '@reapit/connect-session'
 
 export const createApiKey = httpHandler<ApiKeyDto, ApiKeyModel>({
-  serialise: {
-    input: async (event): Promise<ApiKeyDto> => {
-      let customer: LoginIdentity | undefined
+  handler: async ({ body, event }): Promise<ApiKeyModel> => {
+    let customer: LoginIdentity | undefined
 
-      try {
-        customer = await connectSessionVerifyDecodeIdToken(
-          event.headers?.Authorization as string,
-          process.env.CONNECT_USER_POOL as string,
-        )
-      } catch (e) {
-        throw new UnauthorizedException(e.message)
-      }
+    try {
+      customer = await connectSessionVerifyDecodeIdToken(
+        event.headers?.Authorization as string,
+        process.env.CONNECT_USER_POOL as string,
+      )
+    } catch (e) {
+      throw new UnauthorizedException(e.message)
+    }
 
-      return event.body
-        ? plainToClass(ApiKeyDto, {
-            ...JSON.parse(event.body),
-            organisationId: customer?.orgId,
-            developerId: customer?.developerId,
-          })
-        : new ApiKeyDto()
-    },
-  },
-  validator: async (dto: ApiKeyDto): Promise<ApiKeyDto> => {
+    const dto = event.body
+      ? plainToClass(ApiKeyDto, {
+          ...body,
+          organisationId: customer?.orgId,
+          developerId: customer?.developerId,
+        })
+      : new ApiKeyDto()
+
     const errors = await validate(dto)
 
     if (errors.length >= 1) {
       throw new ValidationException(errors as any)
     }
 
-    return dto
-  },
-  handler: async ({ body }): Promise<ApiKeyModel> => {
-    return create(body)
+    return create(dto)
   },
 })
