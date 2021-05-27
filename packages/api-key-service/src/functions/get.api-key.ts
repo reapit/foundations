4f77e3de4f8ae37e1efaf7c09a8c9543d27980d2
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-05-27 12:26:44 +0100

    fix: corrected auth header key

--- packages/api-key-service/src/functions/get.api-key.ts
+++ packages/api-key-service/src/functions/get.api-key.ts
@@ -1,21 +1,15 @@
 import { ApiKeyModel } from '@/models'
 import { httpHandler, UnauthorizedException } from '@homeservenow/serverless-aws-handler'
 import { connectSessionVerifyDecodeIdToken, LoginIdentity } from '@reapit/connect-session'
-import { authorised } from '@reapit/node-utils'
 import { getApiKey as get } from './../services'
 
 export const getApiKey = httpHandler<void, ApiKeyModel>({
-  serialise: {
-    input: async (event): Promise<void> => {
-      authorised(event)
-    },
-  },
   handler: async ({ event }) => {
     let customer: LoginIdentity | undefined
 
     try {
       customer = await connectSessionVerifyDecodeIdToken(
-        event.headers['reapit-connect-token'] as string,
+        event.headers['authorization'] as string,
         process.env.CONNECT_USER_POOL as string,
       )
     } catch (e) {
