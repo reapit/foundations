a3c82a34e2340f31cf7e15e3dabcf7892a669612
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-05-28 10:42:23 +0100

    feat: added mock for new connect with public keys

--- packages/connect-session/src/utils/__mocks__/verify-decode-id-token.ts
+++ packages/connect-session/src/utils/__mocks__/verify-decode-id-token.ts
@@ -1,3 +1,5 @@
 import { mockLoginIdentity } from '../../__mocks__/session'
 
 export const connectSessionVerifyDecodeIdToken = () => new Promise((resolve) => resolve(mockLoginIdentity))
+
+export const connectSessionVerifyDecodeIdTokenWithPublicKeys = () => new Promise((resolve) => resolve(mockLoginIdentity))
