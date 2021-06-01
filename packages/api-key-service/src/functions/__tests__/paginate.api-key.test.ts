e0c8699eb278e2ce2c57cb91e34fb3e51ef82a48
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-01 08:52:59 +0100

    fix: test authorisation

--- packages/api-key-service/src/functions/__tests__/paginate.api-key.test.ts
+++ packages/api-key-service/src/functions/__tests__/paginate.api-key.test.ts
@@ -8,7 +8,7 @@ const mockDeveloperId = uuid()
 jest.mock('../../core/db')
 jest.mock('@reapit/connect-session', () => ({
   connectSessionVerifyDecodeIdTokenWithPublicKeys: jest.fn((header) => {
-    return header ? { mockDeveloperId } : undefined
+    return header ? { developerId: mockDeveloperId } : undefined
   }),
 }))
 
@@ -42,6 +42,7 @@ describe('Paginate ApiKey', () => {
         {},
         {
           Authorization: '1234',
+          'Content-Type': 'application/json',
         },
       ),
       {} as Context,
