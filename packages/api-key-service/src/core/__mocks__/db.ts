7418efa7b0c47b34584e627cdf305675a6387cbb
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-01 08:51:39 +0100

    feat: improved mock returns

--- packages/api-key-service/src/core/__mocks__/db.ts
+++ packages/api-key-service/src/core/__mocks__/db.ts
@@ -1,5 +1,12 @@
+import { v4 as uuid } from 'uuid'
+
 export const db = {
-  put: jest.fn(),
+  put: jest.fn((ob) => ({
+    ...ob,
+    id: uuid(),
+    apiKey: uuid(),
+    keyCreatedAt: new Date().toISOString(),
+  })),
   get: jest.fn(),
   getBatch: jest.fn(),
   delete: jest.fn(),
