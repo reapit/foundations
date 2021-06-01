3f9612d48a6d7e69ba5f7b0a22cda858080c36ac
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-01 12:24:27 +0100

    feat: added update test file

---
+++ packages/api-key-service/src/functions/__tests__/update.api-key.test.ts
@@ -0,0 +1,90 @@
+import { v4 as uuid } from 'uuid'
+
+const mockDeveloperId = uuid()
+
+jest.mock('../../core/db')
+jest.mock('@reapit/connect-session', () => ({
+  connectSessionVerifyDecodeIdTokenWithPublicKeys: jest.fn((header) => {
+    return header ? { developerId: mockDeveloperId } : undefined
+  }),
+}))
+
+import { updateApiKey } from './../'
+import { HttpStatusCode } from '@homeservenow/serverless-aws-handler'
+import { Context } from 'aws-lambda'
+import { mockRequestHandlerContext } from '../tests/mock.hander.context'
+
+describe('Update ApiKey', () => {
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
+
+  it('Can result in unauthorised', async () => {
+    const result = await updateApiKey(mockRequestHandlerContext({}), {} as Context)
+
+    expect(result.statusCode).toBe(HttpStatusCode.UNAUTHORIZED)
+  })
+
+  it('Can result in validation errors', async () => {
+    const result = await updateApiKey(
+      mockRequestHandlerContext(
+        {},
+        {
+          Authorization: '1234',
+          'Content-Type': 'application/json',
+        },
+        {
+          id: '1234',
+        }
+      ),
+      {} as Context,
+    )
+
+    const body = JSON.parse(result.body)
+
+    expect(result.statusCode).toBe(HttpStatusCode.BAD_REQUEST)
+    expect(body).toHaveProperty('data')
+    expect(body.data.length).toBe(2)
+  })
+
+  it('Can result in not found', async () => {
+    const result = await updateApiKey(mockRequestHandlerContext({}, {
+      Authoriztion: '1234',
+      'Content-Type': 'application/json',
+    }, {
+      id: '345678',
+    }), {} as Context);
+
+    expect(result.statusCode).toBe(HttpStatusCode.NOT_FOUND)
+  })
+
+  it('Can result in update of api-key', async () => {
+    const result = await updateApiKey(
+      mockRequestHandlerContext(
+        {
+          keyExpiresAt: '2021-06-21T12:12:12',
+          name: 'changed name',
+          entityType: 'deployment',
+        },
+        {
+          Authorization: '1234',
+          'Content-Type': 'application/json',
+        },
+        {
+          id: '1234',
+        }
+      ),
+      {} as Context,
+    )
+
+    const body = JSON.parse(result.body)
+
+    expect(result.statusCode).toBe(HttpStatusCode.OK)
+    expect(body).toHaveProperty('apiKey')
+    expect(body).toHaveProperty('id')
+    expect(body).toHaveProperty('entityType')
+    expect(body).toHaveProperty('keyCreatedAt')
+    expect(body).toHaveProperty('developerId')
+    expect(body.name).toBe('changed name')
+  })
+})
