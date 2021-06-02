4cc578825652f385b91e3fc3ca9de873f265e335
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-06-02 10:09:44 +0100

    fix: added config to deployment webpack conf

--- packages/deployment-service/webpack.config.js
+++ packages/deployment-service/webpack.config.js
@@ -1,7 +1,12 @@
 const slsw = require('serverless-webpack')
-const { webpackConfigNode } = require('@reapit/ts-scripts')
+const { webpackConfigNode, PATHS } = require('@reapit/ts-scripts')
+const CopyPlugin = require('copy-webpack-plugin')
 
 webpackConfigNode.entry = slsw.lib.entries
 webpackConfigNode.optimization.minimize = slsw.lib.webpack.isLocal ? false : true
+
+webpackConfigNode.plugins.push(new CopyPlugin({ patterns: [{ from: 'config.json', to: PATHS.output }] }))
+webpackConfigNode.plugins.push(new CopyPlugin({ patterns: [{ from: 'public-keys.json', to: PATHS.output }] }))
+
 
 module.exports = webpackConfigNode
