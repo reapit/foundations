dd4f1fdf0e100e981a7003ce27999665392e57bf
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-06-03 17:27:10 +0100

    fix: linting with tests

--- packages/api-key-verify/src/__tests__/api-key.test.ts
+++ packages/api-key-verify/src/__tests__/api-key.test.ts
@@ -11,54 +11,53 @@ const MIXED_API_KEY = 'mixed-api-key'
 describe('ApiKey', () => {
   beforeAll(() => {
     // @ts-ignore
-    jest.spyOn(DataMapper.prototype, 'query').mockImplementation((
-      valueConstructor: any,
-      keyCondition: any,
-      options?: any,
-  ) => {
-      // @ts-ignore
-      const apiKeyHeader = (keyCondition as {[s: string]: any}).apiKey as string
+    jest
+      .spyOn(DataMapper.prototype, 'query')
+        // @ts-ignore
+      .mockImplementation((valueConstructor: any, keyCondition: any) => {
+        const apiKeyHeader = (keyCondition as { [s: string]: any }).apiKey as string
 
-      const createApiKeyModel = (apiKey: string, keyExpiresAt: Date): ApiKeyModel => {
-        const model = new ApiKeyModel()
-        model.apiKey = apiKey
-        model.keyExpiresAt = keyExpiresAt.toISOString()
+        const createApiKeyModel = (apiKey: string, keyExpiresAt: Date): ApiKeyModel => {
+          const model = new ApiKeyModel()
+          model.apiKey = apiKey
+          model.keyExpiresAt = keyExpiresAt.toISOString()
 
-        return model
-      }
-
-      const mockAsyncIterator = ([first, second, third]: [any, any, any] | [any]): QueryIterator<StringToAnyObjectMap> => {
-        const asyncIteratorMock = new Object()
-
-        asyncIteratorMock[Symbol.asyncIterator] = async function*() {
-          yield first
-          yield second
-          yield third
+          return model
         }
 
-        return asyncIteratorMock as QueryIterator<any>
-      }
+        const mockAsyncIterator = ([first, second, third]:
+          | [any, any, any]
+          | [any]): QueryIterator<StringToAnyObjectMap> => {
+          const asyncIteratorMock = new Object()
 
-      const expires = new Date()
-      if (apiKeyHeader === SUCCESS_API_KEY) {
-        expires.setDate(expires.getDate() + 3)
-        return mockAsyncIterator([createApiKeyModel(SUCCESS_API_KEY, expires)])
-      } else if (apiKeyHeader === EXPIRED_API_KEY) {
-        expires.setDate(expires.getDate() - 3)
-        return mockAsyncIterator([createApiKeyModel(EXPIRED_API_KEY, expires)])
-      } else if (apiKeyHeader === MIXED_API_KEY) {
-        return mockAsyncIterator([
-          createApiKeyModel(MIXED_API_KEY, expires),
-          createApiKeyModel(MIXED_API_KEY, expires),
-          createApiKeyModel(MIXED_API_KEY, expires),
-        ])
-      }
+          asyncIteratorMock[Symbol.asyncIterator] = async function* () {
+            yield first
+            yield second
+            yield third
+          }
 
-      return {
-        [Symbol.asyncIterator]: async function*() {
-        } as any,
-      } as QueryIterator<any>
-    })
+          return asyncIteratorMock as QueryIterator<any>
+        }
+
+        const expires = new Date()
+        if (apiKeyHeader === SUCCESS_API_KEY) {
+          expires.setDate(expires.getDate() + 3)
+          return mockAsyncIterator([createApiKeyModel(SUCCESS_API_KEY, expires)])
+        } else if (apiKeyHeader === EXPIRED_API_KEY) {
+          expires.setDate(expires.getDate() - 3)
+          return mockAsyncIterator([createApiKeyModel(EXPIRED_API_KEY, expires)])
+        } else if (apiKeyHeader === MIXED_API_KEY) {
+          return mockAsyncIterator([
+            createApiKeyModel(MIXED_API_KEY, expires),
+            createApiKeyModel(MIXED_API_KEY, expires),
+            createApiKeyModel(MIXED_API_KEY, expires),
+          ])
+        }
+
+        return {
+          [Symbol.asyncIterator]: async function* () {} as any,
+        } as QueryIterator<any>
+      })
   })
 
   afterAll(() => {
@@ -66,29 +65,28 @@ describe('ApiKey', () => {
   })
 
   describe('getApiKey', () => {
-
     it('Can get apiKey', async () => {
-      const result = await getApiKey({region: 'eu-west-2'})(SUCCESS_API_KEY)
+      const result = await getApiKey({ region: 'eu-west-2' })(SUCCESS_API_KEY)
 
       expect(result?.apiKey).toBe(SUCCESS_API_KEY)
       expect(new Date(result?.keyExpiresAt as string) > new Date()).toBeTruthy()
     })
 
     it('Can get expired apiKey', async () => {
-      const result = await getApiKey({region: 'eu-west-2'})(EXPIRED_API_KEY)
+      const result = await getApiKey({ region: 'eu-west-2' })(EXPIRED_API_KEY)
 
       expect(result?.apiKey).toBe(EXPIRED_API_KEY)
       expect(new Date(result?.keyExpiresAt as string) < new Date()).toBeTruthy()
     })
 
     it('Returns undefined on no apiKey', async () => {
-      const result = await getApiKey({region: 'eu-west-2'})(NOT_FOUND_API_KEY)
+      const result = await getApiKey({ region: 'eu-west-2' })(NOT_FOUND_API_KEY)
 
       expect(typeof result).toBe('undefined')
     })
 
     it('Can get latest apiKey', async () => {
-      const result = await getApiKey({region: 'eu-west-2'})(MIXED_API_KEY)
+      const result = await getApiKey({ region: 'eu-west-2' })(MIXED_API_KEY)
 
       expect(result?.apiKey).toBe(MIXED_API_KEY)
       // TODO add expect for the most recent
@@ -98,19 +96,18 @@ describe('ApiKey', () => {
   describe('resolveApiKey', () => {
     it('Throws not found exception on no apiKey', async () => {
       try {
-        await resolveApiKey({region: 'eu-west-2'})(NOT_FOUND_API_KEY)
+        await resolveApiKey({ region: 'eu-west-2' })(NOT_FOUND_API_KEY)
         expect(true).toBeFalsy()
-
-      }catch (e) {
+      } catch (e) {
         expect(e).toBeInstanceOf(ApiKeyNotFoundException)
       }
     })
 
     it('Throws expired exception on expired apiKey', async () => {
       try {
-        await resolveApiKey({region: 'eu-west-2'})(EXPIRED_API_KEY)
+        await resolveApiKey({ region: 'eu-west-2' })(EXPIRED_API_KEY)
         expect(true).toBeFalsy()
-      }catch (e) {
+      } catch (e) {
         expect(e).toBeInstanceOf(ApiKeyExpiredException)
       }
     })
