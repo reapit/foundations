cff0e61845dbf7f91c4643448d6e04e9a3cb5fb9
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-08 16:25:17 +0100

    refactor: restructured argument decorators

--- packages/cli/src/decorators/index.ts
+++ packages/cli/src/decorators/index.ts
@@ -1,27 +1,66 @@
 import 'reflect-metadata'
 export const COMMAND_OPTIONS = 'COMMAND_OPTIONS'
-export const PROPERTY_NAME = 'PROPERTY_NAME'
-export const OPTION_NAME = 'OPTION_NAME'
+export const ARGUMENT_OPTIONS = 'PARAMETER_NAME'
 
 export type CommandOptions = {
   name: string
   description: string
 }
 
+export interface ArgsType {
+  description?: string
+  name: string
+  type: 'parameter' | 'option'
+  shortName?: string
+}
+
+export type ParameterType = ArgsType & {
+  default?: any
+  type: 'parameter'
+}
+
+
+export type OptionType = ArgsType & {
+  default?: any
+  shortName: string
+  type: 'option'
+}
+
+export interface ArgsInputInterface {
+  default?: any
+  description?: string
+  name: string
+}
+
+export interface ParameterInputInterface extends ArgsInputInterface {
+}
+
+export interface OptionInputInterface extends ArgsInputInterface {
+  shortName?: string
+}
+
 export const Command = (options: CommandOptions | { default: true }): ClassDecorator => (target) =>
   Reflect.defineMetadata(COMMAND_OPTIONS, options, target)
 
-export type PropertyOptions = {
-  default?: any
+
+export const Param = (options: ParameterInputInterface): ParameterDecorator => (target, propertyKey, parameterIndex) => {
+  const params = Reflect.getOwnMetadata(ARGUMENT_OPTIONS, target.constructor) || []
+
+  params[parameterIndex] = {
+    ...options,
+    type: 'parameter',
+  }
+
+  Reflect.defineMetadata(ARGUMENT_OPTIONS, params, target.constructor)
 }
 
-export const Property = (options?: PropertyOptions): PropertyDecorator => (target, propertyKey) =>
-  Reflect.defineMetadata(PROPERTY_NAME, options, target, propertyKey)
+export const Optional = (options: OptionInputInterface): ParameterDecorator => (target, propertyKey, parameterIndex) => {
+  const params = Reflect.getOwnMetadata(ARGUMENT_OPTIONS, target.constructor) || []
 
-export type OptionOptions = {
-  default?: any
-  shortName: string
+  params[parameterIndex] = {
+    ...options,
+    type: 'option',
+  }
+
+  Reflect.defineMetadata(ARGUMENT_OPTIONS, params, target.constructor)
 }
-
-export const Option = (options: OptionOptions): PropertyDecorator => (target, propertyKey) =>
-  Reflect.defineMetadata(OPTION_NAME, options, target, propertyKey)
