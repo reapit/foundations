cc29dc800dd9c4665aff7a74b01b8ea0af1adcb8
Will McVay <wmcvay@reapit.com>
2021-06-08 19:53:20 +0100

    feat: #2627 mobile menu working

--- packages/elements/src/components-v3/Nav/use-nav.stories.tsx
+++ packages/elements/src/components-v3/Nav/use-nav.stories.tsx
@@ -1,28 +1,46 @@
 import React from 'react'
 import { useNavState } from '../../hooks/use-nav-state'
-import { elNavItemActive } from './__styles__/index'
-import { Nav, NavItem } from './index'
+import { useMediaQuery } from '../../hooks/use-media-query'
+import { elNavSubItemExpanded, elNavItemActive, elNavSubItemActive, elNavItemExpanded } from './__styles__/index'
+import { Nav, NavItem, NavSubItem, NavSubNav } from './index'
 import { Icon } from '../Icon'
 import { cx } from 'linaria'
+import { elIntentNeutral } from '../../styles-v3/base/intent'
+import { elMLAuto, elMr2 } from '../../styles-v3/base/spacing'
 
 export const UseNavStory = () => {
-  const { navState, setNavState } = useNavState()
-  const { navItemId } = navState
+  const { navState, setNavState } = useNavState(1)
+  const { navItemIndex } = navState
   const navigate = () => console.log('Navigating - dispatch to React router or similar')
   const logOut = () => console.log('Logging the user out')
 
   return (
     <Nav>
-      <NavItem className={cx(navItemId === 'ICON' && elNavItemActive)} onClick={setNavState('ICON', navigate)}>
+      <NavItem
+        className={cx(navItemIndex === 0 && elNavItemActive)}
+        onClick={setNavState({
+          navItemIndex: 0,
+          callback: navigate,
+        })}
+      >
         <Icon icon="reapitHouse" />
       </NavItem>
-      <NavItem className={cx(navItemId === 'APPS' && elNavItemActive)} onClick={setNavState('APPS', navigate)}>
+      <NavItem
+        className={cx(navItemIndex === 1 && elNavItemActive)}
+        onClick={setNavState({
+          navItemIndex: 1,
+          callback: navigate,
+        })}
+      >
         <Icon icon="apps" />
         Apps
       </NavItem>
       <NavItem
-        className={cx(navItemId === 'ANALYTICS' && elNavItemActive)}
-        onClick={setNavState('ANALYTICS', navigate)}
+        className={cx(navItemIndex === 2 && elNavItemActive)}
+        onClick={setNavState({
+          navItemIndex: 2,
+          callback: navigate,
+        })}
       >
         <Icon icon="analytics" />
         Analytics
@@ -31,7 +49,90 @@ export const UseNavStory = () => {
         <Icon icon="marketplace" />
         Marketplace
       </NavItem>
-      <NavItem onClick={setNavState('LOGOUT', logOut)} isSecondary>
+      <NavItem
+        onClick={setNavState({
+          navItemIndex: 4,
+          callback: logOut,
+        })}
+        isSecondary
+      >
+        <Icon icon="logout" />
+        Logout
+      </NavItem>
+    </Nav>
+  )
+}
+
+export const UseNavMobileSubMenuStory = () => {
+  const { navState, setNavState } = useNavState(1)
+  const { isMobile } = useMediaQuery()
+  const { navItemIndex, navMenuOpen, navSubMenuOpen, navSubItemIndex } = navState
+  const navigate = () => console.log('Navigating - dispatch to React router or similar')
+  const logOut = () => console.log('Logging the user out')
+
+  return (
+    <Nav>
+      <NavItem
+        className={cx(navItemIndex === 0 && elNavItemActive)}
+        onClick={setNavState({
+          navItemIndex: 0,
+          callback: navigate,
+        })}
+      >
+        <Icon icon={isMobile ? 'reapitLogo' : 'reapitHouse'} />
+        {isMobile && (
+          <Icon
+            className={cx(elIntentNeutral, elMLAuto, elMr2)}
+            icon={navMenuOpen ? 'hamburgerOpen' : 'hamburger'}
+            onClick={setNavState({
+              navMenuOpen: !navMenuOpen,
+              navSubMenuOpen: false,
+            })}
+          />
+        )}
+      </NavItem>
+      <NavItem
+        className={cx(navItemIndex === 1 && elNavItemActive, navMenuOpen && elNavItemExpanded)}
+        onClick={
+          isMobile
+            ? setNavState({ navItemIndex: 1, navSubMenuOpen: !navSubMenuOpen })
+            : setNavState({ navItemIndex: 1, callback: navigate })
+        }
+      >
+        <Icon icon="apps" />
+        Apps
+        {isMobile && <Icon className={cx(elIntentNeutral, elMLAuto)} icon={navSubMenuOpen ? 'arrowUp' : 'arrowDown'} />}
+      </NavItem>
+      <NavSubNav>
+        <NavSubItem
+          className={cx(navSubItemIndex === 0 && elNavSubItemActive, navSubMenuOpen && elNavSubItemExpanded)}
+          onClick={setNavState({ navSubItemIndex: 0, callback: navigate })}
+        >
+          <span>App List</span>
+        </NavSubItem>
+        <NavSubItem
+          className={cx(navSubItemIndex === 1 && elNavSubItemActive, navSubMenuOpen && elNavSubItemExpanded)}
+          onClick={setNavState({ navSubItemIndex: 1, callback: navigate })}
+        >
+          <span>Create App</span>
+        </NavSubItem>
+      </NavSubNav>
+      <NavItem
+        className={cx(navItemIndex === 2 && elNavItemActive, navMenuOpen && elNavItemExpanded)}
+        onClick={setNavState({ navItemIndex: 2, callback: navigate })}
+      >
+        <Icon icon="analytics" />
+        Analytics
+      </NavItem>
+      <NavItem className={cx(navMenuOpen && elNavItemExpanded)} href="https://marketplace.reapit.cloud">
+        <Icon icon="marketplace" />
+        Marketplace
+      </NavItem>
+      <NavItem
+        className={cx(navMenuOpen && elNavItemExpanded)}
+        onClick={setNavState({ navItemIndex: 4, callback: logOut })}
+        isSecondary
+      >
         <Icon icon="logout" />
         Logout
       </NavItem>
--- packages/elements/src/hooks/use-media-query/index.tsx
+++ packages/elements/src/hooks/use-media-query/index.tsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from 'react'
 
 export interface MediaType {
-
+  isMobile: boolean
+  isTablet: boolean
+  isDesktop: boolean
 }
 
-export const useViewPortSize = (): ViewPortSize => {
+export const useMediaQuery = (): MediaType => {
   const [windowWidth, setWindowWidth] = useState(() => window.innerWidth)
+
   useEffect(() => {
     const onResize = () => setWindowWidth(window.innerWidth)
     window.addEventListener('resize', onResize)
     return () => window.removeEventListener('resize', onResize)
   })
 
-  if (windowWidth <= 768) return 'MOBILE'
-  if (windowWidth <= 1024) return 'TABLET'
-  return 'DESKTOP'
+  const isMobile = windowWidth < 768
+  const isTablet = windowWidth >= 768 && windowWidth <= 1024
+  const isDesktop = windowWidth > 1024
+
+  return {
+    isMobile,
+    isTablet,
+    isDesktop,
+  }
 }
