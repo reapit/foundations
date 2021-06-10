183b5036c7a9c6bbf6a56f5055120b56ebb898aa
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-10 14:46:51 +0100

    feat: added api key creation method

--- packages/deployment-service-ui/src/components/pages/api-key/list.tsx
+++ packages/deployment-service-ui/src/components/pages/api-key/list.tsx
@@ -1,6 +1,8 @@
 import { reapitConnectBrowserSession } from '@/core/connect-session'
-import { configurationApiKeyApiService } from '@/platform-api/configuration-api'
+import { configurationApiKeyApiCreateService, configurationApiKeyApiService } from '@/platform-api/configuration-api'
 import { ReapitConnectSession, useReapitConnect } from '@reapit/connect-session'
+import { H3, Section } from '@reapit/elements'
+import { Button, Table } from '@reapit/elements'
 import { Loader } from '@reapit/elements/v3'
 import { ApiKeyInterface } from '@reapit/foundations-ts-definitions'
 import React, { useEffect, useState } from 'react'
@@ -9,6 +11,7 @@ export default () => {
   const { connectSession } = useReapitConnect(reapitConnectBrowserSession)
   const [apiKeys, setApiKeys] = useState<ApiKeyInterface[]>([])
   const [loading, setLoading] = useState<boolean>(false)
+  const [creationLoading, setCreationLoading] = useState<boolean>(false)
 
   useEffect(() => {
     const fetchApiKeys = async () => {
@@ -25,10 +28,50 @@ export default () => {
     }
   }, [connectSession])
 
+  const createApiKey = async () => {
+    setCreationLoading(true)
+
+    const apiKey = await configurationApiKeyApiCreateService(connectSession as ReapitConnectSession, {})
+
+    console.log(apiKey)
+
+    setCreationLoading(false)
+    if (apiKey) {
+      setApiKeys([...apiKeys, ...[apiKey]])
+    }
+  }
+
   return (
-    <>
-      <h1>Api Keys</h1>
-      {loading ? <Loader /> : <h2>Not Loading</h2>}
-    </>
+    <Section>
+      <H3>Api Keys</H3>
+      <Button onClick={createApiKey} loading={creationLoading} type="button" variant="primary">
+        Create new ApiKey
+      </Button>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Table
+          data={apiKeys}
+          columns={[
+            {
+              Header: 'Name',
+              accessor: 'name',
+            },
+            {
+              Header: 'Created',
+              accessor: 'createdAt',
+              Cell: ({ cell, data }) => {
+                console.log(cell, data)
+                return <span>Created at</span>
+              },
+            },
+            {
+              Header: 'ApiKey',
+              accessor: 'apiKey',
+            },
+          ]}
+        />
+      )}
+    </Section>
   )
 }
--- packages/deployment-service-ui/src/platform-api/configuration-api.ts
+++ packages/deployment-service-ui/src/platform-api/configuration-api.ts
@@ -1,19 +1,51 @@
 import { fetcher } from '@reapit/elements'
 import { ReapitConnectSession } from '@reapit/connect-session'
-import { ApiKeyInterface } from '@reapit/foundations-ts-definitions'
+import { ApiKeyEntityType, ApiKeyInterface } from '@reapit/foundations-ts-definitions'
 import { BASE_HEADERS } from '../constants/api'
 
 export const configurationApiKeyApiService = async (
   session: ReapitConnectSession,
 ): Promise<ApiKeyInterface[] | undefined> => {
   try {
-    const response: ApiKeyInterface[] | undefined = await fetcher({
+    const response: { items: ApiKeyInterface[] } | undefined = await fetcher({
       api: 'https://ey4eq5tak9.execute-api.eu-west-2.amazonaws.com',
       url: '/dev/api-key',
       method: 'GET',
       headers: {
         ...BASE_HEADERS,
         Authorization: `${session.idToken}`,
+      },
+    })
+
+    if (response) {
+      return response.items
+    }
+
+    throw new Error('No response returned by API')
+  } catch (err) {
+    console.error('Error fetching Configuration Appointment Types', err.message)
+  }
+}
+
+export const configurationApiKeyApiCreateService = async (
+  session: ReapitConnectSession,
+  apiKey: Partial<ApiKeyInterface>,
+): Promise<ApiKeyInterface | undefined> => {
+  try {
+    const keyExpiresAt = new Date()
+    keyExpiresAt.setFullYear(keyExpiresAt.getFullYear() + 1)
+    const response: ApiKeyInterface | undefined = await fetcher({
+      api: 'https://ey4eq5tak9.execute-api.eu-west-2.amazonaws.com',
+      url: '/dev/api-key',
+      method: 'POST',
+      headers: {
+        ...BASE_HEADERS,
+        Authorization: `${session.idToken}`,
+      },
+      body: {
+        entityType: ApiKeyEntityType.DEPLOYMENT,
+        keyExpiresAt: keyExpiresAt.toISOString(),
+        ...apiKey,
       },
     })
 
