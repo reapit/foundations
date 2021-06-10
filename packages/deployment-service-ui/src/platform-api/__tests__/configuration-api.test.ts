01aaf199806866e4783cd23df56f4cc2e3fc3fda
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-10 12:28:39 +0100

    feat: started implementing api key table

--- packages/deployment-service-ui/src/platform-api/__tests__/configuration-api.test.ts
+++ packages/deployment-service-ui/src/platform-api/__tests__/configuration-api.test.ts
@@ -1,4 +1,4 @@
-import { configurationAppointmentsApiService } from '../configuration-api'
+import { configurationApiKeyApiService } from '../configuration-api'
 import { ListItemModel } from '@reapit/foundations-ts-definitions'
 import { mockBrowserSession } from '../__mocks__/session'
 import { fetcher } from '@reapit/elements'
@@ -13,17 +13,17 @@ const mockConfigurationAppointments = [
   },
 ] as ListItemModel[]
 
-describe('configurationAppointmentsApiService', () => {
+describe('configurationApiKeyApiService', () => {
   it('should return a response from the config service', async () => {
     mockedFetch.mockReturnValueOnce(mockConfigurationAppointments)
-    expect(await configurationAppointmentsApiService(mockBrowserSession)).toEqual(mockConfigurationAppointments)
+    expect(await configurationApiKeyApiService(mockBrowserSession)).toEqual(mockConfigurationAppointments)
     expect(fetcher).toHaveBeenCalledTimes(1)
   })
 
   it('should catch an error if no response from config service', async () => {
     const errorSpy = jest.spyOn(console, 'error')
     mockedFetch.mockReturnValueOnce(undefined as any)
-    await configurationAppointmentsApiService(mockBrowserSession)
+    await configurationApiKeyApiService(mockBrowserSession)
     expect(errorSpy).toHaveBeenCalledWith(
       'Error fetching Configuration Appointment Types',
       'No response returned by API',
