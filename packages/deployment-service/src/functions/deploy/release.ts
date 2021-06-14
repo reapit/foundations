59cebc169aaf254591b8f6f8408c9d2d5d2335cb
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-14 12:34:43 +0100

    chore: formatting and linting

--- packages/deployment-service/src/functions/deploy/release.ts
+++ packages/deployment-service/src/functions/deploy/release.ts
@@ -1,17 +1,17 @@
-import { resolveDeveloperId } from "@/utils"
-import { httpHandler, NotFoundException, ValidationException } from "@homeservenow/serverless-aws-handler"
-import { IsNotEmpty, IsString, validate } from "class-validator"
+import { resolveDeveloperId } from '@/utils'
+import { httpHandler, NotFoundException, ValidationException } from '@homeservenow/serverless-aws-handler'
+import { IsNotEmpty, IsString, validate } from 'class-validator'
 import { s3Client } from '../../services'
 
 /**
  * TODO
- * 
+ *
  * [x] enable cli to upload code zip to S3 and add version files
- * 
- * [ ] take zip from bucket and deploy with serverless 
+ *
+ * [ ] take zip from bucket and deploy with serverless
  * [x] list deployment versions (list file version from S3)
  * [ ] enable rollback deployments
- * 
+ *
  */
 
 class SimpleDeployDto {
@@ -35,7 +35,8 @@ const validator = async (dto: SimpleDeployDto): Promise<SimpleDeployDto> => {
 }
 const fileSeparator = '-'
 
-const fileName = (developerId: string, project: string, version: string): string => ([developerId, project, version].join(fileSeparator)) + '.zip'
+const fileName = (developerId: string, project: string, version: string): string =>
+  [developerId, project, version].join(fileSeparator) + '.zip'
 
 /**
  * Deploy a new release
@@ -65,16 +66,21 @@ export const releases = httpHandler<SimpleDeployDto, any[]>({
   handler: async ({ event, body }) => {
     const developerId = await resolveDeveloperId(event)
 
-    const files = await new Promise<Object[]>((resolve, reject) => s3Client.listObjectsV2({
-      Bucket: '',
-      Delimiter: [developerId, body.projectName].join(fileSeparator),
-    }, (err, data) => {
-      if (typeof data !== 'undefined' && Array.isArray(data)) resolve(data.Contents as Object[])
-      reject()
-    }))
+    const files = await new Promise<Object[]>((resolve, reject) =>
+      s3Client.listObjectsV2(
+        {
+          Bucket: '',
+          Delimiter: [developerId, body.projectName].join(fileSeparator),
+        },
+        (err, data) => {
+          if (typeof data !== 'undefined' && Array.isArray(data)) resolve(data.Contents as Object[])
+          reject()
+        },
+      ),
+    )
 
     return files
-  }
+  },
 })
 
 /**
@@ -84,13 +90,18 @@ export const releaseVersion = httpHandler({
   handler: async ({ event }) => {
     const developerId = await resolveDeveloperId(event)
 
-    const file = await new Promise<AWS.S3.Body>((resolve, reject) => s3Client.getObject({
-      Bucket: '',
-      Key: fileName(developerId, event.pathParameters?.project as string, event.pathParameters?.version as string)
-    }, (err, data) => {
-      if (typeof data !== 'undefined' && Array.isArray(data)) resolve(data.Body as AWS.S3.Body)
-      reject()
-    }))
+    const file = await new Promise<AWS.S3.Body>((resolve, reject) =>
+      s3Client.getObject(
+        {
+          Bucket: '',
+          Key: fileName(developerId, event.pathParameters?.project as string, event.pathParameters?.version as string),
+        },
+        (err, data) => {
+          if (typeof data !== 'undefined' && Array.isArray(data)) resolve(data.Body as AWS.S3.Body)
+          reject()
+        },
+      ),
+    )
 
     if (!file) {
       throw new NotFoundException()
