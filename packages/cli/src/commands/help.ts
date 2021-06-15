96a2ad35d8bb74d7df65d1893c6ed637a094946d
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-15 16:01:21 +0100

    refactor: non ambiguously arrow func :shrug:

--- packages/cli/src/commands/help.ts
+++ packages/cli/src/commands/help.ts
@@ -4,14 +4,16 @@ import { ParentCommand } from './../parent.command'
 export class HelpCommand extends AbstractCommand {
   commands: (AbstractCommand | ParentCommand)[] = []
 
+  sortCommands(a: ParentCommand | AbstractCommand): number {
+    return a instanceof ParentCommand ? 1 : -1
+  }
+
   setCommands(commands: (AbstractCommand | ParentCommand)[]) {
     this.commands = commands
   }
   async run() {
-    this.commands
-      .sort((a) => (a instanceof ParentCommand ? 1 : -1))
-      .forEach((command) => {
-        command.printConfig()
-      })
+    this.commands.sort(this.sortCommands).forEach((command) => {
+      command.printConfig()
+    })
   }
 }
