30c1dda68ca9b2e09add6e3048cde802d7d6292e
Will McVay <wmcvay@reapit.com>
2021-06-15 11:42:39 +0100

    feat: #2630 wip

---
+++ packages/elements/src/components-v3/Pagination/__styles__/index.ts
@@ -0,0 +1,7 @@
+import { styled } from 'linaria/react'
+
+export const ElPaginationWrap = styled.div``
+
+export const ElPaginationText = styled.div``
+
+export const ElPaginationButton = styled.button``
