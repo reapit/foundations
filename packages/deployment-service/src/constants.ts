a8b40a994de3bc9b4e58adf29c2e61fa496db860
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-17 13:25:37 +0100

    fix: corrected table name for dynamo decorator

--- packages/deployment-service/src/constants.ts
+++ packages/deployment-service/src/constants.ts
@@ -1,7 +1,7 @@
 import { DefaultHeadersInterface } from '@homeservenow/serverless-aws-handler'
 
 export enum TABLE_NAMES {
-  PIPELINE = 'Cloud_Pipelines_Service',
+  PIPELINE = 'Cloud_Pipeline_Service',
   PIPELINE_RUNNER = 'Cloud_Pipeline_Runners_Service',
   TASK = 'Cloud_Task_Service',
 }
