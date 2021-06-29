c40a8dafb878f3e6806ec153f4d4db25736a507d
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-29 19:32:55 +0100

    feat: added status indicator component

---
+++ packages/elements/src/components/status-indicator/__styles__/index.tsx
@@ -0,0 +1,47 @@
+import { styled } from 'linaria/react'
+import {
+  elIntentPrimary,
+  elIntentSecondary,
+  elIntentCritical,
+  elIntentSuccess,
+  elIntentDanger,
+} from '../../../styles/intent'
+
+export const ElStatusIndicator = styled.span`
+  border-radius: 3rem;
+  display: inline-block;
+  width: .75rem;
+  height: .75rem;
+  margin: 0 .25rem;
+  background: var(--color-grey-light);
+
+  &${elIntentPrimary} {
+    background-image: linear-gradient(to right, var(--intent-primary), var(--intent-primary));
+    color: var(--intent-primary-text);
+    outline-color: var(--intent-primary-dark);
+  }
+
+  &${elIntentSecondary} {
+    background-image: linear-gradient(to right, var(--intent-secondary), var(--intent-secondary));
+    color: var(--intent-secondary-text);
+    outline-color: var(--intent-secondary-dark);
+  }
+
+  &${elIntentCritical} {
+    background-image: linear-gradient(to right, var(--intent-critical), var(--intent-critical));
+    color: var(--intent-critical-text);
+    outline-color: var(--intent-critical-dark);
+  }
+
+  &${elIntentSuccess} {
+    background-image: linear-gradient(to right, var(--intent-success), var(--intent-success));
+    color: var(--intent-success-text);
+    outline-color: var(--intent-success-dark);
+  }
+
+  &${elIntentDanger} {
+    background-image: linear-gradient(to right, var(--intent-danger), var(--intent-danger));
+    color: var(--intent-danger-text);
+    outline-color: var(--intent-danger-dark);
+  }
+`
