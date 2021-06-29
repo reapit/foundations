292835fdf2cb6b1121d213cb09f0cc41a8bb77eb
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-06-29 19:38:15 +0100

    feat: added tests for staus indicator

---
+++ packages/elements/src/components/status-indicator/__tests__/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { StatusIndicator } from '..'
+
+describe('Table component', () => {
+  it('should match a snapshot', () => {
+    const wrapper = shallow(<StatusIndicator />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match a snapshot', () => {
+    const wrapper = shallow(<StatusIndicator intent="primary" />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match a snapshot', () => {
+    const wrapper = shallow(<StatusIndicator intent="secondary" />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match a snapshot', () => {
+    const wrapper = shallow(<StatusIndicator intent="danger" />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match a snapshot', () => {
+    const wrapper = shallow(<StatusIndicator intent="success" />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should match a snapshot', () => {
+    const wrapper = shallow(<StatusIndicator intent="critical" />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
