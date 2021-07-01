a36aceef57c5e7c8f4833c24062c410c4eb3eda1
Will McVay <wmcvay@reapit.com>
2021-07-01 16:47:37 +0100

    feat: #4407 ready for an interim PR

--- packages/elements/src/components/nav/__styles__/index.ts
+++ packages/elements/src/components/nav/__styles__/index.ts
@@ -1,6 +1,6 @@
 import { css } from 'linaria'
 import { styled } from 'linaria/react'
-import { colorBlueLight } from '../../../styles/variables'
+import { colorBlueLight } from '../../../styles/globals'
 
 const chevronRight = (fill: string) =>
   `data:image/svg+xml;utf8,<svg width="18" height="40" viewBox="0 0 18 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 0L8.8012 0C10.5501 0 13.0962 2.1362 12.6186 2.80527L17.6261 18.8053C17.8695 19.5832 17.8695 20.4168 17.6261 21.1947L12.6186 37.1947C12.0962 38.8638 10.5501 40 8.8012 40H0V0Z" fill="${encodeURIComponent(
