d73ae44dc54cafb4e894fdfda6e45c8985d45d76
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-01 15:02:53 +0100

    feat: added col helpers for new columns

--- packages/elements/src/components/grid/grid-responsive.tsx
+++ packages/elements/src/components/grid/grid-responsive.tsx
@@ -5,7 +5,7 @@ import { MediaType, useMediaQuery } from '../../hooks/use-media-query'
 import * as units from './__styles__/units'
 
 export type GridUnitType = 0 | 1 | 2 | 3 | 4 | 6 | 8 | 12
-export type ColUnitType = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+export type ColUnitType = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20
 
 export interface GridResponsiveProps extends HTMLAttributes<HTMLElement> {
   colGapMobile?: GridUnitType
