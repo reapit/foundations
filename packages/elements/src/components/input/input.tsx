0e4d82518349a951cd36873150883d19cc3ad203
Will McVay <wmcvay@reapit.com>
2021-07-01 19:08:46 +0100

    fix: fixes build

--- packages/elements/src/components/input/input.tsx
+++ packages/elements/src/components/input/input.tsx
@@ -4,12 +4,5 @@ import { ElInput } from './__styles__'
 export interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 export const Input: React.FC<IInput> = ({ ...rest }) => {
-  return (
-    <ElInput {...rest} />
-  )
+  return <ElInput {...rest} />
 }
-
-<label class="file">
-  <input type="file" id="file" aria-label="File browser example">
-  <span class="file-custom"></span>
-</label>
