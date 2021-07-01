92b31cf8b01ce80aa1d367cd3086f64a5f27c72e
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-01 13:53:10 +0100

    feat: added additional button tests

--- packages/elements/src/components/button/__tests__/index.tsx
+++ packages/elements/src/components/button/__tests__/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import { Button, IButton } from '../index'
+import { Button, IButton, FloatingButton, ButtonGroup } from '../index'
 
 const props: IButton = {
   type: 'submit',
@@ -13,6 +13,18 @@ const props: IButton = {
 describe('Button', () => {
   it('should match a snapshot', () => {
     expect(shallow(<Button {...props}>button text</Button>)).toMatchSnapshot()
+  })
+
+  it('should match a snapshot', () => {
+    expect(shallow(<FloatingButton icon="addSystem" {...props}>button text</FloatingButton>)).toMatchSnapshot()
+  })
+
+  it('should match a snapshot', () => {
+    expect(shallow(<ButtonGroup>
+      <Button>1</Button>
+      <Button>2</Button>
+      <Button>3</Button>
+    </ButtonGroup>)).toMatchSnapshot()
   })
 
   it('should fire a click event correctly', () => {
