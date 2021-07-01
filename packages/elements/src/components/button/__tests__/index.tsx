4a233fcba2029c400cda1b3dc53bf962485178ca
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-01 14:07:15 +0100

    feat: added test for resolving size func

--- packages/elements/src/components/button/__tests__/index.tsx
+++ packages/elements/src/components/button/__tests__/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import { Button, IButton, FloatingButton, ButtonGroup } from '../index'
+import { Button, IButton, FloatingButton, ButtonGroup, resolveButtonSize } from '../index'
+import { elButtonSize2, elButtonSize3, elButtonSize4 } from '../__styles__'
 
 const props: IButton = {
   type: 'submit',
@@ -31,6 +32,12 @@ describe('Button', () => {
     const wrapper = shallow(<Button {...props}>button text</Button>)
     wrapper.simulate('click')
     expect(props.onClick).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return button size class', () => {
+    expect(resolveButtonSize(2)).toBe(elButtonSize2)
+    expect(resolveButtonSize(3)).toBe(elButtonSize3)
+    expect(resolveButtonSize(4)).toBe(elButtonSize4)
   })
 
   afterEach(() => {
