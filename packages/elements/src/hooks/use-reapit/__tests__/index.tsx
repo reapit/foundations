9a065eee2c6e6515542f6b2cb065cf2d25b2f4e5
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-02 17:24:51 +0100

    chore: remove use-reapit provider :sob:

--- packages/elements/src/hooks/use-reapit/__tests__/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import { ReapitProvider } from './../'
-
-describe('NavStateProvider', () => {
-  it('should match snapshot', () => {
-    expect(shallow(<ReapitProvider />)).toMatchSnapshot()
-  })
-})
