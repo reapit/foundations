5e43d2d32e0fe539efad7df5661843c3d26cb1e2
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-02 10:33:27 +0100

    docs: added flex story book examples

--- packages/elements/src/storybook/demo-block.tsx
+++ packages/elements/src/storybook/demo-block.tsx
@@ -5,3 +5,17 @@ export const GridDemoBlock = styled.div`
   height: 3rem;
   background-color: var(--color-blue-light2);
 `
+
+export const GridDemoBlockWithMargin = styled.div`
+  height: 3rem;
+  background-color: var(--color-blue-light2);
+  margin: .5rem;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`
