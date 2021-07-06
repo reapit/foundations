073780c7c70fdcd83203d4565132f34087cd3191
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-06 16:52:32 +0100

    chore: remove dynamo table names

--- packages/deployment-service/src/constants.ts
+++ packages/deployment-service/src/constants.ts
@@ -1,11 +1,5 @@
 import { DefaultHeadersInterface } from '@homeservenow/serverless-aws-handler'
 
-export enum TABLE_NAMES {
-  PIPELINE = 'Cloud_Pipeline_Service',
-  PIPELINE_RUNNER = 'Cloud_Pipeline_Runners_Service',
-  TASK = 'Cloud_Task_Service',
-}
-
 export const defaultOutputHeaders: DefaultHeadersInterface = {
   'Access-Control-Allow-Origin': '*',
 }
