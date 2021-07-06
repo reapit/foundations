85e69ffea0490566e75dda2bc38b8aadf6798c99
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-06 16:43:20 +0100

    feat: added db config values

--- packages/deployment-service/src/config/db.ts
+++ packages/deployment-service/src/config/db.ts
@@ -1,5 +1,12 @@
 import { ConnectionOptions } from 'typeorm'
+import * as entities from '@/entities'
 
 export const dbConfig: ConnectionOptions = {
   type: 'mysql',
+  logging: ['error'],
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  entities: Object.keys(entities).map((module) => entities[module]),
 }
