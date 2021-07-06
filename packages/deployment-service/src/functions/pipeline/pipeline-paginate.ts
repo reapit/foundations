7c119a34a84c6c23d291673f1cf7bab97a64a160
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-06 16:09:40 +0100

    feat: updated db usage with repos

--- packages/deployment-service/src/functions/pipeline/pipeline-paginate.ts
+++ packages/deployment-service/src/functions/pipeline/pipeline-paginate.ts
@@ -2,14 +2,7 @@ import { httpHandler } from '@homeservenow/serverless-aws-handler'
 import { PipelineEntity } from '@/entities'
 import * as service from '@/services/pipeline'
 import { resolveDeveloperId } from './../../utils'
-
-type Pagintation<T> = {
-  items: T[]
-  meta: {
-    // count: number
-    nextCursor: string
-  }
-}
+import { Pagination } from 'nestjs-typeorm-paginate'
 
 /**
  * Return pagination response for signed in user
@@ -18,15 +11,15 @@ export const pipelinePaginate = httpHandler({
   defaultOutputHeaders: {
     'Access-Control-Allow-Origin': '*',
   },
-  handler: async ({ event }): Promise<Pagintation<PipelineEntity>> => {
+  handler: async ({ event }): Promise<Pagination<PipelineEntity>> => {
     const developerId = await resolveDeveloperId(event)
 
-    const response = await service.batchGetPipelines(
+    const response = await service.paginatePipelines(
       developerId,
-      event?.queryStringParameters?.nextCursor ? { id: event?.queryStringParameters?.nextCursor } : undefined,
+      event?.queryStringParameters?.page ? Number(event?.queryStringParameters?.page) : undefined,
     )
 
-    const pagination: Pagintation<PipelineEntity> = {
+    const pagination: Pagination<PipelineEntity> = {
       items: [],
       meta: response[1],
     }
