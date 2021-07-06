fdb10b444fb401a6b1b576874e73726450096e03
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-06 13:19:16 +0100

    feat: testing of github downloading zip

--- packages/deployment-service/src/functions/pipeline/pipeline-run.ts
+++ packages/deployment-service/src/functions/pipeline/pipeline-run.ts
@@ -2,12 +2,21 @@ import { findPipelineById } from '@/services'
 import { ownership } from '@/utils'
 import { resolveDeveloperId } from '../../utils'
 import { httpHandler, HttpStatusCode, NotFoundException } from '@homeservenow/serverless-aws-handler'
-import { execSync } from 'child_process'
+// import { execSync } from 'child_process'
 import { resolve } from 'path'
 import { defaultOutputHeaders } from '../../constants'
+import fs from 'fs'
+import request from 'request'
+import AdmZip from 'adm-zip'
 
 const cloneDir = 'project'
-const dir = resolve('/tmp')
+const dir = resolve('/tmp', cloneDir)
+const cloneZip = 'resource.zip'
+
+const unzip = () => {
+  const zip = new AdmZip(`${dir}/${cloneZip}`)
+  zip.extractAllTo(`${dir}`, true)
+}
 
 export const pipelineRun = httpHandler({
   defaultOutputHeaders,
@@ -24,14 +33,27 @@ export const pipelineRun = httpHandler({
     await ownership(pipeline.developerId, developerId)
 
     if (!pipeline.repository) {
-      throw new Error('No repository set')
+      throw new NotFoundException('No repository set')
+    }
+
+    // make sure tmp dir exists
+    if (!fs.existsSync(dir)) {
+      console.log('making dir')
+      fs.mkdirSync(dir)
+    } else {
+      // TODO delete dir and recreate
     }
 
     try {
-      const clone = execSync(`git clone ${pipeline.repository} ${cloneDir}`, {
-        cwd: dir,
+      await new Promise<void>((resolve) => {
+        request(`https://codeload.github.com/${pipeline.repository}/zip/refs/heads/master`)
+          .pipe(fs.createWriteStream(`${dir}/${cloneZip}`))
+          .on('close', function () {
+            resolve()
+          })
       })
-      console.log('clone', clone.toString())
+
+      // console.log(result)
     } catch (e) {
       // console.error(e)
       console.log('message', e.message)
@@ -42,55 +64,65 @@ export const pipelineRun = httpHandler({
       }
     }
 
-    try {
-      const yarn = execSync('npm i', {
-        cwd: resolve(dir, cloneDir),
-      })
-      console.log('yarn', yarn.toString())
-    } catch (e) {
-      console.log(e.output.toString())
-      console.log('yarn errors')
-      console.error(e)
-      console.log('npm install failed')
-      return {
-        statusCode: 500,
-        headers: defaultOutputHeaders,
-      }
-    }
+    unzip()
 
-    try {
-      const yarn = execSync('npm run build', {
-        cwd: resolve(dir, cloneDir),
-      })
-      console.log('yarn build', yarn.toString())
-    } catch (e) {
-      console.log('yarn build errors')
-      console.error(e)
-      console.log('npm build failed')
-      return {
-        statusCode: 500,
-        headers: defaultOutputHeaders,
-      }
-    }
+    const dirName = `${pipeline.repository.split('/').pop()}-master`
 
-    try {
-      const serverless = execSync('serverless deploy', {
-        cwd: resolve(dir, cloneDir),
-      })
-      console.log('serverless', serverless.toString())
-    } catch (e) {
-      console.log('sefverless error')
-      console.error(e)
-
-      return {
-        statusCode: 500,
-        headers: defaultOutputHeaders,
-      }
-    }
+    console.log(fs.existsSync(resolve(dir, dirName)))
 
     return {
-      statusCode: HttpStatusCode.OK,
-      body: pipeline,
+      statusCode: 200,
     }
+
+    // try {
+    //   const yarn = execSync('npm i', {
+    //     cwd: resolve(dir, cloneDir),
+    //   })
+    //   console.log('yarn', yarn.toString())
+    // } catch (e) {
+    //   console.log(e.output.toString())
+    //   console.log('yarn errors')
+    //   console.error(e)
+    //   console.log('npm install failed')
+    //   return {
+    //     statusCode: 500,
+    //     headers: defaultOutputHeaders,
+    //   }
+    // }
+
+    // try {
+    //   const yarn = execSync('npm run build', {
+    //     cwd: resolve(dir, cloneDir),
+    //   })
+    //   console.log('yarn build', yarn.toString())
+    // } catch (e) {
+    //   console.log('yarn build errors')
+    //   console.error(e)
+    //   console.log('npm build failed')
+    //   return {
+    //     statusCode: 500,
+    //     headers: defaultOutputHeaders,
+    //   }
+    // }
+
+    // try {
+    //   const serverless = execSync('serverless deploy', {
+    //     cwd: resolve(dir, cloneDir),
+    //   })
+    //   console.log('serverless', serverless.toString())
+    // } catch (e) {
+    //   console.log('sefverless error')
+    //   console.error(e)
+
+    //   return {
+    //     statusCode: 500,
+    //     headers: defaultOutputHeaders,
+    //   }
+    // }
+
+    // return {
+    //   statusCode: HttpStatusCode.OK,
+    //   body: pipeline,
+    // }
   },
 })
