9a6ef80bef1a8e833fb06ed18023e7f578ca6f24
Will McVay <wmcvay@reapit.com>
2021-07-06 14:15:17 +0100

    feat: #3939 fixes gql hosting issues

--- packages/developer-portal/src/components/pages/graphql/index.tsx
+++ packages/developer-portal/src/components/pages/graphql/index.tsx
@@ -7,34 +7,65 @@ import {
   SecondaryNavItem,
   Title,
 } from '@reapit/elements'
-import React, { FC, useEffect, useRef } from 'react'
+import React, { Dispatch, FC, MutableRefObject, SetStateAction, useEffect, useRef, useState } from 'react'
 import { useHistory, useLocation } from 'react-router'
 import Routes from '../../../constants/routes'
 import { navigate } from '../../ui/menu'
 import { reapitConnectBrowserSession } from '../../../core/connect-session'
-import { useReapitConnect } from '@reapit/connect-session'
+import { useReapitConnect, ReapitConnectSession } from '@reapit/connect-session'
 import { graphQLWrapper } from './__styles__/index'
+import { GRAPHQL_CONSTANTS } from '../../../constants/graphql'
+
+const { scriptSrc, scriptId, localStorageKey, settings } = GRAPHQL_CONSTANTS
+
+export const handleLoadGQLPlayground = (setLoaded: Dispatch<SetStateAction<boolean>>) => () => {
+  const existingScript = document.getElementById(scriptId)
+  if (!existingScript) {
+    const script = document.createElement('script')
+    script.src = scriptSrc
+    script.id = scriptId
+    document.body.appendChild(script)
+    script.onload = () => {
+      setLoaded(true)
+    }
+  } else {
+    setLoaded(true)
+  }
+}
+
+export const handleInitGQLPlayground = (
+  loaded: boolean,
+  graphQlRef: MutableRefObject<HTMLDivElement | null>,
+  connectSession: ReapitConnectSession | null,
+) => () => {
+  if (!loaded) {
+    localStorage.removeItem(localStorageKey)
+  }
+
+  if (loaded && connectSession?.accessToken && connectSession?.idToken) {
+    const { accessToken, idToken } = connectSession
+    window.GraphQLPlayground.init(graphQlRef.current, {
+      endpoint: window.reapit.config.graphQLUri,
+      headers: {
+        authorization: idToken,
+        ['reapit-connect-token']: accessToken,
+      },
+      settings,
+    })
+  }
+}
 
 export const GraphQLPage: FC = () => {
   const location = useLocation()
   const history = useHistory()
   const { connectSession } = useReapitConnect(reapitConnectBrowserSession)
+  const [loaded, setLoaded] = useState(false)
   const graphQlRef = useRef<HTMLDivElement | null>(null)
   const { pathname } = location
 
-  useEffect(() => {
-    window.GraphQLPlayground.init(graphQlRef.current, {
-      endpoint: window.reapit.config.graphQLUri,
-      headers: {
-        authorization: connectSession?.idToken,
-        ['reapit-connect-token']: connectSession?.accessToken,
-      },
-      settings: {
-        ['tracing.tracingSupported']: false,
-        ['editor.theme']: 'light',
-      },
-    })
-  }, [graphQlRef, connectSession])
+  useEffect(handleLoadGQLPlayground(setLoaded), [])
+
+  useEffect(handleInitGQLPlayground(loaded, graphQlRef, connectSession), [graphQlRef, connectSession, loaded])
 
   return (
     <FlexContainer isFlexAuto>
