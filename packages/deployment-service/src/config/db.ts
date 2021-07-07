b8447178f1cd8aea4f15c771f878d964aae290db
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-07 11:04:37 +0100

    chore: corrected db type

--- packages/deployment-service/src/config/db.ts
+++ packages/deployment-service/src/config/db.ts
@@ -1,12 +1,14 @@
 import { ConnectionOptions } from 'typeorm'
-import * as entities from '@/entities'
 
 export const dbConfig: ConnectionOptions = {
-  type: 'mysql',
+  type: 'aurora-data-api',
   logging: ['error'],
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE as string,
   password: process.env.DB_PASSWORD,
-  entities: Object.keys(entities).map((module) => entities[module]),
+  region: '',
+  secretArn: '',
+  resourceArn: '',
+  entities: ['**/*/*.entity.ts'],
 }
