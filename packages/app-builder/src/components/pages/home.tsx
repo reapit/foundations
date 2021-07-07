f3053716b5c2cbe1c71e329c8e98856f49766b1f
Josh Balfour <josh@joshbalfour.co.uk>
2021-07-07 10:49:27 +0100

    drag and drop text works

--- packages/app-builder/src/components/pages/home.tsx
+++ packages/app-builder/src/components/pages/home.tsx
@@ -1,30 +1,115 @@
-import React, { FC, useEffect, useState } from 'react'
-import { useReapitConnect, ReapitConnectSession } from '@reapit/connect-session'
-import { reapitConnectBrowserSession } from '../../core/connect-session'
-import { configurationAppointmentsApiService } from '../../platform-api/configuration-api'
-import { ListItemModel } from '@reapit/foundations-ts-definitions'
-import { Title } from '@reapit/elements'
+// @ts-nocheck
+import React, { FC, useState, useEffect } from 'react'
+import { FlexContainer, Button, SecondaryNavContainer, SecondaryNavItem } from '@reapit/elements'
+import { Editor, Frame, Element, useNode, useEditor } from '@craftjs/core'
+import ContentEditable from 'react-contenteditable'
 
 export type AuthenticatedProps = {}
 
-export const Authenticated: FC<AuthenticatedProps> = () => {
-  const { connectSession } = useReapitConnect(reapitConnectBrowserSession)
-  const [appointmentConfigTypes, setAppointmentConfigTypes] = useState([] as ListItemModel[])
+const Text = ({ text, ...props }) => {
+  const {
+    connectors: { connect, drag },
+    selected,
+    actions: { setProp },
+  } = useNode((state) => ({
+    selected: state.events.selected,
+    dragged: state.events.dragged,
+  }))
+
+  const [editable, setEditable] = useState(false)
 
   useEffect(() => {
-    const fetchAppoinmentConfigs = async () => {
-      const serviceResponse = await configurationAppointmentsApiService(connectSession as ReapitConnectSession)
-      if (serviceResponse) {
-        setAppointmentConfigTypes(serviceResponse)
-      }
+    if (selected) {
+      return
     }
-    if (connectSession) {
-      fetchAppoinmentConfigs()
-    }
-  }, [connectSession])
 
-  console.log('Appointment Config Types are: ', appointmentConfigTypes)
-  return <Title>App Builder</Title>
+    setEditable(false)
+  }, [selected])
+
+  return (
+    <div
+      {...props}
+      className="el-flex-auto"
+      ref={(ref) => connect(drag(ref))}
+      onClick={() => selected && setEditable(true)}
+    >
+      <ContentEditable
+        html={text}
+        disabled={!editable}
+        onChange={(e) =>
+          setProp(
+            (props) =>
+              (props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, '')),
+            500
+          )
+        }
+        tagName="p"
+      />
+    </div>
+  )
+}
+
+const Sidebar = () => {
+  const { connectors, query, actions } = useEditor()
+
+  return (
+    <SecondaryNavContainer>
+      <SecondaryNavItem>
+        <button ref={(ref) => connectors.create(ref, <Text text="Hi world" />)}>Drag to add text</button>
+      </SecondaryNavItem>
+      <SecondaryNavItem>
+          <Button onClick={() => {
+            window.localStorage.saveState = query.serialize()
+          }} intent="primary">save</Button>
+      </SecondaryNavItem>
+      <SecondaryNavItem>
+          <Button onClick={() => {
+            const state = window.localStorage.saveState
+            if (!state) {
+              return alert('nothing to load')
+            }
+            actions.deserialize(state)
+          }} intent="secondary">load</Button>
+      </SecondaryNavItem>
+    </SecondaryNavContainer>
+  )
+}
+
+const Container = ({ children }) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode()
+  return (
+    <div ref={(ref) => connect(drag(ref))}>{children}</div>
+  )
+}
+
+export const Authenticated: FC<AuthenticatedProps> = () => {
+
+  return (
+    <FlexContainer>
+
+      <Editor resolver={{
+        Text,
+        Container,
+      }}>
+        <Sidebar />
+        <div style={{ minHeight: 800 }}>
+          <Frame>
+            <Element
+              canvas
+              is={Container}
+            >
+              <Text
+                size="small"
+                text="I'm here by default!"
+              />
+            </Element>
+          </Frame>
+        </div>
+      </Editor>
+    </FlexContainer>
+  )
 }
 
 export default Authenticated
