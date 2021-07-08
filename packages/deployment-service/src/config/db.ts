b997fa39ee92103f1c7f83604f16c7d05c292699
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-08 11:33:50 +0100

    feat: resolved arns and config

--- packages/deployment-service/src/config/db.ts
+++ packages/deployment-service/src/config/db.ts
@@ -4,12 +4,11 @@ import { TaskEntity, PipelineEntity, PipelineRunnerEntity } from './../entities'
 export const dbConfig: ConnectionOptions = {
   type: 'aurora-data-api',
   logging: ['error'],
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  database: process.env.DB_DATABASE as string,
-  password: process.env.DB_PASSWORD,
-  region: '',
-  secretArn: '',
-  resourceArn: '',
+  username: process.env.AURORA_USERNAME as string,
+  database: process.env.AURORA_DATABASE as string,
+  password: process.env.AURORA_PASSWORD as string,
+  region: process.env.DYNAMO_DB_REGION as string,
+  secretArn: process.env.AURORA_SECRET_ARN as string,
+  resourceArn: process.env.AURORA_RESOURCE_ARN as string,
   entities: [PipelineEntity, PipelineRunnerEntity, TaskEntity],
 }
