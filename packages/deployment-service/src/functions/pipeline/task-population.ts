a8f8c8c5c08b6a9bc6e5dc9b80e559bbab07ad19
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-12 13:44:49 +0100

    feat: task runner func

--- packages/deployment-service/src/functions/pipeline/task-population.ts
+++ packages/deployment-service/src/functions/pipeline/task-population.ts
@@ -3,6 +3,8 @@ import { createBatchTasks } from './../../services'
 import { AppTypeEnum, TaskRunnerFunctions } from '@reapit/foundations-ts-definitions'
 import { Handler, Context, Callback } from 'aws-lambda'
 import { plainToClass } from 'class-transformer'
+import { sqs } from './../../services'
+import { QueueNames } from '../../constants'
 
 export const workflowCreation = async (pipelineRunner: PipelineRunnerEntity): Promise<TaskEntity[]> => {
   const batchTasks: Partial<TaskEntity>[] =
@@ -39,21 +41,48 @@ export const workflowCreation = async (pipelineRunner: PipelineRunnerEntity): Pr
 export const taskPopulation: Handler = async (event: any, context: Context, callback: Callback): Promise<void> => {
   // TODO stop all currently running pipelines for pipeline
 
-  const tasks = await Promise.all(
-    event.Records.map((record) => {
+  await Promise.all(
+    event.Records.map(async (record) => {
       const pipelineRunner = plainToClass(PipelineRunnerEntity, JSON.parse(record.body))
-
-      console.log('pipeline', pipelineRunner)
 
       // TODO find pipeline runners for all pipeline settings OR find pipeline on pipeline creation and sync settings?
 
-      return workflowCreation(pipelineRunner)
+      const tasks = await workflowCreation(pipelineRunner)
+
+      const firstTask = tasks[0]
+
+      const queueUrl = await new Promise<string>((resolve, reject) =>
+        sqs.getQueueUrl(
+          {
+            QueueName: QueueNames.TASK_RUNNER,
+          },
+          (error, data) => {
+            if (error) {
+              console.error(error)
+              reject()
+            }
+            typeof data.QueueUrl === 'undefined' ? reject() : resolve(data.QueueUrl)
+          },
+        ),
+      )
+
+      await new Promise<void>((resolve, reject) =>
+        sqs.sendMessage(
+          {
+            MessageBody: JSON.stringify(firstTask),
+            QueueUrl: queueUrl,
+          },
+          (error) => {
+            if (error) {
+              reject(error)
+            }
+            resolve()
+          },
+        ),
+      )
     }),
   )
 
-  console.log('tasks', tasks)
-
   // TODO start first task exec
-
   return callback(null, `Successfully processed ${event.Records.length} records.`)
 }
