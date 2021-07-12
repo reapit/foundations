8bc173a68e373ea2b7095d20f61d57efbb3c0741
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-12 16:49:14 +0100

    feat: added task runner methods and conditions for loop

--- packages/deployment-service/src/functions/pipeline/task-runner.ts
+++ packages/deployment-service/src/functions/pipeline/task-runner.ts
@@ -1,30 +1,136 @@
-import { TaskEntity } from './../../entities'
+import { PipelineRunnerEntity, TaskEntity, TaskWorkflow } from './../../entities'
 import { Handler, Context, Callback } from 'aws-lambda'
 import { plainToClass } from 'class-transformer'
 import * as services from './../../services'
-import { DeploymentStatus } from '../../../../foundations-ts-definitions/types'
+import { DeploymentStatus, TaskRunnerFunctions } from '@reapit/foundations-ts-definitions'
+import { resolveExecutable } from './../../executables'
+import { QueueNames } from '../../constants'
+
+const failure = async (task: TaskEntity, error?: Error): Promise<void> => {
+  console.error(error)
+
+  await Promise.all([
+    services.updatePipelineRunnerEntity(task.pipelineRunner as PipelineRunnerEntity, {
+      buildStatus: DeploymentStatus.FAILED,
+    }),
+    services.updateTask(task, {
+      status: DeploymentStatus.FAILED,
+    }),
+  ])
+}
+
+const overallSuccess = async (task: TaskEntity): Promise<void> => {
+  await Promise.all([
+    services.updatePipelineRunnerEntity(task.pipelineRunner as PipelineRunnerEntity, {
+      buildStatus: DeploymentStatus.SUCCESS,
+    }),
+    services.updateTask(task, {
+      status: DeploymentStatus.SUCCESS,
+    }),
+  ])
+}
+
+const startTask = async (task: TaskEntity): Promise<void> => {
+  await services.updateTask(task, {
+    status: DeploymentStatus.RUNNING,
+  })
+}
+
+const completeAndStartNext = async (task: TaskEntity, nextTask: TaskEntity): Promise<void> => {
+  const queueUrl = await new Promise<string>((resolve, reject) =>
+    services.sqs.getQueueUrl(
+      {
+        QueueName: QueueNames.TASK_RUNNER,
+      },
+      (error, data) => {
+        if (error) {
+          reject()
+        }
+        typeof data.QueueUrl === 'undefined' ? reject() : resolve(data.QueueUrl)
+      },
+    ),
+  )
+
+  await Promise.all([
+    new Promise<void>((resolve, reject) =>
+      services.sqs.sendMessage(
+        {
+          MessageBody: JSON.stringify(nextTask),
+          QueueUrl: queueUrl,
+        },
+        (error) => {
+          if (error) {
+            reject(error)
+          }
+          resolve()
+        },
+      ),
+    ),
+    services.updateTask(task, {
+      status: DeploymentStatus.SUCCESS,
+    }),
+  ])
+}
+
+const getNextTask = async (task: TaskEntity): Promise<TaskEntity | undefined> => {
+  const workflow: TaskWorkflow = task.pipelineRunner?.pipeline?.workflow as TaskWorkflow
+  const currentTaskIndex = workflow.indexOf(task.functionName as TaskRunnerFunctions)
+  if (currentTaskIndex === -1) {
+    return undefined
+  }
+
+  const nextTaskFunction = workflow[currentTaskIndex + 1]
+
+  if (!nextTaskFunction) {
+    return undefined
+  }
+
+  return services.findTaskByPipelineRunnerAndFunction(task.pipelineRunner as PipelineRunnerEntity, nextTaskFunction)
+}
 
 /**
  * SQS event for running tasks and determining pipeline results
  */
 export const taskRunner: Handler = async (event: any, context: Context, callback: Callback): Promise<void> => {
-  console.log('TaskRunner called')
   await Promise.all(
     event.Records.map(async (record) => {
       const cachedTask = plainToClass(TaskEntity, JSON.parse(record.body))
 
-      console.log('task runner', cachedTask)
-
       const task = await services.findTaskById(cachedTask.id as string)
 
       if (!task || task.pipelineRunner?.buildStatus !== DeploymentStatus.RUNNING) {
-        console.log('fuck off lint')
+        if (!task) {
+          return
+        }
+
+        if (task?.pipelineRunner?.buildStatus === DeploymentStatus.PENDING) {
+          await services.updatePipelineRunnerEntity(
+            {
+              ...task.pipelineRunner,
+            },
+            {
+              buildStatus: DeploymentStatus.RUNNING,
+            },
+          )
+        }
       }
 
-      // Should start pipeline (set status to running from pending)
-      // Should start task's executable
-      // Log task's executable output
-      // start next task or complete pipeline runner
+      try {
+        const executable = resolveExecutable(task)
+
+        await startTask(task)
+        await executable(task)
+
+        const nextTask = await getNextTask(task)
+
+        if (nextTask) {
+          await completeAndStartNext(task, nextTask)
+        } else {
+          overallSuccess(task)
+        }
+      } catch (e) {
+        await failure(task, e)
+      }
     }),
   )
 
