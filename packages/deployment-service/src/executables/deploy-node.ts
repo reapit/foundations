55a2c1dc24cec2ec15c07521bf1d2a2e4b54e6b5
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-12 14:46:37 +0100

    feat: updated func type and exports

--- packages/deployment-service/src/executables/deploy-node.ts
+++ packages/deployment-service/src/executables/deploy-node.ts
@@ -1,8 +1,8 @@
-import { PipelineEntity, TaskEntity } from './../entities'
+import { TaskEntity } from './../entities'
 import { ExecutableType } from './executable'
 
-export const deployNode: ExecutableType = (task: TaskEntity, pipeline: PipelineEntity): Promise<boolean> => {
-  console.log('executable', task, pipeline)
+export const deployNode: ExecutableType = (task: TaskEntity): Promise<boolean> => {
+  console.log('executable', task)
 
   return Promise.resolve(true)
 }
