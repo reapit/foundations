5f7b2e2272bad644863ba3d583d49ea59276a780
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-12 16:46:34 +0100

    feat: added console log helpers

--- packages/deployment-service/src/executables/build.ts
+++ packages/deployment-service/src/executables/build.ts
@@ -1,8 +1,9 @@
 import { TaskEntity } from './../entities'
 import { ExecutableType } from './executable'
 
-export const build: ExecutableType = (task: TaskEntity): Promise<boolean> => {
+export const build: ExecutableType = (task: TaskEntity): Promise<true | never> => {
   //TODO get codebase from S3 bucket
+  console.log('building...')
 
   console.log('executable', task)
 
