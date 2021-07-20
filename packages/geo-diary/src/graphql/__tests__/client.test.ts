aa49d376f0943b010d6c13d97b0960891ea5b2e5
Will McVay <wmcvay@reapit.com>
2021-07-20 16:12:14 +0100

    feat: #4603 #4611 remaining serverless and gql refactors

--- packages/geo-diary/src/graphql/__tests__/client.test.ts
+++ packages/geo-diary/src/graphql/__tests__/client.test.ts
@@ -1,70 +1,13 @@
-import { Operation, IdGetterObj } from 'apollo-boost'
-import { ErrorResponse } from 'apollo-link-error'
-import { GraphQLError } from 'graphql'
-import { getClient, generateRequest, onError, dataIdFromObject } from '@/graphql/client'
-import { mockBrowserSession } from '../__mocks__/session'
+import { dataIdFromObject } from '@/graphql/client'
+import { StoreObject } from '@apollo/client'
 
 describe('client', () => {
-  describe('getClient', () => {
-    it('should run correctly', () => {
-      const mockClientUri = 'mockUri'
-      const result = getClient(mockBrowserSession, mockClientUri)
-      expect(result).toBeDefined()
-    })
-  })
-  describe('generateRequest', () => {
-    it('should run correctly', (done) => {
-      // @ts-ignore
-      const mockOperation = {
-        setContext: jest.fn(),
-      } as Operation
-      generateRequest(mockBrowserSession)(mockOperation)
-      setTimeout(() => {
-        expect(mockOperation.setContext).toBeCalled()
-        done()
-      }, 1000)
-    })
-  })
-  describe('onError', () => {
-    it('should run correctly when have both', () => {
-      const mockError = {
-        graphQLErrors: [new GraphQLError('123')] as ReadonlyArray<GraphQLError>,
-        networkError: new Error('abc'),
-      } as ErrorResponse
-      onError(mockError)
-    })
-
-    it('should run correctly when no graphQLErrors', () => {
-      const mockError = {
-        graphQLErrors: [] as ReadonlyArray<GraphQLError>,
-        networkError: new Error('abc'),
-      } as ErrorResponse
-      onError(mockError)
-    })
-
-    it('should run correctly when no networkError', () => {
-      const mockError = {
-        graphQLErrors: [new GraphQLError('123')] as ReadonlyArray<GraphQLError>,
-        networkError: undefined,
-      } as ErrorResponse
-      onError(mockError)
-    })
-
-    it('should run correctly when no error', () => {
-      const mockError = {
-        graphQLErrors: [] as ReadonlyArray<GraphQLError>,
-        networkError: undefined,
-      } as ErrorResponse
-      onError(mockError)
-    })
-  })
-
   describe('dataIdFromObject', () => {
     it('should run correctly', () => {
       const mockObject = {
         __typename: 'mockType',
         id: '1',
-      } as IdGetterObj
+      } as Readonly<StoreObject>
       const result = dataIdFromObject(mockObject)
       const output = `${mockObject.__typename}:${mockObject.id}`
       expect(result).toEqual(output)
