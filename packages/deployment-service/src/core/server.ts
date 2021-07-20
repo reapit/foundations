1d2728ff8ba0c81d73c6c0d3e687e7fb727e8c15
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-20 16:15:29 +0100

    feat: serverless setup

---
+++ packages/deployment-service/src/core/server.ts
@@ -0,0 +1,31 @@
+import express, { json, urlencoded } from 'express'
+import session from 'express-session'
+import memoryStore from 'memorystore'
+import { v4 as uuid } from 'uuid'
+import cors from 'cors'
+import router from './routes'
+const MemoryStore = memoryStore(session as any)
+
+const app = express()
+
+app.use(cors())
+
+app.use(
+  session({
+    secret: uuid(),
+    resave: false,
+    saveUninitialized: true,
+    store: new MemoryStore({
+      checkPeriod: 86400000, // prune expired entries every 24h
+    }),
+  }),
+)
+
+// app.use(urlencoded())
+// app.use(json())
+app.get('/ok', (_req, res) => {
+  res.status(200).send('ok').end()
+})
+app.use(router)
+
+export default app
