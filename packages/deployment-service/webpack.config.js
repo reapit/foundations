50db77fc49a99a0fe3d66f8cf5c1c2cbe99b9fd1
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-21 11:21:01 +0100

    feat: attempting webpack build

--- packages/deployment-service/webpack.config.js
+++ packages/deployment-service/webpack.config.js
@@ -2,7 +2,7 @@ const slsw = require('serverless-webpack')
 const { webpackConfigNode, PATHS } = require('@reapit/ts-scripts')
 const CopyPlugin = require('copy-webpack-plugin')
 
-webpackConfigNode.entry = slsw.lib.entries
+webpackConfigNode.entry = ['./src/index.ts']
 webpackConfigNode.optimization.minimize = false //slsw.lib.webpack.isLocal ? false : true
 
 webpackConfigNode.plugins.push(new CopyPlugin({ 
