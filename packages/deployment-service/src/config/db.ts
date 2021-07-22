7fcd3d1b541af1f4fca8d9a8036b9d79d6c591e3
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-22 11:34:13 +0100

    feat: added docker mysql setup

--- packages/deployment-service/src/config/db.ts
+++ packages/deployment-service/src/config/db.ts
@@ -1,13 +1,27 @@
 import { ConnectionOptions } from 'typeorm'
 import { TaskEntity, PipelineEntity, PipelineRunnerEntity, ReleaseEntity } from './../entities'
 
-export const dbConfig: ConnectionOptions = {
-  type: 'aurora-data-api',
+const defaultDbConfig = {
   logging: true,
-  database: process.env.AURORA_DATABASE as string,
-  region: process.env.DYNAMO_DB_REGION as string,
-  secretArn: process.env.AURORA_SECRET_ARN as string,
-  resourceArn: process.env.AURORA_RESOURCE_ARN as string,
-  entities: [PipelineEntity, PipelineRunnerEntity, TaskEntity, ReleaseEntity],
   synchronize: false,
+  entities: [PipelineEntity, PipelineRunnerEntity, TaskEntity, ReleaseEntity],
 }
+
+export const dbConfig: ConnectionOptions =
+  process.env.NODE_ENV === 'prod' // TODO solve this condition
+    ? {
+        type: 'aurora-data-api',
+        region: process.env.AURORA_REGION as string,
+        secretArn: process.env.AURORA_SECRET_ARN as string,
+        resourceArn: process.env.AURORA_RESOURCE_ARN as string,
+        database: process.env.AURORA_DATABASE as string,
+        ...defaultDbConfig,
+      }
+    : {
+        type: 'mysql',
+        host: process.env.MYSQL_HOST,
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        ...defaultDbConfig,
+      }
