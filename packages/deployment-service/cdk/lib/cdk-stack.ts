5bc719864e04b09d940812f289bc956f28b3c50a
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-23 11:12:11 +0100

    feat: added stack to cdk runner

--- packages/deployment-service/cdk/lib/cdk-stack.ts
+++ packages/deployment-service/cdk/lib/cdk-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+import { DeploymentStack } from './deployment-stack';
+
+const prefix = 'cloud_'
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const app = new cdk.App()
+
+    const stagingStack = new DeploymentStack(app, prefix, 'staging')
   }
 }
