a03842205223367344334d79b6021584cd2d15f7
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-26 09:42:54 +0100

    chore: segregate outputs of js

--- packages/deployment-service/webpack.config.js
+++ packages/deployment-service/webpack.config.js
@@ -2,8 +2,18 @@ const slsw = require('serverless-webpack')
 const { webpackConfigNode, PATHS } = require('@reapit/ts-scripts')
 const CopyPlugin = require('copy-webpack-plugin')
 
-webpackConfigNode.entry = ['./src/index.ts']
+webpackConfigNode.entry = {
+  main: './src/index.ts',
+  taskPopulation: './src/functions/pipeline/task-population.ts',
+  taskRunner: './src/functions/pipeline/task-runner.ts',
+}
 webpackConfigNode.optimization.minimize = false //slsw.lib.webpack.isLocal ? false : true
+
+// webpackConfigNode.output = {
+//   libraryTarget: 'commonjs',
+//   path: `${process.cwd()}/dist`,
+//   filename: '[name]/index.js',
+// }
 
 webpackConfigNode.plugins.push(new CopyPlugin({ 
   patterns: [
