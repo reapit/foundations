460d7c995a4e3c5b236b7e4ad637800b851d3cfa
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-07-26 09:41:54 +0100

    feat: added sqs functions to cdk

--- packages/deployment-service/cdk/lib/deployment-stack.ts
+++ packages/deployment-service/cdk/lib/deployment-stack.ts
@@ -1,68 +1,39 @@
 import * as cdk from '@aws-cdk/core'
 import * as ec2 from '@aws-cdk/aws-ec2'
 import * as lambda from '@aws-cdk/aws-lambda'
-import * as path from 'path'
 import * as api from '@aws-cdk/aws-apigatewayv2'
+import * as awsAuthorizers from '@aws-cdk/aws-apigatewayv2-authorizers'
+import * as cognito from '@aws-cdk/aws-cognito'
 import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations'
 import { Bucket } from '@aws-cdk/aws-s3'
 import * as rds from '@aws-cdk/aws-rds'
-import { Peer, Port, SecurityGroup, CfnNatGateway, CfnEIP } from '@aws-cdk/aws-ec2'
+import { Peer, Port, SecurityGroup } from '@aws-cdk/aws-ec2'
+import * as sqs from '@aws-cdk/aws-sqs'
+import * as eventSource from '@aws-cdk/aws-lambda-event-sources'
+import * as assets from '@aws-cdk/aws-s3-assets'
+import * as config from '../../config.json'
+import * as path from 'path'
 
 const maxAzs = 2
 
 export class DeploymentStack extends cdk.Stack {
   constructor(app: cdk.App, name: string, stage: 'production' | 'staging') {
-    super(app, `${name}_deployment-service-${stage}`)
+    super(app, `${name}deployment-service-${stage}`)
 
     const vpc = new ec2.Vpc(this, 'VPC', {
       cidr: '10.0.0.0/16',
 		  maxAzs,
     })
 
-    // TODO add vpc ingress stuffs and subnets
-    const ingressSecurityGroup = new SecurityGroup(this, 'ingress-security-group', {
+    const ingressSecurityGroup = new SecurityGroup(this, `ingress-security-group-${stage}`, {
       vpc,
       allowAllOutbound: false,
-      securityGroupName: `${name}-IngressSecurityGroup`,
+      securityGroupName: `${name}IngressSecurityGroup-${stage}`,
     })
     ingressSecurityGroup.addIngressRule(Peer.ipv4('0.0.0.0/0'), Port.tcp(80))
     ingressSecurityGroup.addIngressRule(Peer.ipv4('0.0.0.0/0'), Port.tcp(443))
 
-    const allocationIds: { [s: string]: string } = Array.from(Array(maxAzs)).reduce((eips, int) => {
-      const eip = new CfnEIP(this, `VPCPublicSubnet${int+1}NATGatewayEIP${int}`, {
-        domain: 'vpc',
-        tags: [
-          {
-            key: 'Name',
-            value: `MyApp/VPC/PublicSubnet${int+1}`,
-          },
-        ]
-      })	    
-      // Do whatever you need with your EIPs here, ie. store their ref for later use
-    
-      // Add a dependency on the VPC to encure allocation happens before the VPC is created
-      vpc.node.addDependency(eip)
-
-      return {
-        ...eips,
-        [eip.ref]: eip.attrAllocationId,
-      }
-    }, {})
-
-    vpc.publicSubnets.forEach((subnet, index) => {
-      // Find the NAT Gateway
-      const natGateway = subnet.node.children.find(child => child.node.id == 'NATGateway') as CfnNatGateway
-      // Delete the default EIP created by CDK
-      subnet.node.tryRemoveChild('EIP')
-      // Override the allocationId on the NATGateway
-      natGateway.allocationId = Object.values(allocationIds)[index]
-    })
-
-    Object.keys(allocationIds).forEach((ref, index) => {
-      new cdk.CfnOutput(this, `vpc-eip-${index}`, { value: ref })
-    })
-
-    new rds.DatabaseInstance(this, `${name}-dev`, {
+    new rds.DatabaseInstance(this, `${name}dev`, {
       vpc,
       engine: rds.DatabaseInstanceEngine.mysql({
         version: rds.MysqlEngineVersion.VER_8_0_25,
@@ -72,16 +43,16 @@ export class DeploymentStack extends cdk.Stack {
 			},
     })
 
-    const deployReleaseS3Bucket = new Bucket(this, `${name}-deploy-release`)
+    const deployReleaseS3Bucket = new Bucket(this, `${name}deploy-release`)
 
-    const httpApi = new api.HttpApi(this, `${name}-http-api`)
+    const httpApi = new api.HttpApi(this, `${name}http-api`)
 
     new api.HttpStage(this, 'Stage', {
       httpApi,
       stageName: 'dev',
     });
 
-    const lith = new lambda.DockerImageFunction(this, `${name}-lith`, {
+    const lith = new lambda.DockerImageFunction(this, `${name}lith`, {
       code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../')),
       vpc,
     })
@@ -89,10 +60,12 @@ export class DeploymentStack extends cdk.Stack {
       handler: lith,
     })
 
-    // TODO resolve the values in string
-    const authorizer = api.HttpAuthorizer.fromHttpAuthorizerAttributes(this, '', {
-      authorizerId: 'arn:aws:cognito-idp:${self:provider.region}:${self:custom.env.AWS_ACCOUNT_ID}:userpool/${self:custom.env.CONNECT_USER_POOL}',
-      authorizerType: 'cognito',
+    const userPoolClient = cognito.UserPoolClient.fromUserPoolClientId(this, `${name}poolClient-${stage}`, '4m6mr1t2d0si3pfl4rludmqei1')
+    const userPool = cognito.UserPool.fromUserPoolId(this, `${name}userPool`, config.CONNECT_USER_POOL)
+
+    const authorizer = new awsAuthorizers.HttpUserPoolAuthorizer({
+      userPool,
+      userPoolClient,
     })
 
     httpApi.addRoutes({
@@ -109,9 +82,38 @@ export class DeploymentStack extends cdk.Stack {
       integration: lithProxy,
     })
 
-    // TODO add aurora to vpc?
-
     // TODO grant access to created resources
     deployReleaseS3Bucket.grantReadWrite(lith)
+
+    const taskPopulationAsset = new assets.Asset(this, `${name}taskPopulationAsset`, {
+      path: path.resolve(__dirname, '..', '..', 'dist', 'taskPopulation.js'),
+    })
+
+    const taskRunnerAsset = new assets.Asset(this, `${name}taskRunnerAsset`, {
+      path: path.resolve(__dirname, '..', '..', 'dist', 'taskRunner.js'),
+    })
+
+    const taskPopulationLambda = new lambda.Function(this, `${name}taskPopulation-${stage}`, {
+      code: lambda.Code.fromBucket(taskPopulationAsset.bucket, taskPopulationAsset.s3ObjectKey),
+      handler: 'taskPopulation.taskPopulation',
+      runtime: lambda.Runtime.NODEJS_14_X,
+    })
+    
+    const taskRunnerLambda = new lambda.Function(this, `${name}taskRunner-${stage}`, {
+      code: lambda.Code.fromBucket(taskRunnerAsset.bucket, taskRunnerAsset.s3ObjectKey),
+      handler: 'taskRunner.taskRunner',
+      runtime: lambda.Runtime.NODEJS_14_X,
+    })
+
+    const taskPopulationQueue = new sqs.Queue(this, `${name}TaskPopulation-${stage}`, {
+      queueName: 'TaskPopulation',
+    })
+
+    const taskRunnerQueue = new sqs.Queue(this, `${name}TaskRunner-${stage}`, {
+      queueName: 'TaskRunner',
+    })
+
+    taskPopulationLambda.addEventSource(new eventSource.SqsEventSource(taskPopulationQueue))
+    taskRunnerLambda.addEventSource(new eventSource.SqsEventSource(taskRunnerQueue))
   }
 }
