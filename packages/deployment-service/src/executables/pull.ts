a36e0026243f76986d0bab98f315dfd95869db2a
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-04 09:51:25 +0100

    feat: attempting to resolve git clone dir

--- packages/deployment-service/src/executables/pull.ts
+++ packages/deployment-service/src/executables/pull.ts
@@ -1,35 +1,43 @@
 import { PipelineEntity, TaskEntity } from './../entities'
 import { ExecutableType } from './executable'
 import { exec } from 'child_process'
+import fs from 'fs'
 
 export const pull: ExecutableType = async (task: TaskEntity, pipeline: PipelineEntity): Promise<true | never> => {
   console.log('pull...')
   console.log('executable', task)
 
   try {
-    const result = await exec(`git pull ${pipeline.repository} /tmp/project`, (error, stdout) => {
+    process.env.GIT_SSH_COMMAND = 'ssh -o StrictHostKeyChecking=no'
+
+    // TODO solve SSH usage
+
+    const developerDir = `/tmp/project/${pipeline.developerId}/`
+    const cloneDir = `${developerDir}${pipeline.repository?.split('/').pop()}`
+
+    if (!fs.existsSync(developerDir)) {
+      fs.mkdirSync(developerDir, {
+        recursive: true,
+      })
+    }
+
+    const child = exec(`git clone ${pipeline.repository} ${cloneDir}`, (error, stdout) => {
       if (error) {
+        console.log('errored in callback')
         console.error(error)
         throw error
       }
 
-      stdout && console.log(stdout.toString())
+      console.log('callback stdout:=> ', stdout)
+    })
+
+    const result = await new Promise<any>((resolve, reject) => {
+      child.addListener('message', (message) => console.log('stdout:=> ', message))
+      child.addListener('error', reject)
+      child.addListener('exit', (code, signal) => resolve({ code, signal }))
     })
 
     console.log('pull - result', result)
-
-    // await new Promise<void>((resolve, reject) => {
-    //   request(`https://codeload.github.com/${pipeline.repository}/${zipLocation}`)
-    //     .pipe(fs.createWriteStream(`${dir}/${cloneZip}`))
-    //     .on('close', () => {
-    //       resolve()
-    //     })
-    //     .on('error', (error) => {
-    //       reject(error)
-    //     })
-    // })
-
-    // unzip()
   } catch (e) {
     console.log('message', e.message)
     console.log('clone failed')
