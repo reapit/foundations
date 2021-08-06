e6284ace7ad01a3768a651f5f4a7f73ad713d419
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-06 09:19:50 +0100

    fix: corrected project dir prefix

--- packages/deployment-service/src/utils/project-dir.ts
+++ packages/deployment-service/src/utils/project-dir.ts
@@ -4,6 +4,6 @@ import { PipelineEntity } from './../entities'
 export const projectDir = (dir: string, pipeline: PipelineEntity) =>
   resolve(dir, `${(pipeline.repository as string).split('/').pop()}-master`)
 
-export const developerDir = (pipeline: PipelineEntity): string => `/tmp/project/${pipeline.developerId}/`
+export const developerDir = (pipeline: PipelineEntity): string => `/mnt/efs1/project/${pipeline.developerId}/`
 export const cloneDir = (pipeline: PipelineEntity): string =>
   `${developerDir(pipeline)}${pipeline.repository?.split('/').pop()}`
