18e981c24cec67bc9c67227971f14c0a01d6c0a2
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-06 09:18:17 +0100

    feat: changed db config to mysql

--- packages/deployment-service/src/config/db.ts
+++ packages/deployment-service/src/config/db.ts
@@ -2,12 +2,12 @@ import { ConnectionOptions } from 'typeorm'
 import { TaskEntity, PipelineEntity, PipelineRunnerEntity, ReleaseEntity } from './../entities'
 
 export const dbConfig: ConnectionOptions = {
-  type: 'aurora-data-api',
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   logging: true,
-  database: process.env.AURORA_DATABASE as string,
-  region: process.env.DYNAMO_DB_REGION as string,
-  secretArn: process.env.AURORA_SECRET_ARN as string,
-  resourceArn: process.env.AURORA_RESOURCE_ARN as string,
-  entities: [PipelineEntity, PipelineRunnerEntity, TaskEntity, ReleaseEntity],
   synchronize: false,
+  entities: [PipelineEntity, PipelineRunnerEntity, TaskEntity, ReleaseEntity],
 }
