56a669ed2323885b272b7aeb1b4b6b9339b49825
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-06 12:30:35 +0100

    feat: resolved exec methods (#4737)

--- packages/deployment-service/src/executables/install.ts
+++ packages/deployment-service/src/executables/install.ts
@@ -1,6 +1,6 @@
 import { PipelineEntity, TaskEntity } from '../entities'
 import { ExecutableType } from './executable'
-import { exec } from 'child_process'
+import { spawn } from 'child_process'
 import { cloneDir } from '../utils'
 
 export const install: ExecutableType = async (task: TaskEntity, pipeline: PipelineEntity): Promise<true | never> => {
@@ -9,14 +9,20 @@ export const install: ExecutableType = async (task: TaskEntity, pipeline: Pipeli
 
   try {
     // TODO optional yarn usage
-    await exec(`HOME="${cloneDir(pipeline)}" npm install --prefix="${cloneDir(pipeline)}"`, (error, stdout) => {
-      if (error) {
-        throw error
-      }
+    const returned = await new Promise((resolve, reject) => {
+      const result = spawn('npm', ['install', '--no-bin-links'], {
+        cwd: cloneDir(pipeline),
+      })
 
-      console.log('npm stdout:=> ', stdout)
+      result.on('message', (message) => console.log('message', message))
+      result.on('error', (code) => reject({ code, result: 'error' }))
+      result.on('exit', (code) => resolve({ code, result: 'exit' }))
+      result.on('close', (code) => resolve({ code, result: 'close' }))
+      result.stdout.on('data', (s) => console.log('stdout', s))
+      result.stderr.on('data', (e) => console.log('error message', e.toString()))
     })
-    // result && console.log('npm', result.toString())
+
+    console.log('npm', returned)
   } catch (e) {
     console.log('npm errors')
     console.error(e)
