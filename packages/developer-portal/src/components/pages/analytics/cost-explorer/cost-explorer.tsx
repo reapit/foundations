db6ac74276dd76cf18c4e8fd7cdf4bc69ba50c7b
Will McVay <wmcvay@reapit.com>
2021-08-09 12:41:37 +0100

    feat: #4596 #4692 tweaks cost explorer seed data (#4746)

--- packages/developer-portal/src/components/pages/analytics/cost-explorer/cost-explorer.tsx
+++ packages/developer-portal/src/components/pages/analytics/cost-explorer/cost-explorer.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react'
-// Removing as part of https://github.com/reapit/foundations/issues/4692 may need to re-instate later
-// import CostCalculator from './cost-calculator'
+import CostCalculator from './cost-calculator'
 import TransactionHistory from './transaction-history'
 import { Grid, GridItem } from '@reapit/elements-legacy'
 import ErrorBoundary from '@/components/hocs/error-boundary'
 import ServiceChart from './service-chart'
 import CostExplorer from './cost-explorer-component'
+import { useReapitConnect } from '@reapit/connect-session'
+import { reapitConnectBrowserSession } from '../../../../core/connect-session'
+import { selectIsCustomer } from '../../../../selector/auth'
 
 export type CostExplorerTabProps = {}
 
 export const CostExplorerTab: React.FC<CostExplorerTabProps> = () => {
+  const { connectSession } = useReapitConnect(reapitConnectBrowserSession)
+  const isCustomerUser = selectIsCustomer(connectSession)
   return (
     <ErrorBoundary>
       <Grid isMultiLine>
@@ -25,7 +29,7 @@ export const CostExplorerTab: React.FC<CostExplorerTabProps> = () => {
           <CostExplorer />
         </GridItem>
       </Grid>
-      {/* <CostCalculator /> */}
+      {isCustomerUser && <CostCalculator />}
     </ErrorBoundary>
   )
 }
