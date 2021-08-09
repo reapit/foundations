56a8a187f7e7ad936591d387ff840fbcfb906048
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-09 14:30:47 +0100

    feat: started adding cdk implementation

--- packages/deployment-service/src/executables/deploy-react.ts
+++ packages/deployment-service/src/executables/deploy-react.ts
@@ -1,11 +1,33 @@
-import { TaskEntity } from './../entities'
+import { PipelineEntity, TaskEntity } from './../entities'
 import { ExecutableType } from './executable'
+import cdk from '@aws-cdk/core'
+import { developerDir } from '../utils'
+import { Bucket } from '@aws-cdk/aws-s3'
 
-// Add clean up step to remove created projects
-
-export const deployReact: ExecutableType = (task: TaskEntity): Promise<true | never> => {
+export const deployReact: ExecutableType = async (task: TaskEntity, pipeline: PipelineEntity): Promise<true | never> => {
   console.log('deploying react...')
   console.log('executable', task)
 
+  const cdkApp = new cdk.App({
+    outdir: `${developerDir(pipeline)}/cdk`,
+  })
+
+  const stack = new cdk.Stack(cdkApp, `${pipeline.developerId}-${pipeline.repository?.split('/').pop()}`, {
+    tags: {
+      deploymentService: 'true',
+    },
+  })
+
+  const bucket = new Bucket(stack, `${pipeline.developerId}-${pipeline.repository?.split('/').pop()}`, {
+    publicReadAccess: true,
+  })
+
+  cdkApp.synth()
+
   return Promise.resolve(true)
 }
+
+deployReact({}, {
+  developerId: 'test',
+  repository: 'test/test-repo',
+} as PipelineEntity)
