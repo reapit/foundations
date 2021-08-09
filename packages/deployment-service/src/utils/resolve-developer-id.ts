35cb9eda9f608f532d8c7b387e4f1b63ba79827b
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-09 09:27:46 +0100

    refactor: removed dependancy on dynamo

--- packages/deployment-service/src/utils/resolve-developer-id.ts
+++ packages/deployment-service/src/utils/resolve-developer-id.ts
@@ -1,18 +1,11 @@
 import { resolveApiKey } from '@reapit/api-key-verify'
-import { DataMapper } from '@aws/dynamodb-data-mapper'
-import { DynamoDB } from 'aws-sdk'
 import { resolveCustomer } from './resolve-customer'
 import { APIGatewayEvent } from 'aws-lambda'
 
 export const resolveDeveloperId = async (event: APIGatewayEvent): Promise<string | never> => {
   if (event.headers['x-api-key']) {
-    const dataMapper = new DataMapper({
-      client: new DynamoDB({
-        region: 'eu-west-2',
-      }),
-    })
 
-    const apiKey = await resolveApiKey(dataMapper)(event.headers['x-api-key'])
+    const apiKey = await resolveApiKey(event.headers['x-api-key'])
 
     return apiKey?.developerId as string
   }
