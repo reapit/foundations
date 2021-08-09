7a69a611f189982818f2a8b0d86cb1d9820130f6
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-09 16:03:09 +0100

    feat: strarted implementing cdk deployment for react deploy

--- packages/deployment-service/src/executables/deploy-react.ts
+++ packages/deployment-service/src/executables/deploy-react.ts
@@ -1,25 +1,48 @@
 import { PipelineEntity, TaskEntity } from './../entities'
 import { ExecutableType } from './executable'
-import cdk from '@aws-cdk/core'
-import { developerDir } from '../utils'
+import * as cdk from '@aws-cdk/core'
+import { developerDir, cloneDir } from '../utils'
 import { Bucket } from '@aws-cdk/aws-s3'
+import * as bucketDeploy from '@aws-cdk/aws-s3-deployment'
+import * as r53 from '@aws-cdk/aws-route53'
+
+const buildDir = 'build'
 
 export const deployReact: ExecutableType = async (task: TaskEntity, pipeline: PipelineEntity): Promise<true | never> => {
   console.log('deploying react...')
   console.log('executable', task)
 
+  const repoName = pipeline.repository?.split('/').pop()
+
   const cdkApp = new cdk.App({
-    outdir: `${developerDir(pipeline)}/cdk`,
+    outdir: `${developerDir(pipeline)}cdk`,
   })
 
-  const stack = new cdk.Stack(cdkApp, `${pipeline.developerId}-${pipeline.repository?.split('/').pop()}`, {
+  const stack = new cdk.Stack(cdkApp, `${pipeline.developerId}-${repoName}`, {
     tags: {
       deploymentService: 'true',
     },
   })
 
-  const bucket = new Bucket(stack, `${pipeline.developerId}-${pipeline.repository?.split('/').pop()}`, {
+  const bucket = new Bucket(stack, `${pipeline.developerId}-${repoName}`, {
     publicReadAccess: true,
+    websiteIndexDocument: 'index.html',
+  })
+
+  new bucketDeploy.BucketDeployment(stack, 'DeployFiles', {
+    sources: [bucketDeploy.Source.asset(`${cloneDir(pipeline)}/${buildDir}`)],
+    destinationBucket: bucket,
+  })
+
+  const zone = r53.HostedZone.fromHostedZoneAttributes(stack, `main-zone`, {
+    hostedZoneId: 'Z02367201ZA0CZPSM3N2H',
+    zoneName: 'dev.paas.reapit.cloud',
+  })
+
+  const cName = new r53.CnameRecord(stack, `${repoName}-cname`, {
+    zone: zone,
+    recordName: repoName,
+    domainName: bucket.bucketWebsiteDomainName
   })
 
   cdkApp.synth()
@@ -29,5 +52,5 @@ export const deployReact: ExecutableType = async (task: TaskEntity, pipeline: Pi
 
 deployReact({}, {
   developerId: 'test',
-  repository: 'test/test-repo',
+  repository: 'test/reapit-react-test',
 } as PipelineEntity)
--- packages/deployment-service/src/utils/project-dir.ts
+++ packages/deployment-service/src/utils/project-dir.ts
@@ -4,6 +4,9 @@ import { PipelineEntity } from './../entities'
 export const projectDir = (dir: string, pipeline: PipelineEntity) =>
   resolve(dir, `${(pipeline.repository as string).split('/').pop()}-master`)
 
-export const developerDir = (pipeline: PipelineEntity): string => `/mnt/efs1/project/${pipeline.developerId}/`
+// const startDir = 'mnt/efs1'
+const startDir = 'tmp'
+
+export const developerDir = (pipeline: PipelineEntity): string => `/${startDir}/project/${pipeline.developerId}/`
 export const cloneDir = (pipeline: PipelineEntity): string =>
   `${developerDir(pipeline)}${pipeline.repository?.split('/').pop()}`
