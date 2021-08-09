943f5d42b9fa71db412a66d8a3951c644e4b8d5d
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-09 09:23:59 +0100

    feat: added invokable method for api key verify

--- packages/api-key-service/src/functions/get-api-key.ts
+++ packages/api-key-service/src/functions/get-api-key.ts
@@ -1,9 +1,10 @@
 import { ApiKeyModel } from '@reapit/api-key-verify'
-import { httpHandler, UnauthorizedException } from '@homeservenow/serverless-aws-handler'
+import { httpHandler, HttpStatusCode, UnauthorizedException } from '@homeservenow/serverless-aws-handler'
 import { connectSessionVerifyDecodeIdTokenWithPublicKeys, LoginIdentity } from '@reapit/connect-session'
 import { getApiKey as get } from '../services'
 import publicKeys from '../../public-keys.json'
 import { defaultOutputHeaders } from './../constants'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda'
 
 export const getApiKey = httpHandler<void, ApiKeyModel>({
   defaultOutputHeaders,
@@ -30,3 +31,49 @@ export const getApiKey = httpHandler<void, ApiKeyModel>({
     })
   },
 })
+
+export const getApiKeyViaInvoke: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
+  let customer: LoginIdentity | undefined
+
+  const payload = JSON.parse(event.body || '{}')
+
+  const apiKeyHeader = payload.apiKey
+
+  if (!apiKeyHeader) {
+    return {
+      body: '',
+      statusCode: HttpStatusCode.NOT_FOUND,
+    }
+  }
+
+  try {
+    customer = await connectSessionVerifyDecodeIdTokenWithPublicKeys(
+      apiKeyHeader,
+      process.env.CONNECT_USER_POOL as string,
+      publicKeys,
+    )
+
+    if (typeof customer === 'undefined' || !customer.developerId) {
+      return {
+        body: '',
+        statusCode: HttpStatusCode.NOT_FOUND,
+      }
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      body: '',
+      statusCode: HttpStatusCode.NOT_FOUND,
+    }
+  }
+
+  const apiKey = await get({
+    id: event.pathParameters?.id,
+    developerId: customer?.developerId,
+  })
+
+  return {
+    body: JSON.stringify(apiKey),
+    statusCode: HttpStatusCode.OK,
+  }
+}
