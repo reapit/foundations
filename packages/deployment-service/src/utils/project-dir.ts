f0617ead7e83ddb986e36632c98ab00012c558b5
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-10 14:14:11 +0100

    fix: corrected efs settings

--- packages/deployment-service/src/utils/project-dir.ts
+++ packages/deployment-service/src/utils/project-dir.ts
@@ -4,8 +4,8 @@ import { PipelineEntity } from './../entities'
 export const projectDir = (dir: string, pipeline: PipelineEntity) =>
   resolve(dir, `${(pipeline.repository as string).split('/').pop()}-master`)
 
-// const startDir = 'mnt/efs1'
-const startDir = 'tmp'
+const startDir = 'mnt/efs1'
+// const startDir = 'tmp'
 
 export const developerDir = (pipeline: PipelineEntity): string => `/${startDir}/project/${pipeline.developerId}/`
 export const cloneDir = (pipeline: PipelineEntity): string =>
