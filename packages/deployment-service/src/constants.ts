2725263cd957237ee64bbac889248c6d0940ad49
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-18 10:19:58 +0100

    feat: updated avaiable queues

--- packages/deployment-service/src/constants.ts
+++ packages/deployment-service/src/constants.ts
@@ -1,15 +1,9 @@
 import { DefaultHeadersInterface } from '@homeservenow/serverless-aws-handler'
-import { resolve } from 'path'
 
 export const defaultOutputHeaders: DefaultHeadersInterface = {
   'Access-Control-Allow-Origin': '*',
 }
 
 export enum QueueNames {
-  TASK_RUNNER = 'https://sqs.eu-west-2.amazonaws.com/028446965111/TaskRunner',
-  TASK_POPULATION = 'https://sqs.eu-west-2.amazonaws.com/028446965111/TaskPopulation',
+  CODE_BUILD_EXECUTOR = 'https://sqs.eu-west-2.amazonaws.com/028446965111/CodebuildExecutor',
 }
-
-export const cloneDir = 'project'
-export const dir = resolve('/tmp', cloneDir)
-export const cloneZip = 'resource.zip'
