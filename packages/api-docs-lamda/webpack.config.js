70f3b453eeeb4646b02f5206d6c787869324f506
Will McVay <wmcvay@reapit.com>
2021-08-18 15:24:06 +0100

    fix: #4236 remove docs lambda

--- packages/api-docs-lamda/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const slsw = require('serverless-webpack')
-const { webpackConfigNode } = require('@reapit/ts-scripts')
-
-webpackConfigNode.entry = slsw.lib.entries
-webpackConfigNode.optimization.minimize = slsw.lib.webpack.isLocal ? false : true
-
-module.exports = webpackConfigNode
