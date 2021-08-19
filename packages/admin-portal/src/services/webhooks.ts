2642dd5ddafd736fc507685168bbc395fcd543b0
Will McVay <wmcvay@reapit.com>
2021-08-19 15:32:40 +0100

    feat: #4766 adds additional admin apps functionality (#4816)

---
+++ packages/admin-portal/src/services/webhooks.ts
@@ -0,0 +1,54 @@
+import { fetcher, setQueryParams } from '@reapit/elements-legacy'
+import { getPlatformHeaders, logger } from '@reapit/utils'
+import { URLS } from './constants'
+import { reapitConnectBrowserSession } from '../core/connect-session'
+
+export interface WebhookModel {
+  id?: string
+  created?: string
+  modified?: string
+  applicationId?: string
+  url?: string
+  description?: string
+  topicIds?: string[]
+  customerIds?: string[]
+  active?: boolean
+}
+
+export type PagingLinkModel = {
+  href?: string
+}
+
+export interface PagedResultWebhookModel {
+  _embedded?: WebhookModel[]
+  pageNumber?: number
+  pageSize?: number
+  pageCount?: number
+  totalCount?: number
+  _links?: {
+    [key: string]: PagingLinkModel
+  }
+}
+
+export interface FetchWebhooksParms {
+  pageNumber?: number
+  pageSize?: number
+  sortBy?: string
+  applicationId?: string[]
+  active?: boolean
+}
+
+export const fetchWebookSubscriptions = async (params: FetchWebhooksParms): Promise<PagedResultWebhookModel> => {
+  try {
+    const response = await fetcher({
+      url: `${URLS.webhookSubscriptions}?${setQueryParams(params)}`,
+      api: window.reapit.config.platformApiUrl,
+      method: 'GET',
+      headers: await getPlatformHeaders(reapitConnectBrowserSession, 'latest'),
+    })
+    return response
+  } catch (error) {
+    logger(error)
+    throw error
+  }
+}
