efc2a1c0eb1a32f1e45c81e5cd52bcd9f2a2dbe8
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-08-20 09:54:21 +0100

    feat: added new queue to enum

--- packages/deployment-service/src/constants.ts
+++ packages/deployment-service/src/constants.ts
@@ -6,4 +6,5 @@ export const defaultOutputHeaders: DefaultHeadersInterface = {
 
 export enum QueueNames {
   CODE_BUILD_EXECUTOR = 'https://sqs.eu-west-2.amazonaws.com/028446965111/CodebuildExecutor',
+  CODE_BUILD_VERSION_DEPLOY = 'https://sqs.eu-west-2.amazonaws.com/028446965111/CodebuildDeploy',
 }
