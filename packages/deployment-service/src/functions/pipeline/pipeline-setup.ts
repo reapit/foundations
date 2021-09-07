165bf6fba543837b9266ae8836c71990da87f7d1
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-07 14:49:28 +0100

    feat: added cname creation and try catch handling

--- packages/deployment-service/src/functions/pipeline/pipeline-setup.ts
+++ packages/deployment-service/src/functions/pipeline/pipeline-setup.ts
@@ -3,45 +3,87 @@ import { Callback, SQSEvent, SQSHandler, Context } from 'aws-lambda'
 import { plainToClass } from 'class-transformer'
 import { PipelineEntity } from '../../entities'
 import { v4 as uuid } from 'uuid'
+import { Route53Client, ChangeResourceRecordSetsCommand } from '@aws-sdk/client-route-53'
+import { sqs } from '../../services'
+import { QueueNames } from 'src/constants'
 
 export const pipelineSetup: SQSHandler = async (event: SQSEvent, context: Context, callback: Callback) => {
   await Promise.all(
     event.Records.map(async (record) => {
-      const message = JSON.parse(record.body)
-      const pipeline = plainToClass(PipelineEntity, message)
+      try {
+        const message = JSON.parse(record.body)
+        const pipeline = plainToClass(PipelineEntity, message)
 
-      const client = new CloudFrontClient({})
+        const r53Client = new Route53Client({})
 
-      const command = new CreateDistributionCommand({
-        DistributionConfig: {
-          // DefaultRootObject: '', // I think this is /index.html?
-          Origins: {
-            Quantity: 1,
-            Items: [
+        const cnameCommand = new ChangeResourceRecordSetsCommand({
+          HostedZoneId: '',
+          ChangeBatch: {
+            Changes: [
               {
-                Id: uuid(), // uuid
-                DomainName: `${process.env.DEPLOYMENT_LIVE_BUCKET_NAME}`, // string of S3 bucket
-                OriginPath: `/pipeline/${pipeline.uniqueRepoName}`, // path to directory in bucket to view
+                Action: 'CREATE',
+                ResourceRecordSet: {
+                  Type: 'CNAME',
+                  Name: pipeline.subDomain,
+                },
               },
             ],
+            Comment: `Adding additional cname for pipeline [${pipeline.id}]`,
           },
-          Aliases: {
-            Quantity: 1,
-            Items: [
-              '', // not sure what this is suppose to be?
-            ],
-          },
-          Comment: 'Cloudfront distribution for project ...',
-          Enabled: true,
-          CallerReference: 'project-name', // another unique reference to prevent distribution duplication
-          DefaultCacheBehavior: {
-            TargetOriginId: '', // no idea
-            ViewerProtocolPolicy: 'redirect-to-https', // no idea
-          },
-        },
-      })
+        })
 
-      return client.send(command)
+        await r53Client.send(cnameCommand)
+
+        const frontClient = new CloudFrontClient({})
+
+        const distroCommand = new CreateDistributionCommand({
+          DistributionConfig: {
+            // DefaultRootObject: '', // I think this is /index.html?
+            Origins: {
+              Quantity: 1,
+              Items: [
+                {
+                  Id: uuid(), // uuid
+                  DomainName: `${process.env.DEPLOYMENT_LIVE_BUCKET_NAME}`, // string of S3 bucket
+                  OriginPath: `/pipeline/${pipeline.uniqueRepoName}`, // path to directory in bucket to view
+                },
+              ],
+            },
+            Aliases: {
+              Quantity: 1,
+              Items: [
+                `${pipeline.subDomain}`, // not sure what this is suppose to be?
+              ],
+            },
+            Comment: 'Cloudfront distribution for project ...',
+            Enabled: true,
+            CallerReference: 'project-name', // another unique reference to prevent distribution duplication
+            DefaultCacheBehavior: {
+              TargetOriginId: '', // no idea
+              ViewerProtocolPolicy: 'redirect-to-https', // no idea
+            },
+          },
+        })
+
+        await frontClient.send(distroCommand)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        await new Promise<void>((resolve, reject) =>
+          sqs.deleteMessage(
+            {
+              ReceiptHandle: record.receiptHandle,
+              QueueUrl: QueueNames.PIPELINE_SETUP,
+            },
+            (err) => {
+              if (err) {
+                reject(err)
+              }
+              resolve()
+            },
+          ),
+        )
+      }
     }),
   )
 
