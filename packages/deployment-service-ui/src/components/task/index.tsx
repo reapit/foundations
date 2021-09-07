2594b10ba3b159a6a39942e5d0cfaf3e8f7897c9
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-07 09:38:01 +0100

    feat: basic timer for elapsed time in UI

--- packages/deployment-service-ui/src/components/task/index.tsx
+++ packages/deployment-service-ui/src/components/task/index.tsx
@@ -5,6 +5,7 @@ import { ElPipelineTask } from './task.element'
 import { shleemy } from 'shleemy'
 import { cx } from '@linaria/core'
 import { taskFunctionToFriendlyName } from '../../utils/friendly-function-name'
+import { useStopwatch } from 'react-timer-hook'
 
 const pipelineStatusToIntent = (status: string): Intent => {
   switch (status) {
@@ -21,18 +22,39 @@ const pipelineStatusToIntent = (status: string): Intent => {
   }
 }
 
-const toElapsedTime = (task: TaskModelInterface) => {
-  return task.elapsedTime ? `🕒 ${task.elapsedTime}secs` : ''
+const toElapsedTime = (seconds?: string) => {
+  return seconds ? `🕒 ${seconds}secs` : ''
+}
+
+const AutoTimer = () => {
+  const { seconds, minutes } = useStopwatch({
+    autoStart: true,
+  })
+
+  const calculatedSeconds = (minutes * 60) + seconds
+
+  return <>{toElapsedTime(calculatedSeconds.toString())}</>
+}
+
+const Timer = ({ started, elapsedTime }: { started: boolean; elapsedTime?: string }) => {
+  return <>{started ? <AutoTimer />: elapsedTime ? toElapsedTime(elapsedTime) : null}</>
 }
 
 export const PipelineTask = ({ task, index }: { task: TaskModelInterface; index: number }) => {
-  const started =
+  const startedAt =
     task.startTime && task.startTime.substr(0, 1) !== '0' ? shleemy(task.startTime).forHumans : 'not started'
+
+  let started = false
+
+  if (task.buildStatus === 'IN_PROGRESS') {
+    started = true
+  }
 
   return (
     <ElPipelineTask className={cx(`order-${index + 1}`)}>
       <StatusIndicator intent={pipelineStatusToIntent(task.buildStatus as string)} shape="tag" />{' '}
-      {taskFunctionToFriendlyName(task.functionName as string)} {toElapsedTime(task)} - {started}
+      {taskFunctionToFriendlyName(task.functionName as string)}{' '}
+      <Timer elapsedTime={task.elapsedTime} started={started} /> - {startedAt}
     </ElPipelineTask>
   )
 }
