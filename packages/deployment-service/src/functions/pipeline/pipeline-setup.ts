3d865efdd72ea767d55d804f70c24d7ca5b2c747
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-08 15:20:30 +0100

    feat: add s3 publish and distro before r53 creation

--- packages/deployment-service/src/functions/pipeline/pipeline-setup.ts
+++ packages/deployment-service/src/functions/pipeline/pipeline-setup.ts
@@ -4,7 +4,7 @@ import { plainToClass } from 'class-transformer'
 import { PipelineEntity } from '../../entities'
 import { v4 as uuid } from 'uuid'
 import { Route53Client, ChangeResourceRecordSetsCommand } from '@aws-sdk/client-route-53'
-import { sqs } from '../../services'
+import { s3Client, sqs } from '../../services'
 import { QueueNames } from '../../constants'
 
 export const pipelineSetup: SQSHandler = async (event: SQSEvent, context: Context, callback: Callback) => {
@@ -14,37 +14,38 @@ export const pipelineSetup: SQSHandler = async (event: SQSEvent, context: Contex
         const message = JSON.parse(record.body)
         const pipeline = plainToClass(PipelineEntity, message)
 
-        const r53Client = new Route53Client({})
+        console.log('pushing to s3')
+        await new Promise<void>((resolve, reject) => s3Client.putObject({
+          Bucket: process.env.DEPLOYMENT_LIVE_BUCKET_NAME as string,
+          Key: `pipeline/${pipeline.uniqueRepoName}/index.html`,
+          Body: '<html><body>Deployment required</body></html>',
+        }, (error) => {
+          error ? reject(error) : resolve()
+        }))
 
-        const cnameCommand = new ChangeResourceRecordSetsCommand({
-          HostedZoneId: process.env.HOSTED_ZONE_ID,
-          ChangeBatch: {
-            Changes: [
-              {
-                Action: 'CREATE',
-                ResourceRecordSet: {
-                  Type: 'A',
-                  Name: pipeline.subDomain,
-                },
-              },
-            ],
-            Comment: `Adding additional cname for pipeline [${pipeline.id}]`,
+        console.log('creating distro', {
+          repoName: pipeline.uniqueRepoName,
+          Origins: {
+            DomainName: `${process.env.DEPLOYMENT_LIVE_BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com`, // url of S3 bucket
+            OriginPath: `/pipeline/${pipeline.uniqueRepoName}`, // path to directory in bucket to view
           },
         })
 
-        await r53Client.send(cnameCommand)
+        const frontClient = new CloudFrontClient({
+          region: process.env.REGION,
+        })
 
-        const frontClient = new CloudFrontClient({})
+        const id = uuid()
 
         const distroCommand = new CreateDistributionCommand({
           DistributionConfig: {
-            // DefaultRootObject: '', // I think this is /index.html?
+            DefaultRootObject: 'index.html',
             Origins: {
               Quantity: 1,
               Items: [
                 {
-                  Id: uuid(), // uuid
-                  DomainName: `${process.env.DEPLOYMENT_LIVE_BUCKET_NAME}`, // string of S3 bucket
+                  Id: id,
+                  DomainName: `${process.env.DEPLOYMENT_LIVE_BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com`, // url of S3 bucket
                   OriginPath: `/pipeline/${pipeline.uniqueRepoName}`, // path to directory in bucket to view
                 },
               ],
@@ -52,22 +53,70 @@ export const pipelineSetup: SQSHandler = async (event: SQSEvent, context: Contex
             Aliases: {
               Quantity: 1,
               Items: [
-                `${pipeline.subDomain}`, // not sure what this is suppose to be?
+                `${pipeline.subDomain}.reapit.cloud`, // not sure what this is suppose to be?
               ],
             },
-            Comment: `Cloudfront distribution for project [${pipeline.id}]`,
+            Comment: `Cloudfront distribution for pipeline [${pipeline.id}]`,
             Enabled: true,
             CallerReference: `${pipeline.subDomain}`, // another unique reference to prevent distribution duplication
             DefaultCacheBehavior: {
-              TargetOriginId: '', // no idea
+              TargetOriginId: id, // has to be the same as origin id :eyes_roll:
               ViewerProtocolPolicy: 'redirect-to-https', // no idea
             },
           },
         })
 
-        await frontClient.send(distroCommand)
+        const result2 = await frontClient.send(distroCommand)
+        console.log('result2', result2)
+
+        const r53Client = new Route53Client({
+          region: 'eu-west-2',
+        })
+
+        console.log(
+          'creating A record',
+          JSON.stringify({
+            HostedZoneId: process.env.HOSTED_ZONE_ID,
+            ChangeBatch: {
+              Changes: [
+                {
+                  Action: 'CREATE',
+                  ResourceRecordSet: {
+                    Type: 'A',
+                    Name: `${pipeline.subDomain}.reapit.cloud`,
+                    Region: process.env.REGION,
+                  },
+                },
+              ],
+              Comment: `Adding additional A record for pipeline [${pipeline.id}]`,
+            },
+          }),
+        )
+
+        const ACommand = new ChangeResourceRecordSetsCommand({
+          HostedZoneId: process.env.HOSTED_ZONE_ID,
+          ChangeBatch: {
+            Changes: [
+              {
+                Action: 'CREATE',
+                ResourceRecordSet: {
+                  Type: 'A',
+                  Name: `${pipeline.subDomain}.reapit.cloud`,
+                  Region: process.env.REGION,
+                },
+              },
+            ],
+            Comment: `Adding additional cname for pipeline [${pipeline.id}]`,
+          },
+        })
+
+        console.log('sending...')
+
+        const result = await r53Client.send(ACommand)
+        console.log('result', result)
       } catch (e) {
         console.error(e)
+        throw e
       } finally {
         await new Promise<void>((resolve, reject) =>
           sqs.deleteMessage(
@@ -89,3 +138,27 @@ export const pipelineSetup: SQSHandler = async (event: SQSEvent, context: Contex
 
   return callback(null, `Successfully processed ${event.Records.length} records.`)
 }
+
+// pipelineSetup({
+//   Records: [{
+//     messageId: '',
+//     receiptHandle: '',
+//     messageAttributes: {},
+//     awsRegion: 'eu-west-2',
+//     md5OfBody: '',
+//     attributes: {
+//       ApproximateReceiveCount: 'string',
+//       SentTimestamp: 'string',
+//       SenderId: 'string',
+//       ApproximateFirstReceiveTimestamp: '',
+//     },
+//     eventSource: '',
+//     eventSourceARN: '',
+//     body: JSON.stringify({
+//       id: '105fc8aa-aa6a-4854-9720-940661bad776',
+//       subDomain: 'simple-roll',
+//       developerId: '614d8827-93c0-485b-839d-da4d103ba767',
+//       repository: "https://github.com/bashleigh/reapit-react-test"
+//     }),
+//   }],
+// }, {} as Context, () => {})
