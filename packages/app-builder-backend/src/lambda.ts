13e763088c0808913b3e5fd1e8dc511f149ac443
Josh Balfour <josh@joshbalfour.co.uk>
2021-09-14 11:59:11 +0100

    property listing works

--- packages/app-builder-backend/src/lambda.ts
+++ packages/app-builder-backend/src/lambda.ts
@@ -1,17 +1,13 @@
 import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server-lambda'
-import { buildSchema } from 'type-graphql'
 import express from 'express'
 
-import { BookResolver } from './resolvers/book-resolver'
-import { AuthorResolver } from './resolvers/author-resolver'
 import { Context } from './types'
 import { ejectAppRoute } from './eject/route'
+import { getSchema } from './get-schema'
 
 const createHandler = async () => {
-  const schema = await buildSchema({
-    resolvers: [BookResolver, AuthorResolver],
-  })
+  const schema = await getSchema()
 
   const server = new ApolloServer({
     schema,
@@ -21,7 +17,8 @@ const createHandler = async () => {
         throw new Error('Must have the authorization header')
       }
       return {
-        accessToken: authorization.split(' ')[1],
+        idToken: authorization?.split(' ')[1],
+        accessToken: event.headers['reapit-connect-token'] as string,
       }
     },
   })
