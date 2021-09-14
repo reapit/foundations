d202ec3f66e488a31f9be096b1656d11892f90e3
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-14 15:47:45 +0100

    feat: unique subdomain check subscriber

---
+++ packages/deployment-service/src/subscribers/sub-domain.ts
@@ -0,0 +1,21 @@
+import { EntitySubscriberInterface, InsertEvent } from 'typeorm'
+import { PipelineEntity } from './../entities'
+import generate from 'project-name-generator'
+
+export class SubDomainSubscriber implements EntitySubscriberInterface {
+  async beforeInsert(event: InsertEvent<PipelineEntity>) {
+    let unique = false
+
+    while (unique === false) {
+      event.entity.subDomain = generate().dashed
+
+      const result = await event.manager.getRepository(PipelineEntity).count({
+        where: {
+          subDomain: event.entity.subDomain,
+        },
+      })
+
+      unique = result === 0
+    }
+  }
+}
