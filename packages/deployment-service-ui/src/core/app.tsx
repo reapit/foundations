115d2d8e931819b03a108b54f95391f632757b77
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-15 13:20:11 +0100

    feat: private pusher channels

--- packages/deployment-service-ui/src/core/app.tsx
+++ packages/deployment-service-ui/src/core/app.tsx
@@ -1,30 +1,49 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 import Router from './router'
 import ErrorBoundary from '@/components/hocs/error-boundary'
-import { injectSwitchModeToWindow } from '@reapit/elements-legacy'
+import { FlexContainerBasic, injectSwitchModeToWindow } from '@reapit/elements-legacy'
+import { Loader } from '@reapit/elements'
 import { PusherProvider } from '@harelpls/use-pusher'
 import { MediaStateProvider, NavStateProvider } from '@reapit/elements'
-import { URLS } from '../constants/api'
+import { BASE_HEADERS, URLS } from '../constants/api'
+import { useReapitConnect } from '@reapit/connect-session'
+import { reapitConnectBrowserSession } from './connect-session'
 
 injectSwitchModeToWindow()
 
 import '@/styles/index.css'
 
 const App = () => {
+  const { connectSession } = useReapitConnect(reapitConnectBrowserSession)
+  const [authenticating, setAuthenticating] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (connectSession) setAuthenticating(false)
+  }, [connectSession])
+
   return (
     <ErrorBoundary>
       <NavStateProvider>
         <MediaStateProvider>
-          <PusherProvider
-            cluster="eu"
-            clientKey={window.reapit.config.PUSHER_KEY}
-            authEndpoint={`${URLS.DEPLOYMENT_SERVICE_HOST}/pusher/auth`}
-            auth={{
-              headers: { Authorization: 'Bearer' },
-            }}
-          >
-            <Router />
-          </PusherProvider>
+          {authenticating ? (
+            <FlexContainerBasic centerContent flexColumn hasBackground hasPadding>
+              <Loader label="authorizing" />
+            </FlexContainerBasic>
+          ) : (
+            <PusherProvider
+              cluster="eu"
+              clientKey={window.reapit.config.PUSHER_KEY}
+              authEndpoint={`${URLS.DEPLOYMENT_SERVICE_HOST}/pusher/auth`}
+              auth={{
+                headers: {
+                  ...BASE_HEADERS,
+                  Authorization: connectSession?.idToken,
+                },
+              }}
+            >
+              <Router />
+            </PusherProvider>
+          )}
         </MediaStateProvider>
       </NavStateProvider>
     </ErrorBoundary>
