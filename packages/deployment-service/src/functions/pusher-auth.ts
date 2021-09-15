6d78c8e5c45583ff42c962e5b6fe09d1eb5cb67e
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-15 13:13:08 +0100

    fix: corrected property names

--- packages/deployment-service/src/functions/pusher-auth.ts
+++ packages/deployment-service/src/functions/pusher-auth.ts
@@ -2,13 +2,26 @@ import { httpHandler } from '@homeservenow/serverless-aws-handler'
 import { pusher } from '../services'
 import Pusher from 'pusher'
 
-export const pusherAuthentication = httpHandler<{ socketId: string; channel: string }, Pusher.AuthResponse>({
+export const pusherAuthentication = httpHandler<{ socket_id: string; channel_name: string }, Pusher.AuthResponse>({
   defaultOutputHeaders: {
     'Access-Control-Allow-Origin': '*',
   },
+  serialise: {
+    input: (event) => {
+      return JSON.parse(
+        '{"' +
+          decodeURI(event.body as string)
+            .replace(/"/g, '\\"')
+            .replace(/&/g, '","')
+            .replace(/=/g, '":"') +
+          '"}',
+      )
+    },
+  },
   handler: async ({ body }) => {
-    const { socketId, channel } = body
-    const auth = pusher.authenticate(socketId, channel)
+    const { socket_id, channel_name } = body
+
+    const auth = pusher.authenticate(socket_id, channel_name)
 
     return auth
   },
