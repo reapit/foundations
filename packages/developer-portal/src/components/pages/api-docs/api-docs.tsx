7efe01db76cc065df6fa823f8073381557220e2f
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-20 11:10:22 +0100

    fix: added an 's' to API

--- packages/developer-portal/src/components/pages/api-docs/api-docs.tsx
+++ packages/developer-portal/src/components/pages/api-docs/api-docs.tsx
@@ -44,7 +44,7 @@ const ApiDocsPage: React.FC = () => {
           <Title>Docs</Title>
           <SecondaryNav className={elMb9}>
             <SecondaryNavItem onClick={navigate(history, Routes.API_DOCS)} active={isDocsPage}>
-              API
+              APIs
             </SecondaryNavItem>
             <SecondaryNavItem onClick={navigate(history, Routes.ANALYTICS_SCHEMA_DOCS)} active={isSchemaPage}>
               Warehouse
