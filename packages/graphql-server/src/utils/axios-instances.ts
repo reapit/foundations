9f40790643cfb858f1d8735bca953844690ff26c
Josh Balfour <josh@joshbalfour.co.uk>
2021-09-22 16:16:31 +0100

    add exponential backoff for rate-limited requests

--- packages/graphql-server/src/utils/axios-instances.ts
+++ packages/graphql-server/src/utils/axios-instances.ts
@@ -1,12 +1,24 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import axiosRetry from 'axios-retry'
+
 import { API_VERSION } from '../constants/api'
 
+const retryConfig: AxiosRequestConfig['axios-retry'] = {
+  retries: 10,
+  retryDelay: axiosRetry.exponentialDelay,
+  retryCondition: (error) => {
+    return error.response && error.response.status === 429
+  },
+}
+
 export const createPlatformAxiosInstance = () => {
-  return axios.create({
+  const instance = axios.create({
     baseURL: process.env.PLATFORM_API_BASE_URL,
     headers: {
       'Content-Type': 'application/json',
       'api-version': API_VERSION,
     },
   })
+  axiosRetry(instance, retryConfig)
+  return instance
 }
