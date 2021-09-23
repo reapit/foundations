4108cae69530427ccb576292c5df24e309ba1270
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-23 14:55:34 +0100

    feat: codebuild topic

--- packages/deployment-service/cdk/lib/create-sns.ts
+++ packages/deployment-service/cdk/lib/create-sns.ts
@@ -1,16 +1,12 @@
 import { Construct } from "@aws-cdk/core"
-import * as sns from '@aws-cdk/aws-sns'
-import * as subs from '@aws-cdk/aws-sns-subscriptions'
+import { Topic } from '@aws-cdk/aws-sns'
 
 
-export const createSnsTopic = (app: Construct) => {
-  const topic = new sns.Topic(app as any, 'cloud-deployment-service-codebuild-topic', {
+export const createSnsTopic = (app: Construct): Topic => {
+  return new Topic(app as any, 'cloud-deployment-service-codebuild-topic', {
     contentBasedDeduplication: true,
     displayName: 'codebuild topic sns for deployment-service',
     fifo: true,
     topicName: 'cloud-deployment-service-codebuild-topic',
   })
-
-  // TODO lambda trigger/endpoint?
-  topic.addSubscription(new subs.UrlSubscription(''));
 }
