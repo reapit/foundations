02605a150728f98c4e0cd44dc5fa9a4660668689
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-23 12:17:14 +0100

    feat: attempting cdk build webpack

--- packages/deployment-service/cdk/bin/cdk.ts
+++ packages/deployment-service/cdk/bin/cdk.ts
@@ -2,20 +2,29 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkStack } from '../lib/cdk-stack';
+import { exec } from 'child_process';
+import * as path from 'path'
 
-const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const bootstrap = async () => {
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  await exec.__promisify__('yarn build', {
+    cwd: path.resolve(__dirname, '..', '..'),
+  })
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  const app = new cdk.App();
+  new CdkStack(app, 'CdkStack', {
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+    * Account/Region-dependent features and context lookups will not work,
+    * but a single synthesized template can be deployed anywhere. */
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+    /* Uncomment the next line to specialize this stack for the AWS Account
+    * and Region that are implied by the current CLI configuration. */
+    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+    /* Uncomment the next line if you know exactly what Account and Region you
+    * want to deploy the stack to. */
+    // env: { account: '123456789012', region: 'us-east-1' },
+
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  })
+}
