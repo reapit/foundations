d4dcf781b74b9805905c84c2606f1257ce684ff3
Will McVay <wmcvay@reapit.com>
2021-10-05 17:24:20 +0100

    feat: #5128 elements wizard (#5170)
    * feat: #5128 wip
    
    * feat: #5128 adds vertical step wizard

--- packages/elements/src/components/steps/__tests__/steps.test.tsx
+++ packages/elements/src/components/steps/__tests__/steps.test.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { ElStep } from '../__styles__'
-import { Steps, StepsProps } from '../index'
-
-const props: StepsProps = {
-  steps: ['1', '2', '3', '4', '5'],
-  onStepClick: jest.fn(),
-}
+import { Steps, StepsProps, StepsVertical, StepsVerticalProps } from '../index'
 
 describe('Steps', () => {
+  const props: StepsProps = {
+    steps: ['1', '2', '3', '4', '5'],
+    onStepClick: jest.fn(),
+  }
+
   it('should match a snapshot', () => {
     expect(shallow(<Steps {...props} />)).toMatchSnapshot()
   })
@@ -19,6 +19,53 @@ describe('Steps', () => {
 
   it('should fire the onStepClick event correctly', () => {
     const wrapper = shallow(<Steps {...props} />)
+    wrapper.find(ElStep).first().simulate('click')
+    expect(props.onStepClick).toHaveBeenCalledTimes(1)
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+})
+
+describe('StepsVertical', () => {
+  const props: StepsVerticalProps = {
+    steps: [
+      {
+        item: '1',
+        content: 'Lorem, ipsum dolor',
+      },
+      {
+        item: '2',
+        content: 'Lorem, ipsum dolor',
+      },
+      {
+        item: '3',
+        content: 'Lorem, ipsum dolor',
+      },
+      {
+        item: '4',
+        content: 'Lorem, ipsum dolor',
+      },
+      {
+        item: '5',
+        content: 'Lorem, ipsum dolor',
+      },
+    ],
+    onStepClick: jest.fn(),
+    selectedStep: '5',
+  }
+
+  it('should match a snapshot', () => {
+    expect(shallow(<StepsVertical {...props} />)).toMatchSnapshot()
+  })
+
+  it('should match a snapshot when selectedStep is different', () => {
+    expect(shallow(<StepsVertical {...props} selectedStep="2" />)).toMatchSnapshot()
+  })
+
+  it('should fire the onStepClick event correctly', () => {
+    const wrapper = shallow(<StepsVertical {...props} />)
     wrapper.find(ElStep).first().simulate('click')
     expect(props.onStepClick).toHaveBeenCalledTimes(1)
   })
