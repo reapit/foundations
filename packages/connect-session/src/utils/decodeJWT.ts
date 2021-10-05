7b0e814843703cf51f71c0b7beb16bc200e70bf2
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-10-05 15:04:02 +0100

    refactor: used const instead of let

--- packages/connect-session/src/utils/decodeJWT.ts
+++ packages/connect-session/src/utils/decodeJWT.ts
@@ -2,16 +2,14 @@ import * as base64 from './base64'
 
 export const decodeJWT = (token: string): { header: any; payload: any } => {
   const parts = token.split('.')
-  let header
-  let payload
 
   if (parts.length !== 3) {
     throw new Error('Cannot decode a malformed JWT')
   }
 
   try {
-    header = JSON.parse(base64.decodeToString(parts[0]))
-    payload = JSON.parse(base64.decodeToString(parts[1]))
+    const header = JSON.parse(base64.decodeToString(parts[0]))
+    const payload = JSON.parse(base64.decodeToString(parts[1]))
 
     return {
       header,
