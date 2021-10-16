b7d1058275ff7f32e0cf97a518cbe74a070e6a40
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-10-16 11:05:09 +0100

    feat: removed rxjs from migrationScript

--- packages/deployment-service/src/functions/migrationRun.ts
+++ packages/deployment-service/src/functions/migrationRun.ts
@@ -1,41 +1,11 @@
 import { OnEventHandler, OnEventRequest, OnEventResponse } from '@aws-cdk/custom-resources/lib/provider-framework/types'
-import { connect, logger } from './../core'
-import { Observable } from 'rxjs'
-import { delay, retryWhen, scan } from 'rxjs/operators'
-import { defer, lastValueFrom } from 'rxjs'
-
-const retryAttempts = 9
-const retryDelay = 3000
-
-const createConnection = async () => {
-  return await lastValueFrom(
-    defer(() => {
-      return connect()
-    }).pipe(handleRetry),
-  )
-}
-
-const handleRetry = <T>(source: Observable<T>) => {
-  return source.pipe(
-    retryWhen((e) =>
-      e.pipe(
-        scan((errorCount, error: Error) => {
-          logger.error(`Unable to connect to the database. Retrying (${errorCount + 1})...`, error.stack)
-          if (errorCount + 1 >= retryAttempts) {
-            throw error
-          }
-          return errorCount + 1
-        }, 0),
-        delay(retryDelay),
-      ),
-    ),
-  )
-}
+import { connect } from './../core'
 
 export const migrationRun: OnEventHandler = async (event: OnEventRequest): Promise<OnEventResponse> => {
+  // console.log('env', process.env)
   const direction: 'run' | 'revert' = event.RequestType === 'Delete' ? 'revert' : 'run'
 
-  const connection = await createConnection()
+  const connection = await connect()
 
   await connection[direction === 'run' ? 'runMigrations' : 'undoLastMigration']()
 
