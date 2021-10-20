dc32eae1bcf0147975c93db72f5b2b9391fba55d
Josh Balfour <joshbalfour@AdminisatorsMBP.localdomain>
2021-10-20 12:14:53 +0100

    fix webpack warning

--- packages/elements/src/components/searchable-dropdown/index.ts
+++ packages/elements/src/components/searchable-dropdown/index.ts
@@ -1,7 +1,13 @@
 export * from './__styles__'
 export {
-  SearchableDropdownProps,
-  ControlledSearchableDropdownProps,
   SearchableDropdown,
   ControlledSearchableDropdown,
 } from './searchable-dropdown'
+
+import {
+  SearchableDropdownProps as T_SearchableDropdownProps,
+  ControlledSearchableDropdownProps as T_ControlledSearchableDropdownProps,
+} from './searchable-dropdown'
+
+export type ControlledSearchableDropdownProps<T> = T_ControlledSearchableDropdownProps<T>
+export type SearchableDropdownProps<T> = T_SearchableDropdownProps<T>
