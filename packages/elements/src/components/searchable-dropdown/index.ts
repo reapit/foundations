cb111218453eb98b045ca237f11e1625b3d5387a
Josh Balfour <josh@joshbalfour.co.uk>
2021-10-20 14:44:38 +0100

    fix: elements webpack warning (#5281)
    * fix webpack warning
    
    * bump version and add to chancelog
    
    * linting
    
    Co-authored-by: Josh Balfour <joshbalfour@AdminisatorsMBP.localdomain>
    Co-authored-by: Will McVay <wmcvay@reapit.com>

--- packages/elements/src/components/searchable-dropdown/index.ts
+++ packages/elements/src/components/searchable-dropdown/index.ts
@@ -1,7 +1,10 @@
 export * from './__styles__'
-export {
-  SearchableDropdownProps,
-  ControlledSearchableDropdownProps,
-  SearchableDropdown,
-  ControlledSearchableDropdown,
+export { SearchableDropdown, ControlledSearchableDropdown } from './searchable-dropdown'
+
+import {
+  SearchableDropdownProps as T_SearchableDropdownProps,
+  ControlledSearchableDropdownProps as T_ControlledSearchableDropdownProps,
 } from './searchable-dropdown'
+
+export type ControlledSearchableDropdownProps<T> = T_ControlledSearchableDropdownProps<T>
+export type SearchableDropdownProps<T> = T_SearchableDropdownProps<T>
