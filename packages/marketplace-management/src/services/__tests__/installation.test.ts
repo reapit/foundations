76c3514813c306cdeef6bf99946001fbcc71e331
Will McVay <wmcvay@reapit.com>
2021-11-02 16:33:10 +0000

    feat: #5345 mm app installs update (#5351)
    Co-authored-by: Josh Balfour <josh@joshbalfour.co.uk>

--- packages/marketplace-management/src/services/__tests__/installation.test.ts
+++ packages/marketplace-management/src/services/__tests__/installation.test.ts
@@ -1,5 +1,5 @@
 import { fetcher } from '@reapit/utils-common'
-import { bulkInstall } from '../installation'
+import { bulkInstall, installOrg, uninstallOrg } from '../installation'
 
 jest.mock('@reapit/utils-common')
 jest.mock('../../core/connect-session')
@@ -7,15 +7,43 @@ jest.mock('../../core/connect-session')
 const mockedFetch = fetcher as jest.Mock
 
 describe('bulkInstall', () => {
-  it('should return a response from the accounts service', async () => {
+  it('should return a response from the installations service', async () => {
     mockedFetch.mockReturnValueOnce({})
     expect(await bulkInstall(['id1'], ['id2'], 'SOME_ID')).toBe(true)
   })
 
-  it('should catch an error if no response from accounts service', async () => {
+  it('should catch an error if no response from installations service', async () => {
     const errorSpy = jest.spyOn(console, 'error')
     mockedFetch.mockReturnValueOnce(undefined as any)
     await bulkInstall(['id1'], ['id2'], 'SOME_ID')
     expect(errorSpy).toHaveBeenLastCalledWith('Failed to create bulk installations')
+  })
+})
+
+describe('installOrg', () => {
+  it('should return a response from the installations service', async () => {
+    mockedFetch.mockReturnValueOnce({})
+    expect(await installOrg({})).toBe(true)
+  })
+
+  it('should catch an error if no response from installations service', async () => {
+    const errorSpy = jest.spyOn(console, 'error')
+    mockedFetch.mockReturnValueOnce(undefined as any)
+    await installOrg({})
+    expect(errorSpy).toHaveBeenLastCalledWith('Failed to create installation')
+  })
+})
+
+describe('uninstallOrg', () => {
+  it('should return a response from the installations service', async () => {
+    mockedFetch.mockReturnValueOnce({})
+    expect(await uninstallOrg({}, 'SOME_ID')).toBe(true)
+  })
+
+  it('should catch an error if no response from installations service', async () => {
+    const errorSpy = jest.spyOn(console, 'error')
+    mockedFetch.mockReturnValueOnce(undefined as any)
+    await uninstallOrg({}, 'SOME_ID')
+    expect(errorSpy).toHaveBeenLastCalledWith('Failed to terminate installation')
   })
 })
