c7ed1cb832acbeffd27571214d9584b7b9173061
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-11-09 16:29:29 +0000

    feat: added basic function to reply to all issues

--- packages/auto-reply-bot/src/index.ts
+++ packages/auto-reply-bot/src/index.ts
@@ -1,3 +1,23 @@
 import { createLambdaFunction, createProbot } from '@probot/adapter-aws-lambda-serverless'
 
-export const webhooks = createLambdaFunction(() => {}, { probot: createProbot() })
+const whitelistEmail = 'reapit.com'
+
+export const webhooks = createLambdaFunction(
+  (app) => {
+    app.on('issues.opened', async (event) => {
+      const authorEmail = event.payload.sender.email
+
+      if (authorEmail.split('@').pop() === whitelistEmail) {
+        // avoid reapit employees
+        // TODO check sender.email is reapit email and not primary email
+        return
+      }
+
+      return Promise.all([
+        event.octokit.issues.createComment(event.issue({ body: 'Hello :wave:' })),
+        event.octokit.issues.addLabels(event.issue({ labels: ['awaiting triage'] })),
+      ])
+    })
+  },
+  { probot: createProbot() },
+)
