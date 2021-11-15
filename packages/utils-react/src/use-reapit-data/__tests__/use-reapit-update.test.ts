d718766d6aa7c0f53a9d8cd654548b3429a84518
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-11-15 15:39:42 +0000

    feat: added additional test for reapit update func

--- packages/utils-react/src/use-reapit-data/__tests__/use-reapit-update.test.ts
+++ packages/utils-react/src/use-reapit-data/__tests__/use-reapit-update.test.ts
@@ -50,7 +50,7 @@ jest.mock('@reapit/utils-common', () => ({
 }))
 
 describe('useReapitUpdate', () => {
-  it('should correctly set loading, fetch data, render a success message and refresh', async () => {
+  it('should correctly set loading, send data without data update', async () => {
     const reapitConnectBrowserSession = {} as unknown as ReapitConnectBrowserSession
     const objectBody = {
       test: true,
@@ -94,5 +94,59 @@ describe('useReapitUpdate', () => {
     expect(result.current[3]).toEqual(null)
 
     expect(mockFetcher).toHaveBeenCalledTimes(1)
+  })
+
+  it('should correctly set loading, send data with data update', async () => {
+    const reapitConnectBrowserSession = {} as unknown as ReapitConnectBrowserSession
+    const objectBody = {
+      test: true,
+    }
+    const mockFetchParams = [
+      'https://api.test.reapit.com/path',
+      {
+        headers: {
+          Authorization: 'Bearer SOME_TOKEN',
+          ['Content-Type']: 'application/json',
+          ['api-version']: 'latest',
+        },
+        method: 'POST',
+        body: JSON.stringify(objectBody),
+      },
+    ]
+
+    mockFetcher.mockReturnValue({
+      headers: {
+        Location: 'https://api.test.reapit.com/path',
+      },
+      json: async () => ({...objectBody, updated: true}),
+    })
+
+    const { result, waitForNextUpdate } = renderHook<{}, ReapitUpdateState<{}, typeof mockData>>(() =>
+      useReapitUpdate<{}, typeof mockData>({
+        reapitConnectBrowserSession,
+        action: 'actionName' as UpdateActionNames,
+        returnUpdatedModel: true,
+      }),
+    )
+    expect(result.current[0]).toBeFalsy()
+    expect(result.current[1]).toBeUndefined()
+    expect(result.current[2]).toBeInstanceOf(Function)
+    expect(result.current[3]).toBeNull()
+
+    act(() => {
+      result.current[2](objectBody)
+    })
+
+    await waitForNextUpdate()
+
+    expect(mockFetcher).toHaveBeenCalledWith(...mockFetchParams)
+    expect(mockFetcher).toHaveBeenCalledTimes(3)
+    expect(mockError).not.toHaveBeenCalled()
+
+    expect(result.current[1]).toEqual({...objectBody, updated: true})
+    expect(result.current[0]).toEqual(false)
+    expect(result.current[3]).toEqual(null)
+
+    expect(mockFetcher).toHaveBeenCalledTimes(3)
   })
 })
