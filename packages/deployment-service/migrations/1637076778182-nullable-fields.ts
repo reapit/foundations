81e05d50fc6d2003b1396c5de50e09bd048397b1
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-11-16 15:57:19 +0000

    fix: migration sql scripts

--- packages/deployment-service/migrations/1637076778182-nullable-fields.ts
+++ packages/deployment-service/migrations/1637076778182-nullable-fields.ts
@@ -3,15 +3,15 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class nullableFields1637076778182 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `pipelines` ALTER COLUMN `cloudFrontId` varchar(255) NULLABLE");
-        await queryRunner.query("ALTER TABLE `pipelines` ALTER COLUMN `aRecordId` varchar(255) NULLABLE");
-        await queryRunner.query("ALTER TABLE `pipelines` ALTER COLUMN `subDomain` varchar(255) NULLABLE");
+        await queryRunner.query("ALTER TABLE `pipelines` CHANGE `cloudFrontId` `cloudFrontId` varchar(255) NULL");
+        await queryRunner.query("ALTER TABLE `pipelines` CHANGE `aRecordId` `aRecordId` varchar(255) NULL");
+        await queryRunner.query("ALTER TABLE `pipelines` CHANGE `subDomain` `subDomain` varchar(255) NULL");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `pipelines` ALTER COLUMN `cloudFrontId` varchar(255) NOT NULL");
-        await queryRunner.query("ALTER TABLE `pipelines` ALTER COLUMN `aRecordId` varchar(255) NOT NULL");
-        await queryRunner.query("ALTER TABLE `pipelines` ALTER COLUMN `subDomain` varchar(255) NOT NULL");
+        await queryRunner.query("ALTER TABLE `pipelines` CHANGE `cloudFrontId` `cloudFrontId` varchar(255) NOT NULL");
+        await queryRunner.query("ALTER TABLE `pipelines` CHANGE `aRecordId` `aRecordId` varchar(255) NOT NULL");
+        await queryRunner.query("ALTER TABLE `pipelines` CHANGE `subDomain` `subDomain` varchar(255) NOT NULL");
     }
 
 }
