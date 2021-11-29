b06eb68e57746a6c11981f7bb4040954e04bb9a8
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-11-29 15:06:42 +0000

    feat: added new command to link existing pipeline to repo

--- packages/cli/src/commands/pipeline/index.ts
+++ packages/cli/src/commands/pipeline/index.ts
@@ -3,11 +3,12 @@ import { ParentCommand } from '../../parent.command'
 import { PipelineCreate } from './create'
 import { DeployPipelineCommand } from './deploy'
 import { PipelineList } from './list'
+import { LinkPipelineCommand } from './link'
 
 @Command({
   name: 'pipeline',
   description: '(BETA) For managing Pipeline deployments',
 })
 export class PipelineCommand extends ParentCommand {
-  commands = [new PipelineCreate(), new PipelineList(), new DeployPipelineCommand()]
+  commands = [new PipelineCreate(), new PipelineList(), new DeployPipelineCommand(), new LinkPipelineCommand()]
 }
