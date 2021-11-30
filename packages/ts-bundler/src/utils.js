16912f3f3095c65c0589d6306fd4bdc298f881ff
Josh Balfour <josh@joshbalfour.co.uk>
2021-11-30 13:02:17 +0000

    remove ts-bundler

--- packages/ts-bundler/src/utils.js
+++
@@ -1,9 +0,0 @@
-const child_process = require('child_process')
-
-const execSync = (cmd, cwd) => {
-  return child_process.execSync(cmd, { stdio: 'inherit', cwd })
-}
-
-module.exports = {
-  execSync,
-}
