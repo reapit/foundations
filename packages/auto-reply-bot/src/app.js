80e35f21526f49e19db6872de1f49cfec812e019
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-12-01 11:32:56 +0000

    feat: implemented new issue responses

--- packages/auto-reply-bot/src/app.js
+++ packages/auto-reply-bot/src/app.js
@@ -1,9 +1,3 @@
-const templateReply =
-  'Thank you for raising a Feature Request. At our next refinement session, the team will discuss the issue and should we agree that development is warranted, we will commit to the work. However, if we need to gather more information or investigate, the relevant label will be added and if applicable, we will add a comment.  \r\nFor more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)'
-
-const labelReply =
-  "We have added the label of 'question' as the nature of your request requires product direction.  \r\nFor information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)"
-
 // The 'from column' the condition is to match
 const nearTermFromColumn = 'second'
 // the 'to column' the condition is to match
@@ -11,48 +5,102 @@ const nearTermToColumn = 'third'
 const nearTerm =
   "This ticket has been moved to our near-term column as have we identified this as a short term goal, we'll assess the effort required and outline a technical specification - please take the time to review this detail. The issue will be prioritised against the needs of other customers and developers. When we're ready to schedule the issue, it will be assigned to a dated GitHub project board for that particular sprint where you can continue to track its progress to completion."
 
+const newIssueAddedConditions = [
+  {
+    labels: ['bug', 'needs triage'],
+    comment: "Thank you for taking the time to report a bug. We prioritise bugs depending on the severity and implications, so please ensure that you have provided as much information as possible. If you haven’t already, it really helps us to investigate the bug you have reported if you provide ‘Steps to Replicate’ and any associated screenshots. \r\n" +
+    "This issue will be reviewed in our weekly refinement sessions and assigned to a specific project board. We may also update the ticket to request additional information, if required. \r\n" +
+    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  },
+  {
+    labels: ['external feature', 'needs triage'],
+    comment: "Thank you for raising a feature request. Feature requests will be prioritised in accordance with the upcoming milestone target dates, customer and developer priorities. \r\n" +
+    "This request will be reviewed in our weekly refinement sessions and assigned to a specific project board or column, depending on the nature of the request and the development work required. \r\n" +
+    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  },
+]
+
+const labelAddedConditions = [
+  {
+    labels: ['backend'],
+    comment: "Your issue has recently been assigned to our ‘Backend’ project board for review. All issues are reviewed in a weekly refinement session and where applicable, a comment and associated label will be added. If required, we will add a technical specification to the ticket. Please take the time to review the information. \r\n" +
+    "When we're ready to schedule the issue, it will be assigned to the relevant board where you can continue to track its progress to completion. \r\n" +
+    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  },
+  {
+    labels: ['front-end'],
+    comment: "Your issue has recently been assigned to our ‘Front-end’ project board for review. All issues are reviewed in a weekly refinement session and where applicable, a comment and associated label will be added. If required, we will add a technical specification to the ticket. Please take the time to review the information. \r\n" +
+    "When we're ready to schedule the issue, it will be moved to the relevant column where you can continue to track its progress to completion. \r\n" +
+    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  },
+  {
+    labels: ['too-big'],
+    comment: "This issue has been reviewed and is too large to be handled in a single issue and requires need to be broken down by our development team. We will add the associated/dependent issues to this ticket when available. \r\n" +
+    "The induvial tickets will then review in our weekly refinement sessions.  " +
+    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  },
+  {
+    labels: ['agency-cloud'],
+    comment: "As this issue relates to AgencyCloud, we are unable to process this through the Platform in accordance with our Developer Processes. \r\n" +
+    "Issues relating to AgencyCloud should not be submitted via the Foundations GitHub repo but should be requested via the Reapit Service Desk by a Reapit Customer. \r\n" +
+    "Please ask a Reapit Customer to raise this issue via the Reapit Service Desk.  " +
+    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  },
+  {
+    labels: ['product-decision'],
+    comment: "The nature of your request requires product direction and therefore we have moved this issue into our ‘Not Ready’ column whilst we obtain the information/direction required. \r\n" +
+    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  },
+  {
+    labels: ['product-decision'],
+    comment: "We need to research or gather more information relating to your request. We have moved this issue into our ‘Not Ready’ column whilst we obtain the information required. \r\n" +
+    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  },
+  {
+    labels: ['response-needed'],
+    comment: "We have recently requested additional information relating to the issue you have raised. Please can you take the time to review this ticket and where applicable, provide the information requested. \r\n" +
+    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  },
+]
+
 export default (app) => {
   app.on('issues.opened', async (event) => {
     if (['OWNER', 'MEMBER'].includes(event.payload.issue.author_association)) {
       return
     }
 
-    const featureRequestLabels = ['external feature', 'needs triage']
-    const bugLabels = ['bug', 'needs triage']
-
     const issueLabels = event.payload.issue.labels.map(label => label.name)
 
-    const isFeatureRequestTemplate = featureRequestLabels.every((label) => issueLabels.includes(label))
-    const isBugTemplate = bugLabels.every((label) => issueLabels.includes(label))
+    for (const [, condition] of newIssueAddedConditions.entries()) {
+      console.log('condition', condition.labels, condition.labels.every(label => issueLabels.includes(label)))
+      if (condition.labels.every(label => issueLabels.includes(label))) {
+        await Promise.all([
+          event.octokit.issues.createComment(event.issue({ body: condition.comment })),
+          // event.octokit.issues.addLabels(event.issue({ labels: ['awaiting triage'] })),
+        ])
 
-    if (!isFeatureRequestTemplate && !isBugTemplate) {
-      console.log('is not feature request or bug template')
-      return
-    }
-
-    if (isFeatureRequestTemplate) {
-      return Promise.all([
-        event.octokit.issues.createComment(event.issue({ body: `Hello :wave:
-        ${templateReply}
-        ` })),
-        event.octokit.issues.addLabels(event.issue({ labels: ['awaiting triage'] })),
-      ])
-    }
-
-    if (isBugTemplate) {
-      return Promise.all([
-        event.octokit.issues.createComment(event.issue({ body: `Hello :wave:
-        ${templateReply}
-        ` })),
-        event.octokit.issues.addLabels(event.issue({ labels: ['awaiting triage'] })),
-      ])
+        break
+      }
     }
   })
 
-  app.on('issues.labeled', (event) => {
+  app.on('issues.labeled', async (event) => {
     // TODO requires 'member of org' condition?
-    if (event.payload.label.name === 'question') {
-      return Promise.all([event.octokit.issues.createComment(event.issue({ body: labelReply }))])
+    if (['OWNER', 'MEMBER'].includes(event.payload.issue.author_association)) {
+      return
+    }
+
+    const issueLabels = event.payload.issue.labels.map(label => label.name)
+
+    for (const [, condition] of labelAddedConditions.entries()) {
+      if (condition.labels.every(label => issueLabels.includes(label))) {
+        await Promise.all([
+          event.octokit.issues.createComment(event.issue({ body: label.comment })),
+          // event.octokit.issues.addLabels(event.issue({ labels: ['awaiting triage'] })),
+        ])
+
+        break
+      }
     }
   })
 
