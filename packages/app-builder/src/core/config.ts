ac96043089feba47535d440bb19aac58d1b3aa67
Josh Balfour <josh@joshbalfour.co.uk>
2021-12-03 11:16:47 +0000

    fix api url

--- packages/app-builder/src/core/config.ts
+++ packages/app-builder/src/core/config.ts
@@ -1,4 +1,4 @@
 export const graphqlUri =
   window.location.hostname.includes('localhost') || window.location.hostname.includes('.local')
     ? 'http://localhost:4000/graphql'
-    : 'https://zbtuirnf0g.execute-api.eu-west-2.amazonaws.com/prod/'
+    : window.location.protocol + '//' + window.location.hostname.replace('app-builder', 'app-builder-backend')
