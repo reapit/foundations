fc19c96864824cd7042903a43fbc4d3b3f656546
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-12-09 12:03:05 +0000

    chore: linting

--- packages/auto-reply-bot/src/app.js
+++ packages/auto-reply-bot/src/app.js
@@ -1,16 +1,18 @@
 const newIssueAddedConditions = [
   {
     labels: ['bug', 'needs-triage'],
-    comment: "Thank you for taking the time to report a bug. We prioritise bugs depending on the severity and implications, so please ensure that you have provided as much information as possible. If you haven’t already, it really helps us to investigate the bug you have reported if you provide ‘Steps to Replicate’ and any associated screenshots. \r\n" +
-    "Please ensure any personal information from the production database is obscured when submitting screenshots.\r\n" +
-    "This issue will be reviewed in our weekly refinement sessions and assigned to a specific project board. We may also update the ticket to request additional information, if required. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+    comment:
+      'Thank you for taking the time to report a bug. We prioritise bugs depending on the severity and implications, so please ensure that you have provided as much information as possible. If you haven’t already, it really helps us to investigate the bug you have reported if you provide ‘Steps to Replicate’ and any associated screenshots. \r\n' +
+      'Please ensure any personal information from the production database is obscured when submitting screenshots.\r\n' +
+      'This issue will be reviewed in our weekly refinement sessions and assigned to a specific project board. We may also update the ticket to request additional information, if required. \r\n' +
+      'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
   },
   {
     labels: ['external-request', 'needs-triage'],
-    comment: "Thank you for raising a feature request. Feature requests will be prioritised in accordance with our roadmap, customer and developer priorities. \r\n" +
-    "This request will be reviewed in our weekly refinement sessions and assigned to a specific project board or column, depending on the nature of the request and the development work required. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+    comment:
+      'Thank you for raising a feature request. Feature requests will be prioritised in accordance with our roadmap, customer and developer priorities. \r\n' +
+      'This request will be reviewed in our weekly refinement sessions and assigned to a specific project board or column, depending on the nature of the request and the development work required. \r\n' +
+      'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
   },
   // {
   //   labels: ['question', 'needs-triage'],
@@ -23,54 +25,64 @@ const newIssueAddedConditions = [
 const labelAddedConditions = [
   {
     labels: ['backend'],
-    comment: "This issue has recently been assigned to our ‘Backend’ project board for review. All issues are reviewed in a weekly refinement session and where applicable, a comment and associated label will be added. If required, we will add a technical specification to the ticket. Please take the time to review the information. \r\n" +
-    "When we're ready to schedule the issue, it will be assigned to the relevant board where you can continue to track its progress to completion. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+    comment:
+      'This issue has recently been assigned to our ‘Backend’ project board for review. All issues are reviewed in a weekly refinement session and where applicable, a comment and associated label will be added. If required, we will add a technical specification to the ticket. Please take the time to review the information. \r\n' +
+      "When we're ready to schedule the issue, it will be assigned to the relevant board where you can continue to track its progress to completion. \r\n" +
+      'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
   },
   {
     labels: ['front-end'],
-    comment: "This issue has recently been assigned to our ‘Front-end’ project board for review. All issues are reviewed in a weekly refinement session and where applicable, a comment and associated label will be added. If required, we will add a technical specification to the ticket. Please take the time to review the information. \r\n" +
-    "When we're ready to schedule the issue, it will be moved to the relevant column where you can continue to track its progress to completion. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+    comment:
+      'This issue has recently been assigned to our ‘Front-end’ project board for review. All issues are reviewed in a weekly refinement session and where applicable, a comment and associated label will be added. If required, we will add a technical specification to the ticket. Please take the time to review the information. \r\n' +
+      "When we're ready to schedule the issue, it will be moved to the relevant column where you can continue to track its progress to completion. \r\n" +
+      'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
   },
   {
     labels: ['too-big'],
-    comment: "This issue has been reviewed and is too big to be handled in a single issue and requires the need to be broken down by our development team. We will add the associated/dependent issues to this ticket when available. \r\n" +
-    "The induvial tickets will then be review in our weekly refinement sessions. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+    comment:
+      'This issue has been reviewed and is too big to be handled in a single issue and requires the need to be broken down by our development team. We will add the associated/dependent issues to this ticket when available. \r\n' +
+      'The induvial tickets will then be review in our weekly refinement sessions. \r\n' +
+      'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
   },
   {
     labels: ['agency-cloud'],
-    comment: "As this issue relates to AgencyCloud, we are unable to process this through the Platform in accordance with our Developer Processes. \r\n" +
-    "Issues relating to AgencyCloud should not be submitted via the Foundations GitHub repo but should be raised via the Reapit Service Desk by a Reapit Customer. \r\n" +
-    "Please ask a Reapit Customer to raise this issue via the Reapit Service Desk. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+    comment:
+      'As this issue relates to AgencyCloud, we are unable to process this through the Platform in accordance with our Developer Processes. \r\n' +
+      'Issues relating to AgencyCloud should not be submitted via the Foundations GitHub repo but should be raised via the Reapit Service Desk by a Reapit Customer. \r\n' +
+      'Please ask a Reapit Customer to raise this issue via the Reapit Service Desk. \r\n' +
+      'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
   },
   {
     labels: ['product-decision'],
-    comment: "The nature of this request requires product direction and therefore we have moved this issue into our ‘Not Ready’ column whilst we obtain the information/direction required. \r\n" +
-    "This issue will be updated when a produt decision has been made. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+    comment:
+      'The nature of this request requires product direction and therefore we have moved this issue into our ‘Not Ready’ column whilst we obtain the information/direction required. \r\n' +
+      'This issue will be updated when a produt decision has been made. \r\n' +
+      'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
   },
   {
     labels: ['investigate'],
-    comment: "We need to research or gather more information relating to this request. We have moved this issue into our ‘Not Ready’ column whilst we obtain the information required. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+    comment:
+      'We need to research or gather more information relating to this request. We have moved this issue into our ‘Not Ready’ column whilst we obtain the information required. \r\n' +
+      'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
   },
   {
     labels: ['response-needed'],
-    comment: "We have recently requested additional information relating to the issue you have raised. Please can you take the time to review this ticket and where applicable, provide the information requested. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+    comment:
+      'We have recently requested additional information relating to the issue you have raised. Please can you take the time to review this ticket and where applicable, provide the information requested. \r\n' +
+      'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
   },
 ]
 
 const movedColumnResponses = {
-  ['Near Term']: "This issue has been updated and moved to our ‘Near Term’ column. We have assessed the effort required and outlined a technical specification - please take the time to review this detail. When we're ready to schedule the issue, it will be assigned to the relevant board where you can continue to track its progress to completion. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
-  ['Mid Term']: "This issue has been updated and moved to our ‘Mid Term’ column. We will assess the effort required and may outline a technical specification. When we're ready to schedule the issue, it will be moved to the ‘Near Term’ column. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
-  ['Long Term']: "Whilst the nature of this request has been accepted, we are unable to commit to a specified sprint and therefore have assigned this issue to the ‘Long Term’ column. We will regularly review any issues and where development capacity is available, or work is aligned with our Roadmap, the issue will be updated. \r\n" +
-    "For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)",
+  ['Near Term']:
+    "This issue has been updated and moved to our ‘Near Term’ column. We have assessed the effort required and outlined a technical specification - please take the time to review this detail. When we're ready to schedule the issue, it will be assigned to the relevant board where you can continue to track its progress to completion. \r\n" +
+    'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
+  ['Mid Term']:
+    "This issue has been updated and moved to our ‘Mid Term’ column. We will assess the effort required and may outline a technical specification. When we're ready to schedule the issue, it will be moved to the ‘Near Term’ column. \r\n" +
+    'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
+  ['Long Term']:
+    'Whilst the nature of this request has been accepted, we are unable to commit to a specified sprint and therefore have assigned this issue to the ‘Long Term’ column. We will regularly review any issues and where development capacity is available, or work is aligned with our Roadmap, the issue will be updated. \r\n' +
+    'For more information on our processes, [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
 }
 
 // Need to obtain projectId of non sprint project board
@@ -82,14 +94,16 @@ export default (app) => {
       return
     }
 
-    const issueLabels = event.payload.issue.labels.map(label => label.name)
+    const issueLabels = event.payload.issue.labels.map((label) => label.name)
 
     for (const [, condition] of newIssueAddedConditions.entries()) {
-      console.log('condition', condition.labels, condition.labels.every(label => issueLabels.includes(label.toLowerCase())))
-      if (condition.labels.every(label => issueLabels.includes(label))) {
-        await Promise.all([
-          event.octokit.issues.createComment(event.issue({ body: condition.comment })),
-        ])
+      console.log(
+        'condition',
+        condition.labels,
+        condition.labels.every((label) => issueLabels.includes(label.toLowerCase())),
+      )
+      if (condition.labels.every((label) => issueLabels.includes(label))) {
+        await Promise.all([event.octokit.issues.createComment(event.issue({ body: condition.comment }))])
 
         break
       }
@@ -102,10 +116,8 @@ export default (app) => {
     }
 
     for (const [, condition] of labelAddedConditions.entries()) {
-      if (condition.labels.every(label => label === event.payload.label.name.toLowerCase())) {
-        await Promise.all([
-          event.octokit.issues.createComment(event.issue({ body: condition.comment })),
-        ])
+      if (condition.labels.every((label) => label === event.payload.label.name.toLowerCase())) {
+        await Promise.all([event.octokit.issues.createComment(event.issue({ body: condition.comment }))])
 
         break
       }
@@ -169,10 +181,14 @@ export default (app) => {
     }
 
     if (event.payload.issue.state === 'closed') {
-
       return Promise.all([
-        event.octokit.issues.createComment(event.issue({ body: "It looks like you have commented on a closed issue. If your comment relates to a bug or feature request, please open a new issue, and include this issue number/url for reference. \r\n" +
-        "For more information on our processes,  [please click here](https://foundations-documentation.reapit.cloud/dev-requests)" })),
+        event.octokit.issues.createComment(
+          event.issue({
+            body:
+              'It looks like you have commented on a closed issue. If your comment relates to a bug or feature request, please open a new issue, and include this issue number/url for reference. \r\n' +
+              'For more information on our processes,  [please click here](https://foundations-documentation.reapit.cloud/dev-requests)',
+          }),
+        ),
       ])
     }
   })
