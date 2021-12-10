0820e56a03903e64c021997a3ccd24a446b6e0e7
Will McVay <wmcvay@reapit.com>
2021-12-10 11:18:57 +0000

    feat: #5634 wip

--- packages/developer-portal/src/components/pages/apps-new/config.tsx
+++ packages/developer-portal/src/components/pages/apps-new/config.tsx
@@ -3,7 +3,11 @@ import { IconNames } from '@reapit/elements'
 
 export enum AppNewStepId {
   'whatUserStep' = 'whatUserStep',
-  'whatAppTypeStep' = 'whatAppTypeStep',
+  'existingCustomerStep' = 'existingCustomerStep',
+  'thirdPartyDevStep' = 'thirdPartyDevStep',
+  'webServicesStep' = 'webServicesStep',
+  'reapitConnectStep' = 'reapitConnectStep',
+  'otherAppStep' = 'otherAppStep',
 }
 
 export type AppNewSteps = AppNewStepId[]
@@ -33,7 +37,63 @@ export const appWizardSteps: AppNewWizardSteps = {
     ),
     iconName: 'userInfographic',
   },
-  [AppNewStepId.whatAppTypeStep]: {
+  [AppNewStepId.existingCustomerStep]: {
+    headingText: '2. What kind of app am I building?',
+    headerText:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente quam neque impedit dolor doloribus corrupti quaerat sint veritatis, temporibus in illo hic dicta similique! Eius, commodi consectetur! Id, fugit exercitationem.',
+    helperPageHeadingText: 'About Apps',
+    helperPage: (
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis omnis veritatis itaque molestias perferendis, hic
+        maiores debitis voluptate nesciunt, voluptas laudantium mollitia nemo possimus aut rem voluptatum ipsam quia
+        quae?
+      </div>
+    ),
+    iconName: 'customerInfographic',
+  },
+  [AppNewStepId.thirdPartyDevStep]: {
+    headingText: '2. What kind of app am I building?',
+    headerText:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente quam neque impedit dolor doloribus corrupti quaerat sint veritatis, temporibus in illo hic dicta similique! Eius, commodi consectetur! Id, fugit exercitationem.',
+    helperPageHeadingText: 'About Apps',
+    helperPage: (
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis omnis veritatis itaque molestias perferendis, hic
+        maiores debitis voluptate nesciunt, voluptas laudantium mollitia nemo possimus aut rem voluptatum ipsam quia
+        quae?
+      </div>
+    ),
+    iconName: 'customerInfographic',
+  },
+  [AppNewStepId.webServicesStep]: {
+    headingText: '2. What kind of app am I building?',
+    headerText:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente quam neque impedit dolor doloribus corrupti quaerat sint veritatis, temporibus in illo hic dicta similique! Eius, commodi consectetur! Id, fugit exercitationem.',
+    helperPageHeadingText: 'About Apps',
+    helperPage: (
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis omnis veritatis itaque molestias perferendis, hic
+        maiores debitis voluptate nesciunt, voluptas laudantium mollitia nemo possimus aut rem voluptatum ipsam quia
+        quae?
+      </div>
+    ),
+    iconName: 'customerInfographic',
+  },
+  [AppNewStepId.reapitConnectStep]: {
+    headingText: '2. What kind of app am I building?',
+    headerText:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente quam neque impedit dolor doloribus corrupti quaerat sint veritatis, temporibus in illo hic dicta similique! Eius, commodi consectetur! Id, fugit exercitationem.',
+    helperPageHeadingText: 'About Apps',
+    helperPage: (
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis omnis veritatis itaque molestias perferendis, hic
+        maiores debitis voluptate nesciunt, voluptas laudantium mollitia nemo possimus aut rem voluptatum ipsam quia
+        quae?
+      </div>
+    ),
+    iconName: 'customerInfographic',
+  },
+  [AppNewStepId.otherAppStep]: {
     headingText: '2. What kind of app am I building?',
     headerText:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente quam neque impedit dolor doloribus corrupti quaerat sint veritatis, temporibus in illo hic dicta similique! Eius, commodi consectetur! Id, fugit exercitationem.',
--- packages/developer-portal/src/components/pages/apps-new/user-options-content.tsx
+++ packages/developer-portal/src/components/pages/apps-new/user-options-content.tsx
@@ -1,40 +1,68 @@
 import { cx } from '@linaria/core'
 import { elMr8, elWFull, Icon } from '@reapit/elements'
-import React, { FC } from 'react'
+import React, { Dispatch, FC, SetStateAction } from 'react'
 import { AppNewStepId } from './config'
-import { useAppWizard } from './use-app-wizard'
+import { AppWizardState, useAppWizard } from './use-app-wizard'
 import { StepOptionItem, stepOptionItemSelected } from './__styles__'
 
+export const handleSetAppWizardState =
+  (setAppWizardState: Dispatch<SetStateAction<AppWizardState>>, nextStep: AppNewStepId) => () => {
+    setAppWizardState((currentState) => ({
+      ...currentState,
+      nextStep,
+    }))
+  }
+
 export const UserOptionsContent: FC = () => {
-  const { appWizardState } = useAppWizard()
+  const { appWizardState, setAppWizardState } = useAppWizard()
   const { nextStep } = appWizardState
 
   return (
     <>
-      <StepOptionItem>
+      <StepOptionItem onClick={handleSetAppWizardState(setAppWizardState, AppNewStepId.existingCustomerStep)}>
         <Icon
-          className={cx(elMr8, nextStep === AppNewStepId.whatAppTypeStep && stepOptionItemSelected)}
+          className={cx(elMr8, nextStep === AppNewStepId.existingCustomerStep && stepOptionItemSelected)}
           fontSize="2rem"
           icon="logoKeyInfographic"
         />
         <div className={elWFull}>Existing Reapit Customer</div>
-        {nextStep === AppNewStepId.whatAppTypeStep && <Icon icon="tickSolidSystem" intent="primary" />}
+        {nextStep === AppNewStepId.existingCustomerStep && <Icon icon="tickSolidSystem" intent="primary" />}
       </StepOptionItem>
-      <StepOptionItem>
-        <Icon className={elMr8} fontSize="2rem" icon="htmlInfographic" />
+      <StepOptionItem onClick={handleSetAppWizardState(setAppWizardState, AppNewStepId.thirdPartyDevStep)}>
+        <Icon
+          className={cx(elMr8, nextStep === AppNewStepId.thirdPartyDevStep && stepOptionItemSelected)}
+          fontSize="2rem"
+          icon="htmlInfographic"
+        />
         <div className={elWFull}>3rd Party Web Developer</div>
+        {nextStep === AppNewStepId.thirdPartyDevStep && <Icon icon="tickSolidSystem" intent="primary" />}
       </StepOptionItem>
-      <StepOptionItem>
-        <Icon className={elMr8} fontSize="2rem" icon="globeInfographic" />
+      <StepOptionItem onClick={handleSetAppWizardState(setAppWizardState, AppNewStepId.webServicesStep)}>
+        <Icon
+          className={cx(elMr8, nextStep === AppNewStepId.webServicesStep && stepOptionItemSelected)}
+          fontSize="2rem"
+          icon="globeInfographic"
+        />
         <div className={elWFull}>WebService to Foundations</div>
+        {nextStep === AppNewStepId.webServicesStep && <Icon icon="tickSolidSystem" intent="primary" />}
       </StepOptionItem>
-      <StepOptionItem>
-        <Icon className={elMr8} fontSize="2rem" icon="doorLockInfographic" />
+      <StepOptionItem onClick={handleSetAppWizardState(setAppWizardState, AppNewStepId.reapitConnectStep)}>
+        <Icon
+          className={cx(elMr8, nextStep === AppNewStepId.reapitConnectStep && stepOptionItemSelected)}
+          fontSize="2rem"
+          icon="doorLockInfographic"
+        />
         <div className={elWFull}>Reapit Connect as ID Provider</div>
+        {nextStep === AppNewStepId.reapitConnectStep && <Icon icon="tickSolidSystem" intent="primary" />}
       </StepOptionItem>
-      <StepOptionItem>
-        <Icon className={elMr8} fontSize="2rem" icon="userHouseInfographic" />
+      <StepOptionItem onClick={handleSetAppWizardState(setAppWizardState, AppNewStepId.otherAppStep)}>
+        <Icon
+          className={cx(elMr8, nextStep === AppNewStepId.otherAppStep && stepOptionItemSelected)}
+          fontSize="2rem"
+          icon="userHouseInfographic"
+        />
         <div className={elWFull}>Other</div>
+        {nextStep === AppNewStepId.otherAppStep && <Icon icon="tickSolidSystem" intent="primary" />}
       </StepOptionItem>
     </>
   )
