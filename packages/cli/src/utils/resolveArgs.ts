ce2791f196c5f5f5751bc3b573a8d3306462d2ad
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-01-11 15:27:44 +0000

    fix: corrected linting for resolve args

--- packages/cli/src/utils/resolveArgs.ts
+++ packages/cli/src/utils/resolveArgs.ts
@@ -7,30 +7,34 @@ export type ParameterType = {
 
 export const resolveArgs = (incomingArgs: string[], args?: ArgsType[]): any[] => {
   const results: any[] = []
-  const incomingParams = incomingArgs.filter(param => !/^[\-\-]/.test(param))
+  const incomingParams = incomingArgs.filter((param) => !/^[\-]/.test(param))
   const argTypes = args?.map((paramOrOption, index) => ({
     paramOrOption,
     index,
   }))
 
-  argTypes?.filter(argOptions => argOptions.paramOrOption.type === 'parameter').forEach(({paramOrOption: param, index: depIndex}, index) => {
-    if (param.required && typeof incomingParams[index] == 'undefined') {
-      console.log(chalk.red(`${param.name} is required`))
-      process.exit(1)
-    }
+  argTypes
+    ?.filter((argOptions) => argOptions.paramOrOption.type === 'parameter')
+    .forEach(({ paramOrOption: param, index: depIndex }, index) => {
+      if (param.required && typeof incomingParams[index] == 'undefined') {
+        console.log(chalk.red(`${param.name} is required`))
+        process.exit(1)
+      }
 
-    results[depIndex] = incomingParams[index] || param.default
-  })
+      results[depIndex] = incomingParams[index] || param.default
+    })
 
-  argTypes?.filter(argOptions => argOptions.paramOrOption.type === 'option').forEach(({paramOrOption: option, index}) => {
-    if (incomingArgs.some(opt => [`-${option.shortName}`, `--${option.name}`].includes(opt))) {
-      results[index] = true
-    } else if (option.default) {
-      results[index] = option.default
-    } else {
-      results[index] = false
-    }
-  })
+  argTypes
+    ?.filter((argOptions) => argOptions.paramOrOption.type === 'option')
+    .forEach(({ paramOrOption: option, index }) => {
+      if (incomingArgs.some((opt) => [`-${option.shortName}`, `--${option.name}`].includes(opt))) {
+        results[index] = true
+      } else if (option.default) {
+        results[index] = option.default
+      } else {
+        results[index] = false
+      }
+    })
 
   return results
 }
