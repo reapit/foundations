3f036079ca9f6fc4a7baced13a11ad424af28740
Will McVay <wmcvay@reapit.com>
2022-02-02 14:24:56 +0000

    feat: #6022 add monthly billing total

--- packages/admin-portal/src/components/pages/usage/usage-table.tsx
+++ packages/admin-portal/src/components/pages/usage/usage-table.tsx
@@ -1,6 +1,15 @@
 import React, { FC } from 'react'
 import { BillingBreakdownForMonthV2Model } from '@reapit/foundations-ts-definitions'
-import { PersistantNotification, Table, TableCell, TableHeader, TableHeadersRow, TableRow } from '@reapit/elements'
+import {
+  PersistantNotification,
+  Table,
+  TableCell,
+  TableHeader,
+  TableHeadersRow,
+  TableRow,
+  Subtitle,
+  BodyText,
+} from '@reapit/elements'
 import { threeColTable } from './__styles__'
 
 export interface UsageTableProps {
@@ -10,68 +19,73 @@ export interface UsageTableProps {
 export const UsageTable: FC<UsageTableProps> = ({ billing }) => {
   const apiCalls = billing?.services?.find((item) => item.name === 'API Requests')?.items ?? []
   return apiCalls.length ? (
-    <Table
-      rows={apiCalls.map(({ name, amount, cost, itemCount, items }) => ({
-        cells: [
-          {
-            label: 'Entity Name',
-            value: name ?? '',
-            icon: 'homeSystem',
-            cellHasDarkText: true,
-            narrowTable: {
-              showLabel: true,
+    <>
+      <Subtitle>Monthly Total</Subtitle>
+      <BodyText hasGreyText>£{billing?.totalCost}</BodyText>
+      <Subtitle>Breakdown</Subtitle>
+      <Table
+        rows={apiCalls.map(({ name, amount, cost, itemCount, items }) => ({
+          cells: [
+            {
+              label: 'Entity Name',
+              value: name ?? '',
+              icon: 'homeSystem',
+              cellHasDarkText: true,
+              narrowTable: {
+                showLabel: true,
+              },
             },
-          },
-          {
-            label: 'Total Number Calls',
-            value: String(amount) ?? '',
-            icon: 'homeSystem',
-            cellHasDarkText: true,
-            narrowTable: {
-              showLabel: true,
+            {
+              label: 'Total Number Calls',
+              value: String(amount) ?? '',
+              icon: 'homeSystem',
+              cellHasDarkText: true,
+              narrowTable: {
+                showLabel: true,
+              },
             },
-          },
-          {
-            label: 'Total Cost',
-            value: cost ? `£${cost}` : '£0',
-            icon: 'homeSystem',
-            cellHasDarkText: true,
-            narrowTable: {
-              showLabel: true,
+            {
+              label: 'Total Cost',
+              value: cost ? `£${cost}` : '£0',
+              icon: 'homeSystem',
+              cellHasDarkText: true,
+              narrowTable: {
+                showLabel: true,
+              },
             },
-          },
-          {
-            label: 'Total Number Endpoints',
-            value: String(itemCount) ?? '',
-            icon: 'homeSystem',
-            cellHasDarkText: true,
-            narrowTable: {
-              showLabel: true,
+            {
+              label: 'Total Number Endpoints',
+              value: String(itemCount) ?? '',
+              icon: 'homeSystem',
+              cellHasDarkText: true,
+              narrowTable: {
+                showLabel: true,
+              },
             },
+          ],
+          expandableContent: {
+            content: (
+              <>
+                <Table>
+                  <TableHeadersRow className={threeColTable}>
+                    <TableHeader>Endpoint Name</TableHeader>
+                    <TableHeader>Endpoint Number Calls</TableHeader>
+                    <TableHeader>Endpoint Cost</TableHeader>
+                  </TableHeadersRow>
+                  {items?.map(({ name, amount, cost }) => (
+                    <TableRow className={threeColTable} key={name}>
+                      <TableCell>{name}</TableCell>
+                      <TableCell>{amount}</TableCell>
+                      <TableCell>{cost ? `£${cost}` : '£0'}</TableCell>
+                    </TableRow>
+                  ))}
+                </Table>
+              </>
+            ),
           },
-        ],
-        expandableContent: {
-          content: (
-            <>
-              <Table>
-                <TableHeadersRow className={threeColTable}>
-                  <TableHeader>Endpoint Name</TableHeader>
-                  <TableHeader>Endpoint Number Calls</TableHeader>
-                  <TableHeader>Endpoint Cost</TableHeader>
-                </TableHeadersRow>
-                {items?.map(({ name, amount, cost }) => (
-                  <TableRow className={threeColTable} key={name}>
-                    <TableCell>{name}</TableCell>
-                    <TableCell>{amount}</TableCell>
-                    <TableCell>{cost ? `£${cost}` : '£0'}</TableCell>
-                  </TableRow>
-                ))}
-              </Table>
-            </>
-          ),
-        },
-      }))}
-    />
+        }))}
+      />
+    </>
   ) : (
     <div>
       <PersistantNotification isExpanded isFullWidth isInline intent="secondary">
