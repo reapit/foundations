c1cc4965cfd69f958213817e749c55a778c7ad4e
Josh Balfour <josh@joshbalfour.co.uk>
2022-02-15 15:50:12 +0000

    use babel instead of ts-jest to see if that fixes CI

--- packages/app-builder-backend/babel.config.js
+++ packages/app-builder-backend/babel.config.js
@@ -1,7 +1,12 @@
 const { baseBabel } = require('@reapit/ts-scripts')
 
-baseBabel.plugins.push(['@babel/plugin-proposal-decorators', { legacy: true }])
-
 module.exports = {
   ...baseBabel,
+  plugins: [
+    ...baseBabel.plugins,
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+  ],
 }
--- packages/app-builder-backend/jest.config.js
+++ packages/app-builder-backend/jest.config.js
@@ -1,5 +1,27 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const { jestNodeGlobalConfig } = require('@reapit/ts-scripts')
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
+
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  ...jestNodeGlobalConfig,
+  testPathIgnorePatterns: ['<rootDir>/src/tests/'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>[/\\\\](node_modules|src/types|src/tests|src/scripts|src/__stubs__|dist)|config|core|models[/\\\\]',
+    '.d.ts',
+    'index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 79,
+      functions: 92,
+      lines: 89,
+      statements: 90,
+    },
+  },
+  moduleNameMapper: {
+    ...jestNodeGlobalConfig.moduleNameMapper,
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>/',
+    }),
+  },
 }
