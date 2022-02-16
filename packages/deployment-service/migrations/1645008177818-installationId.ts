265b6d1f963d26615d3cee8606a8a280a211478e
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-02-16 10:48:31 +0000

    chore: convert to 2 tabs

--- packages/deployment-service/migrations/1645008177818-installationId.ts
+++ packages/deployment-service/migrations/1645008177818-installationId.ts
@@ -2,12 +2,11 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class installationId1645008177818 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `pipelines` ADD `installationId` varchar(50) NULL");
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query("ALTER TABLE `pipelines` ADD `installationId` varchar(50) NULL");
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `pipelines` DROP COLUMN `installationId`");
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query("ALTER TABLE `pipelines` DROP COLUMN `installationId`");
+	}
 }
