161c8c7c7e75f272ec1b39a962befe56992a20b9
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-02-22 09:40:06 +0000

    feat: added bitbucket client entity

---
+++ packages/deployment-service/src/entities/bitbucket-client.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity } from 'typeorm'
+import { AbstractEntity } from './abstract-entity'
+
+export type BitbucketClientData = {
+  productType: 'bitbucket',
+  principle: {
+    account_id: string,
+    display_name?: string,
+    type: 'user',
+    job_title: string,
+    account_status: string,
+    nickname: string,
+  },
+  baseUrl: string,
+  publicKey: string,
+  user: {
+    display_name: string,
+
+  },
+  key: string,
+  sharedSecret: string,
+  clientKey: string,
+  baseApiUrl: string,
+}
+
+@Entity('bitbucket_clients')
+export class BitbucketClientEntity extends AbstractEntity {
+  @Column({
+    unique: true,
+  })
+  clientKey: string
+
+  @Column({
+    type: 'json',
+  })
+  data: BitbucketClientData
+}
