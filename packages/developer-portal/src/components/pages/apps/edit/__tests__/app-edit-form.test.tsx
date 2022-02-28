6f23f73766dc967ce118636be328f677e43d864c
Will McVay <wmcvay@reapit.com>
2022-02-28 16:38:35 +0000

    feat: #6178 fixes a test

--- packages/developer-portal/src/components/pages/apps/edit/__tests__/app-edit-form.test.tsx
+++ packages/developer-portal/src/components/pages/apps/edit/__tests__/app-edit-form.test.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import { Control } from 'react-hook-form'
 import { render } from '../../../../../tests/react-testing'
 import { AppEditForm } from '../app-edit-form'
 import { AppEditTab } from '../edit-page-tabs'
-import { AppEditFormSchema } from '../form-schema/form-fields'
 
 jest.mock('../../state/use-app-state')
 
 describe('AppEditForm', () => {
   it('should match a snapshot', () => {
-    expect(
-      render(
-        <AppEditForm
-          register={jest.fn()}
-          errors={{}}
-          tab={AppEditTab.permissions}
-          control={{} as Control<AppEditFormSchema, object>}
-          getValues={jest.fn()}
-        />,
-      ),
-    ).toMatchSnapshot()
+    expect(render(<AppEditForm tab={AppEditTab.permissions} />)).toMatchSnapshot()
   })
 })
