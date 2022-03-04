3f2ea303e4cbfd4add2f063c5dd6e775871b27d1
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-03-04 12:00:35 +0000

    fix: whoops

--- packages/developer-portal/src/components/pages/apps/edit/create-pipeline.tsx
+++ packages/developer-portal/src/components/pages/apps/edit/create-pipeline.tsx
@@ -42,7 +42,6 @@ export const pipelineCreateFormHandle =
     appId: string,
   ) =>
   async (values: PipelineModelInterface) => {
-    console.log('ffs am here')
     const result = await createPipeline({
       ...values,
       appId,
