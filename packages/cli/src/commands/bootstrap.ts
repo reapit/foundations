4a6d4c94115807a0e609747c85e7b5bf00c4c7fd
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-03-15 14:53:34 +0000

    refactor: #6150 renaming functions (#6383)
    * refactor: moved and renamed deploy commands
    
    * refactor: renamed and restructured bootstrap command
    
    * docs: changed bootstrap desc
    
    * refactor: converted to spawn from exec

---
+++ packages/cli/src/commands/bootstrap.ts
@@ -0,0 +1,132 @@
+import { Command, Param } from '../decorators'
+import { AbstractCommand } from '../abstract.command'
+import { spawn } from 'child_process'
+import ora, { Ora } from 'ora'
+import chalk from 'chalk'
+import fs from 'fs'
+
+type ReapitConfig = {
+  connectClientId: string
+  connectUserPoolId: string
+  [s: string]: any
+}
+
+@Command({
+  name: 'bootstrap',
+  description: 'Create application templates to start a new project',
+})
+export class BootstrapCommand extends AbstractCommand {
+  async checkFolderExists(path: string, spinner: Ora): Promise<void | never> {
+    if (fs.existsSync(path)) {
+      spinner.fail(`Folder already exists [${path}]`)
+      process.exit(1)
+    }
+  }
+
+  async createReactApp(spinner: Ora, name: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      spinner.start('Creating app with create-react-app (this might take a while)')
+      const proc = spawn('npx', ['create-react-app', name, '--template', '@reapit/cra-template-foundations'], {
+        shell: true,
+      })
+      proc.stdout.on('data', (data) => console.log(data.toString()))
+      proc.stderr.on('data', (data) => console.log(data.toString()))
+
+      proc.on('error', (error) => {
+        console.error(error)
+        reject(error)
+      })
+
+      proc.on('close', () => {
+        resolve()
+      })
+    })
+  }
+
+  private async reactBoostrap(spinner: Ora, name: string, clientId: string, userPool: string) {
+    try {
+      await this.createReactApp(spinner, name)
+    } catch (e) {
+      console.error(e)
+      process.exit(1)
+    }
+
+    spinner.start('Replacing reapit config params')
+
+    await this.updateConfigValues(name, clientId, userPool)
+
+    spinner.info('Finished param config setup')
+
+    console.log(`
+
+
+    ${chalk.green('Happy hacking commrade!')}
+
+      cd into ${name} and start coding!
+
+      If you use visual studio, use \`code ${name}\`
+    `)
+  }
+
+  private async nodeBootstrap() {
+    this.writeLine(chalk.red('Unfortunately, bootstrap node has not yet been implemented'))
+  }
+
+  /**
+   * Update the reapit.config.json file in the react template
+   *
+   * @param path
+   * @param clientId
+   * @param userPool
+   */
+  protected async updateConfigValues(path: string, clientId: string, userPool: string): Promise<void | never> {
+    const reapitConfig = await this.resolveConfigFile<ReapitConfig>(`${path}/src/reapit.config.json`)
+
+    if (!reapitConfig) {
+      throw new Error('file not found')
+    }
+
+    reapitConfig.connectClientId = clientId
+    reapitConfig.connectUserPoolId = userPool
+
+    await this.writeConfigFile(`${path}/src/reapit.config.json`, reapitConfig)
+  }
+
+  async run(
+    @Param({
+      name: 'name',
+      required: true,
+    })
+    name: string,
+    @Param({
+      name: 'type',
+      required: true,
+    })
+    type: string,
+    @Param({
+      name: 'clientId',
+      required: true,
+    })
+    clientId: string,
+    @Param({
+      name: 'userPool',
+      required: true,
+    })
+    userPool: string,
+  ) {
+    const spinner = ora()
+
+    await this.checkFolderExists(name, spinner)
+
+    if (type === 'react') {
+      await this.reactBoostrap(spinner, name, clientId, userPool)
+      return
+    } else if (type === 'node') {
+      await this.nodeBootstrap()
+      return
+    }
+
+    this.writeLine('Only [react] or [node] are accepted bootstrap types')
+    process.exit(127)
+  }
+}
--- packages/cli/src/commands/pipeline/index.ts
+++ packages/cli/src/commands/pipeline/index.ts
@@ -1,10 +1,8 @@
 import { Command } from '../../decorators'
 import { ParentCommand } from '../../parent.command'
 import { PipelineCreate } from './create'
-import { DeployPipelineCommand } from './deploy'
 import { PipelineList } from './list'
 import { LinkPipelineCommand } from './link'
-import { DeployLocalCommand } from './deploy-local'
 import { DeletePipelineCommand } from './delete'
 
 @Command({
@@ -12,12 +10,5 @@ import { DeletePipelineCommand } from './delete'
   description: '(BETA) For managing Pipeline deployments',
 })
 export class PipelineCommand extends ParentCommand {
-  commands = [
-    new PipelineCreate(),
-    new PipelineList(),
-    new DeployPipelineCommand(),
-    new LinkPipelineCommand(),
-    new DeployLocalCommand(),
-    new DeletePipelineCommand(),
-  ]
+  commands = [new PipelineCreate(), new PipelineList(), new LinkPipelineCommand(), new DeletePipelineCommand()]
 }
