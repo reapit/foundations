f76b9e612780a27ca8443d963217caf736dd7dea
Josh Balfour <josh@joshbalfour.co.uk>
2022-03-17 14:23:57 +0000

    return tasks with paginated pipeline runners

--- packages/deployment-service/src/functions/pipeline/pipeline-runner-get.ts
+++ packages/deployment-service/src/functions/pipeline/pipeline-runner-get.ts
@@ -3,6 +3,7 @@ import { ownership, resolveCreds } from './../../utils'
 import { httpHandler, NotFoundException } from '@homeservenow/serverless-aws-handler'
 import * as service from '../../services'
 import { defaultOutputHeaders } from './../../constants'
+import { findByPipelineId } from '../../services'
 
 /**
  * Get a pipeline by id
@@ -20,6 +21,8 @@ export const pipelineRunnerGet = httpHandler<void, PipelineRunnerEntity>({
 
     await ownership(pipelineRunner.pipeline.id as string, developerId)
 
+    const tasks = await findByPipelineId(pipelineRunner.pipeline.id as string)
+    pipelineRunner.tasks = tasks.filter((task) => task.pipelineRunner?.id === pipelineRunner.id)
     return pipelineRunner
   },
 })
