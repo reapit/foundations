cd60beea499773768efa9436867e311ed3129bfe
Will McVay <wmcvay@reapit.com>
2022-03-17 14:36:03 +0000

    feat: #6392 adds animated graphics

--- packages/developer-portal/src/components/pages/apps/edit/__styles__/index.ts
+++ packages/developer-portal/src/components/pages/apps/edit/__styles__/index.ts
@@ -1,19 +1,9 @@
 import { css } from '@linaria/core'
-import { styled } from '@linaria/react'
 
 export const appDescriptionHeight = css`
   height: 20rem;
 
   .pell-actionbar {
     height: 40px;
-  }
-`
-
-export const IconContainer = styled.div`
-  position: relative;
-  height: 140px;
-
-  svg {
-    height: 140px;
   }
 `
