b3ccd5830884c6adb87e8272df7818f2ac30fa41
Josh Balfour <josh@joshbalfour.co.uk>
2022-03-17 14:47:29 +0000

    add tasks to pipeline table

--- packages/developer-portal/src/components/pages/apps/edit/pipeline-runner-table.tsx
+++ packages/developer-portal/src/components/pages/apps/edit/pipeline-runner-table.tsx
@@ -4,13 +4,17 @@ import {
   elMt3,
   elP6,
   FlexContainer,
+  Icon,
   Loader,
   Pagination,
   Table,
   TableCell,
+  TableExpandableRow,
+  TableExpandableRowTriggerCell,
   TableHeader,
   TableHeadersRow,
   TableRow,
+  TableRowContainer,
 } from '@reapit/elements'
 import { PipelineModelInterface, PipelineRunnerModelInterface } from '@reapit/foundations-ts-definitions'
 import { useEvent } from '@harelpls/use-pusher'
@@ -73,6 +77,33 @@ export const addNewPipelineDeployment =
       })
   }
 
+const TaskList = ({ tasks }: { tasks: PipelineRunnerModelInterface['tasks'] }) => {
+  if (!tasks) {
+    return null
+  }
+  return (
+    <Table>
+      <TableHeadersRow>
+        <TableHeader>Task</TableHeader>
+        <TableHeader>Status</TableHeader>
+        <TableHeader>Started</TableHeader>
+        <TableHeader>Finished</TableHeader>
+      </TableHeadersRow>
+      {tasks.map((task) => (
+        <TableRow key={task.id}>
+          <TableCell>{task.functionName}</TableCell>
+          <TableCell>{task.buildStatus}</TableCell>
+          <TableCell>{task.startTime ? dateToHuman(task.startTime) : '-'}</TableCell>
+          <TableCell>{task.endTime ? dateToHuman(task.endTime) : '-'}</TableCell>
+        </TableRow>
+      ))}
+    </Table>
+  )
+}
+
+const isoDateToHuman = (isoDateStr: string) => DateTime.fromISO(isoDateStr).toLocaleString(DateTime.DATETIME_SHORT)
+const dateToHuman = (date: Date) => DateTime.fromJSDate(date).toLocaleString(DateTime.DATETIME_SHORT)
+
 export const PipelineDeploymentTable: React.FC<{
   pipeline: PipelineModelInterface
   initialDeployments: null | { items: PipelineRunnerModelInterface[]; meta: PipelineRunnerMeta }
@@ -84,6 +115,7 @@ export const PipelineDeploymentTable: React.FC<{
   const [pagination, setPagination] = useState<{
     items: PipelineRunnerModelInterface[]
   } | null>(initialDeployments)
+  const [isOpen, setIsOpen] = useState<string>('')
   useEffect(
     pipelineRunnerSetter({
       initialDeployments,
@@ -135,6 +167,7 @@ export const PipelineDeploymentTable: React.FC<{
           <TableHeader>Status</TableHeader>
           <TableHeader>Version</TableHeader>
           <TableHeader>Currently Deployed</TableHeader>
+          {pagination?.items.some((item) => !!item.tasks?.length) && <TableHeader></TableHeader>}
         </TableHeadersRow>
         {loading ? (
           <FlexContainer isFlexAlignCenter isFlexJustifyCenter className={cx(elP6)}>
@@ -142,15 +175,35 @@ export const PipelineDeploymentTable: React.FC<{
           </FlexContainer>
         ) : pagination !== null ? (
           pagination.items.map((deployment) => (
-            <TableRow key={deployment.id}>
-              <TableCell>{deployment.type}</TableCell>
-              <TableCell>
-                {deployment.created && DateTime.fromISO(deployment.created).toLocaleString(DateTime.DATETIME_SHORT)}
-              </TableCell>
-              <TableCell>{deployment.buildStatus}</TableCell>
-              <TableCell>{deployment.buildVersion}</TableCell>
-              <TableCell>{deployment.currentlyDeployed ? 'Deployed' : ''}</TableCell>
-            </TableRow>
+            <TableRowContainer key={deployment.id}>
+              <TableRow>
+                <TableCell>{deployment.type}</TableCell>
+                <TableCell>{deployment.created && isoDateToHuman(deployment.created)}</TableCell>
+                <TableCell>{deployment.buildStatus}</TableCell>
+                <TableCell>{deployment.buildVersion}</TableCell>
+                <TableCell>{deployment.currentlyDeployed ? 'Deployed' : ''}</TableCell>
+                {!!deployment.tasks?.length && (
+                  <TableExpandableRowTriggerCell
+                    isOpen={isOpen === deployment.id}
+                    onClick={() => {
+                      if (!deployment.id) {
+                        return
+                      }
+                      if (isOpen === deployment.id) {
+                        setIsOpen('')
+                      } else {
+                        setIsOpen(deployment.id)
+                      }
+                    }}
+                  />
+                )}
+              </TableRow>
+              {!!deployment.tasks?.length && (
+                <TableExpandableRow isOpen={isOpen === deployment.id}>
+                  <TaskList tasks={deployment.tasks} />
+                </TableExpandableRow>
+              )}
+            </TableRowContainer>
           ))
         ) : null}
       </Table>
