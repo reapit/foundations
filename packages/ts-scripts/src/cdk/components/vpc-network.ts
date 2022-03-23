8b40335e11fb8bf29b25ed354a8b1274cd2f59dc
Josh Balfour <josh@joshbalfour.co.uk>
2022-03-23 16:13:38 +0000

    aws changed a default, let's change it back

--- packages/ts-scripts/src/cdk/components/vpc-network.ts
+++ packages/ts-scripts/src/cdk/components/vpc-network.ts
@@ -2,7 +2,9 @@ import * as cdk from 'aws-cdk-lib'
 import { aws_ec2 as ec2 } from 'aws-cdk-lib'
 
 export const createVpc = (scope: cdk.Stack, name: string): ec2.Vpc => {
-  const vpc = new ec2.Vpc(scope, name)
+  const vpc = new ec2.Vpc(scope, name, {
+    maxAzs: 2,
+  })
 
   return vpc
 }
