be662f364cf4c897531c24e0144a488379ae566f
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-03-28 14:12:46 +0100

    fix: corrected bad rebase

--- packages/developer-portal/src/components/pages/apps/edit/index.tsx
+++ packages/developer-portal/src/components/pages/apps/edit/index.tsx
@@ -61,12 +61,6 @@ export const AppEditPage: FC = () => {
               text: 'AgencyCloud Integration',
               isChecked: tab === AppEditTab.acIntegration,
             },
-            {
-              id: AppEditTab.pipelines,
-              value: AppEditTab.pipelines,
-              text: 'Pipelines',
-              isChecked: tab === AppEditTab.pipelines,
-            },
           ].filter(Boolean) as TabsOption[]
         }
       />
