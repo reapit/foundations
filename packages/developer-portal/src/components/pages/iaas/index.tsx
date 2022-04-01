d21eacd5b3c1e5c71d299550b744455b6060fa2e
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-04-01 11:54:44 +0100

    feat: converted to v3 layout

--- packages/developer-portal/src/components/pages/iaas/index.tsx
+++ packages/developer-portal/src/components/pages/iaas/index.tsx
@@ -1,6 +1,19 @@
 import { reapitConnectBrowserSession } from '@/core/connect-session'
 import { useReapitConnect } from '@reapit/connect-session'
-import { BodyText, elMt6, Loader, Pagination, Table, TableHeader, TableHeadersRow, Title } from '@reapit/elements'
+import {
+  BodyText,
+  elMt6,
+  Loader,
+  PageContainer,
+  Pagination,
+  SecondaryNav,
+  SecondaryNavContainer,
+  SecondaryNavItem,
+  Table,
+  TableHeader,
+  TableHeadersRow,
+  Title,
+} from '@reapit/elements'
 import { PipelineModelInterface } from '@reapit/foundations-ts-definitions'
 import { GetActionNames, getActions } from '@reapit/utils-common'
 import { useReapitGet } from '@reapit/utils-react'
@@ -39,37 +52,45 @@ export const IaaS: FC = () => {
 
   return (
     <>
-      <Title>IaaS</Title>
-      <BodyText>A List of results of your pipelines</BodyText>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          <Table>
-            <TableHeadersRow>
-              <TableHeader>Name</TableHeader>
-              <TableHeader>Status</TableHeader>
-              <TableHeader>App Type</TableHeader>
-              <TableHeader>Repository</TableHeader>
-              <TableHeader>Actions</TableHeader>
-            </TableHeadersRow>
-            {pagination?.items?.map((pipeline) => (
-              <PipelineRow pipeline={pipeline} key={pipeline.id} />
-            ))}
-          </Table>
-          <div className={elMt6}>
-            {pagination && (
-              <Pagination
-                currentPage={pagination.meta.currentPage}
-                numberPages={pagination.meta.totalPages}
-                callback={(page) => {
-                  setPage(page)
-                }}
-              />
-            )}
-          </div>
-        </>
-      )}
+      <SecondaryNavContainer>
+        <Title>IaaS</Title>
+        <BodyText>Infrastructure as a Service management.</BodyText>
+        <SecondaryNav>
+          <SecondaryNavItem active={true}>Pipelines</SecondaryNavItem>
+        </SecondaryNav>
+      </SecondaryNavContainer>
+      <PageContainer>
+        <BodyText>A List of results of your pipelines</BodyText>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <Table>
+              <TableHeadersRow>
+                <TableHeader>Name</TableHeader>
+                <TableHeader>Status</TableHeader>
+                <TableHeader>App Type</TableHeader>
+                <TableHeader>Repository</TableHeader>
+                <TableHeader>Actions</TableHeader>
+              </TableHeadersRow>
+              {pagination?.items?.map((pipeline) => (
+                <PipelineRow pipeline={pipeline} key={pipeline.id} />
+              ))}
+            </Table>
+            <div className={elMt6}>
+              {pagination && (
+                <Pagination
+                  currentPage={pagination.meta.currentPage}
+                  numberPages={pagination.meta.totalPages}
+                  callback={(page) => {
+                    setPage(page)
+                  }}
+                />
+              )}
+            </div>
+          </>
+        )}
+      </PageContainer>
     </>
   )
 }
