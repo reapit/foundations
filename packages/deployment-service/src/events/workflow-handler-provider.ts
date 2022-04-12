0fb7557c9a1b6a685de073550ab5d98726a7a64d
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-04-12 16:13:58 +0100

    feat: added providers to workflow array

--- packages/deployment-service/src/events/workflow-handler-provider.ts
+++ packages/deployment-service/src/events/workflow-handler-provider.ts
@@ -1,14 +1,41 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleInit } from '@nestjs/common'
 import { AbstractWorkflow } from './abstract-workflow'
 import { WORKFLOW_INJECTABLE } from './workflow-decorator'
 import { SQSRecord } from 'aws-lambda'
+import { MODULE_METADATA } from '@nestjs/common/constants'
+import { ModuleRef, ModulesContainer } from '@nestjs/core'
 
 @Injectable()
-export class WorkflowHandlerProvider {
-  private readonly workflows: AbstractWorkflow<any>[]
+export class WorkflowHandlerProvider implements OnModuleInit {
+  private readonly workflows: AbstractWorkflow<any>[] = []
+
+  constructor(
+    private readonly moduleContainer: ModulesContainer,
+    private readonly moduleRef: ModuleRef,
+  ) {}
+
+  onModuleInit() {
+    [...this.moduleContainer.values()].forEach(({ metatype }) => {
+      const metadata = Reflect.getMetadata(MODULE_METADATA.PROVIDERS, metatype);
+
+      if (!metadata) {
+        return;
+      }
+
+      const providers = [
+        ...metadata.filter((metatype: any) => typeof metatype === 'function'),
+      ];
+
+      providers.map(provider => {
+        if (Reflect.hasOwnMetadata(WORKFLOW_INJECTABLE, provider)) {
+          this.workflows.push(this.moduleRef.get(provider, { strict: false }))
+        }
+      })
+    })
+  }
 
   findQueueWorkflows(queue): AbstractWorkflow<any>[] {
-    return this.workflows.filter((workflow) => Reflect.getMetadata(WORKFLOW_INJECTABLE, workflow) === queue)
+    return this.workflows.filter((workflow) => workflow.queue === queue)
   }
 
   async handleMultiple(queue: string, records: SQSRecord[]): Promise<void[]> {
