13e2e5eab09e895c7273ad67284cefbfc6c9a053
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-05-04 09:06:18 +0100

    fix: removed duplication completely

--- packages/deployment-service/src/functions/codebuild/codebuild-executor.ts
+++ packages/deployment-service/src/functions/codebuild/codebuild-executor.ts
@@ -173,15 +173,6 @@ export const codebuildExecutor: SQSHandler = async (
                     : `${pipeline.packageManager} install`,
                 ],
               },
-              // pre_build: {
-              //   commands: [
-              //     `${
-              //       pipeline.packageManager === PackageManagerEnum.NPM
-              //         ? `${pipeline.packageManager} run`
-              //         : pipeline.packageManager
-              //     } ${pipeline.buildCommand}`,
-              //   ],
-              // },
               build: {
                 commands: [
                   `${
