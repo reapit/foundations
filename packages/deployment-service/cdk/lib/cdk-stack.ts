5657084fdb73a7a09776b108c493e4b5f78b8a3b
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-05-09 15:25:17 +0100

    feat: added envs to codebuild

--- packages/deployment-service/cdk/lib/cdk-stack.ts
+++ packages/deployment-service/cdk/lib/cdk-stack.ts
@@ -415,11 +415,7 @@ export const createStack = () => {
       handler: `${fileLocPrefix}pipelineTearDown`,
       queue: queues[QueueNames.PIPELINE_TEAR_DOWN],
       timeout: 600,
-      policies: [
-        ...policies.commonBackendPolicies,
-        policies.cloudFrontPolicy,
-        policies.route53Policy,
-      ],
+      policies: [...policies.commonBackendPolicies, policies.cloudFrontPolicy, policies.route53Policy],
     },
     pusherAuth: {
       handler: `${fileLocPrefix}pusherAuthentication`,
