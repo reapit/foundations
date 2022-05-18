e8a4268e3e6bfe5f67735ed45b923489878fc673
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-05-18 16:45:14 +0100

    feat: added company resolver

---
+++ packages/app-builder-backend/src/resolvers/__tests__/company-resolver.test.ts
@@ -0,0 +1,441 @@
+import gql from 'graphql-tag'
+import { FieldNode, GraphQLSchema, OperationDefinitionNode } from 'graphql'
+import deepEqual from 'fast-deep-equal'
+import 'reflect-metadata'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { SchemaLink } from '@apollo/client/link/schema'
+
+import { Context } from '../../types'
+import { mockCompanies } from '../__mocks__/mock-companies'
+import { mockCompany } from '../__mocks__/mock-company'
+import config from '../../config.json'
+import { MetadataSchemaType } from '../../utils/extract-metadata'
+
+jest.mock('node-fetch', () => require('fetch-mock-jest').sandbox())
+const fetchMock = require('node-fetch')
+
+const mockQuery = (name: string, variables: Record<string, any> | undefined, data: Record<string, any>) => {
+  fetchMock.post(
+    (url, options) => {
+      if (url === config.graphqlUri || url === 'http://localhost:4001/') {
+        const body = JSON.parse(options.body)
+        if (variables && deepEqual(variables, body.variables)) {
+          return true
+        }
+        const parsedQuery = gql`
+          ${body.query}
+        `
+        const query = parsedQuery.definitions.find(
+          (def) => def.kind === 'OperationDefinition',
+        ) as OperationDefinitionNode
+        const selection = query.selectionSet.selections[0] as FieldNode
+        if (selection.name.value === name) {
+          return true
+        }
+      }
+      return false
+    },
+    { data },
+  )
+}
+
+const setupCompaniesMocks = () => {
+  mockQuery('GetCompanies', undefined, {
+    GetCompanies: mockCompanies,
+  })
+  // mockQuery('SearchCompanies', undefined, {
+  //   SearchContacts: mockCompanies,
+  // })
+  mockQuery(
+    'GetCompanyById',
+    { id: 'MKT2200002' },
+    {
+      GetCompanyById: mockCompany,
+    },
+  )
+  mockQuery(
+    'CreateCompany',
+    {
+      id: 'MKT22000047',
+      name: 'Reapit Company',
+      branch: 'Oxford',
+      notes: null,
+      created: '2022-04-01T09:12:38.000Z',
+      modified: '2022-04-19T06:11:11.000Z',
+      active: false,
+      vatRegistered: false,
+      typeIds: ['SP'],
+      supplierTypeId: null,
+      workPhone: '4455667788',
+      mobilePhone: null,
+      email: 'example@email.com',
+      address: {
+        buildingNumber: '15',
+        buildingName: '123',
+        line1: 'Example street',
+        line2: 'Solihull',
+        line3: 'West Midlands',
+        line4: '',
+        postcode: 'B91 2XX',
+        country: 'GB',
+      },
+    },
+    {
+      CreateCompany: mockCompany,
+    },
+  )
+  mockQuery(
+    'UpdateCompany',
+    {
+      id: 'MKT22000047',
+      name: 'Reapit Company',
+      branch: 'Oxford',
+      notes: null,
+      created: '2022-04-01T09:12:38.000Z',
+      modified: '2022-04-19T06:11:11.000Z',
+      active: false,
+      vatRegistered: false,
+      typeIds: ['SP'],
+      supplierTypeId: null,
+      workPhone: '4455667788',
+      mobilePhone: null,
+      email: 'example@email.com',
+      address: {
+        buildingNumber: '15',
+        buildingName: '123',
+        line1: 'Example street',
+        line2: 'Solihull',
+        line3: 'West Midlands',
+        line4: '',
+        postcode: 'B91 2XX',
+        country: 'GB',
+      },
+    },
+    {
+      UpdateCompany: mockCompany,
+    },
+  )
+}
+
+const listCompaniesQuery = gql`
+  query ListCompanies {
+    listCompanies {
+      id
+      created
+      modified
+      name
+      branch
+      notes
+      active
+      vatRegistered
+      typeIds
+      supplierTypeId
+      workPhone
+      mobilePhone
+      email
+      metadata
+      address {
+        buildingName
+        buildingNumber
+        line1
+        line2
+        line3
+        line4
+        postcode
+        country
+      }
+    }
+  }
+`
+
+const getGraphqlClient = async () => {
+  const schema: GraphQLSchema = await require('../../get-schema').getSchema()
+  const context: Context = {
+    accessToken: 'accessToken',
+    idToken: 'idToken',
+    apiUrl: '',
+    customEntities: [],
+    operationMetadata: {} as Record<MetadataSchemaType, any>,
+    storeCachedMetadata: function (): void {},
+    getCachedMetadata: function () {},
+  }
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    ssrMode: true,
+    link: new SchemaLink({ schema, context }),
+  })
+}
+
+describe('company-resolver', () => {
+  let client: ApolloClient<any>
+  beforeEach(async () => {
+    fetchMock.resetHistory()
+    setupCompaniesMocks()
+    client = await getGraphqlClient()
+  })
+
+  describe('listCompanies', () => {
+    it('should return companies', async () => {
+      const result = await client.query({
+        query: listCompaniesQuery,
+      })
+      expect(result.data.listCompanies).toBeDefined()
+      expect(result.data.listCompanies[0]).toEqual({
+        __typename: 'Company',
+        id: 'MKT22000047',
+        name: 'Reapit Company',
+        branch: 'Oxford',
+        notes: null,
+        created: '2022-04-01T09:12:38.000Z',
+        modified: '2022-04-19T06:11:11.000Z',
+        active: false,
+        vatRegistered: false,
+        typeIds: ['SP'],
+        supplierTypeId: null,
+        workPhone: '4455667788',
+        mobilePhone: null,
+        email: 'example@email.com',
+        address: {
+          __typename: 'CompanyAddress',
+          buildingNumber: '15',
+          buildingName: '123',
+          line1: 'Example street',
+          line2: 'Solihull',
+          line3: 'West Midlands',
+          line4: '',
+          postcode: 'B91 2XX',
+          country: 'GB',
+        },
+      })
+    })
+  })
+
+  // describe('searchContacts', () => {
+  //   it('should return contacts', async () => {
+  //     const result = await client.query({
+  //       query: gql`
+  //         query SearchContacts($query: String!) {
+  //           searchContacts(query: $query) {
+  //             id
+  //             forename
+  //             surname
+  //             title
+  //             email
+  //             marketingConsent
+  //           }
+  //         }
+  //       `,
+  //       variables: {
+  //         query: 'Smith',
+  //       },
+  //     })
+  //     expect(result.data.searchContacts).toBeDefined()
+  //     expect(result.data.searchContacts[0]).toEqual({
+  //       __typename: 'Contact',
+  //       id: 'RPT20000017',
+  //       title: 'Mr',
+  //       forename: 'John',
+  //       surname: 'Smith',
+  //       email: 'example@email.com',
+  //       marketingConsent: 'grant',
+  //     })
+  //   })
+  // })
+
+  describe('getCompany', () => {
+    it('should return company', async () => {
+      const result = await client.query({
+        query: gql`
+          query ($id: String!) {
+            getCompany(id: $id) {
+              id
+              created
+              modified
+              name
+              branch
+              notes
+              active
+              vatRegistered
+              typeIds
+              supplierTypeId
+              workPhone
+              mobilePhone
+              email
+              metadata
+              address {
+                buildingName
+                buildingNumber
+                line1
+                line2
+                line3
+                line4
+                postcode
+                country
+              }
+            }
+          }
+        `,
+        variables: {
+          id: 'MKT22000047',
+        },
+      })
+      expect(result.data.getCompany).toBeDefined()
+      expect(result.data.getCompany).toEqual({
+        __typename: 'Company',
+        id: 'MKT22000047',
+        name: 'Reapit Company',
+        branch: 'Oxford',
+        notes: null,
+        created: '2022-04-01T09:12:38.000Z',
+        modified: '2022-04-19T06:11:11.000Z',
+        active: false,
+        vatRegistered: false,
+        typeIds: ['SP'],
+        supplierTypeId: null,
+        workPhone: '4455667788',
+        mobilePhone: null,
+        email: 'example@email.com',
+        address: {
+          buildingNumber: '15',
+          buildingName: '123',
+          line1: 'Example street',
+          line2: 'Solihull',
+          line3: 'West Midlands',
+          line4: '',
+          postcode: 'B91 2XX',
+          country: 'GB',
+        },
+      })
+    })
+  })
+
+  describe('createCompany', () => {
+    it('should create company', async () => {
+      const result = await client.mutate({
+        mutation: gql`
+          mutation CreateCompany($input: CompanyInput!) {
+            createCompany(company: $input) {
+              id
+              created
+              modified
+              name
+              branch
+              notes
+              active
+              vatRegistered
+              typeIds
+              supplierTypeId
+              workPhone
+              mobilePhone
+              email
+              metadata
+              address {
+                buildingName
+                buildingNumber
+                line1
+                line2
+                line3
+                line4
+                postcode
+                country
+              }
+            }
+          }
+        `,
+        variables: {
+          input: {
+            name: 'Reapit Company',
+            branch: 'Oxford',
+            notes: null,
+            created: '2022-04-01T09:12:38.000Z',
+            modified: '2022-04-19T06:11:11.000Z',
+            active: false,
+            vatRegistered: false,
+            typeIds: ['SP'],
+            supplierTypeId: null,
+            workPhone: '4455667788',
+            mobilePhone: null,
+            email: 'example@email.com',
+            address: {
+              buildingNumber: '15',
+              buildingName: '123',
+              line1: 'Example street',
+              line2: 'Solihull',
+              line3: 'West Midlands',
+              line4: '',
+              postcode: 'B91 2XX',
+              country: 'GB',
+            },
+          },
+        },
+      })
+
+      expect(result.data.createCompany).toBeDefined()
+    })
+  })
+  describe('updateCompany', () => {
+    it('should update company', async () => {
+      const result = await client.mutate({
+        mutation: gql`
+          mutation UpdateCompany($id: String!, $input: CompanyInput!) {
+            updateCompany(id: $id, company: $input) {
+              id
+              created
+              modified
+              name
+              branch
+              notes
+              active
+              vatRegistered
+              typeIds
+              supplierTypeId
+              workPhone
+              mobilePhone
+              email
+              metadata
+              address {
+                buildingName
+                buildingNumber
+                line1
+                line2
+                line3
+                line4
+                postcode
+                country
+              }
+            }
+          }
+        `,
+        variables: {
+          id: 'MKT22000047',
+          input: {
+            name: 'Reapit Company',
+            branch: 'Oxford',
+            notes: null,
+            created: '2022-04-01T09:12:38.000Z',
+            modified: '2022-04-19T06:11:11.000Z',
+            active: false,
+            vatRegistered: false,
+            typeIds: ['SP'],
+            supplierTypeId: null,
+            workPhone: '4455667788',
+            mobilePhone: null,
+            email: 'example@email.com',
+            address: {
+              buildingNumber: '15',
+              buildingName: '123',
+              line1: 'Example street',
+              line2: 'Solihull',
+              line3: 'West Midlands',
+              line4: '',
+              postcode: 'B91 2XX',
+              country: 'GB',
+            },
+          },
+        },
+      })
+
+      expect(result.data.updateCompany).toBeDefined()
+    })
+  })
+})
---
+++ packages/app-builder-backend/src/resolvers/company-resolver.ts
@@ -0,0 +1,248 @@
+import { Company, CompanyFragment, CompanyInput } from '../entities/company'
+import { query } from '../utils/graphql-fetch'
+import { Context } from '@apollo/client'
+import { gql } from 'apollo-server-core'
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql'
+
+const getCompaniesQuery = gql`
+  ${CompanyFragment}
+  {
+    GetCompanies {
+      _embedded {
+        ...CompanyFragment
+      }
+    }
+  }
+`
+
+const getCompanyQuery = gql`
+  ${CompanyFragment}
+  query GetCompany($id: String!) {
+    GetCompanyById(id: $id) {
+      ...CompanyFragment
+    }
+  }
+`
+
+const createCompanyMutation = gql`
+  ${CompanyFragment}
+  mutation CreateCompany(
+    $name: String!
+    $branch: String
+    $notes: String
+    $active: Boolean
+    $vatRegistered: Boolean
+    $typeIds: [String]
+    $supplierTypeId: String
+    $workPhone: String
+    $mobilePhone: String
+    $email: String!
+    $metadata: JSON
+    $address: CompanyAddressInput!
+  ) {
+    CreateCompany(
+      name: $name
+      branch: $branch
+      notes: $notes
+      active: $active
+      vatRegistered: $vatRegistered
+      typeIds: $typeIds
+      supplierTypeId: $supplierTypeId
+      workPhone: $workPhone
+      mobilePhone: $mobilePhone
+      email: $email
+      metadata: $metadata
+      address: $address
+    ) {
+      ...CompanyFragment
+    }
+  }
+`
+
+const updateCompanyMutation = gql`
+  ${CompanyFragment}
+  mutation UpdateCompany(
+    $id: String!
+    $name: String!
+    $branch: String
+    $notes: String
+    $active: Boolean
+    $vatRegistered: Boolean
+    $typeIds: [String]
+    $supplierTypeId: String
+    $workPhone: String
+    $mobilePhone: String
+    $email: String!
+    $metadata: JSON
+    $address: CompanyAddressInput!
+  ) {
+    UpdateCompany(
+      id: $id
+      name: $name
+      branch: $branch
+      notes: $notes
+      active: $active
+      vatRegistered: $vatRegistered
+      typeIds: $typeIds
+      supplierTypeId: $supplierTypeId
+      workPhone: $workPhone
+      mobilePhone: $mobilePhone
+      email: $email
+      metadata: $metadata
+      address: $address
+    ) {
+      ...CompanyFragment
+    }
+  }
+`
+
+type CompanyAPIResponse<T> = Company & {
+  _embedded: T
+  _eTag: string
+}
+
+type CompaniesEmbeds = {}
+
+const getApiCompany = async (
+  id: string,
+  accessToken: string,
+  idToken: string,
+): Promise<CompanyAPIResponse<CompaniesEmbeds> | null> => {
+  return query<CompanyAPIResponse<CompaniesEmbeds> | null>(getCompanyQuery, { id }, 'GetCompanyById', {
+    accessToken,
+    idToken,
+  })
+}
+
+const addDefaultEmbeds = (company: Company): Company => ({
+  ...company,
+})
+
+const hoistEmbeds = <T, E>(object: T & { _embedded: any }): T & E => {
+  const { _embedded, ...rest } = object
+  return { ...rest, ..._embedded }
+}
+
+const convertDates = (company: Company): Company => ({
+  ...company,
+  created: new Date(company.created),
+  modified: new Date(company.modified),
+})
+
+const getCompany = async (id: string, accessToken: string, idToken: string): Promise<Company | null> => {
+  const company = await getApiCompany(id, accessToken, idToken)
+
+  if (!company) {
+    return null
+  }
+
+  // const hoistedCompanies = hoistEmbeds<CompanyAPIResponse<CompaniesEmbeds>, CompaniesEmbeds>(company)
+  return convertDates(addDefaultEmbeds(company))
+}
+
+const getCompanies = async (accessToken: string, idToken: string): Promise<Company[]> => {
+  const companies = await query<{ _embedded: CompanyAPIResponse<CompaniesEmbeds>[] }>(
+    getCompaniesQuery,
+    {},
+    'GetCompanies',
+    {
+      accessToken,
+      idToken,
+    },
+  )
+
+  return companies._embedded
+    .map((c) => hoistEmbeds<CompanyAPIResponse<CompaniesEmbeds>, CompaniesEmbeds>(c))
+    .map(addDefaultEmbeds)
+    .map(convertDates)
+}
+
+const createCompany = async (accessToken: string, idToken: string, company: CompanyInput): Promise<Company> => {
+  const res = await query<CompanyAPIResponse<null>>(createCompanyMutation, company, 'CreateCompany', {
+    accessToken,
+    idToken,
+  })
+  const { id } = res
+  const newCompany = await getCompany(id, accessToken, idToken)
+  if (!newCompany) {
+    throw new Error('Failed to create company')
+  }
+  return newCompany
+}
+
+const updateCompany = async (
+  accessToken: string,
+  idToken: string,
+  id: string,
+  company: CompanyInput,
+): Promise<Company> => {
+  const existingCompany = await getApiCompany(id, accessToken, idToken)
+  if (!existingCompany) {
+    throw new Error(`Company with id ${id} not found`)
+  }
+  const { _eTag } = existingCompany
+  await query<CompanyAPIResponse<null>>(updateCompanyMutation, { ...company, id, _eTag }, 'UpdateCompany', {
+    accessToken,
+    idToken,
+  })
+
+  const newCompany = await getCompany(id, accessToken, idToken)
+  if (!newCompany) {
+    throw new Error('Company not found')
+  }
+  return newCompany
+}
+
+const entityName = 'company'
+
+@Resolver(() => Company)
+export class CompanyResolver {
+  @Authorized()
+  @Query(() => [Company])
+  async listCompanies(@Ctx() { idToken, accessToken, storeCachedMetadata }: Context): Promise<Company[]> {
+    const companies = await getCompanies(accessToken, idToken)
+    companies?.forEach((company) => {
+      storeCachedMetadata(entityName, company.id, company.metadata)
+    })
+    return companies
+  }
+
+  @Authorized()
+  @Query(() => Company)
+  async GetCompany(
+    @Ctx() { idToken, accessToken, storeCachedMetadata }: Context,
+    @Arg('id') id: string,
+  ): Promise<Company> {
+    const company = await getCompany(id, accessToken, idToken)
+    if (!company) {
+      throw new Error(`Company with id ${id} not found`)
+    }
+    storeCachedMetadata(entityName, id, company.metadata)
+    return company
+  }
+
+  @Authorized()
+  @Mutation(() => Company)
+  async createCompany(
+    @Ctx() { idToken, accessToken, storeCachedMetadata, operationMetadata }: Context,
+    @Arg(entityName) company: CompanyInput,
+  ): Promise<Company> {
+    const { [entityName]: metadata } = operationMetadata
+    const newCompany = await createCompany(accessToken, idToken, { ...company, metadata })
+    storeCachedMetadata(entityName, newCompany.id, newCompany.metadata)
+    return newCompany
+  }
+
+  @Authorized()
+  @Mutation(() => Company)
+  async updateCompany(
+    @Ctx() { idToken, accessToken, storeCachedMetadata, operationMetadata }: Context,
+    @Arg('id') id: string,
+    @Arg(entityName) company: CompanyInput,
+  ): Promise<Company> {
+    const { [entityName]: metadata } = operationMetadata
+    const newCompany = await updateCompany(accessToken, idToken, id, { ...company, metadata })
+    storeCachedMetadata(entityName, newCompany.id, newCompany.metadata)
+    return newCompany
+  }
+}
