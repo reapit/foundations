5fb8368e48cadf799734800b66bcf468cc10d946
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-05-20 10:49:55 +0100

    feat: #5923 office shared (#6906)
    * feat: added office resolver
    
    * feat: added office resolver test

---
+++ packages/app-builder-backend/src/resolvers/__mocks__/mock-office.ts
@@ -0,0 +1,9 @@
+export const mockOffice = {
+  id: 'CNO',
+  name: 'create new office 5',
+  created: '2020-03-16T08:54:08.000Z',
+  modified: '2020-03-16T08:54:08.000Z',
+  workPhone: '0987654321',
+  email: 'tester@yahoo.com',
+  metadata: {},
+}
