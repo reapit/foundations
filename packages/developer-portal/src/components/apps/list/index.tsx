cddbe4bb4762e26ecad61943851fe34a1366ab10
Will McVay <wmcvay@reapit.com>
2022-05-23 16:57:14 +0100

    feat: #6921 paginate apps

--- packages/developer-portal/src/components/apps/list/index.tsx
+++ packages/developer-portal/src/components/apps/list/index.tsx
@@ -1,5 +1,16 @@
 import React, { FC } from 'react'
-import { Button, ButtonGroup, Col, FlexContainer, Title, useMediaQuery, Grid, useModal } from '@reapit/elements'
+import {
+  Button,
+  ButtonGroup,
+  Col,
+  FlexContainer,
+  Title,
+  useMediaQuery,
+  Grid,
+  useModal,
+  Pagination,
+  elMb11,
+} from '@reapit/elements'
 import { AppCard } from './app-card'
 import { useAppState } from '../state/use-app-state'
 import { Helper } from '../page/helper'
@@ -9,8 +20,11 @@ export const AppsList: FC = () => {
   const { appsDataState } = useAppState()
   const { Modal, openModal, closeModal } = useModal()
 
-  const { apps } = appsDataState
-
+  const { apps, appsSetPageNumber } = appsDataState
+  const pageNumber = apps?.pageNumber ?? 1
+  const totalCount = apps?.totalCount ?? 0
+  const pageSize = apps?.pageSize ?? 0
+  const numberPages = Math.ceil((totalCount ?? 1) / (pageSize ?? 1))
   return (
     <>
       <FlexContainer isFlexJustifyBetween>
@@ -33,13 +47,16 @@ export const AppsList: FC = () => {
           </ButtonGroup>
         </Modal>
       )}
-      <Grid>
+      <Grid className={elMb11}>
         {apps?.data?.map((app) => (
           <Col key={app.id}>
             <AppCard app={app} />
           </Col>
         ))}
       </Grid>
+      {numberPages > 1 && (
+        <Pagination callback={appsSetPageNumber} currentPage={pageNumber} numberPages={numberPages} />
+      )}
     </>
   )
 }
