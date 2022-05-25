12dc7f839b51036542b5ba3a8e04f377a1e4eaf1
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-05-25 17:35:29 +0100

    refactor: removed dead utils files

--- packages/deployment-service/src/utils/index.ts
+++ packages/deployment-service/src/utils/index.ts
@@ -1,4 +1,1 @@
-export * from './ownership'
-export * from './resolve-customer'
-export * from './resolve-creds'
 export * from './recurse-dir'
--- packages/deployment-service/src/utils/ownership.ts
+++
@@ -1,7 +0,0 @@
-import { ForbiddenException } from '@homeservenow/serverless-aws-handler'
-
-export const ownership = (developerId: string | undefined, customerDeveloperId: string): void | never => {
-  if (!developerId || developerId !== customerDeveloperId) {
-    throw new ForbiddenException()
-  }
-}
