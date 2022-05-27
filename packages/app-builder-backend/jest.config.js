d1eb6fdadc4ade4bfbc0776aaa4d61be23182b76
Josh Balfour <josh@joshbalfour.co.uk>
2022-05-27 14:01:54 +0100

    update tests

--- packages/app-builder-backend/jest.config.js
+++ packages/app-builder-backend/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   coveragePathIgnorePatterns: [
-    '<rootDir>[/\\\\](node_modules|src/types|src/tests|src/scripts)[/\\\\]',
+    '<rootDir>[/\\\\](node_modules|src/types|src/tests|src/scripts|dist)[/\\\\]',
     'index.tsx',
     '.d.ts',
   ],
