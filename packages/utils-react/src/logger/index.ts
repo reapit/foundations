174de008371f2a7aae601cbdc5fe53a13109a954
Will McVay <wmcvay@reapit.com>
2022-06-07 13:58:10 +0100

    feat: #6919 sentry logs improvements

--- packages/utils-react/src/logger/index.ts
+++ packages/utils-react/src/logger/index.ts
@@ -7,25 +7,24 @@ import { ReapitConnectSession } from '@reapit/connect-session'
  * Used to send custom logging message to Sentry
  */
 export const logger = (error: Error, connectSession?: ReapitConnectSession | null) => {
-  // if (process.env.NODE_ENV === 'production') {
-  console.log('here')
-  Sentry.withScope((scope) => {
-    scope.setExtra('Error', {
+  if (process.env.NODE_ENV === 'production') {
+    Sentry.setContext('Error', {
       message: error.message ?? 'No error message caputured',
       page: window.location.href,
       connectSession: connectSession?.loginIdentity ?? 'No Session Logged',
       permissionGroups: connectSession?.loginIdentity.groups ?? [],
+      accessToken: connectSession?.accessToken ?? 'No Access Token Logged',
+      idToken: connectSession?.idToken ?? 'No Id Token Logged',
     })
-    // if (connectSession) {
-    //   Sentry.setUser({
-    //     email: connectSession.loginIdentity.email ?? 'No email logged',
-    //     username: connectSession.loginIdentity.name ?? 'No name logged',
-    //     id: connectSession.loginIdentity.clientId ?? 'No client id logged',
-    //   })
-    // }
+    if (connectSession) {
+      Sentry.setUser({
+        email: connectSession.loginIdentity.email ?? 'No email logged',
+        username: connectSession.loginIdentity.name ?? 'No name logged',
+        id: connectSession.loginIdentity.clientId ?? 'No client id logged',
+      })
+    }
     Sentry.captureException(error)
-  })
-  // } else {
-  //   console.error(error.message)
-  // }
+  } else {
+    console.error(error.message)
+  }
 }
