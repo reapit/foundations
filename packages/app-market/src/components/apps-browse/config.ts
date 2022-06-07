000196c08c76c9f50905b1622a13d03dff90e97f
Will McVay <wmcvay@reapit.com>
2022-06-07 17:03:14 +0100

    feat: #6995 wip

--- packages/app-market/src/components/apps-browse/config.ts
+++ packages/app-market/src/components/apps-browse/config.ts
@@ -2,38 +2,38 @@ import { AppsBrowseConfigCollection } from './use-apps-browse-state'
 
 const appsBrowseConfigCollectionDev: AppsBrowseConfigCollection = {
   featuredHeroApps: [
-    {
-      filters: {
-        id: ['FOO'],
-      },
-      content: {
-        title: 'New in AppMarket',
-        brandColour: '',
-        strapline: 'Geo Diary',
-        heroImageUrl: 'http://placekitten.com/1000/360',
-      },
-    },
-    {
-      filters: {
-        id: ['FOO'],
-      },
-      content: {
-        title: 'Discover Amazing Apps',
-        brandColour: '',
-        strapline: 'AML Checklist',
-        heroImageUrl: 'http://placekitten.com/1000/360',
-      },
-    },
-    {
-      filters: {
-        id: ['FOO'],
-      },
-      content: {
-        brandColour: '',
-        strapline: 'Geo Foo Bar',
-        heroImageUrl: 'http://placekitten.com/1000/360',
-      },
-    },
+    // {
+    //   filters: {
+    //     id: ['FOO'],
+    //   },
+    //   content: {
+    //     title: 'New in AppMarket',
+    //     brandColour: '',
+    //     strapline: 'Geo Diary',
+    //     heroImageUrl: 'http://placekitten.com/1000/360',
+    //   },
+    // },
+    // {
+    //   filters: {
+    //     id: ['FOO'],
+    //   },
+    //   content: {
+    //     title: 'Discover Amazing Apps',
+    //     brandColour: '',
+    //     strapline: 'AML Checklist',
+    //     heroImageUrl: 'http://placekitten.com/1000/360',
+    //   },
+    // },
+    // {
+    //   filters: {
+    //     id: ['FOO'],
+    //   },
+    //   content: {
+    //     brandColour: '',
+    //     strapline: 'Geo Foo Bar',
+    //     heroImageUrl: 'http://placekitten.com/1000/360',
+    //   },
+    // },
   ],
   appsFilters: [],
   featuredApps: [],
