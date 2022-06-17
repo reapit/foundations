17396499f2cc65b1a1ee7c298fdcccccb8149262
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-06-17 14:34:33 +0100

    feat: additional unit tests (#7083)
    * feat: added tests for parameter provider
    
    * feat: added test file for pipeline controller
    
    * feat: added pipeline edit test
    
    * feat: delete pipeline controller test
    
    * feat: pipeline setup workflow test
    
    * feat: added additional requirements for function calls
    
    * feat: failure test for pipeline setup workflow
    
    * chore: remove console log

---
+++ packages/deployment-service/src/pipeline/__test__/pipeline-setup-workflow.test.ts
@@ -0,0 +1,161 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { PipelineSetupWorkflow } from '../pipeline-setup-workflow'
+import { PusherProvider, SqsProvider } from '../../events'
+import { PipelineProvider } from '../pipeline-provider'
+import { SQS, S3 } from 'aws-sdk'
+import { CloudFrontClient } from '@aws-sdk/client-cloudfront'
+import { Route53Client } from '@aws-sdk/client-route-53'
+import { S3Provider } from '../../s3'
+import { v4 as uuid } from 'uuid'
+import { SQSRecord } from 'aws-lambda'
+
+const mockS3Provider = {
+  upload: jest.fn(),
+}
+
+const mockPipelineProvider = {
+  update: jest.fn(),
+}
+
+const mockPusherProvider = {
+  trigger: jest.fn(),
+}
+
+const mockCloudFrontClient = {
+  send: jest.fn(),
+}
+
+const mockRoute53Client = {
+  send: jest.fn(),
+}
+
+const mockSqsProvider = {
+  deleteMessage: jest.fn(),
+}
+
+describe('PipelineSetupWorkflow', () => {
+  let module: TestingModule
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      providers: [
+        PipelineSetupWorkflow,
+        {
+          provide: S3,
+          useValue: {},
+        },
+        {
+          provide: S3Provider,
+          useValue: mockS3Provider,
+        },
+        {
+          provide: PusherProvider,
+          useValue: mockPusherProvider,
+        },
+        {
+          provide: PipelineProvider,
+          useValue: mockPipelineProvider,
+        },
+        {
+          provide: SQS,
+          useValue: {},
+        },
+        {
+          provide: SqsProvider,
+          useValue: mockSqsProvider,
+        },
+        {
+          provide: CloudFrontClient,
+          useValue: mockCloudFrontClient,
+        },
+        {
+          provide: Route53Client,
+          useValue: mockRoute53Client,
+        },
+      ],
+    }).compile()
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('Successfully provision', async () => {
+    const pipelineSetupWorkflow = module.get<PipelineSetupWorkflow>(PipelineSetupWorkflow)
+    const pipelineId = uuid()
+    const developerId = uuid()
+
+    mockCloudFrontClient.send.mockImplementationOnce(() => ({
+      Distribution: {
+        DomainName: 'domain',
+        Id: 'id',
+      },
+    }))
+
+    mockRoute53Client.send.mockImplementationOnce(() => ({
+      ChangeInfo: {
+        Id: 'id',
+      },
+    }))
+
+    mockPipelineProvider.update.mockImplementation((prev, dto) => ({
+      ...prev,
+      ...dto,
+    }))
+
+    await pipelineSetupWorkflow.run({
+      receiptHandle: 'receipt',
+      body: JSON.stringify({
+        id: pipelineId,
+        developerId,
+        buildStatus: 'PROVISION_REQUEST',
+      }),
+    } as SQSRecord)
+
+    expect(mockPusherProvider.trigger).toHaveBeenLastCalledWith(`private-${developerId}`, 'pipeline-update', {
+      aRecordId: 'id',
+      buildCommand: 'build',
+      buildStatus: 'READY_FOR_DEPLOYMENT',
+      cloudFrontId: 'id',
+      id: pipelineId,
+      message: 'Pipeline successfully created',
+      outDir: 'build',
+      developerId,
+    })
+    expect(mockCloudFrontClient.send).toHaveBeenCalled()
+    expect(mockRoute53Client.send).toHaveBeenCalled()
+    expect(mockSqsProvider.deleteMessage).toHaveBeenCalled()
+  })
+
+  it('Failed to provision result', async () => {
+    const pipelineSetupWorkflow = module.get<PipelineSetupWorkflow>(PipelineSetupWorkflow)
+    const pipelineId = uuid()
+    const developerId = uuid()
+
+    mockCloudFrontClient.send.mockImplementationOnce(() => undefined)
+
+    mockPipelineProvider.update.mockImplementation((prev, dto) => ({
+      ...prev,
+      ...dto,
+    }))
+
+    await pipelineSetupWorkflow.run({
+      receiptHandle: 'receipt',
+      body: JSON.stringify({
+        id: pipelineId,
+        developerId,
+        buildStatus: 'PROVISION_REQUEST',
+      }),
+    } as SQSRecord)
+
+    expect(mockPusherProvider.trigger).toHaveBeenLastCalledWith(`private-${developerId}`, 'pipeline-update', {
+      buildCommand: 'build',
+      buildStatus: 'FAILED_TO_PROVISION',
+      id: pipelineId,
+      message: 'Failed to architect',
+      outDir: 'build',
+      developerId,
+    })
+    expect(mockRoute53Client.send).not.toHaveBeenCalled()
+    expect(mockSqsProvider.deleteMessage).toHaveBeenCalled()
+  })
+})
