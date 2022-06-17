0808f5d426330ebb1364d164175f4b07e67dbebd
Will McVay <wmcvay@reapit.com>
2022-06-17 10:40:55 +0100

    feat: #7078 tweaks on consents and pipelines

--- packages/developer-portal/src/components/apps/pipeline/pipeline-configure.tsx
+++ packages/developer-portal/src/components/apps/pipeline/pipeline-configure.tsx
@@ -20,7 +20,6 @@ import {
 import { httpsUrlRegex, UpdateActionNames, updateActions } from '@reapit/utils-common'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
-import errorMessages from '@/constants/error-messages'
 import { useReapitUpdate } from '@reapit/utils-react'
 import { reapitConnectBrowserSession } from '../../../core/connect-session'
 import { useReapitConnect } from '@reapit/connect-session'
@@ -56,8 +55,15 @@ const schema: SchemaOf<PipelineModelSchema> = object().shape({
   branch: string().required('Required - eg "main", "master"').test(specialCharsTest),
   repository: string()
     .trim()
-    .required(errorMessages.FIELD_REQUIRED)
-    .matches(httpsUrlRegex, 'Should be a secure https url'),
+    .test({
+      name: 'isValidDescription',
+      message: 'Should be a secure https url if supplied',
+      test: (value) => {
+        if (!value) return true
+        return httpsUrlRegex.test(value)
+      },
+    })
+    .test(specialCharsTest),
   buildCommand: string().trim().required('A build command is required eg "build" or "bundle"').test(specialCharsTest),
   packageManager: boolean().required('Required - either yarn or NPM'),
   outDir: string().required('Required eg "dist" or "public').test(specialCharsTest),
@@ -71,12 +77,18 @@ export const handlePipelineUpdate =
     appId: string | null,
   ) =>
   async (values: PipelineModelSchema) => {
-    const result = await updatePipeline({
+    const updateModel = {
       ...values,
       packageManager: values.packageManager ? PackageManagerEnum.YARN : PackageManagerEnum.NPM,
       appId: appId ?? '',
       appType: AppTypeEnum.REACT, // TODO make this an option
-    })
+    }
+
+    if (!updateModel.repository) {
+      delete updateModel.repository
+    }
+
+    const result = await updatePipeline(updateModel)
 
     if (result) {
       setAppPipelineSaving(false)
@@ -115,6 +127,7 @@ export const getDefaultValues = (appPipeline: PipelineModelInterface | null, app
   return {
     name: appDetail?.name,
     buildCommand: 'build',
+    outDir: 'build',
     branch: 'main',
     packageManager: true,
   }
