fc5e347ce4a6d5677f5660e0665283c2d1aca281
Will McVay <wmcvay@reapit.com>
2022-06-22 10:47:50 +0100

    feat: #6995 tweak to config

--- packages/app-market/src/components/apps-browse/config.ts
+++ packages/app-market/src/components/apps-browse/config.ts
@@ -40,15 +40,37 @@ const appsBrowseConfigCollectionDev: AppsBrowseConfigCollection = {
     },
     {
       filters: {
-        id: ['795fc578-8232-4dbd-a5ff-1240135c0a52'],
+        id: [
+          '795fc578-8232-4dbd-a5ff-1240135c0a52',
+          'dab46391-b07d-4869-8614-139aa798cc1f',
+          'cfbe6f29-4003-4648-aa5a-8eb777bd606e',
+        ],
       },
       content: {
-        title: 'Stremline Your Worflow',
+        title: 'Reapit Developed Apps',
         brandColour: '#0061a8',
         strapline: 'Lorem ipsum dolor sit amet, consectetur adip',
         imageUrl: 'http://placekitten.com/100/100',
       },
       configType: 'appsFilters',
+    },
+    {
+      filters: {
+        desktopIntegrationTypeId: ['Contact', 'IdCheck', 'Applicant', 'Tenancy'],
+      },
+      content: {
+        title: 'Indentity Apps',
+      },
+      configType: 'featuredApps',
+    },
+    {
+      filters: {
+        isFree: true,
+      },
+      content: {
+        title: 'Free Apps',
+      },
+      configType: 'simpleApps',
     },
   ],
 }
