fab82b6cbe81c36f6bda4054a6d445a4496fee82
Josh Balfour <josh@joshbalfour.co.uk>
2022-06-23 16:15:16 +0100

    fix scriptName (#7143)

--- packages/ts-scripts/src/cdk/bin/rpt-cdk.ts
+++ packages/ts-scripts/src/cdk/bin/rpt-cdk.ts
@@ -10,7 +10,7 @@ if (command) {
   try {
     execSync(
       `yarn cdk ${command} --require-approval never --output=${templatesDir} --all -a "cd ${cwd} && yarn dlx ts-node ${
-        scriptName.split(' ')[0]
+        scriptName.split('--')[0]
       }"`,
       {
         stdio: 'inherit',
