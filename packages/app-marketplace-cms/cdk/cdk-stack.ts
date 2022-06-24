f9b32a87d178d2134c927cd253666586b40c1f9e
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-06-24 14:50:45 +0100

    feat: added cdk setup

---
+++ packages/app-marketplace-cms/cdk/cdk-stack.ts
@@ -0,0 +1,41 @@
+import * as path from 'path'
+import { addLambdaToApi, createApi, createBaseStack, createTable, createFunction } from '@reapit/ts-scripts/src/cdk'
+import config from '../config.json'
+
+export const createStack = async () => {
+  const stack = createBaseStack({
+    namespace: 'cloud',
+    appName: 'app-marketplace-cms',
+    component: 'service',
+    accountId: config.AWS_ACCOUNT_ID,
+  })
+
+  const api = createApi(stack, 'apigateway', undefined)
+
+  const dynamodb = createTable(stack, config.DYNAMO_MARKETPLACE_CMS_TABLE_NAME, 'id')
+  const env = {
+    ...config,
+    DYNAMO_MARKETPLACE_CMS_TABLE_NAME: dynamodb.tableName,
+  }
+
+  const httpLambda = createFunction(
+    stack,
+    'marketplace-app-cms-http',
+    path.resolve('bundle.zip'),
+    'packages/app-marketplace-cms/src/http.handler',
+    env,
+  )
+  dynamodb.grantReadWriteData(httpLambda)
+
+  addLambdaToApi(
+    stack,
+    api,
+    httpLambda,
+    {
+      path: '{proxy+}',
+      method: 'ANY',
+    },
+    // @ts-ignore
+    config.AUTHORIZER_ID as string,
+  )
+}
