207c6a8890c3846be0d2effa033e919110d4b968
Josh Balfour <josh@joshbalfour.co.uk>
2022-06-28 17:34:12 +0100

    fix created/modified types

--- packages/app-builder-backend/src/entities/company.ts
+++ packages/app-builder-backend/src/entities/company.ts
@@ -104,12 +104,6 @@ export class CompanyAddressInput {
 
 @InputType()
 export class CompanyInput {
-  @Field(() => GraphQLISODateTime)
-  created: Date
-
-  @Field(() => GraphQLISODateTime)
-  modified: Date
-
   @Field(() => CompanyAddressInput)
   address: CompanyAddressInput
 
