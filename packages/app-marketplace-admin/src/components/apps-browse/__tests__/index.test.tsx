297727583478c438ab8d8dbfbcc567b1d79e215e
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-07-06 10:10:25 +0100

    chore: remove tests

--- packages/app-marketplace-admin/src/components/apps-browse/__tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '../../../tests/react-testing'
-import AppsBrowsePage from '..'
-
-describe('AppsBrowsePage', () => {
-  it('should match a snapshot', () => {
-    expect(render(<AppsBrowsePage />)).toMatchSnapshot()
-  })
-})
