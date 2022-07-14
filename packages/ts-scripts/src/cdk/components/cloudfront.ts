8ce30c5170875768d220cef4bb026e7aab94ffef
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-07-14 14:28:53 +0100

    feat: added missing sentryclirc file (#7290)
    * feat: added missing sentryclirc file
    
    * fix: added type to stack parameter
    
    * feat: added missing package severless-offline
    
    * chore: linting

--- packages/ts-scripts/src/cdk/components/cloudfront.ts
+++ packages/ts-scripts/src/cdk/components/cloudfront.ts
@@ -1,5 +1,9 @@
 import * as cdk from 'aws-cdk-lib'
 
-export const createCloudfront = (stack, name: string, options: cdk.aws_cloudfront.CloudFrontWebDistributionProps) => {
+export const createCloudfront = (
+  stack: cdk.Stack,
+  name: string,
+  options: cdk.aws_cloudfront.CloudFrontWebDistributionProps,
+) => {
   return new cdk.aws_cloudfront.CloudFrontWebDistribution(stack, name, options)
 }
