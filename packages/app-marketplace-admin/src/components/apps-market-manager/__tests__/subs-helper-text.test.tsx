01c6f6f163a972653aa9287a24714298b581acb5
Will McVay <wmcvay@reapit.com>
2022-08-10 14:23:29 +0100

    feat: #7405 cms helper text

---
+++ packages/app-marketplace-admin/src/components/apps-market-manager/__tests__/subs-helper-text.test.tsx
@@ -0,0 +1,13 @@
+import { AppsBrowseConfigEnum } from '@reapit/foundations-ts-definitions'
+import React from 'react'
+import { render } from '../../../tests/react-testing'
+import { ModalHelperText, SubsHelperText } from '../subs-helper-text'
+
+describe('SubsHelperText', () => {
+  Object.values(AppsBrowseConfigEnum).map((type) => {
+    it(`should match a snapshot for ${type}`, () => {
+      expect(render(<SubsHelperText type={type} />)).toMatchSnapshot()
+      expect(render(<ModalHelperText type={type} />)).toMatchSnapshot()
+    })
+  })
+})
