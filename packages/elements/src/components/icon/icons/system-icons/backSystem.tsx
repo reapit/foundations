f041c564d5abdb63d4a880b54333b1833751a682
Will McVay <wmcvay@reapit.com>
2022-09-02 08:38:42 +0100

    feat: #7607 wip

--- packages/elements/src/components/icon/icons/system-icons/backSystem.tsx
+++ packages/elements/src/components/icon/icons/system-icons/backSystem.tsx
@@ -2,6 +2,6 @@ import React, { memo } from 'react'
 
 export default memo(() => (
   <svg width="1em" height="1em" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2 5.5L8 9.39711L8 1.60289L2 5.5Z" fill="currentColor" />
+    <path d="M0 6L9 0V12L0 6Z" fill="currentColor" />
   </svg>
 ))
--- packages/elements/src/components/icon/icons/system-icons/nextSystem.tsx
+++ packages/elements/src/components/icon/icons/system-icons/nextSystem.tsx
@@ -2,6 +2,6 @@ import React, { memo } from 'react'
 
 export default memo(() => (
   <svg width="1em" height="1em" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9 5.5L3 9.39711L3 1.60289L9 5.5Z" fill="currentColor" />
+    <path d="M12 6L3 12V0L12 6Z" fill="currentColor" />
   </svg>
 ))
