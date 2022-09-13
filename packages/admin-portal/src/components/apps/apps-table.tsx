bb60e61485c9a334dfe82134e6809425604ddb48
Will McVay <wmcvay@reapit.com>
2022-09-13 13:05:04 +0100

    feat: #7635 adds featured app behaviour

--- packages/admin-portal/src/components/apps/apps-table.tsx
+++ packages/admin-portal/src/components/apps/apps-table.tsx
@@ -36,32 +36,65 @@ export const handleDeleteApp =
     }
   }
 
-export const handleRefreshApps =
+export const handleRefreshAppsDelete =
   (
     appsRefresh: () => void,
-    setAppId: Dispatch<SetStateAction<string | null>>,
+    setAppIdDelete: Dispatch<SetStateAction<string | null>>,
     closeModal: () => void,
     appDeleted?: boolean,
   ) =>
   () => {
     if (appDeleted) {
       appsRefresh()
-      setAppId(null)
+      setAppIdDelete(null)
       closeModal()
     }
   }
 
+export const handleRefreshAppsFeatured =
+  (appsRefresh: () => void, setAppIdFeatured: Dispatch<SetStateAction<string | null>>, shouldRefresh?: boolean) =>
+  () => {
+    if (shouldRefresh) {
+      appsRefresh()
+      setAppIdFeatured(null)
+    }
+  }
+
 export const handleOpenModal =
-  (openModal: () => void, setAppId: Dispatch<SetStateAction<string | null>>, appId?: string) => () => {
-    if (appId) {
-      setAppId(appId)
+  (openModal: () => void, setAppIdDelete: Dispatch<SetStateAction<string | null>>, appIdDelete?: string) => () => {
+    if (appIdDelete) {
+      setAppIdDelete(appIdDelete)
       openModal()
+    }
+  }
+
+export const handleToggleFeatured =
+  (
+    featureApp: SendFunction<void, boolean>,
+    unFeatureApp: SendFunction<void, boolean>,
+    apps: AppSummaryModelPagedResult | null,
+    appIdFeatured: string | null,
+  ) =>
+  () => {
+    if (appIdFeatured) {
+      const isFeatured = apps?.data?.find((app) => app.id === appIdFeatured)?.isFeatured
+      const updateFeatured = isFeatured ? unFeatureApp : featureApp
+
+      updateFeatured()
+    }
+  }
+
+export const handleAppIdFeatured =
+  (setAppIdFeatured: Dispatch<SetStateAction<string | null>>, appId?: string) => () => {
+    if (appId) {
+      setAppIdFeatured(appId)
     }
   }
 
 export const AppsTable: FC<AppsTableProps> = ({ apps, appsRefresh }) => {
   const { Modal, openModal, closeModal } = useModal()
-  const [appId, setAppId] = useState<string | null>(null)
+  const [appIdDelete, setAppIdDelete] = useState<string | null>(null)
+  const [appIdFeatured, setAppIdFeatured] = useState<string | null>(null)
   const [indexExpandedRow, setIndexExpandedRow] = useState<number | null>(null)
 
   const [, , deleteApp, appDeleted] = useReapitUpdate<void, boolean>({
@@ -69,11 +102,34 @@ export const AppsTable: FC<AppsTableProps> = ({ apps, appsRefresh }) => {
     action: updateActions(window.reapit.config.appEnv)[UpdateActionNames.deleteApp],
     method: 'DELETE',
     uriParams: {
-      appId,
+      appId: appIdDelete,
     },
   })
 
-  useEffect(handleRefreshApps(appsRefresh, setAppId, closeModal, appDeleted), [appDeleted])
+  const [, , featureApp, appFeatured] = useReapitUpdate<void, boolean>({
+    reapitConnectBrowserSession,
+    action: updateActions(window.reapit.config.appEnv)[UpdateActionNames.featureApp],
+    method: 'PUT',
+    uriParams: {
+      appId: appIdFeatured,
+    },
+  })
+
+  const [, , unFeatureApp, appUnfeatured] = useReapitUpdate<void, boolean>({
+    reapitConnectBrowserSession,
+    action: updateActions(window.reapit.config.appEnv)[UpdateActionNames.unFeatureApp],
+    method: 'DELETE',
+    uriParams: {
+      appId: appIdFeatured,
+    },
+  })
+
+  useEffect(handleRefreshAppsDelete(appsRefresh, setAppIdDelete, closeModal, appDeleted), [appDeleted])
+  useEffect(handleToggleFeatured(featureApp, unFeatureApp, apps, appIdFeatured), [apps, appIdFeatured])
+  useEffect(handleRefreshAppsFeatured(appsRefresh, setAppIdFeatured, appFeatured || appUnfeatured), [
+    appFeatured,
+    appUnfeatured,
+  ])
 
   return apps?.data?.length ? (
     <div className={elMb11}>
@@ -82,97 +138,113 @@ export const AppsTable: FC<AppsTableProps> = ({ apps, appsRefresh }) => {
       <Table
         indexExpandedRow={indexExpandedRow}
         setIndexExpandedRow={setIndexExpandedRow}
-        rows={apps.data.map(({ name, id, summary, developer, isListed, isDirectApi, created, developerId }) => ({
-          cells: [
-            {
-              label: 'App Name',
-              value: name ?? '',
-              cellHasDarkText: true,
-              narrowTable: {
-                showLabel: true,
+        rows={apps.data.map(
+          ({ name, id, summary, developer, isListed, isDirectApi, isFeatured, created, developerId }) => ({
+            cells: [
+              {
+                label: 'App Name',
+                value: name ?? '',
+                cellHasDarkText: true,
+                narrowTable: {
+                  showLabel: true,
+                },
               },
-            },
-            {
-              label: 'AppId',
-              value: id ?? '',
-              narrowTable: {
-                showLabel: true,
+              {
+                label: 'AppId',
+                value: id ?? '',
+                narrowTable: {
+                  showLabel: true,
+                },
               },
-            },
-            {
-              label: 'Summary',
-              value: summary ?? '',
-              narrowTable: {
-                showLabel: true,
+              {
+                label: 'Summary',
+                value: summary ?? '',
+                narrowTable: {
+                  showLabel: true,
+                },
               },
-            },
-            {
-              label: 'Developer Name',
-              value: developer ?? '',
-              narrowTable: {
-                showLabel: true,
+              {
+                label: 'Developer Name',
+                value: developer ?? '',
+                narrowTable: {
+                  showLabel: true,
+                },
               },
-            },
-            {
-              label: 'Listed',
-              value: <Icon icon={isListed ? 'checkSystem' : 'closeSystem'} />,
-              narrowTable: {
-                showLabel: true,
+              {
+                label: 'Listed',
+                value: <Icon icon={isListed ? 'checkSystem' : 'closeSystem'} />,
+                narrowTable: {
+                  showLabel: true,
+                },
               },
-            },
-            {
-              label: 'Integration',
-              value: <Icon icon={isDirectApi ? 'checkSystem' : 'closeSystem'} />,
-              narrowTable: {
-                showLabel: true,
+              {
+                label: 'Integration',
+                value: <Icon icon={isDirectApi ? 'checkSystem' : 'closeSystem'} />,
+                narrowTable: {
+                  showLabel: true,
+                },
               },
-            },
-            {
-              label: 'Created',
-              value: toLocalTime(created) ?? '',
-              narrowTable: {
-                showLabel: true,
+              {
+                label: 'Featured',
+                value: <Icon icon={isFeatured ? 'checkSystem' : 'closeSystem'} />,
+                narrowTable: {
+                  showLabel: true,
+                },
               },
-            },
-          ],
-          expandableContent: {
-            content: (
-              <>
-                <ButtonGroup alignment="center">
-                  <Button
-                    intent="secondary"
-                    onClick={openNewPage(`${window.reapit.config.developerPortalUri}/apps/${id}`)}
-                  >
-                    Preview
-                  </Button>
-                  <CheckAWSButton appId={id ?? ''} />
-                  <Button type="button" intent="danger" onClick={handleOpenModal(openModal, setAppId, id)}>
-                    Delete
-                  </Button>
-                  <CreateSubscriptionsButton
-                    appId={id}
-                    developerId={developerId}
-                    subscriptionType="applicationListing"
-                  />
-                </ButtonGroup>
-                <Modal title={`Confirm ${name} Deletion`}>
-                  <BodyText>
-                    Are your sure you want to remove the app &lsquo;{name}&rsquo;? By clicking &lsquo;delete&rsquo; it
-                    will remove all app data including all revisions and listings.
-                  </BodyText>
+              {
+                label: 'Created',
+                value: toLocalTime(created) ?? '',
+                narrowTable: {
+                  showLabel: true,
+                },
+              },
+            ],
+            expandableContent: {
+              content: (
+                <>
                   <ButtonGroup alignment="center">
-                    <Button fixedWidth intent="secondary" onClick={closeModal}>
-                      Cancel
+                    <Button
+                      intent="secondary"
+                      onClick={openNewPage(`${window.reapit.config.developerPortalUri}/apps/${id}`)}
+                    >
+                      Preview
                     </Button>
-                    <Button fixedWidth intent="danger" onClick={handleDeleteApp(deleteApp, setIndexExpandedRow, appId)}>
-                      Confirm
+                    <CheckAWSButton appId={id ?? ''} />
+                    <Button type="button" intent="danger" onClick={handleOpenModal(openModal, setAppIdDelete, id)}>
+                      Delete
+                    </Button>
+                    <CreateSubscriptionsButton
+                      appId={id}
+                      developerId={developerId}
+                      subscriptionType="applicationListing"
+                    />
+                    <Button intent="primary" onClick={handleAppIdFeatured(setAppIdFeatured, id)}>
+                      Togggle Featured
                     </Button>
                   </ButtonGroup>
-                </Modal>
-              </>
-            ),
-          },
-        }))}
+                  <Modal title={`Confirm ${name} Deletion`}>
+                    <BodyText>
+                      Are your sure you want to remove the app &lsquo;{name}&rsquo;? By clicking &lsquo;delete&rsquo; it
+                      will remove all app data including all revisions and listings.
+                    </BodyText>
+                    <ButtonGroup alignment="center">
+                      <Button fixedWidth intent="secondary" onClick={closeModal}>
+                        Cancel
+                      </Button>
+                      <Button
+                        fixedWidth
+                        intent="danger"
+                        onClick={handleDeleteApp(deleteApp, setIndexExpandedRow, appIdDelete)}
+                      >
+                        Confirm
+                      </Button>
+                    </ButtonGroup>
+                  </Modal>
+                </>
+              ),
+            },
+          }),
+        )}
       />
     </div>
   ) : (
