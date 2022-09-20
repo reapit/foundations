73c7df466dbd3f7ceb571bd3ded73f2a77a22d34
Will McVay <wmcvay@reapit.com>
2022-09-20 09:44:54 +0100

    chore: #7713 remove timeouts for web apps in desktop mode

--- packages/connect-session/src/browser/index.ts
+++ packages/connect-session/src/browser/index.ts
@@ -63,6 +63,8 @@ export class ReapitConnectBrowserSession {
   }
 
   private setIdleTimeoutListeners() {
+    if (this.connectIsDesktop) return
+
     const resetTimer = () => {
       clearTimeout(this.idleTimeoutCountdown)
       this.idleTimeoutCountdown = window.setTimeout(this.connectLogoutRedirect, this.connectApplicationTimeout)
