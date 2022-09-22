3eecb77ed7345e26e4f170bae175ac5970ce21c3
Will McVay <wmcvay@reapit.com>
2022-09-22 15:37:55 +0100

    feat: #7750 adds dev edition toggle

--- packages/admin-portal/src/components/developers/developers-table.tsx
+++ packages/admin-portal/src/components/developers/developers-table.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, FC, SetStateAction, useState } from 'react'
-import { DeveloperModel, DeveloperModelPagedResult } from '@reapit/foundations-ts-definitions'
+import { DeveloperModel, DeveloperModelPagedResult, UpdateDeveloperModel } from '@reapit/foundations-ts-definitions'
 import {
   PersistentNotification,
   Table,
@@ -10,11 +10,13 @@ import {
   Button,
   useModal,
 } from '@reapit/elements'
-import { toLocalTime } from '@reapit/utils-common'
+import { toLocalTime, UpdateActionNames, updateActions } from '@reapit/utils-common'
 import { CreateSubscriptionsButton } from '../subscriptions/create-subscriptions'
 import { MembersTable } from './members-table'
 import DeveloperStatusModal from './developer-status-modal'
 import { AppsTable } from './apps-table'
+import { SendFunction, useReapitUpdate } from '@reapit/utils-react'
+import { reapitConnectBrowserSession } from '../../core/connect-session'
 
 export interface DevelopersTableProps {
   developers: DeveloperModelPagedResult | null
@@ -60,11 +62,37 @@ export const handleDevIdApps =
     }
   }
 
+export const handleToggleDevEdition =
+  (
+    developer: DeveloperModel,
+    updateDeveloper: SendFunction<UpdateDeveloperModel, boolean>,
+    refreshDevelopers: () => void,
+    paysDeveloperEdition: boolean,
+  ) =>
+  async () => {
+    const newValue = paysDeveloperEdition ? 0.0 : 300.0
+
+    const response = await updateDeveloper(
+      { ...developer, companyName: developer.company, developerEditionSubscriptionCost: newValue },
+      { uriParams: { developerId: developer?.id } },
+    )
+
+    if (response) {
+      refreshDevelopers()
+    }
+  }
+
 export const DevelopersTable: FC<DevelopersTableProps> = ({ developers, refreshDevelopers }) => {
   const { Modal, openModal, closeModal } = useModal()
   const [developerUpdate, setDeveloperUpdate] = useState<DeveloperModel | null>(null)
   const [devIdMembers, setDevIdMembers] = useState<string | null>(null)
   const [devIdApps, setDevIdApps] = useState<string | null>(null)
+
+  const [, , updateDeveloper] = useReapitUpdate<UpdateDeveloperModel, boolean>({
+    reapitConnectBrowserSession,
+    action: updateActions(window.reapit.config.appEnv)[UpdateActionNames.updateDeveloper],
+    method: 'PUT',
+  })
 
   return developers?.data?.length ? (
     <div className={elMb11}>
@@ -72,7 +100,12 @@ export const DevelopersTable: FC<DevelopersTableProps> = ({ developers, refreshD
       <BodyText hasGreyText>{developers.totalCount}</BodyText>
       <Table
         rows={developers.data.map((developer) => {
-          const { company, name, id, jobTitle, status, agreedTerms } = developer
+          const { company, name, id, jobTitle, status, agreedTerms, developerEditionSubscriptionCost } = developer
+
+          const paysDeveloperEdition = Boolean(
+            developerEditionSubscriptionCost === null || Math.round(developerEditionSubscriptionCost ?? 0),
+          )
+
           return {
             cells: [
               {
@@ -124,6 +157,18 @@ export const DevelopersTable: FC<DevelopersTableProps> = ({ developers, refreshD
                       onClick={handleOpenModal(openModal, setDeveloperUpdate, developer)}
                     >
                       Update Status
+                    </Button>
+                    <Button
+                      type="button"
+                      intent="primary"
+                      onClick={handleToggleDevEdition(
+                        developer,
+                        updateDeveloper,
+                        refreshDevelopers,
+                        paysDeveloperEdition,
+                      )}
+                    >
+                      {paysDeveloperEdition ? 'Pays For DevEdition' : 'DevEdition is Free'}
                     </Button>
                     <CreateSubscriptionsButton developerId={id} subscriptionType="developerRegistration" />
                     <Button intent="secondary" onClick={handleDevIdMembers(setDevIdMembers, setDevIdApps, id)}>
