cf92d4bae78d8f095f8f0c5b09276e3cabf725cc
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-09-26 14:16:06 +0100

    feat: added ory provider to get around cognito mismatch

--- packages/connect-session-next/src/auth/index.ts
+++ packages/connect-session-next/src/auth/index.ts
@@ -1,1 +1,2 @@
 export * from './keycloak.provider'
+export * from './ory.provider'
---
+++ packages/connect-session-next/src/auth/ory.provider.ts
@@ -0,0 +1,7 @@
+import { CognitoProvider } from "./cognito.provider"
+
+export class OryProvider extends CognitoProvider {
+  getLoginEndpoint(loginRedirectUri: string | undefined): string {
+    return this.getAuthorizeEndpoint(loginRedirectUri)
+  }
+}
