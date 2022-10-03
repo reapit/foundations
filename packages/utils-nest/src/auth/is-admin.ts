11fd9076088c6888492aee1833d3ff8646de1879
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-10-03 12:03:16 +0100

    docs: added comment blocks for admin funcs

--- packages/utils-nest/src/auth/is-admin.ts
+++ packages/utils-nest/src/auth/is-admin.ts
@@ -1,10 +1,28 @@
 import { adminReadonlyGroup, adminWriteGroup } from './consts'
 import { CredsType } from './cred-guard'
 
+/**
+ * Loose admin check. Checks if scopes present for readonly or write admin
+ * 
+ * @param creds CredsType
+ * @returns boolean
+ */
 export const isAdmin = (creds: CredsType): boolean =>
   creds.type === 'jwt' && creds.groups.some((group) => [adminWriteGroup, adminReadonlyGroup].includes(group))
 
+/**
+ * Returns true if creds have required scope for write access
+ * 
+ * @param creds CredsType
+ * @returns boolean
+ */
 export const isWriteAdmin = (creds: CredsType): boolean =>
   creds.type === 'jwt' && creds.groups.includes(adminWriteGroup)
-export const isReadonlyAdmin = (creds: CredsType): boolean =>
-  creds.type === 'jwt' && creds.groups.includes(adminReadonlyGroup)
+
+/**
+ * Returns true if creds have required scopes for readonly admin
+ * 
+ * @param creds CredsType
+ * @returns boolean
+ */
+export const isReadonlyAdmin = (creds: CredsType): boolean => isAdmin(creds)
