b46d3bea99888728959060ddfcdeeffc4c2707ce
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-10-03 09:34:12 +0100

    docs: remove line comment

--- packages/utils-nest/src/auth/token-provider.ts
+++ packages/utils-nest/src/auth/token-provider.ts
@@ -6,7 +6,6 @@ export class TokenProvider {
   async resolve(authorization: string): Promise<LoginIdentity> {
     try {
       const claim = await connectSessionVerifyDecodeIdTokenWithPublicKeys(authorization)
-      // const claim = decode<Partial<LoginIdentity> & { [s: string]: string }>(authorization)
 
       if (!claim) {
         throw new Error('unauthorised')
