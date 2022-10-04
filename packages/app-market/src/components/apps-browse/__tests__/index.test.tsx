2415f78d190c337e29d63c099059fbec10304dd7
Will McVay <wmcvay@reapit.com>
2022-10-04 11:15:42 +0100

    feat: app market sort categories

--- packages/app-market/src/components/apps-browse/__tests__/index.test.tsx
+++ packages/app-market/src/components/apps-browse/__tests__/index.test.tsx
@@ -3,6 +3,7 @@ import { render } from '../../../tests/react-testing'
 import AppsBrowsePage from '..'
 
 jest.mock('../../../core/analytics')
+jest.mock('../../../core/use-apps-browse-state')
 
 describe('AppsBrowsePage', () => {
   it('should match a snapshot', () => {
