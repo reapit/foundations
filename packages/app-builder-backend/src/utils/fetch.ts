419b5ae806ea8076b539745806a8ecc901c49199
Josh Balfour <josh@joshbalfour.co.uk>
2022-10-10 15:37:10 +0100

    lint

--- packages/app-builder-backend/src/utils/fetch.ts
+++ packages/app-builder-backend/src/utils/fetch.ts
@@ -1,7 +1,7 @@
 // workaround cos node-fetch is a cjs module
 
 // eslint-disable-next-line no-new-func
-const importDynamic = new Function('modulePath', 'return import(modulePath)');
+const importDynamic = new Function('modulePath', 'return import(modulePath)')
 
 export const fetch = async (url: RequestInfo, init?: RequestInit | undefined) => {
   const module = await importDynamic('node-fetch')
