a77a144a20a81dd6dccfbc3e8c0065c1fedd8b37
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-10-12 15:13:23 +0100

    fix: added clientId for issue with creation (no longer used unless a previous app

--- packages/deployment-service/src/pipeline/pipeline-controller.ts
+++ packages/deployment-service/src/pipeline/pipeline-controller.ts
@@ -66,7 +66,8 @@ export class PipelineController {
       ...previousPipeline,
       ...dto,
       developerId: creds.developerId as string,
-      // clientId: creds.clientId,
+      // required for current DB but currently we don't need this value
+      clientId: creds.type === 'jwt' ? creds.clientId as string : '',
       buildStatus: previousPipeline ? 'READY_FOR_DEPLOYMENT' : undefined,
       // Temp plug, singular appId/clientId for pipeline - later requires multiple pipelines
       id: dto.appId,
