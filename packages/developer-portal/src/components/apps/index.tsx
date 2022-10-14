3b5c159752d26621aec467f919f4539b125ad51b
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-10-14 14:48:42 +0100

    fix: corrected content-type for auth with pusher-js (#7920)
    Co-authored-by: Will McVay <wmcvay@reapit.com>

--- packages/developer-portal/src/components/apps/index.tsx
+++ packages/developer-portal/src/components/apps/index.tsx
@@ -1,7 +1,7 @@
 import { PusherProvider } from '@harelpls/use-pusher'
 import { useReapitConnect } from '@reapit/connect-session'
 import React, { FC } from 'react'
-import { COGNITO_HEADERS, URLS } from '../../constants/api'
+import { URLS } from '../../constants/api'
 import { reapitConnectBrowserSession } from '../../core/connect-session'
 import { AppsPage } from './page'
 import { AppProvider } from './state/use-app-state'
@@ -18,7 +18,6 @@ export const Apps: FC = () => {
       authEndpoint={`${URLS.DEPLOYMENT_SERVICE_HOST}pusher/auth`}
       auth={{
         headers: {
-          ...COGNITO_HEADERS,
           Authorization: connectSession.idToken,
         },
       }}
