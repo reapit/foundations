ec28656fdc9652fe50017311d55c88c5a1c7fb9f
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-10-20 13:26:31 +0100

    feat: added download button for installations csv

---
+++ packages/developer-portal/src/components/apps/installations/download-installations-csv.tsx
@@ -0,0 +1,91 @@
+import React, { FC, useEffect, useState } from 'react'
+import { AppSummaryModelPagedResult, InstallationModelPagedResult } from '@reapit/foundations-ts-definitions'
+import { GetActionNames, getActions } from '@reapit/utils-common'
+import { useReapitGet } from '@reapit/utils-react'
+import FileSaver from 'file-saver'
+import Papa from 'papaparse'
+import { reapitConnectBrowserSession } from '../../../core/connect-session'
+import { Button, elMb3 } from '@reapit/elements'
+
+export const DownloadInstallationsCSV: FC<{
+  appId: string
+  developerId: string
+}> = ({ appId, developerId }) => {
+  const [download, setDownload] = useState<boolean>(false)
+
+  const [installations] = useReapitGet<InstallationModelPagedResult>({
+    reapitConnectBrowserSession,
+    action: getActions(window.reapit.config.appEnv)[GetActionNames.getInstallations],
+    queryParams: {
+      appId,
+      isInstalled: true,
+      developerId,
+      pageSize: 999,
+    },
+    fetchWhenTrue: [download],
+  })
+
+  const appIds = new Set(installations?.data?.map((installation) => installation.appId).filter(Boolean) ?? [])
+  const appIdArray = [...appIds]
+
+  const [apps] = useReapitGet<AppSummaryModelPagedResult>({
+    reapitConnectBrowserSession,
+    action: getActions(window.reapit.config.appEnv)[GetActionNames.getApps],
+    queryParams: {
+      id: appIdArray,
+      pageSize: 999,
+    },
+    fetchWhenTrue: [appIdArray.length],
+  })
+
+  useEffect(() => {
+    if (apps && installations && apps.data && installations.data) {
+      const installationWithAppNames = {
+        ...installations,
+        data: installations.data.map((installation) => {
+          const appName = apps.data?.find((app) => app.id === installation.appId)?.name ?? ''
+
+          return {
+            ...installation,
+            appName,
+          }
+        }),
+      }
+
+      const csv = Papa.unparse({
+        fields: [
+          'App Name',
+          'Client',
+          'Customer Name',
+          'Created On',
+          'Uninstalled On',
+          'Status',
+          'Installed By',
+          'Uninstalled By',
+        ],
+        data: installationWithAppNames.data.map(
+          ({ appName, client, customerName, created, terminatesOn, status, installedBy, uninstalledBy }) => [
+            appName,
+            client,
+            customerName,
+            created,
+            terminatesOn,
+            status,
+            installedBy,
+            uninstalledBy,
+          ],
+        ),
+      })
+
+      const blob = new Blob([csv], { type: 'data:text/csv;charset=utf-8,' })
+      FileSaver.saveAs(blob, 'installs.csv')
+      if (download && apps && installations) setDownload(false)
+    }
+  }, [apps, installations, download])
+
+  return (
+    <Button className={elMb3} intent="primary" loading={download} disabled={download} onClick={() => setDownload(true)}>
+      Download CSV
+    </Button>
+  )
+}
