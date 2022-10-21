f01836e283ecd1cc387e1f7265041476e532d28f
Will McVay <wmcvay@reapit.com>
2022-10-21 11:11:00 +0100

    feat: #7803 remove aml checklist

--- packages/aml-checklist/src/actions/identity-types.ts
+++
@@ -1,7 +0,0 @@
-import { actionCreator } from '../utils/actions'
-import ActionTypes from '../constants/action-types'
-import { ListItemModel } from '@reapit/foundations-ts-definitions'
-
-export const identityTypesRequestData = actionCreator<void>(ActionTypes.IDENTITY_TYPES_REQUEST_DATA)
-export const identityTypesReceiveData = actionCreator<ListItemModel[]>(ActionTypes.IDENTITY_TYPES_RECEIVE_DATA)
-export const identityTypesRequestFailure = actionCreator<void>(ActionTypes.IDENTITY_TYPES_REQUEST_FAILURE)
