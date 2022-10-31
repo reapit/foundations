0e67d35e46aaf6a3653aedf49c9593a768cdeeeb
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-10-31 16:05:46 +0000

    feat: added creds cors options to v1

--- packages/graphql-server/src/index.ts
+++ packages/graphql-server/src/index.ts
@@ -3,4 +3,13 @@ import { config } from './graphql-common'
 
 const server = new ApolloServer(config)
 
-export const graphqlHandler = server.createHandler()
+export const graphqlHandler = server.createHandler({
+  expressGetMiddlewareOptions: {
+    cors: {
+      credentials: true,
+      origin: (requestOptions, callback) => {
+        callback(null, requestOptions)
+      },
+    },
+  },
+})
