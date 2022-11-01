ade876ffe4bdab6843de4507eab58a7066c3179c
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-11-01 11:23:40 +0000

    revert: remove cors from v1

--- packages/graphql-server/src/index.ts
+++ packages/graphql-server/src/index.ts
@@ -3,13 +3,4 @@ import { config } from './graphql-common'
 
 const server = new ApolloServer(config)
 
-export const graphqlHandler = server.createHandler({
-  expressGetMiddlewareOptions: {
-    cors: {
-      credentials: true,
-      origin: (requestOptions, callback) => {
-        callback(null, requestOptions)
-      },
-    },
-  },
-})
+export const graphqlHandler = server.createHandler()
