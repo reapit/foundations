1121c0ae1b8d97be4a2927f5a33709c3f0cbf155
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-11-02 13:23:38 +0000

    feat: added platform authorizer to graphql v2

--- packages/graphql-server-v2/cdk-stack.ts
+++ packages/graphql-server-v2/cdk-stack.ts
@@ -2,8 +2,9 @@
 import 'source-map-support/register'
 import path from 'path'
 import { execSync } from 'child_process'
-import { createApi, createFunction, createBaseStack, output, addLambdaToApi } from '@reapit/ts-scripts/src/cdk'
+import { createApi, createFunction, createBaseStack, output } from '@reapit/ts-scripts/src/cdk'
 import config from './config.json'
+import { aws_apigateway, aws_lambda } from 'aws-cdk-lib'
 
 const namespace = 'cloud'
 const appName = 'foundations'
@@ -37,7 +38,26 @@ const createStack = () => {
 
   const lambdaFunction = createFunction(stack, 'graphql', entrypoint, handler, config)
   const api = createApi(stack, 'api')
-  addLambdaToApi(stack, api, lambdaFunction, { path: '/{proxy+}', method: 'ANY' }, config.COGNITO_USERPOOL_ID)
+
+  const lambdaInt = new aws_apigateway.LambdaIntegration(lambdaFunction)
+  const resource = api.root.resourceForPath('')
+  const authorizerLambda = aws_lambda.Function.fromFunctionArn(stack, 'reapit-cloud-platform-lookup', 'arn:aws:lambda:eu-west-2:028446965111:function:reapit-lambdas-gateway-authorizer')
+
+  const authorizer = new aws_apigateway.RequestAuthorizer(stack, 'reapit-cloud-platform-authorizer', {
+    identitySources: [
+      aws_apigateway.IdentitySource.header('Authorization'),
+    ],
+    handler: authorizerLambda,
+  })
+
+  resource.addProxy({
+    defaultIntegration: lambdaInt,
+    defaultMethodOptions: {
+      authorizer,
+    },
+  })
+
+  // addLambdaToApi(stack, api, lambdaFunction, { path: '/{proxy+}', method: 'ANY' }, config.COGNITO_USERPOOL_ID)
   output(stack, 'api-url', api.url)
 }
 
