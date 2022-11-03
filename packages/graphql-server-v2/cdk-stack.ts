9e8bb1641931e7a473e272d8f187ba18b63a7c5b
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-11-03 15:09:56 +0000

    feat: changed gateway to platform with platform authorizer

--- packages/graphql-server-v2/cdk-stack.ts
+++ packages/graphql-server-v2/cdk-stack.ts
@@ -2,9 +2,9 @@
 import 'source-map-support/register'
 import path from 'path'
 import { execSync } from 'child_process'
-import { createApi, createFunction, createBaseStack, output } from '@reapit/ts-scripts/src/cdk'
+import { createFunction, createBaseStack } from '@reapit/ts-scripts/src/cdk'
 import config from './config.json'
-import { aws_apigateway, aws_lambda } from 'aws-cdk-lib'
+import { aws_apigateway } from 'aws-cdk-lib'
 
 const namespace = 'cloud'
 const appName = 'foundations'
@@ -37,28 +37,23 @@ const createStack = () => {
   const entrypoint = path.resolve(__dirname, 'bundle.zip')
 
   const lambdaFunction = createFunction(stack, 'graphql', entrypoint, handler, config)
-  const api = createApi(stack, 'api')
 
-  const lambdaInt = new aws_apigateway.LambdaIntegration(lambdaFunction)
-  const resource = api.root.resourceForPath('')
-  const authorizerLambda = aws_lambda.Function.fromFunctionArn(stack, 'reapit-cloud-platform-lookup', 'arn:aws:lambda:eu-west-2:028446965111:function:reapit-lambdas-gateway-authorizer')
-
-  const authorizer = new aws_apigateway.RequestAuthorizer(stack, 'reapit-cloud-platform-authorizer', {
-    identitySources: [
-      aws_apigateway.IdentitySource.header('Authorization'),
-    ],
-    handler: authorizerLambda,
+  const api = aws_apigateway.RestApi.fromRestApiAttributes(stack, 'existing-api-gateway-lookup', {
+    restApiId: config.REST_API_ID,
+    rootResourceId: config.ROOT_RESOURCE_ID,
   })
 
-  resource.addProxy({
-    defaultIntegration: lambdaInt,
-    defaultMethodOptions: {
-      authorizer,
+  const lambdaInt = new aws_apigateway.LambdaIntegration(lambdaFunction)
+  const resource = api.root.resourceForPath('graphql')
+
+  resource.addMethod('ANY', lambdaInt, {
+    authorizer: {
+      authorizationType: aws_apigateway.AuthorizationType.CUSTOM,
+      authorizerId: '2h4msu',
     },
   })
 
-  // addLambdaToApi(stack, api, lambdaFunction, { path: '/{proxy+}', method: 'ANY' }, config.COGNITO_USERPOOL_ID)
-  output(stack, 'api-url', api.url)
+  resource.addMethod('OPTIONS', lambdaInt)
 }
 
 createStack()
