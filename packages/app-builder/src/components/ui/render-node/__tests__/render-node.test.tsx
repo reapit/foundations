0c34308c90047b8d04d3da4cefa0d4eb70c5aec8
Josh Balfour <josh@joshbalfour.co.uk>
2022-11-03 16:36:59 +0000

    fix tests

--- packages/app-builder/src/components/ui/render-node/__tests__/render-node.test.tsx
+++ packages/app-builder/src/components/ui/render-node/__tests__/render-node.test.tsx
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react'
 import { RenderNode } from '../index'
 import { MockedProvider } from '@apollo/client/testing'
 
+import routeData from 'react-router'
 jest.mock('@craftjs/core', () => {
   return {
     useEditor: () => ({}),
@@ -14,6 +15,20 @@ jest.mock('@craftjs/core', () => {
 })
 
 describe('RenderNode', () => {
+  const mockParams = {
+    appId: '123',
+    pageId: '456',
+  }
+  const mockLocation = {
+    pathname: '/123/456',
+    hash: '',
+    search: '',
+    state: '',
+  }
+  beforeEach(() => {
+    jest.spyOn(routeData, 'useParams').mockReturnValue(mockParams)
+    jest.spyOn(routeData, 'useLocation').mockReturnValue(mockLocation)
+  })
   it('should match a snapshot', () => {
     const { asFragment } = render(
       <MockedProvider>
