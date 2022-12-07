b34455d075c2591463f2efec7431e999d978885d
Will McVay <wmcvay@reapit.com>
2022-12-07 16:52:53 +0000

    feat: #8187 add test cases

--- packages/marketplace-management/src/components/ui/apps/__tests__/app-referrals-display.test.tsx
+++ packages/marketplace-management/src/components/ui/apps/__tests__/app-referrals-display.test.tsx
@@ -3,6 +3,7 @@ import { ReferralsDisplay, handleReferralText } from '../app-referrals-display'
 import { render } from '../../../../tests/react-testing'
 import { mockReferralTypes } from '../../../../services/__stubs__/referral-types'
 import { mockInstallationsList } from '../../../../services/__stubs__/installations'
+import { InstallationModel } from '@reapit/foundations-ts-definitions'
 
 jest.mock('@reapit/utils-react', () => ({
   useReapitGet: jest.fn(() => [mockReferralTypes]),
@@ -21,5 +22,42 @@ describe('handleReferralText', () => {
     const curried = handleReferralText(mockInstallationsList.data ?? [], mockReferralTypes)
     const result = curried()
     expect(result).toEqual('Testimonial, Removals')
+  })
+
+  it('handleReferralText should return an empty string if installations are empty', () => {
+    const curried = handleReferralText([], mockReferralTypes)
+    const result = curried()
+    expect(result).toEqual('')
+  })
+
+  it('handleReferralText should return an empty string if ReferralTypes are empty', () => {
+    const curried = handleReferralText(mockInstallationsList.data ?? [], {})
+    const result = curried()
+    expect(result).toEqual('')
+  })
+
+  it('handleReferralText should return an empty string if both are empty', () => {
+    const curried = handleReferralText([], {})
+    const result = curried()
+    expect(result).toEqual('')
+  })
+
+  it('handleReferralText should return an empty string if a name is not found', () => {
+    const curried = handleReferralText(mockInstallationsList.data ?? [], {
+      ...mockReferralTypes,
+      _embedded: [{ id: 'NOT_KNOWN', name: 'FOO' }],
+    })
+    const result = curried()
+    expect(result).toEqual('')
+  })
+
+  it('handleReferralText should return an empty string if no active installations', () => {
+    const installationInactiveWithMeta = {
+      ...(mockInstallationsList.data as InstallationModel[])[0],
+      status: 'NOT ACTIVE',
+    }
+    const curried = handleReferralText([installationInactiveWithMeta], mockReferralTypes)
+    const result = curried()
+    expect(result).toEqual('')
   })
 })
