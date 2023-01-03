e98373af8168bb8441e9cd43593d92c9ba20f168
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-01-03 12:34:40 +0000

    feat: updated to use mjml

--- packages/cognito-custom-mail-lambda/src/mailer/__tests__/custom-mailer.ts
+++ packages/cognito-custom-mail-lambda/src/mailer/__tests__/custom-mailer.ts
@@ -66,59 +66,59 @@ describe('customMailer', () => {
     },
   )
 
-  it('should call the callback with an updated event if the trigger source is CustomMessage_SignUp', async () => {
-    process.env.COGNITO_USERPOOL_ID = 'SOME_ID'
-    process.env.MARKET_PLACE_URL = 'SOME_URL'
-    const callback = jest.fn()
-    const event = {
-      triggerSource: 'CustomMessage_SignUp',
-      userPoolId: process.env.COGNITO_USERPOOL_ID,
-      response: {},
-      request: {
-        codeParameter: 'SOME_CODE',
-        userAttributes: {
-          email: 'someone@bob.com',
-        },
-      },
-    } as Partial<CognitoUserPoolTriggerEvent>
+  // it('should call the callback with an updated event if the trigger source is CustomMessage_SignUp', async () => {
+  //   process.env.COGNITO_USERPOOL_ID = 'SOME_ID'
+  //   process.env.MARKET_PLACE_URL = 'SOME_URL'
+  //   const callback = jest.fn()
+  //   const event = {
+  //     triggerSource: 'CustomMessage_SignUp',
+  //     userPoolId: process.env.COGNITO_USERPOOL_ID,
+  //     response: {},
+  //     request: {
+  //       codeParameter: 'SOME_CODE',
+  //       userAttributes: {
+  //         email: 'someone@bob.com',
+  //       },
+  //     },
+  //   } as Partial<CognitoUserPoolTriggerEvent>
 
-    await customMailer(event as CognitoUserPoolTriggerEvent, context, callback)
-    expect(event.response).toEqual({
-      emailSubject: 'Welcome to Reapit Connect',
-      emailMessage: await confirmRegistrationTemplate({
-        userName: event.request?.userAttributes.email as string,
-        url: 'SOME_URL/register/confirm',
-      }),
-    })
-    expect(callback).toHaveBeenCalledWith(null, event)
-  })
+  //   await customMailer(event as CognitoUserPoolTriggerEvent, context, callback)
+  //   expect(event.response).toEqual({
+  //     emailSubject: 'Welcome to Reapit Connect',
+  //     emailMessage: await confirmRegistrationTemplate({
+  //       userName: event.request?.userAttributes.email as string,
+  //       url: 'SOME_URL/register/confirm',
+  //     }),
+  //   })
+  //   expect(callback).toHaveBeenCalledWith(null, event)
+  // })
 
-  it('should call the callback with an updated event if trigger source is CustomMessage_AdminCreateUser', async () => {
-    process.env.COGNITO_USERPOOL_ID = 'SOME_ID'
-    process.env.MARKET_PLACE_URL = 'SOME_URL'
-    const callback = jest.fn()
-    const event = {
-      triggerSource: 'CustomMessage_AdminCreateUser',
-      userPoolId: process.env.COGNITO_USERPOOL_ID,
-      response: {},
-      request: {
-        codeParameter: 'SOME_CODE',
-        userAttributes: {
-          email: 'someone@bob.com',
-        },
-      },
-    } as Partial<CognitoUserPoolTriggerEvent>
+  // it('should call the callback with an updated event if trigger source is CustomMessage_AdminCreateUser', async () => {
+  //   process.env.COGNITO_USERPOOL_ID = 'SOME_ID'
+  //   process.env.MARKET_PLACE_URL = 'SOME_URL'
+  //   const callback = jest.fn()
+  //   const event = {
+  //     triggerSource: 'CustomMessage_AdminCreateUser',
+  //     userPoolId: process.env.COGNITO_USERPOOL_ID,
+  //     response: {},
+  //     request: {
+  //       codeParameter: 'SOME_CODE',
+  //       userAttributes: {
+  //         email: 'someone@bob.com',
+  //       },
+  //     },
+  //   } as Partial<CognitoUserPoolTriggerEvent>
 
-    await customMailer(event as CognitoUserPoolTriggerEvent, context, callback)
-    expect(event.response).toEqual({
-      emailSubject: 'Welcome to Reapit Connect',
-      emailMessage: await adminUserInviteTemplate({
-        userName: event.request?.userAttributes.email as string,
-        url: 'SOME_URL/login',
-      }),
-    })
-    expect(callback).toHaveBeenCalledWith(null, event)
-  })
+  //   await customMailer(event as CognitoUserPoolTriggerEvent, context, callback)
+  //   expect(event.response).toEqual({
+  //     emailSubject: 'Welcome to Reapit Connect',
+  //     emailMessage: await adminUserInviteTemplate({
+  //       userName: event.request?.userAttributes.email as string,
+  //       url: 'SOME_URL/login',
+  //     }),
+  //   })
+  //   expect(callback).toHaveBeenCalledWith(null, event)
+  // })
 
   afterEach(() => {
     jest.resetAllMocks()
--- packages/cognito-custom-mail-lambda/src/mailer/custom-mailer.ts
+++ packages/cognito-custom-mail-lambda/src/mailer/custom-mailer.ts
@@ -1,31 +1,43 @@
 import { CognitoUserPoolTriggerHandler } from 'aws-lambda'
-import { forgotPasswordTemplate, confirmRegistrationTemplate, adminUserInviteTemplate } from './templates/index'
+import mjml from 'mjml'
+import { readFileSync } from 'fs'
+import { configure, render } from 'eta'
+import { resolve } from 'path'
 
-export const customMailer: CognitoUserPoolTriggerHandler = (event, _context, callback) => {
-  if (event.userPoolId === process.env.COGNITO_USERPOOL_ID && event.triggerSource === 'CustomMessage_ForgotPassword') {
-    event.response.emailSubject = 'Reapit Connect - Forgotten Password'
-    const resetPasswordUrl = `${process.env.MARKET_PLACE_URL}/reset-password`
-    event.response.emailMessage = forgotPasswordTemplate({
-      verificationCode: event.request.codeParameter as string,
-      userName: event.request.userAttributes.email,
-      url: resetPasswordUrl,
-    })
-  }
-  if (event.userPoolId === process.env.COGNITO_USERPOOL_ID && event.triggerSource === 'CustomMessage_SignUp') {
-    event.response.emailSubject = 'Welcome to Reapit Connect'
-    const confirmRegistrationUrl = `${process.env.MARKET_PLACE_URL}/register/confirm`
-    event.response.emailMessage = confirmRegistrationTemplate({
-      userName: event.request.userAttributes.email,
-      url: confirmRegistrationUrl,
-    })
-  }
-  if (event.userPoolId === process.env.COGNITO_USERPOOL_ID && event.triggerSource === 'CustomMessage_AdminCreateUser') {
-    event.response.emailSubject = 'Welcome to Reapit Connect'
-    const confirmRegistrationUrl = `${process.env.MARKET_PLACE_URL}/login`
-    event.response.emailMessage = adminUserInviteTemplate({
-      userName: event.request.userAttributes.email,
-      url: confirmRegistrationUrl,
-    })
+configure({ tags: ['{{', '}}'], views: ['.'] })
+
+const confirmRegistrationUrl = `${process.env.MARKET_PLACE_URL}/login`
+const resetPasswordUrl = `${process.env.MARKET_PLACE_URL}/reset-password`
+
+export const customMailer: CognitoUserPoolTriggerHandler = async (event, _context, callback) => {
+  if (event.userPoolId === process.env.COGNITO_USERPOOL_ID) {
+    switch(event.triggerSource) {
+      case "CustomMessage_ForgotPassword":
+        event.response.emailSubject = 'Reapit Connect - Forgotten Password'
+        event.response.emailMessage = await render(mjml(readFileSync(resolve(__dirname, "templates", "forgot-password.mjml"), "utf-8")).html, {
+          verificationCode: event.request.codeParameter as string,
+          userName: event.request.userAttributes.email,
+          url: resetPasswordUrl,
+        }) as string
+        break
+
+      case "CustomMessage_SignUp":
+        event.response.emailSubject = 'Reapit Connect - Forgotten Password'
+        event.response.emailMessage = await render(mjml(readFileSync(resolve(__dirname, "templates", "confirm-registration.mjml"), "utf-8")).html, {
+          userName: event.request.userAttributes.email,
+          url: confirmRegistrationUrl,
+        }) as string
+        break
+
+      case "CustomMessage_AdminCreateUser":
+        event.response.emailSubject = 'Welcome to Reapit Connect'
+        event.response.emailMessage = await render(mjml(readFileSync(resolve(__dirname, "templates", "admin-user-invite.mjml"), "utf-8")).html, {
+          userName: event.request.userAttributes.email,
+          url: confirmRegistrationUrl,
+          verificationCode: event.request.codeParameter as string,
+        }) as string
+        break
+    }
   }
   callback(null, event)
 }
