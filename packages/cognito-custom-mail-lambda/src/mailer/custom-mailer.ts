67baf4a31a858295f2f056c45f6efc81df3615af
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-01-03 13:00:15 +0000

    refactor: converted to string-template method

--- packages/cognito-custom-mail-lambda/src/mailer/custom-mailer.ts
+++ packages/cognito-custom-mail-lambda/src/mailer/custom-mailer.ts
@@ -1,41 +1,48 @@
 import { CognitoUserPoolTriggerHandler } from 'aws-lambda'
 import mjml from 'mjml'
 import { readFileSync } from 'fs'
-import { configure, render } from 'eta'
 import { resolve } from 'path'
-
-configure({ tags: ['{{', '}}'], views: ['.'] })
+import format from 'string-template'
 
 const confirmRegistrationUrl = `${process.env.MARKET_PLACE_URL}/login`
 const resetPasswordUrl = `${process.env.MARKET_PLACE_URL}/reset-password`
 
 export const customMailer: CognitoUserPoolTriggerHandler = async (event, _context, callback) => {
   if (event.userPoolId === process.env.COGNITO_USERPOOL_ID) {
-    switch(event.triggerSource) {
-      case "CustomMessage_ForgotPassword":
+    switch (event.triggerSource) {
+      case 'CustomMessage_ForgotPassword':
         event.response.emailSubject = 'Reapit Connect - Forgotten Password'
-        event.response.emailMessage = await render(mjml(readFileSync(resolve(__dirname, "templates", "forgot-password.mjml"), "utf-8")).html, {
-          verificationCode: event.request.codeParameter as string,
-          userName: event.request.userAttributes.email,
-          url: resetPasswordUrl,
-        }) as string
+        event.response.emailMessage = format(
+          mjml(readFileSync(resolve(__dirname, 'templates', 'forgot-password.mjml'), 'utf-8')).html,
+          {
+            verificationCode: event.request.codeParameter as string,
+            userName: event.request.userAttributes.email,
+            url: resetPasswordUrl,
+          },
+        )
         break
 
-      case "CustomMessage_SignUp":
+      case 'CustomMessage_SignUp':
         event.response.emailSubject = 'Reapit Connect - Forgotten Password'
-        event.response.emailMessage = await render(mjml(readFileSync(resolve(__dirname, "templates", "confirm-registration.mjml"), "utf-8")).html, {
-          userName: event.request.userAttributes.email,
-          url: confirmRegistrationUrl,
-        }) as string
+        event.response.emailMessage = format(
+          mjml(readFileSync(resolve(__dirname, 'templates', 'confirm-registration.mjml'), 'utf-8')).html,
+          {
+            userName: event.request.userAttributes.email,
+            url: confirmRegistrationUrl,
+          },
+        )
         break
 
-      case "CustomMessage_AdminCreateUser":
+      case 'CustomMessage_AdminCreateUser':
         event.response.emailSubject = 'Welcome to Reapit Connect'
-        event.response.emailMessage = await render(mjml(readFileSync(resolve(__dirname, "templates", "admin-user-invite.mjml"), "utf-8")).html, {
-          userName: event.request.userAttributes.email,
-          url: confirmRegistrationUrl,
-          verificationCode: event.request.codeParameter as string,
-        }) as string
+        event.response.emailMessage = format(
+          mjml(readFileSync(resolve(__dirname, 'templates', 'admin-user-invite.mjml'), 'utf-8')).html,
+          {
+            userName: event.request.userAttributes.email,
+            url: confirmRegistrationUrl,
+            verificationCode: event.request.codeParameter as string,
+          },
+        )
         break
     }
   }
