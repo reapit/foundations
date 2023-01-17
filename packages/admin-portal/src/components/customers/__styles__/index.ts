9cebdc918489b2d1f0d151c5fe034653ac3bb141
Will McVay <wmcvay@reapit.com>
2023-01-17 12:03:09 +0000

    feat: #8325 #8317 admin portal updates (#8334)

---
+++ packages/admin-portal/src/components/customers/__styles__/index.ts
@@ -0,0 +1,5 @@
+import { css } from '@linaria/core'
+
+export const fourColTable = css`
+  grid-template-columns: repeat(4, 1fr);
+`
---
+++ packages/admin-portal/src/tests/__stubs__/office-groups.ts
@@ -0,0 +1,21 @@
+import { OfficeGroupModelPagedResult } from '@reapit/foundations-ts-definitions'
+
+export const mockOfficeGroupModelPagedResult: OfficeGroupModelPagedResult = {
+  _embedded: [
+    {
+      id: '12ce73ac-76b6-45ec-8b92-a3d1a10c2ffc',
+      created: '2021-04-29T07:03:21.0000000Z',
+      organisationId: '201a8d35-f682-41a8-95ed-079133e4b517',
+      customerId: 'RES-GGTN',
+      name: 'Group 10',
+      tag: 'GGTN',
+      officeIds: 'AAE,CSL,AAS',
+      status: 'inactive',
+    },
+  ],
+  pageNumber: 1,
+  pageSize: 12,
+  pageCount: 1,
+  totalPageCount: 1,
+  totalCount: 1,
+}
