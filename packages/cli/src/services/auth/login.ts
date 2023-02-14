81748aad694add7ac3a1021876778e7d846011e1
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-02-14 16:28:47 +0000

    refactor: added static clientId and port setup for individual client setup

--- packages/cli/src/services/auth/login.ts
+++ packages/cli/src/services/auth/login.ts
@@ -13,25 +13,40 @@ import { writeFileSync, promises, existsSync } from 'fs'
 import 'isomorphic-fetch'
 import decode from 'jwt-decode'
 import ora from 'ora'
+import portastic from 'portastic'
 
 export class LoginService {
   private server
   private readonly connectOAuthUrl: string = 'https://connect.dev.paas.reapit.cloud' // TODO Change dev to env
-  private readonly port: number = 8080
+  private readonly clientId: string = '2c8jt1gp2hb0tcc8s88m8lig3d'
+  private readonly ports: number[] = [9000, 9001, 9002, 9003, 9004]
+  protected currentPort?: number
   private readonly connectUrl: string = `${this.connectOAuthUrl}/login`
-  private readonly redirect = `http://localhost:${this.port}`
+  private readonly redirect = `http://localhost:${this.currentPort}`
   protected connectSession?: ReapitConnectSession
 
   static storageLocation: string = `${homeDir()}/.reapit-connect.json`
 
-  private oauthUrl = (clientId: string, authCode: string) =>
-    `${this.connectOAuthUrl}/token?grant_type=authorization_code&client_id=${clientId}&code=${authCode}&redirect_uri=${this.redirect}`
+  private oauthUrl = (authCode: string) =>
+    `${this.connectOAuthUrl}/token?grant_type=authorization_code&client_id=${this.clientId}&code=${authCode}&redirect_uri=${this.redirect}`
 
-  private refreshUrl = (clientId: string, refreshToken: string) =>
-    `${this.connectOAuthUrl}/token?grant_type=refresh_token&client_id=${clientId}&refresh_token=${refreshToken}&redirect_uri=${this.redirect}`
+  private refreshUrl = (refreshToken: string) =>
+    `${this.connectOAuthUrl}/token?grant_type=refresh_token&client_id=${this.clientId}&refresh_token=${refreshToken}&redirect_uri=${this.redirect}`
 
-  protected async obtainAuthCode(clientId: string): Promise<string> {
+  protected async obtainAuthCode(): Promise<string | never> {
     const spinner = ora('Awaiting Response from Reapit Connect...')
+    this.currentPort = await portastic.find({
+      min: this.ports[0],
+      max: this.ports[this.ports.length - 1],
+    })[0]
+
+    if (!this.currentPort) {
+      throw new Error(
+        `Cannot find open port, Configurable ports are [${this.ports.join(
+          ', ',
+        )}] please make sure one of these ports are available`,
+      )
+    }
 
     return new Promise((resolve) => {
       this.server = http.createServer((request, response) => {
@@ -54,17 +69,22 @@ export class LoginService {
         resolve(code as string)
       })
 
-      this.server.listen(this.port, 'localhost', () => {
+      this.server.listen(this.currentPort, 'localhost', () => {
         console.log('Awaiting response from reapit-connect.')
         console.log('Please login to reapit-connect to continue.')
         console.log('')
         spinner.start()
       })
 
-      open(`${this.connectUrl}?response_type=code&client_id=${clientId}&redirect_uri=${this.redirect}`)
+      open(`${this.connectUrl}?response_type=code&client_id=${this.clientId}&redirect_uri=${this.redirect}`)
     })
   }
 
+  /**
+   * create a reapitConnectSession and set stored session from cognito session
+   *
+   * @param cognitoSession
+   */
   private async createReapitSessionFronCognito(cognitoSession: CoginitoSession): Promise<void> {
     const loginIdentity: LoginIdentity | undefined = await connectSessionVerifyDecodeIdToken(cognitoSession.id_token)
 
@@ -81,12 +101,11 @@ export class LoginService {
 
   /**
    * To initiate the login process of obtaining the session for the the CLI
-   * @param clientId the app's clientId
    */
-  protected async obtainSessionFromReapitConnect(clientId: string): Promise<void> {
-    const authCode = await this.obtainAuthCode(clientId)
+  protected async obtainSessionFromReapitConnect(): Promise<void> {
+    const authCode = await this.obtainAuthCode()
 
-    const response = await fetch(this.oauthUrl(clientId, authCode), {
+    const response = await fetch(this.oauthUrl(authCode), {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -101,8 +120,12 @@ export class LoginService {
     await this.storeCurrentSession()
   }
 
-  protected async refreshSession(clientId: string, refreshToken: string): Promise<void> {
-    const response = await fetch(this.refreshUrl(clientId, refreshToken), {
+  /**
+   * Get a session using a refresh token
+   * @param refreshToken
+   */
+  protected async refreshSession(refreshToken: string): Promise<void> {
+    const response = await fetch(this.refreshUrl(refreshToken), {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -128,6 +151,9 @@ export class LoginService {
     })
   }
 
+  /**
+   * Is the current session expired
+   */
   private get sessionExpired() {
     if (this.connectSession) {
       const decoded = decode<DecodedToken<CoginitoAccess>>(this.connectSession.accessToken)
@@ -157,17 +183,16 @@ export class LoginService {
 
   /**
    * Obtain a ReapitConnectSession by logging in via the browser or using a refresh token with a stored session
-   * @param clientId string - clientId of a reapit app
    *
    * @returns ReapitConnectSession
    */
-  public async getSession(clientId: string): Promise<ReapitConnectSession | never> {
+  public async getSession(): Promise<ReapitConnectSession | never> {
     const storedSession = await this.getStoredSession()
 
-    if (!storedSession) await this.obtainSessionFromReapitConnect(clientId)
+    if (!storedSession) await this.obtainSessionFromReapitConnect()
 
     if (this.sessionExpired && this.connectSession) {
-      await this.refreshSession(clientId, this.connectSession.refreshToken)
+      await this.refreshSession(this.connectSession.refreshToken)
     }
 
     return this.connectSession as ReapitConnectSession
