7b679b5e9cbbf971567a7ef4cdfae60a34b2ca26
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-03-27 17:51:08 +0100

    fix: corrected implementation for api key invoke tests

--- packages/api-key-verify/jest.config.js
+++ packages/api-key-verify/jest.config.js
@@ -1,28 +1,19 @@
-const { jestNodeGlobalConfig } = require('@reapit/ts-scripts')
-const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('./tsconfig.json')
-
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  // ...jestNodeGlobalConfig,
-  // testPathIgnorePatterns: ['<rootDir>/src/tests/'],
-  // coveragePathIgnorePatterns: [
-  //   '<rootDir>[/\\\\](node_modules|src/types|src/tests|src/scripts|src/__stubs__|dist)|config|core|models[/\\\\]',
-  //   '.d.ts',
-  //   'index.ts',
-  //   '<rootDir>/src/api-key-model.ts',
-  // ],
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 79,
-  //     functions: 92,
-  //     lines: 89,
-  //     statements: 90,
-  //   },
-  // },
-  // moduleNameMapper: {
-  //   ...jestNodeGlobalConfig.moduleNameMapper,
-  //   ...pathsToModuleNameMapper(compilerOptions.paths, {
-  //     prefix: '<rootDir>/',
-  //   }),
-  // },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/dist'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: './src/tests/coverage',
+  coveragePathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|src/types|src/tests|src/scripts)[/\\\\]', '.d.ts'],
+  reporters: ['default', 'github-actions'],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 40,
+      lines: 60,
+      statements: 40,
+    },
+  },
+  coverageReporters: ['json-summary', 'text', 'lcov'],
 }
--- packages/api-key-verify/src/__tests__/api-key.test.ts
+++ packages/api-key-verify/src/__tests__/api-key.test.ts
@@ -1,45 +1,51 @@
 import { ApiKeyModel } from '../api-key-model'
 import { resolveApiKey } from '../api-key'
-import * as AWS from 'aws-sdk'
 
 const SUCCESS_API_KEY = 'success-api-key'
 const NOT_FOUND_API_KEY = 'not-found-api-key'
 const EXPIRED_API_KEY = 'expired-api-key'
 
-const lambda = new AWS.Lambda()
+jest.mock('aws-sdk', () => {
+  const mLambda = {
+    invoke: jest.fn((keyCondition: any, callBack) => new Promise((resolve) => {
+        const apiKeyHeader = JSON.parse(keyCondition.Payload).apiKey as string
 
-describe('ApiKey', () => {
-  beforeAll(() => {
-    // @ts-ignore
-    jest
-      .spyOn(lambda, 'invoke')
-      // @ts-ignore
-      .mockImplementation((_valueConstructor: any, keyCondition: any) => {
-        console.log('mock implementation')
-        const apiKeyHeader = (keyCondition as { [s: string]: any }).apiKey as string
-
-        const createApiKeyModel = (apiKey: string, keyExpiresAt: Date): ApiKeyModel => {
+        const createApiKeyModel = (apiKey: string, keyExpiresAt: Date): string => {
           const model = new ApiKeyModel()
           model.apiKey = apiKey
           model.keyExpiresAt = keyExpiresAt.toISOString()
 
-          return model
+          return JSON.stringify(model)
         }
 
         const expires = new Date()
         if (apiKeyHeader === SUCCESS_API_KEY) {
           expires.setDate(expires.getDate() + 3)
-          return createApiKeyModel(SUCCESS_API_KEY, expires)
+          resolve(callBack(undefined, {
+            Payload: createApiKeyModel(SUCCESS_API_KEY, expires),
+          }))
+          return
         } else if (apiKeyHeader === EXPIRED_API_KEY) {
           expires.setDate(expires.getDate() - 3)
-          return createApiKeyModel(EXPIRED_API_KEY, expires)
+          resolve(callBack(undefined, {
+            Payload: createApiKeyModel(EXPIRED_API_KEY, expires),
+          }))
+          return
         }
 
-        return {}
-      })
-  })
+        resolve(callBack(undefined, {
+          Payload: undefined,
+        }))
+        return
+    })),
+  }
+  return { Lambda: jest.fn(() => mLambda) }
+})
+
+describe('ApiKey', () => {
 
   describe('resolveApiKey', () => {
+    
     it('Can get apiKey', async () => {
       const result = await resolveApiKey({ apiKey: SUCCESS_API_KEY, functionName: '' })
 
@@ -48,16 +54,22 @@ describe('ApiKey', () => {
     })
 
     it('Can get expired apiKey', async () => {
-      const result = await resolveApiKey({ apiKey: EXPIRED_API_KEY, functionName: '' })
-
-      expect(result?.apiKey).toBe(EXPIRED_API_KEY)
-      expect(new Date(result?.keyExpiresAt as string) < new Date()).toBeTruthy()
+      
+      try {
+        await resolveApiKey({ apiKey: EXPIRED_API_KEY, functionName: '' })
+      } catch(e: any) {
+        expect(e.message).toBe('Api Key expired')
+      }
     })
 
     it('Returns undefined on no apiKey', async () => {
-      const result = await resolveApiKey({ apiKey: NOT_FOUND_API_KEY, functionName: '' })
+      try {
+        await resolveApiKey({ apiKey: NOT_FOUND_API_KEY, functionName: '' })
 
-      expect(typeof result).toBe('undefined')
+        expect(true).toBeFalsy()
+      } catch(e: any) {
+        expect(e.message).toBe('Api Key not found')
+      }
     })
   })
 
