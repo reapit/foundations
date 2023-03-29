900d468b07d1efe4b9b564ec20c89859ad617563
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-03-29 16:09:05 +0100

    feat: changed build method

---
+++ packages/utils-nest/tsup.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'tsup'
+import fs from 'fs'
+import { resolve } from 'path'
+
+const pkgJson = JSON.parse(fs.readFileSync(resolve(__dirname, 'package.json'), 'utf-8'))
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  target: 'node19',
+  clean: true,
+  esbuildOptions: (opts) => {
+    opts.resolveExtensions = ['.ts', '.js']
+  },
+  noExternal: Object.keys(pkgJson.dependencies),
+  // external: [
+  //   '@nestjs/microservices',
+  //   'cache-manager',
+  //   '@nestjs/websockets',
+  //   '@reapit/utils-nest',
+  // ],
+})
