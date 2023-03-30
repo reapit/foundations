b0fbcb3834763d08ec8ce0766c0590336470d03b
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-03-30 10:22:32 +0100

    fix: corrected api-key-verify jest config

--- packages/api-key-verify/jest.config.js
+++ packages/api-key-verify/jest.config.js
@@ -1,3 +1,6 @@
+const { compilerOptions } = require('./tsconfig')
+const { pathsToModuleNameMapper } = require('ts-jest')
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
@@ -16,4 +19,9 @@ module.exports = {
     },
   },
   coverageReporters: ['json-summary', 'text', 'lcov'],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>/',
+    }),
+  },
 }
