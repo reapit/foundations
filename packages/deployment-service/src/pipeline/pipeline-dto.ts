b3f397271dfe763724f1d1ec45a5f468dbc1d129
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-04-26 09:27:15 +0100

    fix: increased outdir validation length

--- packages/deployment-service/src/pipeline/pipeline-dto.ts
+++ packages/deployment-service/src/pipeline/pipeline-dto.ts
@@ -33,7 +33,7 @@ export class PipelineDto {
   packageManager: PackageManagerEnum = PackageManagerEnum.YARN
 
   @IsString()
-  @MaxLength(10)
+  @MaxLength(40)
   outDir: string = 'build'
 
   @IsString()
