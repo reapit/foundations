9dbd7c5a02ee28a18e7e70717cdc3ed5a6e8ece1
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-04-28 14:46:39 +0100

    fix: corrected package manager values in tests

--- packages/developer-portal/src/components/apps/pipeline/__tests__/pipeline-configure.test.tsx
+++ packages/developer-portal/src/components/apps/pipeline/__tests__/pipeline-configure.test.tsx
@@ -51,7 +51,7 @@ describe('handlePipelineUpdate', () => {
     expect(updatePipeline).toHaveBeenCalledWith({
       ...values,
       appId,
-      packageManager: 'yarn',
+      packageManager: 'npm',
       appType: 'react',
     })
   })
@@ -97,7 +97,7 @@ describe('getDefaultValues', () => {
       repository,
       buildCommand,
       outDir,
-      packageManager: packageManager === PackageManagerEnum.YARN,
+      packageManager,
     })
   })
 
@@ -109,7 +109,7 @@ describe('getDefaultValues', () => {
       buildCommand: 'build',
       branch: 'main',
       outDir: 'build',
-      packageManager: true,
+      packageManager: PackageManagerEnum.NPM,
     })
   })
 })
