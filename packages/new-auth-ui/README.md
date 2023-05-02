97119cffe3e308e3f103f46e4a4065dd1559431c
Will McVay <wmcvay@reapit.com>
2023-05-02 10:46:53 +0100

    feat: #7350 remove connect session next and new auth ui

--- packages/new-auth-ui/README.md
+++
@@ -1,5 +0,0 @@
-# Developer Portal
-
-![lines](./src/tests/badges/badge-lines.svg) ![functions](./src/tests/badges/badge-functions.svg) ![branches](./src/tests/badges/badge-branches.svg) ![statements](./src/tests/badges/badge-statements.svg)
-
-The core frontend of the Reapit Foundations platform. Contains all functionality in Developer Portal applications.
--- packages/new-auth-ui/src/core/__styles__/media.ts
+++
@@ -1,7 +0,0 @@
-export const forSmallMobileOnly = '@media (max-width: 414px)'
-export const forMobileOnly = '@media (max-width: 768px)'
-export const forTabletAndBelow = '@media (max-width: 1023px)'
-export const forTabletOnly = '@media (min-width: 769px) and (max-width: 1023px)'
-export const forTabletAndDesktop = '@media (min-width: 769px)'
-export const forDesktopOnly = '@media (min-width: 1024px)'
-export const forIEOnly = '@media all and (-ms-high-contrast:none)'
