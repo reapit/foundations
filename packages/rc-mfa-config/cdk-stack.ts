f69db048b52ce394c995b39f51703b6f09fae003
Will McVay <wmcvay@reapit.com>
2023-05-02 13:58:31 +0100

    feat: #7350 missing cdk config (#8971)

---
+++ packages/rc-mfa-config/cdk-stack.ts
@@ -0,0 +1,24 @@
+import { createBaseStack, createSite, getAccountId } from '@reapit/ts-scripts/src/cdk'
+import { join } from 'path'
+
+const createStack = async () => {
+  const accountId = await getAccountId()
+  const stack = createBaseStack({
+    namespace: 'cloud',
+    appName: 'reapit-mfa-config',
+    component: 'site',
+    accountId,
+    region: 'eu-west-2',
+  })
+
+  await createSite(stack, {
+    env: process.env.APP_STAGE === 'production' ? 'prod' : 'dev',
+    location: join(__dirname, 'build'),
+  })
+}
+
+const bootstrap = async () => {
+  await createStack()
+}
+
+bootstrap()
