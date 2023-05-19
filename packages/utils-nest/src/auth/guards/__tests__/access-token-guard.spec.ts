d53998e604b1671b018603b69edb14a33e183ce2
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-05-19 11:16:35 +0100

    feat: added additional fail tests

--- packages/utils-nest/src/auth/guards/__tests__/access-token-guard.spec.ts
+++ packages/utils-nest/src/auth/guards/__tests__/access-token-guard.spec.ts
@@ -48,4 +48,48 @@ describe('AccessTokenGuard', () => {
       expect(true).toBeFalsy() // fail if execptioned
     }
   })
+
+  it('Can fail if no token provided', async () => {
+    ;(accessTokenProvider.fetchIdentity as jest.Mock).mockImplementationOnce(() => true)
+
+    try {
+      const result = await guard.canActivate({
+        // @ts-ignore
+        switchToHttp: () => ({
+          // @ts-ignore
+          getRequest: () => {
+            return {
+              headers: {},
+            }
+          },
+        }),
+      })
+
+      expect(result).toBeFalsy() // Fail if pass, was not rejected
+    } catch (e) {
+      expect(true).toBeTruthy() // pass: want exception to be thrown
+    }
+  })
+
+  it('Can fail if token does not return user info', async () => {
+    ;(accessTokenProvider.fetchIdentity as jest.Mock).mockImplementationOnce(() => false)
+
+    try {
+      const result = await guard.canActivate({
+        // @ts-ignore
+        switchToHttp: () => ({
+          // @ts-ignore
+          getRequest: () => {
+            return {
+              headers: { authorization: 'Bearer some-access-token' },
+            }
+          },
+        }),
+      })
+
+      expect(result).toBeFalsy() // Fail if pass, was not rejected
+    } catch (e) {
+      expect(true).toBeTruthy() // pass: want exception to be thrown
+    }
+  })
 })
