e336dcca88362fc6020532bc4f04739ca1869b29
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-05-24 15:26:09 +0100

    feat: added mini tsup zip script

---
+++ packages/ts-scripts/src/tsup-zip/index.ts
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+import { execSync } from 'child_process';
+
+// required for complex excluded packages in tsconfig
+// const packageDependencies = (folder: string, tmpDir: string) => {
+//   const pack = JSON.parse(fs.readFileSync(`../../node_modules/${folder}/package.json`, 'utf8'))
+
+//   execSync(`cp -r ${path.resolve(__dirname, '..', '..', '..', 'node_modules', folder)} ${tmpDir}/node_modules`)
+
+//   if (!pack.dependencies || !Object.keys(pack.dependencies)) return
+
+//   Object.keys(pack.dependencies).forEach((dep) => {
+//     console.log(`copying ${dep}...`)
+//     execSync(
+//       `cp -r ${path.resolve(__dirname, '..', '..', '..', 'node_modules', dep)} ${tmpDir}/node_modules`
+//     )
+//     packageDependencies(dep, tmpDir)
+//   })
+// }
+
+const bundle = (includes: string[], packageFolder: string) => {
+  const tmpDir = fs.mkdtempSync([os.tmpdir(), `${packageFolder}-build-`].join(path.sep))
+
+  fs.mkdirSync(`${tmpDir}/node_modules`)
+
+  fs.mkdirSync(`${tmpDir}/packages`)
+  fs.mkdirSync(`${tmpDir}/packages/${packageFolder}`)
+
+  includes.forEach((folder) => {
+    execSync(`cp -r ${path.resolve(process.cwd(), folder)} ${tmpDir}/packages/${packageFolder}/${folder}`)
+  })
+
+  execSync(`cd ${tmpDir} && zip -q -r ${path.resolve('bundle.zip')} .`)
+}
+
+const [folderPackage] = process.argv.slice(2)
+
+bundle(['dist'], folderPackage)
