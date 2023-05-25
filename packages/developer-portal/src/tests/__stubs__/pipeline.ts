d2cec12a336adafe2bb92adba913c27feae51f38
Will McVay <wmcvay@reapit.com>
2023-05-25 10:56:13 +0100

    fix: #8353 fixes sonar bug (#9150)

--- packages/developer-portal/src/tests/__stubs__/pipeline.ts
+++ packages/developer-portal/src/tests/__stubs__/pipeline.ts
@@ -26,8 +26,7 @@ export const mockPipelineRunnerResponse: PipelineRunnerResponse = {
       created: '2022-04-20T14:30:26.396Z',
       modified: '2022-04-20T14:30:34.000Z',
       buildStatus: 'SUCCEEDED',
-      s3BuildLogsLocation:
-        'https://cloud-deployment-usercodedeploymentlogdevd52b2ed4df81d2ac2d65.s3.eu-west-2.amazonaws.com/40a3fd04-617f-432a-bc60-9ee4dc890f2d.gz?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAVI3TO7F7IOG7QAP7%2F20220420%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220420T143034Z&X-Amz-Expires=604800&X-Amz-Security-Token=FwoGZXIvYXdzEHAaDC0WYfIcqQ5h6Lh%2FqCK2ATFhz7WT5v9UVTDO9TaD0zQkh8MrHhdvxopm2W6JdULyCFbXbCGFU8e%2Fv9C0Hh6RaHd2IyfvZ4KFhxoGiUNOGBjorM5UkXkU5R4lNYHyh3%2F4tOhlZYEhv8AiWe1kJpego%2FJ6f2Ar06NX%2BOvMEBeqGH5mpsrFLIFkVMTc4QMnywwoQGZ7WX9lNw%2Bjh7q8N3%2BfypaiNKuQd5Jetk0sNlr0IZvSSPKXjGva72bbqdu8Vka6HyJcowCJKIqygJMGMi2wRmSczuSNERUtIvguJNZFNSA7y96gcYBzq8GBE%2F%2Bb0iqYSoaqk4LZP0pWd4Y%3D&X-Amz-Signature=2e379e98ae32fefa3f00ff4cbf6827ef5bae5b2d93e40298c5de6a003b1fe059&X-Amz-SignedHeaders=host',
+      s3BuildLogsLocation: 'SOME_LOCATION',
       tasks: [
         {
           id: '2f187892-ed0a-41c9-b4b3-2fa0db20b014',
