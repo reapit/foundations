49e1fd79ad066a3cba52ba5eb52eff255bed2bf3
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-05-30 14:39:39 +0100

    fix: added catch to unawaited promise

--- packages/graphql-server-v2/src/http-local.ts
+++ packages/graphql-server-v2/src/http-local.ts
@@ -1,4 +1,6 @@
 import { createPlatformAxiosInstance } from './axios'
 import { bootstrap } from './server'
 
-bootstrap(createPlatformAxiosInstance()).then((app) => app.listen(4000, () => console.log(`running on port ${4000}`)))
+bootstrap(createPlatformAxiosInstance())
+  .then((app) => app.listen(4000, () => console.log(`running on port ${4000}`)))
+  .catch(error => console.error(error))
