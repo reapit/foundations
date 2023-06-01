f0283e3c6fd9a198c4abe814ed90f83448c40aa1
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-06-01 11:45:28 +0100

    feat: added ability to bundle node_packages using tszip-bundler

--- packages/ts-scripts/src/tsup-zip/index.ts
+++ packages/ts-scripts/src/tsup-zip/index.ts
@@ -1,29 +1,37 @@
 import fs from 'fs'
 import path from 'path'
 import os from 'os'
-import { execSync } from 'child_process';
+import { execSync } from 'child_process'
 
 // required for complex excluded packages in tsconfig
-// const packageDependencies = (folder: string, tmpDir: string) => {
-//   const pack = JSON.parse(fs.readFileSync(`../../node_modules/${folder}/package.json`, 'utf8'))
+const packageDependencies = (folder: string, tmpDir: string) => {
+  const pack = JSON.parse(fs.readFileSync(`../../node_modules/${folder}/package.json`, 'utf8'))
 
-//   execSync(`cp -r ${path.resolve(__dirname, '..', '..', '..', 'node_modules', folder)} ${tmpDir}/node_modules`)
+  execSync(`cp -r ${path.resolve(__dirname, '..', '..', '..', 'node_modules', folder)} ${tmpDir}/node_modules`)
 
-//   if (!pack.dependencies || !Object.keys(pack.dependencies)) return
+  if (!pack.dependencies || !Object.keys(pack.dependencies)) return
 
-//   Object.keys(pack.dependencies).forEach((dep) => {
-//     console.log(`copying ${dep}...`)
-//     execSync(
-//       `cp -r ${path.resolve(__dirname, '..', '..', '..', 'node_modules', dep)} ${tmpDir}/node_modules`
-//     )
-//     packageDependencies(dep, tmpDir)
-//   })
-// }
+  Object.keys(pack.dependencies).forEach((dep) => {
+    console.log(`copying ${dep}...`)
+    execSync(`cp -r ${path.resolve(__dirname, '..', '..', '..', 'node_modules', dep)} ${tmpDir}/node_modules`)
+    packageDependencies(dep, tmpDir)
+  })
+}
+
+const findConfig = () => {
+  if (!fs.existsSync(path.resolve('tsup-zip.config.json'))) return {}
+
+  return JSON.parse(fs.readFileSync(path.resolve('tsup-zip.config.json')).toString())
+}
 
 const bundle = (includes: string[], packageFolder: string) => {
   const tmpDir = fs.mkdtempSync([os.tmpdir(), `${packageFolder}-build-`].join(path.sep))
+  const config = findConfig()
 
   fs.mkdirSync(`${tmpDir}/node_modules`)
+  ;(config['bundle-packages'] || []).forEach((folder) => {
+    packageDependencies(folder, tmpDir)
+  })
 
   fs.mkdirSync(`${tmpDir}/packages`)
   fs.mkdirSync(`${tmpDir}/packages/${packageFolder}`)
