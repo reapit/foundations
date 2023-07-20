7891c0e4f0b1437329c703f7de6dac22def10055
Will McVay <wmcvay@reapit.com>
2023-07-20 20:13:41 +0100

    chore: deploy rc service (#9493)

--- packages/rc-service/cdk-stack.ts
+++ packages/rc-service/cdk-stack.ts
@@ -1,7 +1,8 @@
-import { createBaseStack, createMultiRegionSite, getAccountId } from '@reapit/ts-scripts/src/cdk'
+import { createBaseStack, createMultiRegionSite, createSite, getAccountId } from '@reapit/ts-scripts/src/cdk'
 import { join } from 'path'
 
 const createStack = async () => {
+  const isProd = process.env.APP_STAGE === 'production'
   const accountId = await getAccountId()
   const stack = createBaseStack({
     namespace: 'cloud',
@@ -9,13 +10,21 @@ const createStack = async () => {
     component: 'site',
     accountId,
     region: 'eu-west-2',
-    crossRegionReferences: true,
+    crossRegionReferences: isProd,
   })
 
-  await createMultiRegionSite(stack, {
-    env: process.env.APP_STAGE === 'production' ? 'prod' : 'dev',
-    location: join(__dirname, 'build'),
-  })
+  // Remove prod check when we go live in ANZ
+  if (isProd) {
+    await createSite(stack, {
+      env: 'prod',
+      location: join(__dirname, 'build'),
+    })
+  } else {
+    await createMultiRegionSite(stack, {
+      env: 'dev',
+      location: join(__dirname, 'build'),
+    })
+  }
 }
 
 const bootstrap = async () => {
