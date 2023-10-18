66f0c728f7e126d0a7a02cb617388e33cf7c333d
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-10-18 13:11:19 +0100

    feat: added github entity and github provider

---
+++ packages/deployment-service/src/github/github.repository.provider.ts
@@ -0,0 +1,35 @@
+import { Repository, UpdateResult } from 'typeorm'
+import { GithubRepositoryEntity } from './github.repository.entity'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class GithubRepositoryProvider {
+  constructor(private readonly repository: Repository<GithubRepositoryEntity>) {}
+
+  async findRepositoriesByUrl(repositoryUrl: string): Promise<GithubRepositoryEntity[]> {
+    return this.repository.find({
+      where: { repositoryUrl },
+      relations: ['pipeline'],
+    })
+  }
+
+  async updateRepositories({
+    repositoryUrl,
+    installationId,
+    repositoryId,
+  }: {
+    repositoryUrl: string
+    installationId: number
+    repositoryId: number
+  }): Promise<UpdateResult> {
+    return this.repository.update(
+      {
+        repositoryUrl,
+      },
+      {
+        installationId,
+        repositoryId,
+      },
+    )
+  }
+}
--- packages/deployment-service/src/github/module.ts
+++ packages/deployment-service/src/github/module.ts
@@ -6,15 +6,25 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { App } from '@octokit/app'
 import GithubConfig from '../config/github'
 import { GithubWebhookController } from './github-webhook-controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { GithubRepositoryEntity } from './github.repository.entity'
+import { GithubRepositoryProvider } from './github.repository.provider'
 
 @Module({
-  imports: [ConfigModule.forFeature(GithubConfig), PipelineModule, PipelineRunnerModule, EventModule],
+  imports: [
+    ConfigModule.forFeature(GithubConfig),
+    PipelineModule,
+    PipelineRunnerModule,
+    EventModule,
+    TypeOrmModule.forFeature([GithubRepositoryEntity]),
+  ],
   providers: [
     {
       provide: App,
       useFactory: (config: ConfigService) => new App(config.get('github')),
       inject: [ConfigService],
     },
+    GithubRepositoryProvider,
   ],
   controllers: [GithubWebhookController],
   exports: [App],
