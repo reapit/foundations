80ef2d410b69bf78463d21275b53747f0a76e821
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2023-10-25 15:33:31 +0100

    feat: added endpoint to delete parameters

--- packages/deployment-service/src/pipeline/parameter-controller.ts
+++ packages/deployment-service/src/pipeline/parameter-controller.ts
@@ -1,7 +1,7 @@
 import { IdTokenGuard, OwnershipProvider, Creds } from '@reapit/utils-nest'
 import type { CredsType } from '@reapit/utils-nest'
 import { PipelineProvider } from './pipeline-provider'
-import { Controller, Get, UseGuards, Param, NotFoundException, Put, Body } from '@nestjs/common'
+import { Controller, Get, UseGuards, Param, NotFoundException, Put, Body, Delete } from '@nestjs/common'
 import { ParameterDto } from './parameter-dto'
 import { ParameterProvider } from './parameter-provider'
 
@@ -46,6 +46,25 @@ export class ParameterController {
     const parameters = await this.paramProvider.obtainParameters(pipeline.id as string)
 
     parameters[parameterDto.key] = parameterDto.value
+
+    await this.paramProvider.saveParameters(pipeline.id as string, parameters)
+  }
+
+  @Delete(':key')
+  async delete(
+    @Creds() creds: CredsType,
+    @Param('pipelineId') pipelineId: string,
+    @Param('key') key: string,
+  ): Promise<void> {
+    const pipeline = await this.pipelineProvider.findById(pipelineId)
+
+    if (!pipeline) throw new NotFoundException()
+
+    this.ownershipProvider.check(pipeline, creds.developerId as string)
+
+    const parameters = await this.paramProvider.obtainParameters(pipeline.id as string)
+
+    delete parameters[key]
 
     await this.paramProvider.saveParameters(pipeline.id as string, parameters)
   }
