c468cdc4c4d20f54558459235afac3156c5a4e58
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-03-07 12:06:49 +0000

    chore: remove react app scaffolder package

--- packages/react-app-scaffolder/app/templates/react-app/src/components/pages/__tests__/home-page.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import HomePage from '../home-page'
-
-describe('HomePage', () => {
-  it('should match a snapshot', () => {
-    expect(render(<HomePage />)).toMatchSnapshot()
-  })
-})
--- packages/react-app-scaffolder/app/templates/react-app/src/core/__mocks__/router.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react'
-
-const Router = () => <div>I am a router</div>
-export default Router
