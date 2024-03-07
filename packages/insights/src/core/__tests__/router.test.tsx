4935e2603b657172c3ca8edc14ae8e6a69c13744
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-03-07 11:46:38 +0000

    chore: remove insights package

--- packages/insights/src/core/__tests__/router.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '../../tests/react-testing'
-import { RoutesComponent } from '../router'
-
-describe('RoutesComponent', () => {
-  it('should match a snapshot', () => {
-    expect(render(<RoutesComponent />)).toMatchSnapshot()
-  })
-})
