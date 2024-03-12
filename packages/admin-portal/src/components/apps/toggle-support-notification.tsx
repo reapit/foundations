d3cccce0084de153ef7e6b076a5589e95634fa11
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-03-12 10:43:57 +0000

    feat: added toggle for notification support

---
+++ packages/admin-portal/src/components/apps/toggle-support-notification.tsx
@@ -0,0 +1,53 @@
+import { BodyText, Button, ButtonGroup, useModal } from '@reapit/elements'
+import { UpdateActionNames, updateActions, useReapitUpdate } from '@reapit/use-reapit-data'
+import React, { FC, useEffect } from 'react'
+import { reapitConnectBrowserSession } from '../../core/connect-session'
+
+export const ToggleSupportNotification: FC<{ appId: string; hasReadAccess: boolean }> = ({ appId, hasReadAccess }) => {
+  const { Modal, openModal, closeModal } = useModal()
+
+  const [loading, , setSupportNotification, done] = useReapitUpdate({
+    reapitConnectBrowserSession,
+    action: updateActions[UpdateActionNames.updateApp],
+    method: 'PUT',
+    uriParams: {
+      appId,
+    },
+    headers: {
+      ['Api-Version']: 'latest',
+    },
+  })
+
+  useEffect(() => {
+    if (done) closeModal()
+  }, [done])
+
+  return (
+    <>
+      <Button onClick={openModal} intent="primary" disabled={hasReadAccess}>
+        Toggle Support Notification
+      </Button>
+
+      <Modal title={'Toggle Support Notifications'}>
+        <BodyText>This will enable the support notifications</BodyText>
+        <ButtonGroup alignment="center">
+          <Button intent="secondary" onClick={closeModal}>
+            Cancel
+          </Button>
+          <Button
+            intent="primary"
+            disabled={loading}
+            loading={loading}
+            onClick={() =>
+              setSupportNotification({
+                sendInternalInstallNotification: true,
+              })
+            }
+          >
+            Confirm
+          </Button>
+        </ButtonGroup>
+      </Modal>
+    </>
+  )
+}
