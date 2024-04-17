07a51e922f5f2c659d4cad34f020de3e4e023d7b
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-04-17 16:43:30 +0100

    fix: updated node version target

--- packages/connect-session-server/tsup.config.ts
+++ packages/connect-session-server/tsup.config.ts
@@ -5,7 +5,7 @@ const pkgJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    target: 'node16',
+    target: 'node18',
     outDir: 'dist',
     clean: true,
     minify: process.env.NODE_ENV === 'production',
