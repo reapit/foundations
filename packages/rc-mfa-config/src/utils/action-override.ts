ef4bc413fd7afd88750ff9911908d1c3de7f4db7
Josh Balfour <josh@joshbalfour.co.uk>
2024-05-14 11:00:54 +0100

    Update action-override.ts

--- packages/rc-mfa-config/src/utils/action-override.ts
+++ packages/rc-mfa-config/src/utils/action-override.ts
@@ -4,6 +4,10 @@ import { getRCConfig } from '../core/connect-session'
 export const useRCAPI = Cookie.get('mfa_native_token_required') === 'true'
 const rcAPIBaseUrl = getRCConfig().connectOAuthUrl + '/rpt'
 
+console.log('cookie:', document.cookie)
+console.log('mfa_native_token_required:', Cookie.get('mfa_native_token_required'))
+console.log('useRCAPI', useRCAPI)
+
 export const actionOverride = <T extends { api: string }>(action: T) => {
   if (!useRCAPI) {
     return action
