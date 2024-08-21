8de8462104355678ce484429f6123323f4a26ebe
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-08-21 15:39:40 +0100

    feat: added authorizer to cdk

--- packages/graphql-server/cdk-stack.ts
+++ packages/graphql-server/cdk-stack.ts
@@ -5,6 +5,7 @@ import path from 'path'
 import { execSync } from 'child_process'
 
 import { createApi, createFunction, createBaseStack, output, addLambdaToApi } from '@reapit/ts-scripts/src/cdk'
+import { Duration, aws_lambda, aws_apigateway } from 'aws-cdk-lib'
 
 import config from './config.json'
 
@@ -38,8 +39,33 @@ const createStack = () => {
   const handler = 'packages/graphql-server/src/index.graphqlHandler'
   const entrypoint = path.resolve(__dirname, 'bundle.zip')
 
-  const lambdaFunction = createFunction(stack, 'graphql', entrypoint, handler, config)
+  const { ISSUERS, ...env } = config
+
+  const lambdaFunction = createFunction(stack, 'graphql', entrypoint, handler, env)
   const api = createApi(stack, 'api')
+
+  const authorizerLambda = createFunction(
+    stack,
+    'graphql-server-authorizer-lambda',
+    path.resolve(__dirname, 'dist', 'authorizer'),
+    'authorizer.authorizerHandler',
+    {
+      ISSUERS: ISSUERS.join(','),
+      CLIENT_ID: config.COGNITO_CLIENT_ID,
+      CONNECT_USER_POOL: config.COGNITO_USERPOOL_ID,
+    },
+    undefined,
+    undefined,
+    undefined,
+    aws_lambda.Runtime.NODEJS_18_X,
+  )
+
+  const authorizer = new aws_apigateway.RequestAuthorizer(stack, 'graphql-server-authorizer', {
+    handler: authorizerLambda,
+    identitySources: [aws_apigateway.IdentitySource.header('authorization')],
+    resultsCacheTtl: Duration.seconds(0),
+  })
+
   addLambdaToApi(stack, api, lambdaFunction, { path: '/{proxy+}', method: 'ANY' }, config.COGNITO_USERPOOL_ID)
   output(stack, 'api-url', api.url)
 }
