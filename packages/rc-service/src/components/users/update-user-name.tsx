e4d7a94a94dc1a871891910aa7726e410fad656d
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-09-17 15:50:15 +0100

    feat: added method to update user's name on organisations

---
+++ packages/rc-service/src/components/users/update-user-name.tsx
@@ -0,0 +1,63 @@
+import React, { FC } from 'react'
+import { Button, FormLayout, InputGroup, InputWrapFull, Modal, Title, useModal } from '@reapit/elements'
+import { UpdateActionNames, updateActions, useReapitUpdate } from '@reapit/use-reapit-data'
+import { reapitConnectBrowserSession } from '../../core/connect-session'
+import { UserModel } from '@reapit/foundations-ts-definitions'
+import { useForm } from 'react-hook-form'
+
+export const UpdateUserName: FC<{ user: UserModel }> = ({ user }) => {
+  const { modalIsOpen, openModal, closeModal } = useModal()
+
+  const [isLoading, , updateUser] = useReapitUpdate<UserModel, UserModel>({
+    uriParams: {
+      userId: user.id,
+    },
+    action: updateActions[UpdateActionNames.updateUser],
+    reapitConnectBrowserSession,
+    method: 'PATCH',
+  })
+
+  const { register, handleSubmit } = useForm<{ name: string }>({
+    defaultValues: {
+      name: user.name,
+    },
+  })
+
+  return (
+    <>
+      <Button intent="primary" onClick={() => openModal()}>
+        Update User&apos;s Name
+      </Button>
+      <Modal
+        isOpen={modalIsOpen}
+        onModalClose={() => {
+          closeModal()
+        }}
+      >
+        <form
+          onSubmit={handleSubmit(({ name }) => {
+            console.log('update name', name)
+
+            updateUser({
+              name,
+            })
+          })}
+        >
+          <FormLayout>
+            <InputWrapFull>
+              <Title>Update Name</Title>
+            </InputWrapFull>
+            <InputWrapFull>
+              <InputGroup label="Name" {...register('name')} />
+            </InputWrapFull>
+            <InputWrapFull>
+              <Button loading={isLoading} disabled={isLoading} intent="primary">
+                Update
+              </Button>
+            </InputWrapFull>
+          </FormLayout>
+        </form>
+      </Modal>
+    </>
+  )
+}
