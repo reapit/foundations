ede808dd4ddd20c331eb2830cc07b02b9d780f3e
Pete Littlewood <plittlewood@reapit.com>
2024-09-20 15:52:36 +0100

    chore: dont return blank if token not present

--- packages/developer-portal/src/utils/token.ts
+++ packages/developer-portal/src/utils/token.ts
@@ -1,8 +1,6 @@
 export const tokenFromCognito = (token: string) => getTokenIssuer(token)?.includes('cognito') ?? false
 
 export const getTokenIssuer = (token: string) => {
-  if (!token) return ''
-
   const decoded = JSON.parse(atob(token.split('.')[1]))
   return decoded?.iss ?? ''
 }
