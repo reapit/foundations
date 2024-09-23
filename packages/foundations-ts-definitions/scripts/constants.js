36964f46063dd8f918292bb2d0b659d38a8c72cf
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-09-23 14:58:44 +0100

    chore: remove foundations-ts-definitions

--- packages/foundations-ts-definitions/scripts/constants.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  FOUNDATIONS_ROOT_FOLDER: path.resolve(__dirname, '..'),
-  FOUNDATIONS_TYPES_FOLDER: path.resolve(__dirname, '../types'),
-  PACKAGE_NAME: 'foundations-ts-definitions',
-  PLATFORM_API_BASE_URL: 'https://platform.dev.paas.reapit.cloud',
-}
