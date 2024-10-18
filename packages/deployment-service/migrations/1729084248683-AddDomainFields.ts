a0d4cef146e744714d01d0178fef61027480c3bc
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-10-18 10:13:10 +0100

    fix: remove verifyName column

--- packages/deployment-service/migrations/1729084248683-AddDomainFields.ts
+++ packages/deployment-service/migrations/1729084248683-AddDomainFields.ts
@@ -5,7 +5,6 @@ export class AddDomainFields1729084248683 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE `pipelines` ADD `verifyDnsValue` varchar(255) NULL')
-    await queryRunner.query('ALTER TABLE `pipelines` ADD `verifyDnsName` varchar(255) NULL')
     await queryRunner.query('ALTER TABLE `pipelines` ADD `customDomain` varchar(255) NULL')
     await queryRunner.query('ALTER TABLE `pipelines` ADD `domainVerified` tinyint NOT NULL')
     await queryRunner.query('ALTER TABLE `pipelines` ADD `certificateArn` varchar(255) NULL')
@@ -13,7 +12,6 @@ export class AddDomainFields1729084248683 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE `pipelines` DROP COLUMN `verifyDnsValue`')
-    await queryRunner.query('ALTER TABLE `pipelines` DROP COLUMN `verifyDnsName`')
     await queryRunner.query('ALTER TABLE `pipelines` DROP COLUMN `customDomain`')
     await queryRunner.query('ALTER TABLE `pipelines` DROP COLUMN `domainVerified`')
     await queryRunner.query('ALTER TABLE `pipelines` DROP COLUMN `certificateArn`')
