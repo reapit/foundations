23ac38fd8f435e98da910d3e0ad1583ecb45308c
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-10-18 10:13:10 +0100

    fix: remove verifyName column

--- packages/deployment-service/src/dns/dns.model.ts
+++ packages/deployment-service/src/dns/dns.model.ts
@@ -1,11 +1,6 @@
-import { IsString, Length, IsUrl, IsNotEmpty } from 'class-validator'
+import { IsUrl, IsNotEmpty } from 'class-validator'
 
 export class CreateDnsModel {
-  @IsString()
-  @Length(3, 20)
-  @IsNotEmpty()
-  recordName: string
-
   @IsUrl()
   @IsNotEmpty()
   customDomain: string
