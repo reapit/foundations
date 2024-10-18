6838f48cf27c0e921b48a086bff42034f5a748ee
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-10-18 12:00:58 +0100

    chore: resolve graphql server build and bundling (#11471)
    * chore: remove gql-server v2
    
    * chore: refactor gql-server to use tsup for build to avoid ts-bundler

--- packages/graphql-server-v2/src/http-local.ts
+++
@@ -1,6 +0,0 @@
-import { createPlatformAxiosInstance } from './axios'
-import { bootstrap } from './server'
-
-bootstrap(createPlatformAxiosInstance())
-  .then((app) => app.listen(4000, () => console.log(`running on port ${4000}`)))
-  .catch((error) => console.error(error))
