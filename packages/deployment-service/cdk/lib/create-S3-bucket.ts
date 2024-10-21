d56352fb5e1862417b7572ca30650d16d757c707
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2024-10-21 21:16:17 +0100

    refactor: change physical name gen method for s3 bucket

--- packages/deployment-service/cdk/lib/create-S3-bucket.ts
+++ packages/deployment-service/cdk/lib/create-S3-bucket.ts
@@ -1,10 +1,39 @@
-import { Stack, Bucket, createBucket, BucketOptions } from '@reapit/ts-scripts/src/cdk'
+import { Stack, Bucket, BucketOptions, PolicyStatement } from '@reapit/ts-scripts/src/cdk'
+import { aws_s3, PhysicalName, aws_iam } from 'aws-cdk-lib'
 
 export enum BucketNames {
   LIVE = 'v2-cloud-deployment-live',
   LOG = 'v2-cloud-deployment-log',
   VERSION = 'v2-cloud-deployment-version',
   REPO_CACHE = 'v2-cloud-deployment-repo-cache',
+}
+
+export const createBucket = (stack: Stack, bucketName: string, options?: BucketOptions): aws_s3.Bucket => {
+  const bucket = new aws_s3.Bucket(options?.stack || stack, bucketName, {
+    publicReadAccess: options?.public,
+    websiteIndexDocument: options?.public ? 'index.html' : undefined,
+    bucketName: bucketName || PhysicalName.GENERATE_IF_NEEDED,
+  })
+  const actions: string[] = []
+  if (options?.get) {
+    actions.push('s3:Get*')
+  }
+  if (options?.list) {
+    actions.push('s3:List*')
+  }
+  if (options?.put) {
+    actions.push('s3:Put*')
+  }
+
+  bucket.addToResourcePolicy(
+    new PolicyStatement({
+      effect: aws_iam.Effect.ALLOW,
+      actions,
+      resources: [bucket.arnForObjects('*')],
+      principals: [new aws_iam.ArnPrincipal('*')],
+    }),
+  )
+  return bucket
 }
 
 export const createS3Buckets = (stack: Stack, usercodeStack: Stack, envStage: string): Record<BucketNames, Bucket> => {
