f88ec24ee675f7f2f7234dbf6fdf39d6db2587a0
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2025-01-28 09:35:01 +0000

    refactor: split form elements into seperate files

---
+++ packages/developer-portal/src/components/form-wizard/form-wizard-step.tsx
@@ -0,0 +1,52 @@
+import { Button, ButtonGroup } from '@reapit/elements'
+import React from 'react'
+import { FormWizardStepPropsInterface } from './interface'
+import { FieldValues, useForm } from 'react-hook-form'
+
+export const FormWizardStep = <TFieldValues extends FieldValues>({
+  formOptions,
+  component: Component,
+  previousStep,
+  canGoBack,
+  isLastStep,
+  onSubmit,
+  isSubmitting = false,
+}: FormWizardStepPropsInterface<TFieldValues>) => {
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    watch,
+    setError,
+    setValue,
+    formState: { errors },
+  } = useForm(formOptions)
+
+  return (
+    <form style={{ marginTop: '1rem' }} onSubmit={handleSubmit(onSubmit)}>
+      <Component
+        register={register}
+        errors={errors}
+        getValues={getValues}
+        watch={watch}
+        setError={setError}
+        setValue={setValue}
+      />
+      <ButtonGroup>
+        <Button
+          disabled={!canGoBack}
+          onClick={(event) => {
+            event.preventDefault()
+            previousStep()
+          }}
+          intent="neutral"
+        >
+          Previous
+        </Button>
+        <Button disabled={isSubmitting} loading={isSubmitting} intent="primary">
+          {isLastStep ? 'Submit' : 'Next'}
+        </Button>
+      </ButtonGroup>
+    </form>
+  )
+}
--- packages/developer-portal/src/components/form-wizard/interface.ts
+++ packages/developer-portal/src/components/form-wizard/interface.ts
@@ -8,6 +8,7 @@ import {
   UseFormWatch,
   UseFormSetError,
   UseFormProps,
+  SubmitHandler,
 } from 'react-hook-form'
 
 export type StepComponent<TFieldValues extends FieldValues> = FC<{
@@ -20,6 +21,24 @@ export type StepComponent<TFieldValues extends FieldValues> = FC<{
 }>
 
 export type Step<TFieldValues extends FieldValues> = {
+  name: string
+  component: StepComponent<TFieldValues>
+  formOptions?: UseFormProps<TFieldValues>
+}
+
+export interface FormWizardPropsInterface<TFieldValues extends FieldValues> {
+  steps: Record<string, Step<TFieldValues>>
+  onSubmit: SubmitHandler<TFieldValues>
+  showStepNumbers?: boolean
+  isSubmitting?: boolean
+}
+
+export interface FormWizardStepPropsInterface<TFieldValues extends FieldValues> {
+  onSubmit: SubmitHandler<TFieldValues>
+  previousStep: () => void
+  canGoBack: boolean
+  isLastStep: boolean
+  isSubmitting?: boolean
   name: string
   component: StepComponent<TFieldValues>
   formOptions?: UseFormProps<TFieldValues>
