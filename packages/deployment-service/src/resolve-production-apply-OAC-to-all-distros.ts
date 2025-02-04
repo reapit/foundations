6207f7f29eee19f1366ca4358dc0e86e85f91cd9
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2025-02-04 10:41:37 +0000

    fix: 739 RPLT custom resource s3 oac repair (#11507)
    * fix: oac timeout to avoid rate limit
    
    * feat: seperated s3 policies and permissions lambda
    
    * fix: commented out custom resources
    
    * feat: added retries to the distro calls
    
    * feat: implemented S3 bucket changes in the correct order
    
    * fix: commented out custom resources so they can be ran in order
    
    * fix: readded pipeline OAC
    
    * chore: remove comments and console logs
    
    * fix: remove cdk from jest coverage
    
    * fix: reduce branch coverage level
    
    * feat: increased max timeout for OAC custom resource
    
    * chore: reduce coverage
    
    * chore: reduce global functions coverage
    
    * fix: remove comment
    
    * refactor: handle one at a time with counter for confirmation
    
    * feat: added oac to provisioned pipelines
    
    ---------
    
    Co-authored-by: Pete Littlewood <30798627+plittlewood-rpt@users.noreply.github.com>

--- packages/deployment-service/src/resolve-production-apply-OAC-to-all-distros.ts
+++ packages/deployment-service/src/resolve-production-apply-OAC-to-all-distros.ts
@@ -3,9 +3,101 @@ import {
   GetDistributionCommand,
   ListDistributionsCommand,
   ListOriginAccessControlsCommand,
+  OriginAccessControlSummary,
   UpdateDistributionCommand,
+  UpdateDistributionCommandOutput,
 } from '@aws-sdk/client-cloudfront'
 import { OnEventHandler } from 'aws-cdk-lib/custom-resources/lib/provider-framework/types'
+
+const updateDistro = async (
+  client: CloudFrontClient,
+  OAC: OriginAccessControlSummary,
+  distroId: string,
+): Promise<UpdateDistributionCommandOutput | void> => {
+  const distributionResult = await client.send(
+    new GetDistributionCommand({
+      Id: distroId,
+    }),
+  )
+
+  await new Promise<void>((resolve) => setTimeout(() => resolve(), 2000))
+
+  const distribution = distributionResult.Distribution
+
+  console.log('distro', distribution)
+
+  if (!distribution) {
+    console.error(`Distribution not found [${distroId}]`)
+    return Promise.resolve()
+  }
+
+  if (distribution.DistributionConfig?.Origins?.Items?.find((origin) => origin.OriginAccessControlId)) {
+    console.log('distro', distribution.Id, 'has been skipped. Origin access aready applied')
+    return Promise.resolve()
+  }
+
+  return client.send(
+    new UpdateDistributionCommand({
+      Id: distroId,
+      DistributionConfig: {
+        ...distribution.DistributionConfig,
+        DefaultCacheBehavior: distribution.DistributionConfig?.DefaultCacheBehavior,
+        Comment: distribution.DistributionConfig?.Comment,
+        Enabled: distribution.DistributionConfig?.Enabled,
+        CallerReference: distribution.DistributionConfig?.CallerReference,
+        Origins: {
+          Quantity: distribution?.DistributionConfig?.Origins?.Quantity,
+          Items: distribution?.DistributionConfig?.Origins?.Items?.map((item) => ({
+            ...item,
+            OriginAccessControlId: OAC?.Id,
+          })),
+        },
+      },
+      IfMatch: distributionResult.ETag,
+    }),
+  )
+}
+
+const retryableUpdateDistro = async (
+  client: CloudFrontClient,
+  OAC: OriginAccessControlSummary,
+  distroId: string,
+): Promise<void> => {
+  try {
+    await updateDistro(client, OAC, distroId)
+  } catch (error) {
+    console.error(error)
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 2000))
+    await retryableUpdateDistro(client, OAC, distroId)
+  }
+}
+
+const updateDistros = async (client: CloudFrontClient, OAC: OriginAccessControlSummary) => {
+  let marker: string | undefined = undefined
+  let counter: number = 1
+
+  do {
+    const fetchDistros = await client.send(
+      new ListDistributionsCommand({
+        Marker: marker,
+        MaxItems: 1,
+      }),
+    )
+
+    if (!fetchDistros.DistributionList.Items) return
+
+    fetchDistros.DistributionList?.Items.forEach(async (distro) => {
+      await retryableUpdateDistro(client, OAC, distro.Id as string)
+    })
+
+    console.log('prev marker', counter, marker)
+
+    marker = fetchDistros.DistributionList.NextMarker
+    counter++
+
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 5000))
+  } while (typeof marker !== 'undefined')
+}
 
 export const resolveProductionApplyOACToAllDistros: OnEventHandler = async (event) => {
   if (event.RequestType === 'Delete')
@@ -18,53 +110,17 @@ export const resolveProductionApplyOACToAllDistros: OnEventHandler = async (even
 
   const client = new CloudFrontClient({})
 
-  const fetchDistros = await client.send(new ListDistributionsCommand())
-
   const OACs = await client.send(new ListOriginAccessControlsCommand({}))
 
-  const OAC = OACs.OriginAccessControlList?.Items ? OACs.OriginAccessControlList?.Items[0] : undefined
+  const OAC = OACs.OriginAccessControlList?.Items
+    ? OACs.OriginAccessControlList?.Items.find((oac) => oac.Name === 'distro-to-s3')
+    : undefined
+
+  console.log('OAC', OAC)
 
   if (!OAC) throw new Error('OAC not found')
 
-  const distros = fetchDistros.DistributionList?.Items || []
-
-  await Promise.all(
-    distros?.map(async (distro) => {
-      const distributionResult = await client.send(
-        new GetDistributionCommand({
-          Id: distro.Id,
-        }),
-      )
-
-      const distribution = distributionResult.Distribution
-
-      if (!distribution) {
-        console.error(`Distribution not found [${distro.Id}]`)
-        return Promise.resolve() // TODO reject
-      }
-
-      return client.send(
-        new UpdateDistributionCommand({
-          Id: distro.Id,
-          DistributionConfig: {
-            ...distribution.DistributionConfig,
-            DefaultCacheBehavior: distribution.DistributionConfig?.DefaultCacheBehavior,
-            Comment: distribution.DistributionConfig?.Comment,
-            Enabled: distribution.DistributionConfig?.Enabled,
-            CallerReference: distribution.DistributionConfig?.CallerReference,
-            Origins: {
-              Quantity: distribution?.DistributionConfig?.Origins?.Quantity,
-              Items: distribution?.DistributionConfig?.Origins?.Items?.map((item) => ({
-                ...item,
-                OriginAccessControlId: OAC?.Id,
-              })),
-            },
-          },
-          IfMatch: distributionResult.ETag,
-        }),
-      )
-    }),
-  )
+  await updateDistros(client, OAC)
 
   return {
     PhysicalResourceId: event.PhysicalResourceId,
