5c520630a262db3920587e89198987de8e9b51fd
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2025-03-18 15:56:28 +0000

    feat: backend github authentication implementation

---
+++ packages/deployment-service/src/config/github-auth.ts
@@ -0,0 +1,7 @@
+import { registerAs } from "@nestjs/config";
+
+export default registerAs('gitub-auth', () => ({
+  client_id: process.env.GITHUB_CLIENT_ID as string,
+  client_secert: process.env.GITHUB_CLIENT_SECRET as string,
+  github_url: process.env.GITHUB_URL,
+}))
