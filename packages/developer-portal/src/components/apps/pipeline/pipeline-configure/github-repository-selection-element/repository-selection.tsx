e6b62cb82cd7e3275535d0b42131565ea542990d
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2025-03-19 10:18:51 +0000

    feat: implemented repository pagination

--- packages/developer-portal/src/components/apps/pipeline/pipeline-configure/github-repository-selection-element/repository-selection.tsx
+++ packages/developer-portal/src/components/apps/pipeline/pipeline-configure/github-repository-selection-element/repository-selection.tsx
@@ -2,26 +2,50 @@ import React, { Dispatch, FC, useContext, useEffect, useState } from 'react'
 import { Installation, Repository } from './types'
 import { GithubAccessToken, GithubContext } from '../../github'
 import { LevelEl, RepositoryEl, RepositorySelectionActive, RepositorySelectionEl } from './__styles__'
-import { Button, elMb6, Loader, Title } from '@reapit/elements'
+import { Button, elMb6, Loader, Pagination, Title } from '@reapit/elements'
 import { cx } from '@linaria/core'
 
-const fetchRepositories = async (
-  githubAccessToken: GithubAccessToken,
-  setRepositories: Dispatch<Repository[]>,
-  setLoading: Dispatch<boolean>,
-  installation: Installation,
-) => {
+type PageData = {
+  total_pages: number
+  per_page: number
+  current_page: number
+}
+
+const fetchRepositories = async ({
+  githubAccessToken,
+  installation,
+  setRepositories,
+  setLoading,
+  pageData,
+  setPageData,
+}: {
+  githubAccessToken: GithubAccessToken
+  setRepositories: Dispatch<Repository[]>
+  setLoading: Dispatch<boolean>
+  installation: Installation
+  setPageData: Dispatch<PageData>
+  pageData: PageData
+}) => {
   setLoading(true)
 
-  const result = await fetch(`https://api.github.com/user/installations/${installation.id}/repositories`, {
-    headers: {
-      authorization: `${githubAccessToken.token_type} ${githubAccessToken.access_token}`,
+  const result = await fetch(
+    `https://api.github.com/user/installations/${installation.id}/repositories?page=${pageData.current_page}&per_page=${pageData.per_page}`,
+    {
+      headers: {
+        authorization: `${githubAccessToken.token_type} ${githubAccessToken.access_token}`,
+      },
     },
-  })
+  )
   const data = await result.json()
   setLoading(false)
 
   setRepositories(data.repositories)
+  const total_pages = Math.ceil(data.total_count / pageData.per_page)
+
+  setPageData({
+    ...pageData,
+    total_pages,
+  })
 }
 
 export const RepositorySelection: FC<{
@@ -34,12 +58,22 @@ export const RepositorySelection: FC<{
   const [repositories, setRepositories] = useState<Repository[] | undefined>()
   const { githubAccessToken } = useContext(GithubContext)
   const [loading, setLoading] = useState<boolean>(false)
+  const [pageData, setPageData] = useState<PageData>({
+    total_pages: 0,
+    per_page: 20,
+    current_page: 1,
+  })
 
   useEffect(() => {
-    fetchRepositories(githubAccessToken as GithubAccessToken, setRepositories, setLoading, installation)
+    fetchRepositories({
+      githubAccessToken: githubAccessToken as GithubAccessToken,
+      setRepositories,
+      setLoading,
+      installation,
+      pageData,
+      setPageData,
+    })
   }, [])
-
-  // TODO need pagination implementation
 
   return (
     <div>
@@ -60,16 +94,35 @@ export const RepositorySelection: FC<{
       </RepositoryEl>
       {loading && <Loader />}
       <div className={cx(elMb6)}>
-        {repositories &&
-          repositories.map((repo) => (
-            <RepositorySelectionEl
-              className={cx(selectedRepository?.id === repo.id && RepositorySelectionActive)}
-              onClick={() => setRepository(repo)}
-              key={repo.id}
-            >
-              {repo.full_name}
-            </RepositorySelectionEl>
-          ))}
+        {repositories && (
+          <>
+            <div key={pageData.current_page}>
+              {repositories.map((repo) => (
+                <RepositorySelectionEl
+                  className={cx(selectedRepository?.id === repo.id && RepositorySelectionActive)}
+                  onClick={() => setRepository(repo)}
+                  key={repo.id}
+                >
+                  {repo.full_name}
+                </RepositorySelectionEl>
+              ))}
+            </div>
+            <Pagination
+              currentPage={pageData.current_page}
+              numberPages={pageData.total_pages}
+              callback={async (current_page) => {
+                await fetchRepositories({
+                  githubAccessToken: githubAccessToken as GithubAccessToken,
+                  setRepositories,
+                  setLoading,
+                  installation,
+                  pageData: { ...pageData, current_page },
+                  setPageData,
+                })
+              }}
+            />
+          </>
+        )}
       </div>
       <Button
         onClick={(event) => {
