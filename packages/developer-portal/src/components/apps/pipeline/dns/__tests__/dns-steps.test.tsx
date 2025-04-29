804d89eb51d12ba9aff4d0b4bbf4736b48c5ee52
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2025-04-29 09:55:16 +0100

    fix: linting

--- packages/developer-portal/src/components/apps/pipeline/dns/__tests__/dns-steps.test.tsx
+++ packages/developer-portal/src/components/apps/pipeline/dns/__tests__/dns-steps.test.tsx
@@ -12,34 +12,54 @@ import { useReapitGet } from '@reapit/use-reapit-data'
 
 describe('DNS Configuration', () => {
   it('Will show configuration page when dns info is returned', () => {
-    (useReapitGet as jest.Mock).mockImplementationOnce(() => [{ customDomain: 'https://custom-domain.reapit.cloud', cloudfrontUrl: 'dfgdfgdfg.cloudfront.com', certificate: {
-      DomainValidationOptions: [
-        {
-          ResourceRecord: {
-            Name: 'dns record name',
-            Value: 'dns record value',
-            Type: 'CNAME',
-          },
+    ;(useReapitGet as jest.Mock).mockImplementationOnce(() => [
+      {
+        customDomain: 'https://custom-domain.reapit.cloud',
+        cloudfrontUrl: 'dfgdfgdfg.cloudfront.com',
+        certificate: {
+          DomainValidationOptions: [
+            {
+              ResourceRecord: {
+                Name: 'dns record name',
+                Value: 'dns record value',
+                Type: 'CNAME',
+              },
+            },
+          ],
         },
-      ],
-    } }])
+      },
+    ])
 
-    expect(render(<DnsConfiguration pipelineId='pipelineId' connectSession={{
-      accessToken: 'accessToken',
-      refreshToken: 'refreshToken',
-      idToken: 'idToken',
-      loginIdentity: {} as LoginIdentity
-    }} />)).toMatchSnapshot()
+    expect(
+      render(
+        <DnsConfiguration
+          pipelineId="pipelineId"
+          connectSession={{
+            accessToken: 'accessToken',
+            refreshToken: 'refreshToken',
+            idToken: 'idToken',
+            loginIdentity: {} as LoginIdentity,
+          }}
+        />,
+      ),
+    ).toMatchSnapshot()
   })
 
   it('Will show setup domain model component', () => {
-    (useReapitGet as jest.Mock).mockImplementationOnce(() => [undefined])
+    ;(useReapitGet as jest.Mock).mockImplementationOnce(() => [undefined])
 
-    expect(render(<DnsConfiguration pipelineId='pipelineId' connectSession={{
-      accessToken: 'accessToken',
-      refreshToken: 'refreshToken',
-      idToken: 'idToken',
-      loginIdentity: {} as LoginIdentity
-    }} />)).toMatchSnapshot()
+    expect(
+      render(
+        <DnsConfiguration
+          pipelineId="pipelineId"
+          connectSession={{
+            accessToken: 'accessToken',
+            refreshToken: 'refreshToken',
+            idToken: 'idToken',
+            loginIdentity: {} as LoginIdentity,
+          }}
+        />,
+      ),
+    ).toMatchSnapshot()
   })
 })
