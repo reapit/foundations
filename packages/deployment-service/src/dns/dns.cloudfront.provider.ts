76ed2048f9bed2c1070cacea04d268a24eca6649
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2025-04-30 09:44:42 +0100

    refactor: remove duplicate code

--- packages/deployment-service/src/dns/dns.cloudfront.provider.ts
+++ packages/deployment-service/src/dns/dns.cloudfront.provider.ts
@@ -21,47 +21,4 @@ export class DnsCloudFrontProvider {
 
     return result.Distribution?.DomainName
   }
-
-  private async getDistribution(pipeline: PipelineEntity): Promise<[Distribution, string | undefined] | never> {
-    const result = await this.cloudfrontClient.send(new GetDistributionCommand({ Id: pipeline.cloudFrontId }))
-
-    if (!result.Distribution) throw new Error(`Distro not found [${pipeline.cloudFrontId}]`)
-
-    return [result.Distribution, result.ETag]
-  }
-
-  async updateDistribution(pipeline: PipelineEntity, commonName: string): Promise<void> {
-    const [distribution, etag] = await this.getDistribution(pipeline)
-
-    try {
-      await this.cloudfrontClient.send(
-        new UpdateDistributionCommand({
-          ...distribution,
-          IfMatch: etag,
-          DistributionConfig: {
-            ...distribution.DistributionConfig,
-            PriceClass: distribution.DistributionConfig?.PriceClass,
-            CallerReference: distribution.DistributionConfig?.CallerReference,
-            Aliases: {
-              Quantity: 2,
-              Items: [...(distribution.DistributionConfig?.Aliases?.Items || []), commonName],
-            },
-            Origins: distribution.DistributionConfig?.Origins,
-            Comment: distribution.DistributionConfig?.Comment,
-            DefaultCacheBehavior: distribution.DistributionConfig?.DefaultCacheBehavior,
-            Enabled: distribution.DistributionConfig?.Enabled,
-            // ViewerCertificate: {
-            //   ACMCertificateArn: certificateArn,
-            //   SSLSupportMethod: 'sni-only',
-            //   MinimumProtocolVersion: 'TLSv1.2_2021',
-            // },
-          },
-        }),
-      )
-    } catch (error) {
-      if (!(error instanceof CNAMEAlreadyExists)) throw error
-
-      throw new UnprocessableEntityException('Duplicate domain. Domain in use by cloudfront')
-    }
-  }
 }
