d140cb9e364f0d171a05b0e0db049a6205bb67e8
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2025-07-21 02:33:20 +0100

    fix: reset pagination on filter change

--- packages/rc-mfa-config/src/components/admin/index.tsx
+++ packages/rc-mfa-config/src/components/admin/index.tsx
@@ -47,8 +47,18 @@ export interface UserFilters {
 }
 
 export const handleSetAdminFilters =
-  (setUserSearch: Dispatch<SetStateAction<UserFilters>>, watch: UseFormWatch<UserFilters>) => () => {
-    const subscription = watch(debounce(setUserSearch, 200))
+  (
+    setUserSearch: Dispatch<SetStateAction<UserFilters>>,
+    watch: UseFormWatch<UserFilters>,
+    setPageNumber: Dispatch<number>,
+  ) =>
+  () => {
+    const subscription = watch(
+      debounce((values) => {
+        setPageNumber(1)
+        setUserSearch(values)
+      }, 200),
+    )
     return () => subscription.unsubscribe()
   }
 
@@ -108,7 +118,7 @@ export const AdminPage: FC = () => {
   const orgId = adminOrgs.length === 1 ? adminOrgs[0].organisationId : null
   const orgName = (users?._embedded && users._embedded[0] && users._embedded[0].organisationName) ?? null
 
-  useEffect(handleSetAdminFilters(setUserSearch, watch), [])
+  useEffect(handleSetAdminFilters(setUserSearch, watch, setPageNumber), [])
   useEffect(handleInitialUserOrgSet(setOrganisationId, orgId), [orgId])
 
   return (
