e627d4d05f11483f75e6e174e17324d7aee5a843
Josh Balfour <josh@joshbalfour.co.uk>
2025-09-17 10:11:17 +0100

    remove sandbox selection toggle

--- packages/developer-portal/src/components/settings/profile/profile-form.tsx
+++ packages/developer-portal/src/components/settings/profile/profile-form.tsx
@@ -1,26 +1,10 @@
 import React, { FC, useEffect } from 'react'
-import {
-  BodyText,
-  Button,
-  ButtonGroup,
-  elMb11,
-  elMb6,
-  FormLayout,
-  InputError,
-  InputGroup,
-  InputWrap,
-  Label,
-  PersistentNotification,
-  Subtitle,
-  ToggleRadio,
-  ToggleRadioOption,
-} from '@reapit/elements'
+import { BodyText, Button, ButtonGroup, elMb11, FormLayout, InputGroup, InputWrap, Subtitle } from '@reapit/elements'
 import { useReapitConnect } from '@reapit/connect-session'
-import { selectIsUserOrUserAdmin } from '../../../utils/auth'
-import { useReapitGet, useReapitUpdate } from '@reapit/use-reapit-data'
+import { useReapitUpdate } from '@reapit/use-reapit-data'
 import { reapitConnectBrowserSession } from '../../../core/connect-session'
 import { Marketplace } from '@reapit/foundations-ts-definitions'
-import { GetActionNames, getActions, UpdateActionNames, updateActions } from '@reapit/use-reapit-data'
+import { UpdateActionNames, updateActions } from '@reapit/use-reapit-data'
 import { useForm } from 'react-hook-form'
 import { useGlobalState } from '../../../core/use-global-state'
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -43,18 +27,7 @@ export const handleRefreshMember = (refreshMember: () => void, updateMemberSucce
 export const ProfileForm: FC = () => {
   const { globalDataState, globalRefreshCurrentMember } = useGlobalState()
   const { connectSession } = useReapitConnect(reapitConnectBrowserSession)
-  const clientId = connectSession?.loginIdentity.clientId
-  const orgName = connectSession?.loginIdentity.orgName
-  const isUserOrUserAdmin = selectIsUserOrUserAdmin(connectSession)
   const { currentMember, currentDeveloper } = globalDataState
-  const isClient = clientId && isUserOrUserAdmin
-  const hasProducts = process.env.swaggerWhitelist.includes(currentDeveloper?.id as string)
-
-  const [sandboxes] = useReapitGet<Marketplace.SandboxModelPagedResult>({
-    reapitConnectBrowserSession,
-    action: getActions[GetActionNames.getSandboxes],
-  })
-
   const [memberUpdating, , updateMember, updateMemberSuccess] = useReapitUpdate<Marketplace.UpdateMemberModel, boolean>(
     {
       reapitConnectBrowserSession,
@@ -133,38 +106,6 @@ export const ProfileForm: FC = () => {
           </InputWrap>
         </FormLayout>
       </div>
-      {hasProducts && (
-        <>
-          <Subtitle hasNoMargin>Customer Data</Subtitle>
-          <BodyText hasGreyText>
-            You can choose which sandbox you wish to view based on your Reapit Product. This is specific and only
-            associated to your developer profile.
-          </BodyText>
-          <FormLayout className={elMb6}>
-            <InputWrap>
-              <InputGroup>
-                <ToggleRadio
-                  hasGreyBg
-                  {...register('sandboxId')}
-                  options={
-                    (sandboxes?.data?.map((sandbox) => ({
-                      id: sandbox.id ?? '',
-                      text: sandbox.name ?? '',
-                      value: sandbox.id ?? '',
-                      isChecked: currentMember?.sandboxId === sandbox.id,
-                    })) as ToggleRadioOption[]) ?? []
-                  }
-                />
-                <Label>Choose Sandbox</Label>
-                {errors.sandboxId?.message && <InputError message={errors.sandboxId?.message} />}
-              </InputGroup>
-            </InputWrap>
-          </FormLayout>
-          <PersistentNotification className={elMb11} isInline isExpanded isFullWidth intent="primary">
-            Please note, you will need to log out and log back in again to see this change take effect
-          </PersistentNotification>
-        </>
-      )}
       <ButtonGroup>
         <Button intent="primary" type="submit" disabled={memberUpdating} loading={memberUpdating}>
           Save Changes
