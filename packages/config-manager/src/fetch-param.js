d7944a119be45903018a4aca4ea52f1e8e24ea10
Josh Balfour <josh@joshbalfour.co.uk>
2025-10-07 13:11:42 +0100

    config-management: update aws sdk to v3

--- packages/config-manager/src/fetch-param.js
+++ packages/config-manager/src/fetch-param.js
@@ -1,34 +1,29 @@
 #!/usr/bin/env node
-const AWS = require('aws-sdk')
+const { SSMClient, GetParameterCommand } = require('@aws-sdk/client-ssm')
 const chalk = require('chalk')
 const fs = require('fs')
 const { spawnSync } = require('child_process')
 const { getParamAndFileName } = require('./utils')
 
-AWS.config.update({ region: 'eu-west-2' })
+const ssm = new SSMClient({ region: 'eu-west-2' })
 
-const ssm = new AWS.SSM()
-
-const fetchParam = (cliArgs) => {
+const fetchParam = async (cliArgs) => {
+  const { format } = cliArgs
+  const { fileName, paramName } = getParamAndFileName(cliArgs)
+  
   try {
-    const { format } = cliArgs
-    const { fileName, paramName } = getParamAndFileName(cliArgs)
     console.log(chalk.bold.blue('Fetching param: ', paramName))
-    return new Promise((resolve) => {
-      const options = { Name: paramName, WithDecryption: true }
-      ssm.getParameter(options, (err, data) => {
-        if (err) {
-          throw new Error(`Something went wrong when fetching your param: ${paramName} ${err.code}`)
-        }
-        const config = (data && data.Parameter && data.Parameter.Value) || {}
-        console.log(chalk.bold.green(`Successfully fetched ${paramName}`))
-        fs.writeFileSync(fileName, config)
-        format !== 'string' && spawnSync(`npx prettier --write ${fileName}`, { shell: false })
-        resolve()
-      })
-    })
+    
+    const command = new GetParameterCommand({ Name: paramName, WithDecryption: true })
+    const data = await ssm.send(command)
+    
+    const config = (data && data.Parameter && data.Parameter.Value) || {}
+    console.log(chalk.bold.green(`Successfully fetched ${paramName}`))
+    fs.writeFileSync(fileName, config)
+    format !== 'string' && spawnSync(`npx prettier --write ${fileName}`, { shell: false })
   } catch (err) {
-    console.log(chalk.red.bold('Error:', err.message))
+    console.log(chalk.red.bold('Error:', err.message || `Something went wrong when fetching your param: ${paramName}`))
+    throw err
   }
 }
 
